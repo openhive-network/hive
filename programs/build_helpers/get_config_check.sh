5679a2a23a593851c615f07b31a6936531244d25
kmochocki <kmochocki@syncad.com>
2020-04-02 23:23:18 +0200

    sh diff test repaired

--- programs/build_helpers/get_config_check.sh
+++ programs/build_helpers/get_config_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 diff -u \
-   <(cat libraries/protocol/get_config.cpp | grep 'result[[]".*"' | cut -d '"' -f 2 | sort | uniq) \
+   <(cat libraries/protocol/get_config.cpp | grep 'result[[]".*"' | cut -d '"' -f 2 | uniq | sed 's/HIVE_/STEEM_/g' | sed 's/HBD_/SBD_/g' | 	sed 's/_HBD/_SBD/g'| sort) \
    <(cat libraries/protocol/include/steem/protocol/config.hpp | grep '[#]define\s\+[A-Z0-9_]\+' | cut -d ' ' -f 2 | sort | uniq)
 exit $?
 
