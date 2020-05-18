8a8e32f716b32fdc73c29a8b6d1847625f43fffe
Tim Cliff <timcliff.steem.witness@gmail.com>
2020-05-18 10:46:49 -0400

    Update Seed Nodes

--- libraries/plugins/p2p/include/steem/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/steem/plugins/p2p/p2p_default_seeds.hpp
@@ -8,28 +8,19 @@ namespace steem{ namespace plugins { namespace p2p {
 const std::vector< std::string > default_seeds;
 #else
 const std::vector< std::string > default_seeds = {
-   "seed-east.steemit.com:2001",          // steemit
-   "seed-central.steemit.com:2001",       // steemit
-   "seed-west.steemit.com:2001",          // steemit
-   "52.74.152.79:2001",                   // smooth.witness
    "anyx.io:2001",                        // anyx
-   "seed.liondani.com:2016",              // liondani
-   "gtg.steem.house:2001",                // gtg
-   "seed.jesta.us:2001",                  // jesta
-   "lafonasteem.com:2001",                // lafona
-   "steem-seed.altcap.io:40696",          // ihashfury
-   "seed.roelandp.nl:2001",               // roelandp
-   "seed.timcliff.com:2001",              // timcliff
-   "steemseed.clayop.com:2001",           // clayop
-   "seed.steemviz.com:2001",              // ausbitbank
-   "steem-seed.lukestokes.info:2001",     // lukestokes
-   "seed.steemian.info:2001",             // drakos
-   "seed.followbtcnews.com:2001",         // followbtcnews
+   "hive-seed.arcange.eu:2001",           // arcange
+   "hive-seed.lukestokes.info:2001",      // lukestokes.mhth
+   "hived.splinterlands.com:2001",        // aggroed
+   "hiveseed-se.privex.io:2001",          // privex
    "node.mahdiyari.info:2001",            // mahdiyari
-   "seed.riversteem.com:2001",            // riverhead
-   "seed1.blockbrothers.io:2001",         // blockbrothers
-   "steemseed-fin.privex.io:2001",        // privex
-   "seed.yabapmatt.com:2001"              // yabapmatt
+   "rpc.ausbit.dev:2001",                 // ausbitbank
+   "seed.buildteam.io:2001",              // thecryptodrive
+   "seed.chitty.me:2001",                 // chitty
+   "seed.hivekings.com:2001",             // drakos
+   "seed.liondani.com:2016",              // liondani
+   "seed.openhive.network:2001",          // gtg
+   "seed.roelandp.nl:2001"                // roelandp
 };
 #endif
 
