f3fb0bbf4aae60061d232b60964664df315c46d6
Krzysztof Leśniak <klesniak@syncad.com>
2020-06-09 13:59:10 +0200

    remove extra semicolon

--- libraries/fc/include/fc/variant.hpp
+++ libraries/fc/include/fc/variant.hpp
@@ -657,4 +657,4 @@ namespace fc
 #include <fc/reflect/reflect.hpp>
 FC_REFLECT_TYPENAME( fc::variant )
 FC_REFLECT_ENUM( fc::variant::type_id, (null_type)(int64_type)(uint64_type)(double_type)(bool_type)(string_type)(array_type)(object_type)(blob_type) )
-FC_REFLECT( fc::blob, (data) );
+FC_REFLECT( fc::blob, (data) )
