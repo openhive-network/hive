b8904165dfaa0a1939ad35e72031b134d005620f
Bartek Wrona <wrona@syncad.com>
2020-06-26 15:46:17 -0400

    - Fixed bug in enum_virtual_ops paging (returning the same ID of operation to resume from for subsequent page. - Eliminated linear complexity from search when operation_begin has been specified

--- libraries/plugins/apis/account_history_api/include/hive/plugins/account_history_api/account_history_api.hpp
+++ libraries/plugins/apis/account_history_api/include/hive/plugins/account_history_api/account_history_api.hpp
@@ -34,6 +34,7 @@ struct api_operation_object
   uint32_t                               trx_in_block = 0;
   uint32_t                               op_in_trx = 0;
   uint32_t                               virtual_op = 0;
+  uint64_t                               operation_id = 0;
   fc::time_point_sec                     timestamp;
   hive::protocol::operation             op;
 
@@ -76,6 +77,33 @@ struct get_account_history_return
   std::map< uint32_t, api_operation_object > history;
 };
 
+enum enum_vops_filter : uint32_t
+{
+  fill_convert_request_operation          = 0x000001,
+  author_reward_operation                 = 0x000002,
+  curation_reward_operation               = 0x000004,
+  comment_reward_operation                = 0x000008,
+  liquidity_reward_operation              = 0x000010,
+  interest_operation                      = 0x000020,
+  fill_vesting_withdraw_operation         = 0x000040,
+  fill_order_operation                    = 0x000080,
+  shutdown_witness_operation              = 0x000100,
+  fill_transfer_from_savings_operation    = 0x000200,
+  hardfork_operation                      = 0x000400,
+  comment_payout_update_operation         = 0x000800,
+  return_vesting_delegation_operation     = 0x001000,
+  comment_benefactor_reward_operation     = 0x002000,
+  producer_reward_operation               = 0x004000,
+  clear_null_account_balance_operation    = 0x008000,
+  proposal_pay_operation                  = 0x010000,
+  sps_fund_operation                      = 0x020000,
+  hardfork_hive_operation                 = 0x040000,
+  hardfork_hive_restore_operation         = 0x080000,
+  delayed_voting_operation                = 0x100000,
+  consolidate_treasury_balance_operation  = 0x200000,
+  effective_comment_vote_operation        = 0x400000
+};
+
 /** Allows to specify range of blocks to retrieve virtual operations for.
   *  \param block_range_begin - starting block number (inclusive) to search for virtual operations
   *  \param block_range_end   - last block number (exclusive) to search for virtual operations
@@ -88,17 +116,32 @@ struct enum_virtual_ops_args
   uint32_t block_range_begin = 1;
   uint32_t block_range_end = 2;
 
-  fc::optional< uint32_t > operation_begin;
+  fc::optional<bool> group_by_block;
+  fc::optional< uint64_t > operation_begin;
   fc::optional< uint32_t > limit;
-  fc::optional< fc::flat_set< std::string > > filter;
+  fc::optional< uint32_t > filter;
+};
+
+struct ops_array_wrapper
+{
+  ops_array_wrapper(uint32_t _block) : block(_block) {}
+
+  uint32_t block = 0;
+  fc::time_point_sec                timestamp;
+  std::vector<api_operation_object> ops;
+
+  bool operator < (const ops_array_wrapper& rhs) const
+    {
+    return block < rhs.block;
+    }
 };
 
 struct enum_virtual_ops_return
 {
   vector<api_operation_object> ops;
- 
+  std::set<ops_array_wrapper> ops_by_block;
   uint32_t                     next_block_range_begin = 0;
-  uint32_t                     next_operation_begin   = 0;
+  uint64_t                     next_operation_begin   = 0;
 };
 
 
@@ -122,7 +165,7 @@ class account_history_api
 } } } // hive::plugins::account_history
 
 FC_REFLECT( hive::plugins::account_history::api_operation_object,
-  (trx_id)(block)(trx_in_block)(op_in_trx)(virtual_op)(timestamp)(op) )
+  (trx_id)(block)(trx_in_block)(op_in_trx)(virtual_op)(timestamp)(op)(operation_id) )
 
 FC_REFLECT( hive::plugins::account_history::get_ops_in_block_args,
   (block_num)(only_virtual) )
@@ -140,7 +183,9 @@ FC_REFLECT( hive::plugins::account_history::get_account_history_return,
   (history) )
 
 FC_REFLECT( hive::plugins::account_history::enum_virtual_ops_args,
-  (block_range_begin)(block_range_end)(operation_begin)(limit)(filter) )
+  (block_range_begin)(block_range_end)(group_by_block)(operation_begin)(limit)(filter) )
+
+FC_REFLECT( hive::plugins::account_history::ops_array_wrapper, (block)(timestamp)(ops) )
 
 FC_REFLECT( hive::plugins::account_history::enum_virtual_ops_return,
-  (ops)(next_block_range_begin)(next_operation_begin) )
+  (ops)(ops_by_block)(next_block_range_begin)(next_operation_begin) )
