62a1d6b5732b578d69060166d6e0f66757057480
kmochocki <kmochocki@syncad.com>
2020-07-16 10:49:44 +0200

    new tests for missing directory and saving to same directory

--- tests/functional/python_tests/hived/snapshot_2.py
+++ tests/functional/python_tests/hived/snapshot_2.py
@@ -9,6 +9,7 @@ sys.path.append("../../")
 
 import hive_utils
 from hive_utils.resources.configini import config as configuration
+from hive_utils.common import wait_n_blocks
 
 
 parser = argparse.ArgumentParser()
@@ -35,7 +36,7 @@ path_to_config = os.path.join(work_dir, config_file_name)
 
 # snapshot dir
 snapshot_root = os.path.join(work_dir, "snapshots")
-os.mkdir(snapshot_root)
+# os.mkdir(snapshot_root)
 
 # (optional) setting up block log
 if args.block_log_path:
@@ -53,7 +54,10 @@ config.plugin = config.plugin + " state_snapshot"	# this plugin is required
 # config generation
 config.generate(path_to_config)
 
-def wait_for_node(node_to_wait, exit_msg, msg):
+def get_base_hv_args():
+	return [ "--stop-replay-at-block", str(args.blocks), "--exit-after-replay" ].copy()
+
+def wait_for_node(node_to_wait : hive_utils.hive_node.HiveNode, exit_msg, msg):
 	from time import sleep
 	print(msg)
 	with node_to_wait:
@@ -61,61 +65,80 @@ def wait_for_node(node_to_wait, exit_msg, msg):
 		while not ret.find(exit_msg):
 			sleep(0.25)
 			ret = node_to_wait.get_output().decode('utf-8')
+	print("node closed")
 
-base_hv_args = [ "--stop-replay-at-block", str(args.blocks), "--exit-after-replay" ]
-hv_args = base_hv_args.copy()
+def dump_snapshot(Node, snapshot_name):
+# setup for snapshot
+	hv_args = get_base_hv_args()
+	hv_args.extend(["--dump-snapshot", snapshot_name])
+	Node.hived_args = hv_args
+# creating snapshot
+	wait_for_node(Node, "Snapshot generation finished", "creating snapshot '{}' ...".format(snapshot_name))
+
+def load_snapshot(Node, snapshot_name):
+# setup for loading snapshot
+	hv_args = get_base_hv_args()
+	hv_args.extend(["--load-snapshot", snapshot_name])
+	Node.hived_args = hv_args
+	os.remove(os.path.join(blockchain_dir, "shared_memory.bin"))
+# loading snapshot
+	wait_for_node(Node, "Validate_invariants finished", "loading snapshot '{}' ...".format(snapshot_name))
+
+def run_for_n_blocks(Node, blocks : int, additional_args : list = []):
+	args.blocks += blocks
+	Node.hived_args = get_base_hv_args()
+	if len(additional_args) > 0:
+		Node.hived_args.extend(additional_args)
+	wait_for_node(Node, "exited cleanly", "waiting for {} blocks...".format(int(args.blocks)))
+
+
+def check_success(node):
+	assert node.last_returncode == 0
+
+hv_args = get_base_hv_args()
 hv_args.append("--replay-blockchain")
 
 # setup for replay
 node = hive_utils.hive_node.HiveNode(
 	args.hived,
 	work_dir, 
-	hv_args.copy()
+	hv_args
 )
 
 # replay
 wait_for_node(node, "exited cleanly", "waiting for replay of {} blocks...".format(int(args.blocks)))
+check_success(node)
 print("replay completed, creating snapshot")
 
-# setup for first snapshot
-first_snapshot_name = "first_snapshot"
-hv_args = base_hv_args.copy()
-hv_args.extend(["--dump-snapshot", first_snapshot_name])
-node.hived_args = hv_args.copy()
 
-# creating snapshot
-wait_for_node(node, "Snapshot generation finished", "creating snapshot '{}' ...".format(first_snapshot_name))
+# try to create snapshot, while directory not exist
+assert not os.path.exists(snapshot_root)
+dump_snapshot(node, "snap_1")
+check_success(node)
+assert os.path.exists(snapshot_root)
+assert os.path.exists(os.path.join(snapshot_root, "snap_1"))
 
-# setup for loading snapshot
-hv_args = base_hv_args.copy()
-hv_args.extend(["--load-snapshot", first_snapshot_name])
-node.hived_args = hv_args.copy()
-os.remove(os.path.join(blockchain_dir, "shared_memory.bin"))
+# load snapshot
+load_snapshot(node, "snap_1")
+check_success(node)
 
-# loading snapshot
-wait_for_node(node, "Validate_invariants finished", "creating snapshot '{}' ...".format(first_snapshot_name))
+# check is replaying 
+run_for_n_blocks(node, 10000, ["--replay-blockchain"])
+check_success(node)
 
-# setup for redumping snapshot
-second_snapshot_name = "second_snapshot"
-hv_args = base_hv_args.copy()
-hv_args.extend(["--dump-snapshot", second_snapshot_name])
-node.hived_args = hv_args.copy()
+# dump to same directory
+dump_snapshot(node, "snap_1")
+check_success(node)
 
-# redumpping snapshot
-wait_for_node(node, "Snapshot generation finished", "creating snapshot '{}' ...".format(second_snapshot_name))
+# load snapshot
+load_snapshot(node, "snap_1")
+check_success(node)
 
-path_to_first_snapshot = os.path.join(snapshot_root, first_snapshot_name)
-path_to_second_snapshot = os.path.join(snapshot_root, second_snapshot_name)
+# check is replaying 
+run_for_n_blocks(node, 10000, ["--replay-blockchain"])
+check_success(node)
 
-
-from hive_utils.common import compare_snapshots
-miss_list = compare_snapshots(path_to_first_snapshot, path_to_second_snapshot)
-
-if len(miss_list) is not 0:
-	for val in miss_list:
-		print("checksum missmatch: '{}'".format(key))
-else:
-	print("all checksums matches")
+print("success")
 
 
 if not args.leave:
@@ -125,4 +148,4 @@ if not args.leave:
 else:
 	print("datadir not deleted: {}".format(work_dir))
 
-exit(len(miss_list))
+exit(0)
