c63edd7ee99137afbfdc684f769fb3cd1cbd28c3
Filip Kaczmarek <fkaczmarek@syncad.com>
2020-07-23 06:54:06 -0400

    Update deploy.sh

--- ciscripts/deploy.sh
+++ ciscripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$CI_PROJECT_DIR/deploy/hived --replay-blockchain --set-benchmark-interval 100000 --stop-replay-at-block ${STOP_DEPLOY_AT} --advanced-benchmark --dump-memory-details  -d $CI_PROJECT_DIR/deploy --shared-file-dir $CI_PROJECT_DIR/deploy 2>&1 | tee $CI_PROJECT_DIR/deploy/replay.log
+$CI_PROJECT_DIR/$CI_ENVIRONMENT_NAME/deploy/hived --replay-blockchain --set-benchmark-interval 100000 --stop-replay-at-block ${STOP_DEPLOY_AT} --advanced-benchmark --dump-memory-details  -d $CI_PROJECT_DIR/$CI_ENVIRONMENT_NAME/deploy --shared-file-dir $CI_PROJECT_DIR/$CI_ENVIRONMENT_NAME/deploy 2>&1 | tee $CI_PROJECT_DIR/$CI_ENVIRONMENT_NAME/deploy/replay.log
