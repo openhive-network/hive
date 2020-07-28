624ae6cf7f518f6ea6f973d1a158a4447f58a199
Filip Kaczmarek <fkaczmarek@syncad.com>
2020-07-28 10:05:56 -0400

    Update deploy.sh

--- ciscripts/deploy.sh
+++ ciscripts/deploy.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-screen -L -Logfile deploy.log -dmS hived-deploy-$CI_ENVIRONMENT_NAME $CI_PROJECT_DIR/sync/$CI_ENVIRONMENT_NAME/hived --replay-blockchain --set-benchmark-interval 100000 --stop-replay-at-block ${STOP_REPLAY_AT} --advanced-benchmark --dump-memory-details  -d $CI_PROJECT_DIR/sync/$CI_ENVIRONMENT_NAME --shared-file-dir $CI_PROJECT_DIR/sync/$CI_ENVIRONMENT_NAME
+HTTPS_PORT=${HTTPS_ENDPOINT_PORT}
+WS_PORT=${WS_ENDPOINT_PORT}
+if nc -z 127.0.0.1 $HTTPS_PORT && nc -z 127.0.0.1 $WS_PORT
+  then
+    screen -S hived-deploy-$CI_ENVIRONMENT_NAME -X quit
+  else
+    screen -L -Logfile deploy.log -dmS hived-deploy-$CI_ENVIRONMENT_NAME $CI_PROJECT_DIR/sync/$CI_ENVIRONMENT_NAME/hived --replay-blockchain --set-benchmark-interval 100000 --stop-replay-at-block ${STOP_REPLAY_AT} --webserver-http-endpoint 127.0.0.1:$HTTP_PORT --webserver-ws-endpoint 127.0.0.1:$WS_PORT --advanced-benchmark --dump-memory-details  -d $CI_PROJECT_DIR/sync/$CI_ENVIRONMENT_NAME --shared-file-dir $CI_PROJECT_DIR/sync/$CI_ENVIRONMENT_NAME
+fi
+
+if nc -z 127.0.0.1 $HTTPS_PORT && nc -z 127.0.0.1 $WS_PORT
+  then
+    echo "Hive server started successfully..."
+    echo "Hive server http endpoint: 127.0.0.1:$HTTPS_PORT"
+    echo "Hive server websocket endpoint: 127.0.0.1:$WS_PORT"
+  else
+    echo "Hive server start failure..."
+fi
