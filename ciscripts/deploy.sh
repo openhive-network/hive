cce138607a83448a2a000e97f5e28f7eb0e229ed
Filip Kaczmarek <fkaczmarek@syncad.com>
2020-07-31 06:06:27 -0400

    Update deploy.sh

--- ciscripts/deploy.sh
+++ ciscripts/deploy.sh
@@ -4,9 +4,9 @@ WS_PORT=${WS_ENDPOINT_PORT}
 if nc -z 127.0.0.1 $HTTPS_PORT && nc -z 127.0.0.1 $WS_PORT
   then
     screen -S hived-deploy-$CI_ENVIRONMENT_NAME -X quit
-  else
-    screen -L -Logfile deploy.log -dmS hived-deploy-$CI_ENVIRONMENT_NAME $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME/hived --replay-blockchain --set-benchmark-interval 100000 --stop-replay-at-block ${STOP_REPLAY_AT} --webserver-http-endpoint 127.0.0.1:$HTTP_PORT --webserver-ws-endpoint 127.0.0.1:$WS_PORT --advanced-benchmark --dump-memory-details  -d $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME --shared-file-dir $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME
 fi
+    
+screen -L -Logfile deploy.log -dmS hived-deploy-$CI_ENVIRONMENT_NAME $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME/hived --replay-blockchain --set-benchmark-interval 100000 --stop-replay-at-block ${STOP_REPLAY_AT} --webserver-http-endpoint 127.0.0.1:$HTTP_PORT --webserver-ws-endpoint 127.0.0.1:$WS_PORT --advanced-benchmark --dump-memory-details  -d $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME --shared-file-dir $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME
 
 if nc -z 127.0.0.1 $HTTPS_PORT && nc -z 127.0.0.1 $WS_PORT
   then
