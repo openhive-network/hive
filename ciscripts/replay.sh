dd591c368788d4c9f4efed09945773009e01b5bf
Filip Kaczmarek <fkaczmarek@syncad.com>
2020-08-04 09:38:30 -0400

    Update replay.sh

--- ciscripts/replay.sh
+++ ciscripts/replay.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME/hived --replay-blockchain --set-benchmark-interval 100000 --stop-replay-at-block ${STOP_REPLAY_AT} --exit-after-replay --advanced-benchmark --dump-memory-details  -d $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME --shared-file-dir $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME 2>&1 | tee $CI_PROJECT_DIR/replay/$CI_ENVIRONMENT_NAME/replay.log
+$CI_PROJECT_DIR/replay/hived --replay-blockchain --set-benchmark-interval 100000 --stop-replay-at-block ${STOP_REPLAY_AT} --exit-after-replay --advanced-benchmark --dump-memory-details  -d $CI_PROJECT_DIR/replay --shared-file-dir $CI_PROJECT_DIR/replay 2>&1 | tee $CI_PROJECT_DIR/replay/replay.log
