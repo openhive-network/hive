0b1b3b65a5f2b4cd16bed0564f87c80caa5bbd20
Wieslaw Kedzierski <wkedzierski@syncad.com>
2020-10-31 23:02:03 +0100

    [WK] Enable cli wallet tests in cmake.

--- tests/functional/python_tests/cli_wallet/tests/006_cli_wallet_help.py
+++ tests/functional/python_tests/cli_wallet/tests/006_cli_wallet_help.py
@@ -9,6 +9,7 @@ from utils.logger     import log, init_logger
 if __name__ == "__main__":
     try:
         init_logger(__file__)
+        log.info("Starting test: {0}".format(__file__))
         error = False
         output = subprocess.run([args.path+"/cli_wallet", "--help"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output_stdout = output.stdout.decode('utf-8')
