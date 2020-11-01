be2617d90f1dd14733fa4a04277f705c0b848422
Wieslaw Kedzierski <wkedzierski@syncad.com>
2020-11-01 22:23:11 +0100

    [WK] Cli wallet changes: * add junit reports, * refactor tests to use conteztmanager to avoid code duplication,

--- tests/functional/python_tests/cli_wallet/tests/000_create_proposal.py
+++ tests/functional/python_tests/cli_wallet/tests/000_create_proposal.py
@@ -6,14 +6,10 @@ from utils.cli_wallet import CliWallet
 from utils.logger     import log, init_logger
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    with Test(__file__):
-=======
     try:
         init_logger(__file__)
         log.info("Starting test: {0}".format(__file__))
         error = False
->>>>>>> [WK] Enable cli wallet tests in cmake.
         wallet = CliWallet( args.path,
                             args.server_rpc_endpoint,
                             args.cert_auth,
