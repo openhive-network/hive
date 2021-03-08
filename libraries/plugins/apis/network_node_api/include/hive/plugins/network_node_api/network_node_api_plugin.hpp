6723a034c68668a7a920a20f097f335396ca9f4a
Gandalf-the-Grey <Gandalf-the-Grey@users.noreply.github.com>
2021-03-08 09:44:16 +0000

    plugin for p2p network connectivity control / debugging #124

---
+++ libraries/plugins/apis/network_node_api/include/hive/plugins/network_node_api/network_node_api_plugin.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <hive/plugins/json_rpc/json_rpc_plugin.hpp>
+#include <hive/plugins/chain/chain_plugin.hpp>
+#include <hive/plugins/p2p/p2p_plugin.hpp>
+
+#include <appbase/application.hpp>
+
+#define HIVE_NETWORK_NODE_API_PLUGIN_NAME "network_node_api"
+
+namespace hive { namespace plugins { namespace network_node_api {
+
+using namespace appbase;
+
+class network_node_api_plugin : public appbase::plugin< network_node_api_plugin >
+{
+public:
+   APPBASE_PLUGIN_REQUIRES(
+      (hive::plugins::json_rpc::json_rpc_plugin)
+      (hive::plugins::chain::chain_plugin)
+      (hive::plugins::p2p::p2p_plugin)
+   )
+
+   network_node_api_plugin();
+   virtual ~network_node_api_plugin();
+
+   static const std::string& name() { static std::string name = HIVE_NETWORK_NODE_API_PLUGIN_NAME; return name; }
+
+   virtual void set_program_options( options_description& cli, options_description& cfg ) override;
+   virtual void plugin_initialize( const variables_map& options ) override;
+   virtual void plugin_startup() override;
+   virtual void plugin_shutdown() override;
+
+   std::shared_ptr< class network_node_api > api;
+};
+
+} } } // hive::plugins::network_node_api
---
+++ libraries/plugins/apis/network_node_api/network_node_api.cpp
@@ -0,0 +1,62 @@
+#include <hive/plugins/network_node_api/network_node_api.hpp>
+#include <hive/plugins/network_node_api/network_node_api_plugin.hpp>
+
+#include <appbase/application.hpp>
+
+namespace hive { namespace plugins { namespace network_node_api {
+
+namespace detail
+{
+   class network_node_api_impl
+   {
+      public:
+         network_node_api_impl() :
+            _p2p( appbase::app().get_plugin< hive::plugins::p2p::p2p_plugin >() )
+            //_chain( appbase::app().get_plugin< hive::plugins::chain::chain_plugin >() )
+         {}
+
+         DECLARE_API_IMPL(
+            (get_info)
+            (add_node)
+            (get_connected_peers)
+         )
+
+         hive::plugins::p2p::p2p_plugin&                      _p2p;
+         //hive::plugins::chain::chain_plugin&                  _chain;
+   };
+
+   DEFINE_API_IMPL( network_node_api_impl, get_info )
+   {
+      return _p2p.get_info();
+   }
+
+   DEFINE_API_IMPL( network_node_api_impl, add_node )
+   {
+      _p2p.add_node(args.endpoint);
+      add_node_return result;
+      return result;
+   }
+
+   DEFINE_API_IMPL( network_node_api_impl, get_connected_peers )
+   {
+      get_connected_peers_return result;
+      result.connected_peers = _p2p.get_connected_peers();
+      return result;
+   }
+
+} // detail
+
+network_node_api::network_node_api() : my( new detail::network_node_api_impl() )
+{
+   JSON_RPC_REGISTER_API( HIVE_NETWORK_NODE_API_PLUGIN_NAME );
+}
+
+network_node_api::~network_node_api() {}
+
+DEFINE_LOCKLESS_APIS( network_node_api,
+   (get_info)
+   (add_node)
+   (get_connected_peers)
+)
+
+} } } // hive::plugins::network_node_api
