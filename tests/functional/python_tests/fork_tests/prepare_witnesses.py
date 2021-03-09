92f64893057b9a5f5a1e214c0df98e404c1fe933
Piotr Batko <pbatko@syncad.com>
2021-03-09 09:23:14 +0100

    Change main net url

--- tests/functional/python_tests/fork_tests/prepare_witnesses.py
+++ tests/functional/python_tests/fork_tests/prepare_witnesses.py
@@ -370,7 +370,7 @@ if __name__ == "__main__":
         url = "http://localhost:8093"
         api_node_url = "http://localhost:58091"
 #        api_node_url = "http://localhost:8091"
-        mainNetUrl = "https://api.steemit.com"
+        mainNetUrl = "https://api.hive.blog"
 
         #initminer key
         set_password(url)
