37ebf5a475d0763f4a5848eeebbcecdf8c5a4a86
Piotr Batko <pbatko@syncad.com>
2021-03-11 14:26:19 +0100

    Add method for connecting wallet to node

--- tests/test_library/wallet.py
+++ tests/test_library/wallet.py
@@ -2,9 +2,13 @@ from pathlib import Path
 import subprocess
 import signal
 
+from node import Node
+
 
 class Wallet:
     def __init__(self, directory=Path()):
+        self.server_websocket_rpc_endpoint = None
+
         self.directory = directory
         self.executable_file_path = None
         self.stdout_file = None
@@ -21,6 +25,9 @@ class Wallet:
         if not self.executable_file_path:
             raise Exception('Missing executable')
 
+        if not self.server_websocket_rpc_endpoint:
+            raise Exception('Server websocket RPC endpoint not set, use Wallet.connect_to method')
+
         self.directory.mkdir()
 
         print('[Wallet] self.executable_file_path =', self.executable_file_path)
@@ -36,7 +43,7 @@ class Wallet:
             [
                 self.executable_file_path,
                 '--chain-id=04e8b5fc4bb4ab3c0ee3584199a2e584bfb2f141222b3a0d1c74e8a75ec8ff39',
-                '-s', 'ws://0.0.0.0:3903',
+                '-s', f'ws://{self.server_websocket_rpc_endpoint}',
                 '-d',
                 '-H', '0.0.0.0:3904',
                 '--rpc-http-allowip', '192.168.10.10',
@@ -48,6 +55,9 @@ class Wallet:
         )
 
         print(f'Wallet run with pid {self.process.pid}')
+
+    def connect_to(self, node: Node):
+        self.server_websocket_rpc_endpoint = node.get_webserver_ws_endpoints()[0]
 
     def close(self):
         self.process.send_signal(signal.SIGINT)
