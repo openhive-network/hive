96e204b8c32dc89cf5e4734f76a261a99d002562
Piotr Batko <pbatko@syncad.com>
2021-03-12 16:49:01 +0100

    Add key generator

---
+++ tests/test_library/key_generator.py
@@ -0,0 +1,22 @@
+import ast
+from pathlib import Path
+import subprocess
+
+
+class KeyGenerator:
+    def __init__(self, executable_path):
+        self.executable_path = Path(executable_path).absolute()
+
+    def generate_keys(self, account_name, secret='secret'):
+        if self.executable_path is None:
+            raise Exception('Missing executable, use KeyGenerator.set_executable_path')
+
+        if account_name == 'initminer':
+            return {
+                'private_key': '5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n',
+                'public_key': 'TST6LLegbAgLAy28EHrffBVuANFWcFgmqRMW13wBmTExqFE9SCkg4',
+                'account_name': account_name,
+            }
+
+        output = subprocess.check_output([self.executable_path, secret, account_name]).decode('utf-8')
+        return ast.literal_eval(output)[0]
