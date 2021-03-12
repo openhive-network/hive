0abd9bf2f69805ffed576640e69674f9eda0cc2b
Piotr Batko <pbatko@syncad.com>
2021-03-12 15:32:51 +0100

    Create two test networks and connect them

--- tests/functional/python_tests/network_creation_experiment/network_creation_experiment.py
+++ tests/functional/python_tests/network_creation_experiment/network_creation_experiment.py
@@ -2,20 +2,36 @@ from network import Network
 
 
 if __name__ == "__main__":
-    net = Network('TestNetwork')
-    net.set_hived_executable_file_path('../../../../build/programs/hived/hived')
-    net.set_wallet_executable_file_path('../../../../build/programs/cli_wallet/cli_wallet')
+    # Create first network
+    alpha_net = Network('Alpha', port_range=range(51000, 52000))
+    alpha_net.set_directory('experimental_network')
+    alpha_net.set_hived_executable_file_path('../../../../build/programs/hived/hived')
+    alpha_net.set_wallet_executable_file_path('../../../../build/programs/cli_wallet/cli_wallet')
 
-    init_node = net.add_node('InitNode')
+    init_node = alpha_net.add_node('InitNode')
     init_node.set_witness('initminer', '5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n')
 
-    net.add_node('TestNode0')
-    net.add_node('TestNode1')
-    net.add_node('TestNode2')
+    alpha_net.add_node('Node0')
+    alpha_net.add_node('Node1')
+    alpha_net.add_node('Node2')
 
-    net.run()
+    # Create second network
+    beta_net = Network('Beta', port_range=range(52000, 53000))
+    beta_net.set_directory('experimental_network')
+    beta_net.set_hived_executable_file_path('../../../../build/programs/hived/hived')
+    beta_net.set_wallet_executable_file_path('../../../../build/programs/cli_wallet/cli_wallet')
 
-    wallet = net.attach_wallet()
+    beta_net.add_node('Node0')
+    beta_net.add_node('Node1')
+    beta_net.add_node('Node2')
+
+    # Run
+    alpha_net.connect_with(beta_net)
+
+    alpha_net.run()
+    beta_net.run()
+
+    wallet = alpha_net.attach_wallet()
 
     while True:
         pass
