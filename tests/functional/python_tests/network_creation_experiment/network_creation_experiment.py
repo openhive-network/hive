d7037047d7cd171ab30934f4df13fea8bb789b21
Piotr Batko <pbatko@syncad.com>
2021-03-12 14:29:52 +0100

    Add initminer witness to InitNode

--- tests/functional/python_tests/network_creation_experiment/network_creation_experiment.py
+++ tests/functional/python_tests/network_creation_experiment/network_creation_experiment.py
@@ -24,6 +24,8 @@ if __name__ == "__main__":
     net.set_wallet_executable_file_path('../../../../build/programs/cli_wallet/cli_wallet')
 
     init_node = net.add_node('InitNode')
+    init_node.set_witness('initminer', '5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n')
+
     net.add_node('TestNode0')
     net.add_node('TestNode1')
     net.add_node('TestNode2')
