b4fe1165333e521822fa684ef62359469fd2fa42
Piotr Batko <pbatko@syncad.com>
2021-03-24 14:22:24 +0100

    Use better syntax for static member access

--- tests/test_library/witness.py
+++ tests/test_library/witness.py
@@ -2,10 +2,10 @@ class Witness:
     key_generator = None
 
     def __init__(self, name, secret='secret'):
-        if self.key_generator is None:
+        if Witness.key_generator is None:
             raise Exception('Missing key generator, set it in Witness.key_generator')
 
-        output = self.key_generator.generate_keys(name, secret)
+        output = Witness.key_generator.generate_keys(name, secret)
 
         self.name = name
         self.secret = secret
