2d29d2532b9febb8a895a1064a0880d75004fd24
Piotr Batko <pbatko@syncad.com>
2021-03-24 15:15:24 +0100

    Add help informations about missing environment variables

--- tests/test_library/paths_to_executables.py
+++ tests/test_library/paths_to_executables.py
@@ -7,9 +7,27 @@ KEY_GENERATOR_PATH_ENVIRONMENT_VARIABLE_NAME = 'KEY_GENERATOR_EXECUTABLE'
 
 def __get_environment_variable(variable_name):
     if variable_name not in os.environ:
-        raise Exception(f'Environment variable {variable_name} not set')
+        __show_message_about_missing_variables()
+        raise Exception(f'Environment variable {variable_name} not set. Read message printed above traceback for details.')
 
-    return Path(os.environ[variable_name])
+    return Path(os.path.expandvars(os.environ[variable_name]))
+
+def __show_message_about_missing_variables():
+    print('Variables listed below are missing. Set them in /etc/environment with correct paths.\n')
+
+    EXAMPLE_HIVE_BUILD_PATH_ENVIRONMENT_VARIABLE_NAME = 'HIVE_BUILD_PATH'
+    print(f'{EXAMPLE_HIVE_BUILD_PATH_ENVIRONMENT_VARIABLE_NAME}=\'/home/dev/hive/build\'')
+
+    if HIVED_PATH_ENVIRONMENT_VARIABLE_NAME not in os.environ:
+        print(f'{HIVED_PATH_ENVIRONMENT_VARIABLE_NAME}=\'${{{EXAMPLE_HIVE_BUILD_PATH_ENVIRONMENT_VARIABLE_NAME}}}/programs/hived/hived\'')
+
+    if CLI_WALLET_PATH_ENVIRONMENT_VARIABLE_NAME not in os.environ:
+        print(f'{CLI_WALLET_PATH_ENVIRONMENT_VARIABLE_NAME}=\'${{{EXAMPLE_HIVE_BUILD_PATH_ENVIRONMENT_VARIABLE_NAME}}}/programs/cli_wallet/cli_wallet\'')
+
+    if KEY_GENERATOR_PATH_ENVIRONMENT_VARIABLE_NAME not in os.environ:
+        print(f'{KEY_GENERATOR_PATH_ENVIRONMENT_VARIABLE_NAME}=\'${{{EXAMPLE_HIVE_BUILD_PATH_ENVIRONMENT_VARIABLE_NAME}}}/programs/util/get_dev_key\'')
+
+    print()
 
 def get_hived_path():
     return __get_environment_variable(HIVED_PATH_ENVIRONMENT_VARIABLE_NAME)
