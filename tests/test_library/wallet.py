2ea8dc02e5f255f652fbd0cfe06d22c8856f2ac2
Piotr Batko <pbatko@syncad.com>
2021-04-06 12:40:36 +0200

    Remove no longer needed comment

--- tests/test_library/wallet.py
+++ tests/test_library/wallet.py
@@ -110,9 +110,6 @@ class Wallet:
         while not self.connected_node.is_ws_listening():
             time.sleep(1)
 
-        # ./cli_wallet --chain-id=04e8b5fc4bb4ab3c0ee3584199a2e584bfb2f141222b3a0d1c74e8a75ec8ff39 -s ws://0.0.0.0:3903
-        #              -d -H 0.0.0.0:3904 --rpc-http-allowip 192.168.10.10 --rpc-http-allowip=127.0.0.1
-
         self.process = subprocess.Popen(
             [
                 str(self.executable_file_path),
