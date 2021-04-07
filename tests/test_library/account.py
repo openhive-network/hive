9394efa7c72e924b45ecb668253a9558b8e5386d
Piotr Batko <pbatko@syncad.com>
2021-04-07 11:20:05 +0200

    Rename witness -> account

--- tests/test_library/account.py
+++ tests/test_library/account.py
@@ -1,12 +1,12 @@
-class Witness:
+class Account:
     key_generator = None
 
     def __init__(self, name, secret='secret'):
-        if Witness.key_generator is None:
+        if Account.key_generator is None:
             from .key_generator import KeyGenerator
-            Witness.key_generator = KeyGenerator()
+            Account.key_generator = KeyGenerator()
 
-        output = Witness.key_generator.generate_keys(name, secret)
+        output = Account.key_generator.generate_keys(name, secret)
 
         self.name = name
         self.secret = secret
