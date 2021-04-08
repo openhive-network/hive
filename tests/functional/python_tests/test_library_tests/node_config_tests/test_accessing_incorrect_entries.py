ed9ba050b5b926857ab695d4988d0cc6596c3138
Piotr Batko <pbatko@syncad.com>
2021-04-08 14:47:08 +0200

    Refactor test with fixture

--- tests/functional/python_tests/test_library_tests/node_config_tests/test_accessing_incorrect_entries.py
+++ tests/functional/python_tests/test_library_tests/node_config_tests/test_accessing_incorrect_entries.py
@@ -3,23 +3,24 @@ import pytest
 from test_library.node_config import NodeConfig
 
 
-def test_getting_unset_but_correct_entry():
-    config = NodeConfig()
-    assert config.plugin is None
+@pytest.fixture
+def empty_config():
+    return NodeConfig()
 
 
-def test_getting_incorrect_entry():
-    config = NodeConfig()
+def test_getting_unset_but_correct_entry(empty_config):
+    assert empty_config.plugin is None
+
+
+def test_getting_incorrect_entry(empty_config):
     with pytest.raises(AttributeError):
-        assert config.incorrect_entry
+        assert empty_config.incorrect_entry
 
 
-def test_setting_correct_entry():
-    config = NodeConfig()
-    config.plugin = 'witness'
+def test_setting_correct_entry(empty_config):
+    empty_config.plugin = 'witness'
 
 
-def test_setting_incorrect_entry():
-    config = NodeConfig()
+def test_setting_incorrect_entry(empty_config):
     with pytest.raises(AttributeError):
-        config.incorrect_entry = 'value'
+        empty_config.incorrect_entry = 'value'
