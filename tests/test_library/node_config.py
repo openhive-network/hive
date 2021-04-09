a178ce8aa1ea9c76ed48e5f04ff1f42cb2849b7c
Piotr Batko <pbatko@syncad.com>
2021-04-09 09:44:12 +0200

    Extend config member list by values commented out in default config

--- tests/test_library/node_config.py
+++ tests/test_library/node_config.py
@@ -1,14 +1,23 @@
 class NodeConfig:
     __slots__ = [
         'entries',  # Temporary workaround
-        'log_appender', 'log_logger', 'backtrace', 'plugin', 'history_disable_pruning', 'account_history_rocksdb_path',
-        'block_data_export_file', 'block_log_info_print_interval_seconds', 'block_log_info_print_irreversible',
-        'block_log_info_print_file', 'shared_file_dir', 'shared_file_size', 'shared_file_full_threshold',
-        'shared_file_scale_rate', 'follow_max_feed_size', 'follow_start_feeds', 'market_history_bucket_size',
-        'market_history_buckets_per_size', 'rc_skip_reject_not_enough_rc', 'rc_compute_historical_rc',
-        'rc_start_at_block', 'snapshot_root_dir', 'statsd_batchsize', 'tags_start_promoted', 'tags_skip_startup_update',
-        'transaction_status_block_depth', 'transaction_status_track_after_block', 'webserver_thread_pool_size',
-        'enable_stale_production', 'required_participation', 'witness_skip_enforce_bandwidth',
+        'log_appender', 'log_console_appender', 'log_file_appender', 'log_logger', 'backtrace', 'plugin',
+        'account_history_track_account_range', 'track_account_range', 'account_history_whitelist_ops',
+        'history_whitelist_ops', 'account_history_blacklist_ops', 'history_blacklist_ops', 'history_disable_pruning',
+        'account_history_rocksdb_path', 'account_history_rocksdb_track_account_range',
+        'account_history_rocksdb_whitelist_ops', 'account_history_rocksdb_blacklist_ops', 'block_data_export_file',
+        'block_log_info_print_interval_seconds', 'block_log_info_print_irreversible', 'block_log_info_print_file',
+        'shared_file_dir', 'shared_file_size', 'shared_file_full_threshold', 'shared_file_scale_rate', 'checkpoint',
+        'flush_state_interval', 'cashout_logging_starting_block', 'cashout_logging_ending_block',
+        'cashout_logging_log_path_dir', 'debug_node_edit_script', 'edit_script', 'follow_max_feed_size',
+        'follow_start_feeds', 'log_json_rpc', 'market_history_bucket_size', 'market_history_buckets_per_size',
+        'p2p_endpoint', 'p2p_max_connections', 'seed_node', 'p2p_seed_node', 'p2p_parameters',
+        'rc_skip_reject_not_enough_rc', 'rc_compute_historical_rc', 'rc_start_at_block', 'rc_account_whitelist',
+        'snapshot_root_dir', 'statsd_endpoint', 'statsd_batchsize', 'statsd_whitelist', 'statsd_blacklist',
+        'tags_start_promoted', 'tags_skip_startup_update', 'transaction_status_block_depth',
+        'transaction_status_track_after_block', 'webserver_http_endpoint', 'webserver_unix_endpoint',
+        'webserver_ws_endpoint', 'webserver_enable_permessage_deflate', 'rpc_endpoint', 'webserver_thread_pool_size',
+        'enable_stale_production', 'required_participation', 'witness', 'private_key', 'witness_skip_enforce_bandwidth',
     ]
 
     class Entry:
