6da7390c3abbf8d0aced0b63b3993c8bc7710356
Piotr Batko <pbatko@syncad.com>
2021-04-12 15:49:48 +0200

    Move checking plugin correctness from node to config

--- tests/test_library/node_config_entry_types/__init__.py
+++ tests/test_library/node_config_entry_types/__init__.py
@@ -1,7 +1,8 @@
 from .boolean import Boolean
 from .integer import Integer
 from .list import List
+from .plugin import Plugin
 from .string import String
 from .untouched import Untouched
 
-__all__ = ['Boolean', 'Integer', 'List', 'String', 'Untouched']
+__all__ = ['Boolean', 'Integer', 'List', 'Plugin', 'String', 'Untouched']
