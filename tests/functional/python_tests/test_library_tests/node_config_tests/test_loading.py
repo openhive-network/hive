b37ec07d29d5a343e82b338e6a0d7ac6581181e6
Piotr Batko <pbatko@syncad.com>
2021-04-12 15:49:48 +0200

    Move checking plugin correctness from node to config

--- tests/functional/python_tests/test_library_tests/node_config_tests/test_loading.py
+++ tests/functional/python_tests/test_library_tests/node_config_tests/test_loading.py
@@ -24,3 +24,14 @@ def test_double_quoted_string_loading(config):
     assert config.account_history_rocksdb_path == 'blockchain/account-history-rocksdb-storage'
     assert config.shared_file_dir == 'blockchain'
     assert config.snapshot_root_dir == 'snapshot'
+
+
+def test_correct_plugins(config):
+    config.load_from_lines(['plugin = witness p2p account_by_key'])
+    assert all(plugin in config.plugin for plugin in ['witness', 'p2p', 'account_by_key'])
+
+
+def test_incorrect_plugins(config):
+    for incorrect_plugin in ['UNDEFINED_PLUGIN', 'witnness', 'p3p', '']:
+        with pytest.raises(Exception):
+            config.load_from_lines([f'plugin = {incorrect_plugin}'])
