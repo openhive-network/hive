92f20f0b057ce36547a84777931e83b20aac1937
Piotr Batko <pbatko@syncad.com>
2021-04-12 15:21:12 +0200

    Add complete list of supported plugins

--- tests/test_library/node.py
+++ tests/test_library/node.py
@@ -42,12 +42,12 @@ class Node:
 
     def add_plugin(self, plugin):
         supported_plugins = {
-            'account_by_key',
-            'account_by_key_api',
-            'condenser_api',
-            'network_broadcast_api',
-            'network_node_api',
-            'witness',
+            'account_by_key', 'account_by_key_api', 'account_history', 'account_history_api', 'account_history_rocksdb',
+            'block_api', 'block_data_export', 'block_log_info', 'chain', 'chain_api', 'comment_cashout_logging',
+            'condenser_api', 'database_api', 'debug_node', 'debug_node_api', 'follow', 'follow_api', 'json_rpc',
+            'market_history', 'market_history_api', 'network_broadcast_api', 'network_node_api', 'p2p', 'rc', 'rc_api',
+            'reputation', 'reputation_api', 'rewards_api', 'state_snapshot', 'stats_export', 'statsd', 'tags',
+            'tags_api', 'test_api', 'transaction_status', 'transaction_status_api', 'webserver', 'witness',
         }
 
         if plugin not in supported_plugins:
