a682d4c79bf5722fac1bde39226dd9b47bde20b9
Piotr Batko <pbatko@syncad.com>
2021-04-12 13:00:19 +0200

    Add config support for handling string entries
    Following entries are changed:
    - account_history_rocksdb_path
    - shared_file_dir
    - snapshot_root_dir

--- tests/functional/python_tests/test_library_tests/node_config_tests/test_loading.py
+++ tests/functional/python_tests/test_library_tests/node_config_tests/test_loading.py
@@ -11,3 +11,16 @@ def config():
 def test_single_value_loading(config):
     config.load_from_lines(['block_log_info_print_file = ILOG'])
     assert config.block_log_info_print_file == 'ILOG'
+
+
+def test_double_quoted_string_loading(config):
+    config.load_from_lines([
+        'account_history_rocksdb_path = "blockchain/account-history-rocksdb-storage"',
+        'shared_file_dir = "blockchain"',
+        'snapshot_root_dir = "snapshot"',
+    ])
+
+    # Output should not contain double quotes inside string
+    assert config.account_history_rocksdb_path == 'blockchain/account-history-rocksdb-storage'
+    assert config.shared_file_dir == 'blockchain'
+    assert config.snapshot_root_dir == 'snapshot'
