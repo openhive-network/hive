0772f29b6a6bf1bd906bc5fd39af720d3589e045
Piotr Batko <pbatko@syncad.com>
2021-04-12 15:49:48 +0200

    Move checking plugin correctness from node to config

--- tests/test_library/node.py
+++ tests/test_library/node.py
@@ -41,18 +41,6 @@ class Node:
         process.wait()
 
     def add_plugin(self, plugin):
-        supported_plugins = {
-            'account_by_key', 'account_by_key_api', 'account_history', 'account_history_api', 'account_history_rocksdb',
-            'block_api', 'block_data_export', 'block_log_info', 'chain', 'chain_api', 'comment_cashout_logging',
-            'condenser_api', 'database_api', 'debug_node', 'debug_node_api', 'follow', 'follow_api', 'json_rpc',
-            'market_history', 'market_history_api', 'network_broadcast_api', 'network_node_api', 'p2p', 'rc', 'rc_api',
-            'reputation', 'reputation_api', 'rewards_api', 'state_snapshot', 'stats_export', 'statsd', 'tags',
-            'tags_api', 'test_api', 'transaction_status', 'transaction_status_api', 'webserver', 'witness',
-        }
-
-        if plugin not in supported_plugins:
-            raise Exception(f'Plugin {plugin} is not supported')
-
         if not self.config.plugin:
             self.config.plugin = []
 
