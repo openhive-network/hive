7641ca3dbfa18e7fafc7ea63e221ababa4df93a0
Piotr Batko <pbatko@syncad.com>
2021-04-12 12:03:40 +0200

    Add some basic type config entries with tests

--- tests/test_library/node_config_entry_types/__init__.py
+++ tests/test_library/node_config_entry_types/__init__.py
@@ -1,3 +1,7 @@
+from .boolean import Boolean
+from .integer import Integer
+from .list import List
+from .string import String
 from .untouched import Untouched
 
-__all__ = ['Untouched']
+__all__ = ['Boolean', 'Integer', 'List', 'String', 'Untouched']
