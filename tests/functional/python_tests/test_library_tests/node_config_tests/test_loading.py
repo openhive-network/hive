1fec345da7e47df93c1acb6a28080a5f7e471e3d
Piotr Batko <pbatko@syncad.com>
2021-04-13 14:23:14 +0200

    Fix unreported wrong key in file when contain underscores

--- tests/functional/python_tests/test_library_tests/node_config_tests/test_loading.py
+++ tests/functional/python_tests/test_library_tests/node_config_tests/test_loading.py
@@ -9,15 +9,20 @@ def config():
 
 
 def test_single_value_loading(config):
-    config.load_from_lines(['block_log_info_print_file = ILOG'])
+    config.load_from_lines(['block-log-info-print-file = ILOG'])
     assert config.block_log_info_print_file == 'ILOG'
+
+
+def test_incorrect_value_with_underscores_loading(config):
+    with pytest.raises(AttributeError):
+        config.load_from_lines(['block_log_info_print_file = ILOG'])
 
 
 def test_double_quoted_string_loading(config):
     config.load_from_lines([
-        'account_history_rocksdb_path = "blockchain/account-history-rocksdb-storage"',
-        'shared_file_dir = "blockchain"',
-        'snapshot_root_dir = "snapshot"',
+        'account-history-rocksdb-path = "blockchain/account-history-rocksdb-storage"',
+        'shared-file-dir = "blockchain"',
+        'snapshot-root-dir = "snapshot"',
     ])
 
     # Output should not contain double quotes inside string
