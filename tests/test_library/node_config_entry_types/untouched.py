ad729d8b49b38c1635f0bfa3f2361f5b6f51ddf8
Piotr Batko <pbatko@syncad.com>
2021-04-13 13:01:59 +0200

    Rewrite Boolean, Integer, Plugin, String, Untouched for new ConfigEntry design

--- tests/test_library/node_config_entry_types/untouched.py
+++ tests/test_library/node_config_entry_types/untouched.py
@@ -2,19 +2,8 @@ from .config_entry import ConfigEntry
 
 
 class Untouched(ConfigEntry):
-    def __init__(self):
-        self.__value = None
+    def _parse_from_text(self, text):
+        self._value = text
 
-    def parse_from_text(self, text):
-        self.__value = text
-
-    def serialize_to_text(self):
-        return self.__value
-
-    @property
-    def value(self):
-        return self.__value
-
-    @value.setter
-    def value(self, value):
-        self.__value = value
+    def _serialize_to_text(self):
+        return self._value
