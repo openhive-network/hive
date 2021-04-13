4dd4c2d7b220a5488b35112b357bd53d86f74315
Piotr Batko <pbatko@syncad.com>
2021-04-13 12:52:12 +0200

    Redesign ConfigEntry to handle unset entries and simplify derived implementations

--- tests/test_library/node_config_entry_types/config_entry.py
+++ tests/test_library/node_config_entry_types/config_entry.py
@@ -1,14 +1,48 @@
 class ConfigEntry:
+    def __init__(self):
+        self._value = None
+        self.__is_set = False
+
+    def _parse_from_text(self, text):
+        raise Exception('Not implemented')
+
+    def _serialize_to_text(self):
+        raise Exception('Not implemented')
+
+    def _get_value(self):
+        return self._value
+
+    def _get_unset_value(self):
+        return None
+
+    def _set_value(self, value):
+        self._value = value
+
+    # Final method, shouldn't be overridden
+    # TODO: When there will be Python 3.8 or newer use @final instead of above comment
     def parse_from_text(self, text):
-        raise Exception('Not implemented')
+        parsed = self._parse_from_text(text)
+        self.__is_set = True
+        return parsed
 
+    # Final method, shouldn't be overridden
     def serialize_to_text(self):
-        raise Exception('Not implemented')
+        return self._serialize_to_text()
 
-    @property
-    def value(self):
-        raise Exception('Not implemented')
+    # Final method, shouldn't be overridden
+    def get_value(self):
+        return self._get_value() if self.__is_set else self._get_unset_value()
 
-    @value.setter
-    def value(self, value):
-        raise Exception('Not implemented')
+    # Final method, shouldn't be overridden
+    def set_value(self, value):
+        self._set_value(value)
+        self.__is_set = True
+
+    # Final method, shouldn't be overridden
+    def is_set(self):
+        return self.__is_set
+
+    # Final method, shouldn't be overridden
+    def clear(self):
+        self.__is_set = False
+        self._value = None
