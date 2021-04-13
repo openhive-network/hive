798c6dd56bdb03b482c60aff5071ee3070ee0ada
Piotr Batko <pbatko@syncad.com>
2021-04-13 13:49:12 +0200

    Don't serialize unset entries

--- tests/functional/python_tests/test_library_tests/node_config_tests/test_serializing.py
+++ tests/functional/python_tests/test_library_tests/node_config_tests/test_serializing.py
@@ -8,6 +8,12 @@ def config():
     return NodeConfig()
 
 
+def test_single_entry_serialization(config):
+    config.required_participation = 0
+    lines = config.write_to_lines()
+    assert 'required-participation = 0' in lines
+
+
 def test_multi_line_list_entry_serialization(config):
     config.private_key += ['A', 'B']
     lines = config.write_to_lines()
