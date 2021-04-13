efa1f416b401c2d7ea3bc5a0ff1cb7b05933638d
Piotr Batko <pbatko@syncad.com>
2021-04-13 13:11:09 +0200

    Rewrite List for new ConfigEntry design

--- node_config_entry_types/list.py
+++ node_config_entry_types/list.py
@@ -3,16 +3,20 @@ from .config_entry import ConfigEntry
 
 class List(ConfigEntry):
     def __init__(self, item_type, separator=' ', begin='', end='', single_line=True):
+        super().__init__()
+
         self.__item_type = item_type
-        self.__values = []
 
         self.__separator = separator
         self.__begin = begin
         self.__end = end
         self.__single_line = single_line
 
-    def parse_from_text(self, text):
-        self.__values = []
+    def _get_unset_value(self):
+        return []
+
+    def _parse_from_text(self, text):
+        self._value = []
 
         import re
         match_result = re.match(fr'^\s*{re.escape(self.__begin)}(.*){re.escape(self.__end)}\s*$', text)
@@ -21,27 +25,19 @@ class List(ConfigEntry):
         for item_text in match_result[1].split(self.__separator):
             item = self.__item_type()
             item.parse_from_text(item_text)
-            self.__values.append(item.value)
+            self._value.append(item.get_value())
 
-    def serialize_to_text(self):
+    def _serialize_to_text(self):
         def serialize_value(value):
             item = self.__item_type()
-            item.value = value
+            item.set_value(value)
             return item.serialize_to_text()
 
-        values = [serialize_value(value) for value in self.__values]
+        values = [serialize_value(value) for value in self._value]
         return self.__begin + self.__separator.join(values) + self.__end if self.__single_line else values
 
-    @property
-    def value(self):
-        return self.__values
-
-    @value.setter
-    def value(self, value):
-        self.__values = value
-
     def __len__(self):
-        return len(self.__values)
+        return len(self._value)
 
     def __iter__(self):
         return self.value.__iter__()
