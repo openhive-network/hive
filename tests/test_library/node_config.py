ed1f40cd36b1b70ec384b98113db49105a561e8c
Piotr Batko <pbatko@syncad.com>
2021-04-13 15:18:04 +0200

    Set types of p2p_endpoint and p2p_seed_node

--- tests/test_library/node_config.py
+++ tests/test_library/node_config.py
@@ -35,6 +35,8 @@ class NodeConfig:
 
         entries = super().__getattribute__('entries')
         entries['plugin'] = List(Plugin)
+        entries['p2p_endpoint'] = Untouched()
+        entries['p2p_seed_node'] = List(Untouched)
         entries['witness'] = List(String, single_line=False)
         entries['private_key'] = List(Untouched, single_line=False)
         for key in ['shared_file_dir', 'account_history_rocksdb_path', 'snapshot_root_dir']:
