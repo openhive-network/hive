0bbe3cf837605503bfc65a1e06162a9d454012ed
Piotr Batko <pbatko@syncad.com>
2021-04-14 16:13:21 +0200

    Add account_history_api.get_ops_in_block call to node

--- tests/test_library/node_api/node_apis.py
+++ tests/test_library/node_api/node_apis.py
@@ -1,3 +1,4 @@
+from .account_history_api import AccountHistoryApi
 from .database_api import DatabaseApi
 from .network_node_api import NetworkNodeApi
 
@@ -6,5 +7,6 @@ class Apis:
     def __init__(self, node):
         self.__node = node
 
+        self.account_history = AccountHistoryApi(self.__node)
         self.database = DatabaseApi(self.__node)
         self.network_node = NetworkNodeApi(self.__node)
