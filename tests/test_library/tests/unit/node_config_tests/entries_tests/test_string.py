a353bde8c96d67a3a8c629829dda7f16b6aed9ca
Piotr Batko <pbatko@syncad.com>
2021-04-15 07:48:12 +0200

    Make Boolean, Integer and String type safe

--- tests/test_library/tests/unit/node_config_tests/entries_tests/test_string.py
+++ tests/test_library/tests/unit/node_config_tests/entries_tests/test_string.py
@@ -25,3 +25,9 @@ def test_serializing(entry, values):
     for expected, input_value in values:
         entry.set_value(input_value)
         assert entry.serialize_to_text() == expected
+
+
+def test_different_type_assignments(entry):
+    for incorrect_value in [True, 123, 2.718]:
+        with pytest.raises(TypeError):
+            entry.set_value(incorrect_value)
