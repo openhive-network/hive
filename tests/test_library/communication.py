179a92e548adb82c3b766529d00dd70694198ac4
Piotr Batko <pbatko@syncad.com>
2021-04-15 12:09:52 +0200

    Request doesn't resend messages if received error

--- tests/test_library/communication.py
+++ tests/test_library/communication.py
@@ -18,18 +18,14 @@ def request(url: str, message: dict, max_attempts=3, seconds_between_attempts=0.
     attempts_left = max_attempts
     while attempts_left > 0:
         result = requests.post(url, data=message)
-
-        success = result.status_code == 200
-        response = json.loads(result.text)
-
-        if not success or 'error' in response:
+        if result.status_code != 200:
             if attempts_left > 0:
                 import time
                 time.sleep(seconds_between_attempts)
             attempts_left -= 1
             continue
 
-        return response
+        return json.loads(result.text)
 
     raise CommunicationError(
         f'Problem occurred during communication with {url}.\nSent: {message}.\nReceived: {response}',
