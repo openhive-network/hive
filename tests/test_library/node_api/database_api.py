fbcfdd90a289be2daa422f72762828b010938283
Piotr Batko <pbatko@syncad.com>
2021-04-16 09:59:18 +0200

    Add database_api.get_dynamic_global_properties call to node

--- tests/test_library/node_api/database_api.py
+++ tests/test_library/node_api/database_api.py
@@ -5,6 +5,9 @@ class DatabaseApi(ApiBase):
     def __init__(self, node):
         super().__init__(node, 'database_api')
 
+    def get_dynamic_global_properties(self):
+        return self._send('get_dynamic_global_properties')
+
     def list_witnesses(self, limit, order, start):
         return self._send(
             'list_witnesses',
