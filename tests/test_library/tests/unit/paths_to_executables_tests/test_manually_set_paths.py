30296309f80db02ed1493ec6230d1a5e2ca8be30
Piotr Batko <pbatko@syncad.com>
2021-04-16 14:46:22 +0200

    Move fixtures to separate file

--- tests/test_library/tests/unit/paths_to_executables_tests/test_manually_set_paths.py
+++ tests/test_library/tests/unit/paths_to_executables_tests/test_manually_set_paths.py
@@ -1,24 +1,4 @@
-import pytest
-
-from test_library.paths_to_executables_implementation import *
-
-@pytest.fixture
-def paths():
-    return PathsToExecutables()
-
-
-@pytest.fixture
-def executables():
-    class Executable:
-        def __init__(self, name):
-            self.name = name
-            self.path = f'{self.name}_path'
-
-    return [
-        Executable('hived'),
-        Executable('cli_wallet'),
-        Executable('get_dev_key'),
-    ]
+from fixtures import paths, executables
 
 
 def test_paths_set_manually(paths, executables):
