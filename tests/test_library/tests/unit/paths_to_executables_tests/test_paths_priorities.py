727d05fcbec4ae8f0fc3f36cf6c575bb7aead685
Piotr Batko <pbatko@syncad.com>
2021-04-21 17:31:49 +0200

    Update priority tests to take into account installed executables

--- tests/test_library/tests/unit/paths_to_executables_tests/test_paths_priorities.py
+++ tests/test_library/tests/unit/paths_to_executables_tests/test_paths_priorities.py
@@ -5,6 +5,7 @@ def test_manually_set_path_priority(paths, executable):
     paths.set_path_of(executable.name, 'path_set_manually')
     paths.parse_command_line_arguments([executable.argument, 'path_set_from_command_line_arguments'])
     paths.set_environment_variables({executable.environment_variable: 'path_set_from_environment_variables'})
+    paths.set_installed_executables({executable.name: 'path_of_installed_executable'})
 
     assert paths.get_path_of(executable.name) == 'path_set_manually'
 
@@ -12,5 +13,13 @@ def test_manually_set_path_priority(paths, executable):
 def test_path_from_command_line_argument_priority(paths, executable):
     paths.parse_command_line_arguments([executable.argument, 'path_set_from_command_line_arguments'])
     paths.set_environment_variables({executable.environment_variable: 'path_set_from_environment_variables'})
+    paths.set_installed_executables({executable.name: 'path_of_installed_executable'})
 
     assert paths.get_path_of(executable.name) == 'path_set_from_command_line_arguments'
+
+
+def test_path_from_environment_variables_priority(paths, executable):
+    paths.set_environment_variables({executable.environment_variable: 'path_set_from_environment_variables'})
+    paths.set_installed_executables({executable.name: 'path_of_installed_executable'})
+
+    assert paths.get_path_of(executable.name) == 'path_set_from_environment_variables'
