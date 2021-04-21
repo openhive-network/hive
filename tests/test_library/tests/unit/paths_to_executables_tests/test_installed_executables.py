be09332c2fc251954db963ff93260e2e26a09812
Piotr Batko <pbatko@syncad.com>
2021-04-21 17:26:14 +0200

    Handle paths of installed executables

---
+++ tests/test_library/tests/unit/paths_to_executables_tests/test_installed_executables.py
@@ -0,0 +1,7 @@
+from fixtures import empty_paths as paths, executables
+
+
+def test_paths_of_installed_executables(paths, executables):
+    for executable in executables:
+        paths.set_installed_executables({executable.name: executable.path})
+        assert paths.get_path_of(executable.name) == executable.path
