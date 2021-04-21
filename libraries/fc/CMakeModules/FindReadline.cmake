7c705bf2ce9d7539290edaa6fe151bd7e44bfc81
mtyszczak <mateusz.tyszczak@gmail.com>
2021-04-21 19:59:32 +0200

    Fix readline static link

--- libraries/fc/CMakeModules/FindReadline.cmake
+++ libraries/fc/CMakeModules/FindReadline.cmake
@@ -10,12 +10,14 @@
 #  Readline_ROOT_DIR         Set this variable to the root installation of
 #                            readline if the module has problems finding the
 #                            proper installation path.
+#  Readline_USE_STATIC_LIBS  Set to ON to force the use of the static
+#                            libraries. Default is OFF.
 #
 # Variables defined by this module:
 #
 #  READLINE_FOUND            System has readline, include and lib dirs found
-#  Readline_INCLUDE_DIR      The readline include directories. 
-#  Readline_LIBRARY          The readline library.
+#  Readline_INCLUDE_DIR      The readline include directories.
+#  Readline_LIBRARIES        The readline libraries.
 
 find_path(Readline_ROOT_DIR
     NAMES include/readline/readline.h
@@ -26,24 +28,34 @@ find_path(Readline_INCLUDE_DIR
     HINTS ${Readline_ROOT_DIR}/include
 )
 
-find_library(Readline_LIBRARY
-    NAMES readline
+set( _Readline_LIBRARIES readline )
+if( Readline_USE_STATIC_LIBS )
+  set( _Readline_LIBRARIES libreadline.a ${_Readline_LIBRARIES} )
+endif()
+
+find_library(Readline_LIBRARIES
+    NAMES ${_Readline_LIBRARIES}
     HINTS ${Readline_ROOT_DIR}/lib
 )
 
-if(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
+if( Readline_USE_STATIC_LIBS )
+  find_library( Tinfo_LIBRARY NAMES libtinfo.a )
+  list( APPEND Readline_LIBRARIES ${Readline_LIBRARIES} ${Tinfo_LIBRARY} )
+endif()
+
+if(Readline_INCLUDE_DIR AND Readline_LIBRARIES AND Ncurses_LIBRARY)
   set(READLINE_FOUND TRUE)
-else(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
-  FIND_LIBRARY(Readline_LIBRARY NAMES readline)
+else(Readline_INCLUDE_DIR AND Readline_LIBRARIES AND Ncurses_LIBRARY)
+  FIND_LIBRARY(Readline_LIBRARIES NAMES readline)
   include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARY )
-  MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARY)
-endif(Readline_INCLUDE_DIR AND Readline_LIBRARY AND Ncurses_LIBRARY)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARIES )
+  MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARIES)
+endif(Readline_INCLUDE_DIR AND Readline_LIBRARIES AND Ncurses_LIBRARY)
 
 mark_as_advanced(
     Readline_ROOT_DIR
     Readline_INCLUDE_DIR
-    Readline_LIBRARY
+    Readline_LIBRARIES
 )
 
-MESSAGE( STATUS "Found Readline: ${Readline_LIBRARY}" )
+MESSAGE( STATUS "Found Readline: ${Readline_LIBRARIES}" )
