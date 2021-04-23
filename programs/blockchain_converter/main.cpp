a883e286856d31ee876753558d58842694fdf885
mtyszczak <mateusz.tyszczak@gmail.com>
2021-04-23 08:45:35 +0200

    Add output default value initialization

--- programs/blockchain_converter/main.cpp
+++ programs/blockchain_converter/main.cpp
@@ -13,17 +13,25 @@ int main( int argc, char** argv )
     boost::program_options::options_description opts;
       opts.add_options()
       ("help,h", "Print this help message and exit.")
+      ("private-key,k", bpo::value< std::string >(), "init miner private key")
+      ("chain-id,d", bpo::value< std::string >(), "chain ID to convert")
+      ("input,i", bpo::value< std::string >(), "input block log")
+      ("output,o", bpo::value< std::string >(), "output block log; defaults to [input]_out" )
       ;
 
     bpo::variables_map options;
 
     bpo::store( bpo::parse_command_line(argc, argv, opts), options );
 
-    if( options.count("help") )
+    if( options.count("help") || !options.count("private-key") || !options.count("chain-id") || !options.count("input") )
     {
       std::cout << opts << "\n";
       return 0;
     }
+
+    std::string out_file;
+    if( !options.count("output") )
+      out_file = options["input"].as< std::string >() + "_out";
 
     return 0;
   }
