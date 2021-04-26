21a9222aa60782e10e0a16ef0146a623d244a834
Piotr Batko <pbatko@syncad.com>
2021-04-26 15:02:18 +0200

    Add name creation with custom bases

--- tests/unit/children_names_tests/test_name_creation_without_base_name.py
+++ tests/unit/children_names_tests/test_name_creation_without_base_name.py
@@ -11,3 +11,8 @@ def names():
 def test_creation_attempt(names):
     with pytest.raises(NameBaseNotSet):
         names.create_name()
+
+
+def test_creation_of_next_names_with_same_base(names):
+    for i in range(5):
+        assert names.create_name('Base') == f'Base{i}'
