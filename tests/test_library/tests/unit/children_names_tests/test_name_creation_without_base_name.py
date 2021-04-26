abb53ecd5f070294c100cc27a11ce7fee233686d
Piotr Batko <pbatko@syncad.com>
2021-04-26 15:04:59 +0200

    Test if names already in use are skipped during creation

--- tests/test_library/tests/unit/children_names_tests/test_name_creation_without_base_name.py
+++ tests/test_library/tests/unit/children_names_tests/test_name_creation_without_base_name.py
@@ -16,3 +16,9 @@ def test_creation_attempt(names):
 def test_creation_of_next_names_with_same_base(names):
     for i in range(5):
         assert names.create_name('Base') == f'Base{i}'
+
+
+def test_registered_names_are_skipped_during_creation(names):
+    registered = names.register_name('Node0')
+    created = names.create_name('Node')
+    assert created != registered
