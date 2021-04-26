a3796e0c1beb5df9d640cc8006158a6f62386e0e
Piotr Batko <pbatko@syncad.com>
2021-04-26 14:10:09 +0200

    Add name collision detection if name is already in use

--- tests/test_library/children_names.py
+++ tests/test_library/children_names.py
@@ -1,9 +1,19 @@
+class NameAlreadyInUse(Exception):
+    pass
+
+
 class ChildrenNames:
     def __init__(self, name_base):
+        self.names = []
         self.name_base = name_base
         self.next_name_number = 0
 
     def create_name(self):
         name = f'{self.name_base}{self.next_name_number}'
+        self.names.append(name)
         self.next_name_number += 1
         return name
+
+    def register_name(self, name):
+        if name in self.names:
+            raise NameAlreadyInUse()
