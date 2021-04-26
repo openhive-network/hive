e1f2e98f863511cc0d1c3cab990ae025ddd93c21
Piotr Batko <pbatko@syncad.com>
2021-04-26 15:02:18 +0200

    Add name creation with custom bases

--- tests/test_library/children_names.py
+++ tests/test_library/children_names.py
@@ -10,20 +10,23 @@ class ChildrenNames:
     def __init__(self, name_base=None):
         self.names = []
         self.name_base = name_base
-        self.next_name_number = 0
+        self.next_name_numbers = {self.name_base: 0}
 
     def create_name(self, name_base=None):
         if self.name_base is None and name_base is None:
             raise NameBaseNotSet('Unable to create a name without name base')
 
+        name_base = self.name_base if name_base is None else name_base
+        if name_base not in self.next_name_numbers:
+            self.next_name_numbers[name_base] = 0
         while True:
-            name = f'{self.name_base}{self.next_name_number}'
+            name = f'{name_base}{self.next_name_numbers[name_base]}'
             if name not in self.names:
                 break
-            self.next_name_number += 1
+            self.next_name_numbers[name_base] += 1
 
         self.names.append(name)
-        self.next_name_number += 1
+        self.next_name_numbers[name_base] += 1
         return name
 
     def register_name(self, name):
