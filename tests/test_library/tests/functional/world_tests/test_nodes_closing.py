4a1cc4d84aa56ae5d1f3b82068a62aa2f078261f
Piotr Batko <pbatko@syncad.com>
2021-04-28 10:32:15 +0200

    Add method allowing network creation in world

--- tests/test_library/tests/functional/world_tests/test_nodes_closing.py
+++ tests/test_library/tests/functional/world_tests/test_nodes_closing.py
@@ -12,3 +12,24 @@ def test_if_single_node_is_closed():
         node.wait_for_synchronization()
 
     assert not node.is_running()
+
+
+def test_if_all_nodes_in_network_are_closed():
+    with World() as world:
+        network = world.create_network()
+
+        init_node = network.create_node('InitNode')
+        init_node.config.enable_stale_production = True
+        init_node.config.required_participation = 0
+        init_node.set_witness('initminer')
+
+        for _ in range(3):
+            network.create_node()
+
+        network.run()
+        network.wait_for_synchronization_of_all_nodes()
+
+        nodes = world.nodes()
+
+    assert len(nodes) == 4
+    assert all([not node.is_running() for node in nodes])
