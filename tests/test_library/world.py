083cec6c37a8a6e5e077419dd815d7ed5ed49222
Piotr Batko <pbatko@syncad.com>
2021-04-28 10:32:15 +0200

    Add method allowing network creation in world

--- tests/test_library/world.py
+++ tests/test_library/world.py
@@ -1,10 +1,11 @@
-from . import Node
+from . import Network, Node
 from .children_names import ChildrenNames
 
 
 class World:
     def __init__(self):
         self.children_names = ChildrenNames()
+        self.__networks = []
         self.__nodes = []
 
     def __enter__(self):
@@ -14,6 +15,23 @@ class World:
         for node in self.__nodes:
             if node.is_running():
                 node.close()
+
+        for network in self.__networks:
+            if network.is_running:
+                network.close()
+
+    def create_network(self, name=None):
+        if name is not None:
+            self.children_names.register_name(name)
+        else:
+            name = self.children_names.create_name('Network')
+
+        network = Network(name)
+        self.__networks.append(network)
+        return network
+
+    def networks(self):
+        return self.__networks
 
     def create_node(self, name=None):
         if name is not None:
@@ -26,4 +44,9 @@ class World:
         return node
 
     def nodes(self):
-        return self.__nodes
+        """Returns list of all nodes in the world (including nodes in networks)"""
+
+        nodes = self.__nodes.copy()
+        for network in self.__networks:
+            nodes += network.nodes
+        return nodes
