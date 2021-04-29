0c4a1d76d0dd0c6f3749cb1f5d0ab77df98f1a3f
Piotr Batko <pbatko@syncad.com>
2021-04-29 19:09:39 +0200

    Test if init node is able to produce blocks at startup

---
+++ tests/test_library/tests/functional_tests/node_tests/test_node_startup.py
@@ -0,0 +1,8 @@
+from test_library import World
+
+
+def test_init_node_startup():
+    with World() as world:
+        init_node = world.create_init_node()
+        init_node.run()
+        init_node.wait_for_block_with_number(1)
