5aa77bcde7e15c8b4dceea8e440354ad89d575c7
mtyszczak <mateusz.tyszczak@gmail.com>
2021-04-30 14:19:00 +0200

    Add converter source file

--- programs/blockchain_converter/converter.hpp
+++ programs/blockchain_converter/converter.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
 #include <memory>
-#include <stdexcept>
 #include <map>
 #include <string>
 
-#include <hive/protocol/operations.hpp>
+#include <fc/crypto/elliptic.hpp>
 
-#include <hive/utilities/key_conversion.hpp>
+#include <hive/protocol/operations.hpp>
 
 namespace hive {
 
   using namespace protocol;
-  using namespace utilities;
 
   namespace converter {
 
@@ -26,37 +24,20 @@ namespace hive {
 
     public:
 
-      derived_keys_map( const std::string& private_key_wif )
-        : private_key_wif( private_key_wif ) {}
+      derived_keys_map( const std::string& private_key_wif );
 
-      derived_keys_map( const fc::ecc::private_key& private_key )
-        : private_key_wif( key_to_wif(private_key) ) {}
+      derived_keys_map( const fc::ecc::private_key& private_key );
 
       /// Generates public key from the private key mapped to the public key from the original block_log
-      public_key_type get_public( const public_key_type& original )const
-      {
-        return at( original ).get_public_key();
-      }
+      public_key_type get_public( const public_key_type& original );
 
       /// Inserts key to the container if public key was not found in the unordered_map.
       /// Returns const reference to the private key mapped to the public key from the original block_log.
-      const fc::ecc::private_key& operator[]( const public_key_type& original )const
-      {
-        if( keys.find( original ) != keys.end() )
-          return (*keys.emplace( original,
-              fc::ecc::private_key::regenerate(fc::sha256::hash(fc::sha512::hash( private_key_wif + ' ' + std::to_string( keys.size() ) )))
-            ).first).second;
-        return keys.at( original );
-      }
+      const fc::ecc::private_key& operator[]( const public_key_type& original );
 
-      const fc::ecc::private_key& at( const public_key_type& original )const
-      {
-        if( keys.find( original ) != keys.end() )
-          return (*keys.emplace( original,
-              fc::ecc::private_key::regenerate(fc::sha256::hash(fc::sha512::hash( private_key_wif + ' ' + std::to_string( keys.size() ) )))
-            ).first).second;
-        return keys.at( original );
-      }
+      const fc::ecc::private_key& at( const public_key_type& original );
+
+      // TODO: Save to file or convert into wallet.json
     };
 
     class convert_operations_visitor
@@ -67,51 +48,31 @@ namespace hive {
     public:
       typedef operation result_type;
 
-      convert_operations_visitor( const std::shared_ptr< derived_keys_map >& derived_keys )
-        : derived_keys( derived_keys ) {}
+      convert_operations_visitor( const std::shared_ptr< derived_keys_map >& derived_keys );
 
-      const account_create_operation& operator()( const account_create_operation& op )const
-      {
-        for( auto& key : op.owner.key_auths )
-        {
-          key.first = derived_keys->get_public(key.first);
-        }
+      const account_create_operation& operator()( account_create_operation& op )const;
 
-        return op;
-      }
+      const account_create_with_delegation_operation& operator()( const account_create_with_delegation_operation& op )const;
 
-      const account_create_with_delegation_operation& operator()( const account_create_with_delegation_operation& op )const
-      { return op; }
+      const account_update_operation& operator()( const account_update_operation& op )const;
 
-      const account_update_operation& operator()( const account_update_operation& op )const
-      { return op; }
+      const account_update2_operation& operator()( const account_update2_operation& op )const;
 
-      const account_update2_operation& operator()( const account_update2_operation& op )const
-      { return op; }
+      const create_claimed_account_operation& operator()( const create_claimed_account_operation& op )const;
 
-      const create_claimed_account_operation& operator()( const create_claimed_account_operation& op )const
-      { return op; }
+      const witness_update_operation& operator()( const witness_update_operation& op )const;
 
-      const witness_update_operation& operator()( const witness_update_operation& op )const
-      { return op; }
+      const witness_set_properties_operation& operator()( const witness_set_properties_operation& op )const;
 
-      const witness_set_properties_operation& operator()( const witness_set_properties_operation& op )const
-      { return op; }
+      const custom_binary_operation& operator()( const custom_binary_operation& op )const;
 
-      const custom_binary_operation& operator()( const custom_binary_operation& op )const
-      { return op; }
+      const pow2_operation& operator()( const pow2_operation& op )const;
 
-      const pow2_operation& operator()( const pow2_operation& op )const
-      { return op; }
+      const report_over_production_operation& operator()( const report_over_production_operation& op )const;
 
-      const report_over_production_operation& operator()( const report_over_production_operation& op )const
-      { return op; }
+      const request_account_recovery_operation& operator()( const request_account_recovery_operation& op )const;
 
-      const request_account_recovery_operation& operator()( const request_account_recovery_operation& op )const
-      { return op; }
-
-      const recover_account_operation& operator()( const recover_account_operation& op )const
-      { return op; }
+      const recover_account_operation& operator()( const recover_account_operation& op )const;
 
       // No signatures modification ops
       template< typename T >
