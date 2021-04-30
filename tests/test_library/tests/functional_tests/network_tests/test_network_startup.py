ab20b9d321b2e90312908f1a730de55d471a0eb3
Piotr Batko <pbatko@syncad.com>
2021-04-30 12:34:10 +0200

    Block network run until live by default

--- tests/test_library/tests/functional_tests/network_tests/test_network_startup.py
+++ tests/test_library/tests/functional_tests/network_tests/test_network_startup.py
@@ -6,7 +6,6 @@ def test_network_startup():
         network = world.create_network()
         network.create_init_node()
         network.create_node()
-        node = network.create_node()
+        network.create_node()
 
         network.run()
-        node.wait_for_block_with_number(1)
