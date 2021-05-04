9a6b8169472e2fe1826e1b5e37a0f1f2aca10428
mtyszczak <mateusz.tyszczak@gmail.com>
2021-05-04 16:06:24 +0200

    Change bc converter ptr to reference in visitor

--- programs/blockchain_converter/converter.cpp
+++ programs/blockchain_converter/converter.cpp
@@ -49,13 +49,9 @@ namespace hive {
       return keys.at( original );
     }
 
-    derived_keys_map::keys_map_type::iterator        derived_keys_map::begin()      { return keys.begin(); }
-    derived_keys_map::keys_map_type::const_iterator  derived_keys_map::begin()const { return keys.begin(); }
-    derived_keys_map::keys_map_type::const_iterator derived_keys_map::cbegin()const { return keys.begin(); }
+    derived_keys_map::const_iterator derived_keys_map::begin()const { return keys.begin(); }
 
-    derived_keys_map::keys_map_type::iterator        derived_keys_map::end()      { return keys.end(); }
-    derived_keys_map::keys_map_type::const_iterator  derived_keys_map::end()const { return keys.end(); }
-    derived_keys_map::keys_map_type::const_iterator derived_keys_map::cend()const { return keys.end(); }
+    derived_keys_map::const_iterator derived_keys_map::end()const { return keys.end(); }
 
     void derived_keys_map::save_wallet_file( const std::string& password, std::string wallet_filename )const
     {
@@ -84,23 +80,23 @@ namespace hive {
       outfile.close();
     }
 
-    convert_operations_visitor::convert_operations_visitor( blockchain_converter* converter )
+    convert_operations_visitor::convert_operations_visitor( blockchain_converter& converter )
       : converter( converter ) {}
 
     const account_create_operation& convert_operations_visitor::operator()( account_create_operation& op )const
     {
-      op.owner.key_auths = converter->convert_authorities( op.owner.key_auths );
-      op.active.key_auths = converter->convert_authorities( op.active.key_auths );
-      op.posting.key_auths = converter->convert_authorities( op.posting.key_auths );
+      op.owner.key_auths = converter.convert_authorities( op.owner.key_auths );
+      op.active.key_auths = converter.convert_authorities( op.active.key_auths );
+      op.posting.key_auths = converter.convert_authorities( op.posting.key_auths );
 
       return op;
     }
 
     const account_create_with_delegation_operation& convert_operations_visitor::operator()( account_create_with_delegation_operation& op )const
     {
-      op.owner.key_auths = converter->convert_authorities( op.owner.key_auths );
-      op.active.key_auths = converter->convert_authorities( op.active.key_auths );
-      op.posting.key_auths = converter->convert_authorities( op.posting.key_auths );
+      op.owner.key_auths = converter.convert_authorities( op.owner.key_auths );
+      op.active.key_auths = converter.convert_authorities( op.active.key_auths );
+      op.posting.key_auths = converter.convert_authorities( op.posting.key_auths );
 
       return op;
     }
@@ -108,11 +104,11 @@ namespace hive {
     const account_update_operation& convert_operations_visitor::operator()( account_update_operation& op )const
     {
       if( op.owner.valid() )
-        (*op.owner).key_auths = converter->convert_authorities( (*op.owner).key_auths );
+        (*op.owner).key_auths = converter.convert_authorities( (*op.owner).key_auths );
       if( op.active.valid() )
-        (*op.active).key_auths = converter->convert_authorities( (*op.active).key_auths );
+        (*op.active).key_auths = converter.convert_authorities( (*op.active).key_auths );
       if( op.posting.valid() )
-        (*op.posting).key_auths = converter->convert_authorities( (*op.posting).key_auths );
+        (*op.posting).key_auths = converter.convert_authorities( (*op.posting).key_auths );
 
       return op;
     }
@@ -120,27 +116,27 @@ namespace hive {
     const account_update2_operation& convert_operations_visitor::operator()( account_update2_operation& op )const
     {
       if( op.owner.valid() )
-        (*op.owner).key_auths = converter->convert_authorities( (*op.owner).key_auths );
+        (*op.owner).key_auths = converter.convert_authorities( (*op.owner).key_auths );
       if( op.active.valid() )
-        (*op.active).key_auths = converter->convert_authorities( (*op.active).key_auths );
+        (*op.active).key_auths = converter.convert_authorities( (*op.active).key_auths );
       if( op.posting.valid() )
-        (*op.posting).key_auths = converter->convert_authorities( (*op.posting).key_auths );
+        (*op.posting).key_auths = converter.convert_authorities( (*op.posting).key_auths );
 
       return op;
     }
 
     const create_claimed_account_operation& convert_operations_visitor::operator()( create_claimed_account_operation& op )const
     {
-      op.owner.key_auths = converter->convert_authorities( op.owner.key_auths );
-      op.active.key_auths = converter->convert_authorities( op.active.key_auths );
-      op.posting.key_auths = converter->convert_authorities( op.posting.key_auths );
+      op.owner.key_auths = converter.convert_authorities( op.owner.key_auths );
+      op.active.key_auths = converter.convert_authorities( op.active.key_auths );
+      op.posting.key_auths = converter.convert_authorities( op.posting.key_auths );
 
       return op;
     }
 
     const witness_update_operation& convert_operations_visitor::operator()( witness_update_operation& op )const
     {
-      op.block_signing_key = converter->get_keys().get_public(op.block_signing_key);
+      op.block_signing_key = converter.get_keys().get_public(op.block_signing_key);
 
       return op;
     }
@@ -153,7 +149,7 @@ namespace hive {
       {
         public_key_type signing_key;
         fc::raw::unpack_from_vector( key_itr->second, signing_key );
-        op.props.at( "key" ) = fc::raw::pack_to_vector( converter->get_keys().get_public(signing_key) );
+        op.props.at( "key" ) = fc::raw::pack_to_vector( converter.get_keys().get_public(signing_key) );
       }
 
       return op;
@@ -162,7 +158,7 @@ namespace hive {
     const custom_binary_operation& convert_operations_visitor::operator()( custom_binary_operation& op )const
     {
       for( auto& auth : op.required_auths )
-        auth.key_auths = converter->convert_authorities( auth.key_auths );
+        auth.key_auths = converter.convert_authorities( auth.key_auths );
 
       return op;
     }
@@ -170,30 +166,30 @@ namespace hive {
     const pow2_operation& convert_operations_visitor::operator()( pow2_operation& op )const
     {
       if( op.new_owner_key.valid() )
-        *op.new_owner_key = converter->get_keys().get_public(*op.new_owner_key);
+        *op.new_owner_key = converter.get_keys().get_public(*op.new_owner_key);
 
       return op;
     }
 
     const report_over_production_operation& convert_operations_visitor::operator()( report_over_production_operation& op )const
     {
-      converter->convert_signed_header( op.first_block );
-      converter->convert_signed_header( op.second_block );
+      converter.convert_signed_header( op.first_block );
+      converter.convert_signed_header( op.second_block );
 
       return op;
     }
 
     const request_account_recovery_operation& convert_operations_visitor::operator()( request_account_recovery_operation& op )const
     {
-      op.new_owner_authority.key_auths = converter->convert_authorities( op.new_owner_authority.key_auths );
+      op.new_owner_authority.key_auths = converter.convert_authorities( op.new_owner_authority.key_auths );
 
       return op;
     }
 
     const recover_account_operation& convert_operations_visitor::operator()( recover_account_operation& op )const
     {
-      op.new_owner_authority.key_auths = converter->convert_authorities( op.new_owner_authority.key_auths );
-      op.recent_owner_authority.key_auths = converter->convert_authorities( op.recent_owner_authority.key_auths );
+      op.new_owner_authority.key_auths = converter.convert_authorities( op.new_owner_authority.key_auths );
+      op.recent_owner_authority.key_auths = converter.convert_authorities( op.recent_owner_authority.key_auths );
 
       return op;
     }
@@ -208,7 +204,7 @@ namespace hive {
 
       for( auto _transaction = _signed_block.transactions.begin(); _transaction != _signed_block.transactions.end(); ++_transaction )
       {
-        _transaction->visit( convert_operations_visitor( this ) );
+        _transaction->visit( convert_operations_visitor( *this ) );
         for( auto _signature = _transaction->signatures.begin(); _signature != _transaction->signatures.end(); ++_signature )
           *_signature = convert_signature_from_header( *_signature, _signed_block ).sign_compact( _transaction->sig_digest( chain_id ), get_canon_type( *_signature ) );
       }
--- programs/blockchain_converter/main.cpp
+++ programs/blockchain_converter/main.cpp
@@ -34,7 +34,7 @@ int main( int argc, char** argv )
       ("output,o", bpo::value< std::string >(), "output block log; defaults to [input]_out" )
       ("wallet-file,w", bpo::value< std::string >()->implicit_value( "wallet.json" ), "optional wallet filename to save generated private keys" )
       ("wallet-password,p", bpo::value< std::string >()->default_value( "password" ), "password to the generated wallet file" )
-      ("log-per-block,l", bpo::value< uint32_t >()->default_value( 0 ), "Displays blocks in JSON format every n blocks")
+      ("log-per-block,l", bpo::value< uint32_t >()->default_value( 0 )->implicit_value( 1 ), "Displays blocks in JSON format every n blocks")
       ("log-specific,s", bpo::value< uint32_t >()->default_value( 0 ), "Displays only block with specified number")
       ;
 
