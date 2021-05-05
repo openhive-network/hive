7ec7f8ec0d16483c06d48de03d829e658467caee
Piotr Batko <pbatko@syncad.com>
2021-05-05 12:04:40 +0200

    Add checking if installation script was run as __main__

--- tests/test_library/install.py
+++ tests/test_library/install.py
@@ -19,25 +19,30 @@ def check_paths_to_executables():
             print(f'{executable}: {path}')
 
 
-this_script_directory = Path(__file__).parent.absolute()
-package_name = this_script_directory.parts[-1]
+def install():
+    this_script_directory = Path(__file__).parent.absolute()
+    package_name = this_script_directory.parts[-1]
 
-destination = Path(subprocess.check_output(['python3', '-m', 'site', '--user-site'], encoding='utf-8').strip())
-destination /= package_name
+    destination = Path(subprocess.check_output(['python3', '-m', 'site', '--user-site'], encoding='utf-8').strip())
+    destination /= package_name
 
-if destination.exists():
-    print(f'You have already installed {package_name}.')
+    if destination.exists():
+        print(f'You have already installed {package_name}.')
+        check_paths_to_executables()
+        exit(0)
+
+    source = this_script_directory / 'package/test_library/'
+    destination.symlink_to(source)
+
+    print(f'''You have successfully installed {package_name}.
+
+    Details:
+      Symlink to [1] created in [2].
+      [1] {source}
+      [2] {destination}''')
+
     check_paths_to_executables()
-    exit(0)
 
-source = this_script_directory / 'package/test_library/'
-destination.symlink_to(source)
 
-print(f'''You have successfully installed {package_name}.
-
-Details:
-  Symlink to [1] created in [2].
-  [1] {source}
-  [2] {destination}''')
-
-check_paths_to_executables()
+if __name__ == '__main__':
+    install()
