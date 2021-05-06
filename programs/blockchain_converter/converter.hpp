c9e0751b0f3b1cf068f179227b5f6d3606884c29
mtyszczak <mateusz.tyszczak@gmail.com>
2021-05-06 16:39:56 +0200

    Fix derived keys creation implementation

--- programs/blockchain_converter/converter.hpp
+++ programs/blockchain_converter/converter.hpp
@@ -19,22 +19,20 @@ namespace hive {
     {
     public:
 
-      typedef std::map< public_key_type, fc::ecc::private_key > keys_map_type;
+      typedef std::map< public_key_type, private_key_type > keys_map_type;
       typedef keys_map_type::const_iterator                     const_iterator;
 
-      derived_keys_map( const std::string& private_key_wif );
-
-      derived_keys_map( const fc::ecc::private_key& _private_key );
+      derived_keys_map( const private_key_type& _private_key );
 
       /// Generates public key from the private key mapped to the public key from the original block_log
       public_key_type get_public( const public_key_type& original );
 
       /// Inserts key to the container if public key from the original block log was not found in the map.
       /// Returns const reference to the generated derived private key.
-      const fc::ecc::private_key& get_private( const public_key_type& original );
+      const private_key_type& get_private( const public_key_type& original );
 
       /// Retrieves private key from the map. Throws std::out_of_range if given public key was not found.
-      const fc::ecc::private_key& at( const public_key_type& original )const;
+      const private_key_type& at( const public_key_type& original )const;
 
       const_iterator begin()const;
 
@@ -44,28 +42,28 @@ namespace hive {
 
     private:
 
-      keys_map_type keys;
+      keys_map_type    keys;
 
-      std::string   private_key_wif;
+      private_key_type _private_key;
     };
 
     class blockchain_converter
     {
     private:
-      fc::ecc::private_key _private_key;
-      chain_id_type        chain_id;
-      derived_keys_map     keys;
+      private_key_type _private_key;
+      chain_id_type    chain_id;
+      derived_keys_map keys;
 
     public:
       /// All converted blocks will be signed using keys derived from the given private key
-      blockchain_converter( const fc::ecc::private_key& _private_key, const chain_id_type& chain_id = HIVE_CHAIN_ID );
+      blockchain_converter( const private_key_type& _private_key, const chain_id_type& chain_id = HIVE_CHAIN_ID );
 
       /// Sets signed_block previous member to the given value and re-signs content of the block using derived keys. Returns current block id.
       block_id_type convert_signed_block( signed_block& _signed_block, const block_id_type& previous_block_id );
 
       void convert_signed_header( signed_block_header& _signed_header );
 
-      const fc::ecc::private_key& convert_signature_from_header( const signature_type& _signature, const signed_block_header& _signed_header );
+      const private_key_type& convert_signature_from_header( const signature_type& _signature, const signed_block_header& _signed_header );
 
       /// Tries to guess canon type using given signature. If not found it is defaulted to fc::ecc::non_canonical
       fc::ecc::canonical_signature_type get_canon_type( const signature_type& _signature )const;
