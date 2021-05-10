b0d9e96eb3a28fce296876d2a75b37601b455158
Piotr Batko <pbatko@syncad.com>
2021-05-10 12:25:43 +0200

    Use create_init_node instead of manual init node configuration

--- tests/functional_tests/world_tests/test_nodes_closing.py
+++ tests/functional_tests/world_tests/test_nodes_closing.py
@@ -3,11 +3,7 @@ from test_library import World
 
 def test_if_single_node_is_closed():
     with World() as world:
-        node = world.create_node()
-        node.config.enable_stale_production = True
-        node.config.required_participation = 0
-        node.set_witness('initminer')
-
+        node = world.create_init_node()
         node.run()
 
     assert not node.is_running()
@@ -17,11 +13,7 @@ def test_if_all_nodes_in_network_are_closed():
     with World() as world:
         network = world.create_network()
 
-        init_node = network.create_node('InitNode')
-        init_node.config.enable_stale_production = True
-        init_node.config.required_participation = 0
-        init_node.set_witness('initminer')
-
+        network.create_init_node()
         for _ in range(3):
             network.create_node()
 
