ff702dd8087eea502f05c0797517a9de8098ebf5
Piotr Batko <pbatko@syncad.com>
2021-05-14 14:46:02 +0200

    Add support for condenser_api.get_account_history call

--- package/test_tools/node_api/condenser_api.py
+++ package/test_tools/node_api/condenser_api.py
@@ -5,5 +5,8 @@ class CondenserApi(ApiBase):
     def __init__(self, node):
         super().__init__(node, 'condenser_api')
 
+    def get_account_history(self, account, start, limit):
+        return self._send('get_account_history', [account, start, limit])
+
     def get_key_references(self, keys):
         return self._send('get_key_references', keys)
