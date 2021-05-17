65eb062ee694708d12cca4ae7b38bfe020ed40f0
Piotr Batko <pbatko@syncad.com>
2021-05-17 12:12:13 +0200

    Handle condenser api way of handling parameters
    Other apis take parameters as dictionaries, but condenser_api takes lists.

--- package/test_tools/node_api/condenser_api.py
+++ package/test_tools/node_api/condenser_api.py
@@ -1,6 +1,14 @@
-from .api_base import ApiBase
+from .api_base import ApiBase, NodeApiCallProxy
+
+
+class CondenserApiCallProxy(NodeApiCallProxy):
+    @staticmethod
+    def _prepare_params(*args, **kwargs):
+        return list(args)
 
 
 class CondenserApi(ApiBase):
+    _NodeApiCallProxyType = CondenserApiCallProxy
+
     def __init__(self, node):
         super().__init__(node, 'condenser_api')
