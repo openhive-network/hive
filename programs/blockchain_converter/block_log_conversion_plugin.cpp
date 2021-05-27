5cff36b2b5bc8b7aa434223ecaf2b7fa81291943
mtyszczak <mateusz.tyszczak@gmail.com>
2021-05-27 23:30:15 +0200

    Make blockchain_converter an appbase app
    - Added block_log_conversion plugin

---
+++ programs/blockchain_converter/block_log_conversion_plugin.cpp
@@ -0,0 +1,225 @@
+#include "block_log_conversion_plugin.hpp"
+
+#include <appbase/application.hpp>
+
+#include <fc/exception/exception.hpp>
+#include <fc/optional.hpp>
+#include <fc/filesystem.hpp>
+#include <fc/io/json.hpp>
+#include <fc/variant.hpp>
+
+#include <hive/chain/block_log.hpp>
+
+#include <hive/protocol/block.hpp>
+
+#include <hive/utilities/key_conversion.hpp>
+
+#include <boost/program_options.hpp>
+
+#include <memory>
+
+#include "converter.hpp"
+
+namespace bpo = boost::program_options;
+
+namespace hive {
+
+using namespace chain;
+using namespace utilities;
+using namespace protocol;
+
+namespace converter { namespace plugins { namespace block_log_conversion {
+
+namespace detail {
+
+  class block_log_conversion_plugin_impl {
+  public:
+    block_log log_in, log_out;
+    uint32_t log_per_block, log_specific;
+    blockchain_converter converter;
+
+    block_log_conversion_plugin_impl( const private_key_type& _private_key, const chain_id_type& chain_id = HIVE_CHAIN_ID )
+      : converter( _private_key, chain_id ) {}
+
+    void convert( uint32_t start_block_num, uint32_t stop_block_num );
+    void open( const fc::path& input, const fc::path& output );
+    void close();
+    void print_wifs();
+  };
+
+  void block_log_conversion_plugin_impl::open( const fc::path& input, const fc::path& output )
+  {
+    try
+    {
+      log_in.open( input );
+    } FC_CAPTURE_AND_RETHROW( (input) )
+
+    try
+    {
+      log_out.open( output );
+    } FC_CAPTURE_AND_RETHROW( (output) )
+
+  }
+
+  void block_log_conversion_plugin_impl::convert( uint32_t start_block_num, uint32_t stop_block_num )
+  {
+    FC_ASSERT( log_in.head(), "Your input block log is empty" ); // TODO: Other way to check if input block_log is empty
+
+    if( !start_block_num )
+      start_block_num = log_out.head() ? log_out.read_head().block_num() : 1;
+
+    if( !stop_block_num || stop_block_num > log_in.head()->block_num() )
+      stop_block_num = log_in.head()->block_num();
+
+    block_id_type last_block_id{};
+
+    for( ; start_block_num <= stop_block_num && !appbase::app().is_interrupt_request(); ++start_block_num )
+    {
+      fc::optional< signed_block > block = log_in.read_block_by_num( start_block_num );
+      FC_ASSERT( block.valid(), "unable to read block" );
+
+      if ( ( log_per_block > 0 && start_block_num % log_per_block == 0 ) || log_specific == start_block_num )
+      {
+        fc::json json_block;
+        fc::variant v;
+        fc::to_variant( *block, v );
+
+        std::cout << "Rewritten block: " << start_block_num
+          << ". Data before conversion: " << json_block.to_string( v ) << '\n';
+      }
+
+      last_block_id = converter.convert_signed_block( *block, last_block_id );
+
+      if( start_block_num % 1000 == 0 ) // Progress
+      {
+        std::cout << "[ " << int( float(start_block_num) / log_in.head()->block_num() * 100 ) << "% ]: " << start_block_num << '/' << log_in.head()->block_num() << " blocks rewritten.\r";
+        std::cout.flush();
+      }
+
+      log_out.append( *block );
+
+      if ( ( log_per_block > 0 && start_block_num % log_per_block == 0 ) || log_specific == start_block_num )
+      {
+        fc::json json_block;
+        fc::variant v;
+        fc::to_variant( *block, v );
+
+        std::cout << "After conversion: " << json_block.to_string( v ) << '\n';
+      }
+    }
+
+    if( !appbase::app().is_interrupt_request() )
+      appbase::app().generate_interrupt_request();
+  }
+
+  void block_log_conversion_plugin_impl::close()
+  {
+    log_in.close();
+    log_out.close();
+    if( block_header::num_from_id( converter.get_previous_block_id() ) + 1 <= HIVE_HARDFORK_0_17_BLOCK_NUM )
+      std::cerr << "\nSecond authority has not been applied on the accounts yet! Try resuming the conversion process\n";
+  }
+
+  void block_log_conversion_plugin_impl::print_wifs()
+  {
+    std::cout << "\nSecond authority wif private keys:\n"
+      << "Owner:   " << key_to_wif( converter.get_second_authority_key( authority::owner ) ) << '\n'
+      << "Active:  " << key_to_wif( converter.get_second_authority_key( authority::active ) ) << '\n'
+      << "Posting: " << key_to_wif( converter.get_second_authority_key( authority::posting ) ) << '\n';
+  }
+
+
+} // detail
+
+  block_log_conversion_plugin::block_log_conversion_plugin() {}
+  block_log_conversion_plugin::~block_log_conversion_plugin() {}
+
+  void block_log_conversion_plugin::set_program_options( bpo::options_description& cli, bpo::options_description& cfg )
+  {
+    cli.add_options()
+      ("input,i", bpo::value< std::string >()->required(), "input block log")
+      ("output,o", bpo::value< std::string >(), "output block log; defaults to [input]_out" );
+  }
+
+  void block_log_conversion_plugin::plugin_initialize( const bpo::variables_map& options )
+  {
+    std::string out_file;
+    if( !options.count("output") )
+      out_file = options["input"].as< std::string >() + "_out";
+    else
+      out_file = options["output"].as< std::string >();
+
+    fc::path block_log_in( options["input"].as< std::string >() );
+    fc::path block_log_out( out_file );
+
+    chain_id_type _hive_chain_id;
+
+    auto chain_id_str = options["chain-id"].as< std::string >();
+
+    try
+    {
+      _hive_chain_id = chain_id_type( chain_id_str );
+    }
+    catch( fc::exception& )
+    {
+      FC_ASSERT( false, "Could not parse chain_id as hex string. Chain ID String: ${s}", ("s", chain_id_str) );
+    }
+
+    fc::optional< private_key_type > private_key = wif_to_key( options["private-key"].as< std::string >() );
+    FC_ASSERT( private_key.valid(), "unable to parse the private key" );
+
+    my = std::make_unique< detail::block_log_conversion_plugin_impl >( *private_key, _hive_chain_id );
+
+    my->log_per_block = options["log-per-block"].as< uint32_t >();
+    my->log_specific = options["log-specific"].as< uint32_t >();
+
+    try
+    {
+      my->open( block_log_in, block_log_out );
+    } FC_CAPTURE_AND_RETHROW()
+
+    private_key_type owner_key;
+    private_key_type active_key;
+    private_key_type posting_key;
+    fc::optional< private_key_type > _owner_key = wif_to_key( options.count("owner-key") ? options["owner-key"].as< std::string >() : "" );
+    fc::optional< private_key_type > _active_key = wif_to_key( options.count("active-key") ? options["active-key"].as< std::string >() : "" );
+    fc::optional< private_key_type > _posting_key = wif_to_key( options.count("posting-key") ? options["posting-key"].as< std::string >() : "" );
+
+    if( _owner_key.valid() )
+      owner_key = *_owner_key;
+    else if( options.count("use-same-key") )
+      owner_key = *private_key;
+    else
+      owner_key = private_key_type::generate();
+
+    if( _active_key.valid() )
+      active_key = *_active_key;
+    else if( options.count("use-same-key") )
+      active_key = *private_key;
+    else
+      active_key = private_key_type::generate();
+
+    if( _posting_key.valid() )
+      posting_key = *_posting_key;
+    else if( options.count("use-same-key") )
+      posting_key = *private_key;
+    else
+      posting_key = private_key_type::generate();
+
+    my->converter.set_second_authority_key( owner_key, authority::owner );
+    my->converter.set_second_authority_key( active_key, authority::active );
+    my->converter.set_second_authority_key( posting_key, authority::posting );
+
+    stop_block_num = options.at("stop-at-block").as< uint32_t >();
+  }
+
+  void block_log_conversion_plugin::plugin_startup() {
+    my->convert( 0, stop_block_num );
+  }
+  void block_log_conversion_plugin::plugin_shutdown()
+  {
+    my->close();
+    my->print_wifs();
+  }
+
+} } } } // hive::converter::plugins::block_log_conversion
--- programs/blockchain_converter/main.cpp
+++ programs/blockchain_converter/main.cpp
@@ -1,198 +1,69 @@
 #include <boost/exception/diagnostic_information.hpp>
 #include <boost/program_options.hpp>
 
-#include <fc/filesystem.hpp>
+#include <appbase/application.hpp>
+
+#include <string>
+#include <exception>
+
+#include <hive/utilities/git_revision.hpp>
+#include <hive/protocol/config.hpp>
+
 #include <fc/exception/exception.hpp>
-#include <fc/io/json.hpp>
 
-#include <hive/utilities/key_conversion.hpp>
+#include "block_log_conversion_plugin.hpp"
 
-#include <hive/protocol/authority.hpp>
-
-#include <hive/chain/block_log.hpp>
-
-#include <iostream>
-#include <memory>
-#include <csignal>
-
-#include "converter.hpp"
-
-using namespace hive::chain;
+using namespace hive::converter;
 using namespace hive::protocol;
 using namespace hive::utilities;
 using namespace hive::converter;
 namespace bpo = boost::program_options;
 
-namespace
+std::string& version_string()
 {
-  volatile sig_atomic_t stop_flag = 0; // signal flag
+  static std::string v_str =
+    "  \"version\" : { \"hive_blockchain_hard_fork\" : \""   + std::string( HIVE_BLOCKCHAIN_VERSION )            + "\", " +
+    "\"hive_git_revision\" : \""                             + std::string( git_revision_sha )  + "\" }";
+
+  return v_str;
 }
-void signal_handler( int signal ) { stop_flag = signal; }
 
 int main( int argc, char** argv )
 {
   try
   {
-    signal( SIGINT, signal_handler );
+    auto& bc_converter_app = appbase::app();
 
     // Setup converter options
-    bpo::options_description generic_opts{"Generic options"};
-      generic_opts.add_options()
-      ("help,h", "Print this help message and exit.");
-    bpo::options_description blocklog_opts{"Block log options"};
-      blocklog_opts.add_options()
-      ("input,i", bpo::value< std::string >(), "input block log")
-      ("output,o", bpo::value< std::string >(), "output block log; defaults to [input]_out" );
+    bpo::options_description logging_opts{"Logging options"};
+      logging_opts.add_options()
+      ("log-per-block,l", bpo::value< uint32_t >()->default_value( 0 ), "displays blocks in JSON format every n blocks")
+      ("log-specific,s", bpo::value< uint32_t >()->default_value( 0 ), "displays only block with specified number");
     bpo::options_description conversion_opts{"Conversion options"};
       conversion_opts.add_options()
-      ("chain-id,c", bpo::value< std::string >(), "new chain ID")
-      ("private-key,k", bpo::value< std::string >(), "private key with which all transactions and blocks will be signed ")
+      ("chain-id,C", bpo::value< std::string >()->required(), "new chain ID")
+      ("private-key,K", bpo::value< std::string >()->required(), "private key with which all transactions and blocks will be signed ")
       ("owner-key,O", bpo::value< std::string >(), "owner key of the second authority")
       ("active-key,A", bpo::value< std::string >(), "active key of the second authority")
       ("posting-key,P", bpo::value< std::string >(), "posting key of the second authority")
-      ("use-same-key,U", "use given private key as the owner, active and posting keys if not specified");
-    bpo::options_description logging_opts{"Logging options"};
-      logging_opts.add_options()
-      ("log-per-block,l", bpo::value< uint32_t >()->default_value( 0 )->implicit_value( 1 ), "Displays blocks in JSON format every n blocks")
-      ("log-specific,s", bpo::value< uint32_t >()->default_value( 0 ), "Displays only block with specified number");
+      ("use-same-key,U", "use given private key as the owner, active and posting keys if not specified")
+      ("stop-at-block,S", bpo::value< uint32_t >()->default_value( 0 ), "stop conversion at the given block number");
 
-    bpo::options_description cmdline_options;
-      cmdline_options.add(generic_opts).add(blocklog_opts).add(conversion_opts).add(logging_opts);
+    bc_converter_app.add_program_options( conversion_opts, logging_opts );
 
-    bpo::positional_options_description pos_opts;
-    pos_opts.add("input", -1);
+    // TODO: bc_converter_app.register_plugin< plugins::node_based_conversion::node_based_conversion_plugin >();
+    bc_converter_app.register_plugin< plugins::block_log_conversion::block_log_conversion_plugin >();
 
-    bpo::variables_map options;
-    bpo::store( bpo::command_line_parser(argc, argv).options(cmdline_options).positional(pos_opts).run(), options );
+    bc_converter_app.set_default_plugins< plugins::block_log_conversion::block_log_conversion_plugin >();
 
-    if( options.count("help") || !options.count("input") || !options.count("private-key") || !options.count("chain-id") )
-    {
-      std::cout << "Changes chain id of the block log and adds second authority to all the accounts. Re-signs blocks using given private key.\n"
-        << cmdline_options << "\n";
-      return 0;
-    }
+    bc_converter_app.set_version_string( version_string() );
+    bc_converter_app.set_app_name( "blockchain_converter" );
 
-    uint32_t log_per_block = options["log-per-block"].as< uint32_t >();
-    uint32_t log_specific = options["log-specific"].as< uint32_t >();
+    if( !bc_converter_app.initialize( argc, argv ) )
+      return -1;
 
-    std::string out_file;
-    if( !options.count("output") )
-      out_file = options["input"].as< std::string >() + "_out";
-    else
-      out_file = options["output"].as< std::string >();
-
-    fc::path block_log_in( options["input"].as< std::string >() );
-    fc::path block_log_out( out_file );
-
-    chain_id_type _hive_chain_id;
-
-    auto chain_id_str = options["chain-id"].as< std::string >();
-
-    try
-    {
-      _hive_chain_id = chain_id_type( chain_id_str );
-    }
-    catch( fc::exception& )
-    {
-      FC_ASSERT( false, "Could not parse chain_id as hex string. Chain ID String: ${s}", ("s", chain_id_str) );
-    }
-
-    fc::optional< private_key_type > private_key = wif_to_key( options["private-key"].as< std::string >() );
-    FC_ASSERT( private_key.valid(), "unable to parse the private key" );
-
-    block_log log_in, log_out;
-
-    log_in.open( block_log_in );
-    log_out.open( block_log_out );
-
-    blockchain_converter converter( *private_key, _hive_chain_id );
-
-    private_key_type owner_key;
-    private_key_type active_key;
-    private_key_type posting_key;
-    fc::optional< private_key_type > _owner_key = wif_to_key( options.count("owner-key") ? options["owner-key"].as< std::string >() : "" );
-    fc::optional< private_key_type > _active_key = wif_to_key( options.count("active-key") ? options["active-key"].as< std::string >() : "" );
-    fc::optional< private_key_type > _posting_key = wif_to_key( options.count("posting-key") ? options["posting-key"].as< std::string >() : "" );
-
-    if( _owner_key.valid() )
-      owner_key = *_owner_key;
-    else if( options.count("use-same-key") )
-      owner_key = *private_key;
-    else
-      owner_key = private_key_type::generate();
-
-    if( _active_key.valid() )
-      active_key = *_active_key;
-    else if( options.count("use-same-key") )
-      active_key = *private_key;
-    else
-      active_key = private_key_type::generate();
-
-    if( _posting_key.valid() )
-      posting_key = *_posting_key;
-    else if( options.count("use-same-key") )
-      posting_key = *private_key;
-    else
-      posting_key = private_key_type::generate();
-
-    converter.set_second_authority_key( owner_key, authority::owner );
-    converter.set_second_authority_key( active_key, authority::active );
-    converter.set_second_authority_key( posting_key, authority::posting );
-
-    block_id_type last_block_id = log_out.head() ? log_out.read_head().id() : block_id_type();
-
-    FC_ASSERT( log_in.head(), "Your input block log is empty" );
-
-    uint32_t block_num;
-
-    for( block_num = block_header::num_from_id( last_block_id ) + 1; block_num <= log_in.head()->block_num() && !stop_flag; ++block_num )
-    {
-      fc::optional< signed_block > block = log_in.read_block_by_num( block_num );
-      FC_ASSERT( block.valid(), "unable to read block" );
-
-      if ( ( log_per_block > 0 && block_num % log_per_block == 0 ) || log_specific == block_num )
-      {
-        fc::json json_block;
-        fc::variant v;
-        fc::to_variant( *block, v );
-
-        std::cout << "Rewritten block: " << block_num
-          << ". Data before conversion: " << json_block.to_string( v ) << '\n';
-      }
-
-      last_block_id = converter.convert_signed_block( *block, last_block_id );
-
-      if( block_num % 1000 == 0 ) // Progress
-      {
-        std::cout << "[ " << int( float(block_num) / log_in.head()->block_num() * 100 ) << "% ]: " << block_num << '/' << log_in.head()->block_num() << " blocks rewritten.\r";
-        std::cout.flush();
-      }
-
-      log_out.append( *block );
-
-      if ( ( log_per_block > 0 && block_num % log_per_block == 0 ) || log_specific == block_num )
-      {
-        fc::json json_block;
-        fc::variant v;
-        fc::to_variant( *block, v );
-
-        std::cout << "After conversion: " << json_block.to_string( v ) << '\n';
-      }
-    }
-
-    if( stop_flag )
-      std::cerr << "\nUser interrupt detected! Saving conversion state...";
-    if( block_num <= HIVE_HARDFORK_0_17_BLOCK_NUM )
-      std::cerr << "\nSecond authority has not been applied on the accounts yet! Try resuming the conversion process";
-
-    log_in.close();
-    log_out.close();
-
-    std::cout << "\nSecond authority wif private keys:\n"
-      << "Owner:   " << key_to_wif( owner_key ) << '\n'
-      << "Active:  " << key_to_wif( active_key ) << '\n'
-      << "Posting: " << key_to_wif( posting_key ) << '\n'
-      << "block_log conversion completed\n";
+    bc_converter_app.startup();
+    bc_converter_app.exec();
 
     return 0;
   }
