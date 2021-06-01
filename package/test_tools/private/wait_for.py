6b6c48bb81af65d008956f9b72d2eff5a83cb023
Piotr Batko <pbatko@syncad.com>
2021-06-01 12:43:21 +0200

    Move default timeout error message to function body

--- package/test_tools/private/wait_for.py
+++ package/test_tools/private/wait_for.py
@@ -1,13 +1,13 @@
 import math
 import time
 
-def wait_for(predicate, timeout=math.inf, timeout_error_message='Waited too long, timeout was reached', poll_time=1.0):
+def wait_for(predicate, timeout=math.inf, timeout_error_message=None, poll_time=1.0):
     assert timeout >= 0
 
     already_waited = 0
     while not predicate():
         if timeout - already_waited <= 0:
-            raise TimeoutError(timeout_error_message)
+            raise TimeoutError(timeout_error_message or 'Waited too long, timeout was reached')
 
         sleep_time = min(poll_time, timeout)
         time.sleep(sleep_time)
