6926f40297682e495ad1ff1e636e05f93a8793f6
Piotr Batko <pbatko@syncad.com>
2021-06-01 13:43:47 +0200

    Make supported plugins available as plugin member

--- package/test_tools/node_config_entry_types/specialized/plugin_entry.py
+++ package/test_tools/node_config_entry_types/specialized/plugin_entry.py
@@ -2,22 +2,22 @@ from ..untouched_entry import Untouched
 
 
 class Plugin(Untouched):
+    SUPPORTED_PLUGINS = [
+        'account_by_key', 'account_by_key_api', 'account_history', 'account_history_api', 'account_history_rocksdb',
+        'block_api', 'block_data_export', 'block_log_info', 'chain', 'chain_api', 'comment_cashout_logging',
+        'condenser_api', 'database_api', 'debug_node', 'debug_node_api', 'follow', 'follow_api', 'json_rpc',
+        'market_history', 'market_history_api', 'network_broadcast_api', 'network_node_api', 'p2p', 'rc', 'rc_api',
+        'reputation', 'reputation_api', 'rewards_api', 'state_snapshot', 'stats_export', 'statsd', 'tags',
+        'tags_api', 'test_api', 'transaction_status', 'transaction_status_api', 'webserver', 'witness',
+    ]
+
     @classmethod
     def __check_if_plugin_is_supported(cls, plugin):
-        supported_plugins = [
-            'account_by_key', 'account_by_key_api', 'account_history', 'account_history_api', 'account_history_rocksdb',
-            'block_api', 'block_data_export', 'block_log_info', 'chain', 'chain_api', 'comment_cashout_logging',
-            'condenser_api', 'database_api', 'debug_node', 'debug_node_api', 'follow', 'follow_api', 'json_rpc',
-            'market_history', 'market_history_api', 'network_broadcast_api', 'network_node_api', 'p2p', 'rc', 'rc_api',
-            'reputation', 'reputation_api', 'rewards_api', 'state_snapshot', 'stats_export', 'statsd', 'tags',
-            'tags_api', 'test_api', 'transaction_status', 'transaction_status_api', 'webserver', 'witness',
-        ]
-
-        if plugin not in supported_plugins:
+        if plugin not in cls.SUPPORTED_PLUGINS:
             raise ValueError(
                 f'Plugin "{plugin}" is not supported.\n'
                 + 'List of supported plugins:\n'
-                + '\n'.join([f'- {supported}' for supported in supported_plugins])
+                + '\n'.join([f'- {supported}' for supported in cls.SUPPORTED_PLUGINS])
             )
 
     @classmethod
