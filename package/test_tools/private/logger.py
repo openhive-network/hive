289326f78d8199d6f14f0f3aa261c3acd1aa72c8
Piotr Batko <pbatko@syncad.com>
2021-06-15 14:16:39 +0200

    Add a method allowing setting directory for logs

--- package/test_tools/private/logger.py
+++ package/test_tools/private/logger.py
@@ -4,24 +4,29 @@ from pathlib import Path
 
 class Logger:
     def __init__(self):
-        self.__log_path = Path('logs/run.log')
-        self.__stream_handler = logging.NullHandler()
-        self.__file_handler = logging.NullHandler()
-        self.__initialized = False
+        self.__directory = Path('logs/')
+        self.__stream_handler = None
+        self.__file_handler = None
 
     def __ensure_initialization(self):
-        if self.__initialized:
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s (%(name)s, %(filename)s:%(lineno)s)')
+
+        if not self.__is_initialized():
+            self.__initialize(formatter)
             return
 
-        self.__initialized = True
-        self.__initialize()
+        if self.__file_handler is None:
+            self.__initialize_file_handler(formatter)
 
-    def __initialize(self):
+    def __is_initialized(self):
+        return self.__stream_handler is not None
+
+    def __initialize(self, formatter):
         self.__remove_old_log_if_exists()
 
         logging.root.setLevel(logging.DEBUG)
 
-        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s (%(name)s, %(filename)s:%(lineno)s)')
+        self.__initialize_file_handler(formatter)
 
         # Configure stream handler
         from sys import stdout
@@ -30,25 +35,39 @@ class Logger:
         self.__stream_handler.setLevel(logging.INFO)
         logging.root.addHandler(self.__stream_handler)
 
-        # Configure file handler
-        self.__log_path.parent.mkdir(exist_ok=True)
-        self.__file_handler = logging.FileHandler(self.__log_path)
+        # Suppress debug logs from selected built-in python libraries
+        logging.getLogger('urllib3.connectionpool').setLevel(logging.INFO)
+
+    def __initialize_file_handler(self, formatter):
+        self.__get_log_path().parent.mkdir(exist_ok=True, parents=True)
+        self.__file_handler = logging.FileHandler(self.__get_log_path(), mode='w')
         self.__file_handler.setFormatter(formatter)
         self.__file_handler.setLevel(logging.DEBUG)
         logging.root.addHandler(self.__file_handler)
 
-        # Suppress debug logs from selected built-in python libraries
-        logging.getLogger('urllib3.connectionpool').setLevel(logging.INFO)
+    def __deinitialize_file_handler(self):
+        self.__file_handler.close()
+        logging.root.removeHandler(self.__file_handler)
+        self.__file_handler = None
 
     def __remove_old_log_if_exists(self):
         try:
-            self.__log_path.unlink()
+            self.__get_log_path().unlink()
         except FileNotFoundError:
             pass
+
+    def __get_log_path(self):
+        return self.__directory / 'run.log'
 
     def show_debug_logs_on_stdout(self):
         self.__ensure_initialization()
         __stream_handler.setLevel(logging.DEBUG)
+
+    def set_directory(self, directory):
+        if self.__file_handler is not None:
+            self.__deinitialize_file_handler()
+
+        self.__directory = Path(directory)
 
     # Wrapped functions from logging library
     def debug(self, message):
