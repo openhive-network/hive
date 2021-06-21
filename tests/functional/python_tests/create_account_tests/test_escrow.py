0852fcdac2d1f28d8bd26a0f0fb2e32a21e5acb3
mtrela <mtrela@syncad.com>
2021-06-21 13:12:39 +0200

    Small fixes - part 2

--- tests/functional/python_tests/create_account_tests/test_escrow.py
+++ tests/functional/python_tests/create_account_tests/test_escrow.py
@@ -9,9 +9,6 @@ def test_escrow():
 
         wallet = init_node.attach_wallet()
 
-        logger.info('Waiting until initminer will be able to create account...')
-        init_node.wait_number_of_blocks(3)
-
         #**************************************************************
         logger.info('create_account...')
         response = wallet.api.create_account('initminer', 'alice', '{}')
