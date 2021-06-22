f20d33e0bd18a8ca49123954b9fd5024d57971dc
Marcin Sobczyk <msobczyk@syncad.com>
2021-06-22 13:56:45 +0200

    add test fixture and split scenarios

--- tests/functional/python_tests/last_irreversible_block_tests/README.md
+++ tests/functional/python_tests/last_irreversible_block_tests/README.md
@@ -1,19 +1,15 @@
 # Fork Tests
 
 ## Scenarios
+    Some scenarios require long and similar setup, therefore we use shared fixtures to perform setup only once. Required
+    setup in fixture 'world_with_witnesses' is 21 active witnesses producing blocks and irreversible block number should
+    be behing head block like in real network (15-20 blocks). With default config (required_participation_rate=33) we must wait
+    until block 63 to schedule any witness other then initminer (before required_participation_rate is to low and blocks won't be produced).
+    Then we must wait for another 21 blocks for every witness to sign at least one block (so last irreversible block proceeds).
+    Every witness must sign at least one block becouse this is required by formula for last irreversible block (else last irreversible
+    block will be equal to head block).
 
-### test_account_history_duplicates
-    pytest -s test_account_history_duplicates.py
-
-    There are 3 scenarios in one test case, becouse these scenarios require long and similar setup. Required setup is
-    21 active witnesses producing blocks and irreversible block number should be behing head block like in real network (15-20 blocks).
-    With default config (required_participation_rate=33) we must wait until block 63 to schedule any witness other then
-    initminer (before required_participation_rate is to low and blocks won't be produced). Then we must wait another
-    21 blocks for every witness to sign at least one block (so last irreversible block proceeds). Every witness must sign
-    at least one block becouse this is required by formula for last irreversible block (else last irreversible block will be
-    equal to head block).
-
-#### i scenario
+### test_no_ah_duplicates_after_fork
     PREREQUISITES
     There exists 8 witness nodes, 4 marked as alpha and 4 marked as beta. There is also initminer and one api node in each subnetwork.
     Witnesses are equally divided between those nodes (half of witnesses are in alpha part, other half in beta part). Last irreversible
@@ -24,20 +20,20 @@
     After some time (when irreversible block number increases in both subnetworks) we join alpha and beta part again
 
     VERIFY
-    Expected behaviour is that nodes in one of subnetworks (random one) will perform undo and synchronize with other subnetwork.
+    Expected behaviour is that nodes in one of subnetworks (random one, alpha or beta) will perform undo and synchronize with other subnetwork.
     We check there are no duplicates in account_history_api after such scenario (issue #117).
-
-#### ii scenario
+### test_no_ah_duplicates_after_restart
     PREREQUISITES
-    It is required that last irreversible block is behind head block about 15-20 blocks (like in real network).
+    Node has account_history_api and account_history_rocksdb plugins turned on, and last irreversible block is behind head block about 15-20 blocks
+    (like in real network).
 
     TRIGGER
     We restart one of nodes.
 
     VERIFY
-    Expected behavour is that after restart node will synchronize and there are no duplicates in account_history_api (issue #117).
+    Expected behavour is that after restart node will enter live sync and there are no duplicates in account_history_api (issue #117).
 
-#### iii scenatio
+### test_enum_virtual_ops
     PREREQUISITES
     Node has account_history_api and account_history_rocksdb plugins turned on, and last irreversible block is behind head block about 15-20 blocks
     (like in real network).
@@ -49,8 +45,6 @@
     We check that query account_history_api.enum_virtual_ops returns only virtual operations (issue #139).
 
 ### test_irreversible_block_while_sync
-    pytest -s test_irreversible_block_while_sync.py
-
     PREREQUISITES
     We need to be before head block number HIVE_START_MINER_VOTING_BLOCK constant (30 in testnet). Also head block number
     must be greater then HIVE_MAX_WITNESSES (21 in both testnet and mainnet).
--- tests/functional/python_tests/last_irreversible_block_tests/conftest.py
+++ tests/functional/python_tests/last_irreversible_block_tests/conftest.py
@@ -1,8 +1,95 @@
 import pytest
-
-from test_tools import automatic_tests_configuration
+from test_tools import automatic_tests_configuration, logger, Account, World
 
 
 @pytest.fixture(autouse=True)
 def configure_test_tools_paths(request):
     automatic_tests_configuration.configure_test_tools_paths(request)
+
+
+@pytest.fixture(scope="package")
+def world_with_witnesses():
+    """
+    Fixture consists of world with 1 init node, 8 witness nodes and 2 api nodes.
+    After fixture creation there are 21 active witnesses, and last irreversible block
+    is behind head block like in real network.
+    """
+    logger.info('Preparing fixture world_with_witnesses')
+    with World() as world:
+        alpha_witness_names = [f'witness{i}-alpha' for i in range(10)]
+        beta_witness_names = [f'witness{i}-beta' for i in range(10)]
+        all_witness_names = alpha_witness_names + beta_witness_names
+
+        # Create first network
+        alpha_net = world.create_network('Alpha')
+        init_node = alpha_net.create_init_node()
+        alpha_net.create_witness_node(witnesses=[f'witness{i}-alpha' for i in range(0, 3)])
+        alpha_net.create_witness_node(witnesses=[f'witness{i}-alpha' for i in range(3, 6)])
+        alpha_net.create_witness_node(witnesses=[f'witness{i}-alpha' for i in range(6, 8)])
+        alpha_net.create_witness_node(witnesses=[f'witness{i}-alpha' for i in range(8, 10)])
+        api_node = alpha_net.create_api_node()
+
+        # Create second network
+        beta_net = world.create_network('Beta')
+        beta_net.create_witness_node(witnesses=[f'witness{i}-beta' for i in range(0, 3)])
+        beta_net.create_witness_node(witnesses=[f'witness{i}-beta' for i in range(3, 6)])
+        beta_net.create_witness_node(witnesses=[f'witness{i}-beta' for i in range(6, 8)])
+        beta_net.create_witness_node(witnesses=[f'witness{i}-beta' for i in range(8, 10)])
+        beta_net.create_api_node()
+
+        # Run
+        alpha_net.connect_with(beta_net)
+
+        logger.info('Running networks, waiting for live...')
+        alpha_net.run()
+        beta_net.run()
+
+        logger.info('Attaching wallets...')
+        wallet = api_node.attach_wallet()
+        # We are waiting here for block 43, because witness participation is counting
+        # by dividing total produced blocks in last 128 slots by 128. When we were waiting
+        # too short, for example 42 blocks, then participation equals 42 / 128 = 32.81%.
+        # It is not enough, because 33% is required. 43 blocks guarantee, that this
+        # requirement is always fulfilled (43 / 128 = 33.59%, which is greater than 33%).
+        logger.info('Wait for block 43 (to fulfill required 33% of witness participation)')
+        init_node.wait_for_block_with_number(43)
+
+        # Prepare witnesses on blockchain
+        with wallet.in_single_transaction():
+            for name in all_witness_names:
+                wallet.api.create_account('initminer', name, '')
+        with wallet.in_single_transaction():
+            for name in all_witness_names:
+                wallet.api.transfer_to_vesting("initminer", name, "1000.000 TESTS")
+        with wallet.in_single_transaction():
+            for name in all_witness_names:
+                wallet.api.update_witness(
+                    name, "https://" + name,
+                    Account(name).public_key,
+                    {"account_creation_fee": "3.000 TESTS", "maximum_block_size": 65536, "sbd_interest_rate": 0}
+                )
+
+        logger.info('Wait 21 blocks to schedule newly created witnesses')
+        init_node.wait_number_of_blocks(21)
+
+        logger.info("Witness state after voting")
+        response = api_node.api.database.list_witnesses(start=0, limit=100, order='by_name')
+        active_witnesses = response["result"]["witnesses"]
+        active_witnesses_names = [witness["owner"] for witness in active_witnesses]
+        logger.info(active_witnesses_names)
+        assert len(active_witnesses_names) == 21
+
+        # Reason of this wait is to enable moving forward of irreversible block
+        logger.info('Wait 21 blocks (when every witness sign at least one block)')
+        init_node.wait_number_of_blocks(21)
+
+        # Network should be set up at this time, with 21 active witnesses, enough participation rate
+        # and irreversible block number lagging behind around 15-20 blocks head block number
+        result = wallet.api.info()["result"]
+        irreversible = result["last_irreversible_block_num"]
+        head = result["head_block_num"]
+        logger.info(f'Network prepared, irreversible block: {irreversible}, head block: {head}')
+
+        assert irreversible + 10 < head
+
+        yield world
