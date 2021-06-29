4f0b907c1fe3a137c143cdeb65092c1e19c92e86
Piotr Batko <pbatko@syncad.com>
2021-06-29 16:26:05 +0200

    Add support for gettting preferred directory for package-fixtures

--- package/test_tools/automatic_tests_configuration/configure_test_tools_paths.py
+++ package/test_tools/automatic_tests_configuration/configure_test_tools_paths.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pytest
 
 from test_tools import logger, World
 
@@ -9,9 +10,24 @@ def get_preferred_directory(request):
         module_name = Path(request.module.__file__).stem
         test_case_name = request.function.__name__
 
-        return current_directory / ('generated_during_' + module_name) / test_case_name
+        return current_directory / f'generated_during_{module_name}' / test_case_name
+    elif request.scope == 'package':
+        return __get_package_directory(request) / f'generated_by_fixture_{request.fixturename}'
     else:
-        raise RuntimeError('Unsupported pytest.fixture scope, supported is only function scope')
+        raise RuntimeError('Unsupported pytest.fixture scope, supported is only function and package scopes')
+
+
+def __get_package_directory(request):
+    assert request.scope == 'package'
+
+    if isinstance(request.node, pytest.Session):
+        package = request.node.items[0].parent.parent
+    elif isinstance(request.node, pytest.Package):
+        package = request.node
+    else:
+        raise RuntimeError(f'Internal error: request.node has unsupported type: {type(request.node)}')
+
+    return Path(package.module.__file__).parent
 
 
 def configure_test_tools_paths(request):
