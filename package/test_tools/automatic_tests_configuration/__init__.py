64bec7670f4b7611414f3d704d2adf3324cccf07
Piotr Batko <pbatko@syncad.com>
2021-06-29 13:27:26 +0200

    Extract to function getting directory for generated files of fixture

--- package/test_tools/automatic_tests_configuration/__init__.py
+++ package/test_tools/automatic_tests_configuration/__init__.py
@@ -1,1 +1,1 @@
-from .configure_test_tools_paths import configure_test_tools_paths
+from .configure_test_tools_paths import configure_test_tools_paths, get_preferred_directory
--- package/test_tools/automatic_tests_configuration/configure_test_tools_paths.py
+++ package/test_tools/automatic_tests_configuration/configure_test_tools_paths.py
@@ -2,12 +2,20 @@ from pathlib import Path
 
 from test_tools import logger, World
 
-def configure_test_tools_paths(request):
-    current_directory = Path(request.module.__file__).parent
-    module_name = Path(request.module.__file__).stem
-    test_case_name = request.function.__name__
 
-    default_directory = current_directory / ('generated_during_' + module_name) / test_case_name
+def get_preferred_directory(request):
+    if request.scope == 'function':
+        current_directory = Path(request.module.__file__).parent
+        module_name = Path(request.module.__file__).stem
+        test_case_name = request.function.__name__
+
+        return current_directory / ('generated_during_' + module_name) / test_case_name
+    else:
+        raise RuntimeError('Unsupported pytest.fixture scope, supported is only function scope')
+
+
+def configure_test_tools_paths(request):
+    default_directory = get_preferred_directory(request)
 
     logger.set_directory(default_directory)
     World.set_default_directory(default_directory)
