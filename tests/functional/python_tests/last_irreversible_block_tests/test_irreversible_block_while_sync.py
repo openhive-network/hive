8f677fc39cf0b1b2a9eacd8e862e4a26b014f4e2
Piotr Batko <pbatko@syncad.com>
2021-06-29 16:20:58 +0200

    Use new world fixture

--- tests/functional/python_tests/last_irreversible_block_tests/test_irreversible_block_while_sync.py
+++ tests/functional/python_tests/last_irreversible_block_tests/test_irreversible_block_while_sync.py
@@ -1,19 +1,18 @@
-from test_tools import logger, World
+from test_tools import logger
 
 
-def test_irreversible_block_while_sync():
-    with World() as world:
-        net = world.create_network()
-        init_node = net.create_init_node()
-        api_node = net.create_api_node()
+def test_irreversible_block_while_sync(world):
+    net = world.create_network()
+    init_node = net.create_init_node()
+    api_node = net.create_api_node()
 
-        logger.info('Running network, waiting for live sync...')
-        net.run()
+    logger.info('Running network, waiting for live sync...')
+    net.run()
 
-        # Wait for block number between constants HIVE_MAX_WITNESSES and HIVE_START_MINER_VOTING_BLOCK
-        logger.info('Waiting for block number 23...')
-        init_node.wait_for_block_with_number(23)
+    # Wait for block number between constants HIVE_MAX_WITNESSES and HIVE_START_MINER_VOTING_BLOCK
+    logger.info('Waiting for block number 23...')
+    init_node.wait_for_block_with_number(23)
 
-        # Test api_node will enter live sync after restart
-        logger.info("Restarting api node...")
-        api_node.restart(timeout=90)
+    # Test api_node will enter live sync after restart
+    logger.info("Restarting api node...")
+    api_node.restart(timeout=90)
