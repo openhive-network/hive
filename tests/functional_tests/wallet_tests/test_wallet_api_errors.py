cb6c7fde2cd7dc8f30196f9ac171adfc6376a5aa
Piotr Batko <pbatko@syncad.com>
2021-06-30 15:14:11 +0200

    Test if wallet raise when communication error occurrs

---
+++ tests/functional_tests/wallet_tests/test_wallet_api_errors.py
@@ -0,0 +1,22 @@
+import pytest
+
+from test_tools.exceptions import CommunicationError
+
+
+@pytest.fixture
+def wallet(world):
+    init_node = world.create_init_node()
+    init_node.run()
+
+    return init_node.attach_wallet()
+
+
+def test_if_raise_when_parameters_are_bad(wallet):
+    with pytest.raises(CommunicationError):
+        wallet.api.create_account('surely', 'bad', 'arguments')
+
+
+def test_if_raise_when_operation_is_invalid(wallet):
+    with pytest.raises(CommunicationError):
+        # Operation is invalid because account "alice" doesn't exists
+        wallet.api.transfer('initminer', 'alice', '1.000 TESTS', 'memo')
