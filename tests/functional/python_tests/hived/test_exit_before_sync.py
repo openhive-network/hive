32d629c0dcd38174301ec5d4c19cc7323d79527a
kmochocki <kmochocki@syncad.com>
2021-07-16 14:12:19 +0200

    implemented --exit-before-sync tests

--- tests/functional/python_tests/hived/test_exit_before_sync.py
+++ tests/functional/python_tests/hived/test_exit_before_sync.py
@@ -1,10 +1,34 @@
+from pathlib import Path
 from test_tools import *
+from .conftest import BLOCK_COUNT
+from shutil import rmtree
+from os import remove
+from os.path import join
 
-def test_exit_before_sync(world : World):
+
+def test_exit_before_sync(world : World, block_log : Path):
   '''
   with specified '--exit-before-sync' flag:
-  1. check exiting after dumping snapshot
-  2. check exiting after loading snapshot
-  3. check after replaying
-  4. check after load+replay combo (it should exit after replay)
+    1. check after replaying
+      - in the middle
+      - full
+    2. check exiting after dumping snapshot
+    3. check exiting after loading snapshot
   '''
+
+  init = world.create_api_node(name='node_1')
+  half_way = int(BLOCK_COUNT / 2.0)
+
+  init.run(replay_from=block_log, stop_at_block=half_way, exit_before_synchronization=True)
+  assert not init.is_running()
+
+  rmtree( join( str(init.directory), 'blockchain' ), ignore_errors=True)
+  init.run(replay_from=block_log, exit_before_synchronization=True)
+  assert not init.is_running()
+
+  snap = init.dump_snapshot(close=True)
+  assert not init.is_running()
+
+  remove( join( str(init.directory), 'blockchain', 'shared_memory.bin' ) )
+  init.run(load_snapshot_from=snap, exit_before_synchronization=True)
+  assert not init.is_running()
--- tests/functional/python_tests/hived/test_snapshot.py
+++ tests/functional/python_tests/hived/test_snapshot.py
@@ -3,34 +3,7 @@
 from pathlib import Path
 from pytest import fixture
 from test_tools import *
-
-BLOCK_COUNT = 30
-SNAPSHOT_NAME = 'snap_1'
-
-# @fixture(scope='module')
-@fixture()
-def block_log(world : World) -> Path:
-  from time import sleep
-  logger.info(f'preparing block log with {BLOCK_COUNT} blocks')
-  node = world.create_init_node()
-  node.run(wait_for_live=True)
-
-  wallet = node.attach_wallet()
-  initminer = node.config.witness[0]
-
-  current_block = 0
-  while current_block <= BLOCK_COUNT:
-    for _ in range(4):
-      # put here some dummy trx so block log does not contain just empty blocks
-      wallet.api.transfer_to_vesting(from_=initminer, to=initminer, amount=Asset.Test(0.001))
-
-      sleep(0.2)
-    current_block = node.get_last_block_number()
-
-  node.close()
-  return node.get_block_log().get_path()
-
-
+from .conftest import BLOCK_COUNT
 
 def test_snapshots(world : World, block_log : Path):
   '''
