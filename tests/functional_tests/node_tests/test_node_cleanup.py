03e9adea8ad82f8acaee4f135e504c4e2658012e
Piotr Batko <pbatko@syncad.com>
2021-07-19 15:43:38 +0200

    Refactor clean up tests to remove duplicated test steps

--- tests/functional_tests/node_tests/test_node_cleanup.py
+++ tests/functional_tests/node_tests/test_node_cleanup.py
@@ -2,46 +2,55 @@ from test_tools import constants
 
 
 def test_default_policy(world):
-    init_node = world.create_init_node()
-    init_node.run()
-
-    world.close()
-
-    assert not important_files_are_removed(init_node)
-    assert unneeded_files_are_removed(init_node)
+    check_if_files_are_removed(
+        world,
+        remove_important_files=False,
+        remove_unneeded_files=True
+    )
 
 
 def test_do_not_remove_files_clean_up_policy(world):
-    init_node = world.create_init_node()
-    init_node.set_clean_up_policy(constants.NodeCleanUpPolicy.DO_NOT_REMOVE_FILES)
-    init_node.run()
-
-    world.close()
-
-    assert not important_files_are_removed(init_node)
-    assert not unneeded_files_are_removed(init_node)
+    check_if_files_are_removed(
+        world,
+        constants.NodeCleanUpPolicy.DO_NOT_REMOVE_FILES,
+        remove_important_files=False,
+        remove_unneeded_files=False
+    )
 
 
 def test_remove_only_unneeded_files_clean_up_policy(world):
-    init_node = world.create_init_node()
-    init_node.set_clean_up_policy(constants.NodeCleanUpPolicy.REMOVE_ONLY_UNNEEDED_FILES)
-    init_node.run()
-
-    world.close()
-
-    assert not important_files_are_removed(init_node)
-    assert unneeded_files_are_removed(init_node)
+    check_if_files_are_removed(
+        world,
+        constants.NodeCleanUpPolicy.REMOVE_ONLY_UNNEEDED_FILES,
+        remove_important_files=False,
+        remove_unneeded_files=True
+    )
 
 
 def test_remove_everything_clean_up_policy(world):
+    check_if_files_are_removed(
+        world,
+        constants.NodeCleanUpPolicy.REMOVE_EVERYTHING,
+        remove_important_files=True,
+        remove_unneeded_files=True
+    )
+
+
+def check_if_files_are_removed(world,
+                               policy: constants.NodeCleanUpPolicy = None,
+                               *,
+                               remove_important_files: bool,
+                               remove_unneeded_files: bool):
     init_node = world.create_init_node()
-    init_node.set_clean_up_policy(constants.NodeCleanUpPolicy.REMOVE_EVERYTHING)
     init_node.run()
+
+    if policy is not None:
+        init_node.set_clean_up_policy(policy)
 
     world.close()
 
-    assert important_files_are_removed(init_node)
-    assert unneeded_files_are_removed(init_node)
+    assert important_files_are_removed(init_node) == remove_important_files
+    assert unneeded_files_are_removed(init_node) == remove_unneeded_files
 
 
 def important_files_are_removed(node):
