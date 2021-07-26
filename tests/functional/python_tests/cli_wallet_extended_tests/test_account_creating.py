19bc25659894207eb036c2ce4517e01fb80c8140
Piotr Batko <pbatko@syncad.com>
2021-07-26 13:31:30 +0200

    Move getting empty account history test to separate test case
    Change from getting history from 1st and 2nd block to only one, last generated block.

--- tests/functional/python_tests/cli_wallet_extended_tests/test_account_creating.py
+++ tests/functional/python_tests/cli_wallet_extended_tests/test_account_creating.py
@@ -3,10 +3,3 @@ def test_account_creation(wallet):
 
     response = wallet.api.list_accounts('a', 1)
     assert response['result'] == ['alice']
-
-
-def test_account_creation_and_much_more(wallet):
-    #**************************************************************
-    response = wallet.api.get_account_history('initminer', 2, 2)
-    #this call has a custom formatter so typical JSON is inaccessible
-    assert len(response['result']) == 0
