f7dd9816ec9a11b131bde22e42fc6107dc7ebeb3
Piotr Batko <pbatko@syncad.com>
2021-07-26 12:16:52 +0200

    Refactor following test
    - Use asset wrapper instead of string
    - Remove unneeded checks for 'result' in responses
    - Compare json as dicts instead of as strings
    - Shorten extracting elements from response

--- tests/functional/python_tests/cli_wallet_extended_tests/test_following.py
+++ tests/functional/python_tests/cli_wallet_extended_tests/test_following.py
@@ -1,24 +1,23 @@
+import json
+
+from test_tools import Asset
+
+
 def test_following(wallet):
-    # **************************************************************
-    response = wallet.api.create_account('initminer', 'alice', '{}')
-    assert 'result' in response
+    wallet.api.create_account('initminer', 'alice', '{}')
+    wallet.api.create_account('initminer', 'bob', '{}')
+    wallet.api.transfer_to_vesting('initminer', 'alice', Asset.Test(100))
 
-    # **************************************************************
-    response = wallet.api.create_account('initminer', 'bob', '{}')
-    assert 'result' in response
-
-    # **************************************************************
-    response = wallet.api.transfer_to_vesting('initminer', 'alice', '100.000 TESTS')
-    assert 'result' in response
-
-    # **************************************************************
     response = wallet.api.follow('alice', 'bob', ['blog'])
-    _result = response['result']
+    operation = response['result']['operations'][0]
+    operation_type, operation_json = operation[0], json.loads(operation[1]['json'])
 
-    _ops = _result['operations']
-    _op = _ops[0]
-
-    assert _op[0] == 'custom_json'
-
-    _value = _op[1]
-    assert _value['json'] == '{"type":"follow_operation","value":{"follower":"alice","following":"@bob","what":["blog"]}}'
+    assert operation_type == 'custom_json'
+    assert operation_json == {
+        'type': 'follow_operation',
+        'value': {
+            'follower': 'alice',
+            'following': '@bob',
+            'what': ['blog']
+        }
+    }
