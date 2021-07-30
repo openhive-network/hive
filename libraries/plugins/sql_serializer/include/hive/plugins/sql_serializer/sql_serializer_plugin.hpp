ce4d3e0faa656fe439c0a91f8dbd1bc9c33fe551
Bartek Wrona <wrona@syncad.com>
2021-07-30 17:27:45 +0200

    [Ign] Chain specific signal handlers moved into internal implementation class.

--- libraries/plugins/sql_serializer/include/hive/plugins/sql_serializer/sql_serializer_plugin.hpp
+++ libraries/plugins/sql_serializer/include/hive/plugins/sql_serializer/sql_serializer_plugin.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <hive/chain/hive_fwd.hpp>
 #include <hive/plugins/chain/chain_plugin.hpp>
-#include <hive/plugins/sql_serializer/sql_serializer_objects.hpp>
 
 #define HIVE_SQL_SERIALIZER_NAME "sql_serializer"
 
@@ -10,10 +9,6 @@ namespace hive { namespace plugins { namespace sql_serializer {
 namespace detail { class sql_serializer_plugin_impl; }
 
 using namespace appbase;
-using chain::block_notification;
-using chain::transaction_notification;
-using chain::operation_notification;
-using chain::reindex_notification;
 
 constexpr size_t prereservation_size = 16'000u;
 
@@ -31,13 +26,6 @@ class sql_serializer_plugin final : public plugin<sql_serializer_plugin>
         return name; 
       }
 
-      void on_pre_reindex(const reindex_notification &note);
-      void on_post_reindex(const reindex_notification &note);
-
-      void on_pre_apply_operation(const operation_notification &note);
-      void on_pre_apply_block(const block_notification& note);
-      void on_post_apply_block(const block_notification &note);
-
       virtual void set_program_options(options_description& cli, options_description& cfg ) override;
       virtual void plugin_initialize(const variables_map& options) override;
       virtual void plugin_startup() override;
@@ -46,8 +34,6 @@ class sql_serializer_plugin final : public plugin<sql_serializer_plugin>
   private:
 
       std::unique_ptr<detail::sql_serializer_plugin_impl> my;
-
-      void handle_transactions(const vector<hive::protocol::signed_transaction>& transactions, const int64_t block_num );
 };
 
 } } } //hive::plugins::sql_serializer
