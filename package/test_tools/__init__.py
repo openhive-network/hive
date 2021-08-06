824f40eb35f9fef741859a0d8ddee06306a87c12
Piotr Batko <pbatko@syncad.com>
2021-08-06 11:09:23 +0200

    Remove main net singleton

--- package/test_tools/__init__.py
+++ package/test_tools/__init__.py
@@ -1,6 +1,5 @@
 from test_tools.private.asset import Asset
 from test_tools.private.block_log import BlockLog
-from test_tools.private.main_net import main_net_singleton as main_net
 from test_tools.private.logger.logger_user_interface import logger
 from test_tools.account import Account
 from test_tools.world import World
--- package/test_tools/private/main_net.py
+++
@@ -1,4 +0,0 @@
-from test_tools.private.remote_node import RemoteNode
-
-
-main_net_singleton = RemoteNode('https://api.hive.blog:443')
