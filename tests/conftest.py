36a5eac8d7505e7dde001a159ba75987c0d69746
Piotr Batko <pbatko@syncad.com>
2021-08-06 19:23:00 +0200

    Handle pylint unused-argument warning and provide explanation comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ def configure_test_tools_paths(request):
 
 @pytest.fixture
 def world(request, configure_test_tools_paths):
+    _ = configure_test_tools_paths  # This fixture is required to guarantee right order of fixtures creation
     directory = automatic_tests_configuration.get_preferred_directory(request)
     with World(directory=directory) as world:
         world.set_clean_up_policy(constants.WorldCleanUpPolicy.REMOVE_ONLY_UNNEEDED_FILES)
