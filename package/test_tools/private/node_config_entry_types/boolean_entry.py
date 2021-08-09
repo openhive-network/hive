24dda1120b06f36c5165c145564b93621161b5e7
Piotr Batko <pbatko@syncad.com>
2021-08-09 17:32:19 +0200

    Handle boolean node config entries with custom text representation
    In config most of booleans are serialized as {1: True, 0: False}, but
    exist also entries with representation {yes: True, no: False}. This
    commit adds support for them.

--- package/test_tools/private/node_config_entry_types/boolean_entry.py
+++ package/test_tools/private/node_config_entry_types/boolean_entry.py
@@ -1,13 +1,28 @@
 from test_tools.private.node_config_entry_types.config_entry import ConfigEntry
+from test_tools.exceptions import ParseError
 
 
 class Boolean(ConfigEntry):
+    def __init__(self, value=None, *, true='1', false='0'):
+        super().__init__(value)
+        self.__true_serialized = true
+        self.__false_serialized = false
+
     def parse_from_text(self, text):
-        self.set_value(int(text) == 1)
+        if text == self.__true_serialized:
+            self.set_value(True)
+        elif text == self.__false_serialized:
+            self.set_value(False)
+        else:
+            raise ParseError(
+                f'Can\'t parse bool value from: {text}. '
+                f'Available values are: {self.__true_serialized} and {self.__false_serialized}.'
+            )
+
         return self._value
 
     def serialize_to_text(self):
-        return '1' if self._value else '0'
+        return self.__true_serialized if self._value else self.__false_serialized
 
     @classmethod
     def _validate(cls, value):
--- tests/unit_tests/node_config_tests/entries_tests/test_boolean.py
+++ tests/unit_tests/node_config_tests/entries_tests/test_boolean.py
@@ -4,31 +4,34 @@ from test_tools.private.node_config_entry_types import Boolean
 
 
 @pytest.fixture
-def entry():
-    return Boolean()
-
-
-@pytest.fixture
-def values():
+def data_sets():
     return [
-        ('1', True),
-        ('0', False),
+        {'entry': Boolean(),                       True: '1',   False: '0'},
+        {'entry': Boolean(true='yes', false='no'), True: 'yes', False: 'no'},
     ]
 
 
-def test_parsing(entry, values):
-    for input_text, expected in values:
-        entry.parse_from_text(input_text)
-        assert entry.get_value() == expected
+def test_parsing(data_sets):
+    for data_set in data_sets:
+        for expected in (True, False):
+            entry = data_set['entry']
+            value_as_text = data_set[expected]
+
+            entry.parse_from_text(value_as_text)
+            assert entry.get_value() == expected
 
 
-def test_serializing(entry, values):
-    for expected, input_value in values:
-        entry.set_value(input_value)
-        assert entry.serialize_to_text() == expected
+def test_serializing(data_sets):
+    for data_set in data_sets:
+        for input_value in (True, False):
+            entry = data_set['entry']
+            expected = data_set[input_value]
+
+            entry.set_value(input_value)
+            assert entry.serialize_to_text() == expected
 
 
-def test_different_type_assignments(entry):
+def test_different_type_assignments():
     for incorrect_value in ['123', 0, 1, 2.718]:
         with pytest.raises(ValueError):
-            entry.set_value(incorrect_value)
+            Boolean().set_value(incorrect_value)
