47473e76c2dbf9ddac190ba000842515c74d2101
Piotr Batko <pbatko@syncad.com>
2021-08-11 15:56:03 +0200

    Shorten key generation in Account class

--- package/test_tools/account.py
+++ package/test_tools/account.py
@@ -3,6 +3,7 @@ from test_tools.private.key_generator import KeyGenerator
 
 class Account:
     __key_generator = KeyGenerator()
+    __generate_keys = __key_generator.generate_keys
 
     def __init__(self, name, secret='secret', with_keys=True):
         self.name = name
@@ -11,7 +12,7 @@ class Account:
         self.public_key = None
 
         if with_keys:
-            output = self.__key_generator.generate_keys(name, secret=secret)[0]
+            output = self.__generate_keys(name, secret=secret)[0]
             self.secret = secret
             self.private_key = output['private_key']
             self.public_key = output['public_key']
@@ -19,7 +20,7 @@ class Account:
     @classmethod
     def create_multiple(cls, number_of_accounts, name_base='account', *, secret='secret'):
         accounts = []
-        for generated in cls.__key_generator.generate_keys(name_base, number_of_accounts=number_of_accounts, secret=secret):
+        for generated in cls.__generate_keys(name_base, number_of_accounts=number_of_accounts, secret=secret):
             account = Account(generated['account_name'], with_keys=False)
             account.secret = secret
             account.private_key = generated['private_key']
