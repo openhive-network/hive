ff90ea8586de8e44d8f70ca750a0cda0de5ddcc3
Piotr Batko <pbatko@syncad.com>
2021-08-11 15:54:53 +0200

    Make key_generator private in Account class

--- package/test_tools/account.py
+++ package/test_tools/account.py
@@ -2,7 +2,7 @@ from test_tools.private.key_generator import KeyGenerator
 
 
 class Account:
-    key_generator = KeyGenerator()
+    __key_generator = KeyGenerator()
 
     def __init__(self, name, secret='secret', with_keys=True):
         self.name = name
@@ -11,7 +11,7 @@ class Account:
         self.public_key = None
 
         if with_keys:
-            output = self.key_generator.generate_keys(name, secret=secret)[0]
+            output = self.__key_generator.generate_keys(name, secret=secret)[0]
             self.secret = secret
             self.private_key = output['private_key']
             self.public_key = output['public_key']
@@ -19,7 +19,7 @@ class Account:
     @classmethod
     def create_multiple(cls, number_of_accounts, name_base='account', *, secret='secret'):
         accounts = []
-        for generated in cls.key_generator.generate_keys(name_base, number_of_accounts=number_of_accounts, secret=secret):
+        for generated in cls.__key_generator.generate_keys(name_base, number_of_accounts=number_of_accounts, secret=secret):
             account = Account(generated['account_name'], with_keys=False)
             account.secret = secret
             account.private_key = generated['private_key']
