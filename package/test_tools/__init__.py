76d72283ff7842dfa610aeb1d93f788ade164e46
Piotr Batko <pbatko@syncad.com>
2021-08-11 17:56:42 +0200

    Move logger singleton creation to separate module

--- package/test_tools/__init__.py
+++ package/test_tools/__init__.py
@@ -1,16 +1,6 @@
-# pylint: disable=import-outside-toplevel, wrong-import-position
-# This is required because of bad implementation of logger.
-# Hopefully it will be rewritten in near future and then above
-# pylint-ignore should be removed.
-
-def __create_logger():
-    from test_tools.private.logger import Logger
-    return Logger()
-
-logger = __create_logger()
-
 from test_tools.private.asset import Asset
 from test_tools.private.block_log import BlockLog
+from test_tools.private.logger_singleton import logger
 from test_tools.private.main_net import main_net_singleton as main_net
 from test_tools.account import Account
 from test_tools.world import World
