b0c17083ee397be906303218366cb723e994a152
Marcin Ickiewicz <mickiewicz@syncad.com>
2021-08-18 10:51:04 +0200

    remove connection holder from sql serializer

--- libraries/plugins/sql_serializer/sql_serializer.cpp
+++ libraries/plugins/sql_serializer/sql_serializer.cpp
@@ -164,140 +164,16 @@ using chain::reindex_notification;
       };
       using cached_containter_t = std::unique_ptr<cached_data_t>;
 
-
-      using block_data_container_t_writer = table_data_writer<hive_blocks>;
-      using transaction_data_container_t_writer = table_data_writer<hive_transactions>;
-      using transaction_multisig_data_container_t_writer = table_data_writer<hive_transactions_multisig>;
-      using operation_data_container_t_writer = table_data_writer<hive_operations>;
-
-
-
-        struct transaction_repr_t
-        {
-          std::unique_ptr<pqxx::connection> _connection;
-          std::unique_ptr<pqxx::work> _transaction;
-
-          transaction_repr_t() = default;
-          transaction_repr_t(pqxx::connection* _conn, pqxx::work* _trx) : _connection{_conn}, _transaction{_trx} {}
-
-          auto get_escaping_charachter_methode() const
-          {
-            return [this](const char *val) -> fc::string { return std::move( this->_connection->esc(val) ); };
-          }
-
-          auto get_raw_escaping_charachter_methode() const
-          {
-            return [this](const char *val, const size_t s) -> fc::string { 
-              pqxx::binarystring __tmp(val, s); 
-              return std::move( this->_transaction->esc_raw( __tmp.str() ) ); 
-            };
-          }
-        };
-        using transaction_t = std::unique_ptr<transaction_repr_t>;
-
-        struct postgress_connection_holder
-        {
-          explicit postgress_connection_holder(const fc::string &url)
-              : connection_string{url} {}
-
-          transaction_t start_transaction() const
-          {
-            pqxx::connection* _conn = new pqxx::connection{ this->connection_string };
-            pqxx::work *_trx = new pqxx::work{*_conn};
-            _trx->exec("SET CONSTRAINTS ALL DEFERRED;");
-
-            return transaction_t{ new transaction_repr_t{ _conn, _trx } };
-          }
-
-          bool exec_transaction(transaction_t &trx, const fc::string &sql) const
-          {
-            if (sql == fc::string())
-              return true;
-            return sql_safe_execution([&trx, &sql]() { trx->_transaction->exec(sql); }, sql.c_str());
-          }
-
-          bool commit_transaction(transaction_t &trx) const
-          {
-            return sql_safe_execution([&]() { trx->_transaction->commit(); }, "commit");
-          }
-
-          void abort_transaction(transaction_t &trx) const
-          {
-            trx->_transaction->abort();
-          }
-
-          bool exec_single_in_transaction(const fc::string &sql, pqxx::result *result = nullptr) const
-          {
-            if (sql == fc::string())
-              return true;
-
-            return sql_safe_execution([&]() {
-              pqxx::connection conn{ this->connection_string };
-              pqxx::work trx{conn};
-              if (result)
-                *result = trx.exec(sql);
-              else
-                trx.exec(sql);
-              trx.commit();
-            }, sql.c_str());
-          }
-
-          template<typename T>
-          bool get_single_value(const fc::string& query, T& _return) const
-          {
-            pqxx::result res;
-            if(!exec_single_in_transaction( query, &res ) && res.empty() ) return false;
-            _return = res.at(0).at(0).as<T>();
-            return true;
-          }
-
-          template<typename T>
-          T get_single_value(const fc::string& query) const
-          {
-            T _return;
-            FC_ASSERT( get_single_value( query, _return ) );
-            return _return;
-          }
-
-          uint get_max_transaction_count() const
-          {
-            // get maximum amount of connections defined by postgres and set half of it as max; minimum is 1
-            return std::max( 1u, get_single_value<uint>("SELECT setting::int / 2 FROM pg_settings WHERE  name = 'max_connections'") );
-          }
-
-        private:
-          fc::string connection_string;
-
-          bool sql_safe_execution(const std::function<void()> &f, const char* msg = nullptr) const
-          {
-            try
-            {
-              f();
-              return true;
-            }
-            catch (const pqxx::pqxx_exception &sql_e)
-            {
-              elog("Exception from postgress: ${e}", ("e", sql_e.base().what()));
-            }
-            catch (const std::exception &e)
-            {
-              elog("Exception: ${e}", ("e", e.what()));
-            }
-            catch (...)
-            {
-              elog("Unknown exception occured");
-            }
-            if(msg) std::cerr << "Error message: " << msg << std::endl;
-            return false;
-          }
-        };
-
-        class sql_serializer_plugin_impl final
+      class sql_serializer_plugin_impl final
         {
         public:
+          using block_data_container_t_writer = table_data_writer<hive_blocks>;
+          using transaction_data_container_t_writer = table_data_writer<hive_transactions>;
+          using transaction_multisig_data_container_t_writer = table_data_writer<hive_transactions_multisig>;
+          using operation_data_container_t_writer = table_data_writer<hive_operations>;
+
           sql_serializer_plugin_impl(const std::string &url, hive::chain::database& _chain_db, const sql_serializer_plugin& _main_plugin) 
-            : connection{url},
-              db_url{url},
+            : db_url{url},
               chain_db{_chain_db},
               main_plugin{_main_plugin}
           {
@@ -338,7 +214,6 @@ using chain::reindex_notification;
           std::unique_ptr<operation_data_container_t_writer> _operation_writer;
           std::unique_ptr<end_massive_sync_processor> _end_massive_sync_processor;
 
-          postgress_connection_holder connection;
           std::string db_url;
           hive::chain::database& chain_db;
           const sql_serializer_plugin& main_plugin;
@@ -422,7 +297,17 @@ using chain::reindex_notification;
 
             if(freshDb)
             {
-              connection.exec_single_in_transaction(PSQL::get_all_type_definitions());
+              auto get_type_definitions = [](const data_processor::data_chunk_ptr& dataPtr, transaction& tx){
+                auto types = PSQL::get_all_type_definitions();
+                if ( types.empty() )
+                  return data_processing_status();;
+
+                tx.exec( types );
+                return data_processing_status();
+              };
+              queries_commit_data_processor processor( db_url, "Get type definitions", get_type_definitions, nullptr );
+              processor.trigger( nullptr, 0 );
+              processor.join();
             }
 
             switch_db_items( false/*mode*/ );
