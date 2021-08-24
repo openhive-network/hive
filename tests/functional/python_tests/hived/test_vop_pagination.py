de46555404faa215c6e2eb7f66f85800ae397854
Krzysztof Mochocki <kmochocki@syncad.com>
2021-08-24 10:39:06 +0200

    reactored test for vops_pagination

--- tests/functional/python_tests/hived/test_vop_pagination.py
+++ tests/functional/python_tests/hived/test_vop_pagination.py
@@ -1,14 +1,26 @@
 from test_tools import World, logger
 from test_tools.private.remote_node import RemoteNode
+from pytest import fixture
+from os import getenv
 
 STEPS = 20
 BLOCKS_PER_STEP = 100
 BLOCK_MAX = 3000000
 BLOCK_MIN = BLOCK_MAX - ( BLOCKS_PER_STEP * STEPS )
 
-from os import getenv
-REMOTE_NODE_URL = getenv('HIVED_MAINNET_ADDRESS')
-assert REMOTE_NODE_URL is not None
+
+@fixture
+def main_net(world : World) -> RemoteNode:
+  REMOTE_NODE_URL = getenv('HIVED_MAINNET_ADDRESS')
+  if REMOTE_NODE_URL is None: 
+    REMOTE_NODE_URL = 'http://192.168.6.136:8091'
+  assert REMOTE_NODE_URL is not None
+  return world.create_remote_node(REMOTE_NODE_URL)
+
+@fixture
+def block_range():
+  return range(BLOCK_MIN, BLOCK_MAX, BLOCKS_PER_STEP)
+
 
 # class that compressing vop
 class compressed_vop:
@@ -78,36 +90,25 @@ def get_vops_one_by_one(range_begin : int, range_end : int, main_net : RemoteNod
   return get_vops_with_step(range_begin, range_end, 1, main_net)
 
 # get same data in given range with diffrent step
-def check_range(range_begin : int, blocks : int, world : World):
-  range_end = range_begin + blocks + 1
-
-  main_net = world.create_remote_node(REMOTE_NODE_URL)
+def check_range(range_begin : int, step : int, main_net : RemoteNode):
+  range_end = range_begin + BLOCKS_PER_STEP + 1
   logger.debug(f"gathering blocks in range [ {range_begin} ; {range_end} )")
-
   all_at_once = get_vops_at_once(range_begin, range_end, main_net)
-  paginated_by_1 = get_vops_one_by_one(range_begin, range_end, main_net)
-  paginated_by_2 = get_vops_with_step(range_begin, range_end, 2, main_net)
-  paginated_by_5 = get_vops_with_step(range_begin, range_end, 5, main_net)
-  paginated_by_10 = get_vops_with_step(range_begin, range_end, 10, main_net)
+  paginated = get_vops_with_step(range_begin, range_end, step, main_net)
+  return all_at_once == paginated
 
-  assert all_at_once == paginated_by_1
-  logger.debug(f"[OK] all == paginated by 1 [ {range_begin} ; {range_end} )")
-  assert all_at_once == paginated_by_2
-  logger.debug(f"[OK] all == paginated by 2 [ {range_begin} ; {range_end} )")
-  assert all_at_once == paginated_by_5
-  logger.debug(f"[OK] all == paginated by 5 [ {range_begin} ; {range_end} )")
-  assert all_at_once == paginated_by_10
-  logger.debug(f"[OK] all == paginated by 10 [ {range_begin} ; {range_end} )")
+def test_vop_pagination_by_1( main_net : RemoteNode, block_range):
+  for i in block_range:
+    assert check_range(i, 1, main_net)
 
-  return True
+def test_vop_pagination_by_2( main_net : RemoteNode, block_range):
+  for i in block_range:
+    assert check_range(i, 2, main_net)
 
-def test_vop_pagination( world : World ):
-  from concurrent.futures import ThreadPoolExecutor
-  with ThreadPoolExecutor(max_workers=4) as executor:
-    for future, block_range in [(executor.submit(check_range, i, BLOCKS_PER_STEP, world), i) for i in range(BLOCK_MIN, BLOCK_MAX, BLOCKS_PER_STEP)]:
-      exception = future.exception()
-      if exception is not None:
-        logger.info(f'exception in range: < {block_range} ; {block_range + BLOCKS_PER_STEP} )')
-        logger.info(exception)
-        assert exception is None
-        break
+def test_vop_pagination_by_5( main_net : RemoteNode, block_range):
+  for i in block_range:
+    assert check_range(i, 5, main_net)
+
+def test_vop_pagination_by_10( main_net : RemoteNode, block_range):
+  for i in block_range:
+    assert check_range(i, 10, main_net)
