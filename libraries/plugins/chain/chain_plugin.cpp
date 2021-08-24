2f64366d593a85d2b0cc990aa46401607ecde4d6
Krzysztof Mochocki <kmochocki@syncad.com>
2021-08-24 09:24:15 +0200

    moved initialization of notifications system

--- libraries/plugins/chain/chain_plugin.cpp
+++ libraries/plugins/chain/chain_plugin.cpp
@@ -658,6 +658,7 @@ void chain_plugin::set_program_options(options_description& cli, options_descrip
 }
 
 void chain_plugin::plugin_initialize(const variables_map& options) {
+  hive::utilities::notifications::process_program_options(options);
   my->shared_memory_dir = app().data_dir() / "blockchain";
 
   if( options.count("shared-file-dir") )
