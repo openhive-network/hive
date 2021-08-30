93d16412a17bc96a43dc3bd35f7f444b3db3daa1
mtrela <mtrela@syncad.com>
2021-08-30 17:22:24 +0200

    Added methods into an extension: `clear_counters`, `get_counter`

--- libraries/ah_manager/utilities.cpp
+++ libraries/ah_manager/utilities.cpp
@@ -7,6 +7,8 @@
 #include <fc/io/json.hpp>
 #include <fc/string.hpp>
 
+#include<map>
+
 using hive::protocol::account_name_type;
 using fc::string;
 
@@ -32,6 +34,8 @@ PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(get_impacted_accounts_array);
 PG_FUNCTION_INFO_V1(get_impacted_accounts);
+PG_FUNCTION_INFO_V1(clear_counters);
+PG_FUNCTION_INFO_V1(get_counter);
 
 flat_set<account_name_type> get_accounts( const std::string& operation_body )
 {
@@ -182,6 +186,48 @@ Datum get_impacted_accounts(PG_FUNCTION_ARGS)
   {
     SRF_RETURN_DONE(funcctx);
   }
+}
+
+using acc_counters_type = std::map<uint64_t, uint64_t>;
+acc_counters_type acc_counters;
+
+Datum clear_counters(PG_FUNCTION_ARGS)
+{
+  try
+  {
+    acc_counters.clear();
+  }
+  catch(...)
+  {
+  }
+  PG_RETURN_UINT32( UInt32GetDatum(0) );
+}
+
+Datum get_counter(PG_FUNCTION_ARGS)
+{
+  try
+  {
+    uint32_t _account_id = PG_GETARG_INT64(0);
+    uint32_t _current_cnt = PG_GETARG_INT64(1) + 1;
+
+    auto found = acc_counters.find( _account_id );
+    if( found == acc_counters.end() )
+    {
+      acc_counters.emplace( std::make_pair(_account_id, _current_cnt) );
+    }
+    else
+    {
+      ++( found->second );
+      _current_cnt = found->second;
+    }
+
+    Datum _result = Int64GetDatum(_current_cnt);
+    PG_RETURN_INT64(_result);
+  }
+  catch(...)
+  {
+  }
+  PG_RETURN_INT64( Int32GetDatum(0) );
 }
 
 }
