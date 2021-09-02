1b9c80b297430266d9b16687c9e7772bf2d6181c
mtyszczak <mateusz.tyszczak@gmail.com>
2021-09-02 15:19:52 +0200

    Remove default arguments from bc plugins

--- programs/blockchain_converter/conversion_plugin.hpp
+++ programs/blockchain_converter/conversion_plugin.hpp
@@ -17,7 +17,7 @@ namespace hive { namespace converter { namespace plugins {
     uint32_t log_per_block = 0, log_specific = 0;
     blockchain_converter converter;
 
-    conversion_plugin_impl( const private_key_type& _private_key, const chain_id_type& chain_id = HIVE_CHAIN_ID, size_t signers_size = 1 )
+    conversion_plugin_impl( const private_key_type& _private_key, const chain_id_type& chain_id, size_t signers_size )
       : converter( _private_key, chain_id, signers_size ) {}
 
     virtual void convert( uint32_t start_block_num, uint32_t stop_block_num ) = 0;
