e0e263727e9aed0e0f3687bc36468520c414e525
Gandalf <gandalf@storm.pl>
2021-09-04 12:34:39 +0000

    Update p2p_default_seeds.hpp

--- libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
@@ -8,18 +8,17 @@ namespace hive{ namespace plugins { namespace p2p {
 const std::vector< std::string > default_seeds;
 #else
 const std::vector< std::string > default_seeds = {
-  "anyx.io:2001",                        // anyx
-  "hive-seed.arcange.eu:2001",           // arcange
-  "hive-seed.lukestokes.info:2001",      // lukestokes.mhth
-  "hived.splinterlands.com:2001",        // aggroed
-  "hiveseed-se.privex.io:2001",          // privex
-  "node.mahdiyari.info:2001",            // mahdiyari
-  "rpc.ausbit.dev:2001",                 // ausbitbank
   "api.hive.blog:2001",                  // blocktrades
-  "seed.hivekings.com:2001",             // drakos
-  "seed.liondani.com:2016",              // liondani
   "seed.openhive.network:2001",          // gtg
-  "hive-seed.roelandp.nl:2001"           // roelandp
+  "rpc.ausbit.dev:2001",                 // ausbitbank
+  "hive-seed.roelandp.nl:2001",          // roelandp
+  "hive-seed.arcange.eu:2001",           // arcange
+  "anyx.io:2001",                        // anyx
+  "hived.splinterlands.com:2001",        // aggroed
+  "node.mahdiyari.info:2001",            // mahdiyari
+  "hive-seed.lukestokes.info:2001",      // lukestokes.mhth
+  "seed.liondani.com:2016",              // liondani
+  "hiveseed-se.privex.io:2001"           // privex
 };
 #endif
 
