56096bb87d3f842e1d8486d51365719dac6fc2e8
Gandalf <gandalf@storm.pl>
2021-09-12 16:40:28 +0000

    Added seed nodes

--- libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
@@ -10,6 +10,7 @@ const std::vector< std::string > default_seeds;
 const std::vector< std::string > default_seeds = {
   "api.hive.blog:2001",                  // blocktrades
   "seed.openhive.network:2001",          // gtg
+  "seed.ecency.com:2001",                // good-karma
   "rpc.ausbit.dev:2001",                 // ausbitbank
   "hive-seed.roelandp.nl:2001",          // roelandp
   "hive-seed.arcange.eu:2001",           // arcange
@@ -17,8 +18,10 @@ const std::vector< std::string > default_seeds = {
   "hived.splinterlands.com:2001",        // aggroed
   "node.mahdiyari.info:2001",            // mahdiyari
   "hive-seed.lukestokes.info:2001",      // lukestokes.mhth
+  "api.deathwing.me:2001",               // deathwing
   "seed.liondani.com:2016",              // liondani
-  "hiveseed-se.privex.io:2001"           // privex
+  "hiveseed-se.privex.io:2001",          // privex
+  "seed.mintrawa.com:2001"               // mintrawa
 };
 #endif
 
