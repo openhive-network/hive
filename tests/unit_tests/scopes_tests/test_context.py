6596ccd28016800ab92a812d4d3684f6564b290d
Piotr Batko <pbatko@syncad.com>
2021-09-24 09:55:54 +0200

    Fix context tests
    Tests were using paths outside of theirs scopes.

--- tests/unit_tests/scopes_tests/test_context.py
+++ tests/unit_tests/scopes_tests/test_context.py
@@ -1,19 +1,19 @@
-from pathlib import Path
-
 from test_tools.private.scope.scope_singleton import context, current_scope
 from test_tools.private.scope.scoped_current_directory import ScopedCurrentDirectory
 
 
 def test_getting_value_set_in_same_scope():
+    directory_path = context.get_current_directory() / 'scoped-directory'
     with current_scope.create_new_scope('test-scope'):
-        ScopedCurrentDirectory('scoped-directory')
-        assert context.get_current_directory() == Path('scoped-directory')
+        ScopedCurrentDirectory(directory_path)
+        assert context.get_current_directory() == directory_path
 
 
 def test_value_is_popped_after_exit_from_scope():
     directory_before = current_scope.context.get_current_directory()
 
+    directory_path = context.get_current_directory() / 'scoped-directory'
     with current_scope.create_new_scope('test-scope'):
-        ScopedCurrentDirectory('scoped-directory')
+        ScopedCurrentDirectory(directory_path)
 
     assert context.get_current_directory() == directory_before
