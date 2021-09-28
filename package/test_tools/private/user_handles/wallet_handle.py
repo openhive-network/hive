bd0edbe9aabfb4f692e3d4634cd3bb4751c8a9b2
Piotr Batko <pbatko@syncad.com>
2021-09-28 16:56:38 +0200

    Add documentation of wallet user handle methods

--- package/test_tools/private/user_handles/wallet_handle.py
+++ package/test_tools/private/user_handles/wallet_handle.py
@@ -15,6 +15,16 @@ class WalletHandle:
                  attach_to: Union[None, 'Node', 'RemoteNode'] = None,
                  additional_arguments: Iterable[str] = (),
                  preconfigure: bool = True):
+        """
+        Creates wallet, runs its process and blocks until wallet will be ready to use.
+
+        :param attach_to: Wallet will send messages to node passed as this parameter. Passing node is optional, but when
+            node is omitted, wallet functionalities are limited.
+        :param additional_arguments: Command line arguments which will be applied during running wallet.
+        :param preconfigure: If set to True, after run wallet will be unlocked with password DEFAULT_PASSWORD and
+            initminer's keys imported.
+        """
+
         # Break import-cycle
         from test_tools import RemoteNode  # pylint: disable=import-outside-toplevel
         if isinstance(attach_to, RemoteNode):
@@ -29,16 +39,52 @@ class WalletHandle:
         self.api = self.__implementation.api
 
     def in_single_transaction(self, *, broadcast=None):
+        """
+        Returns context manager allowing aggregation of multiple operations to single transaction. Must be used within
+        `with` statement. All operations from API calls within this context are grouped to single transaction and
+        optionally sent to blockchain at exit from context.
+
+        :param broadcast: If set to True, this is default behavior, sends (broadcasts) transaction to blockchain.
+            If set to False, only builds transaction without sending, which may be accessed with `get_response` method
+            of returned context manager object.
+            Otherwise behavior is undefined.
+        """
+
         return self.__implementation.in_single_transaction(broadcast=broadcast)
 
     def run(self, *, timeout, preconfigure: bool = True):
+        """
+        Runs wallet's process and blocks until wallet will be ready to use.
+
+        :param timeout: TimeoutError will be raised, if wallet won't start before specified timeout.
+        :param preconfigure: If set to True, after run wallet will be unlocked with password DEFAULT_PASSWORD and
+            initminer's keys imported.
+        """
+
         self.__implementation.run(timeout=timeout, preconfigure=preconfigure)
 
     def restart(self, *, preconfigure: bool = True):
+        """
+        Closes wallet's process, runs it again and blocks until wallet will be ready to use.
+
+        :param preconfigure: If set to True, after run wallet will be unlocked with password DEFAULT_PASSWORD and
+            initminer's keys imported.
+        """
+
         self.__implementation.restart(preconfigure=preconfigure)
 
     def close(self):
+        """
+        Terminates wallet's process and performs needed cleanups. Blocks until wallet process will be finished. Closing
+        is performed by sending SIGINT signal. If wallet doesn't close before timeout, sends SIGKILL and emits warning
+        message.
+        """
+
         self.__implementation.close()
 
     def is_running(self) -> bool:
+        """
+        Returns True if wallet's process is running, otherwise False.
+        """
+
         return self.__implementation.is_running()
