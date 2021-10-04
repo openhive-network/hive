197e95034d2f827ad0d20afd67a7f386ff531141
mtrela <mtrela@syncad.com>
2021-10-04 14:06:41 +0200

    Libraries `protocol`, `schema` treated as a submodule

--- libraries/schema/include/hive/schema/schema.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <hive/schema/abstract_schema.hpp>
-#include <hive/schema/schema_impl.hpp>
-#include <hive/schema/schema_types.hpp>
--- libraries/schema/include/hive/schema/schema_types.hpp
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <hive/schema/schema_types/flat_map.hpp>
-#include <hive/schema/schema_types/flat_set.hpp>
-#include <hive/schema/schema_types/static_variant.hpp>
-#include <hive/schema/schema_types/optional.hpp>
-#include <hive/schema/schema_types/pair.hpp>
-#include <hive/schema/schema_types/vector.hpp>
