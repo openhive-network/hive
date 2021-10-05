d605ed15656ee06a08223e33e9a2bfa060698ec3
mtrela <mtrela@syncad.com>
2021-10-05 14:48:16 +0200

    Added `protocol` submodule

--- libraries/protocol/include/hive/protocol/schema_types.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <hive/protocol/schema_types/account_name_type.hpp>
-#include <hive/protocol/schema_types/asset_symbol_type.hpp>
-#include <hive/protocol/schema_types/fixed_string.hpp>
