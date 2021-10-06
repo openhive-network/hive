4a4e83f8c5abad11ee2947733f82146e478cd1bf
Radek Masłowski <rmaslowski@syncad.com>
2021-10-06 11:24:14 +0200

    Format test_wallet_cli.py

--- tests/functional/python_tests/cli_wallet/test_wallet_cli.py
+++ tests/functional/python_tests/cli_wallet/test_wallet_cli.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
+from subprocess import PIPE, run as run_executable
 
-from test_tools import paths_to_executables, logger
+from test_tools import logger, paths_to_executables
+
 
 def test_help_option():
-  from subprocess import PIPE, run as run_executable
-  only_args_to_be_founded = [
-    '--help', '--offline', '--server-rpc-endpoint', '--cert-authority',
-    '--retry-server-connection', '--rpc-endpoint', '--rpc-tls-endpoint',
-    '--rpc-tls-certificate', '--rpc-http-endpoint', '--unlock',
-    '--daemon', '--rpc-http-allowip', '--wallet-file', '--chain-id'
-  ]
+    only_args_to_be_founded = [
+        '--help', '--offline', '--server-rpc-endpoint', '--cert-authority',
+        '--retry-server-connection', '--rpc-endpoint', '--rpc-tls-endpoint',
+        '--rpc-tls-certificate', '--rpc-http-endpoint', '--unlock',
+        '--daemon', '--rpc-http-allowip', '--wallet-file', '--chain-id'
+    ]
 
-  cli_wallet_path = paths_to_executables.get_path_of('cli_wallet')
-  process = run_executable([cli_wallet_path, "--help"], stdout=PIPE, stderr=PIPE)
-  stdout = process.stdout.decode('utf-8')
-  args_founded = [ arg for arg in stdout.split() if "--" in arg ]
-  diff = list(set(args_founded)^set(only_args_to_be_founded))
-  logger.info(f'found: {diff}')
-  assert len(diff) == 0
+    cli_wallet_path = paths_to_executables.get_path_of('cli_wallet')
+    process = run_executable([cli_wallet_path, "--help"], stdout=PIPE, stderr=PIPE)
+    stdout = process.stdout.decode('utf-8')
+    args_founded = [arg for arg in stdout.split() if "--" in arg]
+    diff = list(set(args_founded) ^ set(only_args_to_be_founded))
+    logger.info(f'found: {diff}')
+
+    assert len(diff) == 0
