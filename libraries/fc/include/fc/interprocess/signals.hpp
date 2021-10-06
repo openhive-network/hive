82c7af357fab672d2f02a3918737553dc59e3d75
mtrela <mtrela@syncad.com>
2021-10-06 12:02:27 +0200

    Directory `fc` moved into `protocol` submodule

--- libraries/fc/include/fc/interprocess/signals.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <functional>
-
-namespace fc 
-{
-  /// handler will be called from ASIO thread
-  void set_signal_handler( std::function<void(int)> handler, int signal_num ); 
-}
