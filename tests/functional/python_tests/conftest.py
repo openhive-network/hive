8b991649cc609d7836f761d1120ffb3610152ee4
Radek Masłowski <rmaslowski@syncad.com>
2021-10-12 11:02:00 +0200

    Move wallet and node fixture to upfolder conftest.

--- tests/functional/python_tests/conftest.py
+++ tests/functional/python_tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from test_tools import constants, World
+from test_tools import constants, Wallet, World
 from test_tools.private.scope import context
 from test_tools.private.scope.scope_fixtures import *  # pylint: disable=wildcard-import, unused-wildcard-import
 
@@ -10,3 +10,15 @@ def world():
     with World(directory=context.get_current_directory()) as world:
         world.set_clean_up_policy(constants.WorldCleanUpPolicy.REMOVE_ONLY_UNNEEDED_FILES)
         yield world
+
+
+@pytest.fixture
+def node(world):
+    init_node = world.create_init_node()
+    init_node.run()
+    return init_node
+
+
+@pytest.fixture
+def wallet(node):
+    return Wallet(attach_to=node)
