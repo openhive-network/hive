a5ec31d80260f96f2048fc1fd8a884e815d6a39d
martin <mlees@oxio.io>
2021-10-12 20:47:55 -0400

    WIP multiple rc delegations

--- tests/unit/plugin_tests/direct_rc_delegation.cpp
+++ tests/unit/plugin_tests/direct_rc_delegation.cpp
@@ -8,6 +8,7 @@
 #include <hive/protocol/hive_operations.hpp>
 
 #include <hive/plugins/rc/rc_objects.hpp>
+#include <hive/plugins/rc/rc_config.hpp>
 #include <hive/plugins/rc/rc_operations.hpp>
 #include <hive/plugins/rc/rc_plugin.hpp>
 
@@ -24,29 +25,47 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_validate )
   try{
     delegate_rc_operation op;
     op.from = "alice";
-    op.to = "bob";
+    op.delegatees = {"eve", "bob"};
     op.max_rc = 10;
     op.validate();
 
+    // alice- is an invalid accoutn name
     op.from = "alice-";
     BOOST_REQUIRE_THROW( op.validate(), fc::assert_exception );
+
+    // Bob- is an invalid account name
     op.from = "alice";
-
-    op.to = "bob-";
+    op.delegatees = {"eve", "bob-"};
     BOOST_REQUIRE_THROW( op.validate(), fc::assert_exception );
-    op.to = "bob";
 
-    op.to = "alice";
+    // Alice is already the delegator
+    op.delegatees = {"alice", "bob"};
+    BOOST_REQUIRE_THROW( op.validate(), fc::assert_exception );
+
+    // Empty vector
+    op.delegatees = {};
+    BOOST_REQUIRE_THROW( op.validate(), fc::assert_exception );
+
+    // bob is defined twice
+    op.delegatees = {"eve", "bob", "bob"};
+    BOOST_REQUIRE_THROW( op.validate(), fc::assert_exception );
+
+    // There is more than HIVE_RC_MAX_ACCOUNTS_PER_DELEGATION_OP accounts in delegatees
+    vector<account_name_type> vector_too_big;
+    for (int i = 0; i < HIVE_RC_MAX_ACCOUNTS_PER_DELEGATION_OP + 1; i++) {
+      vector_too_big.push_back( "actor" + std::to_string(i) );
+    }
+    op.delegatees = vector_too_big;
     BOOST_REQUIRE_THROW( op.validate(), fc::assert_exception );
   }
   FC_LOG_AND_RETHROW()
 }
 
-BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
+BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply_single )
 {
   try
   {
-    BOOST_TEST_MESSAGE( "Testing:  delegate_rc_operation_apply" );
+    BOOST_TEST_MESSAGE( "Testing:  delegate_rc_operation_apply_single to a single account" );
     ACTORS( (alice)(bob)(dave) )
     vest( HIVE_INIT_MINER_NAME, "alice", ASSET( "10.000 TESTS" ) );
     int64_t alice_vests = alice.vesting_shares.amount.value;
@@ -54,7 +73,7 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
     // Delegating more rc than I have should fail
     delegate_rc_operation op;
     op.from = "alice";
-    op.to = "bob";
+    op.delegatees = {"bob"};
     op.max_rc = alice_vests + 1;
 
     custom_json_operation custom_op;
@@ -64,25 +83,19 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
     BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
 
     // Delegating to a non-existing account should fail
-    op.to = "eve";
+    op.delegatees = {"eve"};
     op.max_rc = 10;
     custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
     BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
 
     // Delegating 0 shouldn't work if there isn't already a delegation that exists (since 0 deletes the delegation)
-    op.to = "bob";
-    op.max_rc = 0;
-    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
-    BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
-
-    // Delegating 0 shouldn't work if there isn't already a delegation that exists (since 0 deletes the delegation)
-    op.to = "bob";
+    op.delegatees = {"bob"};
     op.max_rc = 0;
     custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
     BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
 
     // Successful delegation
-    op.to = "bob";
+    op.delegatees = {"bob"};
     op.max_rc = 10;
     custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
     push_transaction(custom_op, alice_private_key);
@@ -95,7 +108,7 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
     BOOST_REQUIRE( delegation->delegated_rc == op.max_rc );
 
     const rc_account_object& from_rc_account = db->get< rc_account_object, by_name >( op.from );
-    const rc_account_object& to_rc_account = db->get< rc_account_object, by_name >( op.to );
+    const rc_account_object& to_rc_account = db->get< rc_account_object, by_name >( op.delegatees[0] );
 
     BOOST_REQUIRE( from_rc_account.delegated_rc == 10 );
     BOOST_REQUIRE( from_rc_account.received_delegated_rc == 0 );
@@ -109,7 +122,7 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
     
     // Decrease the delegation
     op.from = "alice";
-    op.to = "bob";
+    op.delegatees = {"bob"};
     op.max_rc = 5;
     custom_op.required_posting_auths.clear();
     custom_op.required_posting_auths.insert( "alice" );
@@ -124,7 +137,7 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
     BOOST_REQUIRE( delegation_decreased->delegated_rc == op.max_rc );
 
     const rc_account_object& from_rc_account_decreased = db->get< rc_account_object, by_name >( op.from );
-    const rc_account_object& to_rc_account_decreased = db->get< rc_account_object, by_name >( op.to );
+    const rc_account_object& to_rc_account_decreased = db->get< rc_account_object, by_name >( op.delegatees[0] );
 
     BOOST_REQUIRE( from_rc_account_decreased.delegated_rc == 5 );
     BOOST_REQUIRE( from_rc_account_decreased.received_delegated_rc == 0 );
@@ -133,7 +146,7 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
 
     // Increase the delegation
     op.from = "alice";
-    op.to = "bob";
+    op.delegatees = {"bob"};
     op.max_rc = 50;
     custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
     push_transaction(custom_op, alice_private_key);
@@ -146,7 +159,7 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
     BOOST_REQUIRE( delegation_increased->delegated_rc == op.max_rc );
 
     const rc_account_object& from_rc_account_increased = db->get< rc_account_object, by_name >( op.from );
-    const rc_account_object& to_rc_account_increased = db->get< rc_account_object, by_name >( op.to );
+    const rc_account_object& to_rc_account_increased = db->get< rc_account_object, by_name >( op.delegatees[0] );
 
     BOOST_REQUIRE( from_rc_account_increased.delegated_rc == 50 );
     BOOST_REQUIRE( from_rc_account_increased.received_delegated_rc == 0 );
@@ -155,7 +168,7 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
 
     // Delete the delegation
     op.from = "alice";
-    op.to = "bob";
+    op.delegatees = {"bob"};
     op.max_rc = 0;
     custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
     push_transaction(custom_op, alice_private_key);
@@ -172,6 +185,167 @@ BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply )
     BOOST_REQUIRE( from_rc_account_deleted.received_delegated_rc == 0 );
     BOOST_REQUIRE( to_rc_account_deleted.delegated_rc == 0 );
     BOOST_REQUIRE( to_rc_account_deleted.received_delegated_rc == 0 );
+  }
+  FC_LOG_AND_RETHROW()
+}
+
+BOOST_AUTO_TEST_CASE( delegate_rc_operation_apply_many )
+{
+  try
+  {
+    BOOST_TEST_MESSAGE( "Testing:  delegate_rc_operation_apply_many to many accounts" );
+    ACTORS( (alice)(bob)(dave)(dan) )
+    vest( HIVE_INIT_MINER_NAME, "alice", ASSET( "10.000 TESTS" ) );
+    int64_t alice_vests = alice.vesting_shares.amount.value;
+
+    // Delegating more rc than alice has should fail
+    delegate_rc_operation op;
+    op.from = "alice";
+    op.delegatees = {"bob", "dave"};
+    op.max_rc = alice_vests;
+
+    custom_json_operation custom_op;
+    custom_op.required_posting_auths.insert( "alice" );
+    custom_op.id = HIVE_RC_PLUGIN_NAME;
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
+
+    // Delegating to a non-existing account should fail
+    op.delegatees = {"bob", "eve"};
+    op.max_rc = 10;
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
+
+    // Delegating 0 shouldn't work if there isn't already a delegation that exists (since 0 deletes the delegation)
+    op.delegatees = {"dave", "bob"};
+    op.max_rc = 0;
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
+
+    // Successful delegations
+    op.delegatees = {"bob", "dave"};
+    op.max_rc = 10;
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    push_transaction(custom_op, alice_private_key);
+
+    generate_block();
+
+    const rc_direct_delegation_object* delegation = db->find< rc_direct_delegation_object, by_from_to >( boost::make_tuple( alice_id, bob_id ) );
+    BOOST_REQUIRE( delegation->from == alice_id );
+    BOOST_REQUIRE( delegation->to == bob_id );
+    BOOST_REQUIRE( delegation->delegated_rc == op.max_rc );
+
+    const rc_account_object& from_rc_account = db->get< rc_account_object, by_name >( op.from );
+    const rc_account_object& to_rc_account = db->get< rc_account_object, by_name >( op.delegatees[0] );
+
+    BOOST_REQUIRE( from_rc_account.delegated_rc == 20 );
+    BOOST_REQUIRE( from_rc_account.received_delegated_rc == 0 );
+    BOOST_REQUIRE( to_rc_account.delegated_rc == 0 );
+    BOOST_REQUIRE( to_rc_account.received_delegated_rc == 10 );
+
+    // Delegating the same amount shouldn't work
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
+
+
+    // Delegating 0 shouldn't work if there isn't already a delegation that exists (since 0 deletes the delegation)
+    // dave/bob got a delegation but not dan so it should fail
+    op.delegatees = {"dave", "bob", "dan"};
+    op.max_rc = 0;
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    BOOST_CHECK_THROW( push_transaction(custom_op, alice_private_key);, fc::exception );
+
+    // Decrease the delegations
+    op.from = "alice";
+    op.delegatees = {"bob", "dave"};
+    op.max_rc = 5;
+    custom_op.required_posting_auths.clear();
+    custom_op.required_posting_auths.insert( "alice" );
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    push_transaction(custom_op, alice_private_key);
+    generate_block();
+
+    const rc_direct_delegation_object* delegation_decreased_bob = db->find< rc_direct_delegation_object, by_from_to >( boost::make_tuple( alice_id, bob_id ) );
+    BOOST_REQUIRE( delegation_decreased_bob->from == alice_id );
+    BOOST_REQUIRE( delegation_decreased_bob->to == bob_id );
+    BOOST_REQUIRE( delegation_decreased_bob->delegated_rc == op.max_rc );
+
+    const rc_direct_delegation_object* delegation_decreased_dave = db->find< rc_direct_delegation_object, by_from_to >( boost::make_tuple( alice_id, dave_id ) );
+    BOOST_REQUIRE( delegation_decreased_dave->from == alice_id );
+    BOOST_REQUIRE( delegation_decreased_dave->to == dave_id );
+    BOOST_REQUIRE( delegation_decreased_dave->delegated_rc == op.max_rc );
+
+    const rc_account_object& from_rc_account_decreased = db->get< rc_account_object, by_name >( op.from );
+    const rc_account_object& bob_rc_account_decreased = db->get< rc_account_object, by_name >( "bob" );
+    const rc_account_object& dave_rc_account_decreased = db->get< rc_account_object, by_name >( "dave" );
+
+    BOOST_REQUIRE( from_rc_account_decreased.delegated_rc == 10 );
+    BOOST_REQUIRE( from_rc_account_decreased.received_delegated_rc == 0 );
+    BOOST_REQUIRE( bob_rc_account_decreased.delegated_rc == 0 );
+    BOOST_REQUIRE( bob_rc_account_decreased.received_delegated_rc == 5 );
+    BOOST_REQUIRE( dave_rc_account_decreased.delegated_rc == 0 );
+    BOOST_REQUIRE( dave_rc_account_decreased.received_delegated_rc == 5 );
+
+    // Increase and create a delegation
+    op.from = "alice";
+    op.delegatees = {"bob", "dan"};
+    op.max_rc = 50;
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    push_transaction(custom_op, alice_private_key);
+
+    generate_block();
+
+    const rc_direct_delegation_object* bob_delegation_increased = db->find< rc_direct_delegation_object, by_from_to >( boost::make_tuple( alice_id, bob_id ) );
+    const rc_direct_delegation_object* dan_delegation_created = db->find< rc_direct_delegation_object, by_from_to >( boost::make_tuple( alice_id, dan_id ) );
+
+    BOOST_REQUIRE( bob_delegation_increased->from == alice_id );
+    BOOST_REQUIRE( bob_delegation_increased->to == bob_id );
+    BOOST_REQUIRE( bob_delegation_increased->delegated_rc == op.max_rc );
+    BOOST_REQUIRE( dan_delegation_created->from == alice_id );
+    BOOST_REQUIRE( dan_delegation_created->to == dan_id );
+    BOOST_REQUIRE( dan_delegation_created->delegated_rc == op.max_rc );
+
+    const rc_account_object& alice_rc_account_increased = db->get< rc_account_object, by_name >( op.from );
+    const rc_account_object& dan_rc_account_created = db->get< rc_account_object, by_name >( "dan" );
+    const rc_account_object& bob_rc_account_increased = db->get< rc_account_object, by_name >( "bob" );
+
+    BOOST_REQUIRE( alice_rc_account_increased.delegated_rc == 105 );
+    BOOST_REQUIRE( alice_rc_account_increased.received_delegated_rc == 0 );
+
+    BOOST_REQUIRE( dan_rc_account_created.delegated_rc == 0 );
+    BOOST_REQUIRE( dan_rc_account_created.received_delegated_rc == 50 );
+    BOOST_REQUIRE( bob_rc_account_increased.delegated_rc == 0 );
+    BOOST_REQUIRE( bob_rc_account_increased.received_delegated_rc == 50 );
+
+    // Delete the delegations
+    op.from = "alice";
+    op.delegatees = {"bob", "dan", "dave"};
+    op.max_rc = 0;
+    custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+    push_transaction(custom_op, alice_private_key);
+
+    generate_block();
+
+    const rc_direct_delegation_object* delegation_bob_deleted = db->find< rc_direct_delegation_object, by_from_to >( boost::make_tuple( alice_id, bob_id ) );
+    const rc_direct_delegation_object* delegation_dan_deleted = db->find< rc_direct_delegation_object, by_from_to >( boost::make_tuple( alice_id, dan_id ) );
+    const rc_direct_delegation_object* delegation_dave_deleted = db->find< rc_direct_delegation_object, by_from_to >( boost::make_tuple( alice_id, dan_id ) );
+    BOOST_REQUIRE( delegation_bob_deleted == nullptr );
+    BOOST_REQUIRE( delegation_dan_deleted == nullptr );
+    BOOST_REQUIRE( delegation_dave_deleted == nullptr );
+
+    const rc_account_object& from_rc_account_deleted = db->get< rc_account_object, by_name >( "alice" );
+    const rc_account_object& bob_rc_account_deleted = db->get< rc_account_object, by_name >( "bob" );
+    const rc_account_object& dave_rc_account_deleted = db->get< rc_account_object, by_name >( "dave" );
+    const rc_account_object& dan_rc_account_deleted = db->get< rc_account_object, by_name >( "dan" );
+
+    BOOST_REQUIRE( from_rc_account_deleted.delegated_rc == 0 );
+    BOOST_REQUIRE( from_rc_account_deleted.received_delegated_rc == 0 );
+    BOOST_REQUIRE( bob_rc_account_deleted.delegated_rc == 0 );
+    BOOST_REQUIRE( bob_rc_account_deleted.received_delegated_rc == 0 );
+    BOOST_REQUIRE( dave_rc_account_deleted.delegated_rc == 0 );
+    BOOST_REQUIRE( dave_rc_account_deleted.received_delegated_rc == 0 );
+    BOOST_REQUIRE( dan_rc_account_deleted.delegated_rc == 0 );
+    BOOST_REQUIRE( dan_rc_account_deleted.received_delegated_rc == 0 );
   }
   FC_LOG_AND_RETHROW()
 }
@@ -232,7 +406,7 @@ BOOST_AUTO_TEST_CASE( update_outdel_overflow )
     // Delegate 10 rc to bob, 80 to dave, alice has 10 remaining rc
     delegate_rc_operation op;
     op.from = "alice";
-    op.to = "bob";
+    op.delegatees = {"bob"};
     op.max_rc = 10;
     custom_json_operation custom_op;
     custom_op.required_posting_auths.insert( "alice" );
@@ -240,7 +414,7 @@ BOOST_AUTO_TEST_CASE( update_outdel_overflow )
     custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
     push_transaction(custom_op, alice_private_key);
 
-    op.to = "dave";
+    op.delegatees = {"dave"};
     op.max_rc = 80;
     custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
     push_transaction(custom_op, alice_private_key);
@@ -270,7 +444,7 @@ BOOST_AUTO_TEST_CASE( update_outdel_overflow )
     delegate_vesting_shares_operation dvso;
     dvso.vesting_shares = ASSET( "0.000005 VESTS");
     dvso.delegator = "alice";
-    dvso.delegatee = "eve";
+    dvso.delegatee = {"eve"};
     push_transaction(dvso, alice_private_key);
 
     const rc_account_object& bob_rc_account_after = db->get< rc_account_object, by_name >("bob");
@@ -365,18 +539,25 @@ BOOST_AUTO_TEST_CASE( update_outdel_overflow_many_accounts )
       }
     });
 
+    int count = 0;
+
+    delegate_rc_operation op;
+    op.from = "alice";
+    op.max_rc = 10;
     // Delegate 10 rc to every actor account
     for (int i = 0; i < NUM_ACTORS; i++) {
-      delegate_rc_operation op;
-      op.from = "alice";
-      op.max_rc = 10;
-      op.to = "actor" + std::to_string(i);
-      custom_json_operation custom_op;
-      custom_op.required_posting_auths.insert( "alice" );
-      custom_op.id = HIVE_RC_PLUGIN_NAME;
-      custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
-      push_transaction(custom_op, alice_private_key);
-      generate_block();
+      op.delegatees.push_back( "actor" + std::to_string(i) );
+      if (count == 50 || i == NUM_ACTORS -1 ) {
+        custom_json_operation custom_op;
+        custom_op.required_posting_auths.insert( "alice" );
+        custom_op.id = HIVE_RC_PLUGIN_NAME;
+        custom_op.json = fc::json::to_string( rc_plugin_operation( op ) );
+        push_transaction(custom_op, alice_private_key);
+        generate_block();
+        op.delegatees = {};
+        count = 0;
+      }
+      count++;
     }
 
     const rc_account_object& actor0_rc_account_before = db->get< rc_account_object, by_name >("actor0");
