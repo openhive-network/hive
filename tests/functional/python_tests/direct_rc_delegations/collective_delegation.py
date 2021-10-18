700b0a722bbe70f761fa63c355d22ca48ec13582
Radek Masłowski <rmaslowski@syncad.com>
2021-10-18 16:35:26 +0200

    Modify delegation rc test to use accounts from snapshot blockchain.

--- tests/functional/python_tests/direct_rc_delegations/collective_delegation.py
+++ tests/functional/python_tests/direct_rc_delegations/collective_delegation.py
@@ -1,62 +1,56 @@
 from concurrent.futures import ThreadPoolExecutor
-from test_tools import Account, logger, Wallet
+from test_tools import context, logger, Wallet
 import time
 
-def test_direct_rc_delegations(wallet, node):
-    creator = 'initminer'
-    delegator = 'delegator'
+
+def test_direct_rc_delegations(world):
+    node = world.create_init_node()
+    node.run(load_snapshot_from='/home/dev/src/hive/tests/functional/python_tests/direct_rc_delegations/generated_during_creation_accounts_snapshot/test_direct_rc_delegations/InitNode/snapshot')
+
     client_accounts = []
-    number_of_accounts = 4000
-    number_of_accounts_in_one_transaction = 200
-    number_of_transactions = int(number_of_accounts / number_of_accounts_in_one_transaction)
-    account_number_absolute = 0
+    number_of_accounts_in_one_transaction = 400
+    number_of_transactions = 2500
+    number_of_accounts = number_of_accounts_in_one_transaction * number_of_transactions
 
-    accounts = Account.create_multiple(number_of_accounts, 'receiver')
-
-    for number_of_transaction in range(number_of_transactions):
-        with wallet.in_single_transaction():
-            for account_number in range(number_of_accounts_in_one_transaction):
-                account_name = accounts[account_number_absolute].name
-                account_key = accounts[account_number_absolute].public_key
-                wallet.api.create_account_with_keys('initminer', account_name, '{}', account_key, account_key,
-                                                    account_key, account_key)
-                client_accounts.append(account_name)
-                account_number_absolute = account_number_absolute + 1
-            logger.info('Group of accounts created')
-    logger.info('End account creations')
-    accounts_to_delegate = split_list(client_accounts, int(number_of_accounts/100))
+    for account_number_absolute in range(number_of_accounts):
+        client_accounts.append(f'receiver-{account_number_absolute}')
+    accounts_to_delegate = split_list(client_accounts, int(number_of_accounts / 100))
     logger.info('End of splitting')
 
+    number_of_threads = 10
+    wallets = []
+    delegators = []
+
+    for thread_number in range(number_of_threads):
+        wallet = Wallet(attach_to=node)
+        wallets.append(wallet)
+        wallet.api.create_account('initminer', f'delegator{thread_number}', '{}')
+        wallet.api.set_password('password')
+        wallet.api.unlock('password')
+        delegators.append(f'delegator{thread_number}')
+        wallet.api.transfer_to_vesting('initminer', f'delegator{thread_number}', '100.010 TESTS')
+
+    accounts_to_delegate_packs = []
+    for thread_number in range(number_of_threads + 1):
+        accounts_to_delegate_packs.append(int(thread_number / number_of_threads * len(accounts_to_delegate)))
+
     tasks_list = []
-    executor = ThreadPoolExecutor(max_workers=10)
+    executor = ThreadPoolExecutor(max_workers=number_of_threads)
+    for thread_number in range(number_of_threads):
+        tasks_list.append(executor.submit(delegation_rc, delegators[thread_number], wallets[thread_number],
+                                          accounts_to_delegate_packs[thread_number],
+                                          accounts_to_delegate_packs[thread_number + 1],
+                                          accounts_to_delegate, thread_number))
 
-    task0 = executor.submit(delegation_rc, 'initminer', 'wallet1', node, 0, int(1/4 * number_of_accounts), accounts_to_delegate)
-    tasks_list.append(task0)
-
-    task1 = executor.submit(delegation_rc, 'initminer', 'wallet1', node, int(1/4 * number_of_accounts), int(2/4 * number_of_accounts), accounts_to_delegate)
-    tasks_list.append(task1)
-
-    task2 = executor.submit(delegation_rc, 'initminer', 'wallet2', node, int(2/4 * number_of_accounts), int(3/4 * number_of_accounts), accounts_to_delegate)
-    tasks_list.append(task2)
-
-    task3 = executor.submit(delegation_rc, 'initminer', 'wallet3', node, int(3/4 * number_of_accounts), number_of_accounts, accounts_to_delegate)
-    tasks_list.append(task3)
-
-    for task in tasks_list:
-        try:
-            print(task.result())
-        except Exception as e:
-            print(e)
-
-    pass
+    for thread_number in tasks_list:
+        thread_number.result()
 
 
-def delegation_rc(creator, wallet, node, first_accounts_pack, last_accounts_pack, accounts_to_delegate):
-    logger.info(f'Thread {first_accounts_pack} work START')
-    wallet = Wallet(attach_to=node)
+def delegation_rc(creator, wallet, first_accounts_pack, last_accounts_pack, accounts_to_delegate, thread_number):
+    logger.info(f'Thread {thread_number} work START')
     for number_of_account_pack in range(first_accounts_pack, last_accounts_pack):
         wallet.api.delegate_rc(creator, accounts_to_delegate[number_of_account_pack], 1)
-    logger.info(f'Thread {first_accounts_pack} work END')
+    logger.info(f'Thread {thread_number} work END')
 
 
 def split_list(alist, wanted_parts):
