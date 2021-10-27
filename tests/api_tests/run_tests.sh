6bde9051a74b59e26cd0b43cf86bf49ff21efb7f
Radek Masłowski <rmaslowski@syncad.com>
2021-10-27 13:00:21 +0200

    Update run_tests.sh

--- tests/api_tests/run_tests.sh
+++ tests/api_tests/run_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
 #pip3 install tox
 #pip3 install tavern
 
 export HIVEMIND_ADDRESS=localhost
-export HIVEMIND_PORT=8090
-export TAVERN_DIR='/home/dev/Patter_tests/src/hive/tests/api_tests' 
+export HIVEMIND_PORT=$1
+export TAVERN_DIR="$2/tests/api_tests"
+pushd $2
 tox -e tavern -- -W ignore::pytest.PytestDeprecationWarning --workers auto --tests-per-worker auto -k account_history_api_patterns --junitxml=results.xml
+popd
