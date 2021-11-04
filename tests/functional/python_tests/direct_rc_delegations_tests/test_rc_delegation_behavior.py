0dc4990391f1a91f9bcf3e2eb40b69ff122c2fa9
Radek Masłowski <rmaslowski@syncad.com>
2021-11-04 12:46:21 +0100

    Clear code.

--- tests/functional/python_tests/direct_rc_delegations_tests/test_rc_delegation_behavior.py
+++ tests/functional/python_tests/direct_rc_delegations_tests/test_rc_delegation_behavior.py
@@ -1,48 +1,28 @@
-from concurrent.futures import ThreadPoolExecutor
-
 import pytest
 
-from test_tools import Account, Asset , constants, exceptions, logger, Wallet
-import time
+from test_tools import Asset, exceptions, Wallet
 
-def test_delegated_rc_account_execute_ops(world, wallet):
-    node = world.create_init_node()
-    node.run()
-    wallet = Wallet(attach_to=node)
 
+def test_delegated_rc_account_execute_ops(wallet: Wallet):
     accounts = []
     number_of_accounts_in_one_transaction = 10
-    number_of_transactions = 1
-    account_number_absolute = 0
-    for number_of_transaction in range(number_of_transactions):
-        with wallet.in_single_transaction():
-            for account_number in range(number_of_accounts_in_one_transaction):
-                wallet.api.create_account('initminer', f'account-{account_number_absolute}', '{}')
-                accounts.append(f'account-{account_number_absolute}')
-                account_number_absolute = account_number_absolute + 1
+    with wallet.in_single_transaction():
+        for account_number in range(number_of_accounts_in_one_transaction):
+            wallet.api.create_account('initminer', f'account-{account_number}', '{}')
+            accounts.append(f'account-{account_number}')
 
     wallet.api.transfer_to_vesting('initminer', accounts[0], Asset.Test(0.1))
-    wallet.api.transfer('initminer', accounts[0], Asset.Test(1), '')
-
     wallet.api.delegate_rc(accounts[0], [accounts[1]], 100)
     wallet.api.create_account(accounts[1], 'alice', '{}')
 
 
-def test_undelegated_rc_account_reject_execute_ops(world):
-    node = world.create_init_node()
-    node.run()
-    wallet = Wallet(attach_to=node)
-
+def test_undelegated_rc_account_reject_execute_ops(wallet: Wallet):
     accounts = []
     number_of_accounts_in_one_transaction = 10
-    number_of_transactions = 1
-    account_number_absolute = 0
-    for number_of_transaction in range(number_of_transactions):
-        with wallet.in_single_transaction():
-            for account_number in range(number_of_accounts_in_one_transaction):
-                wallet.api.create_account('initminer', f'account-{account_number_absolute}', '{}')
-                accounts.append(f'account-{account_number_absolute}')
-                account_number_absolute = account_number_absolute + 1
+    with wallet.in_single_transaction():
+        for account_number in range(number_of_accounts_in_one_transaction):
+            wallet.api.create_account('initminer', f'account-{account_number}', '{}')
+            accounts.append(f'account-{account_number}')
 
     wallet.api.transfer_to_vesting('initminer', accounts[0], Asset.Test(0.1))
     wallet.api.transfer('initminer', accounts[0], Asset.Test(1), '')
@@ -56,52 +36,42 @@ def test_undelegated_rc_account_reject_execute_ops(world):
         wallet.api.create_account(accounts[1], 'bob', '{}')
 
 
-def test_delegations_when_delegator_lost_power(world):
-    node = world.create_init_node()
-    node.run()
-    wallet = Wallet(attach_to=node)
-
+def test_delegations_when_delegator_lost_power(wallet: Wallet):
     accounts = []
     number_of_accounts_in_one_transaction = 10
-    number_of_transactions = 1
-    account_number_absolute = 0
-    for number_of_transaction in range(number_of_transactions):
-        with wallet.in_single_transaction():
-            for account_number in range(number_of_accounts_in_one_transaction):
-                wallet.api.create_account('initminer', f'account-{account_number_absolute}', '{}')
-                accounts.append(f'account-{account_number_absolute}')
-                account_number_absolute = account_number_absolute + 1
+    with wallet.in_single_transaction():
+        for account_number in range(number_of_accounts_in_one_transaction):
+            wallet.api.create_account('initminer', f'account-{account_number}', '{}')
+            accounts.append(f'account-{account_number}')
 
     wallet.api.transfer_to_vesting('initminer', accounts[0], Asset.Test(0.01))
     state1 = wallet.api.find_rc_accounts([accounts[0]])
     state2 = wallet.api.get_account(accounts[0])
     number_of_rc = rc_account_info(accounts[0], 'max_rc', wallet)
-    wallet.api.delegate_rc(accounts[0], [accounts[1]], number_of_rc - 10)
-    assert account_info(accounts[0], 'voting_manabar', wallet)['current_mana'] > 3
+    wallet.api.delegate_rc(accounts[0], [accounts[1]], number_of_rc - 4)
 
+    assert rc_account_info(accounts[0], 'rc_manabar', wallet)['current_mana'] > 3
+
+    state3 = wallet.api.find_rc_accounts([accounts[0]])
     vests_to_withdraw = account_info(accounts[0], 'vesting_shares', wallet)
     wallet.api.withdraw_vesting(accounts[0], vests_to_withdraw)
 
-    state3 = wallet.api.find_rc_accounts([accounts[0]])
-    state4 = wallet.api.get_account(accounts[0])
+    state4 = wallet.api.find_rc_accounts([accounts[0]])
+    state5 = wallet.api.get_account(accounts[0])
 
     pass
 
-def test_same_value_rc_delegation(world):
-    node = world.create_init_node()
-    node.run()
-    wallet = Wallet(attach_to=node)
+
+def test_same_value_rc_delegation(node, wallet: Wallet):
 
     accounts = []
     number_of_accounts_in_one_transaction = 10
     number_of_transactions = 1
-    account_number_absolute = 0
     for number_of_transaction in range(number_of_transactions):
         with wallet.in_single_transaction():
             for account_number in range(number_of_accounts_in_one_transaction):
-                wallet.api.create_account('initminer', f'account-{account_number_absolute}', '{}')
-                accounts.append(f'account-{account_number_absolute}')
-                account_number_absolute = account_number_absolute + 1
+                wallet.api.create_account('initminer', f'account-{account_number}', '{}')
+                accounts.append(f'account-{account_number}')
 
     wallet.api.transfer_to_vesting('initminer', accounts[0], Asset.Test(10))
     wallet.api.transfer_to_vesting('initminer', accounts[1], Asset.Test(10))
@@ -120,20 +90,14 @@ def test_same_value_rc_delegation(world):
         wallet.api.delegate_rc(accounts[0], [accounts[2]], 10)
 
 
-def test_delegations_rc_to_one_receiver(world):
-    node = world.create_init_node()
-    node.run()
-    wallet = Wallet(attach_to=node)
-
+def test_delegations_rc_to_one_receiver(wallet: Wallet):
     accounts = []
     number_of_accounts = 120
-    account_number_absolute = 0
 
     with wallet.in_single_transaction():
         for account_number in range(number_of_accounts):
-            wallet.api.create_account('initminer', f'account-{account_number_absolute}', '{}')
-            accounts.append(f'account-{account_number_absolute}')
-            account_number_absolute = account_number_absolute + 1
+            wallet.api.create_account('initminer', f'account-{account_number}', '{}')
+            accounts.append(f'account-{account_number}')
 
     number_of_transfers = 100
     account_number_absolute = 1
@@ -151,63 +115,49 @@ def test_delegations_rc_to_one_receiver(world):
             account_number_absolute = account_number_absolute + 1
 
 
-def test_reject_of_delegation_of_delegated_rc(world):
-    node = world.create_init_node()
-    node.run()
-    wallet = Wallet(attach_to=node)
-
+def test_reject_of_delegation_of_delegated_rc(wallet: Wallet):
     accounts = []
     number_of_accounts = 10
-    account_number_absolute = 0
 
     with wallet.in_single_transaction():
         for account_number in range(number_of_accounts):
-            wallet.api.create_account('initminer', f'account-{account_number_absolute}', '{}')
-            accounts.append(f'account-{account_number_absolute}')
-            account_number_absolute = account_number_absolute + 1
+            wallet.api.create_account('initminer', f'account-{account_number}', '{}')
+            accounts.append(f'account-{account_number}')
 
     wallet.api.transfer_to_vesting('initminer', accounts[0], Asset.Test(0.1))
-    number_of_rc = rc_account_info(accounts[0], 'rc_manabar', wallet)['current_mana']
-    wallet.api.delegate_rc(accounts[0], [accounts[1]], number_of_rc - 3)
+    wallet.api.delegate_rc(accounts[0], [accounts[1]], 100)
 
     with pytest.raises(exceptions.CommunicationError):
-        wallet.api.delegate_rc(accounts[1], [accounts[2]], number_of_rc - 3 - 3)
+        wallet.api.delegate_rc(accounts[1], [accounts[2]], 50)
 
 
-def test_cost_of_doing_transaction(world):
-
-    node = world.create_init_node()
-    node.run()
-    wallet = Wallet(attach_to=node)
-
+def test_cost_of_doing_transaction(wallet: Wallet):
     accounts = []
     number_of_accounts = 10
-    account_number_absolute = 0
 
     with wallet.in_single_transaction():
         for account_number in range(number_of_accounts):
-            wallet.api.create_account('initminer', f'account-{account_number_absolute}', '{}')
-            accounts.append(f'account-{account_number_absolute}')
-            account_number_absolute = account_number_absolute + 1
+            wallet.api.create_account('initminer', f'account-{account_number}', '{}')
+            accounts.append(f'account-{account_number}')
 
     wallet.api.transfer_to_vesting('initminer', accounts[0], Asset.Test(0.1))
 
     wallet.api.delegate_rc(accounts[0], [accounts[1]], 100)
     number_of_mana = rc_account_info(accounts[0], 'rc_manabar', wallet)['current_mana']
-    number_of_RC = rc_account_info(accounts[0], 'max_rc', wallet)
-    assert number_of_mana == number_of_RC - 3 # cost of operation is 3
+    number_of_rc = rc_account_info(accounts[0], 'max_rc', wallet)
+    assert number_of_mana == number_of_rc - 3  # cost of operation is 3
 
     wallet.api.delegate_rc(accounts[0], [accounts[1]], 3000)
     number_of_mana = rc_account_info(accounts[0], 'rc_manabar', wallet)['current_mana']
-    number_of_RC = rc_account_info(accounts[0], 'max_rc', wallet)
-    assert number_of_mana == number_of_RC - 3 - 3
+    number_of_rc = rc_account_info(accounts[0], 'max_rc', wallet)
+    assert number_of_mana == number_of_rc - 3 - 3
 
-    pass
 
 def rc_account_info(account, name_of_data, wallet):
     data_set = wallet.api.find_rc_accounts([account])['result'][0]
     specyfic_data = data_set[name_of_data]
     return specyfic_data
+
 
 def account_info(account, name_of_data, wallet):
     data_set = wallet.api.get_account(account)['result']
