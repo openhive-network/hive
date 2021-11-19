5d136997069fd383de8913c68ba209b76de7033c
Bartek Wrona <wrona@syncad.com>
2021-11-19 09:18:24 +0100

    CI build/runtime/test images upgraded to ubuntu 20.04 and referenced at CI stages.

--- ciscripts/build_ci_docker_images.sh
+++ ciscripts/build_ci_docker_images.sh
@@ -1,7 +1,9 @@
-#! /bin/sh
+#! /bin/bash
+
+set -euo pipefail 
 
 REGISTRY=$1
 
-docker build --target=runtime --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/runtime -f Dockerfile.ci .
-docker build --target=builder --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/builder -f Dockerfile.ci .
-docker build --target=test --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/test -f Dockerfile.ci .
+docker build --target=runtime --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/runtime:ubuntu-20.04 -f Dockerfile.ci .
+docker build --target=builder --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/builder:ubuntu-20.04 -f Dockerfile.ci .
+docker build --target=test --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/test:ubuntu-20.04 -f Dockerfile.ci .
