1b3c2c1d6b51e7ab94cacd12778aab612bd0b5b0
Radek Masłowski <rmaslowski@syncad.com>
2021-11-19 13:09:07 +0100

    Merge tests on stop_after_replay

--- tests/functional/python_tests/hived/conftest.py
+++ tests/functional/python_tests/hived/conftest.py
@@ -1,31 +1,32 @@
 from pathlib import Path
+from typing import Tuple
+
 from pytest import fixture
 from test_tools import *
-from test_tools import constants, context, Wallet
+from test_tools import BlockLog, Wallet
 
-BLOCK_COUNT = 30
 
 @fixture(scope='package')
-def block_log() -> Path:
-  from time import sleep
-  with World(directory=context.get_current_directory()) as world:
-    world.set_clean_up_policy(constants.WorldCleanUpPolicy.REMOVE_ONLY_UNNEEDED_FILES)
-    logger.info(f'preparing block log with {BLOCK_COUNT} blocks')
-    node = world.create_init_node()
-    node.run(wait_for_live=True)
+def block_log_helper() -> Tuple[BlockLog, int]:
+    BLOCK_COUNT = 30
+    with World() as world:
+        logger.info(f'preparing block log with {BLOCK_COUNT} blocks')
+        node = world.create_init_node()
+        node.run(wait_for_live=True)
 
-    wallet = Wallet(attach_to=node)
-    initminer = node.config.witness[0]
+        node.wait_for_block_with_number(BLOCK_COUNT)
+        block_log_length = node.get_last_block_number()
 
-    current_block = 0
-    while current_block <= BLOCK_COUNT:
-      for _ in range(4):
-        # put here some dummy trx so block log does not contain just empty blocks
-        wallet.api.transfer_to_vesting(from_=initminer, to=initminer, amount=Asset.Test(0.001))
+        node.close()
+        logger.info(f'prepared block log with {block_log_length} blocks')
+        yield node.get_block_log(), block_log_length
 
-        sleep(0.2)
-      current_block = node.get_last_block_number()
 
-    node.close()
-    logger.info(f'prepared block log with {BLOCK_COUNT} blocks')
-    yield node.get_block_log().get_path()
+@fixture(scope='package')
+def block_log(block_log_helper) -> Path:
+    return block_log_helper[0].get_path()
+
+
+@fixture(scope='package')
+def block_log_length(block_log_helper) -> int:
+    return block_log_helper[1]
--- tests/functional/python_tests/hived/test_hived_options.py
+++ tests/functional/python_tests/hived/test_hived_options.py
@@ -2,74 +2,79 @@ from pathlib import Path
 
 import pytest
 
-from test_tools import exceptions, World
-from .conftest import BLOCK_COUNT
+from test_tools import World
 
 
 def test_dump_config(world: World):
-    node = world.create_init_node('init_0')
-
+    node = world.create_init_node()
     old_config = dict()
     for key, value in node.config.__dict__.items():
         old_config[key] = value
-
     node.run()
     node.wait_number_of_blocks(2)
     node.close()
     node.dump_config()
-
     assert node.config.__dict__ == old_config
 
 
-def test_exit_before_sync(world: World, block_log: Path):
-    from shutil import rmtree
-    from os import remove
-    from os.path import join
-
-    init = world.create_api_node(name='node_1')
-    half_way = int(BLOCK_COUNT / 2.0)
-
-    init.run(replay_from=block_log, stop_at_block=half_way, exit_before_synchronization=True)
-    assert not init.is_running()
-
-    rmtree(join(str(init.directory), 'blockchain'), ignore_errors=True)
-    init.run(replay_from=block_log, exit_before_synchronization=True)
-    assert not init.is_running()
-
-    snap = init.dump_snapshot(close=True)
-    assert not init.is_running()
-
-    remove(join(str(init.directory), 'blockchain', 'shared_memory.bin'))
-    init.run(load_snapshot_from=snap, exit_before_synchronization=True)
-    assert not init.is_running()
-
-
-def test_unsupported_plugin(world: World):
-    init_node = world.create_init_node()
-    init_node.config.plugin.append('UNSUPPORTED_PLUGIN')
-
-    with pytest.raises(exceptions.InternalNodeError):
-        init_node.run()
-
-
-def test_corrupted_shared_memory_file(world: World):
-    corrupted_content = b'000'
-
+def test_no_warning_about_deprecated_flag_exit_after_replay_when_it_is_not_used(world: World):
     node = world.create_init_node()
     node.run()
-    node.close()
 
-    shared_memory_file_path = node.directory / 'blockchain/shared_memory.bin'
-    stderr_file_path = node.directory / 'stderr.txt'
+    with open(node.directory / 'stderr.txt') as file:
+        stderr = file.read()
 
-    with open(shared_memory_file_path, 'wb') as file:
-        file.write(corrupted_content)
+    warning = "flag `--exit-after-replay` is deprecated, please consider usage of `--exit-before-sync`"
+    assert warning not in stderr
 
-    with pytest.raises(TimeoutError):
-        node.run()
 
-    with open(stderr_file_path, 'r') as file:
-        stderr_file_content = file.read()
+def test_warning_about_deprecated_flag_exit_after_replay(block_log, world: World):
+    node = world.create_api_node()
 
-    assert 'Error opening database. If the binary or configuration has changed, replay the blockchain explicitly ' \
-           'using `--force-replay`.' in stderr_file_content
+    node.run(replay_from=block_log, arguments=['--exit-after-replay'])
+
+    with open(node.directory / 'stderr.txt') as file:
+        stderr = file.read()
+
+    warning = "flag `--exit-after-replay` is deprecated, please consider usage of `--exit-before-sync`"
+    assert warning in stderr
+
+
+@pytest.mark.parametrize(
+    'way_to_stop', [
+        {'arguments': ['--exit-after-replay']},
+        {'exit_before_synchronization': True},
+    ]
+)
+def test_stop_after_replay(way_to_stop, world: World, block_log: Path, block_log_length: int):
+    network = world.create_network()
+    node_which_should_not_synchronize = network.create_api_node()
+    node_with_new_blocks = network.create_init_node()
+
+    # Generate new blocks, which are not present in source block log,
+    # to check if other node will try to download them (synchronize).
+    node_with_new_blocks.run(replay_from=block_log, wait_for_live=True)
+    node_with_new_blocks.wait_number_of_blocks(6)
+
+    node_which_should_not_synchronize.run(replay_from=block_log, **way_to_stop)
+    assert not node_which_should_not_synchronize.is_running()
+
+    node_with_new_blocks.close()  # Now node_which_should_not_synchronize will surely not synchronize.
+
+    node_which_should_not_synchronize.run(wait_for_live=False)
+    assert node_which_should_not_synchronize.get_last_block_number() == block_log_length  # No new blocks.
+
+
+@pytest.mark.parametrize(
+    'way_to_stop', [
+        {'arguments': ['--exit-after-replay']},
+        {'exit_before_synchronization': True},
+    ]
+)
+def test_stop_after_replay_in_load_from_snapshot(way_to_stop, world: World, block_log: Path):
+    node = world.create_api_node()
+    node.run(replay_from=block_log, **way_to_stop)
+    snap = node.dump_snapshot(close=True)
+    Path(node.directory / 'blockchain' / 'shared_memory.bin').unlink()
+    node.run(load_snapshot_from=snap, **way_to_stop)
+    assert not node.is_running()
