acd2b5c0f05fb6ac9dc5a360984b9e1e6f9e828a
Radek Masłowski <rmaslowski@syncad.com>
2021-12-03 15:41:57 +0100

    Edit unit test

--- tests/functional/python_tests/hivemind_tests/unit_tests.py
+++ tests/functional/python_tests/hivemind_tests/unit_tests.py
@@ -3,27 +3,20 @@ import re
 import subprocess
 import time
 
-from test_tools import communication, logger, World, Wallet, hivemind
-# database = "postgresql://dev:devdevdev@localhost:5432/hivemind_pyt"
+from test_tools import communication, logger, Hivemind, World, Wallet
 def test_hivemind_start_sync(world: World):
     node_test = world.create_init_node()
     node_test.run()
 
-    hivemind1 = hivemind.Hivemind(
-        host='localhost',
-        port='5432',
+    hivemind = Hivemind(
         database_name='hivemind_pyt',
-        user='dev',
-        password='devdevdev',
-        node=node_test
+        database_user='dev',
+        database_password='devdevdev',
+        sync_with=node_test
     )
-
-    hivemind1.environment_setup()
-    hivemind1.run_sync(node_test)  # sync
-    hivemind1.run_server() #server
+    hivemind.detabase_prepare()
+    hivemind.run()
     time.sleep(10)
     message = communication.request(url='http://localhost:8080', message={"jsonrpc":"2.0","id":0,"method":"hive.db_head_state","params":{}})
-    time.sleep(100)
+    print()
 
-    # communication.request()
-    # accounts_number = hivemind1.how_many_accounts()
