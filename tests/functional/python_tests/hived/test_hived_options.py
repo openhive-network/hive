0e2f3f96ac9491733af1e98a7b8bf4a7d1160b58
Radek Masłowski <rmaslowski@syncad.com>
2021-12-07 10:40:47 +0100

    Add 4 new tests for exit_after_replay flag

--- tests/functional/python_tests/hived/test_hived_options.py
+++ tests/functional/python_tests/hived/test_hived_options.py
@@ -77,7 +77,7 @@ def test_exit_after_replay_behavior(world: World, block_log: Path):
     init = net.create_api_node()
     half_way = int(BLOCK_COUNT / 2.0)
 
-    init.run(replay_from=block_log, stop_at_block=half_way, with_arguments=['--exit-after-replay'])
+    init.run(replay_from=block_log, stop_at_block=half_way, with_arguments=['--exit-after-replay']) #osobny test
     assert not init.is_running()
 
     background_node = net.create_init_node()
@@ -90,9 +90,45 @@ def test_exit_after_replay_behavior(world: World, block_log: Path):
 
     background_node.close()
 
-    snap = init.dump_snapshot(close=True)
+    snap = init.dump_snapshot(close=True) #osobny test
     assert not init.is_running()
 
-    remove(join(str(init.directory), 'blockchain', 'shared_memory.bin'))
+    remove(join(str(init.directory), 'blockchain', 'shared_memory.bin')) # osobny test
     init.run(load_snapshot_from=snap, with_arguments=['--exit-after-replay'])
     assert not init.is_running()
+
+def test_exit_after_replay_stop_in_half_way(world: World, block_log: Path):
+    node = world.create_api_node()
+    half_way = int(BLOCK_COUNT / 2.0)
+
+    node.run(replay_from=block_log, stop_at_block=half_way, with_arguments=['--exit-after-replay'])
+    assert not node.is_running()
+
+def test_exit_after_replay_not_run_from_half_way(world: World, block_log: Path):
+    node = world.create_api_node()
+    half_way = int(BLOCK_COUNT / 2.0)
+
+    node.run(replay_from=block_log, stop_at_block=half_way, with_arguments=['--exit-after-replay'])
+
+    background_node = world.create_init_node()
+    background_node.run(replay_from=block_log, wait_for_live=True)
+    background_node.wait_number_of_blocks(2)
+
+    rmtree(join(str(node.directory), 'blockchain'), ignore_errors=True)
+    node.run(replay_from=block_log, with_arguments=['--exit-after-replay'])
+    assert not node.is_running()
+
+def test_exit_after_replay_stop_after_dump(world: World, block_log: Path):
+    node = world.create_api_node()
+    node.run(replay_from=block_log, with_arguments=['--exit-after-replay'])
+    snap = node.dump_snapshot(close=True) #osobny test
+    assert not node.is_running()
+
+
+def test_exit_after_replay_stop_after_load_snapshot(world: World, block_log: Path):
+    node = world.create_api_node()
+    node.run(replay_from=block_log, with_arguments=['--exit-after-replay'])
+    snap = node.dump_snapshot(close=True)
+    remove(join(str(node.directory), 'blockchain', 'shared_memory.bin'))
+    node.run(load_snapshot_from=snap, with_arguments=['--exit-after-replay'])
+    assert not node.is_running()
