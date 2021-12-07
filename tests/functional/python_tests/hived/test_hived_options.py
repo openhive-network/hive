68b12e96e78d6ff855329287cd61b7b71e3f720d
Radek Masłowski <rmaslowski@syncad.com>
2021-12-07 10:48:14 +0100

    Add 4 exit_before_sync tests

--- tests/functional/python_tests/hived/test_hived_options.py
+++ tests/functional/python_tests/hived/test_hived_options.py
@@ -132,3 +132,40 @@ def test_exit_after_replay_stop_after_load_snapshot(world: World, block_log: Pat
     remove(join(str(node.directory), 'blockchain', 'shared_memory.bin'))
     node.run(load_snapshot_from=snap, with_arguments=['--exit-after-replay'])
     assert not node.is_running()
+
+def test_exit_before_sync_stop_in_half_way(world: World, block_log: Path):
+    node = world.create_api_node()
+    half_way = int(BLOCK_COUNT / 2.0)
+
+    node.run(replay_from=block_log, stop_at_block=half_way, exit_before_synchronization=True)
+    assert not node.is_running()
+
+def test_exit_before_sync_not_run_from_half_way(world: World, block_log: Path):
+    node = world.create_api_node()
+    half_way = int(BLOCK_COUNT / 2.0)
+
+    node.run(replay_from=block_log, stop_at_block=half_way, exit_before_synchronization=True)
+
+    background_node = world.create_init_node()
+    background_node.run(replay_from=block_log, wait_for_live=True)
+    background_node.wait_number_of_blocks(2)
+
+    rmtree(join(str(node.directory), 'blockchain'), ignore_errors=True)
+    node.run(replay_from=block_log, exit_before_synchronization=True)
+    assert not node.is_running()
+
+def test_exit_before_sync_stop_after_dump(world: World, block_log: Path):
+    node = world.create_api_node()
+    node.run(replay_from=block_log, exit_before_synchronization=True)
+    snap = node.dump_snapshot(close=True)
+    assert not node.is_running()
+
+
+def test_exit_before_sync_stop_after_load_snapshot(world: World, block_log: Path):
+    node = world.create_api_node()
+    node.run(replay_from=block_log, exit_before_synchronization=True)
+    snap = node.dump_snapshot(close=True)
+    remove(join(str(node.directory), 'blockchain', 'shared_memory.bin'))
+    node.run(load_snapshot_from=snap, exit_before_synchronization=True)
+    assert not node.is_running()
+    
