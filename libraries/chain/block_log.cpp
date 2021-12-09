7c11161c15c530087a6c5efc82c2f03194e06232
Mariusz Trela <mtrela@syncad.com>
2021-12-09 13:36:15 +0100

    Refactoring - part 4

--- libraries/chain/block_log.cpp
+++ libraries/chain/block_log.cpp
@@ -30,8 +30,84 @@ namespace hive { namespace chain {
 
     class block_log_impl
     {
+      private:
+
+        file_manager mgr;
+
       public:
-        file_manager file_mgr;
+
+        void open( const fc::path& file )
+        {
+          mgr.prepare( file );
+        }
+
+        void close()
+        {
+          mgr.close();
+        }
+
+        uint64_t append( const signed_block& b )
+        {
+          try
+          {
+            return mgr.append( b );
+          }
+          FC_LOG_AND_RETHROW()
+        }
+
+        // threading guarantees:
+        // no restrictions
+        void flush()
+        {
+          // writes to file descriptors are flushed automatically
+        }
+
+        optional< signed_block > read_block_by_num( uint32_t block_num )
+        {
+          try
+          {
+            return mgr.read_block_by_num( block_num );
+          }
+          FC_CAPTURE_LOG_AND_RETHROW((block_num))
+        }
+
+        vector<signed_block> read_block_range_by_num( uint32_t first_block_num, uint32_t count )
+        {
+          try
+          {
+            return mgr.get_block_log_idx()->read_block_range( first_block_num, count, mgr.get_block_log_file() );
+          }
+          FC_CAPTURE_LOG_AND_RETHROW((first_block_num)(count))
+        }
+
+        std::tuple< optional<block_id_type>, optional<public_key_type> > read_data_by_num( uint32_t block_num )
+        {
+          try
+          {
+            return mgr.get_hash_idx()->read_data_by_num( block_num );
+          }
+          FC_CAPTURE_LOG_AND_RETHROW((block_num))
+        }
+
+        std::map< uint32_t, std::tuple< optional<block_id_type>, optional<public_key_type> > > read_data_range_by_num( uint32_t first_block_num, uint32_t count )
+        {
+          try
+          {
+            return mgr.get_hash_idx()->read_data_range_by_num( first_block_num, count );
+          }
+          FC_CAPTURE_LOG_AND_RETHROW((first_block_num)(count))
+        }
+
+        // not thread safe, but it's only called when opening the block log, we can assume we're the only thread accessing it
+        signed_block read_head()
+        {
+          return mgr.read_head();
+        }
+
+        const boost::shared_ptr<signed_block> head()
+        {
+          return mgr.get_block_log_file().head.load();
+        }
     };
 
   } // end namespace detail
@@ -46,21 +122,17 @@ namespace hive { namespace chain {
 
   void block_log::open( const fc::path& file )
   {
-    my->file_mgr.prepare( file );
+    my->open( file );
   }
 
   void block_log::close()
   {
-    my->file_mgr.close();
+    my->close();
   }
 
   uint64_t block_log::append( const signed_block& b )
   {
-    try
-    {
-      return my->file_mgr.append( b );
-    }
-    FC_LOG_AND_RETHROW()
+    return my->append( b );
   }
 
   // threading guarantees:
@@ -72,49 +144,32 @@ namespace hive { namespace chain {
 
   optional< signed_block > block_log::read_block_by_num( uint32_t block_num )const
   {
-    try
-    {
-      return my->file_mgr.read_block_by_num( block_num );
-    }
-    FC_CAPTURE_LOG_AND_RETHROW((block_num))
+    return my->read_block_by_num( block_num );
   }
 
   vector<signed_block> block_log::read_block_range_by_num( uint32_t first_block_num, uint32_t count )const
   {
-    try
-    {
-      return my->file_mgr.get_block_log_idx()->read_block_range( first_block_num, count, my->file_mgr.get_block_log_file() );
-    }
-    FC_CAPTURE_LOG_AND_RETHROW((first_block_num)(count))
+    return my->read_block_range_by_num( first_block_num, count );
   }
 
   std::tuple< optional<block_id_type>, optional<public_key_type> > block_log::read_data_by_num( uint32_t block_num )
   {
-    try
-    {
-      return my->file_mgr.get_hash_idx()->read_data_by_num( block_num );
-    }
-    FC_CAPTURE_LOG_AND_RETHROW((block_num))
+    return my->read_data_by_num( block_num );
   }
 
   std::map< uint32_t, std::tuple< optional<block_id_type>, optional<public_key_type> > > block_log::read_data_range_by_num( uint32_t first_block_num, uint32_t count )
   {
-    try
-    {
-      return my->file_mgr.get_hash_idx()->read_data_range_by_num( first_block_num, count );
-    }
-    FC_CAPTURE_LOG_AND_RETHROW((first_block_num)(count))
+    return my->read_data_range_by_num( first_block_num, count );
   }
 
-  // not thread safe, but it's only called when opening the block log, we can assume we're the only thread accessing it
   signed_block block_log::read_head()const
   {
-    return my->file_mgr.read_head();
+    return my->read_head();
   }
 
   const boost::shared_ptr<signed_block> block_log::head()const
   {
-    return my->file_mgr.get_block_log_file().head.load();
+    return my->head();
   }
 
 } } // hive::chain
