0d6622daa28f13435694c7c1387d08ca6499faac
Mariusz Trela <mtrela@syncad.com>
2021-12-14 15:40:56 +0100

    Type of serialization is chosen automatically

--- libraries/protocol/misc_utilities.cpp
+++ libraries/protocol/misc_utilities.cpp
@@ -4,6 +4,24 @@ namespace hive { namespace protocol {
 
 thread_local bool dynamic_serializer::legacy_enabled = false;
 
+const std::string legacy_switcher::serialization_detector = "Invalid cast from";
+
+legacy_switcher::legacy_switcher( bool val ) : old_legacy_enabled( dynamic_serializer::legacy_enabled )
+{
+  dynamic_serializer::legacy_enabled = val;
+}
+
+legacy_switcher::~legacy_switcher()
+{
+  dynamic_serializer::legacy_enabled = old_legacy_enabled;
+}
+
+bool legacy_switcher::try_another_serialization( const fc::bad_cast_exception& e )
+{
+  auto _found = e.to_string().find( serialization_detector );
+  return _found != std::string::npos;
+}
+
 std::string trim_legacy_typename_namespace( const std::string& name )
 {
   auto start = name.find_last_of( ':' ) + 1;
