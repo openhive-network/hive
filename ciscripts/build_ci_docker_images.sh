a007d776f9e35d0c4fc46635c71dd6ff73caaf62
Bartek Wrona <wrona@syncad.com>
2021-12-29 21:39:46 +0000

    Fixed CI python problems during regression tests execution - Introduced .venv environment for python tests execution to eliminate external environment impact. - CI Docker base test image extended by required python3-venv package. Also all packages are tagged as ubuntu-20.04-1 - referenced python packages are explicitly installed in CI steps

--- ciscripts/build_ci_docker_images.sh
+++ ciscripts/build_ci_docker_images.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 REGISTRY=$1
 
-docker build --target=runtime --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/runtime:ubuntu-20.04 -f Dockerfile.ci .
-docker build --target=builder --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/builder:ubuntu-20.04 -f Dockerfile.ci .
-docker build --target=test --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/test:ubuntu-20.04 -f Dockerfile.ci .
+docker build --target=runtime --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/runtime:ubuntu-20.04-1 -f Dockerfile.ci .
+docker build --target=builder --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/builder:ubuntu-20.04-1 -f Dockerfile.ci .
+docker build --target=test --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/test:ubuntu-20.04-1 -f Dockerfile.ci .
