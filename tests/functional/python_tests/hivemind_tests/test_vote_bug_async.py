8f028884040665cb3e5f9d37b614647deb477ad6
Radek Masłowski <rmaslowski@syncad.com>
2021-12-29 15:11:42 +0100

    Improve temporary test i

--- tests/functional/python_tests/hivemind_tests/test_vote_bug_async.py
+++ tests/functional/python_tests/hivemind_tests/test_vote_bug_async.py
@@ -9,54 +9,75 @@ import functional.hive_utils
 
 
 def test_vote_bug(world:World):
-
     node_test = world.create_init_node()
     node_test.run()
     wallet = Wallet(attach_to=node_test)
 
-    wallet.api.create_account('initminer', 'alice', '{}')
-    wallet.api.create_account('initminer', 'bob', '{}')
-    wallet.api.transfer('initminer', 'alice', Asset.Tbd(70008.543), 'avocado')
-    wallet.api.transfer('initminer', 'bob', Asset.Tbd(78008.543), 'banana')
-    wallet.api.transfer_to_vesting('initminer', 'alice', Asset.Test(1000000))
-    wallet.api.transfer_to_vesting('initminer', 'bob', Asset.Test(1000000))
+    with wallet.in_single_transaction():
+        wallet.api.create_account('initminer', 'alice', '{}')
+        wallet.api.create_account('initminer', 'bob', '{}')
+        wallet.api.create_account('initminer', 'tommy0', '{}')
+        wallet.api.create_account('initminer', 'tommy1', '{}')
+        wallet.api.create_account('initminer', 'tommy2', '{}')
+        wallet.api.create_account('initminer', 'tommy3', '{}')
+        wallet.api.create_account('initminer', 'tommy4', '{}')
+        wallet.api.create_account('initminer', 'tommy5', '{}')
+        wallet.api.create_account('initminer', 'tommy6', '{}')
 
-    #operations to prepare bug
+    with wallet.in_single_transaction():
+        wallet.api.transfer('initminer', 'alice', Asset.Tbd(70008.543), 'avocado')
+        wallet.api.transfer('initminer', 'bob', Asset.Tbd(700088.543), 'banana')
+        wallet.api.transfer('initminer', 'tommy0', Asset.Tbd(70008.543), 'blueberry')
+        wallet.api.transfer('initminer', 'tommy1', Asset.Tbd(70008.543), 'apple')
+        wallet.api.transfer('initminer', 'tommy2', Asset.Tbd(70008.543), 'cucumber')
+        wallet.api.transfer('initminer', 'tommy3', Asset.Tbd(70008.543), 'onion')
+        wallet.api.transfer('initminer', 'tommy4', Asset.Tbd(70008.543), 'grappa')
+        wallet.api.transfer('initminer', 'tommy5', Asset.Tbd(70008.543), 'orange')
+        wallet.api.transfer('initminer', 'tommy6', Asset.Tbd(70008.543), 'mandarinian')
+
+    with wallet.in_single_transaction():
+        wallet.api.transfer_to_vesting('initminer', 'alice', Asset.Test(1000000))
+        wallet.api.transfer_to_vesting('initminer', 'bob', Asset.Test(100000))
+        wallet.api.transfer_to_vesting('initminer', 'tommy0', Asset.Test(1000000))
+        wallet.api.transfer_to_vesting('initminer', 'tommy1', Asset.Test(1000000))
+        wallet.api.transfer_to_vesting('initminer', 'tommy2', Asset.Test(1000000))
+        wallet.api.transfer_to_vesting('initminer', 'tommy3', Asset.Test(1000000))
+        wallet.api.transfer_to_vesting('initminer', 'tommy4', Asset.Test(1000000))
+        wallet.api.transfer_to_vesting('initminer', 'tommy5', Asset.Test(1000000))
+        wallet.api.transfer_to_vesting('initminer', 'tommy6', Asset.Test(1000000))
+
+    # operations to prepare bug
     wallet.api.post_comment('alice', 'hello-world2', '', 'xyz2', 'something about world2', 'just nothing2', '{}')
     wallet.api.vote('bob', 'alice', 'hello-world2', 99)
 
     logger.info(node_test.get_last_block_number())
-    node_test.wait_for_block_with_number(12)
+    node_test.wait_for_block_with_number(10)
     generate_blocks(node_test, 1300)
 
-    # time.sleep(10)
-    # hivemind = Hivemind(
-    #     database_name='hivemind_pyt',
-    #     database_user='dev',
-    #     database_password='devdevdev',
-    # )
-    #
-    # task2 = threading.Thread(target=hivemind_run, args=[hivemind, node_test])
-    # task2.start()
-    http_endpoint = node_test.get_http_endpoint()
-    logger.info(http_endpoint)
-    input('1320, uruchom i od razu wyłaćz')
+    # http_endpoint = node_test.get_http_endpoint()
+    # logger.info(http_endpoint)
+    # input('1320, uruchom i od razu wyłącz hajfmańda')
+    # logger.info('Generacja bloków po w celu obudzenia synca')
 
-    input('Wyłącz hiveminda')
-
-    logger.info('Generacja bloków po w celu obudzenia synca')
+    hivemind = Hivemind(
+        database_name='hivemind_pyt',
+        database_user='dev',
+        database_password='devdevdev',
+    )
+    task2 = threading.Thread(target=hivemind_run, args=[hivemind, node_test])
+    task2.start()
+    time.sleep(20)
     generate_blocks(node_test, 1)
     time.sleep(5)
-    task1 = threading.Thread(target=voter, args=[wallet, node_test])
+    task1 = threading.Thread(target=votes_multi, args=[wallet, node_test])
     task1.start()
+
     time.sleep(10)
     generate_blocks(node_test, 1)
-    input('1320, uruchom i od razu wyłacz po raz drugi')
+    task3 = threading.Thread(target=hivemind_run2, args=[hivemind, node_test])
+    task3.start()
+    time.sleep(40)
 
-    time.sleep(5)
-    time.sleep(25)
-    logger.info('Wyłącz program')
-    time.sleep(10)
 
 def generate_blocks(node, number_of_blocks):
     node.api.debug_node.debug_generate_blocks(
@@ -75,7 +96,14 @@ def voter(wallet:Wallet, node_test):
     logger.info('Vote end')
 
 def hivemind_run(hivemind:Hivemind, node_test):
-    hivemind.run(sync_with=node_test, run_server=False)
+    hivemind.run(sync_with=node_test, run_server=False, with_time_offset='+3898')
+    time.sleep(10)
+    hivemind.stop()
+
+def hivemind_run2(hivemind: Hivemind, node_test):
+    hivemind.run(sync_with=node_test, run_server=False, with_time_offset='+3898', database_prepare= False)
+    time.sleep(10)
+    hivemind.stop()
 
 def voter_with_stop(args, wallet:Wallet, node_test):
     t = threading.currentThread()
