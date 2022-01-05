6c144153c820cfcbb83c254e174f392fd07f4499
Marcin Sobczyk <msobczyk@syncad.com>
2022-01-05 15:46:54 +0100

    add missin op_in_trx in AH api

--- libraries/plugins/apis/account_history_api/include/hive/plugins/account_history_api/account_history_api.hpp
+++ libraries/plugins/apis/account_history_api/include/hive/plugins/account_history_api/account_history_api.hpp
@@ -23,6 +23,7 @@ struct api_operation_object
     trx_id( op_obj.trx_id ),
     block( op_obj.block ),
     trx_in_block( op_obj.trx_in_block ),
+    op_in_trx( op_obj.op_in_trx ),
     virtual_op( op_obj.virtual_op ),
     timestamp( op_obj.timestamp )
   {
