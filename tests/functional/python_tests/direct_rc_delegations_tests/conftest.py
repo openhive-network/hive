e7951189d2f2ac6fe1c50a6d5a960bc6d10792ba
Martin Lees <mlees@oxio.io>
2022-01-16 00:06:40 -0500

    Update test_tools and functional tests to trigger hardfork 26

--- tests/functional/python_tests/direct_rc_delegations_tests/conftest.py
+++ tests/functional/python_tests/direct_rc_delegations_tests/conftest.py
@@ -6,7 +6,8 @@ from test_tools import Wallet
 @pytest.fixture
 def node(world):
     init_node = world.create_init_node()
-    init_node.run()
+    # The actual HF time does not matter as long as it's in the past
+    init_node.run(env={"HIVE_HF26_TIME": "1598558400"})
     return init_node
 
 
