b5ead9a4756fb3c825a3dfcdf1c482282f4fe4d7
Radek Masłowski <rmaslowski@syncad.com>
2022-01-20 12:13:08 +0100

    Modify on threads test

--- tests/functional/python_tests/cli_wallet/test_virtual_operations_bug_on_threads.py
+++ tests/functional/python_tests/cli_wallet/test_virtual_operations_bug_on_threads.py
@@ -16,13 +16,16 @@ def test_direct_rc_delegations(world):
     wallet.api.post_comment('bob', 'hello-world', '', 'xyz', 'something about world', 'just nothing', '{}')
 
     client_accounts = []
-    number_of_accounts_in_one_transaction = 400
+    number_of_accounts_in_one_transaction = 110
     number_of_transactions = 10
     number_of_accounts = number_of_accounts_in_one_transaction * number_of_transactions
     account_number_absolute = 0
+    wallets = []
 
     accounts = Account.create_multiple(number_of_accounts, 'receiver')
     for number_of_transaction in range(number_of_transactions):
+        wallet = Wallet(attach_to=node)
+        wallets.append(wallet)
         with wallet.in_single_transaction():
             for account_number in range(number_of_accounts_in_one_transaction):
                 account_name = accounts[account_number_absolute].name
@@ -36,12 +39,10 @@ def test_direct_rc_delegations(world):
     accounts_to_delegate = split_list(client_accounts, int(number_of_accounts / 100))
     logger.info('End of splitting')
 
-    number_of_threads = 10
-    wallets = []
-    delegators = []
+    # number_of_threads = 10
+    # delegators = []
     # for thread_number in range(number_of_threads):
-    #     wallet = Wallet(attach_to=node)
-    #     wallets.append(wallet)
+    # 
     #     wallet.api.create_account('initminer', f'delegator{thread_number}', '{}')
     #     delegators.append(f'delegator{thread_number}')
     #     wallet.api.transfer_to_vesting('initminer', f'delegator{thread_number}', '100.010 TESTS')
@@ -50,15 +51,61 @@ def test_direct_rc_delegations(world):
     # for thread_number in range(number_of_threads + 1):
     #     accounts_to_delegate_packs.append(int(thread_number / number_of_threads * len(accounts_to_delegate)))
 
+
+    # tasks_list = []
+    # executor = ThreadPoolExecutor(max_workers=number_of_threads)
+    # for thread_number in range(number_of_threads-1):
+    #     tasks_list.append(executor.submit(delegation_rc, wallets[thread_number],
+    #                                       accounts_to_delegate[thread_number], thread_number))
+
+    # for thread_number in tasks_list:
+    #     thread_number.result()
+
+    node.wait_number_of_blocks(20)
     tasks_list = []
-    executor = ThreadPoolExecutor(max_workers=number_of_threads)
-    for thread_number in range(number_of_threads-1):
-        tasks_list.append(executor.submit(delegation_rc, wallets[thread_number],
-                                          accounts_to_delegate[thread_number], thread_number))
+    executor = ThreadPoolExecutor(max_workers=10)
+    task0 = executor.submit(delegation_rc, 'InitNode.Wallet0', 'initminer', 0)
+    tasks_list.append(task0)
 
-    for thread_number in tasks_list:
-        thread_number.result()
+    task1 = executor.submit(delegation_rc, 'InitNode.Wallet1',  'initminer', 1)
+    tasks_list.append(task1)
 
+    task2 = executor.submit(delegation_rc, 'InitNode.Wallet2', 'initminer', 2)
+    tasks_list.append(task2)
+
+    task3 = executor.submit(delegation_rc, 'InitNode.Wallet3', accounts_to_delegate[3], 3)
+    tasks_list.append(task3)
+
+    task4 = executor.submit(delegation_rc, 'InitNode.Wallet4',  accounts_to_delegate[4], 4)
+    tasks_list.append(task4)
+
+    task5 = executor.submit(delegation_rc, 'InitNode.Wallet5', accounts_to_delegate[5], 5)
+    tasks_list.append(task5)
+
+    task6 = executor.submit(delegation_rc, 'InitNode.Wallet6', accounts_to_delegate[6], 6)
+    tasks_list.append(task6)
+
+    task7 = executor.submit(delegation_rc, 'InitNode.Wallet7',  accounts_to_delegate[7], 7)
+    tasks_list.append(task7)
+
+    task8 = executor.submit(delegation_rc, 'InitNode.Wallet8', accounts_to_delegate[8], 8)
+    tasks_list.append(task8)
+
+    task9 = executor.submit(delegation_rc, 'InitNode.Wallet9', accounts_to_delegate[9], 9)
+    tasks_list.append(task9)
+
+
+    node.wait_number_of_blocks(50)
+    api1 = node.api.account_history.enum_virtual_ops(
+        block_range_begin=20,
+        block_range_end=50,
+        include_reversible=False,
+        group_by_block=False,
+        operation_begin=0
+    )
+
+    logger.info(api1)
+    time.sleep(10000)
 
 def delegation_rc(wallet, accounts_to_delegate, thread_number):
     logger.info(f'Thread {thread_number} work START')
