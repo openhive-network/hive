d381085cb8b2edf5ce8866032b2409b61714c74e
Radek Masłowski <rmaslowski@syncad.com>
2022-01-25 14:21:47 +0100

    Stan po odejściu Krzyska(od komputera)

--- tests/functional/python_tests/direct_rc_delegations/account_creation_on_threads.py
+++ tests/functional/python_tests/direct_rc_delegations/account_creation_on_threads.py
@@ -1,7 +1,23 @@
 import time
 from concurrent.futures import ThreadPoolExecutor
 from test_tools import Account, logger, Wallet
-import threading
+
+def create_account_creation_operation( acc : Account ):
+    pattern = ['account_create', {'fee': '0.000 TESTS', 'creator': 'initminer', 'new_account_name': 'receiver2-6',
+                                  'owner': {'weight_threshold': 1, 'account_auths': [], 'key_auths': [
+                                      ['TST5kUFuULqeDLGERSNEnETLZDAFTNAjocHxZQkuLkERUz1WXUdpz', 1]]},
+                                  'active': {'weight_threshold': 1, 'account_auths': [], 'key_auths': [
+                                      ['TST5kUFuULqeDLGERSNEnETLZDAFTNAjocHxZQkuLkERUz1WXUdpz', 1]]},
+                                  'posting': {'weight_threshold': 1, 'account_auths': [], 'key_auths': [
+                                      ['TST5kUFuULqeDLGERSNEnETLZDAFTNAjocHxZQkuLkERUz1WXUdpz', 1]]},
+                                  'memo_key': 'TST5kUFuULqeDLGERSNEnETLZDAFTNAjocHxZQkuLkERUz1WXUdpz',
+                                  'json_metadata': '{}'}]
+    pattern[1]['new_account_name'] = acc.name
+    for key_type in ['owner', 'active', 'posting']:
+        pattern[1][key_type]['key_auths'][0][0] = acc.public_key
+    pattern[1]['memo_key'] = acc.public_key
+    return pattern
+
 
 def test_direct_rc_delegations(wallet, node):
     # logger.info('Start account creations')
@@ -19,14 +35,20 @@ def test_direct_rc_delegations(wallet, node):
     accounts_thread_2 = []
     accounts_thread_3 = []
 
+    # wallet.api.create_account('initminer', 'bob', "{}")
+    # wallet.api.create_account('initminer', 'alice', "{}")
+    # wallet.api.transfer_to_vesting('initminer', 'bob', '100.010 TESTS')
+    # wallet.api.transfer_to_vesting('initminer', 'alice', '100.010 TESTS')
+
     tasks_list = []
     executor = ThreadPoolExecutor(max_workers=3)
     task1 = executor.submit(delegation_rc, wallet1, 1, accounts_thread_1)
     tasks_list.append(task1)
 
+    # delegation_rc(wallet1, 1, accounts_thread_1)
     task2 = executor.submit(delegation_rc, wallet2, 2, accounts_thread_2)
     tasks_list.append(task2)
-
+    #
     task3 = executor.submit(delegation_rc, wallet3, 3, accounts_thread_3)
     tasks_list.append(task3)
 
@@ -69,19 +91,49 @@ def test_direct_rc_delegations(wallet, node):
 def delegation_rc(wallet, thread_number, client_accounts):
     logger.info(f'Thread {thread_number} work START')
     logger.info(f'Start account creations---THREAD{thread_number}')
-    number_of_accounts_in_one_transaction = 50
-    number_of_transactions = 20
+    number_of_accounts_in_one_transaction = 1750
+    number_of_transactions = 5
     number_of_accounts = number_of_accounts_in_one_transaction * number_of_transactions
     account_number_absolute = 0
+    start = time.perf_counter()
     accounts = Account.create_multiple(number_of_accounts, f'receiver{thread_number}')
-    for number_of_transaction in range(number_of_transactions):
-        with wallet.in_single_transaction():
-            for account_number in range(number_of_accounts_in_one_transaction):
-                account_name = accounts[account_number_absolute].name
-                account_key = accounts[account_number_absolute].public_key
-                wallet.api.create_account_with_keys('initminer', account_name, '{}', account_key, account_key,
-                                                    account_key, account_key)
-                client_accounts.append(account_name)
-                account_number_absolute = account_number_absolute + 1
+    private_keys = [acc.private_key for acc in accounts]
+
+    for i in range(number_of_transactions):
+        logger.info('import_keys - start')
+        wallet.api.import_keys(list(private_keys[
+                i * number_of_accounts_in_one_transaction: i * number_of_accounts_in_one_transaction + number_of_accounts_in_one_transaction]))
+        logger.info('import_keys - end')
+
+    print(f'czas na przygotowanie acc: {time.perf_counter() - start :.2f}s')
+
+
+
+    trx_pat = {'ref_block_num': 11, 'ref_block_prefix': 3457124627, 'expiration': '1970-01-01T00:00:00', 'extensions': [],'signatures': [],'transaction_id': '0000000000000000000000000000000000000000', 'block_num': 0, 'transaction_num': 0, 'operations': []}
+
+    start = time.perf_counter()
+
+    for i in range(number_of_transactions):
+        for acc in list(accounts[i * number_of_accounts_in_one_transaction : i * number_of_accounts_in_one_transaction + number_of_accounts_in_one_transaction]):
+            trx_pat['operations'].append(create_account_creation_operation(acc))
+        wallet.api.sign_transaction(trx_pat)
+        trx_pat['operations'].clear()
+
+    print(f'czas na dodanie accountów: {time.perf_counter() - start :.2f}s')
+    print()
+    if False:
+        for number_of_transaction in range(number_of_transactions):
+            with wallet.in_single_transaction():
+                start = time.perf_counter()
+                # wallet.api.sign_transaction()
+
+                for account_number in range(number_of_accounts_in_one_transaction):
+                    account_name = accounts[account_number_absolute].name
+                    account_key = accounts[account_number_absolute].public_key
+                    wallet.api.create_account_with_keys('initminer', account_name, '{}', account_key, account_key,account_key, account_key)
+                    # wallet.api.sign_transaction(create_account_creation_operation())
+                    client_accounts.append(account_name)
+                    account_number_absolute = account_number_absolute + 1
+                logger.info(f'Thread {thread_number}: {time.perf_counter() - start:.2f}s')
     logger.info(f'End account creations----THREAD{thread_number}')
     logger.info(f'Thread {thread_number} work END')
