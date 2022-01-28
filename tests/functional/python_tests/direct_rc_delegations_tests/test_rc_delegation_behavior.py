c58ca8b00a164971de4e769a35831da28a915c4a
Radek Masłowski <rmaslowski@syncad.com>
2022-01-28 12:29:29 +0100

    fixup! Add test_multidelegation for 100000 accounts

--- tests/functional/python_tests/direct_rc_delegations_tests/test_rc_delegation_behavior.py
+++ tests/functional/python_tests/direct_rc_delegations_tests/test_rc_delegation_behavior.py
@@ -149,29 +149,25 @@ def test_power_up_delegator(wallet: Wallet):
     assert rc0 == rc1
 
 
+@pytest.mark.node_shared_file_size('16G')
 def test_multidelegation(wallet: Wallet):
-    client_accounts = []
-    number_of_accounts_in_one_transaction = 400
-    number_of_transactions = 2
-    number_of_accounts = number_of_accounts_in_one_transaction * number_of_transactions
+
     number_of_accounts = 100000
-
-
-
     accounts = wallet.create_accounts(number_of_accounts, 'receiver')
     accounts_to_delegate = split_list(accounts, int(number_of_accounts / 100))
     logger.info('End of splitting')
 
     number_of_threads = 10
-    wallets = []
     delegators = []
 
-    for thread_number in range(number_of_threads):
-        wallet = Wallet(attach_to=node)
-        wallets.append(wallet)
-        wallet.api.create_account('initminer', f'delegator{thread_number}', '{}')
-        delegators.append(f'delegator{thread_number}')
-        wallet.api.transfer_to_vesting('initminer', f'delegator{thread_number}', '100.010 TESTS')
+    with wallet.in_single_transaction():
+        for thread_number in range(number_of_threads):
+            wallet.api.create_account('initminer', f'delegator{thread_number}', '{}')
+            delegators.append(f'delegator{thread_number}')
+
+    with wallet.in_single_transaction():
+        for thread_number in range(number_of_threads):
+            wallet.api.transfer_to_vesting('initminer', f'delegator{thread_number}', '100.010 TESTS')
 
     accounts_to_delegate_packs = []
     for thread_number in range(number_of_threads + 1):
@@ -180,7 +176,7 @@ def test_multidelegation(wallet: Wallet):
     tasks_list = []
     executor = ThreadPoolExecutor(max_workers=number_of_threads)
     for thread_number in range(number_of_threads):
-        tasks_list.append(executor.submit(delegation_rc, delegators[thread_number], wallets[thread_number],
+        tasks_list.append(executor.submit(delegation_rc, delegators[thread_number], wallet,
                                           accounts_to_delegate_packs[thread_number],
                                           accounts_to_delegate_packs[thread_number + 1],
                                           accounts_to_delegate, thread_number))
