0ebd9b8b630de0fcb3e3b57bb87e2b0c4fa71af0
Piotr Batko <pbatko@syncad.com>
2022-01-31 12:07:31 +0100

    Rename parameter (refactor)

--- tests/functional/python_tests/direct_rc_delegations_tests/conftest.py
+++ tests/functional/python_tests/direct_rc_delegations_tests/conftest.py
@@ -7,7 +7,7 @@ from test_tools import Wallet
 def node(world):
     init_node = world.create_init_node()
     # The actual HF time does not matter as long as it's in the past
-    init_node.run(env={"HIVE_HF26_TIME": "1598558400"})
+    init_node.run(environment_variables={"HIVE_HF26_TIME": "1598558400"})
     return init_node
 
 
