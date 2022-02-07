2a387b55fe068cbea02170dd2d89e04c50400cfe
Bartek Wrona <wrona@syncad.com>
2022-02-07 15:05:03 +0100

    HF26 date moved to: - 1. February 2022 for TESTNET build (to activate it automatically) - 30. June 2022 for MAINNET build (probably this is still a subject to change).

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -56,7 +56,13 @@
 long next_hf_time()
 {
   // current "next hardfork" is HF26
-  long hfTime = 1646136000; //   Tuesday, 1 March 2022 12:00:00
+  long hfTime =
+
+#ifdef IS_TEST_NET
+    1643716800; // Tuesday, 1 February 2022 12:00:00
+#else
+    1656590400; // Thursday, 30 June 2022 12:00:00
+#endif /// IS_TEST_NET
 
   const char* value = getenv("HIVE_HF26_TIME");
   if(value != nullptr)
