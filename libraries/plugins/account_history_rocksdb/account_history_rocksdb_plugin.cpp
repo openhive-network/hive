694099d3df7ed30699086e11a29e83eaa3f48351
Krzysztof Mochocki <kmochocki@syncad.com>
2022-02-11 12:31:03 +0100

    rebase fixes

--- libraries/plugins/account_history_rocksdb/account_history_rocksdb_plugin.cpp
+++ libraries/plugins/account_history_rocksdb/account_history_rocksdb_plugin.cpp
@@ -655,11 +655,10 @@ private:
     auto s = _writeBuffer.Put(_columnHandles[Columns::OPERATION_BY_ID], idSlice, Slice(serializedObj.data(), serializedObj.size()));
     checkStatus(s);
 
-    uint64_t encoded_id = (uint64_t) obj.id;
-    if( obj.is_virtual )
-    {
-      encoded_id |= VIRTUAL_OP_FLAG;
-    }
+    // uint64_t location = ( (uint64_t) obj.trx_in_block << 32 ) | ( (uint64_t) obj.op_in_trx << 16 ) | ( obj.virtual_op );
+
+    //if obj is a virtual operation, encode this fact into top bit of id to speed up queries that need to distinguish ops vs virtual ops
+    uint64_t encoded_id = obj.is_virtual ? VIRTUAL_OP_FLAG | obj.id : obj.id;
 
     op_by_block_num_slice_t blockLocSlice(block_op_id_pair(obj.block, encoded_id));
 
@@ -1322,7 +1321,7 @@ std::pair< uint32_t, uint64_t > account_history_rocksdb_plugin::impl::enumVirtua
         }
 
         /// Accept only virtual operations
-        if(op.is_virtual)
+        if (op.is_virtual)
           if(processor(op, op.id, false))
             ++cntLimit;
 
@@ -1405,7 +1404,7 @@ std::pair< uint32_t, uint64_t > account_history_rocksdb_plugin::impl::enumVirtua
         }
 
         /// Accept only virtual operations
-        if(op.is_virtual)
+        if (op.is_virtual)
           if(processor(op, op.id, false))
             ++cntLimit;
 
