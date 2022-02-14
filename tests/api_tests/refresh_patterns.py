84639ee489fb00b7ea376868407edc234ccbca4c
Krzysztof Mochocki <kmochocki@syncad.com>
2022-02-14 12:39:40 +0100

    updated submodule and script for refreshing patterns

--- tests/api_tests/refresh_patterns.py
+++ tests/api_tests/refresh_patterns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import re
 
 from os.path import join
 from yaml import safe_load
@@ -9,13 +10,17 @@ from requests import post
 from sys import argv
 from argparse import ArgumentParser
 
+DEFAULT_PATTERN = '.*/account_history_api.*'
+
 engine = ArgumentParser()
 engine.add_argument('URL', type=str, help='reference node, which will provide pattern data (Ex. http://localhost:8091)')
 engine.add_argument('-s', dest='SUFIX', type=str, default=None, help='this suffix will be attached to file extension, which is handy for fast removal of patterns (Ex. "xxx", which allows to execute command: `find . | grep \'json.xxx\' | xargs rm` )')
+engine.add_argument('-p', '--pattern', dest='pattern', type=str, default='.*/account_history_api/.*', help=f'this will be used as regex for path matching (default: `{DEFAULT_PATTERN}`)')
 args = engine.parse_args(argv[1:])
 
 URL = args.URL
 SUFIX = f'.{args.SUFIX}' if args.SUFIX else ''
+PATTERN = re.compile(args.pattern)
 
 def load_yaml(filename : str) -> dict:
 	with open(filename, 'rt') as file:
@@ -25,6 +30,7 @@ def create_pattern(url : str, tav_file : str, directory : str):
 	PATTERN_FILE = tav_file.split('.')[0] + '.pat.json' + SUFIX
 	TAVERN_FILE = join(directory, tav_file)
 	OUTPUT_PATTERN_FILE = join(directory, f'{PATTERN_FILE}')
+	print(f'processing: {TAVERN_FILE}')
 
 	test_options = load_yaml(TAVERN_FILE)
 	request = test_options['stages'][0]['request']
@@ -47,7 +53,7 @@ from concurrent.futures import ProcessPoolExecutor
 futures = []
 with ProcessPoolExecutor(max_workers=6) as exec:
 	for parent_path, _, filenames in os.walk('.'):
-		if 'tavern' in parent_path and 'account_history_api' in parent_path:
+		if 'tavern' in parent_path and PATTERN.match(parent_path) is not None:
 			for tavernfile in filter(lambda x: x.endswith('tavern.yaml'), filenames):
 				futures.append( exec.submit(create_pattern, URL, tavernfile, parent_path) )
 
