dfb403457ade0a12c4d59ca331609d7c662f0150
Radek Masłowski <rmaslowski@syncad.com>
2022-02-17 10:04:54 +0100

    Update conftest

--- tests/functional/python_tests/direct_rc_delegations_tests/conftest.py
+++ tests/functional/python_tests/direct_rc_delegations_tests/conftest.py
@@ -2,11 +2,18 @@ import pytest
 
 from test_tools import Wallet
 
+def pytest_configure(config):
+    config.addinivalue_line('markers', 'node_shared_file_size: Shared file size of node from `node` fixture')
 
 @pytest.fixture
-def node(world):
+def node(world, request):
     init_node = world.create_init_node()
     # The actual HF time does not matter as long as it's in the past
+
+    shared_file_size = request.node.get_closest_marker('node_shared_file_size')
+    if shared_file_size:
+        init_node.config.shared_file_size = shared_file_size.args[0]
+
     init_node.run(environment_variables={"HIVE_HF26_TIME": "1598558400"})
     return init_node
 
