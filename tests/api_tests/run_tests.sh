76a5d25fc82e4b69038809dc6554253397623670
Krzysztof Mochocki <kmochocki@syncad.com>
2022-02-17 11:17:39 +0100

    increased max workers

--- tests/api_tests/run_tests.sh
+++ tests/api_tests/run_tests.sh
@@ -8,4 +8,4 @@ else
     export HIVEMIND_PORT=$1
 fi
 export TAVERN_DIR="$2/tests/api_tests"
-tox -e tavern -- --tb=long -W ignore::pytest.PytestDeprecationWarning --workers 1 --tests-per-worker auto -k 'not get_transaction_hex and (get_transaction or get_account_history or enum_virtual_ops or get_ops_in_block)' --junitxml=results.xml
+tox -e tavern -- --tb=long -W ignore::pytest.PytestDeprecationWarning --workers 5 --tests-per-worker auto -k 'not get_transaction_hex and (get_transaction or get_account_history or enum_virtual_ops or get_ops_in_block)' --junitxml=results.xml
