1dcee5a00575fe7d8b09dafa42151891094a43b3
ABW <andrzejl@syncad.com>
2022-02-23 16:55:26 +0100

    [Fix] unused version of get_resource_user overwritten by actually used version

--- libraries/plugins/rc/rc_plugin.cpp
+++ libraries/plugins/rc/rc_plugin.cpp
@@ -10,6 +10,9 @@
 #include <hive/plugins/rc/rc_objects.hpp>
 #include <hive/plugins/rc/rc_operations.hpp>
 
+#include <hive/plugins/rc/resource_count.hpp>
+#include <hive/plugins/rc/resource_user.hpp>
+
 #include <hive/chain/account_object.hpp>
 #include <hive/chain/database.hpp>
 #include <hive/chain/database_exceptions.hpp>
@@ -27,6 +30,9 @@
 namespace hive { namespace plugins { namespace rc {
 
 using hive::plugins::block_data_export::block_data_export_plugin;
+
+void count_resources( const optional_automated_action& action, count_resources_result& result );
+account_name_type get_resource_user( const optional_automated_action& action );
 
 namespace detail {
 
@@ -175,59 +181,6 @@ std::vector< std::pair< int64_t, account_name_type > > dump_all_rc_accounts( con
   }
 
   return result;
-}
-
-struct get_resource_user_visitor
-{
-  typedef account_name_type result_type;
-
-  get_resource_user_visitor() {}
-
-  account_name_type operator()( const witness_set_properties_operation& op )const
-  {
-    return op.owner;
-  }
-
-  account_name_type operator()( const recover_account_operation& op )const
-  {
-    return op.account_to_recover;
-  }
-
-  template< typename Op >
-  account_name_type operator()( const Op& op )const
-  {
-    flat_set< account_name_type > req;
-    op.get_required_active_authorities( req );
-    for( const account_name_type& account : req )
-      return account;
-    op.get_required_owner_authorities( req );
-    for( const account_name_type& account : req )
-      return account;
-    op.get_required_posting_authorities( req );
-    for( const account_name_type& account : req )
-      return account;
-    return account_name_type();
-  }
-};
-
-account_name_type get_resource_user( const signed_transaction& tx )
-{
-  get_resource_user_visitor vtor;
-
-  for( const operation& op : tx.operations )
-  {
-    account_name_type resource_user = op.visit( vtor );
-    if( resource_user != account_name_type() )
-      return resource_user;
-  }
-  return account_name_type();
-}
-
-account_name_type get_resource_user( const optional_automated_action& action )
-{
-  get_resource_user_visitor vtor;
-
-  return action.visit( vtor );
 }
 
 void use_account_rcs(
