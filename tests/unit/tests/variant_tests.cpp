c93fc4dcadf68e1c018f8311064998cac2b26d8e
mtyszczak <mateusz.tyszczak@gmail.com>
2022-02-24 19:35:06 +0100

    Add strict type checks to fc variant tests

--- tests/unit/tests/variant_tests.cpp
+++ tests/unit/tests/variant_tests.cpp
@@ -24,22 +24,25 @@ BOOST_AUTO_TEST_SUITE( variant_tests )
 
 HIVE_AUTO_TEST_CASE( as_int64,
   int64_t initial = -1;
-  
+
   fc::variant v = initial;
+  BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::int64_type );
 
   BOOST_REQUIRE_EQUAL( v.as_int64(), initial );
 )
 
 HIVE_AUTO_TEST_CASE( as_uint64,
   uint64_t initial = -1;
-  
+
   fc::variant v = initial;
+  BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::uint64_type );
 
   BOOST_REQUIRE_EQUAL( v.as_uint64(), initial );
 )
 
 HIVE_AUTO_TEST_CASE( as_bool,
   fc::variant v = true;
+  BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::bool_type );
   BOOST_REQUIRE_EQUAL( v.as_bool(), true );
 
   // Make sure that true is not a false-positive result
@@ -49,8 +52,9 @@ HIVE_AUTO_TEST_CASE( as_bool,
 
 HIVE_AUTO_TEST_CASE( as_double,
   double initial = 3.141592;
-  
+
   fc::variant v = initial;
+  BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::double_type );
 
   BOOST_REQUIRE_EQUAL( v.as_double(), initial );
 )
@@ -58,9 +62,10 @@ HIVE_AUTO_TEST_CASE( as_double,
 HIVE_AUTO_TEST_CASE( as_blob,
   fc::blob initial;
   initial.data = { 'a', 'l', 'i', 'c', 'e' };
-  
+
   fc::variant v = initial;
   fc::blob out = v.as_blob();
+  BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::blob_type );
 
   BOOST_REQUIRE_EQUAL( out.data.size(), initial.data.size() );
 
@@ -70,13 +75,21 @@ HIVE_AUTO_TEST_CASE( as_blob,
 
 HIVE_AUTO_TEST_CASE( as_string,
   std::string initial = "alice";
-  
+
   fc::variant v = initial;
+  BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::string_type );
 
   BOOST_REQUIRE_EQUAL( v.as_string(), initial );
 )
 
-// TODO: as_array, as_object and extended nested object tests along with the variant_object and mutable_variant_object tests
+HIVE_AUTO_TEST_CASE( null_type,
+  fc::variant v;
+  BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::null_type );
+
+  v = 3.141592;
+  v.clear();
+  BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::null_type );
+)
 
 BOOST_AUTO_TEST_SUITE_END()
 #endif
