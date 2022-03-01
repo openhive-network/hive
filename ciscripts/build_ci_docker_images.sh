be34fd24cadaed9f504fc70a3ecd2a1f8f5672c3
Bartek Wrona <wrona@syncad.com>
2022-03-01 17:31:12 +0100

    CI test image contains local copy of 5M blocklog to simplify testing.

--- ciscripts/build_ci_docker_images.sh
+++ ciscripts/build_ci_docker_images.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail 
 
-REGISTRY=$1
+REGISTRY=${1:-registry.gitlab.syncad.com/hive/hive}
 
-docker build --target=runtime --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/runtime:ubuntu-20.04-1 -f Dockerfile.ci .
-docker build --target=builder --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/builder:ubuntu-20.04-1 -f Dockerfile.ci .
-docker build --target=test --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/test:ubuntu-20.04-1 -f Dockerfile.ci .
+docker build --target=runtime --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/runtime:ubuntu-20.04-2 -f Dockerfile.ci .
+docker build --target=builder --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/builder:ubuntu-20.04-2 -f Dockerfile.ci .
+docker build --target=test --build-arg CI_REGISTRY_IMAGE=$REGISTRY -t $REGISTRY/test:ubuntu-20.04-2 -f Dockerfile.ci .
