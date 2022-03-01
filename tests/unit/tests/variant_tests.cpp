14c27185c644b1ecfcdae4de9dcaea0f67221574
mtyszczak <mateusz.tyszczak@gmail.com>
2022-03-01 09:47:15 +0100

    Add basic numeric type conversion variant tests

--- tests/unit/tests/variant_tests.cpp
+++ tests/unit/tests/variant_tests.cpp
@@ -5,6 +5,8 @@
 #include <fc/exception/exception.hpp>
 
 #include <string>
+#include <functional>
+#include <limits>
 
 BOOST_AUTO_TEST_SUITE( variant_tests )
 
@@ -90,6 +92,71 @@ HIVE_AUTO_TEST_CASE( null_type,
   v.clear();
   BOOST_REQUIRE_EQUAL( v.get_type(), fc::variant::null_type );
 )
+
+// Type conversions
+
+namespace
+{
+
+template< typename TypeTested >
+using variant_retrieve_member_fn_t = TypeTested (fc::variant::*)() const;
+
+#define HIVE_TEST_VARIANT_FUNCTION_INVOKE(object, member_ptr) ((object).*(member_ptr))()
+
+template< typename TypeTested >
+void test_type_conversion( variant_retrieve_member_fn_t< TypeTested > variant_retrieve_fn )
+{
+  fc::variant v;
+  TypeTested expected = std::numeric_limits< unsigned >::max();
+
+  const auto type_conversion_helper = [&]( auto&& input ) {
+    v = input;
+    BOOST_REQUIRE_EQUAL( (HIVE_TEST_VARIANT_FUNCTION_INVOKE( v, variant_retrieve_fn )), expected );
+  };
+
+  type_conversion_helper( std::to_string( expected ) );
+  type_conversion_helper( double( expected ) );
+  type_conversion_helper( uint64_t( expected ) );
+  type_conversion_helper( int64_t( expected ) );
+
+  expected = true;
+  type_conversion_helper( bool( expected ) );
+  expected = false;
+  type_conversion_helper( bool( expected ) );
+
+  v.clear();
+  BOOST_REQUIRE_EQUAL( (HIVE_TEST_VARIANT_FUNCTION_INVOKE( v, variant_retrieve_fn )), 0 );
+
+  // Objects:
+  v = fc::variant_object{};
+  BOOST_REQUIRE_THROW( (HIVE_TEST_VARIANT_FUNCTION_INVOKE( v, variant_retrieve_fn )), fc::bad_cast_exception );
+  v = fc::mutable_variant_object{};
+  BOOST_REQUIRE_THROW( (HIVE_TEST_VARIANT_FUNCTION_INVOKE( v, variant_retrieve_fn )), fc::bad_cast_exception );
+
+  // Arrays:
+  v = fc::variants{};
+  BOOST_REQUIRE_THROW( (HIVE_TEST_VARIANT_FUNCTION_INVOKE( v, variant_retrieve_fn )), fc::bad_cast_exception );
+
+  // Blob:
+  v = fc::blob{};
+  BOOST_REQUIRE_THROW( (HIVE_TEST_VARIANT_FUNCTION_INVOKE( v, variant_retrieve_fn )), fc::bad_cast_exception );
+}
+
+}
+
+HIVE_AUTO_TEST_CASE( as_int64_conversions,
+  test_type_conversion< int64_t >( &fc::variant::as_int64 );
+)
+
+HIVE_AUTO_TEST_CASE( as_uint64_conversions,
+  test_type_conversion< uint64_t >( &fc::variant::as_uint64 );
+)
+
+HIVE_AUTO_TEST_CASE( as_double_conversions,
+  test_type_conversion< double >( &fc::variant::as_double );
+)
+
+// TODO: conversion, as_array, as_object and extended nested object tests along with the variant_object and mutable_variant_object tests
 
 BOOST_AUTO_TEST_SUITE_END()
 #endif
