a37bcf6d3e652b934ad8b57ac7e1cdf4f312ad4a
Michał Kudela <kudmich@syncad.com>
2022-03-09 14:02:03 +0100

    Added test_zero_false (0==False)

--- tests/functional/python_tests/cli_wallet_extended_tests/tests_all_account_transaction.py
+++ tests/functional/python_tests/cli_wallet_extended_tests/tests_all_account_transaction.py
@@ -2,7 +2,8 @@ import pytest
 
 from all_account_transaction import compare_transactions, assign_transactions_keys
 
-def test_diffrent_transactions():
+
+def test_different_transactions():
     dataA = [
         0,
         {
@@ -62,10 +63,10 @@ def test_diffrent_transactions():
             "operation_id": 0
         }
     ]
-    assert compare_transactions('develop', 'test', [dataA], assign_transactions_keys([dataB])) != []
+    assert compare_transactions('develop', 'test', [dataA], [dataB]) != [dataB]
 
 
-def test_identical_transactions():
+def test_same_transactions():
     data = [
         0,
         {
@@ -95,4 +96,130 @@ def test_identical_transactions():
             "operation_id": 0
         }
     ]
-    assert compare_transactions('develop', 'test', [data], assign_transactions_keys([data])) == []
+    assert compare_transactions('develop', 'test', [data], [data]) == []
+
+
+def test_different_key():
+    dataA = [
+        0,
+        {
+            "trx_id": "data",
+            "block": 111111,
+            "trx_in_block": 0,
+            "op_in_trx": 0,
+            "virtual_op": 1,
+            "timestamp": "2016-06-30T17:22:18",
+            "op": {
+                "type": "account_created_operation",
+                "value": {
+                    "new_account_name": "gtg",
+                    "creator": "",
+                    "initial_vesting_shares": {
+                        "amount": "0",
+                        "precision": 6,
+                        "nai": "@@000000037"
+                    },
+                    "initial_delegation": {
+                        "amount": "0",
+                        "precision": 6,
+                        "nai": "@@000000037"
+                    }
+                }
+            },
+            "operation_id": 0
+        }
+    ]
+
+    dataB = [
+        0,
+        {
+            "trx_id": "data",
+            "block": 222222,
+            "trx_in_block": 0,
+            "op_in_trx": 0,
+            "virtual_op": 1,
+            "timestamp": "2016-06-30T17:22:18",
+            "op": {
+                "type": "account_created_operation",
+                "value": {
+                    "new_account_name": "gtg",
+                    "creator": "",
+                    "initial_vesting_shares": {
+                        "amount": "0",
+                        "precision": 6,
+                        "nai": "@@000000037"
+                    },
+                    "initial_delegation": {
+                        "amount": "0",
+                        "precision": 6,
+                        "nai": "@@000000037"
+                    }
+                }
+            },
+            "operation_id": 0
+        }
+    ]
+    assert compare_transactions('develop', 'test', [dataA], [dataB]) != [dataB]
+
+
+def test_zero_false_problem():
+    transacrion_number_false = [
+        0,
+        {
+            "trx_id": "data",
+            "block": 111111,
+            "trx_in_block": 0,
+            "op_in_trx": 0,
+            "virtual_op": 1,
+            "timestamp": "2016-06-30T17:22:18",
+            "op": {
+                "type": "account_created_operation",
+                "value": {
+                    "new_account_name": "gtg",
+                    "creator": "",
+                    "initial_vesting_shares": {
+                        "amount": "0",
+                        "precision": 6,
+                        "nai": "@@000000037"
+                    },
+                    "initial_delegation": {
+                        "amount": "0",
+                        "precision": 6,
+                        "nai": "@@000000037"
+                    }
+                }
+            },
+            "operation_id": 0
+        }
+    ]
+
+    transaction_number_zero = [
+        0,
+        {
+            "trx_id": "data",
+            "block": 111111,
+            "trx_in_block": False,
+            "op_in_trx": 0,
+            "virtual_op": 1,
+            "timestamp": "2016-06-30T17:22:18",
+            "op": {
+                "type": "account_created_operation",
+                "value": {
+                    "new_account_name": "gtg",
+                    "creator": "",
+                    "initial_vesting_shares": {
+                        "amount": "0",
+                        "precision": 6,
+                        "nai": "@@000000037"
+                    },
+                    "initial_delegation": {
+                        "amount": "0",
+                        "precision": 6,
+                        "nai": "@@000000037"
+                    }
+                }
+            },
+            "operation_id": 0
+        }
+    ]
+    assert compare_transactions('develop', 'test', [transacrion_number_false], [transaction_number_zero]) == [transaction_number_zero]
