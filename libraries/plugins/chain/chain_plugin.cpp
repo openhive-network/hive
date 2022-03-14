b009b1d20ced62e2769f0f132c680c7a3b16228d
Eric Frias <efrias@syncad.com>
2022-03-14 15:01:43 +0000

    Remove 1ms sleep in block processing thread

--- libraries/plugins/chain/chain_plugin.cpp
+++ libraries/plugins/chain/chain_plugin.cpp
@@ -404,9 +404,9 @@ void chain_plugin_impl::start_write_processing()
       } //if queue not empty
       if( !is_syncing )
         std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
-      else //DLN remove this temp code to stop busy looping during sync
-        std::this_thread::sleep_for( std::chrono::milliseconds(1) );
-        //std::this_thread::sleep_for( std::chrono::milliseconds( 20000 ) );
+      // else //DLN remove this temp code to stop busy looping during sync
+      //   std::this_thread::sleep_for( std::chrono::milliseconds(1) );
+      //   //std::this_thread::sleep_for( std::chrono::milliseconds( 20000 ) );
 
       auto now = fc::time_point::now();
       if((now - last_popped_block_time) > block_wait_max_time)
