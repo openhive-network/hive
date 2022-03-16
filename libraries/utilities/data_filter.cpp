b22d12daf88999e0e97000b30267c027fb89883c
mtrela <mtrela@syncad.com>
2022-03-16 11:40:38 +0100

    Introduced `operation_filter` class

--- libraries/utilities/data_filter.cpp
+++ libraries/utilities/data_filter.cpp
@@ -1,12 +1,19 @@
 #include <hive/utilities/data_filter.hpp>
 #include <hive/utilities/plugin_utilities.hpp>
 
+#include <boost/algorithm/string.hpp>
+
 #define DIAGNOSTIC(s)
 //#define DIAGNOSTIC(s) s
 
 namespace hive { namespace plugins {
 
-account_filter::account_filter( const std::string& _filter_name ): filter_name( _filter_name )
+data_filter::data_filter( const std::string& _filter_name ): filter_name( _filter_name )
+{
+
+}
+
+account_filter::account_filter( const std::string& _filter_name ): data_filter( _filter_name )
 {
 }
 
@@ -22,7 +29,7 @@ const account_filter::account_name_range_index& account_filter::get_tracked_acco
 
 bool account_filter::is_tracked_account( const account_name_type& name ) const
 {
-  if( tracked_accounts.empty() )
+  if( empty() )
   {
     DIAGNOSTIC( ilog("[${fn}] Set of tracked accounts is empty.", ("fn", filter_name) ); )
     return true;
@@ -73,6 +80,87 @@ void account_filter::fill( const boost::program_options::variables_map& options,
 {
   typedef std::pair< account_name_type, account_name_type > pairstring;
   HIVE_LOAD_VALUE_SET(options, option_name, tracked_accounts, pairstring);
+}
+
+operation_filter::operation_filter( const std::string& _filter_name ): data_filter( _filter_name )
+{
+}
+
+std::string operation_filter::get_op_name( const operation& op )
+{
+  string _op_name;
+  op.visit( fc::get_static_variant_name( _op_name ) );
+
+  return _op_name;
+}
+
+bool operation_filter::add_operation( const std::string& name, operation& op )
+{
+  static std::map< string, int64_t > _ops = fc::prepare_variant_info<operation>();
+
+  auto itr = _ops.find( name );
+
+  if( itr != _ops.end() )
+  {
+    int64_t which = itr->second;
+    op.set_which( which );
+
+    return true;
+  }
+  else
+    return false;
+}
+
+bool operation_filter::empty() const
+{
+  return tracked_operations.empty();
+}
+
+bool operation_filter::is_tracked_operation( const operation& op ) const
+{
+  if( empty() )
+  {
+    DIAGNOSTIC( ilog("[${fn}] Set of tracked operations is empty.", ("fn", filter_name) ); )
+    return true;
+  }
+
+  bool _result = tracked_operations.find( op ) != tracked_operations.end();
+
+  DIAGNOSTIC
+  (
+    if( _result )
+      ilog("[${fn}] Operation: ${op} matched to defined operations", ("fn", filter_name)("op", get_op_name( op )) );
+    else
+      ilog("[${fn}] Operation: ${op} ignored due to defined tracking filters.", ("fn", filter_name)("op", get_op_name( op )) );
+  )
+
+  return _result;
+}
+
+void operation_filter::fill( const boost::program_options::variables_map& options, const std::string& option_name )
+{
+  if( options.count( option_name ) > 0 )
+  {
+    flat_set<std::string> _ops;
+
+    auto _set_of_ops = options.at( option_name ).as<std::vector<std::string>>();
+    for( auto& item : _set_of_ops )
+    {
+      vector<string> _names;
+      boost::split( _names, item, boost::is_any_of(" \t,") );
+
+      std::copy( _names.begin(), _names.end(), std::inserter( _ops, _ops.begin() ) );
+    }
+
+    for( auto& item : _ops )
+    {
+      operation op;
+      if( add_operation( item, op ) )
+      {
+        tracked_operations.emplace( op );
+      }
+    }
+  }
 }
 
 } } // hive::utilities
--- libraries/utilities/include/hive/utilities/data_filter.hpp
+++ libraries/utilities/include/hive/utilities/data_filter.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <hive/protocol/types.hpp>
+#include <hive/protocol/operations.hpp>
+
 #include <hive/utilities/plugin_utilities.hpp>
 
 #include <boost/container/flat_map.hpp>
@@ -9,8 +11,20 @@
 namespace hive { namespace plugins {
 
 using protocol::account_name_type;
+using protocol::operation;
 
-class account_filter
+class data_filter
+{
+  protected:
+
+    std::string filter_name;
+
+  public:
+  
+    data_filter( const std::string& _filter_name );
+};
+
+class account_filter: public data_filter
 {
   public:
 
@@ -18,7 +32,6 @@ class account_filter
 
   private:
 
-    std::string filter_name;
     account_name_range_index tracked_accounts;
 
   public:
@@ -28,6 +41,29 @@ class account_filter
     bool empty() const;
     const account_name_range_index& get_tracked_accounts() const;
     bool is_tracked_account( const account_name_type& name ) const;
+
+    void fill( const boost::program_options::variables_map& options, const std::string& option_name );
+};
+
+class operation_filter: public data_filter
+{
+  public:
+
+    using operations = flat_set<operation>;
+
+  private:
+
+    operations tracked_operations;
+
+    std::string get_op_name( const operation& op );
+    bool add_operation( const std::string& name, operation& op );
+
+  public:
+
+    operation_filter( const std::string& _filter_name );
+
+    bool empty() const;
+    bool is_tracked_operation( const operation& op ) const;
 
     void fill( const boost::program_options::variables_map& options, const std::string& option_name );
 };
