8d85c6964eca872c9dd2dd93cdfc7ecf17d734f1
Krzysztof Mochocki <kmochocki@syncad.com>
2022-03-16 15:01:27 +0100

    moved pattern tests to subdirectory

--- tests/api_tests/pattern_tests/run_tests.sh
+++ tests/api_tests/pattern_tests/run_tests.sh
@@ -7,5 +7,5 @@ else
     export HIVEMIND_ADDRESS=localhost
     export HIVEMIND_PORT=$1
 fi
-export TAVERN_DIR="$2/tests/api_tests"
+export TAVERN_DIR="$2/tests/api_tests/pattern_tests/"
 tox -e tavern -- --tb=long -W ignore::pytest.PytestDeprecationWarning --workers 5 --tests-per-worker auto -k 'not get_transaction_hex and (get_transaction or get_account_history or enum_virtual_ops or get_ops_in_block)' --junitxml=results.xml
