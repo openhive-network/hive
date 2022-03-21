350666a15e7081fc37d2151e3779900f4d645121
Piotr Batko <pbatko@syncad.com>
2022-03-21 14:14:32 +0100

    Refactor tests with parametrization, to make adding test cases easier

--- tests/schemas/tests/test_fundamental_schemas.py
+++ tests/schemas/tests/test_fundamental_schemas.py
@@ -1,13 +1,25 @@
+from typing import Any, Dict
+
 import pytest
 
 from schemas import validate_message
 from schemas.predefined import *
 
 
-def test_validation_of_correct_type():
-    validate_message('example-string', Str())
+@pytest.mark.parametrize(
+    'correct_value, schema', [
+        ('example-string', Str()),
+    ]
+)
+def test_validation_of_correct_type(correct_value: Any, schema: Dict[str, Any]):
+    validate_message(correct_value, schema)
 
 
-def test_validation_of_incorrect_type():
+@pytest.mark.parametrize(
+    'incorrect_value, schema', [
+        (1, Str()),
+    ]
+)
+def test_validation_of_incorrect_type(incorrect_value: Any, schema: Dict[str, Any]):
     with pytest.raises(AssertionError):
-        validate_message(1, Str())
+        validate_message(incorrect_value, schema)
