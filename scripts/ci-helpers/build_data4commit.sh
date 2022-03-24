74d602f61d5eaece30e5bf944fc522c41c925b9e
Bartek Wrona <wrona@syncad.com>
2022-03-24 09:32:47 +0100

    Support for building a data (API) instance holding their state produced until 5M blocks.

---
+++ scripts/ci-helpers/build_data4commit.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTSDIR="$SCRIPTPATH/.."
+SRCROOTDIR="$SCRIPTSDIR/.."
+
+LOG_FILE=build_data4commit.log
+source "$SCRIPTSDIR/common.sh"
+
+COMMIT="$1"
+
+REGISTRY=${2:-registry.gitlab.syncad.com/hive/hive}
+
+export BLOCK_LOG_SUFFIX="-5m"
+
+CI_IMAGE_TAG=:ubuntu20.04-3
+BUILD_IMAGE_TAG=:$COMMIT
+
+pushd "$SRCROOTDIR"
+pwd
+
+"$SCRIPTSDIR/ci-helpers/build_instance4commit.sh" $COMMIT $REGISTRY
+
+docker build --target=data \
+  --build-arg CI_REGISTRY_IMAGE=$REGISTRY --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG --build-arg BLOCK_LOG_SUFFIX="-5m" \
+  --build-arg COMMIT=$COMMIT --build-arg BUILD_IMAGE_TAG=$BUILD_IMAGE_TAG -t $REGISTRY/data$BUILD_IMAGE_TAG -f Dockerfile .
+
+popd
