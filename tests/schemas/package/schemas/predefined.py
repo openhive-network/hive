d32217de28b8679412636717aceeb7e7283cff15
Michał Kudela <kudmich@syncad.com>
2022-03-25 11:39:00 +0100

    Add Map schema

--- tests/schemas/package/schemas/predefined.py
+++ tests/schemas/package/schemas/predefined.py
@@ -1,1 +1,1 @@
-from schemas.__private.fundamental_schemas import Any_ as Any, Bool, Float, Null, String
+from schemas.__private.fundamental_schemas import Any_ as Any, Bool, Float, Map, Null, String
