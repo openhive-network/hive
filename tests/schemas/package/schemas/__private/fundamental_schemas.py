b39552c2461ac8d20478f5ab94cfa407132f2dfb
Michał Kudela <kudmich@syncad.com>
2022-03-25 11:39:00 +0100

    Add Map schema

--- tests/schemas/package/schemas/__private/fundamental_schemas.py
+++ tests/schemas/package/schemas/__private/fundamental_schemas.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import pprint
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional
 
 import jsonschema
 
@@ -60,6 +60,39 @@ class Bool(Schema):
 class Float(Schema):
     def _create_core_of_schema(self) -> Dict[str, Any]:
         return {'type': 'number'}
+
+
+class Map(Schema):
+    def __init__(self, schema: Dict, required_keys: Optional[List[str]] = None,
+                 allow_additional_properties: bool = False, **options: Any):
+        """
+        :param schema: Collection of properties. Each of the individual properties has a key
+        (represents the name of the key in instance), and schema (represents the required type,
+        for the previously specified key).
+        :param required_keys: The keys contained in this parameter are required for successful validation.
+        The other keys specified in the schema are optional, their absence will not affect the validation process.
+        :param allow_additional_properties: By default map does not accept additional undefined properties.
+        All instance elements not defined in the schema will be reported as a 'ValidationError'.
+        Set this parameter to True, it will allow the correct validation of additional properties in the instance.
+        :param options: Other options that can be given in the form of a dictionary.
+        """
+
+        super().__init__(**options)
+        self.__allow_additional_properties: bool = allow_additional_properties
+        self.__schema = schema
+        self.__required_keys: List[str] = list(self.__schema.keys()) if required_keys is None else required_keys
+
+    def _create_core_of_schema(self) -> Dict[str, Any]:
+        items_as_dicts = self.__schema.copy()
+        for key, schema in self.__schema.items():
+            self._assert_that_schema_has_correct_type(schema)
+            items_as_dicts[key] = schema._create_schema()
+        return {
+            'type': 'object',
+            'properties': items_as_dicts,
+            'required': self.__required_keys,
+            'additionalProperties': self.__allow_additional_properties
+        }
 
 
 class Null(Schema):
--- tests/schemas/tests/test_fundamental_schemas.py
+++ tests/schemas/tests/test_fundamental_schemas.py
@@ -18,6 +18,81 @@ from schemas.predefined import *
         (Float(), 3.141593),
         (Float(), 128),  # Ints are also floats
 
+        # Map
+        (Map({}), {}),
+        (
+            Map({'k1': Str(), 'k2': Float()}),
+            {'k1': '1', 'k2': 1}
+        ),
+        (
+            # Nested map
+            Map({
+                'k1': Map({
+                    'k2': Map({
+                        'k3': Bool()
+                    })
+                })
+            }),
+            {'k1': {'k2': {'k3': False}}}
+        ),
+
+        # Map - required_keys
+        (
+            # When optional key is missing.
+            Map(
+                {
+                    'name': Str(),
+                    'email': Str(),
+                    'telephone': Float()  # Optional
+                },
+                required_keys=['name', 'email']
+            ),
+            {
+                'name': 'Josh',
+                'email': 'josh@josh.com',
+                # Note, that 'telephone' is missing
+            }
+        ),
+        (
+            # When optional key is present.
+            Map(
+                {
+                    'name': Str(),
+                    'email': Str(),
+                    'telephone': Float()
+                },
+                required_keys=['name', 'email']
+            ),
+            {
+                'name': 'Josh',
+                'email': 'josh@josh.com',
+                'telephone': 3.141593,
+            }
+        ),
+
+        # Map - allow_additional_properties
+        (
+            Map(
+                {'name': Str()},
+                allow_additional_properties=True
+            ),
+            {
+                'name': 'Josh',
+                'extra-key': 'extra-key',
+            }
+        ),
+        (
+            Map(
+                {'name': Str()},
+                required_keys=['name'],
+                allow_additional_properties=True
+            ),
+            {
+                'name': 'Josh',
+                'extra-key': 'extra-key',
+            }
+        ),
+
         # Null
         (Null(), None),
 
@@ -42,6 +117,64 @@ def test_validation_of_correct_type(schema, instance):
         (Float(), None),
         (Float(), True),
         (Float(), '3.141593'),
+
+        # Map
+        (Map({}), 5),
+        (Map({'key-0': Str()}), 1),
+        (Map({'key-0': Str()}), 'example-string'),
+        (Map({'key-0': Str()}), {'key-0': 1}),
+        (Map({'key-0': Str()}), {'key-0': '0', 'key-1': '1'}),  # By default, map doesn't allow additional properties
+
+        # Map: required_keys
+        (
+            # Missing required key
+            Map(
+                {
+                    'name': Str(),
+                    'email': Str(),
+                    'telephone': Float()
+                },
+                required_keys=['name', 'email']
+            ),
+            {
+                'name': 'Josh',
+                # Note, that 'email' is missing
+                'telephone': 3.141593,
+            }
+        ),
+
+        # Map: allow_additional_properties
+        (
+            # Test if additional properties are disabled by default
+            Map(
+                {
+                    'name': Str(),
+                    'email': Str(),
+                    'telephone': Float()
+                },
+            ),
+            {
+                'name': 'Josh',
+                'email': 'josh@josh.com',
+                'telephone': 3.141593,
+                'extra-key': 'extra-key'  # This property is additional, wasn't included in schema
+            }
+        ),
+
+        (
+            Map(
+                {
+                    'name': Str(),
+                    'email': Str(),
+                    'telephone': {'type': 'string'}  # Should be Str()
+                },
+            ),
+            {
+                'name': 3,
+                'email': 'josh@josh.com',
+                'telephone': '123-45-67'
+            }
+        ),
 
         # Null
         (Null(), 0),
