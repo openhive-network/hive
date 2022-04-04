d23a3a97d58adcf4da235903e3193a71cc487fd6
Radek Masłowski <rmaslowski@syncad.com>
2022-04-04 15:02:14 +0200

    Refresh tests

--- tests/functional/python_tests/wallet_bridge_api_tests/proposals_tests/test_find_proposals.py
+++ tests/functional/python_tests/wallet_bridge_api_tests/proposals_tests/test_find_proposals.py
@@ -4,14 +4,6 @@ import test_tools.exceptions
 
 import proposals_tools
 
-# TODO BUG LIST!
-"""
-1. Problem with calling wallet_bridge_api.find_proposals with the wrong data types in the arguments(# BUG1) [SOLVED!]
-2. Problem with calling wallet_bridge_api.find_proposals with argument "['True']"
-     Sent: {"jsonrpc": "2.0", "id": 1, "method": "wallet_bridge_api.find_proposals", "params": [["True"]]}'
-     Received: 'message': "Parse Error:Couldn't parse int64_t"
-     (# BUG2)  [SOLVED!]
-"""
 
 ACCOUNTS = [f'account-{i}' for i in range(5)]
 
@@ -19,7 +11,7 @@ CORRECT_VALUES = [
     [0],
     [0, 1, 2, 3, 4],
     [6],
-    [True],  # BUG2
+    [True],
 ]
 
 
@@ -52,7 +44,7 @@ def tests_with_correct_values_in_quotes(node, wallet, proposal_ids):
 
 @pytest.mark.parametrize(
     'proposal_id', [
-        -1,  # OUT OFF LIMITS: too low id
+        [-1],  # OUT OFF LIMITS: too low id
     ]
 )
 def tests_with_incorrect_values(node, wallet, proposal_id):
@@ -60,14 +52,14 @@ def tests_with_incorrect_values(node, wallet, proposal_id):
     proposals_tools.prepare_proposals(wallet, ACCOUNTS)
 
     with pytest.raises(test_tools.exceptions.CommunicationError):
-        node.api.wallet_bridge.find_proposals([proposal_id])
+        node.api.wallet_bridge.find_proposals(proposal_id)
 
 
 @pytest.mark.parametrize(
     'proposal_id', [
-        'invalid-argument',
+        ['invalid-argument'],
     ]
 )
 def tests_with_incorrect_type_of_argument(node, proposal_id):
     with pytest.raises(test_tools.exceptions.CommunicationError):
-        node.api.wallet_bridge.find_proposals([proposal_id])
+        node.api.wallet_bridge.find_proposals(proposal_id)
--- tests/functional/python_tests/wallet_bridge_api_tests/proposals_tests/test_list_proposals.py
+++ tests/functional/python_tests/wallet_bridge_api_tests/proposals_tests/test_list_proposals.py
@@ -6,15 +6,6 @@ import test_tools.exceptions
 
 import proposals_tools
 
-# TODO BUG LIST!
-"""
-1. Problem with calling wallet_bridge_api.list_proposals with the wrong data types in the arguments(# BUG1) [SOLVED!]
-
-2. Problem with calling wallet_bridge_api.list_proposals with argument "['True']"
-     Sent: {"jsonrpc": "2.0", "id": 1, "method": "wallet_bridge_api.list_proposals", "params": [[[""], "True", "29", "0", "0"]]}'
-     Received: 'message': "Parse Error:Couldn't parse int64_t"
-     (# BUG2) [SOLVED !]
-"""
 
 ACCOUNTS = [f'account-{i}' for i in range(5)]
 
@@ -42,59 +33,72 @@ def date_from_now(*, weeks):
     future_data = datetime.now() + timedelta(weeks=weeks)
     return future_data.strftime('%Y-%m-%dT%H:%M:%S')
 
+
 CORRECT_VALUES = [
         # START
         # At the moment there is an assumption, that no more than one start parameter is passed, more are ignored
-
             # by creator
         ([''], 100, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
+
         ([ACCOUNTS[1]], 100, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
+
         # Start from nonexistent account (name "account-2a" is alphabetically between 'account-2' and 'account-3').
         (['account-2a'], 100, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
-        ([ACCOUNTS[1], date_from_now(weeks=20)], 100, ORDER_BY['by_creator'],
-         ORDER_DIRECTION['ascending'], STATUS['all']),
-        ([ACCOUNTS[1], date_from_now(weeks=25)], 100, ORDER_BY['by_creator'],
-         ORDER_DIRECTION['ascending'], STATUS['all']),
+
+        ([ACCOUNTS[1], date_from_now(weeks=20)], 100, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'],
+         STATUS['all']),
+
+        ([ACCOUNTS[1], date_from_now(weeks=25)], 100, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'],
+         STATUS['all']),
+
         ([ACCOUNTS[1], date_from_now(weeks=20), date_from_now(weeks=25)], 100, ORDER_BY['by_creator'],
          ORDER_DIRECTION['ascending'], STATUS['all']),
+
         ([ACCOUNTS[1], date_from_now(weeks=20), date_from_now(weeks=25), 'additional_argument'], 100,
          ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
 
             # by start date
-        ([], 100, 'by_start_date', 'ascending', 'all'),
-        ([date_from_now(weeks=20)], 100, ORDER_BY['by_start_date'], ORDER_DIRECTION['ascending'],
-         STATUS['all']),
+        ([], 100, ORDER_BY['by_start_date'], ORDER_DIRECTION['ascending'], STATUS['all']),
+
+        ([date_from_now(weeks=20)], 100, ORDER_BY['by_start_date'], ORDER_DIRECTION['ascending'], STATUS['all']),
+
         ([date_from_now(weeks=20), date_from_now(weeks=25)], 100, ORDER_BY['by_start_date'],
          ORDER_DIRECTION['ascending'], STATUS['all']),
-        ([date_from_now(weeks=20), ACCOUNTS[1]], 100, ORDER_BY['by_start_date'],
-         ORDER_DIRECTION['ascending'], STATUS['all']),
+
+        ([date_from_now(weeks=20), ACCOUNTS[1]], 100, ORDER_BY['by_start_date'], ORDER_DIRECTION['ascending'],
+         STATUS['all']),
+
         ([date_from_now(weeks=20), ACCOUNTS[1], date_from_now(weeks=25)], 100, ORDER_BY['by_start_date'],
          ORDER_DIRECTION['ascending'], STATUS['all']),
+
         ([date_from_now(weeks=20), ACCOUNTS[1], date_from_now(weeks=25), 'additional_argument'], 100,
          ORDER_BY['by_start_date'], ORDER_DIRECTION['ascending'], STATUS['all']),
 
             # by end date
         ([], 100, ORDER_BY['by_end_date'], ORDER_DIRECTION['ascending'], STATUS['all']),
-        ([date_from_now(weeks=25)], 100, ORDER_BY['by_end_date'], ORDER_DIRECTION['ascending'],
+
+        ([date_from_now(weeks=25)], 100, ORDER_BY['by_end_date'], ORDER_DIRECTION['ascending'], STATUS['all']),
+
+        ([date_from_now(weeks=25), date_from_now(weeks=20)], 100, ORDER_BY['by_end_date'], ORDER_DIRECTION['ascending'],
          STATUS['all']),
-        ([date_from_now(weeks=25), date_from_now(weeks=20)], 100, ORDER_BY['by_end_date'],
-         ORDER_DIRECTION['ascending'], STATUS['all']),
-        ([date_from_now(weeks=25), ACCOUNTS[1]], 100, ORDER_BY['by_end_date'],
-         ORDER_DIRECTION['ascending'], STATUS['all']),
+
+        ([date_from_now(weeks=25), ACCOUNTS[1]], 100, ORDER_BY['by_end_date'], ORDER_DIRECTION['ascending'],
+         STATUS['all']),
+
         ([date_from_now(weeks=25), ACCOUNTS[1], date_from_now(weeks=25)], 100, ORDER_BY['by_end_date'],
          ORDER_DIRECTION['ascending'], STATUS['all']),
+
         ([date_from_now(weeks=25), ACCOUNTS[1], date_from_now(weeks=25), 'additional_argument'], 100,
          ORDER_BY['by_end_date'], ORDER_DIRECTION['ascending'], STATUS['all']),
 
         # LIMIT
         ([''], 0, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
         ([''], 1000, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
-        ([''], True, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),   # BUG2
+        ([''], True, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
 
         # ORDER BY
         ([''], 100, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
         ([''], 100, ORDER_BY['by_end_date'], ORDER_DIRECTION['ascending'], STATUS['all']),
-
 
         # ORDER DIRECTION
         ([''], 100, ORDER_BY['by_creator'], ORDER_DIRECTION['ascending'], STATUS['all']),
@@ -126,6 +130,7 @@ def tests_with_correct_values_with_quotes(node, wallet, start, limit, order_by, 
 
     for start_number in range(len(start)):
         start[start_number] = proposals_tools.add_quotes_to_bool_or_numeric(start[start_number])
+
     limit = proposals_tools.add_quotes_to_bool_or_numeric(limit)
     order_by = proposals_tools.add_quotes_to_bool_or_numeric(order_by)
     order_direction = proposals_tools.add_quotes_to_bool_or_numeric(order_direction)
@@ -136,7 +141,6 @@ def tests_with_correct_values_with_quotes(node, wallet, start, limit, order_by, 
             node.api.wallet_bridge.list_proposals(start, limit, order_by, order_direction, status)
     else:
         node.api.wallet_bridge.list_proposals(start, limit, order_by, order_direction, status)
-
 
 
 @pytest.mark.parametrize(
