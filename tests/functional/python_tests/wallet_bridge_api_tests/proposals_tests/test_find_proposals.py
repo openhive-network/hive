05fc19299506e5099b037306e0bc9c78c90b29ae
Radek Masłowski <rmaslowski@syncad.com>
2022-04-05 11:16:53 +0200

    Rebuild test_find_proposals

--- tests/functional/python_tests/wallet_bridge_api_tests/proposals_tests/test_find_proposals.py
+++ tests/functional/python_tests/wallet_bridge_api_tests/proposals_tests/test_find_proposals.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 import test_tools.exceptions
@@ -7,16 +9,14 @@ import proposals_tools
 
 ACCOUNTS = [f'account-{i}' for i in range(5)]
 
-CORRECT_VALUES = [
-    [0],
-    [0, 1, 2, 3, 4],
-    [6],
-    [True],
-]
-
 
 @pytest.mark.parametrize(
-    'proposal_ids', CORRECT_VALUES
+    'proposal_ids', [
+        [0],
+        [0, 1, 2, 3, 4],
+        [6],
+        [True],
+    ]
 )
 def tests_with_correct_values(node, wallet, proposal_ids):
     proposals_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
@@ -26,25 +26,42 @@ def tests_with_correct_values(node, wallet, proposal_ids):
 
 
 @pytest.mark.parametrize(
-    'proposal_ids', CORRECT_VALUES
+    'proposal_ids', [
+        [0],
+        [0, 1, 2, 3, 4],
+        [6],
+    ]
 )
-def tests_with_correct_values_in_quotes(node, wallet, proposal_ids):
+def tests_with_correct_values_as_strings(node, wallet, proposal_ids):
     for proposal_id_number in range(len(proposal_ids)):
-        proposal_ids[proposal_id_number] = proposals_tools.convert_bool_or_numeric_to_string(proposal_ids[proposal_id_number])
+        proposal_ids[proposal_id_number] = json.dumps(proposal_ids[proposal_id_number])
 
     proposals_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
     proposals_tools.prepare_proposals(wallet, ACCOUNTS)
 
-    if proposal_ids == ['True']:   # Bool in quotes have special work and not throw exception
-        with pytest.raises(test_tools.exceptions.CommunicationError):
-            node.api.wallet_bridge.find_proposals(proposal_ids)
-    else:
-        node.api.wallet_bridge.find_proposals(proposal_ids)
+    node.api.wallet_bridge.find_proposals(proposal_ids)
+
+
+@pytest.mark.parametrize(
+    'proposal_ids', [
+        [0],
+        [0, 1, 2, 3, 4],
+        [6],
+    ]
+)
+def test_list_of_arguments_as_string(node, wallet, proposal_ids):
+
+    proposals_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
+    proposals_tools.prepare_proposals(wallet, ACCOUNTS)
+
+    with pytest.raises(test_tools.exceptions.CommunicationError):
+        node.api.wallet_bridge.find_proposals(json.dumps(proposal_ids))
 
 
 @pytest.mark.parametrize(
     'proposal_id', [
         [-1],  # OUT OFF LIMITS: too low id
+        [json.dumps(True)],
     ]
 )
 def tests_with_incorrect_values(node, wallet, proposal_id):
