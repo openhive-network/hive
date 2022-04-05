b1f0f081b248072f1bd86972f93ba354a793f810
Radek Masłowski <rmaslowski@syncad.com>
2022-04-05 11:16:53 +0200

    Rebuild test_find_proposals

--- tests/functional/python_tests/wallet_bridge_api_tests/proposals_tests/test_find_proposals.py
+++ tests/functional/python_tests/wallet_bridge_api_tests/proposals_tests/test_find_proposals.py
@@ -4,23 +4,28 @@ import pytest
 
 import test_tools.exceptions
 
-import proposals_tools
+from . import local_tools
 
 
 ACCOUNTS = [f'account-{i}' for i in range(5)]
 
 
+CORRECT_VALUES = [
+            [0],
+            [0, 1, 2, 3, 4],
+            [6],
+        ]
+
 @pytest.mark.parametrize(
     'proposal_ids', [
-        [0],
-        [0, 1, 2, 3, 4],
-        [6],
+        *CORRECT_VALUES,
+        *local_tools.as_string(CORRECT_VALUES),
         [True],
     ]
 )
-def tests_with_correct_values(node, wallet, proposal_ids):
-    proposals_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
-    proposals_tools.prepare_proposals(wallet, ACCOUNTS)
+def test_find_proposals_with_correct_values(node, wallet, proposal_ids):
+    local_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
+    local_tools.prepare_proposals(wallet, ACCOUNTS)
 
     node.api.wallet_bridge.find_proposals(proposal_ids)
 
@@ -32,27 +37,10 @@ def tests_with_correct_values(node, wallet, proposal_ids):
         [6],
     ]
 )
-def tests_with_correct_values_as_strings(node, wallet, proposal_ids):
-    for proposal_id_number in range(len(proposal_ids)):
-        proposal_ids[proposal_id_number] = json.dumps(proposal_ids[proposal_id_number])
 
-    proposals_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
-    proposals_tools.prepare_proposals(wallet, ACCOUNTS)
-
-    node.api.wallet_bridge.find_proposals(proposal_ids)
-
-
-@pytest.mark.parametrize(
-    'proposal_ids', [
-        [0],
-        [0, 1, 2, 3, 4],
-        [6],
-    ]
-)
-def test_list_of_arguments_as_string(node, wallet, proposal_ids):
-
-    proposals_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
-    proposals_tools.prepare_proposals(wallet, ACCOUNTS)
+def test_find_proposals_with_list_of_arguments_as_string(node, wallet, proposal_ids):
+    local_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
+    local_tools.prepare_proposals(wallet, ACCOUNTS)
 
     with pytest.raises(test_tools.exceptions.CommunicationError):
         node.api.wallet_bridge.find_proposals(json.dumps(proposal_ids))
@@ -61,12 +49,12 @@ def test_list_of_arguments_as_string(node, wallet, proposal_ids):
 @pytest.mark.parametrize(
     'proposal_id', [
         [-1],  # OUT OFF LIMITS: too low id
-        [json.dumps(True)],
+        ['true'],
     ]
 )
-def tests_with_incorrect_values(node, wallet, proposal_id):
-    proposals_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
-    proposals_tools.prepare_proposals(wallet, ACCOUNTS)
+def test_find_proposals_with_incorrect_values(node, wallet, proposal_id):
+    local_tools.create_accounts_with_vests_and_tbd(wallet, ACCOUNTS)
+    local_tools.prepare_proposals(wallet, ACCOUNTS)
 
     with pytest.raises(test_tools.exceptions.CommunicationError):
         node.api.wallet_bridge.find_proposals(proposal_id)
@@ -77,6 +65,6 @@ def tests_with_incorrect_values(node, wallet, proposal_id):
         ['invalid-argument'],
     ]
 )
-def tests_with_incorrect_type_of_argument(node, proposal_id):
+def test_find_proposals_with_incorrect_type_of_argument(node, proposal_id):
     with pytest.raises(test_tools.exceptions.CommunicationError):
         node.api.wallet_bridge.find_proposals(proposal_id)
