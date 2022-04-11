16b07784ca83ed7df8697c78f41643d81b295ad9
Mariusz Trela <mtrela@syncad.com>
2022-04-11 13:56:11 +0200

    `wallet_bridge_api::get_conversion_requests` fixed

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -2340,10 +2340,11 @@ serializer_wrapper<annotated_signed_transaction> wallet_api::publish_feed(
   return { my->sign_transaction( tx, broadcast ) };
 }
 
-serializer_wrapper<vector< database_api::api_convert_request_object >> wallet_api::get_conversion_requests( const string& owner_account )
+serializer_wrapper<vector< database_api::api_convert_request_object >> wallet_api::get_conversion_requests( fc::variant owner_account )
 {
   my->require_online();
-  return { my->_remote_wallet_bridge_api->get_conversion_requests( {owner_account}, LOCK ) };
+  vector<variant> args{std::move(owner_account)};
+  return { my->_remote_wallet_bridge_api->get_conversion_requests( {args}, LOCK ) };
 }
 
 serializer_wrapper<vector< database_api::api_collateralized_convert_request_object >> wallet_api::get_collateralized_conversion_requests( fc::variant owner_account )
