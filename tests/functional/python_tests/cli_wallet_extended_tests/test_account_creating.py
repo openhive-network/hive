ed66a12a4e91e7af485c29070ab5d76440c6488f
Piotr Batko <pbatko@syncad.com>
2022-04-25 16:02:04 +0200

    Add test of account creation with too long name

--- tests/functional/python_tests/cli_wallet_extended_tests/test_account_creating.py
+++ tests/functional/python_tests/cli_wallet_extended_tests/test_account_creating.py
@@ -1,3 +1,16 @@
+import pytest
+
+import test_tools as tt
+
+
 def test_account_creation(wallet):
     wallet.api.create_account('initminer', 'alice', '{}')
     assert wallet.api.list_accounts('a', 1) == ['alice']
+
+
+def test_creation_of_account_with_too_long_name(wallet):
+    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+        wallet.api.create_account('initminer', 'too-long-account-name', '{}')
+
+    error_message = exception.value.response['error']['message']
+    assert 'Account name too-long-account-name is invalid' in error_message
