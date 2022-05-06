3b7383929967ded5415ade0d4112dade2bcbdc21
Mariusz Trela <mtrela@syncad.com>
2022-05-06 11:27:47 +0200

    Some fixes/preparations

--- libraries/wallet/include/hive/wallet/wallet.hpp
+++ libraries/wallet/include/hive/wallet/wallet.hpp
@@ -91,7 +91,7 @@ class wallet_api
 {
   public:
     wallet_api( const wallet_data& initial_data, const chain_id_type& _hive_chain_id,
-        const fc::api< hive::plugins::wallet_bridge_api::wallet_bridge_api >& remote_api, fc::promise< int >::ptr& exit_promise, bool is_daemon, format_type _format, bool _legacy_format );
+        const fc::api< hive::plugins::wallet_bridge_api::wallet_bridge_api >& remote_api, fc::promise< int >::ptr& exit_promise, bool is_daemon, format_type _format, bool legacy_format );
     virtual ~wallet_api();
 
     bool copy_wallet_file( const string& destination_filename );
