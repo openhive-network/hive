59d43e0e1168df67dc458b5e8485e48bb7c24309
Bartek Wrona <wrona@syncad.com>
2022-05-18 20:09:01 +0200

    Defined build_ci_images.sh script to simplify CI base images build.

--- scripts/ci-helpers/build_ci_base_image.sh
+++ scripts/ci-helpers/build_ci_base_image.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
-set -euo pipefail 
-
 REGISTRY=${1:-registry.gitlab.syncad.com/hive/hive/}
-CI_IMAGE_TAG=:ubuntu20.04-3
+CI_IMAGE_TAG=:ubuntu20.04-4
 
 docker build --target=runtime \
   --build-arg CI_REGISTRY_IMAGE=$REGISTRY --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
   -t ${REGISTRY}runtime$CI_IMAGE_TAG -f Dockerfile .
+
 
 docker build --target=ci-base-image \
   --build-arg CI_REGISTRY_IMAGE=$REGISTRY --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
