e7c89500aca68a7714a66fe59912b7bb28b45940
kristupas <kristupas@pcstransfer.reputeme.com>
2022-05-23 16:37:18 +0000

    Fixed pattern bug #297

--- tests/api_tests/pattern_tests/run_tests.sh
+++ tests/api_tests/pattern_tests/run_tests.sh
@@ -10,13 +10,13 @@ fi
 export TAVERN_DIR="$2/tests/api_tests/pattern_tests/"
 
 if [ -z "$3" ]; then
-    TEST_NAMES='not get_transaction_hex and (get_transaction or get_account_history or enum_virtual_ops or get_ops_in_block)'
+    TEST_NAMES="not get_transaction_hex and (get_transaction or get_account_history or enum_virtual_ops or get_ops_in_block)"
 else
     TEST_NAMES=$3
     ./gen_new_style_calls.py --tavern-dir=$TAVERN_DIR
 fi
 
-tox -e tavern -- --tb=long -W ignore::pytest.PytestDeprecationWarning --workers 5 --tests-per-worker auto -k $TEST_NAMES --junitxml=results.xml
+tox -e tavern -- --tb=long -W ignore::pytest.PytestDeprecationWarning --workers 5 --tests-per-worker auto -k "$TEST_NAMES" --junitxml=results.xml
 
 #rm -rf $TAVERN_DIR/tavern/hafah_api_negative
 #rm -rf $TAVERN_DIR/tavern/hafah_api_patterns
