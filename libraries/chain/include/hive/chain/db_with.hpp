b98d93d57aae33ab4285818c03486acfe3798e54
ABW <andrzejl@syncad.com>
2022-05-25 18:38:00 +0200

    disable pending transaction execution limit for testnet
    The limit is especially problematic during debugging, but in some cases it could also negatively affect unit tests.

--- libraries/chain/include/hive/chain/db_with.hpp
+++ libraries/chain/include/hive/chain/db_with.hpp
@@ -82,8 +82,10 @@ struct pending_transactions_restorer
 
     auto handle_tx = [&]( signed_transaction& tx )
     {
+#ifndef IS_TEST_NET //especially during debugging that limit is highly problematic
       if( apply_trxs && fc::time_point::now() - start > HIVE_PENDING_TRANSACTION_EXECUTION_LIMIT )
         apply_trxs = false;
+#endif
 
       if( apply_trxs )
       {
