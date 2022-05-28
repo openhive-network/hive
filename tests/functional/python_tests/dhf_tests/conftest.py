3a610af65b05c8c9e3bff81feb473cfd3d195604
Mateusz Żebrak <mzebrak@syncad.com>
2022-05-28 11:35:41 +0200

    Make test_list_voter_proposal_sort.py runnable through pytest and use test_tools node initialization

---
+++ tests/functional/python_tests/dhf_tests/conftest.py
@@ -0,0 +1,50 @@
+from typing import Final
+
+from beembase.operations import Create_proposal
+import pytest
+import test_tools as tt
+
+from . import test_utils
+from ... import hive_utils
+
+CREATOR: Final[str] = "initminer"
+TREASURY: Final[str] = "hive.fund"
+
+
+def create_proposals(node, accounts, start_date, end_date, wif=None):
+    tt.logger.info("Creating proposals...")
+    for account in accounts:
+        tt.logger.info(
+            "New proposal ==> ({},{},{},{},{},{},{})".format(
+                account["name"],
+                account["name"],
+                start_date,
+                end_date,
+                "24.000 TBD",
+                "Proposal from account {}".format(account["name"]),
+                test_utils.get_permlink(account["name"]),
+            )
+        )
+        op = Create_proposal(
+            **{
+                "creator": account["name"],
+                "receiver": account["name"],
+                "start_date": start_date,
+                "end_date": end_date,
+                "daily_pay": "24.000 TBD",
+                "subject": "Proposal from account {}".format(account["name"]),
+                "permlink": test_utils.get_permlink(account["name"]),
+            }
+        )
+        node.finalizeOp(op, account["name"], "active")
+    if wif is not None:
+        hive_utils.debug_generate_blocks(node.rpc.url, wif, 5)
+    else:
+        hive_utils.common.wait_n_blocks(node.rpc.url, 5)
+
+
+@pytest.fixture
+def node():
+    init_node = tt.InitNode()
+    init_node.run()
+    return init_node
