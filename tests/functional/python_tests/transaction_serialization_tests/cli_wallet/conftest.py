f1db6a9227cdb4b288d7f635094186236aeb1f0c
Radek Masłowski <rmaslowski@syncad.com>
2022-06-02 15:23:57 +0200

    WIP!!!

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/conftest.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/conftest.py
@@ -5,6 +5,9 @@ from test_tools import constants, World
 from test_tools.private.scope import context
 from test_tools.private.scope.scope_fixtures import *  # pylint: disable=wildcard-import, unused-wildcard-import
 
+def pytest_addoption(parser):
+    parser.addoption( "--http_endpoint", action="store", type=str, help='specifies http_endpoint of reference node')
+    parser.addoption( "--ws_endpoint", action="store", type=str, help='specifies ws_endpoint of reference node')
 
 @pytest.fixture
 def world():
