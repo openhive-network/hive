79511afc723a23571f5c82e69f508f3ec7570e28
Piotr Batko <pbatko@syncad.com>
2022-06-06 14:20:38 +0200

    Add decorators simpifying wallet asset serialization tests

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/conftest.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/conftest.py
@@ -8,3 +8,13 @@ def node():
     node = tt.InitNode()
     node.run()
     return node
+
+
+@pytest.fixture
+def prepared_wallet(node, request):
+    if request.param == 'modern':
+        yield tt.Wallet(attach_to=node, additional_arguments=['--transaction-serialization=hf26'])
+    elif request.param == 'legacy':
+        yield tt.Wallet(attach_to=node, additional_arguments=['--transaction-serialization=legacy'])
+    else:
+        raise RuntimeError(f'Unsupported argument value "{request.param}", should be "modern" or "legacy"')
