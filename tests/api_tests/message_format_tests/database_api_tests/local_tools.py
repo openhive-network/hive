1c84d9a758c7dd7d4a0cc14783d484ca95fdde98
Radek Masłowski <rmaslowski@syncad.com>
2022-06-06 14:21:03 +0200

    Move support function to create founded accounts to global 'local_tools'

--- tests/api_tests/message_format_tests/database_api_tests/local_tools.py
+++ tests/api_tests/message_format_tests/database_api_tests/local_tools.py
@@ -1,27 +1,10 @@
 import json
 import os
 import subprocess
-from typing import Optional
 
 from test_tools import Account, Asset
-
+from local_tools import create_account_and_fund_it
 from ..local_tools import date_from_now
-
-
-def create_account_and_fund_it(wallet, name: str, tests: Optional[Asset.Test] = None,
-                               vests: Optional[Asset.Test] = None, tbds: Optional[Asset.Tbd] = None):
-    assert any(asset is not None for asset in [tests, vests, tbds]), 'You forgot to fund account'
-
-    wallet.api.create_account('initminer', name, '{}')
-
-    if tests is not None:
-        wallet.api.transfer('initminer', name, tests, 'memo')
-
-    if vests is not None:
-        wallet.api.transfer_to_vesting('initminer', name, vests)
-
-    if tbds is not None:
-        wallet.api.transfer('initminer', name, tbds, 'memo')
 
 
 def create_and_cancel_vesting_delegation(wallet, delegator, delegatee):
--- tests/api_tests/message_format_tests/database_api_tests/test_api_database_message_format.py
+++ tests/api_tests/message_format_tests/database_api_tests/test_api_database_message_format.py
@@ -3,9 +3,9 @@ import pytest
 from test_tools import Account, Asset
 
 from ..local_tools import date_from_now
-from .local_tools import create_account_and_fund_it, create_and_cancel_vesting_delegation, create_proposal,\
+from .local_tools import create_and_cancel_vesting_delegation, create_proposal,\
     generate_sig_digest, prepare_escrow, request_account_recovery, transfer_and_withdraw_from_savings
-
+from local_tools import create_account_and_fund_it
 
 def test_find_account_recovery_requests(node, wallet):
     wallet.api.create_account('initminer', 'alice', '{}')
