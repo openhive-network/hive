094e3e99e8c2ebe218788bd69f93aca026b566cc
mtyszczak <mateusz.tyszczak@gmail.com>
2022-06-08 21:55:18 +0200

    fixup! Add test of account creation with too long name

--- tests/functional/python_tests/cli_wallet_extended_tests/test_account_creating.py
+++ tests/functional/python_tests/cli_wallet_extended_tests/test_account_creating.py
@@ -13,4 +13,4 @@ def test_creation_of_account_with_too_long_name(wallet):
         wallet.api.create_account('initminer', 'too-long-account-name', '{}')
 
     error_message = exception.value.response['error']['message']
-    assert 'Invalid account name. It is too long, should have up to 16 characters.' in error_message
+    assert 'Account name too-long-account-name is invalid' in error_message
