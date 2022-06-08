53ce6a26d342373460edbee3e6429279a36fc8b9
Radek Masłowski <rmaslowski@syncad.com>
2022-06-08 11:12:49 +0200

    WIP

--- tests/functional/python_tests/direct_rc_delegations_tests/test_list_direct_rc_delegations.py
+++ tests/functional/python_tests/direct_rc_delegations_tests/test_list_direct_rc_delegations.py
@@ -14,12 +14,27 @@ from test_tools import Asset
                 ]
         ),
         (
+                'alice', 'carol',
+                [{'from': 'alice', 'to': 'carol', 'delegated_rc': 10}]
+        ),
+        (
                 'alice', '',
                 [
                     {'from': 'alice', 'to': 'bob', 'delegated_rc': 5},
                     {'from': 'alice', 'to': 'carol', 'delegated_rc': 10}
                 ]
         ),
+        # Alice as 'to' parameter allow list delegations to bob and carol, because id of alice is less then id
+        # alice's and bob's
+        (
+                'alice', 'alice',
+                [
+                    {'from': 'alice', 'to': 'bob', 'delegated_rc': 5},
+                    {'from': 'alice', 'to': 'carol', 'delegated_rc': 10}
+                ]
+        ),
+        # Initminer as 'to' parameter allow list delegations to bob and carol, because id of initminer is less then id
+        # alice's and bob's  (initminer was created first)
         (
                 'alice', 'initminer',
                 [
@@ -27,13 +42,23 @@ from test_tools import Asset
                     {'from': 'alice', 'to': 'carol', 'delegated_rc': 10}
                 ]
         ),
-        (
-                'alice', 'carol',
-                [{'from': 'alice', 'to': 'carol', 'delegated_rc': 10}]
-        ),
+        # Those cases aren't return any delegations, because weren't any delegations from bob, carol and initminer
+        ('bob', 'alice', []),
+        ('bob', 'bob', []),
+        ('bob', 'carol', []),
+        ('bob', 'initminer', []),
+        ('carol', 'alice', []),
+        ('carol', 'bob', []),
+        ('carol', 'carol', []),
+        ('carol', 'initminer', []),
+        ('initminer', 'alice', []),
+        ('initminer', 'bob', []),
+        ('initminer', 'carol', []),
+        ('initminer', 'initminer', []),
     ]
 )
 def test_list_rc_direct_delegations(node, wallet, from_, to, expected_delegations):
+    # 'to' parameter is name of account, but accounts are listing by id involved with account, NOT alphabetically
     with wallet.in_single_transaction():
         wallet.api.create_account('initminer', 'alice', '{}')
         wallet.api.create_account('initminer', 'bob', '{}')
@@ -45,36 +70,8 @@ def test_list_rc_direct_delegations(node, wallet, from_, to, expected_delegation
         wallet.api.delegate_rc('alice', ['bob'], 5)
         wallet.api.delegate_rc('alice', ['carol'], 10)
 
-    delegations = node.api.wallet_bridge.list_rc_direct_delegations([from_, to], 100)
+    delegations = wallet.api.list_rc_direct_delegations([from_, to], 100)
     assert len(delegations) == len(expected_delegations)
 
     for delegation, expected_delegation in zip(delegations, expected_delegations):
         assert delegation == expected_delegation
-
-
-@pytest.mark.parametrize(
-    'from_, to', [
-        ('alice', 'andrew'),
-        ('bob', 'andrew'),
-        ('bob', 'initminer'),
-        ('andrew', 'initminer'),
-        ('bob', 'alice'),
-        ('andrew', 'alice'),
-        ('initminer', 'alice'),
-        ('andrew', 'bob'),
-        ('initminer', 'bob'),
-        ('initminer', 'andrew'),
-    ]
-)
-def test_list_rc_direct_delegations_with_expected_empty_output(node, wallet, from_, to):
-    # 'to' parameter is name of account, but accounts are listing by id involved with account, NOT alphabetically
-    with wallet.in_single_transaction():
-        wallet.api.create_account('initminer', 'alice', '{}')
-        wallet.api.create_account('initminer', 'bob', '{}')
-        wallet.api.create_account('initminer', 'andrew', '{}')
-
-    wallet.api.transfer_to_vesting('initminer', 'alice', Asset.Test(0.1))
-    wallet.api.delegate_rc('alice', ['bob'], 5)
-    delegations = node.api.wallet_bridge.list_rc_direct_delegations([from_, to], 100)
-
-    assert len(delegations) == 0
