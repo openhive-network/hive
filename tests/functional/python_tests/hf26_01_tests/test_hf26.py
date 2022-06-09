c84ad617157c922f0669a7157e442010aea777b4
Mariusz <mtrela@syncad.com>
2022-06-09 15:41:36 +0200

    Added `test_after_hf26_without_majority` test, but an exception is not raised

--- tests/functional/python_tests/hf26_01_tests/test_hf26.py
+++ tests/functional/python_tests/hf26_01_tests/test_hf26.py
@@ -11,13 +11,13 @@ def prepare_wallets(network_number : int, world : World):
     wallet_hf26   = Wallet(attach_to=api_node, additional_arguments=['--transaction-serialization=hf26'])
     return wallet_legacy, wallet_hf26
 
-def test_before_hf26(world_before_hf26):
+def test_before_hf26(world_during_hf25):
     logger.info( "Attaching legacy/hf26 wallets..." )
 
     #for debug purposes
     network_number = 0
 
-    wallet_legacy, wallet_hf26 = prepare_wallets(network_number, world_before_hf26)
+    wallet_legacy, wallet_hf26 = prepare_wallets(network_number, world_during_hf25)
 
     logger.info( "Creating `legacy` operations..." )
     result = wallet_legacy.api.create_account('initminer', 'alice', '{}')
@@ -29,13 +29,13 @@ def test_before_hf26(world_before_hf26):
 
     assert exception.value.response['error']['message'].find('missing required active authority'), "Incorrect error in `hf26` transfer operation"
 
-def test_after_hf26(world_after_hf26):
+def test_after_hf26(world_during_hf26):
     logger.info( "Attaching legacy/hf26 wallets..." )
 
     #for debug purposes
     network_number = 1
 
-    wallet_legacy, wallet_hf26 = prepare_wallets(network_number, world_after_hf26)
+    wallet_legacy, wallet_hf26 = prepare_wallets(network_number, world_during_hf26)
 
     logger.info( "Creating `legacy` operations..." )
     result = wallet_legacy.api.create_account('initminer', 'alice', '{}')
@@ -43,3 +43,21 @@ def test_after_hf26(world_after_hf26):
 
     logger.info( "Creating `hf26` operations..." )
     wallet_hf26.api.transfer('initminer', 'alice', { "amount": "200", "precision": 3, "nai": "@@000000021" }, '', 'true')
+
+def test_after_hf26_without_majority(world_during_hf26_without_majority):
+    logger.info( "Attaching legacy/hf26 wallets..." )
+
+    #for debug purposes
+    network_number = 2
+
+    wallet_legacy, wallet_hf26 = prepare_wallets(network_number, world_during_hf26_without_majority)
+
+    logger.info( "Creating `legacy` operations..." )
+    result = wallet_legacy.api.create_account('initminer', 'alice', '{}')
+    result = wallet_legacy.api.transfer('initminer', 'alice', '10.000 TESTS', '', 'true')
+
+    logger.info( "Creating `hf26` operations..." )
+    with pytest.raises(test_tools.exceptions.CommunicationError) as exception:
+      wallet_hf26.api.transfer('initminer', 'alice', { "amount": "200", "precision": 3, "nai": "@@000000021" }, '', 'true')
+
+    assert exception.value.response['error']['message'].find('missing required active authority'), "Incorrect error in `hf26` transfer operation"
