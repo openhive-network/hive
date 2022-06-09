6b65e417ea6fd5a36a0fdc8da0acbb03763d338b
Michał Kudela <kudmich@syncad.com>
2022-06-09 09:12:22 +0200

    Prepare wallet bridge API conftest to testing remote nodes

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/conftest.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
-from test_tools import Wallet
+from test_tools import RemoteNode, Wallet
+
 
 def pytest_configure(config):
     config.addinivalue_line('markers', 'enabled_plugins: Enabled plugins in node from `node` fixture')
+
 
 @pytest.fixture
 def node(world, request):
@@ -20,3 +22,15 @@ def node(world, request):
 @pytest.fixture
 def wallet(node):
     return Wallet(attach_to=node)
+
+
+@pytest.fixture
+def node5m():
+    return RemoteNode(http_endpoint='http://hive-3.pl.syncad.com:18092')
+
+
+@pytest.fixture
+def node64m():
+    return RemoteNode(http_endpoint='https://api.hive.blog:443')
+    # FIXME uncomment after repair 64m consensus node
+    # return RemoteNode(http_endpoint='http://hive-staging.pl.syncad.com:8090')
