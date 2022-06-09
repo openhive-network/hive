9a8814fe295d766eec2ff1863f4498bb0bf79dd2
Michał Kudela <kudmich@syncad.com>
2022-06-09 09:06:54 +0200

    Add account history API tests

---
+++ tests/api_tests/message_format_tests/account_history_api/test_get_account_history.py
@@ -0,0 +1,69 @@
+import pytest
+
+import test_tools as tt
+
+from ..local_tools import run_for
+
+ACCOUNT = 'initminer'
+
+
+@pytest.mark.parametrize(
+    'start, limit', [
+        (0, 1),
+        (1.1, 1),
+        (1, 1.1),
+        (1000, 1000),
+        (-1, 1000),
+        ('-1', 1000),
+        (-1, '1000'),
+        (True, 1),  # bool is treated like numeric (0:1)
+        (True, True),
+        (None, True),  # none is treated like numeric (0)
+    ]
+)
+@run_for('testnet', 'mainnet_5m', 'mainnet_64m')
+def test_get_account_history_with_correct_values(prepared_node, start, limit):
+    prepared_node.api.account_history.get_account_history(
+        account='initminer',
+        start=start,
+        limit=limit,
+        include_reversible=True,
+    )
+
+
+@pytest.mark.parametrize(
+    'account_name, start, limit', [
+        # Invalid account name
+        (None, 0, 100),
+        ({}, 0, 100),
+        ([ACCOUNT], 0, 100),
+
+        # Invalid start
+        (ACCOUNT, '', 100),
+        (ACCOUNT, 'alice', 100),
+        (ACCOUNT, None, 100),
+        (ACCOUNT, [], 100),
+        (ACCOUNT, {}, 100),
+        (ACCOUNT, '1.1', 100),
+
+        # Invalid limit
+        (ACCOUNT, 1, ''),
+        (ACCOUNT, 1, 'alice'),
+        (ACCOUNT, 1, '1.1'),
+        (ACCOUNT, 0, 0),
+        (ACCOUNT, 1, 100),
+        (ACCOUNT, 1000, 1001),
+        (ACCOUNT, 0, None),
+        (ACCOUNT, 0, [100]),
+        (ACCOUNT, 0, {}),
+    ]
+)
+@run_for('testnet', 'mainnet_5m', 'mainnet_64m')
+def test_get_account_history_with_incorrect_values(prepared_node, account_name, start, limit):
+    with pytest.raises(tt.exceptions.CommunicationError):
+        prepared_node.api.account_history.get_account_history(
+            account=account_name,
+            start=start,
+            limit=limit,
+            include_reversible=True,
+        )
---
+++ tests/api_tests/message_format_tests/account_history_api/test_get_ops_in_block.py
@@ -0,0 +1,73 @@
+import pytest
+
+import test_tools as tt
+
+from ..local_tools import run_for
+
+
+@pytest.mark.parametrize(
+    'block_num, virtual_operation, include_reversible', [
+        # Valid block num
+        (1, True, True),
+        ('1', True, True),
+        (1.1, True, True),
+        (True, True, True),  # block number given as bool is converted to number
+        (None, True, True),  # block number given as None is converted to 0
+        (0, True, True),  # returns an empty response, blocks are numbered from 1
+
+        # Valid virtual operations
+        (1, True, True),  # virtual operations given as bool is converted to number (True:1, False:0)
+        (1, False, True),
+        (1, None, True),  # None is converted to 0
+        (1, 0, True),  # virtual_operation as number is converted to bool
+        (1, 1, True),
+        (1, 2, True),
+
+        # Valid include reversible
+        (1, True, True),
+        (1, True, False),
+        (1, True, 'true'),
+        (1, True, 'false'),
+        (1, True, 0),
+        (1, True, 1),
+    ]
+)
+@run_for('testnet', 'mainnet_5m', 'mainnet_64m')
+def test_get_ops_in_block_with_correct_values(prepared_node, block_num, virtual_operation, include_reversible):
+    prepared_node.api.account_history.get_ops_in_block(
+        block_num=block_num,
+        only_virtual=virtual_operation,
+        include_reversible=include_reversible,
+    )
+
+
+@pytest.mark.parametrize(
+    'block_num, virtual_operation, include_reversible', [
+        # Invalid block number
+        ('incorrect_string_argument', True, True),
+        ('', True, True),
+        ([1], True, True),
+        ({}, True, True),
+
+        # Invalid virtual operation
+        (1, 'incorrect_string_argument', True),
+        (1, [True], True),
+        (1, {}, True),
+        (1, '1', True),
+
+        # Invalid include reversible
+        (1, True, ''),
+        (1, True, 'TRUE'),
+        (1, True, 'FALSE'),
+        (1, True, []),
+        (1, True, {}),
+    ]
+)
+@run_for('testnet', 'mainnet_5m', 'mainnet_64m')
+def test_get_ops_in_block_with_incorrect_type_of_arguments(prepared_node, block_num, virtual_operation, include_reversible):
+    with pytest.raises(tt.exceptions.CommunicationError):
+        prepared_node.api.account_history.get_ops_in_block(
+            block_num=block_num,
+            only_virtual=virtual_operation,
+            include_reversible=include_reversible,
+        )
