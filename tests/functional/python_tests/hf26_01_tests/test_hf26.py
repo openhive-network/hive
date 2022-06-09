cf6ac3082426614741feee14e89b0d65bde17e9b
Mariusz <mtrela@syncad.com>
2022-06-09 12:48:28 +0200

    Added `test_after_hf26`

--- tests/functional/python_tests/hf26_01_tests/test_hf26.py
+++ tests/functional/python_tests/hf26_01_tests/test_hf26.py
@@ -24,3 +24,14 @@ def test_before_hf26(world_before_hf26):
       wallet_hf26.api.transfer('initminer', 'alice', { "amount": "200", "precision": 3, "nai": "@@000000021" }, '', 'true')
 
     assert exception.value.response['error']['message'].find('missing required active authority'), "Incorrect error in `hf26` transfer operation"
+
+def test_after_hf26(world_after_hf26):
+    logger.info( "Attaching legacy/hf26 wallets..." )
+    wallet_legacy, wallet_hf26 = prepare_wallets(world_after_hf26)
+
+    logger.info( "Creating `legacy` operations..." )
+    result = wallet_legacy.api.create_account('initminer', 'alice', '{}')
+    result = wallet_legacy.api.transfer('initminer', 'alice', '10.000 TESTS', '', 'true')
+
+    logger.info( "Creating `hf26` operations..." )
+    wallet_hf26.api.transfer('initminer', 'alice', { "amount": "200", "precision": 3, "nai": "@@000000021" }, '', 'true')
