a23ad51838c5e82e5aba9f06d30982f7201e35f3
Piotr Batko <pbatko@syncad.com>
2022-06-10 11:34:57 +0200

    [USE IT AS FIXUP] Remove redundancy in account creating tests

--- tests/functional/python_tests/cli_wallet_extended_tests/test_account_creating.py
+++ tests/functional/python_tests/cli_wallet_extended_tests/test_account_creating.py
@@ -8,25 +8,26 @@ def test_account_creation(wallet):
     assert wallet.api.list_accounts('a', 1) == ['alice']
 
 
-def test_creation_of_account_with_too_long_name(wallet):
+@pytest.mark.parametrize(
+    'account_name, expected_error_message',
+    [
+        [
+            'too-long-account-name',
+            'Account name \'too-long-account-name\' is too long. Use maximum of 16 characters.',
+        ],
+        [
+            'to',
+            'Account name \'to\' is too short. Use at least 3 characters.',
+        ],
+        [
+            '...',
+            'Account name \'...\' is not valid. Please follow the RFC 1035 rules.',
+        ],
+    ],
+)
+def test_creation_of_account_with_invalid_name(wallet, account_name, expected_error_message):
     with pytest.raises(tt.exceptions.CommunicationError) as exception:
-        wallet.api.create_account('initminer', 'too-long-account-name', '{}')
+        wallet.api.create_account('initminer', account_name, '{}')
 
     error_message = exception.value.response['error']['message']
-    assert 'Account name \'too-long-account-name\' is too long. Use maximum of 16 characters.' in error_message
-
-
-def test_creation_of_account_with_too_short_name(wallet):
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
-        wallet.api.create_account('initminer', 'to', '{}')
-
-    error_message = exception.value.response['error']['message']
-    assert 'Account name \'to\' is too short. Use at least 3 characters.' in error_message
-
-
-def test_creation_of_account_with_invalid_sequence(wallet):
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
-        wallet.api.create_account('initminer', '...', '{}')
-
-    error_message = exception.value.response['error']['message']
-    assert 'Account name \'...\' is not valid. Please follow the RFC 1035 rules.' in error_message
+    assert expected_error_message in error_message
