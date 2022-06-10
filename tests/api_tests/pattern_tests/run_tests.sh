2bc648ede8e803186058f29b8af61f30359d4ddd
Krzysztof Mochocki <kmochocki@syncad.com>
2022-06-10 13:43:50 +0200

    Fix error formatting for pattern tests

--- tests/api_tests/pattern_tests/run_tests.sh
+++ tests/api_tests/pattern_tests/run_tests.sh
@@ -8,4 +8,12 @@ else
     export HIVEMIND_PORT=$1
 fi
 export TAVERN_DIR="$2/tests/api_tests/pattern_tests/"
-tox -e tavern -- --tb=long -W ignore::pytest.PytestDeprecationWarning --workers 5 --tests-per-worker auto -k 'not get_transaction_hex and (get_transaction or get_account_history or enum_virtual_ops or get_ops_in_block)' --junitxml=results.xml
+tox -e tavern --                                 \
+    --tb=line                                    \
+    --workers 5                                  \
+    --cache-clear                                \
+    -p no:logging                                \
+    --show-capture=log                           \
+    --junitxml=results.xml                       \
+    -W ignore::pytest.PytestDeprecationWarning   \
+    --tests-per-worker auto -k 'not get_transaction_hex and (get_transaction or get_account_history or enum_virtual_ops or get_ops_in_block)'
