1fc5deb6851aabc28b44a9af4d8e561786c25d10
Bartek Wrona <wrona@syncad.com>
2022-06-13 23:55:42 +0200

    build_data.sh/build_data4commit.sh scripts allow to pass additional parameters to underlying build_instance.sh script i.e. to allow usage of --export-binaries option

--- scripts/ci-helpers/build_data4commit.sh
+++ scripts/ci-helpers/build_data4commit.sh
@@ -8,13 +8,14 @@ LOG_FILE=build_data4commit.log
 source "$SCRIPTSDIR/common.sh"
 
 COMMIT=${1:?"Missing arg 1 to specify COMMIT"}
-REGISTRY=${2:?"Missing arg #2 to specify target container registry"}
-
+shift
+REGISTRY=${1:?"Missing arg #2 to specify target container registry"}
+shift
 BRANCH="master"
 
 BUILD_IMAGE_TAG=$COMMIT
 
 do_clone "$BRANCH" "./hive-${COMMIT}" https://gitlab.syncad.com/hive/hive.git "$COMMIT"
 
-"$SCRIPTSDIR/ci-helpers/build_data.sh" "$BUILD_IMAGE_TAG" "./hive-${COMMIT}" "$REGISTRY"
+"$SCRIPTSDIR/ci-helpers/build_data.sh" "$BUILD_IMAGE_TAG" "./hive-${COMMIT}" "$REGISTRY" "$@"
 
