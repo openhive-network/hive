44d8a59c8794fbfffac771f85f69ab137615a594
Michał Kudela <kudmich@syncad.com>
2022-06-15 15:23:21 +0200

    Extend tests wallet bridge API, non-arguments methods tested on mainnet

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_get_chain_properties.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_get_chain_properties.py
@@ -1,6 +1,6 @@
 from .local_tools import run_for
 
 
-@run_for('testnet')
+@run_for('testnet', 'mainnet_5m', 'mainnet_64m')
 def test_get_chain_properties(prepared_node):
     prepared_node.api.wallet_bridge.get_chain_properties()
