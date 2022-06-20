eb841615ee5062631ab03eecdbdf725dc72d255e
Michał Kudela <kudmich@syncad.com>
2022-06-20 12:32:49 +0200

    Wallet bridge API 'rc' methods support mainnet

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_list_rc_direct_delegations.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_list_rc_direct_delegations.py
@@ -4,17 +4,21 @@ import test_tools as tt
 
 from .local_tools import as_string, run_for
 
+TESTNET_ACCOUNTS = [f'account-{i}' for i in range(3)]
 
-ACCOUNTS = [f'account-{i}' for i in range(3)]
+MAINNET_ACCOUNTS = [
+    "gtg",
+    "gthibode",
+]
 
 CORRECT_VALUES = [
         # FROM_, TO
-        (ACCOUNTS[0], ACCOUNTS[1], 100),
-        (ACCOUNTS[0], '', 100),
+        (TESTNET_ACCOUNTS[0], TESTNET_ACCOUNTS[1], 100),
+        (TESTNET_ACCOUNTS[0], '', 100),
 
         # LIMIT
-        (ACCOUNTS[0], ACCOUNTS[1], 0),
-        (ACCOUNTS[0], ACCOUNTS[1], 1000),
+        (TESTNET_ACCOUNTS[0], TESTNET_ACCOUNTS[1], 0),
+        (TESTNET_ACCOUNTS[0], TESTNET_ACCOUNTS[1], 1000),
 ]
 
 
@@ -22,15 +26,24 @@ CORRECT_VALUES = [
     'from_, to, limit', [
         *CORRECT_VALUES,
         *as_string(CORRECT_VALUES),
-        (ACCOUNTS[0], ACCOUNTS[1], True),  # bool is treated like numeric (0:1)
+        (TESTNET_ACCOUNTS[0], TESTNET_ACCOUNTS[1], True),  # bool is treated like numeric (0:1)
     ]
 )
 @run_for('testnet')
 def test_list_rc_direct_delegations_with_correct_value_in_testnet(prepared_node, from_, to, limit):
     wallet = tt.Wallet(attach_to=prepared_node)
-    create_accounts_and_delegate_rc_from_account0_to_account1(wallet, accounts=ACCOUNTS)
+    create_accounts_and_delegate_rc_from_account0_to_account1(wallet, accounts=TESTNET_ACCOUNTS)
 
-    prepared_node.api.wallet_bridge.list_rc_direct_delegations([from_, to], limit)
+    result = prepared_node.api.wallet_bridge.list_rc_direct_delegations([from_, to], limit)
+    if len(result) != 0:
+        assert len(result) > 0
+
+
+@pytest.mark.skip(reason='Method will be available after hardfork 1.26')
+@run_for('mainnet_5m', 'mainnet_64m')
+def test_list_rc_direct_delegations_with_correct_value_mainnet(prepared_node):
+    response = prepared_node.api.wallet_bridge.list_rc_direct_delegations([MAINNET_ACCOUNTS[0], MAINNET_ACCOUNTS[1]], 10)
+    assert len(response) > 0
 
 
 @pytest.mark.parametrize(
@@ -41,18 +54,18 @@ def test_list_rc_direct_delegations_with_correct_value_in_testnet(prepared_node,
         ('true', '', 100),
 
         # TO
-        (ACCOUNTS[0], 'true', 100),
-        (ACCOUNTS[0], 'non-exist-acc', 100),
+        (TESTNET_ACCOUNTS[0], 'true', 100),
+        (TESTNET_ACCOUNTS[0], 'non-exist-acc', 100),
 
         # LIMIT
-        (ACCOUNTS[0], ACCOUNTS[1], -1),
-        (ACCOUNTS[0], '', 1001),
+        (TESTNET_ACCOUNTS[0], TESTNET_ACCOUNTS[1], -1),
+        (TESTNET_ACCOUNTS[0], '', 1001),
     ]
 )
 @run_for('testnet')
-def test_list_rc_direct_delegations_with_incorrect_value(prepared_node, from_, to, limit):
+def test_list_rc_direct_delegations_with_incorrect_value_in_testnet(prepared_node, from_, to, limit):
     wallet = tt.Wallet(attach_to=prepared_node)
-    create_accounts_and_delegate_rc_from_account0_to_account1(wallet, accounts=ACCOUNTS)
+    create_accounts_and_delegate_rc_from_account0_to_account1(wallet, accounts=TESTNET_ACCOUNTS)
 
     with pytest.raises(tt.exceptions.CommunicationError):
         prepared_node.api.wallet_bridge.list_rc_direct_delegations([from_, to], limit)
@@ -61,30 +74,76 @@ def test_list_rc_direct_delegations_with_incorrect_value(prepared_node, from_, t
 @pytest.mark.parametrize(
     'from_, to, limit', [
         # FROM_
-        ('100', ACCOUNTS[1], 100),
-        (100, ACCOUNTS[1], 100),
-        (True, ACCOUNTS[1], 100),
-        ('', ACCOUNTS[1], 100),
-        ([ACCOUNTS[0]], ACCOUNTS[1], 100),
+        ('', '', 100),
+        ('non-exist-acc', '', 100),
 
         # TO
-        (ACCOUNTS[0], 100, 100),
-        (ACCOUNTS[0], '100', 100),
-        (ACCOUNTS[0], True, 100),
-        (ACCOUNTS[0], [ACCOUNTS[1]], 100),
+        (MAINNET_ACCOUNTS[0], 'non-exist-acc', 100),
 
         # LIMIT
-        (ACCOUNTS[0], ACCOUNTS[1], 'incorrect_string_argument'),
-        (ACCOUNTS[0], ACCOUNTS[1], 'true'),
-        (ACCOUNTS[0], ACCOUNTS[1], [100]),
+        (MAINNET_ACCOUNTS[0], MAINNET_ACCOUNTS[1], -1),
+        (MAINNET_ACCOUNTS[0], '', 1001),
+    ]
+)
+@run_for('mainnet_5m', 'mainnet_64m')
+def test_list_rc_direct_delegations_with_incorrect_value_in_mainnet(prepared_node, from_, to, limit):
+    with pytest.raises(tt.exceptions.CommunicationError):
+        prepared_node.api.wallet_bridge.list_rc_direct_delegations([from_, to], limit)
+
+
+@pytest.mark.parametrize(
+    'from_, to, limit', [
+        # FROM_
+        ('100', TESTNET_ACCOUNTS[1], 100),
+        (100, TESTNET_ACCOUNTS[1], 100),
+        (True, TESTNET_ACCOUNTS[1], 100),
+        ('', TESTNET_ACCOUNTS[1], 100),
+        ([TESTNET_ACCOUNTS[0]], TESTNET_ACCOUNTS[1], 100),
+
+        # TO
+        (TESTNET_ACCOUNTS[0], 100, 100),
+        (TESTNET_ACCOUNTS[0], '100', 100),
+        (TESTNET_ACCOUNTS[0], True, 100),
+        (TESTNET_ACCOUNTS[0], [TESTNET_ACCOUNTS[1]], 100),
+
+        # LIMIT
+        (TESTNET_ACCOUNTS[0], TESTNET_ACCOUNTS[1], 'incorrect_string_argument'),
+        (TESTNET_ACCOUNTS[0], TESTNET_ACCOUNTS[1], 'true'),
+        (TESTNET_ACCOUNTS[0], TESTNET_ACCOUNTS[1], [100]),
 
     ]
 )
 @run_for('testnet')
-def test_list_rc_direct_delegations_with_incorrect_type_of_arguments(prepared_node, from_, to, limit):
+def test_list_rc_direct_delegations_with_incorrect_type_of_arguments_in_testnet(prepared_node, from_, to, limit):
     wallet = tt.Wallet(attach_to=prepared_node)
-    create_accounts_and_delegate_rc_from_account0_to_account1(wallet, accounts=ACCOUNTS)
+    create_accounts_and_delegate_rc_from_account0_to_account1(wallet, accounts=TESTNET_ACCOUNTS)
 
+    with pytest.raises(tt.exceptions.CommunicationError):
+        prepared_node.api.wallet_bridge.list_rc_direct_delegations([from_, to], limit)
+
+
+@pytest.mark.parametrize(
+    'from_, to, limit', [
+        # FROM_
+        ('100', MAINNET_ACCOUNTS[1], 100),
+        (100, MAINNET_ACCOUNTS[1], 100),
+        ('', MAINNET_ACCOUNTS[1], 100),
+        ([MAINNET_ACCOUNTS[0]], MAINNET_ACCOUNTS[1], 100),
+
+        # TO
+        (MAINNET_ACCOUNTS[0], 100, 100),
+        (MAINNET_ACCOUNTS[0], '100', 100),
+        (MAINNET_ACCOUNTS[0], [MAINNET_ACCOUNTS[1]], 100),
+
+        # LIMIT
+        (MAINNET_ACCOUNTS[0], MAINNET_ACCOUNTS[1], 'incorrect_string_argument'),
+        (MAINNET_ACCOUNTS[0], MAINNET_ACCOUNTS[1], 'true'),
+        (MAINNET_ACCOUNTS[0], MAINNET_ACCOUNTS[1], [100]),
+
+    ]
+)
+@run_for('mainnet_5m', 'mainnet_64m')
+def test_list_rc_direct_delegations_with_incorrect_type_of_arguments_in_mainnet(prepared_node, from_, to, limit):
     with pytest.raises(tt.exceptions.CommunicationError):
         prepared_node.api.wallet_bridge.list_rc_direct_delegations([from_, to], limit)
 
@@ -93,4 +152,3 @@ def create_accounts_and_delegate_rc_from_account0_to_account1(wallet, accounts):
     wallet.create_accounts(len(accounts))
     wallet.api.transfer_to_vesting('initminer', accounts[0], tt.Asset.Test(0.1))
     wallet.api.delegate_rc(accounts[0], [accounts[1]], 5)
-    
