12188676595fe3369c0f5f817bf95855dfbb08b6
Michał Kudela <kudmich@syncad.com>
2022-06-20 12:32:49 +0200

    Wallet bridge API 'rc' methods support mainnet

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_find_rc_accounts.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_find_rc_accounts.py
@@ -3,16 +3,20 @@ import pytest
 import test_tools as tt
 from hive_local_tools import run_for
 from hive_local_tools.api.message_format import as_string
+from hive_local_tools.api.message_format.wallet_bridge_api.constants import ACCOUNTS, MAINNET_ACCOUNT
 
-ACCOUNTS = [f'account-{i}' for i in range(3)]
 
 CORRECT_VALUES = [
+    [ACCOUNTS[0]],
+    [MAINNET_ACCOUNT],
+    ACCOUNTS,
+    [True],  # in mainnet it corresponds to an account named 'true'
+]
+
+VALUES_EXPECT_EMPTY_RESPONSES = [
     [''],
     ['non-exist-acc'],
-    [ACCOUNTS[0]],
-    ACCOUNTS,
-    [100],
-    [True],
+    [100]
 ]
 
 
@@ -22,11 +26,42 @@ CORRECT_VALUES = [
         *as_string(CORRECT_VALUES),
     ]
 )
-@run_for("testnet")
-def test_find_rc_accounts_with_correct_value(node, rc_accounts):
-    wallet = tt.Wallet(attach_to=node)
-    wallet.create_accounts(len(ACCOUNTS))
-    node.api.wallet_bridge.find_rc_accounts(rc_accounts)
+@run_for("testnet", "live_mainnet")
+def test_find_rc_accounts_with_correct_value(node, should_prepare, rc_accounts):
+    if should_prepare:
+        wallet = tt.Wallet(attach_to=node)
+        wallet.create_account("true")
+        wallet.create_accounts(len(ACCOUNTS))
+    found_accounts = node.api.wallet_bridge.find_rc_accounts(rc_accounts)
+    assert len(found_accounts) > 0
+
+
+@pytest.mark.parametrize(
+    'rc_accounts', [
+        *CORRECT_VALUES,
+        *as_string(CORRECT_VALUES),
+    ]
+)
+@run_for("mainnet_5m")
+def test_find_rc_accounts_with_correct_values_and_existing_accounts_in_mainnet_5m(node, should_prepare, rc_accounts):
+    # rc service wasn't available until the HF20. The expected response for 5 million block node is always an empty list
+    found_accounts = node.api.wallet_bridge.find_rc_accounts(rc_accounts)
+    assert found_accounts == []
+
+
+@pytest.mark.parametrize(
+    'rc_accounts', [
+        *VALUES_EXPECT_EMPTY_RESPONSES,
+        *as_string(VALUES_EXPECT_EMPTY_RESPONSES),
+    ]
+)
+@run_for("testnet", "mainnet_5m", "live_mainnet")
+def test_find_rc_accounts_with_correct_values_and_not_existing_accounts(node, should_prepare, rc_accounts):
+    if should_prepare:
+        wallet = tt.Wallet(attach_to=node)
+        wallet.create_accounts(len(ACCOUNTS))
+    rc_account = node.api.wallet_bridge.find_rc_accounts(rc_accounts)
+    assert len(rc_account) == 0
 
 
 @pytest.mark.parametrize(
@@ -38,7 +73,7 @@ def test_find_rc_accounts_with_correct_value(node, rc_accounts):
         'incorrect_string_argument',
     ]
 )
-@run_for("testnet")
+@run_for("testnet", "mainnet_5m", "live_mainnet")
 def test_find_rc_accounts_with_incorrect_type_of_argument(node, rc_accounts):
     with pytest.raises(tt.exceptions.CommunicationError):
         node.api.wallet_bridge.find_rc_accounts(rc_accounts)
