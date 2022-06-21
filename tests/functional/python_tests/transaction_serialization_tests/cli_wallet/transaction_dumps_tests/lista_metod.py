e97aa3183898f1551683079f3a3d555ca2716bc5
Radek Masłowski <rmaslowski@syncad.com>
2022-06-21 15:31:22 +0200

    Dodaj liste metod

---
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_dumps_tests/lista_metod.py
@@ -0,0 +1,64 @@
+'''
+normalize_brain_key--------------- Nie działa
+claim_account_creation--------------- DONE
+claim_account_creation_nonblocking--------------- DONE
+create_account--------------- DONE
+create_account_with_keys--------------- DONE
+create_funded_account_with_keys--------------- DONE
+create_account_delegated--------------- DEPRECATED
+create_account_with_keys_delegated--------------- DEPRECATED
+update_account--------------- DONE
+update_account_auth_key--------------- DONE
+update_account_auth_account-------------- DONE
+update_account_auth_threshold-------------- DONE
+update_account_meta-------------- DONE
+update_account_memo_key-------------- DONE
+delegate_vesting_shares------------- DONE
+delegate_vesting_shares_nonblocking------------- DONE
+delegate_vesting_shares_and_transfer------------- DONE
+delegate_vesting_shares_and_transfer_nonblocking------------- DONE
+get_collateralized_conversion_requests ------------- nic nie zwraca
+update_witness------------- DONE
+set_voting_proxy------------- DONE
+vote_for_witness------------- DONE
+transfer------------- DONE
+escrow_transfer------------- DONE
+escrow_approve------------- DONE
+escrow_dispute------------- DONE
+escrow_release------------- DONE
+transfer_to_vesting------------- DONE
+transfer_nonblocking---------------- DONE
+transfer_and_broadcast HELPER FUNCTION
+transfer_to_vesting_nonblocking---------------- DONE
+transfer_to_vesting_and_broadcast HELPER FUNCTION
+transfer_to_savings---------------- DONE
+transfer_from_savings---------------- DONE
+cancel_transfer_from_savings---------------- DONE
+withdraw_vesting---------------- DONE
+set_withdraw_vesting_route---------------- DONE
+convert_hbd ---------------- DONE
+convert_hive_with_collateral ---------------- DONE
+estimate_hive_collateral--------------- Nie działa
+publish_feed---------------- DONE
+sign_transaction--------------- DONE
+get_prototype_operation--------------- NIE DZIAŁA
+create_order---------------- DONE
+cancel_order---------------- DONE
+post_comment---------------- DONE
+vote---------------- DONE
+set_transaction_expiration--------------- NIE DZIAŁA
+request_account_recovery---------------- DONE
+recover_account---------------- DONE
+change_recovery_account---------------- DONE
+get_account_history------------- NIE DZIAŁA
+follow--------------- zwraca custom json operation
+decline_voting_rights---------------- DONE
+claim_reward_balance------------- NIE testowalne
+create_proposal--------------- DONE
+update_proposal-------------- DONE
+update_proposal_votes--------------- DONE
+remove_proposal--------------- DONE
+recurrent_transfer--------------- DONE
+find_recurrent_transfers--------------- nie jest transackją
+delegate_rc--------------- wyrzuca custom json operations
+'''
