bc924d000c48012c63f31f75b159d48271a55c93
Radek Masłowski <rmaslowski@syncad.com>
2022-06-23 14:47:10 +0200

    Introduce running tests with marks

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/test_asset_serialization_in_methods_parameters.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/test_asset_serialization_in_methods_parameters.py
@@ -1,9 +1,12 @@
+import pytest
+
 import test_tools as tt
 
 from .....local_tools import create_account_and_fund_it, date_from_now
 from .local_tools import run_for_all_cases
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_amount=tt.Asset.Test(100))
 def test_transfer_of_hives(prepared_wallet, transfer_amount):
     prepared_wallet.api.create_account('initminer', 'alice', '{}')
@@ -11,6 +14,7 @@ def test_transfer_of_hives(prepared_wallet, transfer_amount):
     prepared_wallet.api.transfer('initminer', 'alice', transfer_amount, 'memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_amount=tt.Asset.Tbd(100))
 def test_transfer_of_hbds(prepared_wallet, transfer_amount):
     prepared_wallet.api.create_account('initminer', 'alice', '{}')
@@ -18,6 +22,7 @@ def test_transfer_of_hbds(prepared_wallet, transfer_amount):
     prepared_wallet.api.transfer('initminer', 'alice', transfer_amount, 'memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_amount=tt.Asset.Test(100))
 def test_transfer_nonblocking_of_hives(prepared_wallet, transfer_amount):
     prepared_wallet.api.create_account('initminer', 'alice', '{}')
@@ -25,6 +30,7 @@ def test_transfer_nonblocking_of_hives(prepared_wallet, transfer_amount):
     prepared_wallet.api.transfer_nonblocking('initminer', 'alice', transfer_amount, 'memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_amount=tt.Asset.Tbd(100))
 def test_transfer_nonblocking_of_hbds(prepared_wallet, transfer_amount):
     prepared_wallet.api.create_account('initminer', 'alice', '{}')
@@ -32,6 +38,7 @@ def test_transfer_nonblocking_of_hbds(prepared_wallet, transfer_amount):
     prepared_wallet.api.transfer_nonblocking('initminer', 'alice', transfer_amount, 'memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(escrow_tbd_amount=tt.Asset.Tbd(1), escrow_tests_amount=tt.Asset.Test(2))
 def test_escrow_transfer(prepared_wallet, escrow_tbd_amount, escrow_tests_amount):
     with prepared_wallet.in_single_transaction():
@@ -42,6 +49,7 @@ def test_escrow_transfer(prepared_wallet, escrow_tbd_amount, escrow_tests_amount
                                         escrow_tbd_amount, date_from_now(weeks=16), date_from_now(weeks=20), '{}')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(escrow_tbd_amount=tt.Asset.Tbd(1), escrow_tests_amount=tt.Asset.Test(2))
 def test_escrow_release(prepared_wallet, escrow_tbd_amount, escrow_tests_amount):
 
@@ -61,18 +69,21 @@ def test_escrow_release(prepared_wallet, escrow_tbd_amount, escrow_tests_amount)
                                        escrow_tests_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(claim_test_amount=tt.Asset.Test(0))
 def test_claim_account_creation(node, prepared_wallet, claim_test_amount):
     node.wait_number_of_blocks(18)
     prepared_wallet.api.claim_account_creation('initminer', claim_test_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(claim_test_amount=tt.Asset.Test(0))
 def test_claim_account_creation_nonblocking(node, prepared_wallet, claim_test_amount):
     node.wait_number_of_blocks(18)
     prepared_wallet.api.claim_account_creation_nonblocking('initminer', claim_test_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(create_account_test_amount=tt.Asset.Test(2))
 def test_create_funded_account_with_keys(node, prepared_wallet, create_account_test_amount):
     key = 'TST8grZpsMPnH7sxbMVZHWEu1D26F3GwLW1fYnZEuwzT4Rtd57AER'
@@ -80,6 +91,7 @@ def test_create_funded_account_with_keys(node, prepared_wallet, create_account_t
                                                         key, key, key, key)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(delegate_vest_amount=tt.Asset.Vest(1))
 def test_delegate_vesting_shares(node, prepared_wallet, delegate_vest_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', vests=tt.Asset.Test(50))
@@ -88,6 +100,7 @@ def test_delegate_vesting_shares(node, prepared_wallet, delegate_vest_amount):
     prepared_wallet.api.delegate_vesting_shares('alice', 'bob', delegate_vest_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(delegate_vest_amount=tt.Asset.Vest(1))
 def test_delegate_vesting_shares_nonblocking(node, prepared_wallet, delegate_vest_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', vests=tt.Asset.Test(50))
@@ -96,6 +109,7 @@ def test_delegate_vesting_shares_nonblocking(node, prepared_wallet, delegate_ves
     prepared_wallet.api.delegate_vesting_shares_nonblocking('alice', 'bob', delegate_vest_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(delegate_vest_amount=tt.Asset.Vest(1), transfer_test_amount=tt.Asset.Test(6))
 def test_delegate_vesting_shares_and_transfer(node, prepared_wallet, delegate_vest_amount, transfer_test_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', tests=tt.Asset.Test(50), vests=tt.Asset.Test(50))
@@ -105,6 +119,7 @@ def test_delegate_vesting_shares_and_transfer(node, prepared_wallet, delegate_ve
                                                              'transfer_memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(delegate_vest_amount=tt.Asset.Vest(1), transfer_test_amount=tt.Asset.Test(6))
 def test_delegate_vesting_shares_and_transfer_nonblocking(node, prepared_wallet, delegate_vest_amount,
                                                                   transfer_test_amount):
@@ -115,6 +130,7 @@ def test_delegate_vesting_shares_and_transfer_nonblocking(node, prepared_wallet,
                                                                          transfer_test_amount, 'transfer_memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_test_amount=tt.Asset.Test(100))
 def test_transfer_to_vesting(prepared_wallet, transfer_test_amount):
     prepared_wallet.api.create_account('initminer', 'alice', '{}')
@@ -122,6 +138,7 @@ def test_transfer_to_vesting(prepared_wallet, transfer_test_amount):
     prepared_wallet.api.transfer_to_vesting('initminer', 'alice', transfer_test_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_test_amount=tt.Asset.Test(100))
 def test_transfer_to_vesting_nonblocking(prepared_wallet, transfer_test_amount):
     prepared_wallet.api.create_account('initminer', 'alice', '{}')
@@ -129,6 +146,7 @@ def test_transfer_to_vesting_nonblocking(prepared_wallet, transfer_test_amount):
     prepared_wallet.api.transfer_to_vesting_nonblocking('initminer', 'alice', transfer_test_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_test_amount=tt.Asset.Test(100))
 def test_transfer_to_savings_tests(prepared_wallet, transfer_test_amount):
     prepared_wallet.api.create_account('initminer', 'alice', '{}')
@@ -136,6 +154,7 @@ def test_transfer_to_savings_tests(prepared_wallet, transfer_test_amount):
     prepared_wallet.api.transfer_to_savings('initminer', 'alice', transfer_test_amount, 'memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_tbd_amount=tt.Asset.Tbd(100))
 def test_transfer_to_savings_tbds(prepared_wallet, transfer_tbd_amount):
     prepared_wallet.api.create_account('initminer', 'alice', '{}')
@@ -143,6 +162,7 @@ def test_transfer_to_savings_tbds(prepared_wallet, transfer_tbd_amount):
     prepared_wallet.api.transfer_to_savings('initminer', 'alice', transfer_tbd_amount, 'memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_test_amount=tt.Asset.Test(1))
 def test_transfer_from_savings_tests(prepared_wallet, transfer_test_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', vests=tt.Asset.Test(10))
@@ -155,6 +175,7 @@ def test_transfer_from_savings_tests(prepared_wallet, transfer_test_amount):
     prepared_wallet.api.transfer_from_savings('alice', 1000, 'bob', transfer_test_amount, 'memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_tbd_amount=tt.Asset.Tbd(1))
 def test_transfer_from_savings_tbds(prepared_wallet, transfer_tbd_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', vests=tt.Asset.Test(10))
@@ -167,6 +188,7 @@ def test_transfer_from_savings_tbds(prepared_wallet, transfer_tbd_amount):
     prepared_wallet.api.transfer_from_savings('alice', 1000, 'bob', transfer_tbd_amount, 'memo')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(withdraw_vests_amount=tt.Asset.Vest(10))
 def test_withdraw_vesting(prepared_wallet, withdraw_vests_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', vests=tt.Asset.Test(500000))
@@ -174,6 +196,7 @@ def test_withdraw_vesting(prepared_wallet, withdraw_vests_amount):
     prepared_wallet.api.withdraw_vesting('alice', withdraw_vests_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(convert_test_amount=tt.Asset.Test(100))
 def test_convert_hive_with_collateral(prepared_wallet, convert_test_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', tests=tt.Asset.Test(100), vests=tt.Asset.Test(100))
@@ -181,6 +204,7 @@ def test_convert_hive_with_collateral(prepared_wallet, convert_test_amount):
     prepared_wallet.api.convert_hive_with_collateral('alice', convert_test_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(convert_tbd_amount=tt.Asset.Tbd(10))
 def test_convert_hbd(prepared_wallet, convert_tbd_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', tests=tt.Asset.Test(100), vests=tt.Asset.Test(100))
@@ -189,12 +213,14 @@ def test_convert_hbd(prepared_wallet, convert_tbd_amount):
     prepared_wallet.api.convert_hbd('alice', convert_tbd_amount)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(estimate_hive_amount=tt.Asset.Test(100))
 def test_estimate_hive_collateral(prepared_wallet, estimate_hive_amount):
     prepared_wallet.api.estimate_hive_collateral(estimate_hive_amount)
 
 
 
+@pytest.mark.testnet
 @run_for_all_cases(create_order_tests_amount=tt.Asset.Test(100), create_order_tbds_amount=tt.Asset.Tbd(100))
 def test_create_order(prepared_wallet, create_order_tests_amount, create_order_tbds_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', tests=tt.Asset.Test(100), vests=tt.Asset.Test(100))
@@ -202,6 +228,7 @@ def test_create_order(prepared_wallet, create_order_tests_amount, create_order_t
     prepared_wallet.api.create_order('alice', 1000, create_order_tests_amount, create_order_tbds_amount, False, 1000)
 
 
+@pytest.mark.testnet
 @run_for_all_cases(create_proposal_tbds_amount=tt.Asset.Tbd(1000000))
 def test_create_proposal(prepared_wallet, create_proposal_tbds_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', tbds=tt.Asset.Tbd(1000000), vests=tt.Asset.Test(1000000))
@@ -212,6 +239,7 @@ def test_create_proposal(prepared_wallet, create_proposal_tbds_amount):
                                         create_proposal_tbds_amount, 'subject-1', 'permlink')
 
 
+@pytest.mark.testnet
 @run_for_all_cases(update_proposal_tbd_amount=tt.Asset.Tbd(10))
 def test_update_proposal(prepared_wallet, update_proposal_tbd_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', tbds=tt.Asset.Tbd(1000000), vests=tt.Asset.Test(1000000))
@@ -225,6 +253,7 @@ def test_update_proposal(prepared_wallet, update_proposal_tbd_amount):
                                         date_from_now(weeks=19))
 
 
+@pytest.mark.testnet
 @run_for_all_cases(transfer_test_amount=tt.Asset.Test(1000000))
 def test_recurrent_transfer_matched(prepared_wallet, transfer_test_amount):
     create_account_and_fund_it(prepared_wallet, 'alice', tests=tt.Asset.Test(1000000), vests=tt.Asset.Test(1000000))
