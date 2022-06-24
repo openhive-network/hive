478fc2afec4e0208ceae7c3bd7d2a21b9bb9ad5e
Michał Kudela <kudmich@syncad.com>
2022-06-24 11:50:42 +0200

    Extend tests wallet bridge API 'witness' methods to mainnet

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_get_active_witnesses.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_get_active_witnesses.py
@@ -1,6 +1,7 @@
 from .local_tools import run_for
 
 
-@run_for('testnet_replayed')
+@run_for('testnet_replayed', 'mainnet_5m', 'mainnet_64m')
 def test_get_active_witnesses_with_correct_value(prepared_node):
-    prepared_node.api.wallet_bridge.get_active_witnesses()
+    witnesses = prepared_node.api.wallet_bridge.get_active_witnesses()['witnesses']
+    assert len(witnesses) == 21
