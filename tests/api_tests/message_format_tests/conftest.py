738e8fc993c36461aa1227e07ee40cc24a91bde4
Michał Kudela <kudmich@syncad.com>
2022-06-24 10:27:22 +0200

    Optionally add witnesses to `node-config` on node startup

--- tests/api_tests/message_format_tests/conftest.py
+++ tests/api_tests/message_format_tests/conftest.py
@@ -15,6 +15,34 @@ def pytest_sessionstart() -> None:
 def prepared_node(request):
     def __create_init_node():
         init_node = tt.InitNode()
+
+        if request.node.get_closest_marker('prepare_witnesses_in_node_config'):
+            witnesses_names = request.node.get_closest_marker('prepare_witnesses_in_node_config').args[0]
+            for name in witnesses_names:
+                witness = tt.Account(name)
+                init_node.config.witness.append(witness.name)
+                init_node.config.private_key.append(witness.private_key)
+
+            init_node.run()
+            wallet = tt.Wallet(attach_to=init_node)
+
+            with wallet.in_single_transaction():
+                for name in witnesses_names:
+                    wallet.api.create_account('initminer', name, '')
+
+            with wallet.in_single_transaction():
+                for name in witnesses_names:
+                    wallet.api.transfer_to_vesting("initminer", name, tt.Asset.Test(1000))
+
+            with wallet.in_single_transaction():
+                for name in witnesses_names:
+                    wallet.api.update_witness(
+                        name, "https://" + name,
+                        tt.Account(name).public_key,
+                        {"account_creation_fee": tt.Asset.Test(3), "maximum_block_size": 65536, "sbd_interest_rate": 0}
+                    )
+            return init_node
+
         init_node.run()
         return init_node
 
