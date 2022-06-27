1f513a3f263343fb3d40256627ea060765f951ce
Radek Masłowski <rmaslowski@syncad.com>
2022-06-27 13:58:58 +0200

    Add test to sign transaction with wallet with other type of serialization
    Testing are legacy format transaction with wallet with enabled nai serialization and nai format transaction with wallet with enabled legacy serialization

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/test_sign_transaction_with_other_serialization_type_wallet.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/test_sign_transaction_with_other_serialization_type_wallet.py
@@ -19,3 +19,15 @@ def wallet(request):
 def test_sign_transaction_with_matching_wallet(wallet):
     transaction = wallet.api.create_account('initminer', 'alice', '{}', broadcast=False)
     wallet.api.sign_transaction(transaction)
+
+
+def test_sign_legacy_transaction_with_nai_wallet(wallet_with_legacy_serialization, wallet_with_nai_serialization):
+    legacy_transaction = wallet_with_legacy_serialization.api.create_account('initminer', 'alice', '{}', broadcast=False)
+    with pytest.raises(tt.exceptions.CommunicationError):
+        wallet_with_nai_serialization.api.sign_transaction(legacy_transaction)
+
+
+def test_sign_nai_transaction_with_legacy_wallet(wallet_with_legacy_serialization, wallet_with_nai_serialization):
+    nai_transaction = wallet_with_nai_serialization.api.create_account('initminer', 'alice', '{}', broadcast=False)
+    with pytest.raises(tt.exceptions.CommunicationError):
+        wallet_with_legacy_serialization.api.sign_transaction(nai_transaction)
