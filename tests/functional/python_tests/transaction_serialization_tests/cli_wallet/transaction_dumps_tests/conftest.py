839cce64b297646e0a34d5b77b3761ee1e7b2a20
Radek Masłowski <rmaslowski@syncad.com>
2022-07-01 12:22:15 +0200

    Make create function to comparition of transactions

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_dumps_tests/conftest.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_dumps_tests/conftest.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 import pytest
 import shutil
@@ -12,32 +13,60 @@ def node():
     return init_node
 
 
-@pytest.fixture
-def legacy_wallet(node, request):
+# @pytest.fixture
+# def legacy_wallet(node, request):
+#     wallet = tt.Wallet(attach_to=node, additional_arguments=[f'--store-transaction={request.fspath.purebasename}',
+#                                                              '--transaction-serialization=legacy'])
+#     yield wallet
+#
+#     for file_extension in ['json', 'bin']:
+#         source_path_json_file = wallet.directory / f'{request.fspath.purebasename}.{file_extension}'
+#         target_path_json_file = Path(
+#             request.fspath.dirname) / f'dumped_{file_extension}_files_legacy_wallet' / f'{request.fspath.purebasename}.json'
+#         shutil.move(source_path_json_file, target_path_json_file)
+#
+#
+# @pytest.fixture
+# def nai_wallet(node, request):
+#     wallet = tt.Wallet(attach_to=node, additional_arguments=[f'--store-transaction={request.fspath.purebasename}',
+#                                                              '--transaction-serialization=hf26'])
+#     yield wallet
+#
+#     for file_extension in ['json', 'bin']:
+#         source_path_json_file = wallet.directory / f'{request.fspath.purebasename}.{file_extension}'
+#         target_path_json_file = Path(
+#             request.fspath.dirname) / f'dumped_{file_extension}_files_nai_wallet' / f'{request.fspath.purebasename}.json'
+#         shutil.move(source_path_json_file, target_path_json_file)
+#
+#
+# @pytest.fixture(params=['legacy_wallet', 'nai_wallet'])
+# def wallet(request):
+#     return request.getfixturevalue(request.param)
+
+
+@pytest.fixture(params=['legacy', 'hf26'])
+def wallet(node, request):
     wallet = tt.Wallet(attach_to=node, additional_arguments=[f'--store-transaction={request.fspath.purebasename}',
-                                                             '--transaction-serialization=legacy'])
+                                                             f'--transaction-serialization={request.param}'])
+
     yield wallet
 
     for file_extension in ['json', 'bin']:
-        source_path_json_file = wallet.directory / f'{request.fspath.purebasename}.{file_extension}'
-        target_path_json_file = Path(
-            request.fspath.dirname) / f'dumped_{file_extension}_files_legacy_wallet' / f'{request.fspath.purebasename}.json'
-        shutil.move(source_path_json_file, target_path_json_file)
+        source_path_file = wallet.directory / f'{request.fspath.purebasename}.{file_extension}'
+        target_path_file = Path(
+            request.fspath.dirname) / f'dumped_{file_extension}_files_{request.param}_wallet' / f'{request.fspath.purebasename}.{file_extension}'
 
 
-@pytest.fixture
-def nai_wallet(node, request):
-    wallet = tt.Wallet(attach_to=node, additional_arguments=[f'--store-transaction={request.fspath.purebasename}',
-                                                             '--transaction-serialization=hf26'])
-    yield wallet
+    # # Compare actual transaction with stored transaction
+    #     stored_transaction = read_from_file(target_path_file)
+    #     actual_transaction = read_from_file(source_path_file)
+    #     assert stored_transaction == actual_transaction
 
-    for file_extension in ['json', 'bin']:
-        source_path_json_file = wallet.directory / f'{request.fspath.purebasename}.{file_extension}'
-        target_path_json_file = Path(
-            request.fspath.dirname) / f'dumped_{file_extension}_files_nai_wallet' / f'{request.fspath.purebasename}.json'
-        shutil.move(source_path_json_file, target_path_json_file)
+    # Store transaction dumps in folder
+        shutil.move(source_path_file, target_path_file)
 
 
-@pytest.fixture(params=['legacy_wallet', 'nai_wallet'])
-def wallet(request):
-    return request.getfixturevalue(request.param)
+def read_from_file(path):
+    with open(path, 'r') as file:
+        return json.load(file)
+
