4a2bd3d5c72bdacc18f89cac4b9c9e2cdc5654fb
Radek Masłowski <rmaslowski@syncad.com>
2022-07-05 11:14:16 +0200

    Make working cancel_order with faketime

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_dumps_tests/test_cancel_order.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_dumps_tests/test_cancel_order.py
@@ -9,8 +9,11 @@ from ......local_tools import create_account_and_fund_it
 def test_cancel_order(node, wallet):
     create_account_and_fund_it(wallet, 'alice', tests=tt.Asset.Test(100), vests=tt.Asset.Test(100))
     wallet.api.create_order('alice', 1, tt.Asset.Test(1), tt.Asset.Tbd(1), False, 1000)
+    node.wait_number_of_blocks(21)
     wallet.close()
     node.close()
-    node.run(time_offset='@2022-07-08 08:00:00')
+    node.run(time_offset='+5s')
     wallet.run(timeout=10)
     wallet.api.cancel_order('alice', 1)
+    # node.run(time_offset='@2022-07-08 08:00:00')
+    
