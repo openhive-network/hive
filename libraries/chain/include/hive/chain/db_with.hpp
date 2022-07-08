3f52c30c59b658934d6b11ee29586264425d8174
Eric Frias <efrias@syncad.com>
2022-07-08 21:56:53 +0000

    Add a single mutex to full_transaction to allow the worker threads to safely precompute info needed for validation

--- libraries/chain/include/hive/chain/db_with.hpp
+++ libraries/chain/include/hive/chain/db_with.hpp
@@ -75,7 +75,7 @@ struct pending_transactions_restorer
     auto head_block_time = _db.head_block_time();
     _db._pending_tx.reserve( _db._popped_tx.size() + _pending_transactions.size() );
 
-#ifndef IS_TEST_NET //especially during debugging that limit is highly problematic
+#if !defined(IS_TEST_NET) || defined NDEBUG //especially during debugging that limit is highly problematic
     auto start = fc::time_point::now();
 #endif
     bool apply_trxs = true;
