a102b0d9f2a1f55c812b17fdb694dbadf0f8552f
Eric Frias <efrias@syncad.com>
2022-07-16 21:22:02 +0000

    Fix tests

--- tests/unit/tests/operation_tests.cpp
+++ tests/unit/tests/operation_tests.cpp
@@ -10476,7 +10476,7 @@ BOOST_AUTO_TEST_CASE( account_witness_block_approve_authorities )
       tx.operations.push_back( op );
 
       BOOST_TEST_MESSAGE( "--- Test failure when no account is not a witness" );
-      HIVE_REQUIRE_THROW( db->push_transaction( tx, 0 ), tx_missing_witness_auth );
+      HIVE_REQUIRE_THROW( push_transaction( tx, 0 ), tx_missing_witness_auth );
     }
 
     witness_block_approve_operation op;
@@ -10488,28 +10488,28 @@ BOOST_AUTO_TEST_CASE( account_witness_block_approve_authorities )
     tx.operations.push_back( op );
 
     BOOST_TEST_MESSAGE( "--- Test failure when no signatures" );
-    HIVE_REQUIRE_THROW( db->push_transaction( tx, 0 ), tx_missing_witness_auth );
+    HIVE_REQUIRE_THROW( push_transaction( tx, 0 ), tx_missing_witness_auth );
 
     BOOST_TEST_MESSAGE( "--- Test failure when signed by a signature not in the account's authority" );
     sign( tx, bob_post_key );
-    HIVE_REQUIRE_THROW( db->push_transaction( tx, 0 ), tx_missing_witness_auth );
+    HIVE_REQUIRE_THROW( push_transaction( tx, 0 ), tx_missing_witness_auth );
 
     BOOST_TEST_MESSAGE( "--- Test failure when duplicate signatures" );
     tx.signatures.clear();
     sign( tx, alice_witness_key );
     sign( tx, alice_witness_key );
-    HIVE_REQUIRE_THROW( db->push_transaction( tx, 0 ), tx_duplicate_sig );
+    HIVE_REQUIRE_THROW( push_transaction( tx, 0 ), tx_duplicate_sig );
 
     BOOST_TEST_MESSAGE( "--- Test failure when signed by an additional signature not in the creator's authority" );
     tx.signatures.clear();
     sign( tx, alice_witness_key );
     sign( tx, alice_private_key );
-    HIVE_REQUIRE_THROW( db->push_transaction( tx, 0 ), tx_irrelevant_sig );
+    HIVE_REQUIRE_THROW( push_transaction( tx, 0 ), tx_irrelevant_sig );
 
     BOOST_TEST_MESSAGE( "--- Test success with witness signature" );
     tx.signatures.clear();
     sign( tx, alice_witness_key );
-    db->push_transaction( tx, 0 );
+    push_transaction( tx, 0 );
 
     validate_database();
   }
