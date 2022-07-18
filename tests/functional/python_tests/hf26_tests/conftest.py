6477c475e11e461fdf9d1b0b7e61fe2fb257c961
Piotr Batko <pbatko@syncad.com>
2022-07-18 15:48:31 +0200

    Refactor HF26 tests
    - sort imports
    - short too long lines
    - remove spaces
      - from default parameters: a = 1 -> a=1
      - from method calls, within parentheses: foo( 'a' ) -> foo('a')
    - replace "strings" with 'strings' (single quotes instead of double)
    - separate tests with 2 empty lines

--- tests/functional/python_tests/hf26_tests/conftest.py
+++ tests/functional/python_tests/hf26_tests/conftest.py
@@ -1,16 +1,11 @@
-
-from typing import Dict, List, Optional
-
-from datetime import datetime, timezone
+from datetime import timezone
+from typing import Dict, Optional
 
 import pytest
 
 import test_tools as tt
 
-from ..local_tools import parse_datetime
-
-from ..local_tools import init_network
-
+from ..local_tools import init_network, parse_datetime
 from ....local_tools import date_from_now
 
 
@@ -21,22 +16,27 @@ def node_hf25() -> tt.InitNode:
     node.run(environment_variables={"HIVE_HF26_TIME": str(date_as_seconds)})
     return node
 
+
 @pytest.fixture
 def node_hf26() -> tt.InitNode:
     node = tt.InitNode()
     node.run()
     return node
 
+
 @pytest.fixture
 def wallet_hf25(node_hf25) -> tt.Wallet:
     return tt.Wallet(attach_to=node_hf25)
+
 
 @pytest.fixture
 def wallet_hf26(node_hf26) -> tt.Wallet:
     return tt.Wallet(attach_to=node_hf26)
 
 
-def prepare_network(witnesses_number: int, network_name : str, allow_create_init_node : bool, allow_create_api_node : bool):
+def prepare_network(
+    witnesses_number: int, network_name: str, allow_create_init_node: bool, allow_create_api_node: bool
+):
     tt.logger.info(f'Prototypes of nodes(init, witness, api) are created...')
 
     witness_names = [f'wit{i}-{network_name}' for i in range(witnesses_number)]
@@ -47,7 +47,7 @@ def prepare_network(witnesses_number: int, network_name : str, allow_create_init
     if allow_create_init_node:
         init_node = tt.InitNode(network=network)
 
-    tt.WitnessNode(witnesses = witness_names, network=network)
+    tt.WitnessNode(witnesses=witness_names, network=network)
 
     api_node = None
     if allow_create_api_node:
@@ -55,8 +55,11 @@ def prepare_network(witnesses_number: int, network_name : str, allow_create_init
 
     return witness_names, network, init_node, api_node
 
+
 def prepare_environment(hard_fork_26_time):
-    all_witness_names, alpha_net, init_node, api_node = prepare_network(witnesses_number=20, network_name='alpha', allow_create_init_node=True, allow_create_api_node=True)
+    all_witness_names, alpha_net, init_node, api_node = prepare_network(
+        witnesses_number=20, network_name='alpha', allow_create_init_node=True, allow_create_api_node=True
+    )
 
     # Run
     tt.logger.info('Running networks, waiting for live...')
@@ -68,10 +71,15 @@ def prepare_environment(hard_fork_26_time):
 
     return alpha_net
 
+
 def prepare_environment_with_2_sub_networks(hard_fork_26_time_alpha, hard_fork_26_time_beta):
     #Because HIVE_HARDFORK_REQUIRED_WITNESSES = 17 // 17 of the 21 dpos witnesses (20 elected and 1 chosen) required for hardfork
-    witness_names_alpha, alpha_net, init_node, api_node = prepare_network(witnesses_number=8, network_name='alpha', allow_create_init_node=True, allow_create_api_node=True)
-    witness_names_beta, beta_net, init_node2, api_node2 = prepare_network(witnesses_number=12, network_name='beta', allow_create_init_node=False, allow_create_api_node=False)
+    witness_names_alpha, alpha_net, init_node, api_node = prepare_network(
+        witnesses_number=8, network_name='alpha', allow_create_init_node=True, allow_create_api_node=True
+    )
+    witness_names_beta, beta_net, init_node2, api_node2 = prepare_network(
+        witnesses_number=12, network_name='beta', allow_create_init_node=False, allow_create_api_node=False
+    )
 
     all_witness_names = witness_names_alpha + witness_names_beta
 
@@ -86,18 +94,20 @@ def prepare_environment_with_2_sub_networks(hard_fork_26_time_alpha, hard_fork_2
     date_as_seconds = calculate_epoch_time(hard_fork_26_time_beta)
     environment_variables_beta: Optional[Dict] = {'HIVE_HF26_TIME': f'{date_as_seconds}'}
 
-    #June 20, 2032 9:45:38 AM
+    # June 20, 2032 9:45:38 AM
     alpha_net.run(environment_variables_alpha)
 
-    #June 1, 2022 7:41:41 AM
+    # June 1, 2022 7:41:41 AM
     beta_net.run(environment_variables_beta)
 
     init_network(init_node, all_witness_names)
 
     return alpha_net, beta_net
 
+
 def calculate_epoch_time(date):
     return int(parse_datetime(date).replace(tzinfo=timezone.utc).timestamp())
+
 
 @pytest.fixture
 def network_before_hf26():
@@ -109,6 +119,7 @@ def network_before_hf26():
         'alpha': alpha_net
     }
 
+
 @pytest.fixture
 def network_after_hf26():
     tt.logger.info('Preparing fixture network_after_hf26')
@@ -118,6 +129,7 @@ def network_after_hf26():
     yield {
         'alpha': alpha_net
     }
+
 
 @pytest.fixture
 def network_after_hf26_without_majority():
--- tests/functional/python_tests/hf26_tests/test_hf26.py
+++ tests/functional/python_tests/hf26_tests/test_hf26.py
@@ -5,14 +5,17 @@ import subprocess
 
 import test_tools as tt
 
-def prepare_wallets(prepared_networks):
-    tt.logger.info( "Attaching legacy/hf26 wallets..." )
 
-    api_node      = prepared_networks['alpha'].node('ApiNode0')
+def prepare_wallets(prepared_networks):
+    tt.logger.info('Attaching legacy/hf26 wallets...')
+
+    api_node = prepared_networks['alpha'].node('ApiNode0')
 
     wallet_legacy = tt.Wallet(attach_to=api_node, additional_arguments=['--transaction-serialization=legacy'])
-    wallet_hf26   = tt.Wallet(attach_to=api_node, additional_arguments=['--transaction-serialization=hf26'])
+    wallet_hf26 = tt.Wallet(attach_to=api_node, additional_arguments=['--transaction-serialization=hf26'])
+
     return wallet_legacy, wallet_hf26
+
 
 def sign_transaction(transaction: dict, private_key: str, serialization_type: str = ''):
     # run sign_transaction executable file
@@ -25,22 +28,26 @@ def sign_transaction(transaction: dict, private_key: str, serialization_type: st
                                      input=input.encode(encoding='utf-8')).decode('utf-8')
     return json.loads(output)['result']
 
+
 def legacy_operation_passed(wallet):
-    tt.logger.info( "Creating `legacy` operations (pass expected)..." )
+    tt.logger.info('Creating `legacy` operations (pass expected)...')
     wallet.api.create_account('initminer', 'alice', '{}')
     wallet.api.transfer('initminer', 'alice', str(tt.Asset.Test(10123)), 'memo')
 
+
 def hf26_operation_passed(wallet):
-    tt.logger.info( "Creating `hf26` operations (pass expected)..." )
+    tt.logger.info('Creating `hf26` operations (pass expected)...')
     wallet.api.transfer('initminer', 'alice', tt.Asset.Test(199).as_nai(), 'memo')
 
+
 def hf26_operation_failed(wallet):
-    tt.logger.info( "Creating `hf26` operations (fail expected)..." )
+    tt.logger.info('Creating `hf26` operations (fail expected)...')
 
     with pytest.raises(tt.exceptions.CommunicationError) as exception:
-      wallet.api.transfer('initminer', 'alice', tt.Asset.Test(200).as_nai(), 'memo')
+        wallet.api.transfer('initminer', 'alice', tt.Asset.Test(200).as_nai(), 'memo')
 
-    assert 'missing required active authority' in exception.value.response['error']['message'], "Incorrect error in `hf26` transfer operation"
+    assert 'missing required active authority' in exception.value.response['error']['message'], 'Incorrect error in `hf26` transfer operation'
+
 
 def test_before_hf26(network_before_hf26):
     wallet_legacy, wallet_hf26 = prepare_wallets(network_before_hf26)
@@ -48,11 +55,13 @@ def test_before_hf26(network_before_hf26):
     legacy_operation_passed(wallet_legacy)
     hf26_operation_failed(wallet_hf26)
 
+
 def test_after_hf26(network_after_hf26):
     wallet_legacy, wallet_hf26 = prepare_wallets(network_after_hf26)
 
     legacy_operation_passed(wallet_legacy)
     hf26_operation_passed(wallet_hf26)
+
 
 def test_after_hf26_without_majority(network_after_hf26_without_majority):
     wallet_legacy, wallet_hf26 = prepare_wallets(network_after_hf26_without_majority)
@@ -124,7 +133,9 @@ def test_verify_prepared_signature(serialization_type, expected_signature):
   "transaction_num": 0
 }
 '''
-    sign_transaction_result = sign_transaction(json.loads(prepared_transaction), tt.Account('initminer').private_key, serialization_type)
+    sign_transaction_result = sign_transaction(
+        json.loads(prepared_transaction), tt.Account('initminer').private_key, serialization_type
+    )
     tt.logger.info(f'sign_transaction_result: {json.dumps(sign_transaction_result)}')
 
     assert sign_transaction_result['sig'] == expected_signature
