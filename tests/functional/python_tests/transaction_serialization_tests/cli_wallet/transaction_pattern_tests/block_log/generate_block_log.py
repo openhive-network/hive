142f5c7a1ef3ce737990249f7cfc3ddc5ff7ebbd
Krzysztof Mochocki <kmochocki@syncad.com>
2022-07-19 11:23:38 +0200

    Remove generation of `private_keys.json` file

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/block_log/generate_block_log.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/block_log/generate_block_log.py
@@ -12,24 +12,14 @@ def prepare_blocklog():
 
     wallet = tt.Wallet(attach_to=node, additional_arguments=['--transaction-serialization=legacy'])
 
-    alice = create_account_and_fund_it(wallet, 'alice', tests=tt.Asset.Test(1000), vests=tt.Asset.Test(1000000),
+    create_account_and_fund_it(wallet, 'alice', tests=tt.Asset.Test(1000), vests=tt.Asset.Test(1000000),
                                        tbds=tt.Asset.Tbd(1000))
-    bob = create_account_and_fund_it(wallet, 'bob', tests=tt.Asset.Test(1000), vests=tt.Asset.Test(1000000),
+    create_account_and_fund_it(wallet, 'bob', tests=tt.Asset.Test(1000), vests=tt.Asset.Test(1000000),
                                      tbds=tt.Asset.Tbd(1000))
 
     # change_recovery_account preparation
-    carol = create_account_and_fund_it(wallet, 'carol', 'alice', tt.Asset.Test(10), tt.Asset.Test(100),
+    create_account_and_fund_it(wallet, 'carol', 'alice', tt.Asset.Test(10), tt.Asset.Test(100),
                                        tt.Asset.Tbd(10))
-    pub_keys = []
-    pub_keys.append(alice['operations'][0][1]['active']['key_auths'][0][0])
-    pub_keys.append(bob['operations'][0][1]['active']['key_auths'][0][0])
-    pub_keys.append(carol['operations'][0][1]['active']['key_auths'][0][0])
-
-    priv_keys = []
-    for key in pub_keys:
-        priv_keys.append(wallet.api.get_private_key(key))
-
-    save_keys_to_json_file(priv_keys)
 
     ####################################################################################################################
     # Cancel_transfer_from_savings preparation
@@ -47,15 +37,6 @@ def prepare_blocklog():
     node.wait_number_of_blocks(21)
     node.close()
     node.get_block_log(include_index=True).copy_to(Path(__file__).parent.absolute())
-
-
-def save_keys_to_json_file(keys):
-    dictionary = {}
-    for key_num in range(len(keys)):
-        dictionary[key_num] = keys[key_num]
-
-    with open(Path().absolute() / 'private_keys.json', 'w') as file:
-        json.dump(dictionary, file)
 
 
 if __name__ == "__main__":
