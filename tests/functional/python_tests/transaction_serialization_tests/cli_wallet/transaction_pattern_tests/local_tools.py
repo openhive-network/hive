06f1d1cadd2c74f9ba0561ee6e619b61e3774c7e
Krzysztof Mochocki <kmochocki@syncad.com>
2022-07-19 12:54:35 +0200

    Update function headers to remove obsolete arguments

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/local_tools.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/local_tools.py
@@ -13,9 +13,9 @@ def dumped_directory_path(extension, type_of_serialization, pattern_name):
     return PACKAGE_PATH / f'dumped_{extension}_files_{type_of_serialization}_wallet' / f'{pattern_name}.{extension}'
 
 
-def verify_correctness_of_generated_transaction_json(wallet, type_of_serialization, request, pattern_name):
+def verify_correctness_of_generated_transaction_json(wallet, pattern_name):
     source_path_file = wallet.directory / f'{pattern_name}.json'
-    target_path_file = dumped_directory_path('json', type_of_serialization, pattern_name)
+    target_path_file = dumped_directory_path('json', wallet.transaction_serialization, pattern_name)
 
     # Compare actual transaction with stored transaction
     with open(source_path_file) as file:
@@ -25,25 +25,25 @@ def verify_correctness_of_generated_transaction_json(wallet, type_of_serializati
     assert actual_json == pattern_json
 
 
-def verify_correctness_of_generated_transaction_bin(wallet, type_of_serialization, request, pattern_name):
+def verify_correctness_of_generated_transaction_bin(wallet, pattern_name):
     source_path_file = wallet.directory / f'{pattern_name}.bin'
-    target_path_file = dumped_directory_path('bin', type_of_serialization, pattern_name)
+    target_path_file = dumped_directory_path('bin', wallet.transaction_serialization, pattern_name)
 
     # Compare actual transaction with stored transaction
     assert filecmp.cmp(source_path_file, target_path_file, shallow=False)
 
 
-def store_transaction(wallet, type_of_serialization, request, pattern_name):
+def store_transaction(wallet, pattern_name):
     for file_extension in ['json', 'bin']:
         source_path_file = wallet.directory / f'{pattern_name}.{file_extension}'
-        target_path_file = dumped_directory_path(file_extension, type_of_serialization, pattern_name)
+        target_path_file = dumped_directory_path(file_extension, wallet.transaction_serialization, pattern_name)
 
         shutil.move(source_path_file, target_path_file)
 
 
-def compare_with_pattern(validate_function, wallet, request, pattern_name):
+def compare_with_pattern(validate_function, wallet, pattern_name):
     generate_patterns = strtobool(os.environ.get('GENERATE_PATTERNS', 'OFF'))
     if not generate_patterns:
-        validate_function(wallet, wallet.transaction_serialization, request, pattern_name)
+        validate_function(wallet, pattern_name)
     else:
-        store_transaction(wallet, wallet.transaction_serialization, request, pattern_name)
+        store_transaction(wallet, pattern_name)
