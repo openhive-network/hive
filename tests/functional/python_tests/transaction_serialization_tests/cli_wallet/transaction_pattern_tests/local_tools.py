f24b4152eadd4a5af4d13e71ae596a417f1d31a8
Krzysztof Mochocki <kmochocki@syncad.com>
2022-07-19 12:42:28 +0200

    Remove unused function

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/local_tools.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/local_tools.py
@@ -6,15 +6,8 @@ import shutil
 from distutils.util import strtobool
 
 
-def move_dumped_files(wallet, files_name):
-    source_path_json_file = wallet.directory / f'{files_name}.json'
-    target_path_json_file = pathlib.Path(__file__).parent.absolute() / 'dump_json_files' / f'{files_name}.json'
 
-    source_path_binary_file = wallet.directory / f'{files_name}.bin'
-    target_path_binary_file = pathlib.Path(__file__).parent.absolute() / 'dump_binary_files' / f'{files_name}.bin'
 
-    shutil.move(source_path_json_file, target_path_json_file)
-    shutil.move(source_path_binary_file, target_path_binary_file)
 
 def verify_correctness_of_generated_transaction_json(wallet, type_of_serialization, request, pattern_name):
     source_path_file = wallet.directory / f'{pattern_name}.json'
