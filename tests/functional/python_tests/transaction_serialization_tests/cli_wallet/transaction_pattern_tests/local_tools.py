2d16571accc04c4451c85c2f5c8b01e0370fab0e
Krzysztof Mochocki <kmochocki@syncad.com>
2022-07-20 13:30:57 +0200

    Extract generation of file path to separate function

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/local_tools.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/local_tools.py
@@ -1,18 +1,21 @@
 import filecmp
 import json
 import os
-import pathlib
 import shutil
+
+from pathlib import Path
 from distutils.util import strtobool
 
+PACKAGE_PATH = Path(__file__).parent
 
 
+def dumped_directory_path(extension, type_of_serialization, pattern_name):
+    return PACKAGE_PATH / f'dumped_{extension}_files_{type_of_serialization}_wallet' / f'{pattern_name}.{extension}'
 
 
 def verify_correctness_of_generated_transaction_json(wallet, type_of_serialization, request, pattern_name):
     source_path_file = wallet.directory / f'{pattern_name}.json'
-    target_path_file = pathlib.Path(
-        request.fspath.dirname) / f'dumped_json_files_{type_of_serialization}_wallet' / f'{pattern_name}.json'
+    target_path_file = dumped_directory_path('json', type_of_serialization, pattern_name)
 
     # Compare actual transaction with stored transaction
     with open(source_path_file) as file:
@@ -24,8 +27,7 @@ def verify_correctness_of_generated_transaction_json(wallet, type_of_serializati
 
 def verify_correctness_of_generated_transaction_bin(wallet, type_of_serialization, request, pattern_name):
     source_path_file = wallet.directory / f'{pattern_name}.bin'
-    target_path_file = pathlib.Path(
-        request.fspath.dirname) / f'dumped_bin_files_{type_of_serialization}_wallet' / f'{pattern_name}.bin'
+    target_path_file = dumped_directory_path('bin', type_of_serialization, pattern_name)
 
     # Compare actual transaction with stored transaction
     assert filecmp.cmp(source_path_file, target_path_file, shallow=False)
@@ -34,8 +36,7 @@ def verify_correctness_of_generated_transaction_bin(wallet, type_of_serializatio
 def store_transaction(wallet, type_of_serialization, request, pattern_name):
     for file_extension in ['json', 'bin']:
         source_path_file = wallet.directory / f'{pattern_name}.{file_extension}'
-        target_path_file = pathlib.Path(
-            request.fspath.dirname) / f'dumped_{file_extension}_files_{type_of_serialization}_wallet' / f'{pattern_name}.{file_extension}'
+        target_path_file = dumped_directory_path(file_extension, type_of_serialization, pattern_name)
 
         shutil.move(source_path_file, target_path_file)
 
