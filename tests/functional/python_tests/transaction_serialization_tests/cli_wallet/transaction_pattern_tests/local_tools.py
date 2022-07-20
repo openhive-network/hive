dc75eabacf2b208906c795462b4e8d14dc65bc9d
Krzysztof Mochocki <kmochocki@syncad.com>
2022-07-20 12:55:13 +0200

    Rename helper function and make it private
    Previous name `dumped_directory_path` was invalid because it
    was suggesting, that this function return path to _directory_, where
    (freshly?) dumped transactions are, which is not true. This function
    returns path to pattern _file_

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/local_tools.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/local_tools.py
@@ -9,7 +9,7 @@ from distutils.util import strtobool
 PACKAGE_PATH = Path(__file__).parent
 
 
-def dumped_directory_path(extension, type_of_serialization, pattern_name):
+def __get_path_of_pattern_file(extension, type_of_serialization, pattern_name):
     return PACKAGE_PATH / f'dumped_files_{type_of_serialization}_wallet' / f'{pattern_name}.{extension}'
 
 
@@ -22,7 +22,7 @@ def __able_to_generate_pattern(validate_function):
         for file_extension in ['json', 'bin']:
             shutil.move(
                 wallet.directory / f'{pattern_name}.{file_extension}',
-                dumped_directory_path(file_extension, wallet.transaction_serialization, pattern_name)
+                __get_path_of_pattern_file(file_extension, wallet.transaction_serialization, pattern_name)
             )
     return impl
 
@@ -30,7 +30,7 @@ def __able_to_generate_pattern(validate_function):
 @__able_to_generate_pattern
 def verify_generated_transaction_with_json_pattern(wallet, pattern_name):
     source_path_file = wallet.directory / f'{pattern_name}.json'
-    target_path_file = dumped_directory_path('json', wallet.transaction_serialization, pattern_name)
+    target_path_file = __get_path_of_pattern_file('json', wallet.transaction_serialization, pattern_name)
 
     # Compare actual transaction with stored transaction
     with open(source_path_file) as file:
@@ -43,7 +43,7 @@ def verify_generated_transaction_with_json_pattern(wallet, pattern_name):
 @__able_to_generate_pattern
 def verify_generated_transaction_with_binary_pattern(wallet, pattern_name):
     source_path_file = wallet.directory / f'{pattern_name}.bin'
-    target_path_file = dumped_directory_path('bin', wallet.transaction_serialization, pattern_name)
+    target_path_file = __get_path_of_pattern_file('bin', wallet.transaction_serialization, pattern_name)
 
     # Compare actual transaction with stored transaction
     assert filecmp.cmp(source_path_file, target_path_file, shallow=False)
