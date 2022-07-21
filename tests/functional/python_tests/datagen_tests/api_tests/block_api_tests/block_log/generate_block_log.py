2013509083271910727255551f67ce43c98a25ae
Bartek Wrona <wrona@syncad.com>
2022-07-22 01:06:34 +0200

    Initial set of tests covering block_api::get_block_range calls.

---
+++ tests/functional/python_tests/datagen_tests/api_tests/block_api_tests/block_log/generate_block_log.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import test_tools as tt
+
+TOTAL_BLOCKS=120
+
+def prepare_block_log():
+    init_node = tt.InitNode()
+    init_node.run()
+    init_node.wait_number_of_blocks(TOTAL_BLOCKS)
+    init_node.close()
+    init_node.get_block_log(include_index=True).copy_to(Path(__file__).parent.absolute()) 
+
+if __name__ == '__main__':
+    prepare_block_log()
