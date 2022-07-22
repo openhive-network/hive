097dbac956b679f8f244475527362cad601473bd
Bartek Wrona <wrona@syncad.com>
2022-07-22 22:39:36 +0200

    Eliminated obsolete type definitions related to previously removed chain_api::push_block.

--- libraries/plugins/apis/chain_api/include/hive/plugins/chain_api/chain_api.hpp
+++ libraries/plugins/apis/chain_api/include/hive/plugins/chain_api/chain_api.hpp
@@ -9,19 +9,6 @@ namespace hive { namespace plugins { namespace chain {
 
 namespace detail { class chain_api_impl; }
 
-struct push_block_args
-{
-  hive::chain::signed_block block;
-  bool                         currently_syncing = false;
-};
-
-
-struct push_block_return
-{
-  bool              success;
-  optional<string>  error;
-};
-
 typedef hive::chain::signed_transaction push_transaction_args;
 
 struct push_transaction_return
@@ -29,7 +16,6 @@ struct push_transaction_return
   bool              success;
   optional<string>  error;
 };
-
 
 class chain_api
 {
@@ -46,6 +32,4 @@ class chain_api
 
 } } } // hive::plugins::chain
 
-FC_REFLECT( hive::plugins::chain::push_block_args, (block)(currently_syncing) )
-FC_REFLECT( hive::plugins::chain::push_block_return, (success)(error) )
 FC_REFLECT( hive::plugins::chain::push_transaction_return, (success)(error) )
