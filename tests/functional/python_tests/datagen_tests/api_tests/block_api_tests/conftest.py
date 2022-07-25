d57d8eb2ec0a36c46f056b593e2c259e228ba198
Piotr Batko <pbatko@syncad.com>
2022-07-25 16:45:25 +0200

    Format and fix minor problems in block API tests

--- tests/functional/python_tests/datagen_tests/api_tests/block_api_tests/conftest.py
+++ tests/functional/python_tests/datagen_tests/api_tests/block_api_tests/conftest.py
@@ -1,11 +1,12 @@
+from pathlib import Path
+
 import pytest
 
 import test_tools as tt
-from pathlib import Path
+
 
 @pytest.fixture
 def replayed_node():
     api_node = tt.ApiNode()
     api_node.run(replay_from=Path(__file__).parent.joinpath('block_log/block_log'), wait_for_live=False)
     return api_node
-
--- tests/functional/python_tests/datagen_tests/api_tests/block_api_tests/test_crash.py
+++ tests/functional/python_tests/datagen_tests/api_tests/block_api_tests/test_crash.py
@@ -1,11 +1,12 @@
-from urllib import response
 import test_tools as tt
 
 from .block_log.generate_block_log import TOTAL_BLOCKS
 
+
 def test_get_block_range_crash(replayed_node):
     response = replayed_node.api.block.get_block_range(starting_block_num=60, count=30)
     assert len(response['blocks']) == 30
+
 
 def test_get_block_range(replayed_node):
     step = 100
@@ -15,9 +16,10 @@ def test_get_block_range(replayed_node):
         tt.logger.info(f"Query for range: start_block: {i}, count: {step} gave a {c} blocks in the response")
         assert c == step
 
+
 def test_get_block_range_out_of_range(replayed_node):
     step = 100
     response = replayed_node.api.block.get_block_range(starting_block_num=TOTAL_BLOCKS+step, count=step)
     c = len(response['blocks'])
-    tt.logger.info(f"Query for range: start_block: {TOTAL_BLOCKS+step}, count: {step} gave a {c} blocks in the response")
+    tt.logger.info(f"Query for range: start_block={TOTAL_BLOCKS+step}, count={step} gave a {c} blocks in the response")
     assert c == 0
