a1d5a7a9d9837919a3e89936f70a8d7b3ace0564
Radek Masłowski <rmaslowski@syncad.com>
2022-07-25 15:39:56 +0200

    Wip2(no work)

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/test_transaction_patterns.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/transaction_pattern_tests/test_transaction_patterns.py
@@ -17,6 +17,12 @@ TYPES_OF_SERIALIZATION = [
 
 METHODS_WITH_CORRECT_ARGUMENTS = [
     ('cancel_order', ('alice', 1, False)),
+    ('cancel_transfer_from_savings', ('alice', 1, False)),
+    ('change_recovery_account', ('initminer', 'alice', False)),
+    ('claim_account_creation', ('initminer', tt.Asset.Test(0), False)),
+    ('claim_account_creation_nonblocking', ('initminer', tt.Asset.Test(0), False)),
+    ('create_proposal', ('alice', 'alice', '2031-01-01T00:00:00', '2031-06-01T00:00:00', tt.Asset.Tbd(1000), 'subject-1', 'permlink', False)),
+
 ]
 
 
@@ -28,66 +34,6 @@ def test_transaction_patterns(replayed_node, wallet_with_pattern_name, verify_pa
     wallet, pattern_name = wallet_with_pattern_name
 
     transaction = getattr(wallet.api, cli_wallet_method)(*arguments)
-    replayed_node.api.wallet_bridge.broadcast_transaction(transaction)
-
-    verify_pattern(wallet, pattern_name)
-
-
-@pytest.mark.testnet
-@pytest.mark.parametrize('verify_pattern', WAYS_OF_PATTERN_VERIFICATION)
-@pytest.mark.parametrize('wallet_with_pattern_name', TYPES_OF_SERIALIZATION, indirect=True)
-def test_cancel_order(replayed_node, wallet_with_pattern_name, verify_pattern):
-    wallet, pattern_name = wallet_with_pattern_name
-
-    transaction = wallet.api.cancel_order('alice', 1, broadcast=False)
-    replayed_node.api.wallet_bridge.broadcast_transaction(transaction)
-
-    verify_pattern(wallet, pattern_name)
-
-
-@pytest.mark.testnet
-@pytest.mark.parametrize('verify_pattern', WAYS_OF_PATTERN_VERIFICATION)
-@pytest.mark.parametrize('wallet_with_pattern_name', TYPES_OF_SERIALIZATION, indirect=True)
-def test_cancel_transfer_from_savings(replayed_node, wallet_with_pattern_name, verify_pattern):
-    wallet, pattern_name = wallet_with_pattern_name
-
-    transaction = wallet.api.cancel_transfer_from_savings('alice', 1, broadcast=False)
-    replayed_node.api.wallet_bridge.broadcast_transaction(transaction)
-
-    verify_pattern(wallet, pattern_name)
-
-
-@pytest.mark.testnet
-@pytest.mark.parametrize('verify_pattern', WAYS_OF_PATTERN_VERIFICATION)
-@pytest.mark.parametrize('wallet_with_pattern_name', TYPES_OF_SERIALIZATION, indirect=True)
-def test_change_recovery_account(replayed_node, wallet_with_pattern_name, verify_pattern):
-    wallet, pattern_name = wallet_with_pattern_name
-
-    transaction = wallet.api.change_recovery_account('initminer', 'alice', broadcast=False)
-    replayed_node.api.wallet_bridge.broadcast_transaction(transaction)
-
-    verify_pattern(wallet, pattern_name)
-
-
-@pytest.mark.testnet
-@pytest.mark.parametrize('verify_pattern', WAYS_OF_PATTERN_VERIFICATION)
-@pytest.mark.parametrize('wallet_with_pattern_name', TYPES_OF_SERIALIZATION, indirect=True)
-def test_claim_account_creation(replayed_node, wallet_with_pattern_name, verify_pattern):
-    wallet, pattern_name = wallet_with_pattern_name
-
-    transaction = wallet.api.claim_account_creation('initminer', tt.Asset.Test(0), broadcast=False)
-    replayed_node.api.wallet_bridge.broadcast_transaction(transaction)
-
-    verify_pattern(wallet, pattern_name)
-
-
-@pytest.mark.testnet
-@pytest.mark.parametrize('verify_pattern', WAYS_OF_PATTERN_VERIFICATION)
-@pytest.mark.parametrize('wallet_with_pattern_name', TYPES_OF_SERIALIZATION, indirect=True)
-def test_claim_account_creation_nonblocking(replayed_node, wallet_with_pattern_name, verify_pattern):
-    wallet, pattern_name = wallet_with_pattern_name
-
-    transaction = wallet.api.claim_account_creation_nonblocking('initminer', tt.Asset.Test(0), broadcast=False)
     replayed_node.api.wallet_bridge.broadcast_transaction(transaction)
 
     verify_pattern(wallet, pattern_name)
@@ -118,19 +64,6 @@ def test_create_funded_account_with_keys(replayed_node, wallet_with_pattern_name
     transaction = wallet.api.create_funded_account_with_keys('initminer', dan.name, tt.Asset.Test(10),
                                                              'memo', '{}', dan.public_key, dan.public_key,
                                                              dan.public_key, dan.public_key, broadcast=False)
-    replayed_node.api.wallet_bridge.broadcast_transaction(transaction)
-
-    verify_pattern(wallet, pattern_name)
-
-
-@pytest.mark.testnet
-@pytest.mark.parametrize('verify_pattern', WAYS_OF_PATTERN_VERIFICATION)
-@pytest.mark.parametrize('wallet_with_pattern_name', TYPES_OF_SERIALIZATION, indirect=True)
-def test_create_proposal(replayed_node, wallet_with_pattern_name, verify_pattern):
-    wallet, pattern_name = wallet_with_pattern_name
-
-    transaction = wallet.api.create_proposal('alice', 'alice', '2031-01-01T00:00:00', '2031-06-01T00:00:00',
-                                             tt.Asset.Tbd(1000), 'subject-1', 'permlink', broadcast=False)
     replayed_node.api.wallet_bridge.broadcast_transaction(transaction)
 
     verify_pattern(wallet, pattern_name)
