dfa87c2cc9aa51dd20994727bce7181c90de39a4
Michał Kudela <kudmich@syncad.com>
2022-07-27 11:21:53 +0200

    Test extension format in update proposal operation

--- tests/functional/python_tests/transaction_serialization_tests/test_extension_serialization_format.py
+++ tests/functional/python_tests/transaction_serialization_tests/test_extension_serialization_format.py
@@ -2,7 +2,11 @@ import pytest
 
 import test_tools as tt
 
-from ....local_tools import create_account_and_fund_it
+from ....local_tools import create_account_and_fund_it, date_from_now
+
+PROPOSAL_START_DATE = date_from_now(weeks=16)
+PROPOSAL_END_DATE = date_from_now(weeks=20)
+PROPOSAL_END_DATE_AFTER_UPDATE = date_from_now(weeks=19)
 
 
 @pytest.mark.parametrize(
@@ -59,6 +63,26 @@ def test_change_comment_operation(node, api, expected_extension):
     block_num = transaction['block_num']
 
     extension = get_extension_from_operation(node, api, block_num)
+    assert extension == expected_extension
+
+
+@pytest.mark.parametrize(
+    'api, expected_extension', [
+        ('block', {'type': 'update_proposal_end_date', 'value': {'end_date': PROPOSAL_END_DATE_AFTER_UPDATE}}),
+        ('condenser', ['update_proposal_end_date', {'end_date': PROPOSAL_END_DATE_AFTER_UPDATE}]),
+    ]
+)
+@pytest.mark.testnet
+def test_update_proposal_operation(node, api, expected_extension):
+    wallet = tt.Wallet(attach_to=node)
+
+    create_account_and_fund_it(wallet, 'alice', tbds=tt.Asset.Tbd(100), vests=tt.Asset.Test(100))
+    wallet.api.post_comment('alice', 'permlink', '', 'parent-permlink', 'title', 'body', '{}')
+    wallet.api.create_proposal('alice', 'alice', PROPOSAL_START_DATE, PROPOSAL_END_DATE, tt.Asset.Tbd(100), 'subject-1', 'permlink')
+    operation = wallet.api.update_proposal(0, 'alice', tt.Asset.Tbd(10), 'subject-1', 'permlink', PROPOSAL_END_DATE_AFTER_UPDATE)
+    block_num = operation['block_num']
+    extension = get_extension_from_operation(node, api, block_num)
+
     assert extension == expected_extension
 
 
