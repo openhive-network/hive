d38c422d941d213a6d3a6e4902d7be43333d4a16
Mateusz Kudela <mkudela@syncad.com>
2022-08-03 14:01:37 +0200

    Refactor transaction_status API tests
    Divide test_find_existing_transaction into couple, small test cases, add
    parametrization to 'unknown_id' variable, change method names to more
    accurate, delete unnecessary parts of code

--- tests/functional/python_tests/datagen_tests/api_tests/transaction_status_api_tests/test_find_transaction.py
+++ tests/functional/python_tests/datagen_tests/api_tests/transaction_status_api_tests/test_find_transaction.py
@@ -1,45 +1,64 @@
-import time
+import pytest
+
 from ......local_tools import date_from_now
 
 
-def test_find_existing_transaction(node, wallet):
-    wallet.api.set_transaction_expiration(90)
+def test_find_transaction_before_broadcasting_it(node, wallet):
     transaction = wallet.api.create_account('initminer', 'alice', '{}', broadcast=False)
-    transaction_id = transaction['transaction_id']
-    assert node.api.transaction_status.find_transaction(transaction_id=transaction_id)['status'] == 'unknown'
+    assert node.api.transaction_status.find_transaction(transaction_id=transaction['transaction_id'])['status'] == 'unknown'
+
+
+def test_find_transaction_right_after_broadcasting_it_manually(node, wallet):
+    transaction = wallet.api.create_account('initminer', 'alice', '{}', broadcast=False)
     node.api.network_broadcast.broadcast_transaction(trx=transaction)
-    assert node.api.transaction_status.find_transaction(transaction_id=transaction_id)['status'] == 'within_mempool'
+    assert node.api.transaction_status.find_transaction(transaction_id=transaction['transaction_id'])['status'] == 'within_mempool'
 
+
+def test_find_transaction_in_reversible_block_pool(node, wallet):
+    transaction = wallet.api.create_account('initminer', 'alice', '{}')
     node.wait_number_of_blocks(1)
-    assert node.api.database.is_known_transaction(id=transaction_id)['is_known'] is True
-    assert node.api.transaction_status.find_transaction(transaction_id=transaction_id)['status'] == 'within_reversible_block'
+    assert node.api.database.is_known_transaction(id=transaction['transaction_id'])['is_known'] is True
+    assert node.api.transaction_status.find_transaction(transaction_id=transaction['transaction_id'])['status'] == 'within_reversible_block'
 
+
+def test_find_transaction_in_irreversible_block_pool(node, wallet):
+    transaction = wallet.api.create_account('initminer', 'alice', '{}')
+    assert node.api.database.is_known_transaction(id=transaction['transaction_id'])['is_known'] is True
+
+    # waiting 22 blocks for the block with the transaction to become irreversible
     node.wait_number_of_blocks(22)
-    assert node.api.transaction_status.find_transaction(transaction_id=transaction_id)['status'] == 'within_irreversible_block'
-
-    time.sleep(90)
-    '''
-        In this test case we can't really use find_transaction from transaction_status API to check status of 
-        transaction (after expiration) because it holds transactions for one hour before it flag it as expired. Instead 
-        of this is_known_transaction from database API is used. 
-    '''
-    assert node.api.database.is_known_transaction(id=transaction_id)['is_known'] is False
+    assert node.api.transaction_status.find_transaction(transaction_id=transaction['transaction_id'])['status'] == 'within_irreversible_block'
 
 
-def test_find_too_old_transaction(node, wallet):
-    unknown_id = '0000000000000000000000000000000000000000'
+@pytest.mark.parametrize(
+    'unknown_id', [
+        '0000000000000000000000000000000000000000',
+        '0000000000000000000000000000000000000001',
+    ]
+)
+def test_find_transaction_with_expiration_set_half_year_earlier(node, wallet, unknown_id):
     response = node.api.transaction_status.find_transaction(transaction_id=unknown_id, expiration=date_from_now(weeks=-24))
     assert response['status'] == 'too_old'
 
 
-def test_find_unknown_transaction(node, wallet):
-    unknown_id = '0000000000000000000000000000000000000000'
+@pytest.mark.parametrize(
+    'unknown_id', [
+        '0000000000000000000000000000000000000000',
+        '0000000000000000000000000000000000000001',
+    ]
+)
+def test_find_transaction_with_unknown_id(node, wallet, unknown_id):
     response = node.api.transaction_status.find_transaction(transaction_id=unknown_id)
     assert response['status'] == 'unknown'
 
 
-def test_find_expired_irreversible_transaction(sped_up_node, wallet):
-    unknown_id = '0000000000000000000000000000000000000001'
+@pytest.mark.parametrize(
+    'unknown_id', [
+        '0000000000000000000000000000000000000000',
+        '0000000000000000000000000000000000000001',
+    ]
+)
+def test_find_transaction_after_producing_empty_blocks_for_hour(sped_up_node, unknown_id):
     sped_up_node.wait_number_of_blocks(1260)
     node_time = sped_up_node.api.database.get_dynamic_global_properties()['time']
     response = sped_up_node.api.transaction_status.find_transaction(transaction_id=unknown_id, expiration=node_time)
