b4cee8247425cb2c461fe7e6ac214805d888f0cb
Mateusz Żebrak <mzebrak@syncad.com>
2022-08-09 13:43:30 +0200

    Update api_node restarting with time_offset in test_no_ah_duplicates_after_restart

--- tests/functional/python_tests/fork_tests/test_no_ah_duplicates_after_restart.py
+++ tests/functional/python_tests/fork_tests/test_no_ah_duplicates_after_restart.py
@@ -1,14 +1,19 @@
 import test_tools as tt
 
-from .local_tools import assert_no_duplicates
+from .local_tools import assert_no_duplicates, get_time_offset_from_iso_8601
 
 
 def test_no_duplicates_in_account_history_plugin_after_restart(prepared_networks):
     # TRIGGER
     # We restart one of nodes.
     api_node = prepared_networks['Alpha'].node('ApiNode0')
-    tt.logger.info('Restarting api node...')
-    api_node.restart()
+
+    head_block_num = api_node.api.condenser.get_dynamic_global_properties()['head_block_number']
+    head_block_timestamp = api_node.api.block.get_block(block_num=head_block_num)['block']['timestamp']
+    time_offset = get_time_offset_from_iso_8601(head_block_timestamp)
+
+    tt.logger.info(f'Restarting api node with time offset: {head_block_timestamp}')
+    api_node.restart(time_offset=time_offset)
 
     # VERIFY
     # Expected behaviour is that restarted node will enter live sync
