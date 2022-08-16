2842b2228bd885baf5ddc7e9f761ca3bd92f2097
Mateusz Kudela <mkudela@syncad.com>
2022-08-16 13:43:17 +0200

    Add market_history API tests

---
+++ tests/api_tests/message_format_tests/market_history_api_tests/test_get_recent_trades.py
@@ -0,0 +1,20 @@
+
+import test_tools as tt
+
+from ..local_tools import run_for
+from ....local_tools import create_account_and_fund_it
+
+
+@run_for('testnet', 'mainnet_5m', 'mainnet_64m')
+def test_get_recent_trades(prepared_node, should_prepare):
+    if should_prepare:
+        wallet = tt.Wallet(attach_to=prepared_node)
+        create_account_and_fund_it(wallet, 'alice', tests=tt.Asset.Test(100), vests=tt.Asset.Test(100))
+        create_account_and_fund_it(wallet, 'bob', tests=tt.Asset.Test(100), vests=tt.Asset.Test(100),
+                                   tbds=tt.Asset.Tbd(100))
+
+        wallet.api.create_order('alice', 0, tt.Asset.Test(100), tt.Asset.Tbd(100), False, 3600)  # Sell 100 HIVE for 100 HBD
+        wallet.api.create_order('bob', 0, tt.Asset.Tbd(100), tt.Asset.Test(100), False, 3600)  # Buy 100 HIVE for 100 HBD
+
+    trades = prepared_node.api.market_history.get_recent_trades(limit=10)
+    assert len(trades) != 0
