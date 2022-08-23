4255e1ebdf8f9735bba265bd53bce4cbff89438e
Radek Masłowski <rmaslowski@syncad.com>
2022-08-23 15:22:42 +0200

    Add test_help.py

---
+++ tests/api_tests/message_format_tests/cli_wallet_tests/endpoint_formatting_tests/test_help.py
@@ -0,0 +1,23 @@
+from .local_tools import verify_json_patterns, verify_text_patterns
+
+
+def test_help(node, wallet_with_json_formatter, wallet_with_text_formatter):
+    help_content_in_json_form = wallet_with_json_formatter.api.help()
+    help_content_in_text_form_parsed_to_list = parse_text_response(wallet_with_text_formatter.api.help())
+
+    assert help_content_in_json_form == help_content_in_text_form_parsed_to_list
+
+
+def test_json_format_pattern(node, wallet_with_json_formatter):
+    json = wallet_with_json_formatter.api.help()
+
+    verify_json_patterns('help', json)
+
+
+def test_text_format_pattern(node, wallet_with_text_formatter):
+    text = wallet_with_text_formatter.api.help()
+
+    verify_text_patterns('help', text)
+
+def parse_text_response(text):
+    return [line for line in text.split('\n') if line != '']
