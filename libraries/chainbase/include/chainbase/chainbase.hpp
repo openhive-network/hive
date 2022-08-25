1173aeb281375f9398e9be5e6f14a2674722733a
Fernando Henrique Soder <fhsoder@gmail.com>
2022-08-25 15:00:31 +0000

    Neither does Win64 as I am seing.

--- libraries/chainbase/include/chainbase/chainbase.hpp
+++ libraries/chainbase/include/chainbase/chainbase.hpp
@@ -952,7 +952,7 @@ namespace chainbase {
 
       unsigned long long get_total_system_memory() const
       {
-#if !defined( __APPLE__ ) // OS X does not support _SC_AVPHYS_PAGES
+#if !(defined( __APPLE__ ) || defined(_WIN64) ) // OS X does not support _SC_AVPHYS_PAGES
         long pages = sysconf(_SC_AVPHYS_PAGES);
         long page_size = sysconf(_SC_PAGE_SIZE);
         return pages * page_size;
