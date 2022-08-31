b9053e1e4bbaebad77ea48824f1395be301e9868
Radek Masłowski <rmaslowski@syncad.com>
2022-08-31 13:52:53 +0200

    Add test difference of number of allowed key authorizations during 1 hour beetween HF25/26
    On HF25 version is allowed to make one key authorization, but on HF26 is allowed to do it twice.

---
+++ tests/functional/python_tests/hf26_tests/test_number_of_allowed_key_auths_consecutive_updates.py
@@ -0,0 +1,31 @@
+import pytest
+
+import test_tools as tt
+
+from ....local_tools import create_account_and_fund_it
+
+
+def test_multiple_auth_key_updates_in_hf25(wallet_hf25):
+    create_account_and_fund_it(wallet_hf25, 'alice', vests=tt.Asset.Test(100))
+
+    owner_keys = [tt.PublicKey(account) for account in ['bob', 'carol']]
+
+    wallet_hf25.api.update_account_auth_key('alice', 'owner', owner_keys[0], 3)
+
+    # In HF25 it was allowed to update authorization key only once per hour, so second try was rejected.
+    with pytest.raises(tt.exceptions.CommunicationError):
+        wallet_hf25.api.update_account_auth_key('alice', 'owner', owner_keys[1], 3)
+
+
+def test_multiple_auth_key_updates_in_hf26(wallet_hf26):
+    create_account_and_fund_it(wallet_hf26, 'alice', vests=tt.Asset.Test(100))
+
+    owner_keys = [tt.PublicKey(account) for account in ['bob', 'carol', 'dan']]
+
+    # In HF26 it was allowed to update authorization key twice per hour...
+    for owner_key in owner_keys[:-1]:
+        wallet_hf26.api.update_account_auth_key('alice', 'owner', owner_key, 3)
+
+    # ...so third consecutive try should be rejected.
+    with pytest.raises(tt.exceptions.CommunicationError):
+        wallet_hf26.api.update_account_auth_key('alice', 'owner', owner_keys[-1], 3)
