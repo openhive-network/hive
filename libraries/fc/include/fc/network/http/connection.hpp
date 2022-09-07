46342898e946bc60e2dc2f04df4a8dcc190be0fe
Krzysztof Mochocki <kmochocki@syncad.com>
2022-09-07 12:50:07 +0200

    Add handling of 204 status_code in hived

--- libraries/fc/include/fc/network/http/connection.hpp
+++ libraries/fc/include/fc/network/http/connection.hpp
@@ -3,13 +3,13 @@
 #include <fc/string.hpp>
 #include <memory>
 
-namespace fc { 
+namespace fc {
   namespace ip { class endpoint; }
   class tcp_socket;
 
   namespace http {
 
-     struct header 
+     struct header
      {
        header( fc::string k, fc::string v )
        :key(fc::move(k)),val(fc::move(v)){}
@@ -19,25 +19,28 @@ namespace fc {
      };
 
      typedef std::vector<header> headers;
-     
-     struct reply 
+
+     struct reply
      {
         enum status_code {
             OK                  = 200,
             RecordCreated       = 201,
+            NoContent           = 204,
             BadRequest          = 400,
             NotAuthorized       = 401,
             NotFound            = 404,
             Found               = 302,
-            InternalServerError = 500
+            InternalServerError = 500,
+
+            NotSet              = -1
         };
-        reply( status_code c = OK):status(c){}
+        reply( status_code c = status_code::NotSet):status(c){}
         int                     status;
         std::vector<header>      headers;
         std::vector<char>        body;
      };
-     
-     struct request 
+
+     struct request
      {
         fc::string get_header( const fc::string& key )const;
         fc::string              remote_endpoint;
@@ -47,32 +50,32 @@ namespace fc {
         std::vector<header>     headers;
         std::vector<char>       body;
      };
-     
+
      std::vector<header> parse_urlencoded_params( const fc::string& f );
-     
+
      /**
       *  Connections have reference semantics, all copies refer to the same
-      *  underlying socket.  
+      *  underlying socket.
       */
-     class connection 
+     class connection
      {
        public:
          connection();
          ~connection();
          // used for clients
          void         connect_to( const fc::ip::endpoint& ep );
-         http::reply  request( const fc::string& method, const fc::string& url, const fc::string& body = std::string(), const headers& = headers());
-     
+         http::reply  request( const fc::string& method, const fc::string& url, const fc::string& body = std::string(), const headers& = headers(), const bool wait_for_response = true);
+
          // used for servers
          fc::tcp_socket& get_socket()const;
-     
+
          http::request    read_request()const;
 
          class impl;
        private:
          std::unique_ptr<impl> my;
      };
-     
+
      typedef std::shared_ptr<connection> connection_ptr;
 
 } } // fc::http
