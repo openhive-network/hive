c86ce6a089f8e750bf7888ec74beb07b1e27078d
Radek Masłowski <rmaslowski@syncad.com>
2022-09-14 11:00:10 +0200

    fixup! Add test_get_account_history.py

--- tests/api_tests/message_format_tests/cli_wallet_tests/methods_formatting_tests/test_get_account_history.py
+++ tests/api_tests/message_format_tests/cli_wallet_tests/methods_formatting_tests/test_get_account_history.py
@@ -2,34 +2,44 @@ import json
 import re
 from typing import Dict
 
+import test_tools as tt
+
 from .local_tools import verify_json_patterns, verify_text_patterns
+from .....local_tools import create_account_and_fund_it
 
 
 def test_get_account_history(node, wallet_with_json_formatter, wallet_with_text_formatter):
+    block_num = prepare_transactions(wallet_with_json_formatter)
+
     # Wait to appear transactions in account_history
     node.wait_number_of_blocks(21)
 
-    json = wallet_with_json_formatter.api.get_account_history('initminer', 2, 2)
-    text_parsed_to_list = parse_text_response(wallet_with_text_formatter.api.get_account_history('initminer', 2, 2))
+    json = wallet_with_json_formatter.api.get_account_history('alice', block_num[-1] + 1, 2)
+    text_parsed_to_list = parse_text_response(wallet_with_text_formatter.api.
+                                              get_account_history('alice', block_num[-1] + 1, 2))
 
     for acc_history_json, acc_history_text in zip(json, text_parsed_to_list):
         assert acc_history_json == acc_history_text
 
 
 def test_json_format_pattern(node, wallet_with_json_formatter):
+    block_num = prepare_transactions(wallet_with_json_formatter)
+
     # Wait to appear transactions in account_history
     node.wait_number_of_blocks(21)
 
-    json = wallet_with_json_formatter.api.get_account_history('initminer', 2, 2)
+    json = wallet_with_json_formatter.api.get_account_history('alice', block_num[-1] + 1, 2)
 
     verify_json_patterns('get_account_history', json)
 
 
 def test_text_format_pattern(node, wallet_with_text_formatter):
+    block_num = prepare_transactions(wallet_with_text_formatter)
+
     # Wait to appear transactions in account_history
     node.wait_number_of_blocks(21)
 
-    text = wallet_with_text_formatter.api.get_account_history('initminer', 2, 2)
+    text = wallet_with_text_formatter.api.get_account_history('alice', block_num[-1] + 1, 2)
 
     verify_text_patterns('get_account_history', text)
 
@@ -47,4 +57,15 @@ def parse_text_response(text):
             },
         }
     lines = text.splitlines()
-    return [parse_single_line_with_order_values(line_to_parse) for line_to_parse in lines[2:-1]]
+    return [parse_single_line_with_order_values(line_to_parse) for line_to_parse in lines[2:]]
+
+
+def prepare_transactions(wallet) -> list:
+    create_account_and_fund_it(wallet, 'alice', tests=tt.Asset.Test(100), vests=tt.Asset.Test(100))
+
+    post_comment_transaction =\
+        wallet.api.post_comment('alice', 'test-permlink', '', 'someone0', 'test-title', 'this is a body', '{}')
+    update_account_auth_key_transaction =\
+        wallet.api.update_account_auth_key('alice', 'owner', tt.Account('some key').public_key, 1)
+
+    return [post_comment_transaction['block_num'], update_account_auth_key_transaction['block_num']]
