11ef332498e77b904c0530a922e697a118f59f74
Mariusz <mtrela@syncad.com>
2022-09-15 09:11:33 +0200

    Test 'test_obi_throw_exception_02' works more stable

--- tests/functional/python_tests/fork_tests/test_obi_throw_exception_02.py
+++ tests/functional/python_tests/fork_tests/test_obi_throw_exception_02.py
@@ -5,19 +5,20 @@ import time
 def test_obi_throw_exception_02(prepare_obi_throw_exception_02):
     # start - A network (consists of a 'A' network(10 witnesses) + a 'B' network(11 witnesses)) produces blocks
 
-    # A witnesses from both networks have an exception in the same time - during 'sleep_time_in_sec' seconds these sub networks can't produce
+    # A witnesses from both networks have an exception in the same time - during 'delay_seconds' seconds none of witnesses can't produce.
     # After production resuming, both sub networks link together and LIB increases,
-    # because sending transaction with `witness_block_approve_operation` inf witness plugin is sent when `finish_apply_block` event occurs
+    # because sending transaction with `witness_block_approve_operation` in a witness plugin is sent when `finish_push_block` event occurs.
 
     sub_networks_data   = prepare_obi_throw_exception_02['sub-networks-data']
     sub_networks        = sub_networks_data[0]
     assert len(sub_networks) == 2
 
-    api_node_0          = sub_networks[0].node('ApiNode0')
-    witness_node_0      = sub_networks[0].node('WitnessNode0')
+    api_node_0      = sub_networks[0].node('ApiNode0')
+    witness_node_0  = sub_networks[0].node('WitnessNode0')
+    init_node_0     = sub_networks[0].node('InitNode0')
 
-    api_node_1          = sub_networks[1].node('ApiNode1')
-    witness_node_1      = sub_networks[1].node('WitnessNode1')
+    api_node_1      = sub_networks[1].node('ApiNode1')
+    witness_node_1  = sub_networks[1].node('WitnessNode1')
 
     logs = []
 
@@ -27,35 +28,37 @@ def test_obi_throw_exception_02(prepare_obi_throw_exception_02):
     logs.append(fork_log("a1", tt.Wallet(attach_to = api_node_1)))
     logs.append(fork_log("w1", tt.Wallet(attach_to = witness_node_1)))
 
-    blocks                  = 10
-    blocks_after_exception  = 20
-
-    tt.logger.info(f'Before an exception')
-    wait(blocks, logs, witness_node_0)
-
-    _a0 = logs[0].collector
-    last_lib_01                                 = get_last_irreversible_block_num(_a0)
-
-    sleep_time_in_sec = 30
-
-    tt.logger.info(f'Artificial exception is thrown during {sleep_time_in_sec} seconds')
-    witness_node_0.api.debug_node.debug_throw_exception(throw_exception = True)
-    witness_node_1.api.debug_node.debug_throw_exception(throw_exception = True)
-
-    tt.logger.info(f'Before sleep {sleep_time_in_sec}')
-    time.sleep(sleep_time_in_sec)
-    tt.logger.info(f'After sleep {sleep_time_in_sec}')
-
-    tt.logger.info(f'Artificial exception is disabled')
-    witness_node_0.api.debug_node.debug_throw_exception(throw_exception = False)
-    witness_node_1.api.debug_node.debug_throw_exception(throw_exception = False)
-
-    wait(blocks_after_exception, logs, witness_node_0)
-
     _a0 = logs[0].collector
     _w0 = logs[1].collector
     _a1 = logs[2].collector
     _w1 = logs[3].collector
+
+    tt.logger.info(f'Before an exception')
+
+    last_block_before_exception = 121
+    blocks_after_exception      = 20
+    delay_seconds               = 5
+
+    while True:
+        wait(1, logs, witness_node_0)
+        if witness_node_0.get_last_block_number() >= last_block_before_exception:
+            break
+
+    last_lib_01 = get_last_irreversible_block_num(_a0)
+
+    tt.logger.info(f'Artificial exception is thrown during {delay_seconds} seconds')
+    witness_node_0.api.debug_node.debug_throw_exception(throw_exception = True)
+    witness_node_1.api.debug_node.debug_throw_exception(throw_exception = True)
+    init_node_0.api.debug_node.debug_throw_exception(throw_exception = True)
+
+    time.sleep(delay_seconds)
+
+    tt.logger.info(f'Artificial exception is disabled')
+    witness_node_0.api.debug_node.debug_throw_exception(throw_exception = False)
+    witness_node_1.api.debug_node.debug_throw_exception(throw_exception = False)
+    init_node_0.api.debug_node.debug_throw_exception(throw_exception = False)
+
+    wait(blocks_after_exception, logs, witness_node_0)
 
     assert get_last_head_block_number(_a0) > last_lib_01 + 1
 
