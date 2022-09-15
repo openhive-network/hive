85ecd48a50b13cf4889367bbc930951fb44ed899
Bartek Wrona <wrona@syncad.com>
2022-09-15 09:35:20 +0200

    docker_image_exists function can now look for the image also in local image repository, instead of remote registry.

--- scripts/ci-helpers/docker_image_utils.sh
+++ scripts/ci-helpers/docker_image_utils.sh
@@ -25,9 +25,12 @@ function docker_image_exists() {
   local image=$1
   local tag=$2
   local registry=${3}
-  
   local  __resultvar=${4:?"Missing required retval variable name"}
+  local look_to_registry=${5:-1}
+
   local imgname=$( build_image_name $image $tag $registry )
+
+  local result=0
 
   local old_set_e=0
 
@@ -35,12 +38,20 @@ function docker_image_exists() {
   set +e
 
   echo "Looking for: $imgname"
-  docker manifest inspect "$imgname" >/dev/null 2>&1
-  local result=$?
+  
+  if [ "${look_to_registry}" -eq "1" ];
+  then
+    docker manifest inspect "$imgname" >/dev/null 2>&1
+    result=$?
+    echo "docker manifest inspect returned $result"
+  else
+    docker image inspect --format '{{.Id}}' "$imgname" >/dev/null 2>&1
+    result=$?
+    echo "docker image inspect returned $result"
+  fi
 
   ((old_set_e)) && set -e
 
-  echo "docker manifest inspect returned $result"
 
   local EXISTS=0
 
