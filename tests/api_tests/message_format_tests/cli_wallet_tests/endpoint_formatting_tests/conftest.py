cf37420667c197f336bcab5c4f37f5358b6ee7b2
Radek Masłowski <rmaslowski@syncad.com>
2022-09-19 14:11:06 +0200

    Make block_log for account_history tests
    Make script to create block log too.
    Refactor conftest to run node in two stated using "marks" - initnode or apinode repalaye with blocklog

--- tests/api_tests/message_format_tests/cli_wallet_tests/endpoint_formatting_tests/conftest.py
+++ tests/api_tests/message_format_tests/cli_wallet_tests/endpoint_formatting_tests/conftest.py
@@ -1,12 +1,20 @@
+from pathlib import Path
+
 import pytest
 
 import test_tools as tt
 
+
 @pytest.fixture
-def node():
-    node = tt.InitNode()
-    node.run()
-    return node
+def node(request):
+    if request.node.get_closest_marker('replayed_node') is None:
+        node = tt.InitNode()
+        node.run()
+        return node
+
+    api_node = tt.ApiNode()
+    api_node.run(replay_from=Path(__file__).parent.joinpath('block_log/block_log'), wait_for_live=False)
+    return api_node
 
 
 @pytest.fixture
---
+++ tests/api_tests/message_format_tests/cli_wallet_tests/methods_formatting_tests/block_log/generate_block_log.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import test_tools as tt
+
+from local_tools import create_account_and_fund_it
+
+
+def prepare_block_log_with_transactions():
+    node = tt.InitNode()
+    node.run()
+
+    wallet = tt.Wallet(attach_to=node)
+
+    prepare_transactions(wallet)
+
+    # Wait to appear transactions in block log
+    node.wait_number_of_blocks(21)
+
+    node.close()
+
+    node.get_block_log(include_index=False).copy_to(Path(__file__).parent.absolute())
+
+
+def prepare_transactions(wallet) -> None:
+    create_account_and_fund_it(wallet, 'alice', tests=tt.Asset.Test(100), vests=tt.Asset.Test(100))
+
+    wallet.api.post_comment('alice', 'test-permlink', '', 'someone0', 'test-title', 'this is a body', '{}')
+
+    wallet.api.update_account_auth_key('alice', 'owner', tt.Account('some key').public_key, 1)
+
+
+if __name__ == '__main__':
+    prepare_block_log_with_transactions()
