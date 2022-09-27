f91a73bb485186ee7891a7cd5215f271d50655d1
Michał Kudela <kudmich@syncad.com>
2022-09-27 16:28:48 +0200

    Remove node overriding

--- tests/functional/python_tests/transaction_serialization_tests/cli_wallet/conftest.py
+++ tests/functional/python_tests/transaction_serialization_tests/cli_wallet/conftest.py
@@ -5,13 +5,6 @@ import test_tools as tt
 
 
 @pytest.fixture
-def node():
-    node = tt.InitNode()
-    node.run()
-    return node
-
-
-@pytest.fixture
 def prepared_wallet(node, request):
     if request.param == 'modern':
         yield tt.Wallet(attach_to=node, additional_arguments=['--transaction-serialization=hf26'])
