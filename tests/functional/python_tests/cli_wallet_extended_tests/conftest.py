c3a8c8f9f737a977097f4df89d5792cee9388054
Michał Kudela <kudmich@syncad.com>
2022-09-27 16:28:48 +0200

    Remove node overriding

--- tests/functional/python_tests/cli_wallet_extended_tests/conftest.py
+++ tests/functional/python_tests/cli_wallet_extended_tests/conftest.py
@@ -4,12 +4,5 @@ import test_tools as tt
 
 
 @pytest.fixture
-def node() -> tt.InitNode:
-    init_node = tt.InitNode()
-    init_node.run()
-    return init_node
-
-
-@pytest.fixture
 def wallet(node) -> tt.Wallet:
     return tt.Wallet(attach_to=node)
