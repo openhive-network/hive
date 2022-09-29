b2a1f22ecd4a3a93c9bd0f7eae56886baa1b9e35
Bartek Wrona <wrona@syncad.com>
2022-09-29 23:17:42 +0200

    DOCKER_BUILDKIT must be enabled, to allow files extraction from image.

--- scripts/ci-helpers/export-binaries.sh
+++ scripts/ci-helpers/export-binaries.sh
@@ -9,6 +9,8 @@ EXPORT_PATH=${2:-"Missing export target directory"}
 
 echo "Attempting to export built binaries from image: ${IMAGE_TAGGED_NAME} into directory: ${EXPORT_PATH}"
 
+export DOCKER_BUILDKIT=1
+
 docker build -o "${EXPORT_PATH}" - << EOF
     FROM scratch
     COPY --from=${IMAGE_TAGGED_NAME} /home/hived/bin/ /
