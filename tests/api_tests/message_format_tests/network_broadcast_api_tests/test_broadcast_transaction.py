8dc795f7c858579788055400e2db030752849324
Michał Kudela <kudmich@syncad.com>
2022-10-03 14:03:46 +0200

    Add tests incorrect_type_of_arguments and additional_argumens to broadcasting tests

--- tests/api_tests/message_format_tests/network_broadcast_api_tests/test_broadcast_transaction.py
+++ tests/api_tests/message_format_tests/network_broadcast_api_tests/test_broadcast_transaction.py
@@ -1,3 +1,5 @@
+import pytest
+
 import test_tools as tt
 
 from ..local_tools import run_for
@@ -10,3 +12,17 @@ def test_broadcast_transaction(prepared_node):
     alice_creation_transaction = wallet.api.create_account('initminer', 'alice', '{}', broadcast=False)
 
     prepared_node.api.network_broadcast.broadcast_transaction(trx=alice_creation_transaction)
+
+
+@pytest.mark.parametrize(
+    'transaction_name', [
+        ['non-exist-transaction'],
+        'non-exist-transaction',
+        100,
+        True
+    ]
+)
+@run_for('testnet')
+def test_broadcast_transaction_with_incorrect_type_of_argument(prepared_node, transaction_name):
+    with pytest.raises(tt.exceptions.CommunicationError):
+        prepared_node.api.network_broadcast.broadcast_transaction(trx=transaction_name)
