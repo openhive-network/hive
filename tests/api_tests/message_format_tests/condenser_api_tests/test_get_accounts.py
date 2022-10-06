f44a39fdea2864d126050fd048b99b0b4d34e3eb
Michał Kudela <kudmich@syncad.com>
2022-10-06 14:15:41 +0200

    Add test for incorrect_type_of_argument in condenser::get_accounts method

--- tests/api_tests/message_format_tests/condenser_api_tests/test_get_accounts.py
+++ tests/api_tests/message_format_tests/condenser_api_tests/test_get_accounts.py
@@ -1,3 +1,5 @@
+import pytest
+
 import test_tools as tt
 
 from ..local_tools import run_for
@@ -9,3 +11,17 @@ def test_get_accounts(prepared_node, should_prepare):
         wallet = tt.Wallet(attach_to=prepared_node)
         wallet.api.create_account('initminer', 'gtg', '{}')
     prepared_node.api.condenser.get_accounts(['gtg'])
+
+
+@pytest.mark.parametrize(
+    'account_key', [
+        [['array-as-argument']],
+        100,
+        True,
+        'incorrect_string_argument'
+    ]
+)
+@run_for('testnet', 'mainnet_5m', 'mainnet_64m')
+def test_get_accounts_with_incorrect_type_of_argument(prepared_node, account_key):
+    with pytest.raises(tt.exceptions.CommunicationError):
+        prepared_node.api.condenser.get_accounts(account_key)
