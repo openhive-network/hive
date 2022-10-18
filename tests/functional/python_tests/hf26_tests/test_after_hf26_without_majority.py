622b7707550fc918a6a96d674aeb1e4d636490e0
Mariusz <mtrela@syncad.com>
2022-10-18 12:12:02 +0200

    All tests from `hf26_tests` work excluding `test_after_hf26_without_majority` test

---
+++ tests/functional/python_tests/hf26_tests/test_after_hf26_without_majority.py
@@ -0,0 +1,7 @@
+from .local_tools import prepare_wallets, legacy_operation_passed, hf26_operation_failed
+
+def test_after_hf26_without_majority(network_after_hf26_without_majority):
+    wallet_legacy, wallet_hf26 = prepare_wallets(network_after_hf26_without_majority)
+
+    legacy_operation_passed(wallet_legacy)
+    hf26_operation_failed(wallet_hf26)
