3b89914a0d73557c65a5ebb8a3c67a7c5a80c18b
Michał Kudela <kudmich@syncad.com>
2022-10-19 12:46:45 +0200

    Prepare `prepared node` to handle tests with different types nodes (testnet, mainnet_5m, mainnet_64m)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from typing import List, Optional, Union
+
 import pytest
 
 from test_tools.__private.scope.scope_fixtures import *  # pylint: disable=wildcard-import, unused-wildcard-import
@@ -9,11 +11,48 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture
-def prepared_node(request):
-    def __create_init_node():
+def prepared_node(request) -> Union[tt.InitNode, tt.RemoteNode]:
+    def __create_init_node() -> tt.InitNode:
         init_node = tt.InitNode()
         init_node.run()
         return init_node
+
+    def __assert_no_duplicated_requests_of_same_node() -> None:
+        requested_markers: List[str] = [marker.name for marker in request.node.iter_markers() if marker.name != 'parametrize']
+        if len(requested_markers) != len(set(requested_markers)):
+            raise RuntimeError(f'Duplicated marker. You used two or more times markers: {requested_markers}. Do not use duplicates.')
+
+    def __assert_no_duplicated_node_in_run_for_params() -> None:
+        for marker in request.node.iter_markers():
+            if marker.name == 'parametrize' and marker.args[0] == 'node':
+                number_of_nodes_specified_in_run_for = len(marker.args[1])
+                number_of_unique_nodes_given_in_the_run_for_parameters = len(set([marker_obj.values[0][0] for marker_obj in marker.args[1]]))
+
+                if number_of_nodes_specified_in_run_for != number_of_unique_nodes_given_in_the_run_for_parameters:
+                    raise RuntimeError(f"Duplicate requested `nodes` in `@run_for' function parameters."
+                                       f" Use only defined nodes: {', '.join(create_node.keys())}.")
+
+    def __get_requested_node() -> Optional[str]:
+        requested_nodes: List[str] = [request.node.get_closest_marker(node).name for node in create_node if
+                                      request.node.get_closest_marker(node) is not None]
+
+        if len(requested_nodes) > 1:
+            raise RuntimeError(f'Incorrect test marking. You cannot mark mixing function `@run_for` with classic mark.\n'
+                               f'To the mark multiple node test, use decorator:\n'
+                               f'@run_for({", ".join(requested_nodes)})\n'
+                               f'def {request.node.originalname}(node):\n'
+                               f'    <do something>')
+
+        return requested_nodes[0] if len(requested_nodes) == 1 else None
+
+    def __run_for_was_used() -> bool:
+        marker_names = [marker for marker in request.node.iter_markers()]
+        for marker in marker_names:
+            if marker.name == 'parametrize' and marker.args[0] == 'node':
+                for mark_name in marker.args[1]:
+                    if marker_names.count(mark_name[1][0].mark) == 1:
+                        return True
+        return False
 
     create_node = {
         'testnet': __create_init_node,
@@ -21,15 +60,25 @@ def prepared_node(request):
         'mainnet_64m': lambda: tt.RemoteNode(http_endpoint=request.config.getoption("--http-endpoint")),
     }
 
-    requested_node = request.param[0]
-    try:
-        yield create_node[requested_node]()
-    except KeyError as exception:
-        supported_nodes = ', '.join(f'"{name}"' for name in create_node.keys())
+    __assert_no_duplicated_requests_of_same_node()
+    requested_nodes: List[str] = [request.node.get_closest_marker(node).name for node in create_node
+                                  if request.node.get_closest_marker(node) is not None]
+
+    if __run_for_was_used():
+        __assert_no_duplicated_node_in_run_for_params()
+        requested_node = __get_requested_node()
+        return create_node[requested_node]()
+
+    if requested_nodes:
         raise RuntimeError(
-            f'Requested node name "{requested_node}" is not supported. '
-            f'Use one of following: {supported_nodes}.'
-        ) from exception
+            f"Incorrect test marking. Use the `@run_for` function to mark the test. Available nodes: {', '.join(create_node.keys())}.\n"
+            f"To mark correctly your test use syntax:\n"
+            f"@run_for({', '.join(requested_nodes)})\n"
+            f"def {request.node.originalname}(node):\n"
+            f"    <do something>"
+        )
+    if list(request.node.iter_markers()) == [] or request.node.get_closest_marker('parametrize') is not None:
+        return create_node['testnet']()
 
 
 @pytest.fixture
