db785f4b897df3331483808232391fc0b4645843
Michał Kudela <kudmich@syncad.com>
2022-10-24 13:52:09 +0200

    Extend universal node to handle replayed-node cases

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/conftest.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/conftest.py
@@ -10,12 +10,5 @@ def pytest_configure(config):
 
 
 @pytest.fixture
-def replayed_node():
-    api_node = tt.ApiNode()
-    api_node.run(replay_from=Path(__file__).parent.joinpath('block_log/block_log'), wait_for_live=False)
-    return api_node
-
-
-@pytest.fixture
 def wallet(node):
     return tt.Wallet(attach_to=node)
--- tests/local_tools.py
+++ tests/local_tools.py
@@ -33,6 +33,23 @@ def date_from_now(*, weeks):
     return future_data.strftime('%Y-%m-%dT%H:%M:%S')
 
 
+def replay_prepared_block_log(func):
+    """
+    Use of a decorator allows you to use the available a local block log. Each test case is marked with
+     `pytest.mark.replay_prepared_block_log`,
+
+    Definition local block_log is available here:
+      /hive/tests/api_tests/message_format_tests/wallet_bridge_api_tests/block_log/generate_block_log.py
+
+    Usage:
+    @replay_prepared_block_log
+    @run_for('testnet', 'mainnet_5m')
+    def test_with_available_block_log(prepared_node):
+        pass
+    """
+    return pytest.mark.replay_prepared_block_log(func)
+
+
 def run_for(*node_names: Literal['testnet', 'mainnet_5m', 'mainnet_64m']):
     """
     Runs decorated test for each node specified as parameter.
