287c22df7cf602d6c659a81983840007bd9190d2
Michał Kudela <kudmich@syncad.com>
2022-10-25 13:27:15 +0200

    [TEST]pytest option duplication remove

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,8 @@ import test_tools as tt
 
 def pytest_addoption(parser):
     parser.addoption("--http-endpoint", action="store", type=str, help='specifies http_endpoint of reference node')
+    parser.addoption("--ws-endpoint", action="store", type=str, help='specifies ws_endpoint of reference node')
+    parser.addoption("--wallet-path", action="store", type=str, help='specifies path of reference cli wallet')
 
 
 @pytest.fixture
