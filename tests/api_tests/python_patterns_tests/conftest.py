7b7d4afb61a87385e1af17ab150c66ce91814f92
Michał Kudela <kudmich@syncad.com>
2022-10-27 14:58:41 +0200

    remove duplicate definition `pytest option (--http-endpoint)`

--- tests/api_tests/python_patterns_tests/conftest.py
+++ tests/api_tests/python_patterns_tests/conftest.py
@@ -2,6 +2,5 @@ from test_tools.__private.scope.scope_fixtures import *
 
 
 def pytest_addoption(parser):
-    parser.addoption("--http-endpoint", action="store", type=str, help='specifies http_endpoint of reference node')
     parser.addoption("--ws-endpoint", action="store", type=str, help='specifies ws_endpoint of reference node')
     parser.addoption("--wallet-path", action="store", type=str, help='specifies path of reference cli wallet')
