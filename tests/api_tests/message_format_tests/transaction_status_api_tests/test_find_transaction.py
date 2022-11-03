87d70425805e40933a5d534486a70973e2f2e48c
Michał Kudela <kudmich@syncad.com>
2022-11-03 09:58:07 +0100

    Add message format test for transaction_status::find_transaction method

---
+++ tests/api_tests/message_format_tests/transaction_status_api_tests/test_find_transaction.py
@@ -0,0 +1,17 @@
+import test_tools as tt
+
+from ....local_tools import run_for
+
+
+@run_for("testnet", "mainnet_5m", "mainnet_64m")
+def test_find_transaction(node, should_prepare):
+    if should_prepare:
+        wallet = tt.Wallet(attach_to=node)
+        wallet.create_account("gtg")
+    head_block_number = node.api.wallet_bridge.get_dynamic_global_properties()["head_block_number"]
+    operation = node.api.account_history.get_ops_in_block(block_num=head_block_number, include_reversible=True)
+
+    assert len(operation["ops"]) > 0, f"Missing transactions in block {head_block_number}"
+
+    transaction_id = operation["ops"][-1]["trx_id"]
+    node.api.transaction_status.find_transaction(transaction_id=transaction_id)
