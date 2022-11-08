948a9bfbbe0e3d2a6ad1ba3ed2e35feb43324ca8
Michał Kudela <kudmich@syncad.com>
2022-11-08 14:04:12 +0100

    Add tests for new methods `RC API` #issue417

---
+++ tests/api_tests/message_format_tests/rc_api_tests/test_get_rc_operation_stats.py
@@ -0,0 +1,7 @@
+from ..local_tools import run_for
+
+
+@run_for('testnet')
+def test_get_rc_stats(prepared_node, should_prepare):
+    a = prepared_node.api.rc.get_rc_operation_stats(operation="vote_operation")
+    print()
