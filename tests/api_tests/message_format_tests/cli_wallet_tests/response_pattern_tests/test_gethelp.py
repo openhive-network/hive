bcda225d83088a05083875ec73540e4b66bfb6a0
Radek Masłowski <rmaslowski@syncad.com>
2022-11-16 10:32:58 +0100

    Modify gethelp pattern tests to cover all methods instead gethelp functional test

--- tests/api_tests/message_format_tests/cli_wallet_tests/response_pattern_tests/test_gethelp.py
+++ tests/api_tests/message_format_tests/cli_wallet_tests/response_pattern_tests/test_gethelp.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 import re
 
+import pytest
+
 import test_tools as tt
+
+from ..local_tools import verify_text_patterns
 
 
 __PATTERNS_DIRECTORY = Path(__file__).with_name('gethelp_response_patterns')
@@ -9,38 +13,16 @@ __PATTERNS_DIRECTORY = Path(__file__).with_name('gethelp_response_patterns')
 __HELP_CONTENT_DIRECTORY = Path(__file__).with_name('help_response_patterns')
 
 
-def test_gethelp(wallet: tt.Wallet):
-    help_functions = __read_and_parse_help_content()
-    failed_functions = []
-    for function in help_functions:
-        try:
-            wallet.api.gethelp(function)
-        except tt.exceptions.CommunicationError:
-            failed_functions.append(function)
-    if len(failed_functions) > 0:
-        assert False, f'Error occurred when gethelp was called for following functions: {failed_functions}'
-
-def test_gethelp_with_get_order_book_argument(wallet):
-    response = wallet.api.gethelp('get_order_book')
-    response_pattern = __read_from_text_pattern('get_order_book')
-
-    assert response == response_pattern
-
-
-def test_gethelp_with_create_account_argument(wallet):
-    response = wallet.api.gethelp('create_account')
-    response_pattern = __read_from_text_pattern('create_account')
-
-    assert response == response_pattern
-
-
-def __read_from_text_pattern(method_name: str) -> str:
-    with open(f'{__PATTERNS_DIRECTORY}/{method_name}.pat.txt', 'r') as text_file:
-        return text_file.read()
-
-
 def __read_and_parse_help_content() -> list:
     with open(f'{__HELP_CONTENT_DIRECTORY}/help.pat.txt', 'r') as text_file:
         help_content = text_file.read()
         # saparate names of functions from "help"
         return [re.match(r'.* ([\w_]+)\(.*', line)[1] for line in help_content.split('\n')[:-1]]
+
+
+@pytest.mark.parametrize(
+    'method', __read_and_parse_help_content()
+)
+def test_gethelp(wallet: tt.Wallet, method):
+    response = wallet.api.gethelp(method)
+    verify_text_patterns(__PATTERNS_DIRECTORY, method, response)
