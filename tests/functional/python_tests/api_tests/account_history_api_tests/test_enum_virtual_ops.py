64e0855684e415207b8063196bc659647aaea32f
Krzysztof Mochocki <kmochocki@syncad.com>
2022-12-02 12:34:36 +0100

    Add first test for enum virtual ops

--- tests/functional/python_tests/api_tests/account_history_api_tests/test_enum_virtual_ops.py
+++ tests/functional/python_tests/api_tests/account_history_api_tests/test_enum_virtual_ops.py
@@ -1,4 +1,8 @@
+import pytest
 from dataclasses import dataclass
+
+import test_tools as tt
+
 from ..local_tools import BaseArgs
 
 @dataclass
@@ -11,3 +15,18 @@ class EnumVirtualOpsArgs(BaseArgs):
     limit: int = None
     filter: int = None
 
+
+@pytest.mark.parametrize('group_by_block, key', (
+    (False, 'ops'),
+    (True, 'ops_by_block')
+))
+def test_no_virtual_operations(ah_node: tt.InitNode, group_by_block: bool, key: str):
+    ah_node.wait_number_of_blocks(5)
+    block_to_start = ah_node.get_last_block_number() - 3
+    response = ah_node.api.account_history.enum_virtual_ops(**EnumVirtualOpsArgs(
+        block_range_begin=block_to_start,
+        block_range_end=block_to_start + 1
+    ).as_dict())
+    assert len(response[key]) == 0
+    assert response['next_block_range_begin'] == 0
+    assert response['next_operation_begin'] == 0
