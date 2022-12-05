0d815aeac49c764dfbe4fd70b8171ee1a63168d7
Mateusz Żebrak <mzebrak@syncad.com>
2022-12-05 09:08:47 +0100

    STASH

--- tests/functional/python_tests/hived/block_log/generate_block_log.py
+++ tests/functional/python_tests/hived/block_log/generate_block_log.py
@@ -1,10 +1,14 @@
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 from copy import deepcopy
 import math
 from pathlib import Path
-from typing import List
+from typing import Final, List
 
 import test_tools as tt
+
+AMOUNT_OF_ALL_ACCOUNTS: Final[int] = 20_000
+ACCOUNTS_PER_CHUNK: Final[int] = 750
+MAX_WORKERS: Final[int] = 6
 
 
 def prepare_node_with_proposal_votes():
@@ -12,9 +16,33 @@ def prepare_node_with_proposal_votes():
     node.run()
     wallet = tt.Wallet(attach_to=node)
 
-    amount_of_all_accounts = 20_000
-    accounts = get_account_names(wallet.create_accounts(amount_of_all_accounts))
+    account_names = [account.name for account in wallet.create_accounts(AMOUNT_OF_ALL_ACCOUNTS)]
 
+    __create_proposal(wallet)
+
+    with ThreadPoolExecutor(
+        max_workers=min(math.ceil(AMOUNT_OF_ALL_ACCOUNTS / ACCOUNTS_PER_CHUNK), MAX_WORKERS)
+    ) as executor:
+        for lower in range(0, AMOUNT_OF_ALL_ACCOUNTS, ACCOUNTS_PER_CHUNK):
+            upper = min(AMOUNT_OF_ALL_ACCOUNTS, lower + ACCOUNTS_PER_CHUNK)
+            executor.submit(__generate_and_broadcast, wallet, account_names[lower:upper])
+            tt.logger.info(f"Pack generated: {lower}:{upper}")
+
+    tt.logger.info("Waiting 21 blocks for the blocks with the transactions to become irreversible")
+    node.wait_number_of_blocks(21)
+
+    head_block_num = node.get_last_block_number()
+    timestamp = node.api.block.get_block(block_num=head_block_num)["block"]["timestamp"]
+    tt.logger.info(f"head block timestamp: {timestamp}")
+
+    with open("timestamp", "w", encoding="utf-8") as file:
+        file.write(f"{timestamp}")
+
+    node.close()
+    node.block_log.copy_to(Path(__file__).parent)
+
+
+def __create_proposal(wallet: tt.Wallet) -> None:
     wallet.create_account("alice", hives=tt.Asset.Test(100), vests=tt.Asset.Test(100), hbds=tt.Asset.Tbd(100))
     wallet.api.post_comment("alice", "permlink", "", "parent-permlink", "title", "body", "{}")
     wallet.api.create_proposal(
@@ -27,68 +55,35 @@ def prepare_node_with_proposal_votes():
         "permlink",
     )
 
+
+def __generate_operations_for_receiver(receiver: str, amount: tt.Asset.Test = tt.Asset.Test(0.1)) -> list:
+    return [
+        ["transfer_to_vesting", {"from": "initminer", "to": receiver, "amount": str(amount)}],
+        ["update_proposal_votes", {"voter": receiver, "proposal_ids": [0], "approve": True, "extensions": []}],
+    ]
+
+
+def __generate_and_broadcast(wallet: tt.Wallet, account_names: List[str]) -> None:
     transaction_template = {
-        'ref_block_num': 0,
-        'ref_block_prefix': 0,
-        'expiration': '2022-11-23T11:55:36',
-        'operations': [],
-        'extensions': [],
-        'signatures': [],
-        'transaction_id': '0000000000000000000000000000000000000000',
-        'block_num': 0,
-        'transaction_num': 0
+        "ref_block_num": 0,
+        "ref_block_prefix": 0,
+        "expiration": "2022-11-23T11:55:36",
+        "operations": [],
+        "extensions": [],
+        "signatures": [],
+        "transaction_id": "0000000000000000000000000000000000000000",
+        "block_num": 0,
+        "transaction_num": 0,
     }
 
-    def generate_operations_for_receiver(receiver: str, amount=tt.Asset.Test(0.1)) -> list:
-        return [
-            ['transfer_to_vesting', {'from': 'initminer', 'to': receiver, 'amount': str(amount)}],
-            ['update_proposal_votes', {'voter': receiver, 'proposal_ids': [0], 'approve': True, 'extensions': []}]
-        ]
+    transaction = deepcopy(transaction_template)
 
-    def generate_and_broadcast(acc_num_start, acc_num_stop):
-        accs = list(accounts[acc_num_start:acc_num_stop])
-        transaction = deepcopy(transaction_template)
-        for name in accs:
-            transaction['operations'].extend(generate_operations_for_receiver(name))
-        try:
-            wallet.api.sign_transaction(transaction)
-        except Exception as e:
-            tt.logger.error(e)
-            raise e
-        else:
-            tt.logger.info(f'Finished: {accs[0]}')
-        return accs[0]
+    for name in account_names:
+        transaction["operations"].extend(__generate_operations_for_receiver(name))
 
-    account_per_chunk = 750
-    max_threads = 6
-    futures = []
-    with concurrent.futures.ThreadPoolExecutor(
-            max_workers=min(math.ceil(amount_of_all_accounts / account_per_chunk), max_threads)
-    ) as executor:
-        for n in range(0, len(accounts), account_per_chunk):
-            futures.append(executor.submit(generate_and_broadcast, n, n + account_per_chunk))
-            tt.logger.info(f"Pack generated: {n}")
-
-        for future in futures:
-            tt.logger.info(f'Got: {future.result()}')
-
-    node.wait_number_of_blocks(21)
-
-    result = wallet.api.info()
-    head_block_num = result['head_block_number']
-    timestamp = node.api.block.get_block(block_num=head_block_num)['block']['timestamp']
-    tt.logger.info(f'head block timestamp: {timestamp}')
-
-    with open('timestamp', 'w') as f:
-        f.write(f'{timestamp}')
-
-    node.close()
-    node.block_log.copy_to(Path(__file__).parent.absolute())
+    wallet.api.sign_transaction(transaction)
+    tt.logger.info(f"Finished: {account_names[-1]}")
 
 
-def get_account_names(accounts: List[tt.Account]) -> List[str]:
-    return [account.name for account in accounts]
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     prepare_node_with_proposal_votes()
