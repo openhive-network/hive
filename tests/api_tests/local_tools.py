4faa11f07f1271a058e6243d7883849b420d469c
Mateusz Kudela <mkudela@syncad.com>
2022-12-13 09:17:33 +0100

    Empty commit

--- tests/api_tests/local_tools.py
+++ tests/api_tests/local_tools.py
@@ -11,4 +11,5 @@ def read_from_json_pattern(directory: Path, method_name: str) -> Dict[str, Any]:
 def write_to_json_pattern(directory: Path, method_name: str, json_response) -> None:
     directory.mkdir(parents=True, exist_ok=True)
     with open(directory / f'{method_name}.pat.json', 'w') as json_file:
+        print()
         json.dump(json_response, json_file)
