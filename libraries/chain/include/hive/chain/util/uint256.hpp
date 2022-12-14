4f4a41c649d20b9c6589c12f96c06ca33cd9f2a9
Marek <marek@syncad.com>
2022-12-14 17:22:09 +0000

    Change libraries/chain/include/hive/chain/util/uint256.hpp according to change fc::unit128 -> __uint128_t

--- libraries/chain/include/hive/chain/util/uint256.hpp
+++ libraries/chain/include/hive/chain/util/uint256.hpp
@@ -8,9 +8,9 @@ namespace hive { namespace chain { namespace util {
 
 inline u256 to256( const fc::uint128& t )
 {
-  u256 v(t.hi);
+  u256 v(fc::uint128_high_bits(t));
   v <<= 64;
-  v += t.lo;
+  v |= fc::uint128_low_bits(t);
   return v;
 }
 
