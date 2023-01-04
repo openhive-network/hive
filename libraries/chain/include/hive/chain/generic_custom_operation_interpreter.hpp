0ee91d26b3fba5da0ea69042e89e3f3f285156d4
Bartek Wrona <wrona@syncad.com>
2023-01-04 22:00:33 +0100

    chain generic_custom_operation_interpreter code adjusted to change done in unpack_from_vector function.

--- libraries/chain/include/hive/chain/generic_custom_operation_interpreter.hpp
+++ libraries/chain/include/hive/chain/generic_custom_operation_interpreter.hpp
@@ -259,11 +259,13 @@ class generic_custom_operation_interpreter
 
         try
         {
-          custom_operations = fc::raw::unpack_from_vector< vector< CustomOperationType > >( outer_o.data );
+          fc::raw::unpack_from_vector( outer_o.data, custom_operations );
         }
         catch ( fc::exception& )
         {
-          custom_operations.push_back( fc::raw::unpack_from_vector< CustomOperationType >( outer_o.data ) );
+          CustomOperationType tmp;
+          fc::raw::unpack_from_vector(outer_o.data, tmp);
+          custom_operations.push_back(tmp);
         }
 
         apply_operations( custom_operations, operation( outer_o ) );
