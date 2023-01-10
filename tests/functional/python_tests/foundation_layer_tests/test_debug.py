7bb5630eb4e19a81e9f0293f0609c144c4d94ca6
Michał Kudela <kudmich@syncad.com>
2023-01-10 13:49:40 +0100

    Move class VestPrice to upper directory

--- tests/functional/python_tests/foundation_layer_tests/test_debug.py
+++ tests/functional/python_tests/foundation_layer_tests/test_debug.py
@@ -2,23 +2,9 @@ import pytest
 import test_tools as tt
 
 from hive_local_tools import run_for
+from hive_local_tools.functional import VestPrice
 
-class VestPrice:
-    def __init__(self, quote : tt.Asset.Vest, base : tt.Asset.Test) -> None:
-        self.base = base
-        self.quote = quote
 
-    @classmethod
-    def from_dgpo(self, dgpo : dict):
-        return self(quote=tt.Asset.Vest(dgpo['total_vesting_shares']['amount']),
-          base=tt.Asset.Test(dgpo['total_vesting_fund_hive']['amount']))
-    
-    def __str__(self) -> str:
-        q = int(self.quote.amount)
-        b = int(self.base.amount)
-        p = q/b
-        p /= 10**self.base.precision
-        return f"{p} {self.quote.token}/{self.base.token}"
 
     def as_nai(self) -> dict:
         return dict({
