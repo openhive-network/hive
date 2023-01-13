bde78862f29cf911a2dda6c56e24f62ceac6f7d8
Konrad Botor <kbotor@syncad.com>
2023-01-13 13:07:29 +0100

    Updated build_data4commit.sh to account for the way it's invoked in haf> pipeline - ref. #406

--- scripts/ci-helpers/build_data4commit.sh
+++ scripts/ci-helpers/build_data4commit.sh
@@ -7,11 +7,8 @@ SCRIPTSDIR="$SCRIPTPATH/.."
 # shellcheck disable=SC2034 
 LOG_FILE=build_data4commit.log
 
-DIRECTORY_PREFIX=${DIRECTORY_PREFIX:"hive-"}
-GIT_REPOSITORY=${GIT_REPOSITORY:"https://gitlab.syncad.com/hive/hive.git"}
-
-echo $DIRECTORY_PREFIX
-echo $GIT_REPOSITORY
+DIRECTORY_PREFIX=${DIRECTORY_PREFIX:-"hive-"}
+GIT_REPOSITORY=${GIT_REPOSITORY:-"https://gitlab.syncad.com/hive/hive.git"}
 
 # shellcheck source=../common.sh
 source "$SCRIPTSDIR/common.sh"
