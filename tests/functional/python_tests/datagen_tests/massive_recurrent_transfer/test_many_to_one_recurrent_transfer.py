af95d13db5b992696ca18281d3db9640bc2ab5c6
Michał Kudela <kudmich@syncad.com>
2023-01-18 13:18:07 +0100

    Use local const in block log generate script (many to one)

--- tests/functional/python_tests/datagen_tests/massive_recurrent_transfer/test_many_to_one_recurrent_transfer.py
+++ tests/functional/python_tests/datagen_tests/massive_recurrent_transfer/test_many_to_one_recurrent_transfer.py
@@ -4,14 +4,12 @@ from pathlib import Path
 import test_tools as tt
 
 from hive_local_tools.functional.python.datagen.massive_recurrent_transfer import ReplayedNodeMaker
-
+from hive_local_tools.constants import MAX_RECURRENT_TRANSFERS_PER_BLOCK
+from .block_logs.block_log_containing_many_to_one_recurrent_transfers import generate_block_log as bl
 
 def test_many_to_one_recurrent_transfer(replayed_node: ReplayedNodeMaker):
-    receiver_accounts = ["bank"]
-    base_accounts = ['hive.fund', 'initminer', 'miners', 'null', 'steem.dao', 'temp']
-
-    block_log_directory = Path(__file__).parent / "block_logs/block_log_containing_many_to_one_recurrent_transfers"
-    with open(block_log_directory / "timestamp", encoding='utf-8') as file:
+    block_log_directory = Path(bl.__file__).parent
+    with open(block_log_directory / "timestamp", encoding="utf-8") as file:
         timestamp = tt.Time.parse(file.read())
 
     replayed_node = replayed_node(
@@ -19,13 +17,12 @@ def test_many_to_one_recurrent_transfer(replayed_node: ReplayedNodeMaker):
         absolute_start_time=timestamp + tt.Time.days(2),
         time_multiplier=50,
     )
-    wallet = tt.Wallet(attach_to=replayed_node)
 
-    number_of_created_accounts = len(wallet.list_accounts()) - len(base_accounts) - len(receiver_accounts)
-    replayed_node.wait_number_of_blocks(math.ceil(number_of_created_accounts / 1000))
+    replayed_node.wait_number_of_blocks(math.ceil(bl.NUMBER_OF_SENDER_ACCOUNTS / MAX_RECURRENT_TRANSFERS_PER_BLOCK))
 
-    bank_balance_after_the_second_top_up_with_recurrent_transfers = (
-        replayed_node.api.wallet_bridge.get_account("bank")["balance"]
+    receiver_balance_after_the_second_top_up = (
+        replayed_node.api.condenser.get_accounts([bl.RECEIVER_ACCOUNT_NAME])[0]["balance"]
     )
 
-    assert int(bank_balance_after_the_second_top_up_with_recurrent_transfers['amount']) == number_of_created_accounts * 2
+    expected_balance = bl.NUMBER_OF_SENDER_ACCOUNTS * bl.SINGLE_TRANSFER_AMOUNT * 2
+    assert receiver_balance_after_the_second_top_up == expected_balance
