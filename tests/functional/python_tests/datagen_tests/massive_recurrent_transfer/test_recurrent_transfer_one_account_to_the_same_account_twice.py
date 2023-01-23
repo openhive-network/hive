f121be45bc4014e9c9d148e679a33050cc28799f
Michał Kudela <kudmich@syncad.com>
2023-01-23 09:33:17 +0100

    Add test for recurrent transfer from one account to the same account

---
+++ tests/functional/python_tests/datagen_tests/massive_recurrent_transfer/test_recurrent_transfer_one_account_to_the_same_account_twice.py
@@ -0,0 +1,25 @@
+import test_tools as tt
+
+from hive_local_tools import run_for
+
+
+@run_for("testnet")
+def test_recurrent_transfer_one_account_to_the_same_account_twice(node):
+    # The second recurrent transfer to the same account is overwriting parameters of the first recurrent transfer
+    wallet = tt.Wallet(attach_to=node)
+
+    wallet.create_account("sender",
+                          hives=tt.Asset.Test(100),
+                          vests=tt.Asset.Test(100))
+
+    wallet.create_account("receiver")
+    with wallet.in_single_transaction():
+        for i in range(2):
+            wallet.api.recurrent_transfer("sender",
+                                          "receiver",
+                                          tt.Asset.Test(0.001),
+                                          f"recurrent transfer to receiver",
+                                          24+i,
+                                          2)
+
+    assert len(wallet.api.find_recurrent_transfers("sender")) == 1
