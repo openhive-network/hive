e08b852868c1c823839df8f298a85efcd510f754
Michał Kudela <kudmich@syncad.com>
2023-01-23 09:38:36 +0100

    Add test for cancellation of a recurrent transfer

---
+++ tests/functional/python_tests/datagen_tests/massive_recurrent_transfer/test_cancel_recurrent_transfer.py
@@ -0,0 +1,31 @@
+import test_tools as tt
+
+from hive_local_tools import run_for
+
+
+@run_for("testnet")
+def test_cancel_recurrent_transfer(node):
+    # To cancel recurrent transfer make this transfer with hive amount equals 0
+    wallet = tt.Wallet(attach_to=node)
+
+    wallet.create_account('sender',
+                          hives=tt.Asset.Test(100),
+                          vests=tt.Asset.Test(100))
+
+    wallet.create_account('receiver')
+
+    wallet.api.recurrent_transfer('sender',
+                                  'receiver',
+                                  tt.Asset.Test(0.001),
+                                  f'recurrent transfer to receiver',
+                                  24,
+                                  2)
+    assert len(wallet.api.find_recurrent_transfers('sender')) == 1
+
+    wallet.api.recurrent_transfer('sender',
+                                  'receiver',
+                                  tt.Asset.Test(0),
+                                  f'recurrent transfer to receiver',
+                                  24,
+                                  2)
+    assert len(wallet.api.find_recurrent_transfers('sender')) == 0
