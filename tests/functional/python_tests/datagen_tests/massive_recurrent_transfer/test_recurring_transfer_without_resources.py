bc35a09e89f1eff53ac537ba372d34a0af041219
Michał Kudela <kudmich@syncad.com>
2023-01-23 09:51:21 +0100

    Add test for recurrent transfer without resources

---
+++ tests/functional/python_tests/datagen_tests/massive_recurrent_transfer/test_recurring_transfer_without_resources.py
@@ -0,0 +1,25 @@
+import pytest
+
+import test_tools as tt
+from hive_local_tools import run_for
+
+
+@run_for("testnet")
+def test_recurrent_transfer_without_resources(node):
+    # "sender" want to order recurrent transfer, but it does not have resources
+    wallet = tt.Wallet(attach_to=node)
+
+    executions = 2
+
+    wallet.create_account("sender")
+
+    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+        wallet.api.recurrent_transfer("sender",
+                                      "initminer",
+                                      tt.Asset.Test(100),
+                                      f"recurrent transfer to receiver",
+                                      24,
+                                      executions)
+
+    expected_error_message = f"Account does not have enough tokens for the first transfer, has"
+    assert expected_error_message in str(exception.value)
