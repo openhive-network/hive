08b0c420ccacc78175e9429b28958435f80e90f1
Lucius <lucius@syncad.com>
2023-01-26 00:48:55 +0100

    Added check of escrow_release_operation to account_history_by_condenser_test

--- tests/unit/db_fixture/database_fixture.cpp
+++ tests/unit/db_fixture/database_fixture.cpp
@@ -927,6 +927,25 @@ void database_fixture::escrow_approve( const string& from, const string& to, con
   push_transaction( tx, key );
 }
 
+void database_fixture::escrow_release( const string& from, const string& to, const string& agent, const string& who,
+                                       const string& receiver, const asset& hive_amount, 
+                                       const asset& hbd_amount, const fc::ecc::private_key& key )
+{
+  escrow_release_operation op;
+  op.from = from;
+  op.to = to;
+  op.agent = agent;
+  op.who = who;
+  op.receiver = receiver;
+  op.hive_amount = hive_amount;
+  op.hbd_amount = hbd_amount;
+
+  signed_transaction tx;
+  tx.operations.push_back( op );
+  tx.set_expiration( db->head_block_time() + HIVE_MAX_TIME_UNTIL_EXPIRATION );
+  push_transaction( tx, key );
+}
+
 account_id_type database_fixture::get_account_id( const string& account_name )const
 {
   return db->get_account( account_name ).get_id();
