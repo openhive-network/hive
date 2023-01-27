12df0074f00b1407a0def7377c2987f6eae89eaa
mtyszczak <mateusz.tyszczak@gmail.com>
2023-01-27 12:30:25 +0100

    Remove redundant for iceberg information from operations

--- programs/blockchain_converter/iceberg_generate_plugin.cpp
+++ programs/blockchain_converter/iceberg_generate_plugin.cpp
@@ -36,7 +36,6 @@ namespace hive {namespace converter { namespace plugins { namespace iceberg_gene
   namespace hp = hive::protocol;
 
   using hive::utilities::wif_to_key;
-  using hive::utilities::key_to_wif;
 
 namespace detail {
 
@@ -44,15 +43,257 @@ namespace detail {
 
   class generate_iceberg_ops_from_op_visitor
   {
-  private:
-    hp::transaction& tx;
-
   public:
     typedef hp::operation result_type;
 
-    generate_iceberg_ops_from_op_visitor( hp::signed_transaction& tx )
-      : tx( tx )
-    {}
+    generate_iceberg_ops_from_op_visitor() {}
+
+    result_type operator()( hp::account_create_operation& op )const
+    {
+      op.owner.clear();
+      op.active.clear();
+      op.posting.clear();
+      op.json_metadata.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::account_create_with_delegation_operation& op )const
+    {
+      op.owner.clear();
+      op.active.clear();
+      op.posting.clear();
+      op.json_metadata.clear();
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::account_update_operation& op )const
+    {
+      op.owner.reset();
+      op.active.reset();
+      op.posting.reset();
+      op.json_metadata.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::account_update2_operation& op )const
+    {
+      op.owner.reset();
+      op.active.reset();
+      op.posting.reset();
+      op.memo_key.reset();
+      op.json_metadata.clear();
+      op.posting_json_metadata.clear();
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::comment_operation& op )const
+    {
+      op.title.clear();
+      op.body.clear();
+      op.json_metadata.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::create_claimed_account_operation& op )const
+    {
+      op.owner.clear();
+      op.active.clear();
+      op.posting.clear();
+      op.json_metadata.clear();
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::transfer_operation& op )const
+    {
+      op.memo.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::escrow_transfer_operation& op )const
+    {
+      op.json_meta.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::witness_update_operation& op )const
+    {
+      op.url.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::witness_set_properties_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::custom_operation& op )const
+    {
+      op.required_auths.clear();
+      op.data.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::custom_json_operation& op )const
+    {
+      op.required_auths.clear();
+      op.required_posting_auths.clear();
+      op.json.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::custom_binary_operation& op )const
+    {
+      op.required_owner_auths.clear();
+      op.required_active_auths.clear();
+      op.required_posting_auths.clear();
+      op.required_auths.clear();
+      op.data.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::request_account_recovery_operation& op )const
+    {
+      op.new_owner_authority.clear();
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::recover_account_operation& op )const
+    {
+      op.new_owner_authority.clear();
+      op.recent_owner_authority.clear();
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::reset_account_operation& op )const
+    {
+      op.new_owner_authority.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::change_recovery_account_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::transfer_to_savings_operation& op )const
+    {
+      op.memo.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::transfer_from_savings_operation& op )const
+    {
+      op.memo.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::recurrent_transfer_operation& op )const
+    {
+      op.memo.clear();
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::create_proposal_operation& op )const
+    {
+      op.subject.clear();
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::update_proposal_operation& op )const
+    {
+      op.subject.clear();
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::update_proposal_votes_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::remove_proposal_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+#ifdef HIVE_ENABLE_SMT
+    result_type operator()( hp::smt_create_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::smt_setup_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::smt_setup_emissions_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::smt_set_setup_parameters_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::smt_set_runtime_parameters_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+
+    result_type operator()( hp::smt_contribute_operation& op )const
+    {
+      op.extensions.clear();
+
+      return op;
+    }
+#endif
 
     // No signatures modification ops
     template< typename T >
@@ -118,9 +359,11 @@ namespace detail {
       if ( ( log_per_block > 0 && start_block_num % log_per_block == 0 ) || log_specific == start_block_num )
         dlog("Rewritten block: ${block_num}. Data before conversion: ${block}", ("block_num", start_block_num)("block", block));
 
+      block.extensions.clear();
+
       for( auto& tx : block.transactions )
       {
-        generate_iceberg_ops_from_op_visitor pci{ tx };
+        generate_iceberg_ops_from_op_visitor pci{};
 
         for( auto& op : tx.operations )
           op = op.visit( pci );
