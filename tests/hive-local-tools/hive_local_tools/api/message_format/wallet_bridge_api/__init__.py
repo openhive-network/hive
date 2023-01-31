535ae5b7711b2ba27e4555f47c6041d445ee7f4b
Michał Kudela <kudmich@syncad.com>
2023-01-31 15:44:22 +0100

    Create get_transaction_from_head_block function

--- tests/hive-local-tools/hive_local_tools/api/message_format/wallet_bridge_api/__init__.py
+++ tests/hive-local-tools/hive_local_tools/api/message_format/wallet_bridge_api/__init__.py
@@ -72,3 +72,16 @@ def prepare_node_with_witnesses(witnesses_names):
                                            # so we have to wait two terms for the witnesses to activate
 
     return node
+
+
+def get_transaction_from_head_block(node):
+    if isinstance(node, tt.InitNode):
+        head_block_number = node.api.database.get_dynamic_global_properties()["head_block_number"]
+        node.wait_for_irreversible_block()
+        transaction_id = node.api.condenser.get_block(head_block_number)["transaction_ids"][0]
+        return transaction_id
+
+    if isinstance(node, tt.RemoteNode):
+        head_block_number = node.api.database.get_dynamic_global_properties()["head_block_number"]
+        transaction_id = node.api.condenser.get_block(head_block_number)["transaction_ids"][0]
+        return transaction_id
