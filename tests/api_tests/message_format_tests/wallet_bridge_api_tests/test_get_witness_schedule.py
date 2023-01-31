6582664bacc61ed9a5d03f13aa4bec0182c38d9a
Michał Kudela <kudmich@syncad.com>
2023-01-31 09:32:41 +0100

    Adapt tests based on deprecated block_log. Extend these test to work on a mainnet

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_get_witness_schedule.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_get_witness_schedule.py
@@ -1,5 +1,17 @@
-def test_get_witness_schedule_with_correct_value_false(replayed_node):
-    replayed_node.api.wallet_bridge.get_witness_schedule(False)
+from hive_local_tools import run_for
+from hive_local_tools.api.message_format.wallet_bridge_api import prepare_node_with_witnesses
+from hive_local_tools.api.message_format.wallet_bridge_api.constants import WITNESSES_NAMES
 
-def test_get_witness_schedule_with_correct_value_true(replayed_node):
-    replayed_node.api.wallet_bridge.get_witness_schedule(True)
+
+@run_for("testnet", "mainnet_5m", "live_mainnet")
+def test_get_witness_schedule_with_correct_value_false(node, should_prepare):
+    if should_prepare:
+        node = prepare_node_with_witnesses(WITNESSES_NAMES)
+    node.api.wallet_bridge.get_witness_schedule(False)
+
+
+@run_for("testnet", "mainnet_5m", "live_mainnet")
+def test_get_witness_schedule_with_correct_value_true(node, should_prepare):
+    if should_prepare:
+        node = prepare_node_with_witnesses(WITNESSES_NAMES)
+    node.api.wallet_bridge.get_witness_schedule(True)
