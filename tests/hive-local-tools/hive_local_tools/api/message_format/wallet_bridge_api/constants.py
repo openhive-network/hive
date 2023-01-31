fddd6b42b49f8130808c0256a574b4ce23ebfb4a
Michał Kudela <kudmich@syncad.com>
2023-01-31 09:24:57 +0100

    Create constants file to wallet_bridge_api tests

---
+++ tests/hive-local-tools/hive_local_tools/api/message_format/wallet_bridge_api/constants.py
@@ -0,0 +1,7 @@
+from typing import Final
+
+MAINNET_ACCOUNT: Final[str] = "initminer"
+
+ACCOUNTS: Final[list] = [f'account-{i}' for i in range(10)]
+
+WITNESSES_NAMES: Final[list] = [f'witness-{i}' for i in range(20)]  # 21-st is initminer
