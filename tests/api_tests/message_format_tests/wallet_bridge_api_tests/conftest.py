ff68e26e47c7d03e0cf8a2d2ba2b9c06a9c7aaeb
Michał Kudela <kudmich@syncad.com>
2023-01-31 09:30:09 +0100

    Prepare environment `wallet_brigde_api` to work without block_log

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/conftest.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/conftest.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pytest
 
 import test_tools as tt
@@ -7,13 +5,6 @@ import test_tools as tt
 
 def pytest_configure(config):
     config.addinivalue_line('markers', 'enabled_plugins: Enabled plugins in node from `node` fixture')
-
-
-@pytest.fixture
-def replayed_node():
-    api_node = tt.ApiNode()
-    api_node.run(replay_from=Path(__file__).parent.joinpath('block_log/block_log'), wait_for_live=False)
-    return api_node
 
 
 @pytest.fixture
