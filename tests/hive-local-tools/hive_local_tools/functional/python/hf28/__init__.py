448787a9f7f0df1a7c83cd7c181f5d84ca68ca3b
Michał Kudela <kudmich@syncad.com>
2023-02-08 11:08:43 +0100

    Add tests for vote for comment after decline voting rihgts

--- tests/hive-local-tools/hive_local_tools/functional/python/hf28/__init__.py
+++ tests/hive-local-tools/hive_local_tools/functional/python/hf28/__init__.py
@@ -11,3 +11,16 @@ def create_proposal(wallet):
                                tt.Asset.Tbd(5),
                                "test subject",
                                "test-permlink")
+
+
+def post_comment(wallet, number_of_comments: int):
+    accounts = wallet.create_accounts(number_of_accounts=number_of_comments, name_base="creator")
+
+    with wallet.in_single_transaction():
+        for account in accounts:
+            wallet.api.transfer_to_vesting('initminer', account.name, tt.Asset.Test(1))
+
+    with wallet.in_single_transaction():
+        for account in accounts:
+            wallet.api.post_comment(f"{account.name}", f"comment-of-{account.name}", "", "someone0",
+                                    "test-title", "this is a body", "{}")
