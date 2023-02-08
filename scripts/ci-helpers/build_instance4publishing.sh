17e21649ccf51226738f22d242aade00eac4404e
Konrad Botor <kbotor@syncad.com>
2023-02-08 14:04:53 +0100

    Updated the scripts to take advantage of job prepare_hived_image - ref. #434

---
+++ scripts/ci-helpers/build_instance4publishing.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+[[ -z "$HIVED_IMAGE_NAME" ]] && echo "Variable HIVED_IMAGE_NAME must be set" && exit 1
+[[ -z "$HIVED_IMAGE_NAME_COMMIT" ]] && echo "Variable HIVED_IMAGE_NAME_COMMIT must be set" && exit 1
+
+set -e
+
+docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
+
+# Pull base_instance image
+echo "Pulling image $HIVED_IMAGE_NAME"
+docker pull "$HIVED_IMAGE_NAME"
+
+pushd "$CI_PROJECT_DIR"
+
+# Download Dockerfile used to build the base_image
+wget "https://gitlab.syncad.com/hive/hive/-/raw/$HIVED_IMAGE_NAME_COMMIT/Dockerfile" -O "Dockerfile.$HIVED_IMAGE_NAME_COMMIT"
+
+# Build the instance image
+TAG="${CI_REGISTRY_IMAGE}/instance:instance-${HIVED_IMAGE_NAME_COMMIT}"
+echo "Building image $TAG"
+docker buildx build --progress=plain --target=instance \
+  --build-arg CI_REGISTRY_IMAGE="$CI_REGISTRY_IMAGE/" \
+  --build-arg BUILD_HIVE_TESTNET="OFF" \
+  --build-arg HIVE_CONVERTER_BUILD="OFF" \
+  --build-arg BUILD_IMAGE_TAG="$HIVED_IMAGE_NAME_COMMIT" \
+  --cache-from=type=registry,ref="$TAG" \
+  --cache-to=type=inline \
+  --tag "$TAG" \
+  --file "Dockerfile.$HIVED_IMAGE_NAME_COMMIT" .
+
+docker push "$TAG"  
+
+popd
