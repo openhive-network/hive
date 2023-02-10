76fa96ab84ddf094153c74915a435ef5af2de944
Michał Kudela <kudmich@syncad.com>
2023-02-10 10:46:32 +0100

    Add test showing problem with vote_for_proposal in reference to decline_voting_rights on hardfork 27

--- tests/functional/python_tests/hf28_tests/tests_showing_problems_on_hardfork_27.py
+++ tests/functional/python_tests/hf28_tests/tests_showing_problems_on_hardfork_27.py
@@ -10,6 +10,7 @@ Related issue: https://gitlab.syncad.com/hive/hive/-/issues/441
 """
 from hive_local_tools import run_for
 from hive_local_tools.constants import TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS
+from hive_local_tools.functional.python.hf28 import create_proposal
 from hive_local_tools.functional.python.hf28.constants import VOTER_ACCOUNT
 
 
@@ -22,3 +23,42 @@ def test_decline_voting_rights_more_than_once_on_hf_27(prepare_environment_on_hf
     assert node.api.database.find_accounts(accounts=[VOTER_ACCOUNT])['accounts'][0]["can_vote"] == False
 
     wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)
+
+
+@run_for("testnet")
+def test_if_proposal_votes_were_removed_after_declining_voting_rights_on_hf_27(prepare_environment_on_hf_27):
+    node, wallet = prepare_environment_on_hf_27
+    create_proposal(wallet)
+
+    # vote for proposal -> decline voting rights -> wait 30 days (60s in testnet) for approval decline voting rights
+    wallet.api.update_proposal_votes(VOTER_ACCOUNT, [0], True)
+    wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)
+    node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
+
+    assert len(node.api.condenser.list_proposal_votes([""], 1000, "by_voter_proposal", "ascending", "all")) == 1
+
+
+@run_for("testnet")
+def test_vote_for_proposal_from_account_that_has_declined_its_voting_rights_on_hf_27(prepare_environment_on_hf_27):
+    node, wallet = prepare_environment_on_hf_27
+    create_proposal(wallet)
+
+    # decline voting rights -> wait 30 days (60s in testnet) for approval decline voting rights -> vote for proposal
+    wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)
+    node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
+
+    wallet.api.update_proposal_votes(VOTER_ACCOUNT, [0], True)
+
+
+@run_for("testnet")
+def test_vote_for_proposal_when_decline_voting_rights_request_is_being_executed_on_hf_27(prepare_environment_on_hf_27):
+    node, wallet = prepare_environment_on_hf_27
+    create_proposal(wallet)
+
+    # decline voting rights -> vote for proposal (before approving the waiver of voting rights) -> wait remaining time
+    head_block_number = wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)["block_num"]
+    node.wait_for_block_with_number(head_block_number + (TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS // 2))
+    wallet.api.update_proposal_votes(VOTER_ACCOUNT, [0], True)
+    node.wait_for_block_with_number(head_block_number + TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
+
+    assert len(node.api.condenser.list_proposal_votes([""], 1000, "by_voter_proposal", "ascending", "all")) == 1
