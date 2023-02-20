d37fa73efcfd3f3407d51de82c3852787f1bea87
Konrad Botor <kbotor@syncad.com>
2023-02-20 14:48:59 +0100

    Merged prepare_docker_image_for_publishing and publish_docker_image jobs into a single job that invokes build_instance.sh and is independent of other jobs - ref. #434

--- scripts/ci-helpers/build_and_publish_instance.sh
+++ scripts/ci-helpers/build_and_publish_instance.sh
@@ -10,14 +10,11 @@ docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD"
 
 pushd "$CI_PROJECT_DIR" || exit 1
 
-docker buildx build --progress=plain --target=base_instance \
-  --build-arg CI_REGISTRY_IMAGE="$CI_REGISTRY_IMAGE/" \
-  --build-arg BUILD_HIVE_TESTNET="OFF" \
-  --build-arg HIVE_CONVERTER_BUILD="OFF" \
-  --build-arg BUILD_IMAGE_TAG="$CI_COMMIT_TAG" \
-  --tag "${CI_REGISTRY_IMAGE}/base_instance:base_instance-${CI_COMMIT_TAG}" \
-  --file Dockerfile .
+# Build base_instance image
+scripts/ci-helpers/build_instance.sh "$CI_COMMIT_TAG" \
+  "." "registry.gitlab.syncad.com/hive/hive"
 
+# Build instance image
 docker buildx build --progress=plain --target=instance \
   --build-arg CI_REGISTRY_IMAGE="$CI_REGISTRY_IMAGE/" \
   --build-arg BUILD_HIVE_TESTNET="OFF" \
