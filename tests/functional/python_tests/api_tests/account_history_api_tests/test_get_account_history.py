dc1666b486be404e33f1d52437b02c6d7645204c
Krzysztof Mochocki <kmochocki@syncad.com>
2023-02-24 16:49:32 +0100

    Extend amount of test cases for pagination testing

--- tests/functional/python_tests/api_tests/account_history_api_tests/test_get_account_history.py
+++ tests/functional/python_tests/api_tests/account_history_api_tests/test_get_account_history.py
@@ -32,23 +32,29 @@ def test_filter_only_transfer_ops(node):
                                                             operation_filter_low=4)
     assert len(response['history']) == 1
 
-
+@pytest.mark.parametrize("step", (1, 2, 4, 8, 16, 32, 64))
 @run_for('testnet')
-def test_pagination(node):
+def test_pagination(node: tt.InitNode, step: int):
+    amount_of_transfers = 59
+    amount_of_operations_from_account_creation = 5
+    total_amount_of_operations = amount_of_transfers + amount_of_operations_from_account_creation
+
     wallet = tt.Wallet(attach_to=node)
     wallet.create_account('alice', hives=100, vests=100)
 
     with wallet.in_single_transaction():
-        for x in range(50):
+        for x in range(amount_of_transfers):
             wallet.api.transfer('alice', 'null', tt.Asset.Test(1), f"transfer-{x}")
     response = node.api.account_history.get_account_history(account='alice', include_reversible=True)
-    ops_from_pagination = []
-    limit = 5
-    start = 5
-    for x in range(int((len(response['history'])/limit))):
-        output = node.api.account_history.get_account_history(account='alice', include_reversible=True, limit=limit,
-                                                              start=start)
-        ops_from_pagination += output['history']
-        start += limit
+    assert len(response['history']) == total_amount_of_operations
 
+    ops_from_pagination = []
+    for start in range(step - 1, total_amount_of_operations, step):
+        output = node.api.account_history.get_account_history(account='alice', include_reversible=True, limit=step,
+                                                              start=start)
+        assert len(output['history']) > 0 or start == 0, f"history was empty for start={start}"
+        ops_from_pagination += output['history']
+        tt.logger.info(f"for start={start}, history has length of {len(output['history'])}")
+
+    ops_from_pagination = list(sorted(ops_from_pagination, key=lambda x: x[0]))
     assert ops_from_pagination == response['history']
