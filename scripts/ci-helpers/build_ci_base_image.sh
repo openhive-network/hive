d7c522604b68ba695670b61d2caa46ce4a6694f1
Bartek Wrona <wrona@syncad.com>
2023-03-15 18:14:00 +0100

    Skipped building of ci-base-image-5m (it is obsolete). Temporary left reference to existing one until data target can be removed from source.

--- scripts/ci-helpers/build_ci_base_image.sh
+++ scripts/ci-helpers/build_ci_base_image.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 REGISTRY=${1:-registry.gitlab.syncad.com/hive/hive/}
-CI_IMAGE_TAG=:ubuntu22.04-2
+CI_IMAGE_TAG=:ubuntu22.04-3
 
 docker buildx build --progress=plain --target=runtime \
   --build-arg CI_REGISTRY_IMAGE=$REGISTRY --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
@@ -11,7 +11,3 @@ docker buildx build --progress=plain --target=runtime \
 docker buildx build --progress=plain --target=ci-base-image \
   --build-arg CI_REGISTRY_IMAGE=$REGISTRY --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
   -t ${REGISTRY}ci-base-image$CI_IMAGE_TAG -f Dockerfile .
-
-docker buildx build --progress=plain --target=ci-base-image-5m \
-  --build-arg CI_REGISTRY_IMAGE=$REGISTRY --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
-  -t ${REGISTRY}ci-base-image-5m$CI_IMAGE_TAG -f Dockerfile .
