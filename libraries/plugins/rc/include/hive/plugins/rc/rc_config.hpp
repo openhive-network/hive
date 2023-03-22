feb1cb2cb785d24848c4c88a7281062ae2ccab65
ABW <andrzejl@syncad.com>
2023-03-22 17:24:29 +0100

    move RC related constants to global config.hpp since they are going to be used in consensus code

--- libraries/plugins/rc/include/hive/plugins/rc/rc_config.hpp
+++ libraries/plugins/rc/include/hive/plugins/rc/rc_config.hpp
@@ -6,15 +6,3 @@
 #define HIVE_RC_SPACE_ID 16
 #endif
 
-#define HIVE_RC_DRC_FLOAT_LEVEL    (20*HIVE_1_PERCENT)
-#define HIVE_RC_MAX_DRC_RATE       1000
-
-#define HIVE_RC_REGEN_TIME         (60*60*24*5) //5 days
-#define HIVE_RC_BUCKET_TIME_LENGTH (60*60) //1 hour
-#define HIVE_RC_WINDOW_BUCKET_COUNT 24
-// 2020.748973 VESTS == 1.000 HIVE when HF20 occurred on mainnet
-// TODO: What should this value be for testnet?
-#define HIVE_RC_HISTORICAL_ACCOUNT_CREATION_ADJUSTMENT      2020748973
-
-#define HIVE_RC_MAX_ACCOUNTS_PER_DELEGATION_OP 100
-
