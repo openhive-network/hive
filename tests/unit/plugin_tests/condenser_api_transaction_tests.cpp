cd1333b2ca48f296d2687282f8aebfef715f4a8f
Lucius <lucius@syncad.com>
2023-04-01 00:51:28 +0200

    Added get_transaction test with various number of operations.

--- tests/unit/plugin_tests/condenser_api_transaction_tests.cpp
+++ tests/unit/plugin_tests/condenser_api_transaction_tests.cpp
@@ -123,6 +123,101 @@ BOOST_AUTO_TEST_CASE( get_transaction_signatures )
 
 } FC_LOG_AND_RETHROW() }
 
+BOOST_AUTO_TEST_CASE( get_transaction_0234 )
+{ try {
+
+  BOOST_TEST_MESSAGE( "testing get_transaction with different number of operations in transactions" );
+
+  db->set_hardfork( HIVE_HARDFORK_1_27 );
+  generate_block();
+
+  ACTORS( (alice2trx)(bob2trx) );
+  generate_block();
+  fund( "alice2trx", ASSET( "300.000 TESTS" ) );
+  fund( "alice2trx", ASSET( "300.000 TBD" ) );
+  generate_block();
+
+  // Check transaction with no operation.
+  signed_transaction tx;
+  tx.set_expiration(db->head_block_time() + HIVE_MAX_TIME_UNTIL_EXPIRATION);
+  BOOST_REQUIRE_THROW( push_transaction( tx, alice2trx_private_key ), fc::assert_exception ); // see transaction::validate
+
+  // Prepare operations that will be used in transactions
+  
+  transfer_operation transfer_op;
+  transfer_op.from = "alice2trx";
+  transfer_op.to = "bob2trx";
+  transfer_op.amount = ASSET( "1.111 TESTS" );
+
+  transfer_to_vesting_operation transfer_to_vesting_op;
+  transfer_to_vesting_op.from = "alice2trx";
+  transfer_to_vesting_op.to = "alice2trx";
+  transfer_to_vesting_op.amount = ASSET( "2.222 TESTS" );
+
+  uint32_t id = 0;
+
+  convert_operation convert_op;
+  convert_op.owner = "alice2trx";
+  convert_op.requestid = id++;
+  convert_op.amount = ASSET( "3.333 TBD" );
+
+  collateralized_convert_operation collateral_op;
+  collateral_op.owner = "alice2trx";
+  collateral_op.requestid = id++;
+  collateral_op.amount = ASSET( "4.444 TESTS" );
+
+  // Two operations transaction
+  signed_transaction tx1;
+  tx1.set_expiration( db->head_block_time() + HIVE_MAX_TIME_UNTIL_EXPIRATION );
+  tx1.operations.push_back( transfer_op );
+  tx1.operations.push_back( transfer_to_vesting_op );
+  push_transaction( tx1, alice2trx_private_key );
+
+  uint32_t block_num = db->head_block_num() + 1;
+  generate_until_irreversible_block( block_num );
+  expected_t expected_transaction = { { // two operations
+    R"~({"ref_block_num":0,"ref_block_prefix":0,"expiration":"2016-01-01T01:00:12","operations":[{"type":"transfer_operation","value":{"from":"alice2trx","to":"bob2trx","amount":{"amount":"1111","precision":3,"nai":"@@000000021"},"memo":""}},{"type":"transfer_to_vesting_operation","value":{"from":"alice2trx","to":"alice2trx","amount":{"amount":"2222","precision":3,"nai":"@@000000021"}}}],"extensions":[],"signatures":["2034528d58768e33b16a8186d37599abc74286816edce59afbe40bcb148b7a6cd97b191f66c3d0059ef25806c48f6b13a2ac9526708e200b5d869c8d96fb001000"],"transaction_id":"69401b4fb88c8df6efe4280bea272ad7a86b1e8f","block_num":5,"transaction_num":0})~",
+    R"~({"ref_block_num":0,"ref_block_prefix":0,"expiration":"2016-01-01T01:00:12","operations":[["transfer",{"from":"alice2trx","to":"bob2trx","amount":"1.111 TESTS","memo":""}],["transfer_to_vesting",{"from":"alice2trx","to":"alice2trx","amount":"2.222 TESTS"}]],"extensions":[],"signatures":["2034528d58768e33b16a8186d37599abc74286816edce59afbe40bcb148b7a6cd97b191f66c3d0059ef25806c48f6b13a2ac9526708e200b5d869c8d96fb001000"],"transaction_id":"69401b4fb88c8df6efe4280bea272ad7a86b1e8f","block_num":5,"transaction_num":0})~"
+    } };
+  test_get_transaction( *this, block_num, expected_transaction );
+
+  // Three operations transaction
+  signed_transaction tx2;
+  tx2.set_expiration( db->head_block_time() + HIVE_MAX_TIME_UNTIL_EXPIRATION );
+  tx2.operations.push_back( transfer_op );
+  tx2.operations.push_back( transfer_to_vesting_op );
+  tx2.operations.push_back( convert_op );
+  push_transaction( tx2, alice2trx_private_key );
+
+  block_num = db->head_block_num() + 1;
+  generate_until_irreversible_block( block_num );
+  expected_transaction = { { // three operations
+    R"~({"ref_block_num":0,"ref_block_prefix":0,"expiration":"2016-01-01T01:01:18","operations":[{"type":"transfer_operation","value":{"from":"alice2trx","to":"bob2trx","amount":{"amount":"1111","precision":3,"nai":"@@000000021"},"memo":""}},{"type":"transfer_to_vesting_operation","value":{"from":"alice2trx","to":"alice2trx","amount":{"amount":"2222","precision":3,"nai":"@@000000021"}}},{"type":"convert_operation","value":{"owner":"alice2trx","requestid":0,"amount":{"amount":"3333","precision":3,"nai":"@@000000013"}}}],"extensions":[],"signatures":["1f5717a4459ec6e06f2b261a9ecdb01ce6d723da8c667b6f71e14f8be8497ecaa418529765268b9c6920f8ee8016ee702dfb72f56c9c65120ff2109f740f3325e2"],"transaction_id":"f70a6173c87e58a03c6b1667e181ee2ed3104d8c","block_num":27,"transaction_num":0})~",
+    R"~({"ref_block_num":0,"ref_block_prefix":0,"expiration":"2016-01-01T01:01:18","operations":[["transfer",{"from":"alice2trx","to":"bob2trx","amount":"1.111 TESTS","memo":""}],["transfer_to_vesting",{"from":"alice2trx","to":"alice2trx","amount":"2.222 TESTS"}],["convert",{"owner":"alice2trx","requestid":0,"amount":"3.333 TBD"}]],"extensions":[],"signatures":["1f5717a4459ec6e06f2b261a9ecdb01ce6d723da8c667b6f71e14f8be8497ecaa418529765268b9c6920f8ee8016ee702dfb72f56c9c65120ff2109f740f3325e2"],"transaction_id":"f70a6173c87e58a03c6b1667e181ee2ed3104d8c","block_num":27,"transaction_num":0})~"
+    } };
+  test_get_transaction( *this, block_num, expected_transaction );
+
+  ++(convert_op.requestid);
+
+  // Four operations transaction
+  signed_transaction tx3;
+  tx3.set_expiration( db->head_block_time() + HIVE_MAX_TIME_UNTIL_EXPIRATION );
+  tx3.operations.push_back( transfer_op );
+  tx3.operations.push_back( transfer_to_vesting_op );
+  tx3.operations.push_back( convert_op );
+  tx3.operations.push_back( collateral_op );
+  push_transaction( tx3, alice2trx_private_key );
+
+  block_num = db->head_block_num() + 1;
+  generate_until_irreversible_block( block_num );
+  expected_transaction = { { // four operations
+    R"~({"ref_block_num":0,"ref_block_prefix":0,"expiration":"2016-01-01T01:01:30","operations":[{"type":"transfer_operation","value":{"from":"alice2trx","to":"bob2trx","amount":{"amount":"1111","precision":3,"nai":"@@000000021"},"memo":""}},{"type":"transfer_to_vesting_operation","value":{"from":"alice2trx","to":"alice2trx","amount":{"amount":"2222","precision":3,"nai":"@@000000021"}}},{"type":"convert_operation","value":{"owner":"alice2trx","requestid":1,"amount":{"amount":"3333","precision":3,"nai":"@@000000013"}}},{"type":"collateralized_convert_operation","value":{"owner":"alice2trx","requestid":1,"amount":{"amount":"4444","precision":3,"nai":"@@000000021"}}}],"extensions":[],"signatures":["1f18d2d28d6fc1168b23dc805f42eb49956a1a6f935420808ef5f382f7866457c94b8bd80b0f25a4325acfc05d14c4ff101f1dae94b6af01075911b4672033011b"],"transaction_id":"c718fc3fd1e6840d8ff79a70dd14a3c255473142","block_num":31,"transaction_num":0})~",
+    R"~({"ref_block_num":0,"ref_block_prefix":0,"expiration":"2016-01-01T01:01:30","operations":[["transfer",{"from":"alice2trx","to":"bob2trx","amount":"1.111 TESTS","memo":""}],["transfer_to_vesting",{"from":"alice2trx","to":"alice2trx","amount":"2.222 TESTS"}],["convert",{"owner":"alice2trx","requestid":1,"amount":"3.333 TBD"}],["collateralized_convert",{"owner":"alice2trx","requestid":1,"amount":"4.444 TESTS"}]],"extensions":[],"signatures":["1f18d2d28d6fc1168b23dc805f42eb49956a1a6f935420808ef5f382f7866457c94b8bd80b0f25a4325acfc05d14c4ff101f1dae94b6af01075911b4672033011b"],"transaction_id":"c718fc3fd1e6840d8ff79a70dd14a3c255473142","block_num":31,"transaction_num":0})~"
+    } };
+  test_get_transaction( *this, block_num, expected_transaction );
+
+} FC_LOG_AND_RETHROW() }
+
 BOOST_AUTO_TEST_SUITE_END() // condenser_get_transaction_tests
 
 #endif
