4549f012138094fffe22d9035ba1cf491e82aa36
Lucius <lucius@syncad.com>
2023-03-31 13:47:46 +0200

    Improved test_get_transaction adding pattern comparison and sample first test.

--- tests/unit/plugin_tests/condenser_api_transaction_tests.cpp
+++ tests/unit/plugin_tests/condenser_api_transaction_tests.cpp
@@ -18,163 +18,71 @@
 //  get_account_history -> ditto get_account_history
 //  enum_virtual_ops -> not used
 
+/// Account history pattern goes first in the pair, condenser version pattern follows.
+typedef std::vector< std::pair< std::string, std::string > > expected_t;
+
 BOOST_FIXTURE_TEST_SUITE( condenser_get_transaction_tests, condenser_api_fixture );
 
-// TODO: Improve by comparing get_transaction results against patterns provided as additional argument.
-void test_get_transaction( const condenser_api_fixture& caf, uint32_t block_num )
+void test_get_transaction( const condenser_api_fixture& caf, uint32_t block_num, const expected_t& expected_transactions )
 {
   auto full_block_ptr = caf.db->fetch_block_by_number( block_num );
   const signed_block& block = full_block_ptr->get_block();
+  ilog( "Block #${b} contains ${n1} transaction(s), ${n2} expected.",
+    ("n1", block.transactions.size())("b", block_num)("n2", expected_transactions.size()) );
+  BOOST_REQUIRE( expected_transactions.size() == block.transactions.size() );
 
-  for( const auto& trx : block.transactions )
+  for( size_t transaction_index = 0; transaction_index < expected_transactions.size(); ++transaction_index )
   {
+    const auto& trx = block.transactions[ transaction_index ];
     const auto& trx_id = trx.id();
     BOOST_REQUIRE( trx_id != hive::protocol::transaction_id_type() );
+    const auto& expected_transaction = expected_transactions[ transaction_index ];
 
-      // Call condenser get_transaction and verify results with result of account history variant.
-      const auto tx_hash = trx_id.str();
-      const auto result = caf.condenser_api->get_transaction( condenser_api::get_transaction_args(1, fc::variant(tx_hash)) );
-      const condenser_api::annotated_signed_transaction op_tx = result.value;
-      ilog("operation transaction is ${tx}", ("tx", op_tx));
-      const account_history::annotated_signed_transaction ah_op_tx = 
-        caf.account_history_api->get_transaction( {tx_hash, false /*include_reversible*/} );
-      ilog("operation transaction is ${tx}", ("tx", ah_op_tx));
-      BOOST_REQUIRE_EQUAL( op_tx.transaction_id, ah_op_tx.transaction_id );
-      BOOST_REQUIRE_EQUAL( op_tx.block_num, ah_op_tx.block_num );
-      BOOST_REQUIRE_EQUAL( op_tx.transaction_num, ah_op_tx.transaction_num );
+    const auto tx_hash = trx_id.str();
+    // Call condenser get_transaction and verify results against expected pattern.
+    const auto cn_trx = caf.condenser_api->get_transaction( condenser_api::get_transaction_args(1, fc::variant(tx_hash)) );
+    // Call account history get_transaction and verify results against expected pattern.
+    const account_history::annotated_signed_transaction ah_trx = 
+      caf.account_history_api->get_transaction( {tx_hash, false /*include_reversible*/} );
 
-      // Do additional checks of condenser variant
-      // Too few arguments
-      BOOST_REQUIRE_THROW( caf.condenser_api->get_transaction( condenser_api::get_transaction_args() ), fc::assert_exception );
-      // Too many arguments
-      BOOST_REQUIRE_THROW( caf.condenser_api->get_transaction( condenser_api::get_transaction_args(2, fc::variant(tx_hash)) ), fc::assert_exception );
-    }
+    ilog("ah trx: ${ah_trx}", (ah_trx));
+    ilog("cn trx: ${cn_trx}", (cn_trx));
+
+    // Compare transactions in their serialized form with expected patterns:
+    BOOST_REQUIRE_EQUAL( expected_transaction.first, fc::json::to_string(ah_trx) );
+    BOOST_REQUIRE_EQUAL( expected_transaction.second, fc::json::to_string(cn_trx) );
+
+    // Do additional checks of condenser variant
+    // Too few arguments
+    BOOST_REQUIRE_THROW( caf.condenser_api->get_transaction( condenser_api::get_transaction_args() ), fc::assert_exception );
+    // Too many arguments
+    BOOST_REQUIRE_THROW( caf.condenser_api->get_transaction( condenser_api::get_transaction_args(2, fc::variant(tx_hash)) ), fc::assert_exception );
+  }
 }
 
-BOOST_AUTO_TEST_CASE( get_transaction_hf1 )
+BOOST_AUTO_TEST_CASE( get_transaction_1 )
 { try {
 
-  BOOST_TEST_MESSAGE( "testing get_transaction on operations of HF1" );
+  BOOST_TEST_MESSAGE( "testing get_transaction with single operation in transaction" );
 
-  auto check_point_tester = [ this ]( uint32_t generate_no_further_than )
-  {
-    generate_until_irreversible_block( 2 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
+  ACTORS( (alice1trx)(bob1trx) );
+  fund( "alice1trx", 100000 );
+  generate_block();
 
-    test_get_transaction( *this, 2 ); // <- TODO: Enhance with patterns
+  uint32_t trx_block = db->head_block_num() + 1;
 
-    // In block 21 maximum block size is being changed:
-    generate_until_irreversible_block( 21 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
+  // Single transaction with single operation inside.
+  transfer( "alice1trx", "bob1trx", ASSET( "3.333 TESTS" ) );
 
-    test_get_transaction( *this, 21 ); // <- TODO: Enhance with patterns
-  };
+  generate_until_irreversible_block( trx_block );
 
-  hf1_scenario( check_point_tester );
-
-} FC_LOG_AND_RETHROW() }
-
-BOOST_AUTO_TEST_CASE( get_transaction_hf12 )
-{ try {
-
-  BOOST_TEST_MESSAGE( "testing get_transaction with hf12_scenario" );
-
-  auto check_point_tester = [ this ]( uint32_t generate_no_further_than )
-  {
-    generate_until_irreversible_block( 3 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
-
-    test_get_transaction( *this, 3 ); // <- TODO: Enhance with patterns
-  };
-
-  hf12_scenario( check_point_tester );
+  expected_t expected_transaction1 = { { // transaction with 1 operation
+    R"~({"ref_block_num":0,"ref_block_prefix":0,"expiration":"2016-01-01T01:00:06","operations":[{"type":"transfer_operation","value":{"from":"alice1trx","to":"bob1trx","amount":{"amount":"3333","precision":3,"nai":"@@000000021"},"memo":""}}],"extensions":[],"signatures":[],"transaction_id":"c88bb2494897d34579370dcc4fab0d3fd2f1625c","block_num":3,"transaction_num":0})~",
+    R"~({"ref_block_num":0,"ref_block_prefix":0,"expiration":"2016-01-01T01:00:06","operations":[["transfer",{"from":"alice1trx","to":"bob1trx","amount":"3.333 TESTS","memo":""}]],"extensions":[],"signatures":[],"transaction_id":"c88bb2494897d34579370dcc4fab0d3fd2f1625c","block_num":3,"transaction_num":0})~"
+    } };
+  test_get_transaction( *this, trx_block, { expected_transaction1 } );
 
 } FC_LOG_AND_RETHROW() }
-
-BOOST_AUTO_TEST_CASE( get_transaction_hf13 )
-{ try {
-
-  BOOST_TEST_MESSAGE( "testing get_transaction with hf13_scenario" );
-
-  auto check_point_1_tester = [ this ]( uint32_t generate_no_further_than )
-  {
-    generate_until_irreversible_block( 3 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
-
-    test_get_transaction( *this, 3 ); // <- TODO: Enhance with patterns
-  };
-
-  auto check_point_2_tester = [ this ]( uint32_t generate_no_further_than )
-  {
-    generate_until_irreversible_block( 25 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
-
-    test_get_transaction( *this, 25 ); // <- TODO: Enhance with patterns
-  };
-
-  hf13_scenario( check_point_1_tester, check_point_2_tester );
-
-} FC_LOG_AND_RETHROW() }
-
-BOOST_AUTO_TEST_CASE( get_transaction_comment_and_reward )
-{ try {
-
-  BOOST_TEST_MESSAGE( "testing get_transaction with comment_and_reward_scenario" );
-
-  // Check virtual operations resulting from scenario's 1st set of actions some blocks later:
-  auto check_point_1_tester = [ this ]( uint32_t generate_no_further_than )
-  {
-    generate_until_irreversible_block( 6 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
-
-    test_get_transaction( *this, 6 ); // <- TODO: Enhance with patterns
-  };
-
-  // Check operations resulting from 2nd set of actions:
-  auto check_point_2_tester = [ this ]( uint32_t generate_no_further_than )
-  { 
-    generate_until_irreversible_block( 28 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
-    
-    test_get_transaction( *this, 25 ); // <- TODO: Enhance with patterns
-  };
-
-  comment_and_reward_scenario( check_point_1_tester, check_point_2_tester );
-
-} FC_LOG_AND_RETHROW() }
-
-BOOST_AUTO_TEST_CASE( get_transaction_convert_and_limit_order )
-{ try {
-
-  BOOST_TEST_MESSAGE( "testing get_transaction with convert_and_limit_order_scenario" );
-
-  auto check_point_tester = [ this ]( uint32_t generate_no_further_than )
-  {
-    generate_until_irreversible_block( 5 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
-
-    test_get_transaction( *this, 5 ); // <- TODO: Enhance with patterns
-
-    generate_until_irreversible_block( 88 );
-    BOOST_REQUIRE( db->head_block_num() <= generate_no_further_than );
-
-    test_get_transaction( *this, 88 ); // <- TODO: Enhance with patterns
-  };
-
-  convert_and_limit_order_scenario( check_point_tester );
-
-} FC_LOG_AND_RETHROW() }
-
-// TODO Create get_transaction_hf8 test here.
-// TODO Create get_transaction_hf23 test here.
-// TODO Create get_transaction_vesting test here.
-// TODO Create get_transaction_witness test here.
-// TODO Create get_transaction_escrow_and_savings test here.
-// TODO Create get_transaction_proposal test here.
-// TODO Create get_transaction_account test here.
-// TODO Create get_transaction_custom test here.
-// TODO Create get_transaction_recurrent_transfer test here.
-// TODO Create get_transaction_decline_voting_rights here.
 
 BOOST_AUTO_TEST_SUITE_END() // condenser_get_transaction_tests
 
