0f047bab375460cfe7490dcd0525133850674482
mkrzeminski <mkrzeminski@syncad.com>
2023-04-01 17:18:21 +0200

    Skip verify_authority for known merkle failed blocks

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -5037,19 +5037,45 @@ void database::validate_transaction(const std::shared_ptr<full_transaction_type>
       const flat_set<public_key_type>& signature_keys = full_transaction->get_signature_keys();
       const required_authorities_type& required_authorities = full_transaction->get_required_authorities();
 
-      hive::protocol::verify_authority(required_authorities,
-                                       signature_keys,
-                                       get_active,
-                                       get_owner,
-                                       get_posting,
-                                       get_witness_key,
-                                       HIVE_MAX_SIG_CHECK_DEPTH,
-                                       has_hardfork(HIVE_HARDFORK_0_20) ? HIVE_MAX_AUTHORITY_MEMBERSHIP : 0,
-                                       has_hardfork(HIVE_HARDFORK_0_20) ? HIVE_MAX_SIG_CHECK_ACCOUNTS : 0,
-                                       false,
-                                       flat_set<account_name_type>(),
-                                       flat_set<account_name_type>(),
-                                       flat_set<account_name_type>());
+
+      //legacy asset
+      switch(block_num)
+      {
+        case  994240:        //"account_creation_fee": "0.1 HIVE"
+        case 1021529:        //"account_creation_fee": "10.0 HIVE"
+        case 3143833:        //"account_creation_fee": "3.00000 HIVE"
+        case 3208405:        //"account_creation_fee": "2.00000 HIVE"
+        case 3695672:        //"account_creation_fee": "3.00 HIVE"
+        case 4338089:        //"account_creation_fee": "0.001 0.001"
+        case 4626205:        //"account_creation_fee": "6.000 6.000"
+        case 4632595:        //"account_creation_fee": "6.000 6.000"
+          break;
+
+      //just wrong merkle
+        case 3705111:
+        case 3705120:
+        case 3713940:
+        case 3714132:
+        case 3714567:
+        case 3714588:
+        case 4138790:
+          break;
+        
+        default:
+          hive::protocol::verify_authority(required_authorities,
+                                          signature_keys,
+                                          get_active,
+                                          get_owner,
+                                          get_posting,
+                                          get_witness_key,
+                                          HIVE_MAX_SIG_CHECK_DEPTH,
+                                          has_hardfork(HIVE_HARDFORK_0_20) ? HIVE_MAX_AUTHORITY_MEMBERSHIP : 0,
+                                          has_hardfork(HIVE_HARDFORK_0_20) ? HIVE_MAX_SIG_CHECK_ACCOUNTS : 0,
+                                          false,
+                                          flat_set<account_name_type>(),
+                                          flat_set<account_name_type>(),
+                                          flat_set<account_name_type>());
+      }
 
       if (_benchmark_dumper.is_enabled())
         _benchmark_dumper.end("transaction", "verify_authority", trx.signatures.size());
