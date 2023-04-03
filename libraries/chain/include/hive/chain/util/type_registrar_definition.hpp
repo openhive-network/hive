ed079b2f66bcae27c59842e591ec607312ab15aa
Bartek Wrona <wrona@syncad.com>
2023-04-03 23:03:25 +0200

    type_registrar class should explicitly instantiated, not specialized.

--- libraries/chain/include/hive/chain/util/type_registrar_definition.hpp
+++ libraries/chain/include/hive/chain/util/type_registrar_definition.hpp
@@ -1,9 +1,11 @@
 #include <hive/chain/util/type_registrar.hpp>
 #include <hive/chain/util/decoded_types_data_storage.hpp>
 
+template <class T>
+void hive::chain::util::type_registrar<T>::register_type(hive::chain::util::decoded_types_data_storage& dtds)
+{
+dtds.register_new_type<T>();
+}
+
 #define HIVE_DEFINE_TYPE_REGISTRAR_REGISTER_TYPE(type_name) \
-  template <> \
-  void hive::chain::util::type_registrar<type_name>::register_type(hive::chain::util::decoded_types_data_storage& dtds) \
-  { \
-    dtds.register_new_type<type_name>();  \
-  }
+  template class hive::chain::util::type_registrar<type_name>;
