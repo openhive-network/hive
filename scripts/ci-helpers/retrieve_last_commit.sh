36472c24ea04419fcb53c53bf33d8a7155b1086f
Bartek Wrona <wrona@syncad.com>
2023-04-12 17:40:40 +0200

    cmake subdirectory and root CMakeLists.txt also affect Hived last commit

--- scripts/ci-helpers/retrieve_last_commit.sh
+++ scripts/ci-helpers/retrieve_last_commit.sh
@@ -3,7 +3,7 @@
 P="${1}"
 pushd "$P" >/dev/null 2>&1
 # this list is used to detect changes affecting hived binaries, list might change in future
-COMMIT=$(git log --pretty=format:"%H" -- libraries/ programs/ scripts/ docker/ Dockerfile | head -1)
+COMMIT=$(git log --pretty=format:"%H" -- libraries/ programs/ scripts/ docker/ Dockerfile cmake CMakeLists.txt | head -1)
 
 popd >/dev/null 2>&1
 
