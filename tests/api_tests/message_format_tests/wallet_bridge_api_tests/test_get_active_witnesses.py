1199130086d353b0d81a01d3f2903d1e08712638
Michał Kudela <kudmich@syncad.com>
2023-04-13 08:49:22 +0200

    Adapt the tests to the changed function

--- tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_get_active_witnesses.py
+++ tests/api_tests/message_format_tests/wallet_bridge_api_tests/test_get_active_witnesses.py
@@ -6,12 +6,12 @@ from hive_local_tools.api.message_format.wallet_bridge_api.constants import WITN
 @run_for("testnet", "mainnet_5m", "live_mainnet")
 def test_get_active_witnesses_with_correct_value_false(node, should_prepare):
     if should_prepare:
-        node = prepare_node_with_witnesses(WITNESSES_NAMES)
+        node = prepare_node_with_witnesses(node, WITNESSES_NAMES)
     node.api.wallet_bridge.get_active_witnesses(False)
 
 
 @run_for("testnet", "live_mainnet")
 def test_get_active_witnesses_with_correct_value_true(node, should_prepare):
     if should_prepare:
-        node = prepare_node_with_witnesses(WITNESSES_NAMES)
+        node = prepare_node_with_witnesses(node, WITNESSES_NAMES)
     node.api.wallet_bridge.get_active_witnesses(True)
