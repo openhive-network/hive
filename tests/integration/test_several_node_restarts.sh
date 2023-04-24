8903869a522fdee3453d46e24fdfec7aca12adc8
Bartek Wrona <wrona@syncad.com>
2023-04-24 23:42:48 +0200

    WIP: CI networking test

--- tests/integration/test_several_node_restarts.sh
+++ tests/integration/test_several_node_restarts.sh
@@ -11,7 +11,7 @@ wait_for_instance() {
   docker inspect ${container_name} || true
 
   CONTAINER_IP=$(docker inspect ${container_name} --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' || true)
-
+  ping -c 3 ${CONTAINER_IP}
   timeout $LIMIT bash <<EOF
   until [ ! -z "$CONTAINER_IP" ]
   do
@@ -24,7 +24,7 @@ EOF
 
   echo "Querying for service status..."
 
-  timeout $LIMIT docker container exec -t ${container_name} bash -c "until curl --max-time 30  --data '{\"jsonrpc\": \"2.0\",\"method\": \"database_api.get_dynamic_global_properties\",\"id\": 1}' ${CONTAINER_IP}:8090; do sleep 3 ; done"
+  docker container exec -t ${container_name} timeout $LIMIT bash -c "until curl --max-time 30  --data '{\"jsonrpc\": \"2.0\",\"method\": \"database_api.get_dynamic_global_properties\",\"id\": 1}' ${CONTAINER_IP}:8090; do sleep 3 ; done"
 }
 
 #SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
@@ -41,10 +41,11 @@ echo "CREATE REPLAY ENVIRONMENT"
 ls -lah
 echo "START FIRST REPLAY: "
 # --docker-option="--network=host"
-"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=haf-instance-5M --webserver-http-endpoint=":8090" --replay --stop-replay-at-block=1000000 --detach
+"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=haf-instance-5M --docker-option="--network=bridge" --replay --stop-replay-at-block=1000000 --detach
 docker ps
-
-wait_for_instance haf-instance-5M
 
 echo "Docker network ls"
 docker network ls
+
+wait_for_instance haf-instance-5M
+
