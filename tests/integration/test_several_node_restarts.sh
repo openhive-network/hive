d1c2c1dc3560b90af45857f09d2fcd9590e03e02
Michał Kudela <kudmich@syncad.com>
2023-04-26 12:07:07 +0200

    Adapt the script to test activities

--- tests/integration/test_several_node_restarts.sh
+++ tests/integration/test_several_node_restarts.sh
@@ -2,15 +2,19 @@
 
 set -xeuo pipefail
 
+instance_name=${1}
+first_replay_block_limit=${2}
+second_replay_block_limit=${3}
+
 wait_for_instance() {
   local container_name=${1}
+  local number_of_blocks_to_replay=${2}
   local LIMIT=300
 
   echo "Waiting for instance hosted by container: ${container_name}."
 
-  docker inspect ${container_name} || true
-
   CONTAINER_IP=$(docker inspect ${container_name} --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' || true)
+  echo "container ip: $CONTAINER_IP"
 
   timeout $LIMIT bash <<EOF
   until [ ! -z "$CONTAINER_IP" ]
@@ -23,33 +27,25 @@ wait_for_instance() {
 EOF
 
   echo "Querying for service status..."
-
-  sleep $LIMIT
-
   docker logs ${container_name}
-
-  docker container exec -t ${container_name} timeout $LIMIT bash -c "until curl --max-time 30  --data '{\"jsonrpc\": \"2.0\",\"method\": \"database_api.get_dynamic_global_properties\",\"id\": 1}' ${CONTAINER_IP}:8090; do sleep 3 ; done"
+  docker container exec -t ${container_name} timeout $LIMIT bash -c "until curl --max-time 30  --data '{\"jsonrpc\": \"2.0\",\"method\": \"database_api.get_dynamic_global_properties\",\"id\": 1}' ${CONTAINER_IP}:8090  | grep -o '\"head_block_number\":${number_of_blocks_to_replay},'; do sleep 3 ; done"
+  docker stop ${container_name}
 }
 
-#SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-#BASE_DIRECTORY=/$(echo "$SCRIPTPATH" | cut -d "/" -f2)
 SOURCE_DATA_DIR=$CI_PROJECT_DIR/data_generated_during_hive_replay
 mkdir -p "$SOURCE_DATA_DIR"
-ls -lah
-echo "ENV:"
-env
-echo "FNI ENV:"
-ls -lah
-echo "CREATE REPLAY ENVIRONMENT"
+
+echo "Create replay environment..."
 "$SCRIPTS_PATH/ci-helpers/prepare_data_and_shm_dir.sh" --data-base-dir="$SOURCE_DATA_DIR" --block-log-source-dir="$BLOCK_LOG_SOURCE_DIR_5M" --config-ini-source="$CONFIG_INI_SOURCE"
-ls -lah
-echo "START FIRST REPLAY: "
-# --docker-option="--network=host"
-"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=haf-instance-5M --docker-option="--network=bridge" --replay --stop-replay-at-block=1000000 --detach
-docker ps
 
-echo "Docker network ls"
-docker network ls
+echo "Start first replay: "
+"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=$instance_name --docker-option="--network=bridge" --replay --stop-replay-at-block=1000000 --detach
+wait_for_instance $instance_name $first_replay_block_limit
+test "$(docker wait $instance_name)" = 0
 
-wait_for_instance haf-instance-5M
+echo "Start second replay: "
+"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=$instance_name --docker-option="--network=bridge" --replay --stop-replay-at-block=1500000 --detach
+wait_for_instance $instance_name $second_replay_block_limit
+test "$(docker wait $instance_name)" = 0
 
+echo "Test passed!"
