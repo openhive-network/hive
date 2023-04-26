d14e1a475a1828adf6b01b721b340dc3d2773960
mkrzeminski <mkrzeminski@syncad.com>
2023-04-26 13:34:23 +0000

    Move cpp file from hive to haf

--- libraries/plugins/apis/database_api/include/hive/plugins/database_api/from_variant_to_full_block_ptr.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once 
-
-namespace consensus_state_provider
-{
-  std::shared_ptr<hive::chain::full_block_type> from_variant_to_full_block_ptr(const fc::variant& v, int block_num_debug );
-}
