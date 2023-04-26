ce9ab9177f531a37b045679633cacf0ede1ab489
ABW <andrzejl@syncad.com>
2023-04-26 15:16:06 +0200

    move RC data transport structure to core code
    The part related to BDE had to be moved to rc_plugin, since core code cannot depend on plugins.
    It will probably go away (preferably with whole BDE).

--- libraries/chain/include/hive/chain/rc/rc_export_objects.hpp
+++ libraries/chain/include/hive/chain/rc/rc_export_objects.hpp
@@ -2,16 +2,14 @@
 
 #include <hive/protocol/types.hpp>
 
-#include <hive/plugins/block_data_export/exportable_block_data.hpp>
-#include <hive/plugins/rc/resource_count.hpp>
+#include <hive/chain/rc/resource_count.hpp>
 
 #include <fc/int_array.hpp>
 
 #include <vector>
 
-namespace hive { namespace plugins { namespace rc {
+namespace hive { namespace chain {
 
-using hive::plugins::block_data_export::exportable_block_data;
 using hive::protocol::account_name_type;
 
 struct rc_info
@@ -41,35 +39,9 @@ struct rc_block_info
   optional< int64_t >       new_accounts_adjustment;
 };
 
-struct exp_rc_data
-  : public exportable_block_data
-{
-  exp_rc_data();
-  virtual ~exp_rc_data();
+} } // hive::chain
 
-  virtual void to_variant( fc::variant& v )const override;
-
-  void add_tx_info( const rc_transaction_info& tx_info )
-  {
-    if( !txs.valid() )
-      txs = std::vector< rc_transaction_info >();
-    txs->emplace_back( tx_info );
-  }
-  void add_opt_action_info( const rc_optional_action_info& action_info )
-  {
-    if( !opt_actions.valid() )
-      opt_actions = std::vector< rc_optional_action_info >();
-    opt_actions->emplace_back( action_info );
-  }
-
-  rc_block_info                                      block;
-  optional< std::vector< rc_transaction_info > >     txs;
-  optional< std::vector< rc_optional_action_info > > opt_actions;
-};
-
-} } } // hive::plugins::rc
-
-FC_REFLECT( hive::plugins::rc::rc_info,
+FC_REFLECT( hive::chain::rc_info,
   (payer)
   (usage)
   (cost)
@@ -78,7 +50,7 @@ FC_REFLECT( hive::plugins::rc::rc_info,
   (op)
 )
 
-FC_REFLECT( hive::plugins::rc::rc_block_info,
+FC_REFLECT( hive::chain::rc_block_info,
   (decay)
   (budget)
   (usage)
@@ -87,10 +59,4 @@ FC_REFLECT( hive::plugins::rc::rc_block_info,
   (share)
   (regen)
   (new_accounts_adjustment)
-)
-
-FC_REFLECT( hive::plugins::rc::exp_rc_data,
-  (block)
-  (txs)
-  (opt_actions)
 )
