296f550c8328525a6b4a26784241a195e730d425
Michał Kudela <kudmich@syncad.com>
2023-04-27 16:30:46 +0200

    Pass SOURCE_DATA_DIR as the first argument

--- tests/integration/test_several_node_restarts.sh
+++ tests/integration/test_several_node_restarts.sh
@@ -2,7 +2,7 @@
 
 set -xeuo pipefail
 
-instance_name=${1}
+SOURCE_DATA_DIR=${1}
 first_replay_block_limit=${2}
 second_replay_block_limit=${3}
 
@@ -16,22 +16,26 @@ wait_for_instance() {
   echo "Querying for service status..."
   docker logs ${container_name}
   docker container exec -t ${container_name} timeout $LIMIT bash -c "until curl --max-time 30  --data '{\"jsonrpc\": \"2.0\",\"method\": \"database_api.get_dynamic_global_properties\",\"id\": 1}' localhost:8090  | grep -o '\"head_block_number\":${number_of_blocks_to_replay},'; do sleep 3 ; done"
-  docker stop ${container_name}
 }
 
-SOURCE_DATA_DIR=$CI_PROJECT_DIR/data_generated_during_hive_replay
+stop_instance() {
+  docker stop hive-instance
+  docker wait hive-instance || true
+}
+
 mkdir -p "$SOURCE_DATA_DIR"
 
 echo "Create replay environment..."
 "$SCRIPTS_PATH/ci-helpers/prepare_data_and_shm_dir.sh" --data-base-dir="$SOURCE_DATA_DIR" --block-log-source-dir="$BLOCK_LOG_SOURCE_DIR_5M" --config-ini-source="$CONFIG_INI_SOURCE"
 
 echo "Start first replay: "
-"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=$instance_name --docker-option="--network=bridge" --replay --stop-replay-at-block=$first_replay_block_limit --detach
-wait_for_instance $instance_name $first_replay_block_limit
-docker wait $instance_name || true
+"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=hive-instance --docker-option="--network=bridge" --replay --stop-replay-at-block=$first_replay_block_limit --detach
+wait_for_instance hive-instance $first_replay_block_limit
+stop_instance hive-instance
 
 echo "Start second replay: "
-"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=$instance_name --docker-option="--network=bridge" --replay --stop-replay-at-block=$second_replay_block_limit --detach
-wait_for_instance $instance_name $second_replay_block_limit
+"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=hive-instance --docker-option="--network=bridge" --replay --stop-replay-at-block=$second_replay_block_limit --detach
+wait_for_instance hive-instance $second_replay_block_limit
+stop_instance hive-instance
 
 echo "Test passed!"
