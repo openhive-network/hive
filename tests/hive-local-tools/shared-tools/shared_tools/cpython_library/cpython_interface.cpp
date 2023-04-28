bb564e1a37ec057ead7b650ff1e99d0895929c7c
“Mariusz Trela” <mtrela@syncad.com>
2023-04-28 12:15:04 +0200

    Add `cpp_calculate_digest` and some examples

--- tests/hive-local-tools/shared-tools/shared_tools/cpython_library/cpython_interface.cpp
+++ tests/hive-local-tools/shared-tools/shared_tools/cpython_library/cpython_interface.cpp
@@ -2,6 +2,7 @@
 
 #include <hive/protocol/asset.hpp>
 #include <hive/protocol/operations.hpp>
+#include <hive/protocol/transaction.hpp>
 
 #include <fc/io/json.hpp>
 
@@ -53,6 +54,34 @@ int cpp_validate_operation( char* content )
 
     validate_visitor _visitor;
     _operation.visit( _visitor );
+
+    _result = 1;
+  } FC_CAPTURE_AND_LOG(( content ))
+
+  return _result;
+}
+
+int cpp_calculate_digest( char* content, unsigned char* digest )
+{
+  if( !content )
+    return false;
+
+  int _result = 0;
+
+  try
+  {
+    if( digest )
+    {
+      fc::variant _v = fc::json::from_string( content );
+      hive::protocol::transaction _transaction = _v.as<hive::protocol::transaction>();
+
+      hive::protocol::serialization_mode_controller::pack_guard guard( hive::protocol::pack_type::hf26 );
+      hive::protocol::digest_type::encoder enc;
+      fc::raw::pack( enc, _transaction );
+      hive::protocol::digest_type _digest = enc.result();
+
+      memcpy( digest, _digest.data(), _digest.data_size() );
+    }
 
     _result = 1;
   } FC_CAPTURE_AND_LOG(( content ))
