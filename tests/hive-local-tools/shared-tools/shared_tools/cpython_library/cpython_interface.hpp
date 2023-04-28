39fad9c878537e65747a8465defa831c4751c05d
“Mariusz Trela” <mtrela@syncad.com>
2023-04-28 12:15:04 +0200

    Add `cpp_calculate_digest` and some examples

--- tests/hive-local-tools/shared-tools/shared_tools/cpython_library/cpython_interface.hpp
+++ tests/hive-local-tools/shared-tools/shared_tools/cpython_library/cpython_interface.hpp
@@ -8,6 +8,8 @@ int add_two_numbers( int param_1, int param_2 );
 
 int cpp_validate_operation( char* content );
 
+int cpp_calculate_digest( char* content, unsigned char* digest );
+
 #ifdef __cplusplus
 }
 #endif
--- tests/hive-local-tools/shared-tools/shared_tools/cpython_library/example.pyx
+++ tests/hive-local-tools/shared-tools/shared_tools/cpython_library/example.pyx
@@ -1,6 +1,8 @@
 from cpython cimport array
 import array
 
+from typing import Tuple
+
 cpdef void info(str param):
     print("This is info: {}".format(param))
 
@@ -23,4 +25,21 @@ def validate_operation(operation: bytes) -> bool | None:
     cdef array.array _operation = array.array('b', operation)
     _operation.append(0)
 
-    return cpp_validate_operation( _operation.data.as_chars )
+    return bool(cpp_validate_operation( _operation.data.as_chars ))
+
+cdef extern from "cpython_interface.hpp":
+    int cpp_calculate_digest( char* content, unsigned char* digest )
+
+def calculate_digest(transaction: bytes) -> Tuple[bool, str] | None:
+    cdef array.array _transaction = array.array('b', transaction)
+    _transaction.append(0)
+
+    _DIGEST_SIZE = 32
+    _buffer = '0' * _DIGEST_SIZE
+    __buffer = bytes(_buffer, 'ascii')
+
+    cdef array.array _digest = array.array('B', __buffer)
+
+    _result = bool(cpp_calculate_digest( _transaction.data.as_chars, _digest.data.as_uchars ))
+
+    return _result, ''.join('{:02x}'.format(c) for c in _digest)
