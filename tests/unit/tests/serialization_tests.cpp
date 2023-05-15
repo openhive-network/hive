9374d0a0884b14fb0257f7c16cc4a0757e9a104f
Lucius <lucius@syncad.com>
2023-05-15 12:51:11 +0200

    Removed remaining legacy operations

--- tests/unit/tests/serialization_tests.cpp
+++ tests/unit/tests/serialization_tests.cpp
@@ -529,15 +529,13 @@ BOOST_AUTO_TEST_CASE( legacy_operation_test )
 {
   try
   {
-    using hive::plugins::condenser_api::legacy_transfer_operation;
-
     transfer_operation op_new, op_legacy;
 
     BOOST_CHECK_NO_THROW(
       op_legacy = serialize_with_legacy< operation >(
         "[\"transfer\",{\"from\":\"alice\",\"to\":\"bob\",\"amount\":\"1.234 TESTS\",\"memo\":\"test\"}]", // condenser_api output
         hive::protocol::transaction_serialization_type::legacy
-      ).get< legacy_transfer_operation >();
+      ).get< transfer_operation >();
     );
 
     BOOST_CHECK_NO_THROW(
