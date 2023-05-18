d1b8050e175020eed9f283c8083925b1959fe8d4
“Mariusz Trela” <mtrela@syncad.com>
2023-05-18 11:57:00 +0200

    Token is generated in `create_session` endpoint

---
+++ programs/beekeeper/token_generator.cpp
@@ -0,0 +1,32 @@
+#include <beekeeper/token_generator.hpp>
+
+#include <random>
+#include <sstream>
+
+namespace beekeeper {
+
+std::string token_generator::generate_token( const std::string& salt, unsigned int length )
+{
+    std::random_device rd;
+    std::string _str_seed = salt + std::to_string( rd() );
+    std::seed_seq seq( _str_seed.begin(), _str_seed.end() );
+
+    std::mt19937 gen;
+    gen.seed( seq );
+
+    std::uniform_int_distribution<> dis(0, 255);
+
+    std::stringstream ss;
+    for( unsigned int i = 0; i < length; i++ )
+    {
+        auto rc = static_cast<unsigned char>( dis( gen ) );
+
+        std::stringstream hexstream;
+        hexstream << std::hex << int(rc);
+        auto hex = hexstream.str(); 
+        ss << (hex.length() < 2 ? '0' + hex : hex);
+    }
+    return ss.str();
+}
+
+};
