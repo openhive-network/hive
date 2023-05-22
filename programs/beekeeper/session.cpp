8ec0936ec4d71733502967f8aba0c1088ef2ba58
“Mariusz Trela” <mtrela@syncad.com>
2023-05-22 13:03:44 +0200

    Every session has own set of opened wallets

--- programs/beekeeper/session.cpp
+++ programs/beekeeper/session.cpp
@@ -5,7 +5,7 @@ namespace beekeeper {
   session::session( const std::string& token, const std::string& notifications_endpoint, types::lock_method_type&& lock_method )
           : notifications_endpoint( notifications_endpoint ), time( token, std::move( lock_method ) )
   {
-
+    wallet = std::make_shared<wallet_manager_impl>();
   }
 
   void session::set_timeout( const std::chrono::seconds& t )
@@ -21,6 +21,11 @@ namespace beekeeper {
   info session::get_info()
   {
     return time.get_info();
+  }
+
+  std::shared_ptr<wallet_manager_impl>& session::get_wallet()
+  {
+    return wallet;
   }
 
 } //beekeeper
