d990b0c49a107ad64ea2e52854aeddae733ee551
Michał Kudela <kudmich@syncad.com>
2023-05-26 10:18:47 +0200

    Create a node with OBI enabled by default

---
+++ tests/functional/python_tests/operation_tests/test_withdraw_vesting_operation/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+import test_tools as tt
+
+
+@pytest.fixture
+def node():
+    node = tt.InitNode()
+    node.config.plugin.append("account_history_api")
+    node.run(time_offset="+0h x5")
+    # On the testnet, the OBI (one block irreversibility) mechanism starts after the 30th block. The OBI is necessary
+    # to change the node's time offset without waiting 21 blocks until they become irreversible.
+    node.wait_for_block_with_number(30)
+    return node
