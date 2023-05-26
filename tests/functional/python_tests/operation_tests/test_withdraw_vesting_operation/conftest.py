8cf8c0859b99fc1b3d618945ef15f11258f995e2
Michał Kudela <kudmich@syncad.com>
2023-05-26 10:18:47 +0200

    Fixture creation to support PowerDown testing

---
+++ tests/functional/python_tests/operation_tests/test_withdraw_vesting_operation/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+
+import test_tools as tt
+from hive_local_tools.functional.python.operation.withdrawe_vesting import PowerDownAccount
+
+
+@pytest.fixture
+def alice(prepared_node, wallet):
+    wallet.create_account("alice")
+    alice = PowerDownAccount("alice", prepared_node, wallet)
+    alice.fund_vests(tt.Asset.Test(13_000))
+    alice.update_account_info()
+    return alice
