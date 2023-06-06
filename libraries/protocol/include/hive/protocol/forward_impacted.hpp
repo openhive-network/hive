0cbd830a11b9590a59269a45313a825f1cf9bc31
mkrzeminski <mkrzeminski@syncad.com>
2023-06-06 17:27:42 +0000

    collect_current_all_accounts_balances_impl

--- libraries/protocol/include/hive/protocol/forward_impacted.hpp
+++ libraries/protocol/include/hive/protocol/forward_impacted.hpp
@@ -88,6 +88,7 @@ namespace consensus_state_provider
   };
   typedef std::vector<collected_account_balances_t> collected_account_balances_collection_t;
   collected_account_balances_collection_t collect_current_all_accounts_balances(const char* context);
+  collected_account_balances_collection_t collect_current_all_accounts_balances_impl(const char* context, const char* shared_memory_bin_path);
   collected_account_balances_collection_t collect_current_account_balance(const char* acount, const char* context);
 }
 
