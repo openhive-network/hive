39872d70ad1006fc1b54bf0cd12753f5dabc2d5a
Michał Kudela <kudmich@syncad.com>
2023-06-22 14:55:30 +0200

    [to_delete] show logs

--- libraries/plugins/witness/witness_plugin.cpp
+++ libraries/plugins/witness/witness_plugin.cpp
@@ -407,6 +407,7 @@ namespace detail {
 
   block_production_condition::block_production_condition_enum witness_plugin_impl::block_production_loop()
   {
+    ilog("witness_plugin_impl::block_production_loop");
     if( fc::time_point::now() < fc::time_point(HIVE_GENESIS_TIME) )
     {
       wlog( "waiting until genesis time to produce block: ${t}", ("t",HIVE_GENESIS_TIME) );
@@ -444,13 +445,13 @@ namespace detail {
         
         break;
       case block_production_condition::not_synced:
-  //         ilog("Not producing block because production is disabled until we receive a recent block (see: --enable-stale-production)");
+           ilog("Not producing block because production is disabled until we receive a recent block (see: --enable-stale-production)");
         break;
       case block_production_condition::not_my_turn:
-  //         ilog("Not producing block because it isn't my turn");
+           ilog("Not producing block because it isn't my turn");
         break;
       case block_production_condition::not_time_yet:
-  //         ilog("Not producing block because slot has not yet arrived");
+           ilog("Not producing block because slot has not yet arrived");
         break;
       case block_production_condition::no_private_key:
         ilog("Not producing block because I don't have the private key for ${scheduled_key}", ("scheduled_key", capture["scheduled_key"]) );
