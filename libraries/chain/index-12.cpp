098c3d1ff5fa605442b8c66ab97de334cb6375fd
ABW <andrzejl@syncad.com>
2023-06-27 15:56:28 +0200

    move registration of RC related indexes to core code

--- libraries/chain/index-12.cpp
+++ libraries/chain/index-12.cpp
@@ -8,16 +8,13 @@ namespace hive { namespace chain {
 
 void initialize_core_indexes_12( database& db )
 {
-  //TODO: uncomment later when moving RC activation to core
-  //HIVE_ADD_CORE_INDEX( hive::chain::rc_expired_delegation_index )
-  //HIVE_ADD_CORE_INDEX( hive::chain::rc_resource_param_index )
-  //HIVE_ADD_CORE_INDEX( hive::chain::rc_pool_index )
-  //HIVE_ADD_CORE_INDEX( hive::chain::rc_stats_index )
+  HIVE_ADD_CORE_INDEX( db, rc_resource_param_index );
+  HIVE_ADD_CORE_INDEX( db, rc_pool_index );
+  HIVE_ADD_CORE_INDEX( db, rc_stats_index );
 }
 
 } }
 
-HIVE_DEFINE_TYPE_REGISTRAR_REGISTER_TYPE( hive::chain::rc_expired_delegation_index )
 HIVE_DEFINE_TYPE_REGISTRAR_REGISTER_TYPE( hive::chain::rc_resource_param_index )
 HIVE_DEFINE_TYPE_REGISTRAR_REGISTER_TYPE( hive::chain::rc_pool_index )
 HIVE_DEFINE_TYPE_REGISTRAR_REGISTER_TYPE( hive::chain::rc_stats_index )
