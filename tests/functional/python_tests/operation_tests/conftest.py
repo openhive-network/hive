1e36f826cde5ec012765a318a7f729aa03a4814e
Mateusz Kudela <mkudela@syncad.com>
2023-06-27 09:29:04 +0200

    Split prepare_environment fixture into three fixtures

--- tests/functional/python_tests/operation_tests/conftest.py
+++ tests/functional/python_tests/operation_tests/conftest.py
@@ -5,12 +5,20 @@ from hive_local_tools.functional import VestPrice
 
 
 @pytest.fixture
-def prepare_environment():
+def speed_up_node() -> tt.InitNode:
     node = tt.InitNode()
     node.config.plugin.append("account_history_api")
     node.run(time_offset="+0h x5")
-    wallet = tt.Wallet(attach_to=node)
+    return node
 
+
+@pytest.fixture
+def wallet(speed_up_node: tt.InitNode) -> tt.Wallet:
+    return tt.Wallet(attach_to=speed_up_node)
+
+
+@pytest.fixture
+def prepared_node(speed_up_node: tt.InitNode, wallet: tt.Wallet) -> tt.InitNode:
     wallet.api.update_witness('initminer', 'http://url.html',
                               tt.Account('initminer').public_key,
                               {'account_creation_fee': tt.Asset.Test(3)}
@@ -19,16 +27,17 @@ def prepare_environment():
     new_price = VestPrice(quote=tt.Asset.Vest(1800), base=tt.Asset.Test(1))
     tt.logger.info(f"new vests price {new_price}.")
     tt.logger.info(f"new vests price {new_price.as_nai()}.")
-    node.api.debug_node.debug_set_vest_price(vest_price=new_price.as_nai())
+    speed_up_node.api.debug_node.debug_set_vest_price(vest_price=new_price.as_nai())
     wallet.api.transfer_to_vesting("initminer", "initminer", tt.Asset.Test(10_000_000))
 
-    node.wait_number_of_blocks(43)
+    speed_up_node.wait_number_of_blocks(43)
 
-    dgpo = node.api.wallet_bridge.get_dynamic_global_properties()
+    dgpo = speed_up_node.api.wallet_bridge.get_dynamic_global_properties()
     total_vesting_shares = dgpo['total_vesting_shares']["amount"]
     total_vesting_fund_hive = dgpo['total_vesting_fund_hive']["amount"]
     tt.logger.info(f"total_vesting_shares: {int(total_vesting_shares)}")
     tt.logger.info(f"total_vesting_fund_hive: {int(total_vesting_fund_hive)}")
     tt.logger.info(f"VESTING_PRICE: {int(total_vesting_shares) // int(total_vesting_fund_hive)}")
 
-    return node, wallet
+    return speed_up_node
+
--- tests/functional/python_tests/operation_tests/test_transfer_to_vesting_operation/test_transfer_to_vesting_operation.py
+++ tests/functional/python_tests/operation_tests/test_transfer_to_vesting_operation/test_transfer_to_vesting_operation.py
@@ -29,52 +29,50 @@ from hive_local_tools.constants import (
         ("alice", "bob"),  # User converts Hive to HP and transfers them to someone else account.
     ]
 )
-def test_transfer_vests(prepare_environment, sender, receiver):
-    node, wallet = prepare_environment
+def test_transfer_vests(prepared_node, wallet, sender, receiver):
 
-    price = get_vesting_price(node)
+    price = get_vesting_price(prepared_node)
     amount = tt.Asset.Test(0.001)
     wallet.create_account(sender, hives=1000)
-    sender_initial_balance = get_hive_balance(node, sender)
-    if receiver and not len(node.api.database.find_accounts(accounts=[receiver])["accounts"]):
+    sender_initial_balance = get_hive_balance(prepared_node, sender)
+    if receiver and not len(prepared_node.api.database.find_accounts(accounts=[receiver])["accounts"]):
         wallet.create_account(receiver)
 
     transaction = wallet.api.transfer_to_vesting(sender, receiver, amount)
 
     if not receiver:
         receiver = sender
-    operation_timestamp = get_transaction_timestamp(node, transaction)
+    operation_timestamp = get_transaction_timestamp(prepared_node, transaction)
 
-    __assert_hive_balance_is_reduced_by_amount(node, sender, sender_initial_balance, amount)
-    __assert_hive_power_balance_is_increased(node, receiver, tt.Asset.Vest(0))
-    __assert_power_up_exchange_rate(node, receiver, amount, price, tolerance=5)
-    __assert_virtual_operation_was_generated(node, "transfer_to_vesting_completed_operation",
+    __assert_hive_balance_is_reduced_by_amount(prepared_node, sender, sender_initial_balance, amount)
+    __assert_hive_power_balance_is_increased(prepared_node, receiver, tt.Asset.Vest(0))
+    __assert_power_up_exchange_rate(prepared_node, receiver, amount, price, tolerance=5)
+    __assert_virtual_operation_was_generated(prepared_node, "transfer_to_vesting_completed_operation",
                                              expected_number_of_virtual_operations=1)
-    __assert_governance_voting_power_is_not_increased(node, wallet, receiver, initial_governance_voting_power=0)
+    __assert_governance_voting_power_is_not_increased(prepared_node, wallet, receiver, initial_governance_voting_power=0)
     # TODO: after resolve #Issue507 add assert for: `RC current_mana is reduced.`
     #  Error message: "The cost of RC was not charged for executing the transaction."
-    __assert_rc_max_mana_is_increased(node, receiver, initial_rc_max_mana=0)
+    __assert_rc_max_mana_is_increased(prepared_node, receiver, initial_rc_max_mana=0)
     __assert_minimal_operation_rc_cost(transaction)
 
     # Jump to day 29
-    jump_to_date(node, operation_timestamp + timedelta(
+    jump_to_date(prepared_node, operation_timestamp + timedelta(
         seconds=DELAYED_VOTING_TOTAL_INTERVAL_SECONDS - DELAYED_VOTING_INTERVAL_SECONDS))
 
-    __assert_governance_voting_power_is_not_increased(node, wallet, receiver, initial_governance_voting_power=0)
+    __assert_governance_voting_power_is_not_increased(prepared_node, wallet, receiver, initial_governance_voting_power=0)
 
     # Jump to day 30
-    jump_to_date(node, operation_timestamp + timedelta(seconds=DELAYED_VOTING_TOTAL_INTERVAL_SECONDS))
+    jump_to_date(prepared_node, operation_timestamp + timedelta(seconds=DELAYED_VOTING_TOTAL_INTERVAL_SECONDS))
 
-    __assert_governance_voting_power_is_increased(node, wallet, receiver, initial_governance_voting_power=0)
+    __assert_governance_voting_power_is_increased(prepared_node, wallet, receiver, initial_governance_voting_power=0)
     # The first virtual operation `delayed_voting_operation` is generated during the preparation of the test environment
-    __assert_virtual_operation_was_generated(node, "delayed_voting_operation", expected_number_of_virtual_operations=2)
+    __assert_virtual_operation_was_generated(prepared_node, "delayed_voting_operation", expected_number_of_virtual_operations=2)
 
 
-def test_transfer_vests_twice(prepare_environment):
+def test_transfer_vests_twice(prepared_node, wallet):
     """
     User converts Hive to HP (Power Up 1) and after 5 days user converts Hive to HP again (Power Up 2).
     """
-    node, wallet = prepare_environment
 
     sender = "alice"
     receiver = "bob"
@@ -84,74 +82,74 @@ def test_transfer_vests_twice(prepare_environment):
     wallet.create_account(sender, hives=1000)
     wallet.create_account(receiver)
 
-    sender_initial_balance = get_hive_balance(node, sender)
-    receiver_initial_max_mana = get_rc_max_mana(node, receiver)
+    sender_initial_balance = get_hive_balance(prepared_node, sender)
+    receiver_initial_max_mana = get_rc_max_mana(prepared_node, receiver)
 
-    receiver_initial_hive_power_balance = get_hive_power(node, receiver)
-    initial_price = get_vesting_price(node)
+    receiver_initial_hive_power_balance = get_hive_power(prepared_node, receiver)
+    initial_price = get_vesting_price(prepared_node)
 
     first_transaction = wallet.api.transfer_to_vesting(sender, receiver, first_amount)
-    initial_time_first_operation = get_transaction_timestamp(node, first_transaction)
+    initial_time_first_operation = get_transaction_timestamp(prepared_node, first_transaction)
 
-    __assert_hive_balance_is_reduced_by_amount(node, sender, sender_initial_balance, first_amount)
-    __assert_hive_power_balance_is_increased(node, receiver, receiver_initial_hive_power_balance)
-    __assert_power_up_exchange_rate(node, receiver, first_amount, initial_price, tolerance=5)
-    __assert_virtual_operation_was_generated(node, "transfer_to_vesting_completed_operation",
+    __assert_hive_balance_is_reduced_by_amount(prepared_node, sender, sender_initial_balance, first_amount)
+    __assert_hive_power_balance_is_increased(prepared_node, receiver, receiver_initial_hive_power_balance)
+    __assert_power_up_exchange_rate(prepared_node, receiver, first_amount, initial_price, tolerance=5)
+    __assert_virtual_operation_was_generated(prepared_node, "transfer_to_vesting_completed_operation",
                                              expected_number_of_virtual_operations=1)
-    __assert_governance_voting_power_is_not_increased(node, wallet, receiver, 0)
-    __assert_rc_max_mana_is_increased(node, receiver, initial_rc_max_mana=receiver_initial_max_mana)
+    __assert_governance_voting_power_is_not_increased(prepared_node, wallet, receiver, 0)
+    __assert_rc_max_mana_is_increased(prepared_node, receiver, initial_rc_max_mana=receiver_initial_max_mana)
     __assert_minimal_operation_rc_cost(first_transaction)
     # TODO: after resolve #Issue507 add assert for: `RC current_mana is reduced.`
     #  Error message: "The cost of RC was not charged for executing the transaction."
 
     # Jump to day 5
-    jump_to_date(node, initial_time_first_operation + timedelta(seconds=5 * DELAYED_VOTING_INTERVAL_SECONDS))
+    jump_to_date(prepared_node, initial_time_first_operation + timedelta(seconds=5 * DELAYED_VOTING_INTERVAL_SECONDS))
 
-    sender_balance = get_hive_balance(node, sender)
-    receiver_hp_balance = get_hive_power(node, receiver)
-    receiver_rc_max_mana = get_rc_max_mana(node, receiver)
-    price_after_time_change = get_vesting_price(node)
+    sender_balance = get_hive_balance(prepared_node, sender)
+    receiver_hp_balance = get_hive_power(prepared_node, receiver)
+    receiver_rc_max_mana = get_rc_max_mana(prepared_node, receiver)
+    price_after_time_change = get_vesting_price(prepared_node)
 
     second_transaction = wallet.api.transfer_to_vesting(sender, receiver, second_amount)
-    initial_time_second_operation = get_transaction_timestamp(node, second_transaction)
+    initial_time_second_operation = get_transaction_timestamp(prepared_node, second_transaction)
 
-    __assert_hive_balance_is_reduced_by_amount(node, sender, sender_balance, second_amount)
-    __assert_hive_power_balance_is_increased(node, receiver, receiver_hp_balance)
-    __assert_power_up_exchange_rate(node, receiver, first_amount + second_amount, price_after_time_change, tolerance=5)
-    __assert_virtual_operation_was_generated(node, "transfer_to_vesting_completed_operation",
+    __assert_hive_balance_is_reduced_by_amount(prepared_node, sender, sender_balance, second_amount)
+    __assert_hive_power_balance_is_increased(prepared_node, receiver, receiver_hp_balance)
+    __assert_power_up_exchange_rate(prepared_node, receiver, first_amount + second_amount, price_after_time_change, tolerance=5)
+    __assert_virtual_operation_was_generated(prepared_node, "transfer_to_vesting_completed_operation",
                                              expected_number_of_virtual_operations=2)
-    __assert_rc_max_mana_is_increased(node, receiver, initial_rc_max_mana=receiver_rc_max_mana)
+    __assert_rc_max_mana_is_increased(prepared_node, receiver, initial_rc_max_mana=receiver_rc_max_mana)
     __assert_minimal_operation_rc_cost(second_transaction)
     # TODO: after resolve #Issue507 add assert for: `RC current_mana is reduced.` (compare to RC after Power up 1).
     #  Error message: "The cost of RC was not charged for executing the transaction."
 
     # Jump to day 29 (DELAYED_VOTING_TOTAL_INTERVAL_SECONDS - DELAYED_VOTING_INTERVAL_SECONDS)
-    jump_to_date(node, initial_time_first_operation + timedelta(
+    jump_to_date(prepared_node, initial_time_first_operation + timedelta(
         seconds=DELAYED_VOTING_TOTAL_INTERVAL_SECONDS - DELAYED_VOTING_INTERVAL_SECONDS))
 
-    __assert_governance_voting_power_is_not_increased(node, wallet, receiver, initial_governance_voting_power=0)
+    __assert_governance_voting_power_is_not_increased(prepared_node, wallet, receiver, initial_governance_voting_power=0)
 
     # Jump to day 30 (DELAYED_VOTING_TOTAL_INTERVAL_SECONDS)
-    jump_to_date(node, initial_time_first_operation + timedelta(seconds=DELAYED_VOTING_TOTAL_INTERVAL_SECONDS))
+    jump_to_date(prepared_node, initial_time_first_operation + timedelta(seconds=DELAYED_VOTING_TOTAL_INTERVAL_SECONDS))
 
-    governance_voting_power = get_governance_voting_power(node, wallet, receiver)
-    __assert_governance_voting_power_is_increased(node, wallet, receiver, initial_governance_voting_power=0)
+    governance_voting_power = get_governance_voting_power(prepared_node, wallet, receiver)
+    __assert_governance_voting_power_is_increased(prepared_node, wallet, receiver, initial_governance_voting_power=0)
     # The first virtual operation `delayed_voting_operation` is generated during the preparation of the test environment
-    __assert_virtual_operation_was_generated(node, "delayed_voting_operation", expected_number_of_virtual_operations=2)
+    __assert_virtual_operation_was_generated(prepared_node, "delayed_voting_operation", expected_number_of_virtual_operations=2)
 
     # Jump to day 34 (29 days after second power up operation)
-    jump_to_date(node, initial_time_second_operation + timedelta(seconds=29 * DELAYED_VOTING_INTERVAL_SECONDS))
+    jump_to_date(prepared_node, initial_time_second_operation + timedelta(seconds=29 * DELAYED_VOTING_INTERVAL_SECONDS))
 
-    __assert_governance_voting_power_is_not_increased(node, wallet, receiver,
+    __assert_governance_voting_power_is_not_increased(prepared_node, wallet, receiver,
                                                       initial_governance_voting_power=governance_voting_power)
 
     # Jump to day 35 (30 days after second power up operation)
-    jump_to_date(node, initial_time_second_operation + timedelta(seconds=30 * DELAYED_VOTING_INTERVAL_SECONDS))
+    jump_to_date(prepared_node, initial_time_second_operation + timedelta(seconds=30 * DELAYED_VOTING_INTERVAL_SECONDS))
 
-    __assert_governance_voting_power_is_increased(node, wallet, receiver,
+    __assert_governance_voting_power_is_increased(prepared_node, wallet, receiver,
                                                   initial_governance_voting_power=governance_voting_power)
     # The first virtual operation `delayed_voting_operation` is generated during the preparation of the test environment
-    __assert_virtual_operation_was_generated(node, "delayed_voting_operation", expected_number_of_virtual_operations=3)
+    __assert_virtual_operation_was_generated(prepared_node, "delayed_voting_operation", expected_number_of_virtual_operations=3)
 
 
 def __assert_governance_voting_power_is_increased(node: tt.InitNode, wallet: tt.Wallet, account: str,
