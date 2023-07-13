5c6c5ef4dfd66c4b29fe7a8d9fabb04e24014b89
“Mariusz Trela” <mtrela@syncad.com>
2023-07-13 14:59:31 +0200

    Extend API for WASM beekeeper

--- programs/beekeeper/beekeeper_wasm/beekeeper_wasm_app_api/include/beekeeper_wasm_app_api/beekeeper_wasm_api.hpp
+++ programs/beekeeper/beekeeper_wasm/beekeeper_wasm_app_api/include/beekeeper_wasm_app_api/beekeeper_wasm_api.hpp
@@ -2,7 +2,7 @@
 
 #include <beekeeper_wasm_app_api/beekeeper_wasm_app.hpp>
 
-#include <fc/optional.hpp>
+#include <fc/io/json.hpp>
 
 #include<vector>
 
@@ -16,6 +16,25 @@ class beekeeper_api
 
     beekeeper::beekeeper_wasm_app app;
 
+    template<typename T>
+    std::string get_string( const T& src )
+    {
+      fc::variant _v;
+      fc::to_variant( src, _v );
+      return fc::json::to_string( _v );
+    }
+
+    template<typename T>
+    std::vector<std::string> convert_to_vector(const T& src )
+    {
+      std::vector<std::string> _result;
+
+      for( auto& item : src )
+        _result.emplace_back( get_string( item ) );
+
+      return _result;
+    }
+
   public:
 
     beekeeper_api( const std::vector<std::string>& _params );
@@ -23,8 +42,28 @@ class beekeeper_api
     int init();
 
     std::string create_session( const std::string& salt );
+    void close_session( const std::string& token );
+
     std::string create( const std::string& token, const std::string& wallet_name, const std::string& password );
+    void unlock( const std::string& token, const std::string& wallet_name, const std::string& password );
+    void open( const std::string& token, const std::string& wallet_name );
+    void close( const std::string& token, const std::string& wallet_name );
+
+    void set_timeout( const std::string& token, int64_t seconds );
+
+    void lock_all( const std::string& token );
+    void lock( const std::string& token, const std::string& wallet_name );
+
     std::string import_key( const std::string& token, const std::string& wallet_name, const std::string& wif_key );
+    void remove_key( const std::string& token, const std::string& wallet_name, const std::string& password, const std::string& public_key );
+
+    std::vector<std::string> list_wallets( const std::string& token );
+    std::vector<std::string> get_public_keys( const std::string& token );
+
+    std::string sign_digest( const std::string& token, const std::string& public_key, const std::string& sig_digest );
+    std::string sign_transaction( const std::string& token, const std::string& transaction, const std::string& chain_id, const std::string& public_key, const std::string& sig_digest );
+
+    std::string get_info( const std::string& token );
 };
 
 }
--- programs/beekeeper/core/include/core/utilities.hpp
+++ programs/beekeeper/core/include/core/utilities.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <core/beekeeper_wallet_base.hpp>
+
 #include <fc/reflect/reflect.hpp>
 
 #include <functional>
@@ -28,6 +30,33 @@ namespace types
   using timepoint_t               = std::chrono::time_point<std::chrono::system_clock>;
 }
 
+namespace utility
+{
+  inline public_key_type get_public_key( const std::string& source )
+  {
+    return public_key_type::from_base58( source, false/*is_sha256*/ );
+  }
+
+  template<typename result_collection_type, typename source_type>
+  result_collection_type get_public_keys( const source_type& source )
+  {
+    result_collection_type _result;
+
+    std::transform( source.begin(), source.end(), std::inserter( _result, _result.end() ),
+    []( const public_key_type& public_key ){ return public_key_type::to_base58( public_key, false/*is_sha256*/ ); } );
+
+    return _result;
+  }
+}
+
+}
+namespace fc
+{
+  void from_variant( const fc::variant& var, beekeeper::wallet_details& vo );
+  void to_variant( const beekeeper::wallet_details& var, fc::variant& vo );
+
+  void from_variant( const fc::variant& var, beekeeper::info& vo );
+  void to_variant( const beekeeper::info& var, fc::variant& vo );
 }
 
 FC_REFLECT( beekeeper::wallet_details, (name)(unlocked) )
