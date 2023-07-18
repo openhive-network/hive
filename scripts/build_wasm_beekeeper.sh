fa3b9fbe4d95b35d22ec1eec2aa8f13ca25aa13c
Bartek Wrona <wrona@syncad.com>
2023-07-18 02:12:52 +0200

    Defined a script dedicated to perform a dockerized build to produce beekeeper WASM version

---
+++ scripts/build_wasm_beekeeper.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+set -xeuo pipefail
+
+if [ $# -eq 0 ]; then
+  echo "Performing a docker run"
+docker run \
+  -it --rm \
+  -v $(pwd)/../:/src \
+  -u $(id -u):$(id -g) \
+  registry.gitlab.syncad.com/hive/common-ci-configuration/emsdk:3.1.43 \
+  /bin/bash /src/scripts/build_wasm_beekeeper.sh 1
+else
+  echo "Performing a build"
+  cd /src
+  mkdir -vp build_wasm
+  rm -rf ./build_wasm/*
+  cd build_wasm
+
+  #-DBoost_DEBUG=TRUE -DBoost_VERBOSE=TRUE -DCMAKE_STATIC_LIBRARY_SUFFIX=".a;.bc" 
+  cmake \
+    -DBoost_NO_WARN_NEW_VERSIONS=1 \
+    -DBoost_USE_STATIC_RUNTIME=ON \
+    -DCMAKE_TOOLCHAIN_FILE=/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" -S /src/programs/beekeeper/beekeeper_wasm/ -B /src/build_wasm/
+  make -j8
+fi
+
+#  emcc -sFORCE_FILESYSTEM test.cpp -o test.js -lembind -lidbfs.js --emrun 
+
+# execute on host machine
+#node helloworld.js
