ee80e3b58505a14ed5976d77d5eb4401ac7147cf
mtyszczak <mateusz.tyszczak@gmail.com>
2023-07-24 15:17:19 +0200

    fixup! Fix emscripten compiler errors

--- libraries/fc/src/log/configure_logging.cpp
+++ libraries/fc/src/log/configure_logging.cpp
@@ -1,6 +1,10 @@
 #include <fc/log/logger_config.hpp>
-#include <fc/log/appender.hpp>
+#include <fc/log/log_message.hpp>
 
+#include <fc/thread/thread.hpp>
+#include <fc/thread/task.hpp>
+
+#include <fc/log/appender.hpp>
 #include <fc/log/console_appender.hpp>
 #include <fc/log/file_appender.hpp>
 
@@ -12,6 +16,11 @@ bool configure_logging()
   static bool reg_file_appender = appender::register_appender<file_appender>( "file" );
 
   return reg_console_appender || reg_file_appender;
+}
+
+const char* log_context::get_current_task_desc()const
+{
+  return fc::thread::current().current_task_desc();
 }
 
 } // namespace fc
--- libraries/fc/src/log/configure_logging_minimal.cpp
+++ libraries/fc/src/log/configure_logging_minimal.cpp
@@ -1,6 +1,7 @@
 #include <fc/log/logger_config.hpp>
-#include <fc/log/appender.hpp>
+#include <fc/log/log_message.hpp>
 
+#include <fc/log/appender.hpp>
 #include <fc/log/console_appender.hpp>
 
 namespace fc {
@@ -10,6 +11,11 @@ bool configure_logging()
   static bool reg_console_appender = appender::register_appender<console_appender>( "console" );
 
   return reg_console_appender;
+}
+
+const char* log_context::get_current_task_desc()const
+{
+  return nullptr;
 }
 
 } // namespace fc
