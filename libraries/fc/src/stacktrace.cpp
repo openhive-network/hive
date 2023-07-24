88a058c9fcc7370547e60c220284257d297f2cb4
mtyszczak <mateusz.tyszczak@gmail.com>
2023-07-24 14:32:42 +0200

    fixup! Fix emscripten compiler errors

--- libraries/fc/src/stacktrace.cpp
+++ libraries/fc/src/stacktrace.cpp
@@ -8,7 +8,7 @@
 #include <fc/macros.hpp>
 #include <fc/stacktrace.hpp>
 
-#if defined(__GNUC__) && !defined( __APPLE__ ) && !defined( __EMSCRIPTEN__ )
+#if defined(__GNUC__) && !defined( __APPLE__ )
 
 #include <cxxabi.h>
 #include <execinfo.h>
