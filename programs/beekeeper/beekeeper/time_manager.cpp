7eff552bec406be0b4147fa0d56ee4b8eabce724
“Mariusz Trela” <mtrela@syncad.com>
2023-07-28 07:39:26 +0200

    Although WASM beekeeper automatic locking is disabled, calling API endpoint triggers locks for every wallet when timeout is reached

--- programs/beekeeper/beekeeper/time_manager.cpp
+++ programs/beekeeper/beekeeper/time_manager.cpp
@@ -1,8 +1,5 @@
 #include <beekeeper/time_manager.hpp>
 
-#include <thread>
-#include <mutex>
-
 namespace beekeeper {
 
 time_manager::time_manager()
@@ -23,51 +20,4 @@ time_manager::~time_manager()
   notification_thread->join();
 }
 
-void time_manager::run()
-{
-  std::lock_guard<std::mutex> guard( methods_mutex );
-
-  const auto& now = std::chrono::system_clock::now();
-
-  auto& _idx = items.get<by_time>();
-
-  auto _it = _idx.begin();
-  while( _it != _idx.end() )
-  {
-    if( now >= _it->time )
-    {
-      _it->lock_method();
-      _it->notification_method();
-      _idx.modify( _it, []( session_data &sd ){ sd.time = types::timepoint_t::max(); });
-    }
-    ++_it;
-  }
-
-}
-
-void time_manager::add( const std::string& token, types::lock_method_type&& lock_method, types::notification_method_type&& notification_method )
-{
-  std::lock_guard<std::mutex> guard( methods_mutex );
-
-  auto& _idx = items.get<by_token>();
-  _idx.emplace( session_data{ token, lock_method, notification_method } );
-}
-
-void time_manager::change( const std::string& token, const types::timepoint_t& time )
-{
-  std::lock_guard<std::mutex> guard( methods_mutex );
-
-  auto& _idx = items.get<by_token>();
-  const auto& _found = _idx.find( token );
-
-  _idx.modify( _found, [&time]( session_data &sd ){ sd.time = time; });
-}
-
-void time_manager::close( const std::string& token )
-{
-  std::lock_guard<std::mutex> guard( methods_mutex );
-
-  auto& _idx = items.get<by_token>();
-  _idx.erase( token );
-}
 } //beekeeper
