712f0893b36cc277754bc08f42518acbf04face6
Michał Kudela <kudmich@syncad.com>
2023-07-28 11:26:32 +0200

    Add additional assertions to tests in test_decline_voting_rights file

--- tests/functional/python_tests/hf28_tests/test_decline_voting_rights.py
+++ tests/functional/python_tests/hf28_tests/test_decline_voting_rights.py
@@ -2,20 +2,27 @@ import pytest
 
 import test_tools as tt
 from hive_local_tools import run_for
-from hive_local_tools.constants import TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS
+from hive_local_tools.constants import OWNER_AUTH_RECOVERY_PERIOD, TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS
 from hive_local_tools.functional.python.hf28.constants import VOTER_ACCOUNT
+from hive_local_tools.functional.python.operation import get_virtual_operation, get_current_mana
 
 
 @run_for("testnet")
 def test_decline_voting_rights(prepare_environment):
     node, wallet = prepare_environment
 
-    wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)
+    mana_before_decline = get_current_mana(node, VOTER_ACCOUNT)
+
+    transaction = wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)
+    assert get_current_mana(node, VOTER_ACCOUNT) < mana_before_decline
+    assert transaction["rc_cost"] > 0
+
     assert len(node.api.database.find_decline_voting_rights_requests(accounts=[VOTER_ACCOUNT])["requests"]) == 1
     assert node.api.database.find_accounts(accounts=[VOTER_ACCOUNT])['accounts'][0]["can_vote"] == True
     node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
     assert len(node.api.database.find_decline_voting_rights_requests(accounts=[VOTER_ACCOUNT])["requests"]) == 0
     assert node.api.database.find_accounts(accounts=[VOTER_ACCOUNT])['accounts'][0]["can_vote"] == False
+    assert len(get_virtual_operation(node, "declined_voting_rights_operation")) == 1
 
 
 @run_for("testnet")
@@ -42,6 +49,11 @@ def test_create_two_decline_voting_rights_requests(prepare_environment):
     response = exception.value.response
     assert "Cannot create new request because one already exists." in response["error"]["message"]
 
+    node.wait_number_of_blocks(OWNER_AUTH_RECOVERY_PERIOD)
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[VOTER_ACCOUNT])["requests"]) == 0
+    assert node.api.database.find_accounts(accounts=[VOTER_ACCOUNT])['accounts'][0]["can_vote"] == False
+    assert len(get_virtual_operation(node, "declined_voting_rights_operation")) == 1
+
 
 @run_for("testnet")
 def test_remove_non_existent_decline_voting_rights_request(prepare_environment):
@@ -64,3 +76,8 @@ def test_remove_decline_voting_rights_request(prepare_environment):
     node.wait_number_of_blocks(1)
     assert len(node.api.database.find_decline_voting_rights_requests(accounts=[VOTER_ACCOUNT])["requests"]) == 0
     assert node.api.database.find_accounts(accounts=[VOTER_ACCOUNT])['accounts'][0]["can_vote"] == True
+
+    node.wait_number_of_blocks(OWNER_AUTH_RECOVERY_PERIOD)
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[VOTER_ACCOUNT])["requests"]) == 0
+    assert node.api.database.find_accounts(accounts=[VOTER_ACCOUNT])['accounts'][0]["can_vote"] == True
+    assert len(get_virtual_operation(node, "declined_voting_rights_operation")) == 0
