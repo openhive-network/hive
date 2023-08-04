f3032e4ee688676215099790f04f4145cfa2fb7c
asuch <asuch@syncad.com>
2023-08-04 10:00:53 +0200

    Prepare simple tests for artifacts generation process.

---
+++ tests/integration/artifacts_generation/test_artifacts_generation.sh
@@ -0,0 +1,42 @@
+#! /bin/bash -x
+
+set -xeuo pipefail
+
+HIVED_BINARY_PATH=${1}
+
+SOURCE_100K_BLOCK_LOG_PATTERN="blockchain/block_log.100k"
+SOURCE_ARTIFACTS_V_1_0_PATTERN="blockchain/block_log.artifacts.v_1_0"
+SOURCE_ARTIFACTS_V_1_1_INTERRUPTED_PATTERN="blockchain/block_log.artifacts.v_1_1.interrupted_at_19313"
+
+test_generate_artifacts_from_scratch() {
+  echo "Test 1 - generate artifacts from scratch."
+  local TEST_DATA_DIR="generate_from_scratch"
+  local TEST_BLOCKCHAIN_DIR="$TEST_DATA_DIR/blockchain"
+  mkdir -p "$TEST_BLOCKCHAIN_DIR"
+  cp "$SOURCE_100K_BLOCK_LOG_PATTERN" "$TEST_BLOCKCHAIN_DIR/block_log"
+  "$HIVED_BINARY_PATH" -d "$TEST_DATA_DIR" --replay --exit-before-sync
+}
+
+test_generate_artifacts_override_old_file() {
+  echo "Test 2 - override old artifacts file and create a new one."
+  local TEST_DATA_DIR="override_old_file_and_generate_from_scratch"
+  local TEST_BLOCKCHAIN_DIR="$TEST_DATA_DIR/blockchain"
+  mkdir -p "$TEST_BLOCKCHAIN_DIR"
+  cp "$SOURCE_100K_BLOCK_LOG_PATTERN" "$TEST_BLOCKCHAIN_DIR/block_log"
+  cp "$SOURCE_ARTIFACTS_V_1_0_PATTERN" "$TEST_BLOCKCHAIN_DIR/block_log.artifacts"
+  "$HIVED_BINARY_PATH" -d "$TEST_DATA_DIR" --replay --exit-before-sync
+}
+
+test_resume_artifacts_generating_process() {
+  echo "Test 3 - Resume artifacts generating process"
+  local TEST_DATA_DIR="resume_artifacts_generating_process"
+  local TEST_BLOCKCHAIN_DIR="$TEST_DATA_DIR/blockchain"
+  mkdir -p "$TEST_BLOCKCHAIN_DIR"
+  cp "$SOURCE_100K_BLOCK_LOG_PATTERN" "$TEST_BLOCKCHAIN_DIR/block_log"
+  cp "$SOURCE_ARTIFACTS_V_1_1_INTERRUPTED_PATTERN" "$TEST_BLOCKCHAIN_DIR/block_log.artifacts"
+  "$HIVED_BINARY_PATH" -d "$TEST_DATA_DIR" --replay --exit-before-sync
+}
+
+test_generate_artifacts_from_scratch
+test_generate_artifacts_override_old_file
+test_resume_artifacts_generating_process
