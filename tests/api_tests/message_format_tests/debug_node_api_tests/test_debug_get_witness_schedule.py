7dedd7b950adc3c84305b5a768a0d643eb985549
kmochocki <kmochocki@syncad.com>
2023-08-08 13:15:25 +0000

    Fix tests

--- tests/api_tests/message_format_tests/debug_node_api_tests/test_debug_get_witness_schedule.py
+++ tests/api_tests/message_format_tests/debug_node_api_tests/test_debug_get_witness_schedule.py
@@ -1,7 +1,12 @@
 from hive_local_tools import run_for
+import test_tools as tt
 
 
 # Tests for debug_node API can be performed only in testnet
 @run_for("testnet")
-def test_debug_get_witness_schedule(node):
-    assert node.api.debug_node.debug_get_witness_schedule()
+def test_debug_get_witness_schedule(node: tt.InitNode):
+    debug_api_method_reponse = node.api.debug_node.debug_get_witness_schedule()
+    database_api_method_response = node.api.database.get_witness_schedule()
+    assert debug_api_method_reponse.dict(
+        by_alias=True, exclude_none=True
+    ) == database_api_method_response.dict(by_alias=True, exclude_none=True)
