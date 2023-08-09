280720b7ac0f6a9d723fa006e16daa90f0602460
Gandalf <Gandalf-the-Grey@users.noreply.github.com>
2023-08-09 09:04:41 +0200

    Replace aggroed with splinterlands on a list of default (initial) seed nodes for a better reference.

--- libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
@@ -15,7 +15,7 @@ const std::vector< std::string > default_seeds = {
   "hive-seed.roelandp.nl:2001",          // roelandp
   "hive-seed.arcange.eu:2001",           // arcange
   "anyx.io:2001",                        // anyx
-  "hived.splinterlands.com:2001",        // aggroed
+  "hived.splinterlands.com:2001",        // splinterlands
   "seed.hive.blue:2001",                 // guiltyparties
   "hive-api.3speak.tv:2001",             // threespeak
   "node.mahdiyari.info:2001",            // mahdiyari
