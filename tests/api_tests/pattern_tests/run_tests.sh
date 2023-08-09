d82ddc4a1bbb11b3d2b8b23e306c1f957b0ad7be
kmochocki <kmochocki@syncad.com>
2023-08-09 18:22:01 +0000

    Remove tox

--- tests/api_tests/pattern_tests/run_tests.sh
+++ tests/api_tests/pattern_tests/run_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export PYTHONPATH="$2/tests/test_tools/package"
-
 if [[ $1 == *":"* ]]; then
     export HIVEMIND_ADDRESS=${1%:*}
     export HIVEMIND_PORT=${1##*:}
@@ -9,20 +7,14 @@ else
     export HIVEMIND_ADDRESS=localhost
     export HIVEMIND_PORT=$1
 fi
+
 export TAVERN_DIR="$2/tests/api_tests/pattern_tests/"
-
 default_testsuite=${3:?"Test suite must be specified as 3-rd script argument"}
-
 export IS_DIRECT_CALL_HAFAH=${4:-FALSE}
 
-tox -e tavern --                                 \
-    --tb=line                                    \
-    -n 8                                         \
+pushd "$TAVERN_DIR"
+pytest -n auto                                   \
     --cache-clear                                \
-    -p no:logging                                \
-    --show-capture=log                           \
     --junitxml=results.xml                       \
-    -W ignore::pytest.PytestDeprecationWarning   \
-    -W ignore::DeprecationWarning                \
-    -k "${default_testsuite}"
-
+    -k "${default_testsuite}" tavern/
+popd
