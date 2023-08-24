266e336d7863845e1b272d9e85ff9c75e8e2dfcf
“Mariusz Trela” <mtrela@syncad.com>
2023-08-24 15:42:40 +0200

    Automatic lock sends a notification when an error occurs

--- programs/beekeeper/beekeeper/include/beekeeper/session_manager.hpp
+++ programs/beekeeper/beekeeper/include/beekeeper/session_manager.hpp
@@ -10,11 +10,11 @@ class session_manager: public session_manager_base
 {
   protected:
 
-    std::shared_ptr<session_base> create_session( const std::string& notifications_endpoint, const std::string& token, std::shared_ptr<time_manager_base> time ) override;
+    std::shared_ptr<session_base> create_session( const std::string& notifications_endpoint, const std::string& token, std::shared_ptr<time_manager_base> ) override;
 
   public:
 
-    session_manager();
+    session_manager( const std::string& notifications_endpoint );
     ~session_manager() override {}
 };
 
