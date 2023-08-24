e82661248c39bb8104643b1a077b368a401977ed
“Mariusz Trela” <mtrela@syncad.com>
2023-08-24 15:42:40 +0200

    Automatic lock sends a notification when an error occurs

--- programs/beekeeper/beekeeper/session_manager.cpp
+++ programs/beekeeper/beekeeper/session_manager.cpp
@@ -4,12 +4,12 @@
 
 namespace beekeeper {
 
-session_manager::session_manager()
+session_manager::session_manager( const std::string& notifications_endpoint )
 {
-  time = std::make_shared<time_manager>();
+  time = std::make_shared<time_manager>( notifications_endpoint );
 }
 
-std::shared_ptr<session_base> session_manager::create_session( const std::string& notifications_endpoint, const std::string& token, std::shared_ptr<time_manager_base> time )
+std::shared_ptr<session_base> session_manager::create_session( const std::string& notifications_endpoint, const std::string& token, std::shared_ptr<time_manager_base> _not_used_time )
 {
   return std::make_shared<session>( notifications_endpoint, token, time );
 }
--- programs/beekeeper/beekeeper/time_manager.cpp
+++ programs/beekeeper/beekeeper/time_manager.cpp
@@ -1,8 +1,11 @@
 #include <beekeeper/time_manager.hpp>
 
+#include <appbase/application.hpp>
+
 namespace beekeeper {
 
-time_manager::time_manager()
+time_manager::time_manager( const std::string& notifications_endpoint )
+              : error_notifications_endpoint( notifications_endpoint )
 {
   notification_thread = std::make_unique<std::thread>( [this]()
     {
@@ -18,6 +21,24 @@ time_manager::~time_manager()
 {
   stop_requested = true;
   notification_thread->join();
+}
+
+void time_manager::send_auto_lock_error_message( const std::string& message )
+{
+  std::string _error_message = exception::exception_handler( "Send auto lock error_message",
+                                [&, this]()
+                                {
+                                  hive::utilities::notifications::notification_handler_wrapper _notification_handler;
+                                  _notification_handler.register_endpoints( { error_notifications_endpoint } );
+
+                                  appbase::application::dynamic_notify( _notification_handler, "Automatic lock error", "message", message );
+                                  return "";
+                                }
+                              );
+  if( !_error_message.empty() )
+  {
+    elog( _error_message );
+  }
 }
 
 } //beekeeper
