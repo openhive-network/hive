af71bce5dc7b3ab8dfaf9f1e13cc0055cedd08d8
Marcin Sobczyk <msobczyk@syncad.com>
2023-09-22 12:13:45 +0200

    Copy datadir content when starting hive as service

---
+++ scripts/copy_datadir.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -xeuo pipefail
+
+
+if [ -n "${DATA_SOURCE+x}" ]
+then
+    if [ "$(realpath "${DATA_SOURCE}/datadir")" != "$(realpath "${DATADIR}")" ]
+    then
+        echo "Creating copy of ${DATA_SOURCE}/datadir inside ${DATADIR}"
+        sudo -Enu hived mkdir -p "${DATADIR}"
+        flock "${DATA_SOURCE}/datadir" sudo -En cp -pr "${DATA_SOURCE}/datadir"/*  "${DATADIR}"
+        if [ "$(realpath "${DATA_SOURCE}/shm_dir")" != "$(realpath "${SHM_DIR}")" ]
+        then
+            echo "Creating copy of ${DATA_SOURCE}/shm_dir inside ${SHM_DIR}"
+            sudo -Enu hived mkdir -p "${SHM_DIR}"
+            flock "${DATA_SOURCE}/datadir" sudo -En cp -pr "${DATA_SOURCE}/shm_dir"/* "${SHM_DIR}"
+        fi
+    fi
+fi
