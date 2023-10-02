f9d6febc831ea37f73bd4e9c941e52a184d2b57f
mtyszczak <mateusz.tyszczak@gmail.com>
2023-10-02 16:56:44 +0200

    Expose fs interface for the beekeeper module

--- programs/beekeeper/beekeeper_wasm/beekeeper.d.ts
+++ programs/beekeeper/beekeeper_wasm/beekeeper.d.ts
@@ -2,6 +2,24 @@ export * from '../../../build_wasm/beekeeper';
 
 import { MainModule } from '../../../build_wasm/beekeeper';
 
-declare function beekeepermodule(): Promise<MainModule>;
+export interface FileSystemType {
+  MEMFS: {};
+  NODEFS: {};
+  IDBFS: {};
+}
+
+interface BeekeeperModule extends MainModule {
+  FS: {
+    // Required types from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/emscripten/index.d.ts for IDBFS usage
+    mkdir(path: string, mode?: number): any;
+    chdir(path: string): void;
+    mount(type: FileSystemType, opts: any, mountpoint: string): any;
+    syncfs(populate: boolean, callback: (e: any) => any): void;
+    syncfs(callback: (e: any) => any, populate?: boolean): void;
+    filesystems: FileSystemType;
+  }
+}
+
+declare function beekeepermodule(): Promise<BeekeeperModule>;
 
 export default beekeepermodule;
