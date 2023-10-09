82e52abb35a5be1b7180854881be4f3a75842627
“Mariusz Trela” <mtrela@syncad.com>
2023-10-09 09:05:34 +0200

    Plugins use an object of `blockchain_worker_thread_pool` class created in a `chain` plugin

--- programs/blockchain_converter/block_log_conversion_plugin.cpp
+++ programs/blockchain_converter/block_log_conversion_plugin.cpp
@@ -42,25 +42,27 @@ namespace detail {
     block_log log_in, log_out;
 
     block_log_conversion_plugin_impl( const hp::private_key_type& _private_key, const hp::chain_id_type& chain_id, appbase::application& app, size_t signers_size = 1 )
-      : conversion_plugin_impl( _private_key, chain_id, signers_size, app, true ), log_in( app ), log_out( app ), theApp( app ) {}
+      : conversion_plugin_impl( _private_key, chain_id, signers_size, app, true ), log_in( app ), log_out( app ), theApp( app ),
+        thread_pool( hive::chain::blockchain_worker_thread_pool( app ) ){}
 
     virtual void convert( uint32_t start_block_num, uint32_t stop_block_num ) override;
     void open( const fc::path& input, const fc::path& output );
     void close();
 
     appbase::application& theApp;
+    hive::chain::blockchain_worker_thread_pool thread_pool;
   };
 
   void block_log_conversion_plugin_impl::open( const fc::path& input, const fc::path& output )
   {
     try
     {
-      log_in.open( input );
+      log_in.open( input, thread_pool );
     } FC_CAPTURE_AND_RETHROW( (input) )
 
     try
     {
-      log_out.open( output );
+      log_out.open( output, thread_pool );
     } FC_CAPTURE_AND_RETHROW( (output) )
   }
 
