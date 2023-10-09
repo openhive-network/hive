5d59a9514bcbe549cb61dbada51f74f1e8a7f471
mtyszczak <mateusz.tyszczak@gmail.com>
2023-10-09 16:53:37 +0200

    Improve beekeeper types in typescript

--- programs/beekeeper/beekeeper_wasm/beekeeper.d.ts
+++ programs/beekeeper/beekeeper_wasm/beekeeper.d.ts
@@ -8,12 +8,14 @@ export interface FileSystemType {
   IDBFS: {};
 }
 
-interface BeekeeperModule extends MainModule {
+export type TFileSystemType = FileSystemType['MEMFS'] | FileSystemType['NODEFS'] | FileSystemType['IDBFS'];
+
+export interface BeekeeperModule extends MainModule {
   FS: {
     // Required types from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/emscripten/index.d.ts for IDBFS usage
     mkdir(path: string, mode?: number): any;
     chdir(path: string): void;
-    mount(type: FileSystemType, opts: any, mountpoint: string): any;
+    mount(type: TFileSystemType, opts: any, mountpoint: string): any;
     syncfs(populate: boolean, callback: (e: any) => any): void;
     syncfs(callback: (e: any) => any, populate?: boolean): void;
     filesystems: FileSystemType;
