f99212c2120fc1db6a4423cac62f1cc729f1a04c
mtyszczak <mateusz.tyszczak@gmail.com>
2023-10-16 20:44:24 +0200

    Add beekeeper storage tests

--- programs/beekeeper/beekeeper_wasm/src/beekeeper.ts
+++ programs/beekeeper/beekeeper_wasm/src/beekeeper.ts
@@ -15,6 +15,11 @@ export interface BeekeeperModule extends MainModule {
   FS: {
     // Required types from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/emscripten/index.d.ts for IDBFS usage
     mkdir(path: string, mode?: number): any;
+    readdir(path: string): string;
+    readFile(path: string, opts: { encoding: "binary"; flags?: string | undefined }): Uint8Array;
+    readFile(path: string, opts: { encoding: "utf8"; flags?: string | undefined }): string;
+    readFile(path: string, opts?: { flags?: string | undefined }): Uint8Array;
+    writeFile(path: string, data: string | ArrayBufferView, opts?: { flags?: string | undefined }): void;
     chdir(path: string): void;
     mount(type: TFileSystemType, opts: any, mountpoint: string): any;
     syncfs(populate: boolean, callback: (e: any) => any): void;
