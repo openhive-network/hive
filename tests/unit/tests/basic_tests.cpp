fda624819c823471e220b576c5f455f55333fbdc
asuch <asuch@syncad.com>
2023-10-19 13:36:00 +0200

    Prepare test for creating array from json

--- tests/unit/tests/basic_tests.cpp
+++ tests/unit/tests/basic_tests.cpp
@@ -862,6 +862,101 @@ BOOST_AUTO_TEST_CASE( fc_json_integer_formatting )
   }
 }
 
+BOOST_AUTO_TEST_CASE( create_array_from_fc_json )
+{
+  {
+    const std::string json = "[\"object_1\", \"object_2\", \"object_3\", \"object_4\"]";
+    fc::variants array;
+    BOOST_CHECK_NO_THROW(array = fc::json::from_string(json).get_array());
+    BOOST_CHECK_EQUAL(array.size(), 4);
+  }
+  {
+    const std::string json = "[    \n\"object_1\"\t\n\t  ,\t\n    \"object_2\"         ,\n\t\n \"object_3\",\t\t\t\n\n\n \"object_4\"\n\n\n\n\t\t, 6, 10\n\n]";
+    fc::variants array;
+    BOOST_CHECK_NO_THROW(array = fc::json::from_string(json).get_array());
+    BOOST_CHECK_EQUAL(array.size(), 6);
+  }
+  {
+    const std::string json = "[\"object_1\", \"obj,ect_2\", \"object_3\", \"object_4\"]";
+    fc::variants array;
+    BOOST_CHECK_NO_THROW(array = fc::json::from_string(json).get_array());
+    BOOST_CHECK_EQUAL(array.size(), 4);
+    BOOST_CHECK_EQUAL(array[1].as_string(), "obj,ect_2");
+  }
+  {
+    const std::string json = "[\"object_1\", {\"k1\":\"v1\", \"k2\":\"v2\"}, \"object_2\", \"object_3\"]";
+    fc::variants array;
+    BOOST_CHECK_NO_THROW(array = fc::json::from_string(json).get_array());
+    BOOST_CHECK_EQUAL(array.size(), 4);
+  }
+  {
+    const std::string json = "[]";
+    fc::variants array;
+    BOOST_CHECK_NO_THROW(array = fc::json::from_string(json).get_array());
+    BOOST_CHECK(array.empty());
+  }
+  {
+    const std::string json = "[\"\"]";
+    fc::variants array;
+    BOOST_CHECK_NO_THROW(array = fc::json::from_string(json).get_array());
+    BOOST_CHECK_EQUAL(array.size(), 1);
+    BOOST_CHECK_EQUAL(array[0].as_string(), "");
+  }
+  {
+    const std::string json = "[[1,2,3,\"object_1\"]]";
+    fc::variants array;
+    BOOST_CHECK_NO_THROW(array = fc::json::from_string(json).get_array());
+    BOOST_CHECK_EQUAL(array.size(), 1);
+    fc::variants array_in_array;
+    BOOST_CHECK_NO_THROW(array_in_array = array[0].get_array());
+    BOOST_CHECK_EQUAL(array_in_array[0].as_uint64(), 1);
+    BOOST_CHECK_EQUAL(array_in_array[1].as_uint64(), 2);
+    BOOST_CHECK_EQUAL(array_in_array[2].as_uint64(), 3);
+    BOOST_CHECK_EQUAL(array_in_array[3].as_string(), "object_1");
+  }
+  {
+    const std::string json = "[\"object_1\" \"object_2\" \"object_3\" \"object_4\"]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::parse_error_exception);
+  }
+  {
+    const std::string json = "[\"object_1\", \"object_2\" \"object_3\" \"object_4\"]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::parse_error_exception);
+  }
+  {
+    const std::string json = "[\"object_1\", \"object_2\", \"object_3\" \n \"object_4\"]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::parse_error_exception);
+  }
+  {
+    const std::string json = "[\"object_1\", \"object_2\", \"object_3\" \t \"object_4\"]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::parse_error_exception);
+  }
+  {
+    const std::string json = "[\"object_1\", \"object_2\", \"object_3\" . \"object_4\"]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::parse_error_exception);
+  }
+  {
+    const std::string json = "[,\"object_1\", \"object_2\", \"object_3\", \"object_4\"]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::parse_error_exception);
+  }
+  {
+    const std::string json = "[\"object_1\", \"object_2\", \"object_3\", \"object_4\",]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::parse_error_exception);
+  }
+  {
+    const std::string json = "[\"object_1\", \"object_2\", \"object_3\" , \"object_4\"";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::eof_exception);
+  }
+  {
+    const std::string json = "\"object_1\", \"object_2\", \"object_3\" , \"object_4\"]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::bad_cast_exception);
+  }
+  {
+    const std::string json = "[\"object_1\",, \"object_3\" , \"object_4\"]";
+    BOOST_CHECK_THROW(fc::json::from_string(json).get_array(), fc::parse_error_exception);
+  }
+}
+
+
 BOOST_AUTO_TEST_CASE( decoding_types_mechanism_test )
 {
   hive::chain::util::decoded_types_data_storage dtds;
