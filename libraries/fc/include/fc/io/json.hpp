cd1ec8eee50f466045a8044f1260d4b08cc4c6a7
asuch <asuch@syncad.com>
2023-10-30 21:59:01 +0100

    Rename fc json flag 'strict_validation' to 'full_json_validation'

--- libraries/fc/include/fc/io/json.hpp
+++ libraries/fc/include/fc/io/json.hpp
@@ -43,13 +43,13 @@ namespace fc
 
          static variant  from_stream( buffered_istream& in, parse_type ptype = legacy_parser, uint32_t depth = 0 );
 
-         static variant  from_string( const string& utf8_str, const bool strict_validation = true, parse_type ptype = legacy_parser, uint32_t depth = 0 );
+         static variant  from_string( const string& utf8_str, const bool full_json_validation = true, parse_type ptype = legacy_parser, uint32_t depth = 0 );
          static variant  fast_from_string( const string& utf8_str );
          static variants variants_from_string( const string& utf8_str, parse_type ptype = legacy_parser, uint32_t depth = 0 );
          static string   to_string( const variant& v, output_formatting format = stringify_large_ints_and_doubles );
          static string   to_pretty_string( const variant& v, output_formatting format = stringify_large_ints_and_doubles );
 
-         static bool     is_valid( const std::string& json_str, const bool strict_validation = true, parse_type ptype = legacy_parser, uint32_t depth = 0 );
+         static bool     is_valid( const std::string& json_str, const bool full_json_validation = true, parse_type ptype = legacy_parser, uint32_t depth = 0 );
          static bool     fast_is_valid( const std::string& json_str );
 
          template<typename T>
