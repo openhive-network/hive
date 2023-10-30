8fa29ea0bd18fafbc25c09eff61b44d543562230
Mateusz Kudela <mkudela@syncad.com>
2023-10-30 15:58:17 +0100

    Fix test_bucket_output_parameters test
    Now test calculate when current bucket closes and checks if it's enough
    time to broadcast all transactions. If not - it waits for new bucket to
    open.

--- tests/python/functional/api_tests/market_history_api_tests/test_get_market_history.py
+++ tests/python/functional/api_tests/market_history_api_tests/test_get_market_history.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import datetime
+
 import pytest
 
 import test_tools as tt
@@ -107,24 +109,44 @@ def test_get_two_buckets(node, bucket_seconds, blocks_to_wait):
 def test_bucket_output_parameters(node, tbds, hive_high, hive_low, non_hive_high, non_hive_low, non_hive_volume):
     wallet = tt.Wallet(attach_to=node)
     wallet.create_account("alice", hives=tt.Asset.Test(1000), vests=tt.Asset.Test(100))
+    epoch_time_zero = tt.Time.parse("1970-01-01T00:00:00")
+    time_needed_for_transactions = 15  # in seconds
+    transactions_done = False
 
-    start_block = node.get_last_block_number()
-    tt.logger.info(f"Starting order creation at block: {start_block}")  # Usually starts at block #3
+    while not transactions_done:
+        hbt = node.get_head_block_time()
+        open_in_seconds_from_epoch_time_zero = (
+            (hbt - epoch_time_zero).total_seconds() // 300
+        ) * 300  # time when current bucket opens at
+        bucket_opens = epoch_time_zero + datetime.timedelta(seconds=open_in_seconds_from_epoch_time_zero)
+        bucket_closes = bucket_opens + datetime.timedelta(seconds=300)
+        # if difference between head block time and when current bucket closes is enough, broadcast transactions
+        if (bucket_closes - node.get_head_block_time()).seconds >= time_needed_for_transactions + 10:
+            start_block = node.get_last_block_number()
+            tt.logger.info(f"Starting order creation at block: {start_block}")  # Usually starts at block #3
+            wallet.api.create_order("alice", 0, tt.Asset.Test(100), tt.Asset.Tbd(10), False, 3600)
+            wallet.api.create_order("initminer", 1, tt.Asset.Tbd(10), tt.Asset.Test(100), False, 3600)
 
-    wallet.api.create_order("alice", 0, tt.Asset.Test(100), tt.Asset.Tbd(10), False, 3600)
-    wallet.api.create_order("initminer", 1, tt.Asset.Tbd(10), tt.Asset.Test(100), False, 3600)
+            wallet.api.create_order("alice", 2, tt.Asset.Test(200), tt.Asset.Tbd(tbds), False, 3600)
+            wallet.api.create_order("initminer", 3, tt.Asset.Tbd(tbds), tt.Asset.Test(200), False, 3600)
 
-    wallet.api.create_order("alice", 2, tt.Asset.Test(200), tt.Asset.Tbd(tbds), False, 3600)
-    wallet.api.create_order("initminer", 3, tt.Asset.Tbd(tbds), tt.Asset.Test(200), False, 3600)
+            wallet.api.create_order("alice", 4, tt.Asset.Test(300), tt.Asset.Tbd(40), False, 3600)
+            wallet.api.create_order("initminer", 5, tt.Asset.Tbd(40), tt.Asset.Test(300), False, 3600)
 
-    wallet.api.create_order("alice", 4, tt.Asset.Test(300), tt.Asset.Tbd(40), False, 3600)
-    wallet.api.create_order("initminer", 5, tt.Asset.Tbd(40), tt.Asset.Test(300), False, 3600)
-
-    end_block = node.get_last_block_number()
-    tt.logger.info(f"Order creation finished at block: {end_block}")  # Usually ends at block #9
-
+            end_block = node.get_last_block_number()  # Usually ends at block #9
+            tt.logger.info(f"Order creation finished at block: {end_block}")
+            transactions_done = True
+        else:
+            tt.logger.info(
+                "There is no enough time left in current bucket to create transactions. Waiting for new "
+                "bucket to open..."
+            )
+            while node.get_head_block_time() < bucket_closes:
+                node.wait_number_of_blocks(1)
     buckets = node.api.market_history.get_market_history(
-        bucket_seconds=300, start=tt.Time.from_now(weeks=-1), end=tt.Time.from_now(weeks=1)
+        bucket_seconds=300,
+        start=tt.Time.from_now(weeks=-1),
+        end=tt.Time.from_now(weeks=1),
     )["buckets"]
 
     assert len(buckets) == 1
