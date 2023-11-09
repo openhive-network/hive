eb0467c16d454393fc40cbdbade0cef8dbc3c884
Lucius <lucius@syncad.com>
2023-11-09 20:53:47 +0100

    Removed switch_forks from block_write_i

--- libraries/chain/include/hive/chain/irreversible_block_writer.hpp
+++ libraries/chain/include/hive/chain/irreversible_block_writer.hpp
@@ -24,12 +24,6 @@ namespace hive { namespace chain {
 
     virtual void pop_block() override { FC_ASSERT( false, "Wrong writer bro" ); }
 
-    virtual void switch_forks( const block_id_type& new_head_block_id, uint32_t new_head_block_num,
-      uint32_t skip, const block_flow_control* pushed_block_ctrl,
-      const block_id_type original_head_block_id, const uint32_t original_head_block_number,
-      apply_block_t apply_block_extended, pop_block_t pop_block_extended ) override
-      { FC_ASSERT( false, "Wrong writer bro" ); }
-
     virtual std::optional<new_last_irreversible_block_t> find_new_last_irreversible_block(
       const std::vector<const witness_object*>& scheduled_witness_objects,
       const std::map<account_name_type, block_id_type>& last_fast_approved_block_by_witness,
