700c8b373f49d64e063d15d7c6de429ef0e0713c
Radek Masłowski <rmaslowski@syncad.com>
2023-11-24 14:45:49 +0100

    Extend Comment class with method get_author_reputation

--- tests/python/hive-local-tools/hive_local_tools/functional/python/operation/__init__.py
+++ tests/python/hive-local-tools/hive_local_tools/functional/python/operation/__init__.py
@@ -32,6 +32,7 @@ from schemas.virtual_operation import (
 
 if TYPE_CHECKING:
     from schemas.apis.account_history_api.response_schemas import EnumVirtualOps
+    from schemas.fields.hive_int import HiveInt
     from schemas.operations import AnyLegacyOperation
 
 
@@ -682,6 +683,13 @@ class Comment:
             assert vote_send is True, "Vote not send. Comment is deleted"
         else:
             raise ValueError(f"Unexpected value for 'mode': '{mode}'")
+
+    def get_author_reputation(self) -> HiveInt:
+        reputations = self.__node.api.reputation.get_account_reputations(account_lower_bound="", limit=1000).reputations
+        for account_reputation in reputations:
+            if account_reputation.account == self.__author.name:
+                return account_reputation.reputation
+        raise ValueError
 
 
 class Vote:
