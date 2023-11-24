0fa10a308148ac938a5764cf7009067d3cd998d6
mkrzeminski <mkrzeminski@syncad.com>
2023-11-24 12:14:51 +0100

    Add pow2_operation support

--- libraries/protocol/forward_impacted.cpp
+++ libraries/protocol/forward_impacted.cpp
@@ -1017,6 +1017,78 @@ private:
     collected_keyauths.emplace_back(collected_keyauth_t{op.worker_account, hive::app::key_t::MEMO, 0, true, /*key*/ op.work.worker, {}, 0});
   }
 
+
+  struct keyauth_pow2_visitor 
+  {
+      using result_type = account_name_type;
+
+      template <typename T>
+      account_name_type operator()(const T& work) const
+      {
+        return work.input.worker_account;
+      }
+
+  };
+
+  result_type operator()( const pow2_operation& op )
+  {
+
+    // []()
+    // {
+    //   static volatile bool stop_in = true;
+    //   wlog("operator()( const pow2_operation& op )");
+    //   wlog("pid= ${pid}", ("pid" , getpid() ));
+
+    //   while(stop_in)
+    //   {
+    //     int a = 0;
+    //     a=a;
+    //   }
+    // }();
+
+
+
+    //keyauth_pow2_visitor visitor;
+
+
+    account_name_type worker_account =     op.work.visit(keyauth_pow2_visitor());
+
+    if(worker_account == "dodl01")
+    {
+      int a = 0;
+      a=a;
+    }
+
+    if(worker_account == "dodl11")
+    {
+      int a = 0;
+      a=a;
+    }
+
+    
+    if(op.new_owner_key)
+    {
+      collected_keyauths.emplace_back(collected_keyauth_t{worker_account, hive::app::key_t::OWNER, 0, true, /*key*/ *op.new_owner_key, {}, 0});
+      collected_keyauths.emplace_back(collected_keyauth_t{worker_account, hive::app::key_t::ACTIVE, 0, true, /*key*/ *op.new_owner_key, {}, 0});
+      collected_keyauths.emplace_back(collected_keyauth_t{worker_account, hive::app::key_t::POSTING, 0, true, /*key*/ *op.new_owner_key, {}, 0});
+      collected_keyauths.emplace_back(collected_keyauth_t{worker_account, hive::app::key_t::MEMO, 0, true, /*key*/ *op.new_owner_key, {}, 0});
+    }
+
+    vector< authority > authorities;
+    op.get_required_authorities(authorities);
+    for(const auto& authority : authorities)
+    {
+      collect_one(op, authority, hive::app::key_t::WITNESS_SIGNING,  worker_account);
+    }
+
+
+    std::string stored_type_name =  op.work.get_stored_type_name( ) ;
+
+    int a =0;
+    a=a;
+
+  }
+
   result_type operator()( const account_create_operation& op )
   {
     collect_one(op, op.owner,   hive::app::key_t::OWNER,   op.new_account_name);
