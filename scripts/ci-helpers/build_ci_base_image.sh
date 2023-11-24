a1f5ab41d07473b36d899bb499d9490dc42b3069
Konrad Botor <kbotor@syncad.com>
2023-11-24 15:29:20 +0100

    Added artifact compression to .pytest_based jobs

--- scripts/ci-helpers/build_ci_base_image.sh
+++ scripts/ci-helpers/build_ci_base_image.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 REGISTRY=${1:-registry.gitlab.syncad.com/hive/hive/}
-CI_IMAGE_TAG=:ubuntu22.04-8
+CI_IMAGE_TAG=:ubuntu22.04-9
 
 docker buildx build --progress=plain --target=runtime \
-  --build-arg CI_REGISTRY_IMAGE=$REGISTRY --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
-  -t ${REGISTRY}runtime$CI_IMAGE_TAG -f Dockerfile .
+  --build-arg CI_REGISTRY_IMAGE="$REGISTRY" --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
+  --tag "${REGISTRY}runtime$CI_IMAGE_TAG" --file Dockerfile .
 
 
 docker buildx build --progress=plain --target=ci-base-image \
-  --build-arg CI_REGISTRY_IMAGE=$REGISTRY --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
-  -t ${REGISTRY}ci-base-image$CI_IMAGE_TAG -f Dockerfile .
+  --build-arg CI_REGISTRY_IMAGE="$REGISTRY" --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
+  --tag "${REGISTRY}ci-base-image$CI_IMAGE_TAG" --file Dockerfile .
