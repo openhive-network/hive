78176b1a9e4bb89212e20135d264ab1984335e47
Michał Kudela <kudmich@syncad.com>
2023-11-29 14:03:10 +0100

    Add beneficiaries options to each comment

--- tests/python/functional/comment_payment_tests/infinite_post_creator/infinite_post_creator.py
+++ tests/python/functional/comment_payment_tests/infinite_post_creator/infinite_post_creator.py
@@ -5,6 +5,7 @@ from random import randint
 
 import test_tools as tt
 from hive_local_tools.constants import ALTERNATE_CHAIN_JSON_FILENAME
+from hive_local_tools.functional.python.operation import create_transaction_with_any_operation
 
 
 def infinite_post_creator():
@@ -39,8 +40,34 @@ def infinite_post_creator():
         )
         tt.logger.info(f"Created: {post['operations'][0][1]}")
 
+        generate_comment_options_operation(wallet, permlink_number)
+        tt.logger.info(f"Created comment_options operation for comment with permlink-{permlink_number}")
+
         node.wait_number_of_blocks(randint(3, 10))
         permlink_number += 1
+
+
+def generate_comment_options_operation(wallet, permlink_num):
+    create_transaction_with_any_operation(
+        wallet,
+        "comment_options",
+        author="initminer",
+        permlink=f"permlink-{permlink_num}",
+        max_accepted_payout="1000000.000 TBD",
+        percent_hbd=50,
+        allow_votes=True,
+        allow_curation_rewards=True,
+        extensions=[
+            [
+                "comment_payout_beneficiaries",
+                {
+                    "beneficiaries": [
+                        {"account": "initminer", "weight": 10000},
+                    ]
+                },
+            ]
+        ],
+    )
 
 
 if __name__ == "__main__":
