132c6d827ad03bd063809cf13cd53a38e50b743d
Lucius <lucius@syncad.com>
2023-12-11 23:59:22 +0100

    Implemented pruned_block_writer::fetch_block_range

--- libraries/chain/include/hive/chain/pruned_block_writer.hpp
+++ libraries/chain/include/hive/chain/pruned_block_writer.hpp
@@ -68,6 +68,8 @@ namespace hive { namespace chain {
     void store_full_block( const std::shared_ptr<full_block_type> full_block );
     std::shared_ptr<full_block_type> retrieve_full_block( uint32_t recent_block_num ) const;
     const full_block_object* find_full_block( uint32_t recent_block_num ) const;
+    full_block_vector_t get_block_range( uint32_t starting_block_num, uint32_t count ) const;
+
 
   private:
     uint16_t _stored_block_number;
