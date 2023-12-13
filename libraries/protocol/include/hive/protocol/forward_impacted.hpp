3662a527b765890f075478372190d37c7fb724c5
mkrzeminski <mkrzeminski@syncad.com>
2023-12-13 12:28:27 +0100

    Keyauth: new files out of forward_impacted

--- libraries/protocol/include/hive/protocol/forward_impacted.hpp
+++ libraries/protocol/include/hive/protocol/forward_impacted.hpp
@@ -14,35 +14,7 @@ void operation_get_impacted_accounts(
   const hive::protocol::operation& op,
   fc::flat_set<protocol::account_name_type>& result );
 
-
-enum class key_t : std::int32_t
-{
-  OWNER,
-  ACTIVE,
-  POSTING,
-  MEMO,
-  WITNESS_SIGNING,
-};
-
-struct collected_keyauth_t
-{
-  std::string account_name;
-  key_t key_kind = key_t::OWNER;
-  uint32_t weight_threshold = 0;
-  bool keyauth_variant = true;
-  fc::ecc::public_key_data key_auth;
-  std::string account_auth;
-  hive::protocol::weight_type w = 0;
-
-};
-
 typedef std::set<std::string> stringset;
-typedef std::vector<collected_keyauth_t> collected_keyauth_collection_t;
-collected_keyauth_collection_t operation_get_keyauths(const hive::protocol::operation& op);
-collected_keyauth_collection_t operation_get_genesis_keyauths();
-collected_keyauth_collection_t operation_get_hf09_keyauths();
-stringset get_operations_used_in_get_keyauths();
-
 
 struct collected_metadata_t
 {
@@ -53,8 +25,6 @@ struct collected_metadata_t
 
 typedef std::vector<collected_metadata_t> collected_metadata_collection_t;
 collected_metadata_collection_t operation_get_metadata(const hive::protocol::operation& op);
-stringset get_operations_used_in_get_metadata();
-
 
 
 typedef std::vector<std::pair<protocol::account_name_type, protocol::asset>> impacted_balance_data;
@@ -67,5 +37,12 @@ void transaction_get_impacted_accounts(
   const hive::protocol::transaction& tx,
   fc::flat_set<protocol::account_name_type>& result
   );
+
+// Helper for metatdata and keyayuth collectors
+template <typename T>
+void exclude_from_used_operations(hive::app::stringset& used_operations)
+{
+  used_operations.erase(fc::get_typename<T>::name());
+}
 
 } } // hive::app
