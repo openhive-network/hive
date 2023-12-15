7678023fc4f266ffacb5e253494605103f43ec41
ABW <andrzejl@syncad.com>
2023-12-15 15:17:46 +0100

    fix compilation of SMT market maker code broken with one of recent changes to json parsing routines

--- libraries/chain/smt_objects/smt_market_maker.cpp
+++ libraries/chain/smt_objects/smt_market_maker.cpp
@@ -19,7 +19,7 @@ namespace hive { namespace chain {
 static std::vector< rational_u64 > load_mm_ticks()
 {
   std::string mm_ticks_json = hive::jsonball::get_market_maker_ticks();
-  fc::variant mm_ticks_var = fc::json::from_string( mm_ticks_json, fc::json::strict_parser );
+  fc::variant mm_ticks_var = fc::json::from_string( mm_ticks_json, fc::json::format_validation_mode::full, fc::json::strict_parser );
   std::vector< std::pair< uint64_t, uint64_t > > mm_ticks_pairs;
   fc::from_variant( mm_ticks_var, mm_ticks_pairs );
   std::vector< rational_u64 > mm_ticks;
