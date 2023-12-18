40d08a89651df19bcbd06b8fd43a33c4efc92dc5
Lucius <lucius@syncad.com>
2023-12-18 11:12:52 +0100

    Implemented remaining methods of pruned_block_writer

--- libraries/chain/include/hive/chain/pruned_block_writer.hpp
+++ libraries/chain/include/hive/chain/pruned_block_writer.hpp
@@ -20,13 +20,12 @@ namespace hive { namespace chain {
     virtual block_read_i& get_block_reader() override { return *this; };
     virtual void store_block( uint32_t current_irreversible_block_num,
                               uint32_t state_head_block_number ) override;
-    virtual void pop_block() override { FC_ASSERT(false, "Not implemented yet!"); }
+    virtual void pop_block() override;
     virtual std::optional<new_last_irreversible_block_t> find_new_last_irreversible_block(
       const std::vector<const witness_object*>& scheduled_witness_objects,
       const std::map<account_name_type, block_id_type>& last_fast_approved_block_by_witness,
       const unsigned witnesses_required_for_irreversiblity,
-      const uint32_t old_last_irreversible ) const override
-      { FC_ASSERT(false, "Not implemented yet!"); }
+      const uint32_t old_last_irreversible ) const override;
 
     // ### block_read_i overrides ###
     virtual uint32_t head_block_num( 
