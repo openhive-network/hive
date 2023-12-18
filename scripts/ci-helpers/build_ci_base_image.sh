78e30a64e1adb36198e07707a43c06415d1e9a74
Bartek Wrona <wrona@syncad.com>
2023-12-18 15:52:02 +0100

    Unified usage CI_BASE_IMAGE and BUILD_IMAGE_TAG to prevent syntax errors reported by latest docker version.

--- scripts/ci-helpers/build_ci_base_image.sh
+++ scripts/ci-helpers/build_ci_base_image.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
 REGISTRY=${1:-registry.gitlab.syncad.com/hive/hive/}
-CI_IMAGE_TAG=:ubuntu22.04-9
+CI_IMAGE_TAG=ubuntu22.04-9
 
 docker buildx build --progress=plain --target=runtime \
   --build-arg CI_REGISTRY_IMAGE="$REGISTRY" --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
-  --tag "${REGISTRY}runtime$CI_IMAGE_TAG" --file Dockerfile .
+  --build-arg BUILD_IMAGE_TAG=$CI_IMAGE_TAG \
+  --tag "${REGISTRY}runtime:$CI_IMAGE_TAG" --file Dockerfile .
 
 
 docker buildx build --progress=plain --target=ci-base-image \
   --build-arg CI_REGISTRY_IMAGE="$REGISTRY" --build-arg CI_IMAGE_TAG=$CI_IMAGE_TAG \
-  --tag "${REGISTRY}ci-base-image$CI_IMAGE_TAG" --file Dockerfile .
+  --build-arg BUILD_IMAGE_TAG=$CI_IMAGE_TAG \
+  --tag "${REGISTRY}ci-base-image:$CI_IMAGE_TAG" --file Dockerfile .
