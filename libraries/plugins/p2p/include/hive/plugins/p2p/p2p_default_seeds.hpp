3cbc89df56cc46cdc71f115fdb6f13d991c5c57d
Gandalf <gandalf@storm.pl>
2024-01-14 11:07:25 +0000

    Update initial public seed node list.

--- libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
@@ -10,21 +10,18 @@ const std::vector< std::string > default_seeds;
 const std::vector< std::string > default_seeds = {
   "seed.hive.blog:2001",                 // blocktrades
   "seed.openhive.network:2001",          // gtg
-  "seed.ecency.com:2001",                // good-karma
-  "rpc.ausbit.dev:2001",                 // ausbitbank
   "hive-seed.roelandp.nl:2001",          // roelandp
   "hive-seed.arcange.eu:2001",           // arcange
   "anyx.io:2001",                        // anyx
   "hived.splinterlands.com:2001",        // splinterlands
-  "seed.hive.blue:2001",                 // guiltyparties
   "hive-api.3speak.tv:2001",             // threespeak
   "node.mahdiyari.info:2001",            // mahdiyari
   "hive-seed.lukestokes.info:2001",      // lukestokes.mhth
   "seed.deathwing.me:2001",              // deathwing
-  "seed.liondani.com:2016",              // liondani
-  "hiveseed-se.privex.io:2001",          // privex
   "seed.mintrawa.com:2001",              // mintrawa
-  "hive-seed.actifit.io:2001"            // actifit
+  "hive-seed.actifit.io:2001",           // actifit
+  "seed.shmoogleosukami.co.uk:2001",     // shmoogleosukami
+  "hiveseed.rishipanthee.com:2001"       // rishi556
 };
 #endif
 
