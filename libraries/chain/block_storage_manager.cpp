98b8d2ac89706ea1737540c80784cfb469a178c2
Lucius <lucius@syncad.com>
2024-01-15 13:05:52 +0100

    Moved block storage manager to separate files

---
+++ libraries/chain/block_storage_manager.cpp
@@ -0,0 +1,140 @@
+#include <hive/chain/block_storage_manager.hpp>
+
+#include <hive/chain/irreversible_block_writer.hpp>
+#include <hive/chain/pruned_block_writer.hpp>
+
+namespace hive { namespace chain {
+
+class no_target_block_storage_t : public target_block_storage_i
+{
+public:
+  virtual ~no_target_block_storage_t() {}
+  virtual void store_irreversible_block( const std::shared_ptr<full_block_type>& full_block ) override {}
+};
+
+class pruned_target_block_storage_t : public target_block_storage_i
+{
+public:
+  pruned_target_block_storage_t( pruned_block_writer* writer ) : _writer( writer ) {}
+  virtual ~pruned_target_block_storage_t() {}
+  virtual void store_irreversible_block( const std::shared_ptr<full_block_type>& full_block ) override
+  {
+    _writer->store_full_block( full_block );
+  }
+
+private:
+  pruned_block_writer* _writer = nullptr;
+};
+
+block_storage_manager_t::block_storage_manager_t( database& db, appbase::application& app ) :
+  _app( app ),
+  _db( db ),
+  _default_block_writer( db, app )
+{}
+
+block_write_chain_i* block_storage_manager_t::get_block_writer()
+{
+  return _current_block_writer;
+}
+
+block_write_chain_i* block_storage_manager_t::init_storage(
+  const std::string& block_storage_type, 
+  const std::string& aux_block_storage_type )
+{
+  if( not block_storage_type.empty() )
+  {
+    if( block_storage_type == "BLOCK_LOG" )
+    {
+      _storage_type = block_storage_t::BLOCK_LOG;
+    }
+    else if( block_storage_type == "PRUNED" )
+    {
+      _storage_type = block_storage_t::PRUNED;
+    }
+    else
+      FC_THROW_EXCEPTION( fc::parse_error_exception, "Not supported block storage type" );
+  }
+
+  if( not aux_block_storage_type.empty() )
+  {
+    if( aux_block_storage_type == "BLOCK_LOG" )
+    {
+      _aux_storage_type = block_storage_t::BLOCK_LOG;
+    }
+    else
+      FC_THROW_EXCEPTION( fc::parse_error_exception, "Not supported auxiliary block storage type" );
+  }
+
+  switch( _storage_type )
+  {
+    case block_storage_t::BLOCK_LOG:
+      _current_block_writer = &_default_block_writer;
+      break;
+    case block_storage_t::PRUNED:
+      _current_block_writer = 
+        new pruned_block_writer( 1024, _db, _app, _default_block_writer.get_fork_db() );
+      break;
+    default: FC_THROW_EXCEPTION( fc::parse_error_exception, "Unsupported block storage value set" );
+      break;
+  }
+
+  return _current_block_writer;
+}
+
+std::shared_ptr< irreversible_block_writer > block_storage_manager_t::get_reindex_block_writer()
+{
+  if( _storage_type == block_storage_t::BLOCK_LOG ||
+      ( _aux_storage_type && *_aux_storage_type == block_storage_t::BLOCK_LOG ) )
+  {
+    return std::make_shared< irreversible_block_writer >( _default_block_writer.get_block_log() );
+  }
+
+  ilog("No auxiliary storage provided for replaying");
+  return std::shared_ptr< irreversible_block_writer >();
+}
+
+target_block_storage_i& block_storage_manager_t::get_target_block_storage()
+{
+  if( not _target_block_storage )
+  {
+    switch( _storage_type )
+    {
+      case block_storage_t::BLOCK_LOG:
+        _target_block_storage = std::make_unique< no_target_block_storage_t >();
+        break;
+      case block_storage_t::PRUNED:
+        {
+          pruned_block_writer* writer = dynamic_cast< pruned_block_writer* >( _current_block_writer );
+          FC_ASSERT( writer, "Current block writer is not pruned one though the flag says so!" );
+          _target_block_storage = std::make_unique< pruned_target_block_storage_t >( writer );
+        }
+        break;
+      default: FC_THROW_EXCEPTION( fc::parse_error_exception, "Unsupported block storage value set" );
+        break;
+    }
+  }
+  
+  return *_target_block_storage;
+}
+
+void block_storage_manager_t::open_storage( 
+  const hive::chain::open_args& db_open_args,
+  hive::chain::blockchain_worker_thread_pool& thread_pool )
+{
+  if( _storage_type == block_storage_t::BLOCK_LOG || 
+      ( _aux_storage_type && *_aux_storage_type == block_storage_t::BLOCK_LOG ) )
+  {
+    _default_block_writer.open( db_open_args.data_dir / "block_log",
+                                db_open_args.enable_block_log_compression,
+                                db_open_args.block_log_compression_level,
+                                db_open_args.enable_block_log_auto_fixing,
+                                thread_pool );
+  }
+}
+
+void block_storage_manager_t::close_storage()
+{
+  _default_block_writer.close();
+}
+
+} } //hive::chain
---
+++ libraries/chain/include/hive/chain/block_storage_manager.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <hive/chain/sync_block_writer.hpp>
+
+#include <appbase/application.hpp>
+
+namespace hive { namespace chain {
+
+//class block_log;
+class block_write_chain_i;
+class blockchain_worker_thread_pool;
+class database;
+//class fork_database;
+class full_block_type;
+class irreversible_block_writer;
+struct open_args;
+using appbase::application;
+
+class target_block_storage_i
+{
+public:
+  virtual ~target_block_storage_i() {}
+  virtual void store_irreversible_block( const std::shared_ptr<full_block_type>& full_block ) = 0;
+};
+
+class block_storage_manager_t final
+{
+public:
+  block_storage_manager_t( database& db, appbase::application& app );
+
+  block_write_chain_i* get_block_writer();
+
+  block_write_chain_i* init_storage( const std::string& block_storage_type, 
+                                     const std::string& aux_block_storage_type );
+
+  std::shared_ptr< irreversible_block_writer > get_reindex_block_writer();
+
+  target_block_storage_i& get_target_block_storage();
+
+  void open_storage( const hive::chain::open_args& db_open_args,
+             hive::chain::blockchain_worker_thread_pool& thread_pool );
+
+  void close_storage();
+
+private:
+  enum class block_storage_t
+  {
+    BLOCK_LOG, // single file (default)
+    PRUNED // a pool of recent blocks, kept in memory (state)
+  };
+  block_storage_t                           _storage_type = block_storage_t::BLOCK_LOG;
+  std::optional< block_storage_t >          _aux_storage_type;
+  appbase::application&                     _app;
+  database&                                 _db;
+  std::unique_ptr< target_block_storage_i > _target_block_storage;
+  sync_block_writer                         _default_block_writer;
+  block_write_chain_i*                      _current_block_writer = nullptr;
+};
+
+} }
