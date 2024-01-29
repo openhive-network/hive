14e34c2d67f903f586c40689911f201207aa1a50
Konrad Botor <kbotor@syncad.com>
2024-01-29 11:14:49 +0100

    Fix run_hived_img.sh to only call tput if it is available and to pass arbitrary Docker options properly

--- scripts/run_hived_img.sh
+++ scripts/run_hived_img.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1; pwd -P )"
 echo "$SCRIPTPATH"
 
-LOG_FILE=run_hived_img.log
+export LOG_FILE=run_hived_img.log
+# shellcheck source=common.sh
 source "$SCRIPTPATH/common.sh"
 
 log_exec_params "$@"
@@ -11,20 +12,22 @@ log_exec_params "$@"
 # This script starts a docker container for a hived image specified on the command line
 
 print_help () {
-    echo "Usage: $0 <hived_docker_img> [OPTION[=VALUE]]... [<hived_option>]..."
-    echo
-    echo "Starts a docker container for a hived docker image."
-    echo "OPTIONS:"
-    echo "  --webserver-http-endpoint=<endpoint>  Maps hived internal http endpoint to the specified external one. Endpoint can be a port number or ip-address:port."
-    echo "  --webserver-ws-endpoint=<endpoint>    Maps hived internal WS endpoint to the specified external one. Endpoint can be a port number or ip-address:port."
-    echo "  --p2p-endpoint=<endpoint>             Map hived internal P2P endpoint to the specified external one. Endpoint can be a port number or ip-address:port."
-    echo "  --data-dir=DIRECTORY_PATH             Specify path to a hived data directory on the host."
-    echo "  --shared-file-dir=DIRECTORY_PATH      Specify directory path to a shared_memory_file.bin on the host."
-    echo "  --name=CONTAINER_NAME                 Specify a dedicated name for the spawned container instance."
-    echo "  --detach                              Starts the container instance in detached mode. Note: you can detach later using Ctrl+p+q key binding."
-    echo "  --docker-option=OPTION                Specify a docker option to be passed to the underlying docker run command."
-    echo "  --help                                Display this help screen and exit"
-    echo
+cat <<EOF
+  Usage: $0 <hived_docker_img> [OPTION[=VALUE]]... [<hived_option>]...
+
+  Starts a docker container for a hived docker image.
+  OPTIONS:
+    --webserver-http-endpoint=<endpoint>  Maps hived internal http endpoint to the specified external one. Endpoint can be a port number or ip-address:port.
+    --webserver-ws-endpoint=<endpoint>    Maps hived internal WS endpoint to the specified external one. Endpoint can be a port number or ip-address:port.
+    --p2p-endpoint=<endpoint>             Map hived internal P2P endpoint to the specified external one. Endpoint can be a port number or ip-address:port.
+    --data-dir=DIRECTORY_PATH             Specify path to a hived data directory on the host.
+    --shared-file-dir=DIRECTORY_PATH      Specify directory path to a shared_memory_file.bin on the host.
+    --name=CONTAINER_NAME                 Specify a dedicated name for the spawned container instance.
+    --detach                              Starts the container instance in detached mode. Note: you can detach later using Ctrl+p+q key binding.
+    --docker-option=OPTION                Specify a docker option to be passed to the underlying docker run command.
+    --help,-h,-?                          Display this help screen and exit
+
+EOF
 }
 
 DOCKER_ARGS=()
@@ -41,14 +44,14 @@ P2_ENDPOINT="0.0.0.0:2001"
 
 add_docker_arg() {
   local arg="$1"
-#  echo "Processing docker argument: ${arg}"
+  # echo "Processing docker argument: ${arg}"
   
   DOCKER_ARGS+=("$arg")
 }
 
 add_hived_arg() {
   local arg="$1"
-#  echo "Processing hived argument: ${arg}"
+  # echo "Processing hived argument: ${arg}"
   
   HIVED_ARGS+=("$arg")
 }
@@ -81,12 +84,14 @@ while [ $# -gt 0 ]; do
 
     --data-dir=*)
         HIVED_DATADIR="${1#*=}"
-        add_docker_arg "-v ${HIVED_DATADIR}:/home/hived/datadir/"
+        add_docker_arg "--volume"
+        add_docker_arg "${HIVED_DATADIR}:/home/hived/datadir/"
         ;;
     --data-dir)
         shift
         HIVED_DATADIR="${1}"
-        add_docker_arg "-v ${HIVED_DATADIR}:/home/hived/datadir/"
+        add_docker_arg "--volume"
+        add_docker_arg "${HIVED_DATADIR}:/home/hived/datadir/"
         ;;
 
     --shared-file-dir=*)
@@ -112,16 +117,24 @@ while [ $# -gt 0 ]; do
       ;;
 
     --docker-option=*)
-        option="${1#*=}"
-        add_docker_arg "$option"
+        # Since some of the Docker options are technicaly two separate args (eg --env SOME_ENV=value)
+        # the proper way to pass them is to split them and pass the args one at a time
+        options_string="${1#*=}"
+        IFS=" " read -ra options <<< "$options_string"
+        for option in "${options[@]}"; do
+          add_docker_arg "$option"
+        done
         ;;
     --docker-option)
         shift
-        option="${1}"
-        add_docker_arg "$option"
-        ;; 
+        options_string="${1}"
+        IFS=" " read -ra options <<< "$options_string"
+        for option in "${options[@]}"; do
+          add_docker_arg "$option"
+        done
+        ;;
 
-    --help)
+    --help|-h|-?)
         print_help
         exit 0
         ;;
@@ -152,7 +165,7 @@ fi
 
 CMD_ARGS+=("${HIVED_ARGS[@]}")
 
-if [ ! -z "$HIVED_DATADIR" ]; then
+if [ -n "$HIVED_DATADIR" ]; then
 
   if [ -z "$HIVED_SHM_FILE_DIR" ]; then
     # if the datadir has been specified, but the shm directory mapping has not, store the shm file in the blockchain subdirectory of the datadir in order to save the shm file outside the container.
@@ -160,12 +173,14 @@ if [ ! -z "$HIVED_DATADIR" ]; then
   fi
 fi
 
-if [ ! -z "$HIVED_SHM_FILE_DIR" ]; then
+if [ -n "$HIVED_SHM_FILE_DIR" ]; then
   mkdir -p "${HIVED_SHM_FILE_DIR}"
 
   # do not pass this arg to the hived directly - it has already passed path: pointed by SHM_DIR variable inside docker_entrypoint.sh
-  add_docker_arg "-e SHM_DIR=/home/hived/shm_dir"
-  add_docker_arg "-v ${HIVED_SHM_FILE_DIR}:/home/hived/shm_dir"
+  add_docker_arg "--env"
+  add_docker_arg "SHM_DIR=/home/hived/shm_dir"
+  add_docker_arg "--volume"
+  add_docker_arg "${HIVED_SHM_FILE_DIR}:/home/hived/shm_dir"
 fi
 
 # Similar to hived, the dockerized version should also immediately start listening on the specified ports 
@@ -173,10 +188,23 @@ add_docker_arg "--publish=${HTTP_ENDPOINT}:8091"
 add_docker_arg "--publish=${WS_ENDPOINT}:8090"
 add_docker_arg "--publish=${P2_ENDPOINT}:2001"
 
-#echo "Using docker image: $IMAGE_NAME"
-#echo "Additional hived args: ${CMD_ARGS[@]}"
+# echo "Using docker image: $IMAGE_NAME"
+# echo "Additional hived args: ${CMD_ARGS[@]}"
+
+# If command 'tput' exists
+if command -v tput &> /dev/null
+then
+  add_docker_arg "--env"
+  add_docker_arg "COLUMNS=$(tput cols)"
+  add_docker_arg "--env"
+  add_docker_arg "LINES=$(tput lines)"
+fi
 
 docker container rm -f -v "$CONTAINER_NAME" 2>/dev/null || true
-docker run --rm -it -e HIVED_UID=$(id -u) -e COLUMNS=$(tput cols) -e LINES=$(tput lines) --name "$CONTAINER_NAME" --stop-timeout=180 ${DOCKER_ARGS[@]} "${IMAGE_NAME}" "${CMD_ARGS[@]}"
+
+# echo "DOCKER_ARGS: ${DOCKER_ARGS[*]}"
+
+# Scritps should use long options for better readability
+docker run --rm --interactive --tty --env HIVED_UID="$(id --user)" --name "$CONTAINER_NAME" --stop-timeout=180 "${DOCKER_ARGS[@]}" "${IMAGE_NAME}" "${CMD_ARGS[@]}"
 
 
