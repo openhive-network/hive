9473ef8cdf99b8152eb5df65119bfe19749f8d00
Krzysztof Leśniak <klesniak@syncad.com>
2024-02-13 10:56:14 +0100

    Extract hived_proxy_fixture

--- tests/unit/plugin_tests/snapshots_tests.cpp
+++ tests/unit/plugin_tests/snapshots_tests.cpp
@@ -5,35 +5,46 @@
 
 #include <hive/plugins/state_snapshot/state_snapshot_plugin.hpp>
 
+#include "../db_fixture/hived_proxy_fixture.hpp"
 #include "../db_fixture/snapshots_fixture.hpp"
 
 using namespace hive::chain;
 using namespace hive::protocol;
 
-BOOST_AUTO_TEST_SUITE( snapshots_tests )
+BOOST_FIXTURE_TEST_SUITE( snapshots_tests, hived_proxy_fixture )
 
 BOOST_AUTO_TEST_CASE( additional_allocation_after_snapshot_load )
 {
-  struct test_fixture : public snapshots_fixture {
-    test_fixture(bool remove_db_files) : snapshots_fixture(remove_db_files, "additional_allocation_after_snapshot_load", 24)
-    {}
+  try
+  {
+    BOOST_TEST_MESSAGE( "--- Testing: additional_allocation_after_snapshot_load" );
 
-    void load_snapshot() override
+    // remove any snapshots from last run
+    const fc::path temp_data_dir = hive::utilities::temp_directory_path();
+    fc::remove_all( ( temp_data_dir / "additional_allocation_after_snapshot_load" ) );
+
     {
-      snapshots_fixture::load_snapshot();
+      reset_fixture(true);
 
-      const auto& index = db->get_index<account_index>();
-      BOOST_REQUIRE_GT(index.get_item_additional_allocation(), 0);
-    }
+      postponed_init();
 
-    void create_objects() override
-    {
-      snapshots_fixture::create_objects();
-
-      set_price_feed( price( ASSET( "1.000 TBD" ), ASSET( "1.000 TESTS" ) ) );
+      generate_block();
+      db()->set_hardfork( 24 );
       generate_block();
 
-      const auto& index = db->get_index<account_index>();
+      // Fill up the rest of the required miners
+      for( int i = HIVE_NUM_INIT_MINERS; i < HIVE_MAX_WITNESSES; i++ )
+      {
+        account_create( HIVE_INIT_MINER_NAME + fc::to_string( i ), init_account_pub_key );
+        fund( HIVE_INIT_MINER_NAME + fc::to_string( i ), HIVE_MIN_PRODUCER_REWARD );
+        witness_create( HIVE_INIT_MINER_NAME + fc::to_string( i ), init_account_priv_key, "foo.bar", init_account_pub_key, HIVE_MIN_PRODUCER_REWARD.amount );
+      }
+      validate_database();
+      set_price_feed( price( ASSET( "1.000 TBD" ), ASSET( "1.000 TESTS" ) ) );
+
+      generate_block();
+
+      const auto& index = db()->get_index<account_index>();
       const size_t initial_allocations = index.get_item_additional_allocation();
 
       ACTOR_DEFAULT_FEE( alice )
@@ -47,27 +58,54 @@ BOOST_AUTO_TEST_CASE( additional_allocation_after_snapshot_load )
       BOOST_CHECK( compare_delayed_vote_count("alice", { static_cast<uint64_t>(get_vesting( "alice" ).amount.value) }) );
       BOOST_CHECK_EQUAL(index.get_item_additional_allocation(), initial_allocations + 2*sizeof(hive::chain::delayed_votes_data));
     }
-  };
-
-  try
-  {
-    BOOST_TEST_MESSAGE( "--- Testing: additional_allocation_after_snapshot_load" );
-
-    // remove any snapshots from last run
-    const fc::path temp_data_dir = hive::utilities::temp_directory_path();
-    fc::remove_all( ( temp_data_dir / "additional_allocation_after_snapshot_load" ) );
-
     {
-      test_fixture fixture(true);
-      fixture.create_objects();
+      reset_fixture(false);
+
+      hive::plugins::state_snapshot::state_snapshot_plugin* plugin = nullptr;
+      postponed_init(
+        {
+          hived_fixture::config_line_t( { "plugin",
+            { "state_snapshot" } }
+          ),
+          hived_fixture::config_line_t( { "dump-snapshot",
+            { std::string("snap") } }
+          ),
+          hived_fixture::config_line_t( { "snapshot-root-dir",
+            { std::string("additional_allocation_after_snapshot_load") } }
+          )
+        },
+        &plugin);
+
+        generate_block();
+        db()->set_hardfork( 24 );
+        generate_block();
     }
+
+
     {
-      test_fixture fixture(false);
-      fixture.dump_snapshot();
-    }
-    {
-      test_fixture fixture(false);
-      fixture.load_snapshot();
+      reset_fixture(false);
+
+      hive::plugins::state_snapshot::state_snapshot_plugin* snapshot = nullptr;
+      postponed_init(
+        {
+          hived_fixture::config_line_t( { "plugin",
+            { "state_snapshot" } }
+          ),
+          hived_fixture::config_line_t( { "load-snapshot",
+            { std::string("snap") } }
+          ),
+          hived_fixture::config_line_t( { "snapshot-root-dir",
+            { std::string("additional_allocation_after_snapshot_load") } }
+          )
+        },
+        &snapshot);
+
+      generate_block();
+      db()->set_hardfork( 24 );
+      generate_block();
+
+      const auto& index = db()->get_index<account_index>();
+      BOOST_REQUIRE_GT(index.get_item_additional_allocation(), 0);
     }
 
   }
