b09a9a7bb233e359ea52183fdf9dfca84b61b7b8
kmochocki <kmochocki@syncad.com>
2024-02-16 09:18:12 +0000

    Fix tests by changing exception type

--- tests/python/functional/hived/wallet_bridge_api_tests/test_api_requirement_assertion.py
+++ tests/python/functional/hived/wallet_bridge_api_tests/test_api_requirement_assertion.py
@@ -40,10 +40,10 @@ def test_no_api_is_missing(node: tt.InitNode, wallet_bridge_api_command: str) ->
 )
 @pytest.mark.enabled_plugins("witness", "wallet_bridge_api")
 def test_reporting_exception_when_database_api_is_missing(node: tt.InitNode, wallet_bridge_api_command: str) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+    with pytest.raises(tt.exceptions.RequestError) as exception:
         getattr(node.api.wallet_bridge, wallet_bridge_api_command)()
 
-    assert "Assert Exception:_database_api: database_api_plugin not enabled." not in str(exception.value)
+    assert "Assert Exception:_database_api: database_api_plugin not enabled." not in exception.value.error
 
 
 @pytest.mark.parametrize(
@@ -55,18 +55,18 @@ def test_reporting_exception_when_database_api_is_missing(node: tt.InitNode, wal
 )
 @pytest.mark.enabled_plugins("witness", "wallet_bridge_api")
 def test_reporting_exception_when_rc_api_is_missing(node: tt.InitNode, wallet_bridge_api_command: str) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+    with pytest.raises(tt.exceptions.RequestError) as exception:
         getattr(node.api.wallet_bridge, wallet_bridge_api_command)()
 
-    assert "Assert Exception:_rc_api: rc_api_plugin not enabled." not in str(exception.value)
+    assert "Assert Exception:_rc_api: rc_api_plugin not enabled." not in exception.value.error
 
 
 @pytest.mark.enabled_plugins("witness", "wallet_bridge_api")
 def test_reporting_exception_when_block_api_is_missing(node: tt.InitNode) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+    with pytest.raises(tt.exceptions.RequestError) as exception:
         node.api.wallet_bridge.get_block()
 
-    assert "Assert Exception:_block_api: block_api_plugin not enabled." not in str(exception.value)
+    assert "Assert Exception:_block_api: block_api_plugin not enabled." not in exception.value.error
 
 
 @pytest.mark.parametrize(
@@ -81,31 +81,31 @@ def test_reporting_exception_when_block_api_is_missing(node: tt.InitNode) -> Non
 def test_reporting_exception_when_account_history_api_is_missing(
     node: tt.InitNode, wallet_bridge_api_command: str
 ) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+    with pytest.raises(tt.exceptions.RequestError) as exception:
         getattr(node.api.wallet_bridge, wallet_bridge_api_command)()
 
-    assert "Assert Exception:_account_history_api: account_history_api_plugin not enabled." in str(exception.value)
+    assert "Assert Exception:_account_history_api: account_history_api_plugin not enabled." in exception.value.error
 
 
 @pytest.mark.enabled_plugins("witness", "wallet_bridge_api")
 def test_reporting_exception_when_account_by_key_api_is_missing(node: tt.InitNode) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+    with pytest.raises(tt.exceptions.RequestError) as exception:
         node.api.wallet_bridge.list_my_accounts()
 
-    assert "Assert Exception:_account_by_key_api: account_by_key_api_plugin not enabled." not in str(exception.value)
+    assert "Assert Exception:_account_by_key_api: account_by_key_api_plugin not enabled." not in exception.value.error
 
 
 @pytest.mark.enabled_plugins("witness", "wallet_bridge_api")
 def test_reporting_exception_when_market_history_api_is_missing(node: tt.InitNode) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+    with pytest.raises(tt.exceptions.RequestError) as exception:
         node.api.wallet_bridge.get_order_book()
 
-    assert "Assert Exception:_market_history_api: market_history_api_plugin not enabled." not in str(exception.value)
+    assert "Assert Exception:_market_history_api: market_history_api_plugin not enabled." not in exception.value.error
 
 
 @pytest.mark.enabled_plugins("witness")
 def test_reporting_exception_when_wallet_bridge_api_is_missing(node: tt.InitNode) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+    with pytest.raises(tt.exceptions.RequestError) as exception:
         node.api.wallet_bridge.get_version()
 
-    assert "Could not find API wallet_bridge_api" in str(exception.value)
+    assert "Could not find API wallet_bridge_api" in exception.value.error
--- tests/python/functional/operation_tests/convert_tests/conftest.py
+++ tests/python/functional/operation_tests/convert_tests/conftest.py
@@ -1,57 +1,55 @@
 from __future__ import annotations
 
-import json
 from datetime import datetime
 
 import pytest
 
 import test_tools as tt
-from hive_local_tools.constants import ALTERNATE_CHAIN_JSON_FILENAME
 from python.functional.operation_tests.conftest import ConvertAccount
 
 
 @pytest.fixture()
-def alice(create_node_and_wallet_for_convert_tests) -> ConvertAccount:
+def alice(create_node_and_wallet_for_convert_tests: tuple[tt.InitNode, tt.Wallet]) -> ConvertAccount:
     node, wallet = create_node_and_wallet_for_convert_tests
     wallet.create_account("alice", hives=200, hbds=200, vests=50)
     return ConvertAccount("alice", node, wallet)
 
 
 @pytest.fixture()
-def create_node_and_wallet_for_convert_tests() -> tuple(tt.InitNode, tt.Wallet):
+def init_witnesses() -> list[str]:
+    return [f"witness-{it}" for it in range(20)]
+
+
+@pytest.fixture()
+def alternate_chain_spec(init_witnesses: list[str]) -> tt.AlternateChainSpecs:
+    return tt.AlternateChainSpecs(
+        genesis_time=int(datetime.now().timestamp()),
+        hardfork_schedule=[tt.HardforkSchedule(hardfork=28, block_num=1)],
+        init_witnesses=init_witnesses,
+        init_supply=20_000_000_000,
+        hbd_init_supply=100_000_000,
+        initial_vesting=tt.InitialVesting(vests_per_hive=1800, hive_amount=10_000_000_000),
+    )
+
+
+@pytest.fixture()
+def create_node_and_wallet_for_convert_tests(
+    init_witnesses: list[str], alternate_chain_spec: tt.AlternateChainSpecs
+) -> tuple[tt.InitNode, tt.Wallet]:
     node = tt.InitNode()
-    init_witnesses = [f"witness-{it}" for it in range(20)]
-    set_initial_parameters_of_blockchain(init_witnesses)
     node.config.witness.extend(init_witnesses)
     node.config.plugin.append("account_history_api")
     node.run(
         time_offset="+0h x5",
-        arguments=[
-            "--alternate-chain-spec",
-            str(tt.context.get_current_directory() / ALTERNATE_CHAIN_JSON_FILENAME),
-        ],
+        alternate_chain_specs=alternate_chain_spec,
     )
     wallet = tt.Wallet(attach_to=node)
     wallet.api.update_witness(
-        "initminer", "http://url.html", tt.Account("initminer").public_key, {"account_creation_fee": tt.Asset.Test(3)}
+        "initminer",
+        "http://url.html",
+        tt.Account("initminer").public_key,
+        {"account_creation_fee": tt.Asset.Test(3)},
     )
 
     node.wait_number_of_blocks(43)
     return node, wallet
-
-
-def set_initial_parameters_of_blockchain(init_witnesses: list) -> None:
-    current_time = datetime.now()
-    alternate_chain_spec_content = {
-        "genesis_time": int(current_time.timestamp()),
-        "hardfork_schedule": [{"hardfork": 28, "block_num": 1}],
-        "init_witnesses": init_witnesses,
-        "init_supply": 20_000_000_000,
-        "hbd_init_supply": 100_000_000,
-        "initial_vesting": {"vests_per_hive": 1800, "hive_amount": 10_000_000_000},
-    }
-
-    directory = tt.context.get_current_directory()
-    directory.mkdir(parents=True, exist_ok=True)
-    with open(directory / ALTERNATE_CHAIN_JSON_FILENAME, "w") as json_file:
-        json.dump(alternate_chain_spec_content, json_file)
