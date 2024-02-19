c7f9d5cbb1970b05abef212d76843460a182e2ec
kmochocki <kmochocki@syncad.com>
2024-02-19 05:22:17 +0000

    Add fixture that returns beeekeeper instance

---
+++ programs/beekeeper/beekeeper_python/tests/conftest.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import shutil
+
+import pytest
+import test_tools as tt
+from test_tools.__private.scope.scope_fixtures import *  # noqa: F403
+
+from beekeepy import BeekeeperHandle
+
+
+@pytest.fixture()
+def beekeeper() -> BeekeeperHandle:
+    beekeeper_wdir = tt.context.get_current_directory() / "beekeeper"
+    if beekeeper_wdir.exists():
+        shutil.rmtree(beekeeper_wdir)
+    beekeeper_wdir.mkdir()
+    tt.logger.add(
+        beekeeper_wdir / "beekeeper_handle.log",
+        filter=lambda args: args["extra"].get("beekeeper_path") == beekeeper_wdir,
+    )
+    return BeekeeperHandle(working_directory=beekeeper_wdir, logger=tt.logger.bind(beekeeper_path=beekeeper_wdir))
