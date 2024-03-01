3889bb4872ca45008ee9ff3fe9820fd5802c8abc
Lucius <lucius@syncad.com>
2024-03-01 14:26:31 +0100

    Introduced block_log_reader_common abstract class

---
+++ libraries/chain/include/hive/chain/block_log_reader_common.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <hive/chain/block_read_interface.hpp>
+
+namespace hive { namespace chain {
+
+  class block_log;
+
+  /**
+   * Abstract class containing common part of all block-log-based implementations
+   * of block read interface
+   */
+  class block_log_reader_common : public block_read_i
+  {
+  protected:
+    /// To be implemented by subclasses.
+    virtual const block_log& get_head_block_log() const = 0;
+
+    /// To be implemented by subclasses.
+    virtual const block_log* get_block_log_corresponding_to( uint32_t block_num ) const = 0;
+
+    using block_range_t = std::vector<std::shared_ptr<full_block_type>>;
+    /// To be implemented by subclasses.
+    virtual block_range_t read_block_range_by_num( uint32_t starting_block_num, uint32_t count ) const = 0;
+
+  public:
+    virtual std::shared_ptr<full_block_type> head_block() const override;
+
+    virtual uint32_t head_block_num( 
+      fc::microseconds wait_for_microseconds = fc::microseconds() ) const override;
+    virtual block_id_type head_block_id( 
+      fc::microseconds wait_for_microseconds = fc::microseconds() ) const override;
+
+    virtual std::shared_ptr<full_block_type> read_block_by_num( uint32_t block_num ) const override;
+
+    //virtual void process_blocks( uint32_t starting_block_number, uint32_t ending_block_number,
+    //                             block_processor_t processor, hive::chain::blockchain_worker_thread_pool& thread_pool ) const override;
+
+    virtual std::shared_ptr<full_block_type> fetch_block_by_number( uint32_t block_num,
+      fc::microseconds wait_for_microseconds = fc::microseconds() ) const override;
+
+    virtual std::shared_ptr<full_block_type> fetch_block_by_id( 
+      const block_id_type& id ) const override; 
+
+    virtual bool is_known_block( const block_id_type& id ) const override;
+
+    virtual std::deque<block_id_type>::const_iterator find_first_item_not_in_blockchain(
+      const std::deque<block_id_type>& item_hashes_received ) const override;
+
+    virtual block_id_type find_block_id_for_num( uint32_t block_num ) const override;
+
+    virtual std::vector<std::shared_ptr<full_block_type>> fetch_block_range( 
+      const uint32_t starting_block_num, const uint32_t count, 
+      fc::microseconds wait_for_microseconds = fc::microseconds() ) const override;
+
+    virtual std::vector<block_id_type> get_blockchain_synopsis(
+      const block_id_type& reference_point, 
+      uint32_t number_of_blocks_after_reference_point ) const override;
+
+    virtual std::vector<block_id_type> get_block_ids(
+      const std::vector<block_id_type>& blockchain_synopsis,
+      uint32_t& remaining_item_count,
+      uint32_t limit) const override;
+  };
+
+} }
