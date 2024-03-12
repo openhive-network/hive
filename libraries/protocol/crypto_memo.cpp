4b28e8e97e4706bd56172033154ee57d5ab59273
“Mariusz Trela” <mtrela@syncad.com>
2024-03-12 08:12:02 +0100

    Add some methods into `crypto_memo` class in order to extend functionality

--- libraries/protocol/crypto_memo.cpp
+++ libraries/protocol/crypto_memo.cpp
@@ -2,26 +2,44 @@
 
 namespace hive { namespace protocol {
 
+crypto_memo::memo_content crypto_memo::build_from_encrypted_content( const crypto_data::public_key_type& from, const crypto_data::public_key_type& to, crypto_data::content&& content )
+{
+  memo_content _result( std::move( content ) );
+  _result.from = from;
+  _result.to = to;
+
+  return _result;
+}
+
+crypto_memo::memo_content crypto_memo::build_from_base58_content( const crypto_data::public_key_type& from, const crypto_data::public_key_type& to, const std::string& content )
+{
+  auto _c = from_string_impl<crypto_data::content>( content );
+  if( !_c )
+    FC_ASSERT( false, "Build from `base58` content failed");
+
+  return build_from_encrypted_content( from, to, std::move( _c.value() ) );
+}
+
+std::optional<crypto_memo::memo_content> crypto_memo::load_from_string( const std::string& data )
+{
+  FC_ASSERT( data.size() > 0 && data[0] == marker );
+  return from_string_impl<memo_content>( data.substr(1) );
+}
+
+std::string crypto_memo::dump_to_string( const memo_content& content )
+{
+  return marker + to_string_impl( content );
+}
+
 std::string crypto_memo::encrypt( const crypto_data::private_key_type& from, const crypto_data::public_key_type& to, const std::string& memo )
 {
   FC_ASSERT( memo.size() > 0 && memo[0] == marker );
-  content _c = encrypt_impl( from, to, memo.substr(1) );
-
-  memo_content _result;
-  _result.check = _c.check;
-  _result.nonce = _c.nonce;
-  _result.encrypted = std::move( _c.encrypted );
-  _result.from = from.get_public_key();
-  _result.to = to;
-
-  return marker + to_string_impl( _result );
+  return dump_to_string( build_from_encrypted_content( from.get_public_key(), to, encrypt_impl( from, to, memo.substr(1) ) ) );
 }
 
 std::string crypto_memo::decrypt( key_finder_type key_finder, const std::string& encrypted_memo )
 {
-  FC_ASSERT( encrypted_memo.size() > 0 && encrypted_memo[0] == marker );
-
-  auto _c = from_string_impl<memo_content>( encrypted_memo.substr(1) );
+  auto _c = load_from_string( encrypted_memo );
   if( !_c )
     return encrypted_memo;
 
--- libraries/protocol/include/hive/protocol/crypto_memo.hpp
+++ libraries/protocol/include/hive/protocol/crypto_memo.hpp
@@ -14,6 +14,9 @@ class crypto_memo: public crypto_data
     {
       public_key_type   from;
       public_key_type   to;
+
+      memo_content(){}
+      memo_content( content&& obj ): content( std::move( obj ) ){}
     };
 
   private:
@@ -25,6 +28,12 @@ class crypto_memo: public crypto_data
   public:
 
     ~crypto_memo() override = default;
+
+    memo_content build_from_encrypted_content( const crypto_data::public_key_type& from, const crypto_data::public_key_type& to, crypto_data::content&& content );
+    memo_content build_from_base58_content( const crypto_data::public_key_type& from, const crypto_data::public_key_type& to, const std::string& content );
+
+    std::optional<memo_content> load_from_string( const std::string& data );
+    std::string dump_to_string( const memo_content& content );
 
     /**
      * Encrypts given memo
