bf8c5279acf79995178ca341a148ef2e2f756566
Mateusz Kudela <mkudela@syncad.com>
2024-03-18 15:53:42 +0100

    Fix and extend decline voting rights related tests

--- tests/python/functional/hf28_tests/test_proxy_behavior_after_declining_voting_rights.py
+++ tests/python/functional/hf28_tests/test_proxy_behavior_after_declining_voting_rights.py
@@ -5,85 +5,139 @@ import pytest
 import test_tools as tt
 from hive_local_tools import run_for
 from hive_local_tools.constants import TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS
-from hive_local_tools.functional.python.hf28.constants import PROXY_ACCOUNT, VOTER_ACCOUNT
-from hive_local_tools.functional.python.operation import get_virtual_operations
-from schemas.operations.virtual import ProxyClearedOperation
+from hive_local_tools.functional.python.operation import Account, get_transaction_timestamp, get_virtual_operations
+from schemas.operations.virtual import DeclinedVotingRightsOperation, ProxyClearedOperation
 
 
 @run_for("testnet")
-def test_proxy_before_waiving_voting_rights(prepare_environment: tuple[tt.InitNode, tt.Wallet]) -> None:
+def test_proxy_before_waiving_voting_rights(
+    prepare_environment: tuple[tt.InitNode, tt.Wallet], voter: Account, proxy: Account
+) -> None:
     node, wallet = prepare_environment
+    transaction = wallet.api.set_voting_proxy(voter.name, proxy.name)
+    voter.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    voter.rc_manabar.update()
 
-    wallet.api.set_voting_proxy(VOTER_ACCOUNT, PROXY_ACCOUNT)
+    transaction = wallet.api.decline_voting_rights(voter.name, True)
+    voter.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    assert (voter := node.api.wallet_bridge.get_account(voter.name)) is not None
+    assert voter.proxy == proxy.name
 
-    wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)
-    assert (voter := node.api.wallet_bridge.get_account(VOTER_ACCOUNT)) is not None
-    assert voter.proxy == PROXY_ACCOUNT
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[voter.name])["requests"]) == 1
+
     node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
 
     assert len(get_virtual_operations(node, ProxyClearedOperation)) == 1
-    assert (voter := node.api.wallet_bridge.get_account(VOTER_ACCOUNT)) is not None
+    assert (voter := node.api.wallet_bridge.get_account(voter.name)) is not None
     assert voter.proxy == ""
 
+    assert node.api.database.find_accounts(accounts=[voter.name]).accounts[0].can_vote is False
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[voter.name])["requests"]) == 0
+    assert len(get_virtual_operations(node, DeclinedVotingRightsOperation)) == 1
+
     node.restart(time_control=tt.OffsetTimeControl(offset="+25h"))
-    assert (proxy := node.api.wallet_bridge.get_account(PROXY_ACCOUNT)) is not None
+    assert (proxy := node.api.wallet_bridge.get_account(proxy.name)) is not None
     assert not any(proxy.proxied_vsf_votes)
 
 
 @run_for("testnet")
-def test_set_proxy_after_waiving_voting_rights(prepare_environment: tuple[tt.InitNode, tt.Wallet]) -> None:
+def test_set_proxy_after_waiving_voting_rights(
+    prepare_environment: tuple[tt.InitNode, tt.Wallet], voter: Account, proxy: Account
+) -> None:
     node, wallet = prepare_environment
 
-    wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)
+    transaction = wallet.api.decline_voting_rights(voter.name, True)
+    voter.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[voter.name])["requests"]) == 1
+
     node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
 
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
-        wallet.api.set_voting_proxy(VOTER_ACCOUNT, PROXY_ACCOUNT)
+    assert node.api.database.find_accounts(accounts=[voter.name]).accounts[0].can_vote is False
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[voter.name])["requests"]) == 0
+    assert len(get_virtual_operations(node, DeclinedVotingRightsOperation)) == 1
 
-    assert "Account has declined the ability to vote and cannot proxy votes." in exception.value.error
+    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+        wallet.api.set_voting_proxy(voter.name, proxy.name)
+
+    assert (
+        "Account has declined the ability to vote and cannot proxy votes." in exception.value.error
+    ), "Error message other than expected."
 
 
 @run_for("testnet")
 def test_set_proxy_when_decline_voting_rights_is_in_progress(
-    prepare_environment: tuple[tt.InitNode, tt.Wallet]
+    prepare_environment: tuple[tt.InitNode, tt.Wallet], voter: Account, proxy: Account
 ) -> None:
     node, wallet = prepare_environment
 
-    head_block_number = wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)["block_num"]
-    node.wait_for_block_with_number(head_block_number + (TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS // 2))
-    wallet.api.set_voting_proxy(VOTER_ACCOUNT, PROXY_ACCOUNT)
-    node.wait_for_block_with_number(head_block_number + TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
+    transaction = wallet.api.decline_voting_rights(voter.name, True)
+    voter.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    voter.rc_manabar.update()
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[voter.name])["requests"]) == 1
+
+    node.wait_for_block_with_number(transaction["block_num"] + (TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS // 2))
+
+    transaction = wallet.api.set_voting_proxy(voter.name, proxy.name)
+    voter.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    node.wait_for_block_with_number(transaction["block_num"] + TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
+
+    assert node.api.database.find_accounts(accounts=[voter.name]).accounts[0].can_vote is False
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[voter.name])["requests"]) == 0
+    assert len(get_virtual_operations(node, DeclinedVotingRightsOperation)) == 1
 
     assert len(get_virtual_operations(node, ProxyClearedOperation)) == 1
     node.restart(time_control=tt.OffsetTimeControl(offset="+25h"))
 
-    assert (voter := node.api.wallet_bridge.get_account(VOTER_ACCOUNT)) is not None
+    assert (voter := node.api.wallet_bridge.get_account(voter.name)) is not None
     assert voter.proxy == ""
 
-    assert (proxy := node.api.wallet_bridge.get_account(PROXY_ACCOUNT)) is not None
+    assert (proxy := node.api.wallet_bridge.get_account(proxy.name)) is not None
     assert not any(proxy.proxied_vsf_votes)
 
 
 @run_for("testnet")
 def test_proxied_vsf_votes_when_principal_account_declined_its_voting_rights(
-    prepare_environment: tuple[tt.InitNode, tt.Wallet]
+    prepare_environment: tuple[tt.InitNode, tt.Wallet], voter: Account, proxy: Account
 ) -> None:
     node, wallet = prepare_environment
 
-    wallet.api.set_voting_proxy(VOTER_ACCOUNT, PROXY_ACCOUNT)
-
+    transaction = wallet.api.set_voting_proxy(voter.name, proxy.name)
+    voter.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    voter.rc_manabar.update()
     node.wait_for_irreversible_block()
     node.restart(time_control=tt.OffsetTimeControl(offset="+25h", speed_up_rate=5))
 
-    assert (proxy := node.api.wallet_bridge.get_account(PROXY_ACCOUNT)) is not None
+    assert (proxy := node.api.wallet_bridge.get_account(proxy.name)) is not None
     assert int(proxy.proxied_vsf_votes[0]) > 0
 
-    wallet.api.decline_voting_rights(VOTER_ACCOUNT, True)
+    transaction = wallet.api.decline_voting_rights(voter.name, True)
+    voter.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[voter.name])["requests"]) == 1
+
     node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
 
-    assert (voter := node.api.wallet_bridge.get_account(VOTER_ACCOUNT)) is not None
+    assert (voter := node.api.wallet_bridge.get_account(voter.name)) is not None
     assert voter.proxy == ""
     assert len(get_virtual_operations(node, ProxyClearedOperation)) == 1
 
-    assert (proxy := node.api.wallet_bridge.get_account(PROXY_ACCOUNT)) is not None
+    assert (proxy := node.api.wallet_bridge.get_account(proxy.name)) is not None
     assert not any(proxy.proxied_vsf_votes)
+
+    assert node.api.database.find_accounts(accounts=[voter.name]).accounts[0].can_vote is False
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=[voter.name])["requests"]) == 0
+    assert len(get_virtual_operations(node, DeclinedVotingRightsOperation)) == 1
--- tests/python/functional/hf28_tests/test_vote_for_comment_using_delegate_vesting_shares.py
+++ tests/python/functional/hf28_tests/test_vote_for_comment_using_delegate_vesting_shares.py
@@ -4,9 +4,11 @@ import test_tools as tt
 from hive_local_tools import run_for
 from hive_local_tools.constants import TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS
 from hive_local_tools.functional.python.hf28 import post_comment
+from hive_local_tools.functional.python.operation import Account, get_transaction_timestamp, get_virtual_operations
+from schemas.operations.virtual import DeclinedVotingRightsOperation
 
 
-@run_for("testnet")
+@run_for("testnet", enable_plugins=["account_history_api"])
 def test_delegate_vesting_shares_without_voting_rights(node: tt.InitNode) -> None:
     """
     Problem description: https://gitlab.syncad.com/hive/hive/-/issues/463
@@ -17,33 +19,59 @@ def test_delegate_vesting_shares_without_voting_rights(node: tt.InitNode) -> Non
     node.set_vest_price(tt.Asset.Vest(1800))
 
     wallet.create_account("alice", vests=100_000)
+    alice = Account("alice", node, wallet)
 
     wallet.create_account("bob")
 
-    wallet.api.decline_voting_rights("alice", True)
+    transaction = wallet.api.decline_voting_rights("alice", True)
+    alice.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=["alice"])["requests"]) == 1
+
     node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
+
+    assert node.api.database.find_accounts(accounts=["alice"]).accounts[0].can_vote is False
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=["alice"])["requests"]) == 0
+    assert len(get_virtual_operations(node, DeclinedVotingRightsOperation)) == 1
 
     wallet.api.delegate_vesting_shares("alice", "bob", tt.Asset.Vest(5))
 
     assert node.api.wallet_bridge.get_accounts(["bob"])[0].post_voting_power == tt.Asset.Vest(5)
 
 
-@run_for("testnet")
+@run_for("testnet", enable_plugins=["account_history_api"])
 def test_vote_for_comment_with_vests_from_delegation_before_decline_voting_rights(node: tt.InitNode) -> None:
     node.restart(time_control=tt.SpeedUpRateTimeControl(speed_up_rate=5))
     wallet = tt.Wallet(attach_to=node)
-
     node.set_vest_price(tt.Asset.Vest(1800))
 
     wallet.create_account("alice", vests=100_000)
+    alice = Account("alice", node, wallet)
     wallet.create_account("bob")
+    bob = Account("bob", node, wallet)
     post_comment(wallet, number_of_comments=2)
 
     wallet.api.delegate_vesting_shares("alice", "bob", tt.Asset.Vest(10_000))
-    wallet.api.vote("bob", "creator-0", "comment-of-creator-0", 100)
+    bob.rc_manabar.update()
+    transaction = wallet.api.vote("bob", "creator-0", "comment-of-creator-0", 100)
+    bob.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
 
-    wallet.api.decline_voting_rights("alice", True)
+    alice.rc_manabar.update()
+    transaction = wallet.api.decline_voting_rights("alice", True)
+    alice.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    assert node.api.database.find_comments(comments=[["creator-0", "comment-of-creator-0"]]).comments[0].net_votes == 1
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=["alice"])["requests"]) == 1
     node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
+
+    assert node.api.database.find_accounts(accounts=["alice"]).accounts[0].can_vote is False
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=["alice"])["requests"]) == 0
+    assert len(get_virtual_operations(node, DeclinedVotingRightsOperation)) == 1
+    assert node.api.database.find_comments(comments=[["creator-0", "comment-of-creator-0"]]).comments[0].net_votes == 1
     node.wait_for_irreversible_block()
 
     node.restart(time_control=tt.OffsetTimeControl(offset="+62m"))
@@ -52,7 +80,7 @@ def test_vote_for_comment_with_vests_from_delegation_before_decline_voting_right
     assert bob.reward_vesting_hive > tt.Asset.Test(0)
 
 
-@run_for("testnet")
+@run_for("testnet", enable_plugins=["account_history_api"])
 def test_vote_for_comment_with_vests_from_delegation_when_decline_voting_rights_is_being_executed(
     node: tt.InitNode,
 ) -> None:
@@ -62,14 +90,31 @@ def test_vote_for_comment_with_vests_from_delegation_when_decline_voting_rights_
     node.set_vest_price(tt.Asset.Vest(1800))
 
     wallet.create_account("alice", vests=100_000)
+    alice = Account("alice", node, wallet)
+
     wallet.create_account("bob")
+    bob = Account("bob", node, wallet)
     post_comment(wallet, number_of_comments=2)
 
-    wallet.api.decline_voting_rights("alice", True)
+    transaction = wallet.api.decline_voting_rights("alice", True)
+    alice.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=["alice"])["requests"]) == 1
 
     wallet.api.delegate_vesting_shares("alice", "bob", tt.Asset.Vest(10_000))
-    wallet.api.vote("bob", "creator-0", "comment-of-creator-0", 100)
+    bob.rc_manabar.update()
+    transaction = wallet.api.vote("bob", "creator-0", "comment-of-creator-0", 100)
+    bob.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+
     node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
+
+    assert node.api.database.find_accounts(accounts=["alice"]).accounts[0].can_vote is False
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=["alice"])["requests"]) == 0
+    assert len(get_virtual_operations(node, DeclinedVotingRightsOperation)) == 1
 
     node.restart(time_control=tt.OffsetTimeControl(offset="+62m"))
     bob = node.api.wallet_bridge.get_accounts(["bob"])[0]
@@ -77,9 +122,9 @@ def test_vote_for_comment_with_vests_from_delegation_when_decline_voting_rights_
     assert bob.reward_vesting_hive > tt.Asset.Test(0)
 
 
-@run_for("testnet")
+@run_for("testnet", enable_plugins=["account_history_api"])
 def test_vote_for_comment_with_vests_from_delegation_after_creating_a_decline_voting_rights_request(
-    node: tt.InitNode | tt.RemoteNode,
+    node: tt.InitNode,
 ) -> None:
     node.restart(time_control=tt.SpeedUpRateTimeControl(speed_up_rate=5))
     wallet = tt.Wallet(attach_to=node)
@@ -87,16 +132,26 @@ def test_vote_for_comment_with_vests_from_delegation_after_creating_a_decline_vo
     node.set_vest_price(tt.Asset.Vest(1800))
 
     wallet.create_account("alice", vests=100_000)
+    alice = Account("alice", node, wallet)
     wallet.create_account("bob")
     post_comment(wallet, number_of_comments=2)
 
     wallet.api.delegate_vesting_shares("alice", "bob", tt.Asset.Vest(10_000))
-
-    wallet.api.decline_voting_rights("alice", True)
+    alice.rc_manabar.update()
+    transaction = wallet.api.decline_voting_rights("alice", True)
+    alice.rc_manabar.assert_rc_current_mana_is_reduced(
+        transaction["rc_cost"], get_transaction_timestamp(node, transaction)
+    )
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=["alice"])["requests"]) == 1
 
     wallet.api.vote("bob", "creator-0", "comment-of-creator-0", 100)
+    assert node.api.database.find_comments(comments=[["creator-0", "comment-of-creator-0"]]).comments[0].net_votes == 1
     node.wait_number_of_blocks(TIME_REQUIRED_TO_DECLINE_VOTING_RIGHTS)
 
+    assert node.api.database.find_accounts(accounts=["alice"]).accounts[0].can_vote is False
+    assert len(node.api.database.find_decline_voting_rights_requests(accounts=["alice"])["requests"]) == 0
+    assert len(get_virtual_operations(node, DeclinedVotingRightsOperation)) == 1
+    assert node.api.database.find_comments(comments=[["creator-0", "comment-of-creator-0"]]).comments[0].net_votes == 1
     node.restart(time_control=tt.OffsetTimeControl(offset="+62m"))
     bob = node.api.wallet_bridge.get_accounts(["bob"])[0]
     assert bob.reward_vesting_balance > tt.Asset.Vest(0)
