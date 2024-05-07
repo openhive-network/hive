f024aad4e157a210306d7fae12686af401b84b45
Radek Masłowski <rmaslowski@syncad.com>
2024-05-07 16:45:39 +0200

    Refactor datagen_tests

--- tests/python/functional/datagen_tests/recurrent_transfer_tests/test_one_execution_of_the_recurrent_transfer.py
+++ tests/python/functional/datagen_tests/recurrent_transfer_tests/test_one_execution_of_the_recurrent_transfer.py
@@ -13,14 +13,19 @@ def test_one_execution_of_the_recurrent_transfer(node: tt.InitNode) -> None:
     wallet.create_account("receiver")
     wallet.create_account("sender", hives=tt.Asset.Test(100), vests=tt.Asset.Test(100))
 
-    with pytest.raises(tt.exceptions.CommunicationError) as exception:
+    with pytest.raises(tt.exceptions.WaxRequestError) as exception:
         wallet.api.recurrent_transfer(
-            "sender", "receiver", tt.Asset.Test(0.001), "recurrent transfer to receiver", recurrence=24, executions=1
+            "sender",
+            "receiver",
+            tt.Asset.Test(0.001),
+            "recurrent transfer to receiver",
+            recurrence=24,
+            executions=1,
         )
 
     expected_error_message = (
-        "Executions must be at least 2, if you set executions to 1 "
-        "the recurrent transfer will execute immediately and delete itself."
+        "Executions must be at least 2, if you set executions to 1 the recurrent transfer will execute immediately and"
+        " delete itself. You should use a normal transfer operation"
     )
 
-    assert expected_error_message in str(exception.value)
+    assert expected_error_message in exception.value.error
