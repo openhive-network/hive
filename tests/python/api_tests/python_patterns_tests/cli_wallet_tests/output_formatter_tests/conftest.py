9c259ebea720273e182b7dbd661b5fa48acda902
Radek Masłowski <rmaslowski@syncad.com>
2024-05-15 14:14:54 +0200

    Refactor python_patterns api tests

--- tests/python/api_tests/python_patterns_tests/cli_wallet_tests/output_formatter_tests/conftest.py
+++ tests/python/api_tests/python_patterns_tests/cli_wallet_tests/output_formatter_tests/conftest.py
@@ -20,14 +20,14 @@ def node(request):
 
 
 @pytest.fixture()
-def wallet_with_text_formatter(node) -> tt.Wallet:
-    return tt.Wallet(
+def wallet_with_text_formatter(node) -> tt.OldWallet:
+    return tt.OldWallet(
         attach_to=node, additional_arguments=["--output-formatter=text", "--transaction-serialization=legacy"]
     )
 
 
 @pytest.fixture()
-def wallet_with_json_formatter(node) -> tt.Wallet:
-    return tt.Wallet(
+def wallet_with_json_formatter(node) -> tt.OldWallet:
+    return tt.OldWallet(
         attach_to=node, additional_arguments=["--output-formatter=json", "--transaction-serialization=legacy"]
     )
