a8bb67b69d32126686959149465a82ed392eb750
Radek Masłowski <rmaslowski@syncad.com>
2024-05-17 15:42:03 +0200

    Add new part of tests- beekeeper_wallet_tests

---
+++ tests/python/functional/beekeeper_wallet_tests/test_account_history.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from hive_local_tools import run_for
+
+if TYPE_CHECKING:
+    import test_tools as tt
+
+
+@run_for("testnet", enable_plugins=["account_history_api"])
+def test_getting_empty_history(wallet: tt.Wallet) -> None:
+    assert len(wallet.api.get_account_history("initminer", -1, 1)) == 0
