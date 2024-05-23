04e72ccb91a723aef5885a7844d709cb9e5af85c
Marcin Sobczyk <msobczyk@syncad.com>
2024-05-23 09:32:34 +0200

    add write permission to datadir copy inside service container

--- scripts/copy_datadir.sh
+++ scripts/copy_datadir.sh
@@ -10,6 +10,7 @@ then
         echo "Creating copy of ${DATA_SOURCE}/datadir inside ${DATADIR}"
         sudo -Enu hived mkdir -p "${DATADIR}"
         flock "${DATA_SOURCE}/datadir" sudo -En cp -pr "${DATA_SOURCE}/datadir"/*  "${DATADIR}"
+        sudo -En chmod --recursive a+rw "${DATADIR}"
         if [ "$(realpath "${DATA_SOURCE}/shm_dir")" != "$(realpath "${SHM_DIR}")" ]
         then
             echo "Creating copy of ${DATA_SOURCE}/shm_dir inside ${SHM_DIR}"
