2e47efb55c51f5072c469c13f47cae8ee7042e5a
“Mariusz Trela” <mtrela@syncad.com>
2024-05-28 11:15:33 +0200

    Extend parameters of `AugSchemeMPL().AggregateVerify`

--- programs/util/bls_m_of_n_scheme.hpp
+++ programs/util/bls_m_of_n_scheme.hpp
@@ -73,12 +73,63 @@ struct bls_m_of_n_scheme: public scheme
       _final_public_key += public_keys[i];
     }
 
-    vector<vector<uint8_t>> _content;
+    //===============================================
+    vector<G1Element> _verify_pub_keys;
+    vector<vector<uint8_t>> _verify_message;
 
-    //for( size_t i = 0; i < _public_keys.size(); ++i )
-    _content.push_back( content );
 
-    bool _result = AugSchemeMPL().AggregateVerify( { _final_public_key }, _content, _final_signature );
+    /*
+        e(G, S) =
+        e(G, S1+S2+…+S1000) =
+        e(G, S1)⋅e(G, S2)⋅…⋅e(G, S1000) =
+        e(G, pk1×H(m1))⋅…⋅e(G, pk1000×H(m1000)) =
+        e(pk1×G, H(m1))⋅…⋅e(pk1000×G, H(m1000)) =
+        e(P1, H(m1))⋅e(P2, H(m2))⋅…⋅e(P1000, H(m1000))
+    */
+
+    /*
+      e(G, S’) =
+      e(G, S1+S3)=
+      e(G, pk1×H(P, m)+pk3×H(P, m)+MK1+MK3) =
+      e(G, pk1×H(P, m)+pk3×H(P, m))⋅e(G, MK1+MK3)=
+      e(pk1×G+pk3×G, H(P, m))⋅e(P, H(P, 1)+H(P, 3))=
+      e(P’, H(P, m))⋅e(P, H(P, 1)+H(P, 3))
+    */
+
+    auto _merge_pk_message = []( const G1Element& _public_key, const std::vector<uint8_t>& _message )
+    {
+      bls::Bytes message = bls::Bytes( _message );
+      std::vector<uint8_t> augMessage = _public_key.Serialize();
+
+      augMessage.reserve(augMessage.size() + message.size());
+      augMessage.insert(augMessage.end(), message.begin(), message.end());
+
+      return augMessage;
+    };
+
+    auto _merge_pk_message_2 = [&]( const G1Element& _public_key, const uint8_t& _number )
+    {
+      return _merge_pk_message( _public_key, { _number } );
+    };
+
+    //e(P’, H(P, m))
+    _verify_pub_keys.push_back( _final_public_key );
+    _verify_message.push_back( _merge_pk_message( _aggregated_public_key, content ) );
+
+    //e(P, H(P, 1)+H(P, 3))
+    _verify_pub_keys.push_back( _aggregated_public_key );
+    vector<uint8_t> _sum_messages;
+    for( size_t i = 0; i < nr_signers; ++i )
+    {
+      std::vector<uint8_t> _tmp = _merge_pk_message_2( _aggregated_public_key, i );
+      std::copy( _tmp.begin(), _tmp.end(), std::back_inserter( _sum_messages ) );
+    }
+    _verify_message.push_back( _sum_messages );
+
+    assert( _verify_pub_keys.size() == _verify_message.size() );
+    assert( _verify_pub_keys.size() == 2 );
+
+    bool _result = AugSchemeMPL().AggregateVerify( _verify_pub_keys, _verify_message, _final_signature );
     std::cout<<"n of m: "<<_result<<std::endl;
   }
 
