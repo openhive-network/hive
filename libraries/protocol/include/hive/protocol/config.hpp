4913627cf74cf0dd9d65476ab101042ba5a4239d
Gandalf <gandalf@storm.pl>
2024-05-30 12:26:10 +0000

    Hive Blockchain version bumped to 1.27.6

--- libraries/protocol/include/hive/protocol/config.hpp
+++ libraries/protocol/include/hive/protocol/config.hpp
@@ -138,7 +138,7 @@ using namespace hive::protocol::testnet_blockchain_configuration;
   #define HIVE_CHAIN_ID                         fc::sha256("4200000000000000000000000000000000000000000000000000000000000000")
 #else
   /// Hive mainnet
-  #define HIVE_BLOCKCHAIN_VERSION               ( version(1, 27, 5) )
+  #define HIVE_BLOCKCHAIN_VERSION               ( version(1, 27, 6) )
   #define OLD_CHAIN_ID                          fc::sha256()
   #define HIVE_CHAIN_ID                         fc::sha256("beeab0de00000000000000000000000000000000000000000000000000000000")
 #endif
