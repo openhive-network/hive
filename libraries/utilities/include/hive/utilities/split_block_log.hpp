9569c966ee735fad2da154c3b8e7c05a8a56664f
Lucius <lucius@syncad.com>
2024-06-04 11:35:33 +0200

    Improved hived block-log-auto-splitting handling

--- libraries/utilities/include/hive/utilities/split_block_log.hpp
+++ libraries/utilities/include/hive/utilities/split_block_log.hpp
@@ -12,9 +12,11 @@ namespace hive { namespace utilities {
  * @brief Tries splitting provided legacy block log file into parts.
  * 
  * @param monolith_path to log file. The split parts go into its directory.
+ * @param head_part_number highest number of part file to be generated, Deduct from source file if 0.
+ * @param part_count how many part files shall be generated. All if 0.
  * @throws lots of FC_ASSERTs.
  */
-void split_block_log( fc::path monolith_path, appbase::application& app,
-                      hive::chain::blockchain_worker_thread_pool& thread_pool );
+void split_block_log( fc::path monolith_path, uint32_t head_part_number, size_t part_count,
+  appbase::application& app, hive::chain::blockchain_worker_thread_pool& thread_pool );
 
 } } // hive::utilities
