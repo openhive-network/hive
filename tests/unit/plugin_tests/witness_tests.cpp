0e7d3b526fab5ea3b12f7714f8268410c45d067b
ABW <andrzejl@syncad.com>
2024-06-21 15:10:01 +0200

    add witness unit test to check situation when long block processing blocks refresh of block production data for very long time
    The test emulates similar python test where we have block processing that causes some blocks to be missed.

--- tests/unit/plugin_tests/witness_tests.cpp
+++ tests/unit/plugin_tests/witness_tests.cpp
@@ -1052,5 +1052,131 @@ BOOST_AUTO_TEST_CASE( block_lock_test )
   FC_LOG_AND_RETHROW()
 }
 
+BOOST_AUTO_TEST_CASE( block_lag_test )
+{
+  using namespace hive::plugins::witness;
+  // ABW: same as block_lock_test, but this test emulates situation where processing of block
+  // previous to the one witness wanted to produce takes a lot of time (we can't achieve that
+  // actually, since we can only slow down marker transaction, however it makes no difference,
+  // since witness can't ask for state data while the block is being processed and it does
+  // not matter if it can't ask before or after block number changes)
+
+  try
+  {
+    initialize( -HIVE_MAX_WITNESSES * 2 * HIVE_BLOCK_INTERVAL,
+      { "wit1", "wit2", "wit3", "wit4", "wit5", "wit6", "wit7", "wit8", "wit9", "wita",
+      "witb", "witc", "witd", "wite", "witf", "witg", "with", "witi", "witj", "witk" },
+      { "with" }
+    ); // representing just one witness
+    bool test_passed = false;
+    fc::logger::get( "user" ).set_log_level( fc::log_level::info ); // suppress fast confirm broadcast messages
+
+    // produce first two schedules of blocks (initminer) so we can get to actual test
+    // note that genesis time was set in the past so we don't have to wait
+    schedule_blocks( HIVE_MAX_WITNESSES * 2 );
+    BOOST_REQUIRE( db->has_hardfork( HIVE_NUM_HARDFORKS ) );
+    db->_log_hardforks = true;
+
+    fc::thread api_thread;
+    api_thread.async( [&]()
+    {
+      BOOST_SCOPE_EXIT( this_ ) { this_->theApp.generate_interrupt_request(); } BOOST_SCOPE_EXIT_END
+      try
+      {
+        ilog( "Starting test thread" );
+        uint32_t block_num = HIVE_MAX_WITNESSES * 2;
+        BOOST_REQUIRE_EQUAL( block_num, db->head_block_num() );
+        fc::time_point_sec next_block_time = get_genesis_time() + ( block_num + 1 ) * HIVE_BLOCK_INTERVAL;
+        fc::sleep_until( next_block_time );
+
+        bool slow_block_next = db->get_scheduled_witness( 2 ) == "with";
+        // there is a chance that out 'with' witness is scheduled to produce first block of
+        // third schedule; in such case slow block should be the last one, because calls to
+        // get_scheduled_witness "wrap" active schedule, and we are doing it under assumption
+        // of missed block (witness is to have that assumption)
+        for( int i = 1; i <= HIVE_MAX_WITNESSES; ++i )
+        {
+          const auto* block_header = &get_block_reader().head_block()->get_block_header();
+          if( block_num == block_header->block_num() )
+          {
+            if( slow_block_next )
+            {
+              // we should already have pending marker transaction;
+              // shortly after we start producing slow block, witness should notice the delay in
+              // block N and try to prepare to take over in case it reaches next timestamp with
+              // block N still missing; it won't be able to acquire lock for a long time, so
+              // its turn will pass while it was waiting; it should notice that in round next
+              // to the one when it detected lag for the first time; there should be 3 missed
+              // blocks - the one that 'with' were to produce and two next
+              ilog( "Producing and reapplying slow block" );
+              // in order to be able to see which block (timestamp) is when blocks will start
+              // to be produced again, we are setting witness plugin to represent all witnesses
+              // from this point
+              get_witness_plugin().set_witnesses( { "initminer", "wit1", "wit2", "wit3", "wit4",
+                "wit5", "wit6", "wit7", "wit8", "wit9", "wita", "witb", "witc", "witd", "wite",
+                "witf", "witg", "with", "witi", "witj", "witk" } );
+            }
+            else
+            {
+              ilog( "Supplementing block with debug plugin" );
+            }
+            schedule_block();
+            block_header = &get_block_reader().head_block()->get_block_header();
+          }
+          block_num = block_header->block_num();
+          ilog( "Block #${n}, ts: ${t}, witness: ${w}", ( "n", block_num )
+            ( "t", block_header->timestamp )( "w", block_header->witness ) );
+          if( slow_block_next )
+            break;
+          else
+            slow_block_next = db->get_scheduled_witness( 2 ) == "with";
+          next_block_time += HIVE_BLOCK_INTERVAL;
+          if( slow_block_next )
+          {
+            ilog( "Adding marker transaction to pending" );
+            // generate marker transaction that takes 10s to process during block reapplication;
+            // it will be waiting as pending for next block to pick it up
+            db_plugin->debug_update( [&]( database& db )
+            {
+              if( db.get_tx_status() == database::TX_STATUS_P2P_BLOCK )
+              {
+                ilog( "Slow processing start" );
+                fc::usleep( fc::seconds( 10 ) );
+                ilog( "Slow processing end" );
+              }
+            } );
+          }
+          fc::sleep_until( next_block_time );
+        }
+        ilog( "Performing test loop end checks" );
+        BOOST_REQUIRE( slow_block_next );
+        // block_num and next_block_time is a number and ts of slow block
+        {
+          const auto& block_header = get_block_reader().head_block()->get_block_header();
+          BOOST_REQUIRE_NE( block_header.witness, "with" );
+        }
+        fc::usleep( fc::seconds( HIVE_BLOCK_INTERVAL ) );
+        // we should now have a first block after slow one, which should be 4 intervals further
+        // (3 missing blocks)
+        {
+          const auto& block_header = get_block_reader().head_block()->get_block_header();
+          BOOST_REQUIRE_EQUAL( block_header.block_num(), block_num + 1 );
+          BOOST_REQUIRE( block_header.timestamp == next_block_time + 4 * HIVE_BLOCK_INTERVAL );
+        }
+
+        ilog( "'API' thread finished" );
+        test_passed = true;
+      }
+      CATCH( "API" )
+    } );
+
+    theApp.wait4interrupt_request();
+    theApp.quit( true );
+    BOOST_REQUIRE( test_passed );
+    ilog( "Test done" );
+  }
+  FC_LOG_AND_RETHROW()
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 #endif
