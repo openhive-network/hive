c88a675ce10305a691c6315fb25b968664107ceb
Bartek Wrona <wrona@syncad.com>
2024-06-26 19:15:47 +0200

    generate_api_doc script adjusted to procedure generating documentation into project Wiki

--- scripts/generate_api_docs.sh
+++ scripts/generate_api_docs.sh
@@ -2,37 +2,38 @@
 
 set -e
 
-UNSTAGED_FILES=($(git diff --name-only))
-STAGED_FILES=($(git diff --name-only --cached))
-
-if   [[ " ${UNSTAGED_FILES[*]} " =~ " programs/beekeeper/beekeeper_wasm/src/interfaces.ts " ]] \
-  || [[ " ${STAGED_FILES[*]} "   =~ " programs/beekeeper/beekeeper_wasm/src/interfaces.ts " ]];
-then
-echo "Beekeeper interfaces.ts file changes detected. Generating documentation"
-
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PROJECT_DIR="${SCRIPTPATH}/../programs/beekeeper/beekeeper_wasm"
 
-# When using TypeScript 4.4.4, we are restricted to a specific typedoc and typedoc-plugin-markdown versions
+REPO_URL="${1:?Missing repository url}"
+REVISION_INFO="${2:?Missing argument pointing git revision}"
+
+DOCUMENTATION_URL=${3}
+
+OUTPUT_DIR=${4:-dist/docs}
+
+INPUT_FILE=src/web.ts
+
+# When using TypeScript, we are restricted to a specific typedoc and typedoc-plugin-markdown versions
 # https://typedoc.org/guides/installation/#requirements
 pushd "${PROJECT_DIR}"
-mkdir -vp build
-pnpm exec typedoc --plugin typedoc-plugin-markdown --theme markdown --excludeInternal --hideBreadcrumbs --hideInPageTOC --out build/docs src/web.ts
-mv build/docs/modules.md build/docs/_modules.md
-rm build/docs/README.md
-pnpm exec concat-md --decrease-title-levels build/docs > api.md
+
+mkdir -vp "${OUTPUT_DIR}"
+
+pnpm exec typedoc --includeVersion \
+  --sourceLinkTemplate "${REPO_URL}/-/blob/{gitRevision}/{path}#L{line}" \
+  --gitRevision "${REVISION_INFO}" \
+  --readme README.md \
+  --plugin typedoc-plugin-markdown \
+  --plugin typedoc-gitlab-wiki-theme \
+  --tsconfig tsconfig.json \
+  --out "${OUTPUT_DIR}" \
+  "${INPUT_FILE}"
+
+  if [[ -n "${DOCUMENTATION_URL}" ]]; then
+    echo "Attempting to replace generated documentation url placeholder: ${DOCUMENTATION_URL}"
+    sed -i "s<\${GEN_DOC_URL}<${DOCUMENTATION_URL}<g" README.md
+  fi
+
 popd
 
-UNSTAGED_FILES=($(git diff --name-only))
-
-if [[ " ${UNSTAGED_FILES[*]} " =~ " programs/beekeeper/beekeeper_wasm/api.md " ]];
-then
-  echo "api.md file changes detected. Stage interface documentation changes before commit"
-  exit 1
-fi
-
-echo "No unstaged changes in the documentation"
-
-else
-echo "No Beekeeper interfaces.ts changes detected. Omitting generating the documentation"
-fi
