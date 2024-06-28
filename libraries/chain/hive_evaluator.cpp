2314bde8715ea4ef4abd61a6c61f3b9c8f2e50fd
“Mariusz Trela” <mtrela@syncad.com>
2024-06-28 07:37:17 +0200

    `account_object` constructor has to get also old creation time, because rc algorithms are based on it

--- libraries/chain/hive_evaluator.cpp
+++ libraries/chain/hive_evaluator.cpp
@@ -264,7 +264,7 @@ void verify_authority_accounts_exist(
 }
 
 const account_object& create_account( database& db, const account_name_type& name, const public_key_type& key,
-  const time_point_sec& time, bool mined, asset fee_for_rc_adjustment, const account_object* recovery_account = nullptr,
+  const time_point_sec& _head_block_creation_time, const time_point_sec& time, bool mined, asset fee_for_rc_adjustment, const account_object* recovery_account = nullptr,
   asset initial_delegation = asset( 0, VESTS_SYMBOL ) )
 {
   if( db.has_hardfork( HIVE_HARDFORK_0_11 ) )
@@ -285,13 +285,15 @@ const account_object& create_account( database& db, const account_name_type& nam
     rc_adjustment_from_fee = ( fee_for_rc_adjustment * dgpo.get_vesting_share_price() ).amount.value;
   }
 
-  return db.create< account_object >( name, key, time, mined, recovery_account,
+  return db.create< account_object >( name, key, _head_block_creation_time, time, mined, recovery_account,
     !db.has_hardfork( HIVE_HARDFORK_0_20__2539 ) /*voting mana 100%*/, initial_delegation, rc_adjustment_from_fee );
 }
 
 void account_create_evaluator::do_apply( const account_create_operation& o )
 {
   const auto& creator = _db.get_account( o.creator );
+
+  const auto& props = _db.get_dynamic_global_properties();
 
   const witness_schedule_object& wso = _db.get_witness_schedule_object();
 
@@ -342,7 +344,7 @@ void account_create_evaluator::do_apply( const account_create_operation& o )
     _db.adjust_balance( _db.get< account_object, by_name >( HIVE_NULL_ACCOUNT ), o.fee );
   }
 
-  const auto& new_account = create_account( _db, o.new_account_name, o.memo_key, _db.get_current_timestamp(),
+  const auto& new_account = create_account( _db, o.new_account_name, o.memo_key, props.time, _db.get_current_timestamp(),
     false /*mined*/, o.fee, &creator );
 
 #ifdef COLLECT_ACCOUNT_METADATA
@@ -448,7 +450,7 @@ void account_create_with_delegation_evaluator::do_apply( const account_create_wi
     _db.adjust_balance( _db.get< account_object, by_name >( HIVE_NULL_ACCOUNT ), o.fee );
   }
 
-  const auto& new_account = create_account( _db, o.new_account_name, o.memo_key, _db.get_current_timestamp(),
+  const auto& new_account = create_account( _db, o.new_account_name, o.memo_key, props.time, _db.get_current_timestamp(),
     false /*mined*/, o.fee, &creator, o.delegation );
 
 #ifdef COLLECT_ACCOUNT_METADATA
@@ -2136,7 +2138,7 @@ void pow_apply( database& db, Operation o )
   auto itr = accounts_by_name.find(o.get_worker_account());
   if(itr == accounts_by_name.end())
   {
-    const auto& new_account = create_account( db, o.get_worker_account(), o.work.worker, db.get_current_timestamp(),
+    const auto& new_account = create_account( db, o.get_worker_account(), o.work.worker, dgp.time, db.get_current_timestamp(),
       true /*mined*/, asset( 0, HIVE_SYMBOL ) );
     // ^ empty recovery account parameter means highest voted witness at time of recovery
 
@@ -2275,7 +2277,7 @@ void pow2_evaluator::do_apply( const pow2_operation& o )
   if(itr == accounts_by_name.end())
   {
     FC_ASSERT( o.new_owner_key.valid(), "New owner key is not valid." );
-    const auto& new_account = create_account( db, worker_account, *o.new_owner_key, _db.get_current_timestamp(),
+    const auto& new_account = create_account( db, worker_account, *o.new_owner_key, dgp.time, _db.get_current_timestamp(),
       true /*mined*/, asset( 0, HIVE_SYMBOL ) );
     // ^ empty recovery account parameter means highest voted witness at time of recovery
 
@@ -2525,6 +2527,7 @@ void create_claimed_account_evaluator::do_apply( const create_claimed_account_op
   FC_ASSERT( _db.has_hardfork( HIVE_HARDFORK_0_20__1771 ), "create_claimed_account_operation is not enabled until hardfork 20." );
 
   const auto& creator = _db.get_account( o.creator );
+  const auto& props = _db.get_dynamic_global_properties();
 
   FC_ASSERT( creator.pending_claimed_accounts > 0, "${creator} has no claimed accounts to create", ( "creator", o.creator ) );
 
@@ -2537,7 +2540,7 @@ void create_claimed_account_evaluator::do_apply( const create_claimed_account_op
     a.pending_claimed_accounts--;
   });
 
-  const auto& new_account = create_account( _db, o.new_account_name, o.memo_key, _db.get_current_timestamp(),
+  const auto& new_account = create_account( _db, o.new_account_name, o.memo_key, props.time, _db.get_current_timestamp(),
     false /*mined*/, _db.get_witness_schedule_object().median_props.account_creation_fee, &creator );
 
 #ifdef COLLECT_ACCOUNT_METADATA
