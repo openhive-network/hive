4619e70f9dec3ac2523592ae87ae3b4de5d79675
Michał Kudela <kudmich@syncad.com>
2024-07-01 12:04:48 +0200

    [tmp] conftest -> change path

--- tests/python/functional/colony/conftest.py
+++ tests/python/functional/colony/conftest.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import tempfile
 from pathlib import Path
 
 import pytest
@@ -8,17 +9,14 @@ import test_tools as tt
 
 
 @pytest.fixture()
-def block_log():
-    protected_block_log_destination = Path(
-        "/home/dev/workspace_ssh/block_log_sources/tests_block_logs/block_log_open_sign/"
+def alternate_chain_spec(block_log):
+    return tt.AlternateChainSpecs.parse_file(
+        Path("/home/dev/workspace_ssh/tests_block_logs_clive/block_log_single_sign/") / tt.AlternateChainSpecs.FILENAME
     )
-    protected_block_log = tt.BlockLog(path=protected_block_log_destination / "block_log")
-    return protected_block_log.copy_to(Path(__file__).parent)
 
 
 @pytest.fixture()
-def alternate_chain_spec(block_log):
-    return tt.AlternateChainSpecs.parse_file(
-        Path("/home/dev/workspace_ssh/block_log_sources/tests_block_logs/block_log_open_sign/")
-        / tt.AlternateChainSpecs.FILENAME
-    )
+def block_log():
+    protected_block_log_destination = Path(__file__).parent / "block_log"
+    protected_block_log = tt.BlockLog(path=protected_block_log_destination / "block_log")
+    return protected_block_log.copy_to(Path(tempfile.gettempdir()))
