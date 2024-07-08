70a92bc09f9c76afe224d7f2ef509a7512d75220
kmochocki <kmochocki@syncad.com>
2024-07-08 15:49:44 +0000

    Update settings

--- programs/beekeeper/beekeeper_python/beekeepy/settings.py
+++ programs/beekeeper/beekeeper_python/beekeepy/settings.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from datetime import timedelta
 from pathlib import Path
 
 from pydantic import Field
@@ -9,9 +10,22 @@ from helpy import Settings as HandleSettings
 
 
 class Settings(HandleSettings):
-    """Defines parameters for handles how to start and behave."""
+    """Defines parameters for beekeeper how to start and behave."""
 
     working_directory: Path = Field(default_factory=lambda: Path.cwd())
+    """Path, where beekeeper binary will store all it's data and logs."""
+
     http_endpoint: HttpUrl | None = None  # type: ignore[assignment]
+    """Endpoint on which python will comunicate with beekeeper, required for remote beekeeper."""
+
     notification_endpoint: HttpUrl | None = None
+    """Endpoint to use for reverse communication between beekeeper and python."""
+
     binary_path: Path | None = None
+    """Alternative path to beekeeper binary."""
+
+    propagate_sigint: bool = True
+    """If set to True (default), sigint will be sent to beekeeper without controll of this library."""
+
+    close_timeout: timedelta = Field(default_factory=lambda: timedelta(seconds=10.0))
+    """This timeout varriable affects time handle waits before beekeepy closes."""
