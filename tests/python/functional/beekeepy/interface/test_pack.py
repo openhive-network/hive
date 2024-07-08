56c51a52a99c8a29b19e1fab3fc514723b7c0f19
kmochocki <kmochocki@syncad.com>
2024-07-08 16:16:27 +0000

    Add missing comments in relation to AAA

--- tests/python/functional/beekeepy/interface/test_pack.py
+++ tests/python/functional/beekeepy/interface/test_pack.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 from beekeepy import PackedBeekeeper, beekeeper_factory
 
 if TYPE_CHECKING:
-    from python.functional.beekeepy.settings_factory import SettingsFactory
+    from hive_local_tools.beekeeper.models import SettingsFactory
 
 
 def import_new_key(packed: PackedBeekeeper, wallet: str, password: str) -> None:
@@ -15,12 +15,17 @@ def import_new_key(packed: PackedBeekeeper, wallet: str, password: str) -> None:
 
 
 def test_packing(settings: SettingsFactory) -> None:
+    # ARRANGE
     name, password = "wallet", "password"
     with beekeeper_factory(settings=settings()) as bk, bk.create_session() as ss, ss.create_wallet(
         name=name, password=password
     ) as wlt:
         assert len(wlt.public_keys) == 0, "Unexpected public keys in wallet"
-        with ProcessPoolExecutor() as exec:
-            future = exec.submit(import_new_key, bk.pack(), name, password)
+        with ProcessPoolExecutor() as executor:
+            # ACT
+            packed_beekeeper = bk.pack()
+            future = executor.submit(import_new_key, packed_beekeeper, name, password)
             exception = future.exception()
+
+            # ASSERT
             assert exception is None, str(exception)
