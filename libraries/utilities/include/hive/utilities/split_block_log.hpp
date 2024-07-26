c277ee51a690e6a0e90ea53599aba74e5e303e39
asuch <asuch@syncad.com>
2024-07-26 17:59:13 +0200

    Update comment in split_block_log

--- libraries/utilities/include/hive/utilities/split_block_log.hpp
+++ libraries/utilities/include/hive/utilities/split_block_log.hpp
@@ -14,6 +14,7 @@ namespace hive { namespace utilities {
  * @param monolith_path to log file. The split parts go into its directory.
  * @param head_part_number highest number of part file to be generated, Deduct from source file if 0.
  * @param part_count how many part files shall be generated. All if 0.
+ * @param splitted_block_log_destination_dir directory where new style multiple block_log files will be stored. By default it's the same place where monolith block_log is.
  * @throws lots of FC_ASSERTs.
  */
 void split_block_log( fc::path monolith_path, uint32_t head_part_number, size_t part_count,
