f0b91cbc26869c0a3e2763a3a1b72be9e49ff047
“Mariusz Trela” <mtrela@syncad.com>
2024-08-01 09:22:48 +0200

    Status of a lock should be considered on upper level

--- tests/unit/tests/beekeeper_tests.cpp
+++ tests/unit/tests/beekeeper_tests.cpp
@@ -58,7 +58,7 @@ BOOST_AUTO_TEST_CASE(wallet_test)
   wallet.set_wallet_name( _wallet_file_name );
   BOOST_REQUIRE_EQUAL( _wallet_file_name , wallet.get_wallet_name() );
 
-  BOOST_REQUIRE_EQUAL(0u, wallet.list_keys().size());
+  BOOST_REQUIRE_EQUAL(0u, wallet.get_keys_details().size());
 
   auto _key_generation = []( size_t nr_keys )
   {
@@ -77,7 +77,7 @@ BOOST_AUTO_TEST_CASE(wallet_test)
 
   auto _prefix = "STM";
   wallet.import_key( _keys_a[0].first, _prefix);
-  BOOST_REQUIRE_EQUAL(1u, wallet.list_keys().size());
+  BOOST_REQUIRE_EQUAL(1u, wallet.get_keys_details().size());
 
   auto privCopy = wallet.get_private_key( _keys_a[0].second );
   BOOST_REQUIRE_EQUAL(_keys_a[0].first, privCopy.key_to_wif());
@@ -85,7 +85,7 @@ BOOST_AUTO_TEST_CASE(wallet_test)
   wallet.lock();
   BOOST_REQUIRE(wallet.is_locked());
   wallet.unlock("pass");
-  BOOST_REQUIRE_EQUAL(1u, wallet.list_keys().size());
+  BOOST_REQUIRE_EQUAL(1u, wallet.get_keys_details().size());
   wallet.save_wallet_file( _wallet_file_name );
   BOOST_REQUIRE( fc::exists( _wallet_file_name ) );
 
@@ -96,7 +96,7 @@ BOOST_AUTO_TEST_CASE(wallet_test)
   BOOST_REQUIRE(wallet2.is_locked());
 
   wallet2.unlock("pass");
-  BOOST_REQUIRE_EQUAL(1u, wallet2.list_keys().size());
+  BOOST_REQUIRE_EQUAL(1u, wallet2.get_keys_details().size());
 
   auto privCopy2 = wallet2.get_private_key( _keys_a[0].second );
   BOOST_REQUIRE_EQUAL(_keys_a[0].first, privCopy2.key_to_wif());
@@ -108,7 +108,7 @@ BOOST_AUTO_TEST_CASE(wallet_test)
 
   wallet.import_keys( _keys, _prefix );
 
-  BOOST_REQUIRE_EQUAL(5u, wallet.list_keys().size());
+  BOOST_REQUIRE_EQUAL(5u, wallet.get_keys_details().size());
   for( auto& keys : _keys_b )
   {
     auto _private_key = wallet.get_private_key( keys.second );
