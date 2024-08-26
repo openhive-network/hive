cd42b09c7c355df2885d25059c5ab6ea0f24b3f2
Radek Masłowski <rmaslowski@syncad.com>
2024-08-26 09:03:12 +0200

    Run potentialy failing tests 1000 times

--- tests/python/functional/api_tests/market_history_api_tests/test_get_market_history.py
+++ tests/python/functional/api_tests/market_history_api_tests/test_get_market_history.py
@@ -8,101 +8,11 @@ import test_tools as tt
 from hive_local_tools import run_for
 
 
-@pytest.mark.skip(reason="https://gitlab.syncad.com/hive/hive/-/issues/449")
-@run_for("testnet")
-def test_get_market_history_with_start_date_after_end(node: tt.InitNode) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError):
-        node.api.market_history.get_market_history(
-            bucket_seconds=60, start=tt.Time.from_now(weeks=10), end=tt.Time.from_now(weeks=-1)
-        )
-
-
-@run_for("testnet")
-def test_exceed_time_range(node: tt.InitNode) -> None:
-    with pytest.raises(tt.exceptions.CommunicationError):
-        node.api.market_history.get_market_history(
-            bucket_seconds=60, start=tt.Time.from_now(years=-100), end=tt.Time.from_now(years=100)
-        )
-
-
-@pytest.mark.parametrize("bucket_seconds", [1, 14, 32, 59, 61, 299, 301, 3599, 86405])
-@pytest.mark.skip(reason="https://gitlab.syncad.com/hive/hive/-/issues/450")
-@run_for("testnet")
-def test_get_market_history_with_wrong_bucket_seconds_value(node: tt.InitNode | tt.RemoteNode, bucket_seconds: int):
-    with pytest.raises(tt.exceptions.CommunicationError):
-        node.api.market_history.get_market_history(
-            bucket_seconds=bucket_seconds, start=tt.Time.from_now(weeks=-1), end=tt.Time.from_now(weeks=1)
-        )
-
-
-@run_for("testnet", enable_plugins=["market_history_api"])
-def test_create_better_offer_and_cancel_it(node: tt.InitNode) -> None:
-    wallet = tt.Wallet(attach_to=node)
-    wallet.create_account("alice", hives=tt.Asset.Test(1000), vests=tt.Asset.Test(100))
-    wallet.create_account("bob", hives=tt.Asset.Test(1000), vests=tt.Asset.Test(100))
-
-    # initial order - hives to hbd
-    wallet.api.create_order("alice", 0, tt.Asset.Test(300), tt.Asset.Tbd(30), False, 3600)
-    # create order with lower price
-    wallet.api.create_order("bob", 1, tt.Asset.Test(301), tt.Asset.Tbd(20), False, 3600)
-    wallet.api.cancel_order("bob", 1)
-    # opposite order - sell hbd for hives
-    wallet.api.create_order("initminer", 2, tt.Asset.Tbd(100), tt.Asset.Test(1000), False, 3600)
-    buckets = node.api.market_history.get_market_history(
-        bucket_seconds=15, start=tt.Time.from_now(weeks=-1), end=tt.Time.from_now(weeks=1)
-    )["buckets"]
-    assert buckets[0]["hive"]["low"] != 301000
-    assert buckets[0]["non_hive"]["low"] != 20000
-
-
-@run_for("testnet", enable_plugins=["market_history_api"])
-def test_get_empty_market_history(node: tt.InitNode) -> None:
-    wallet = tt.Wallet(attach_to=node)
-    wallet.create_account("alice", hives=tt.Asset.Test(500), vests=tt.Asset.Test(100))
-
-    wallet.api.create_order("alice", 0, tt.Asset.Test(300), tt.Asset.Tbd(30), False, 3600)
-    wallet.api.create_order("initminer", 2, tt.Asset.Tbd(100), tt.Asset.Test(1000), False, 3600)
-
-    # wait 6 blocks - more than 15 seconds
-    node.wait_number_of_blocks(6)
-    # ask for history from last 15 seconds
-    buckets = node.api.market_history.get_market_history(
-        bucket_seconds=15, start=tt.Time.from_now(seconds=-15), end=tt.Time.from_now(weeks=1)
-    )["buckets"]
-    assert len(buckets) == 0
-
-
-@pytest.mark.parametrize(
-    # 6, 22 blocks - more than 15 and 60 seconds - to exceed bucket time.
-    ("bucket_seconds", "blocks_to_wait"),
-    [
-        (15, 6),
-        (60, 22),
-    ],
-)
-@run_for("testnet", enable_plugins=["market_history_api"])
-def test_get_two_buckets(node: tt.InitNode, bucket_seconds: int, blocks_to_wait: int) -> None:
-    wallet = tt.Wallet(attach_to=node)
-    wallet.create_account("alice", hives=tt.Asset.Test(500), vests=tt.Asset.Test(100))
-
-    wallet.api.create_order("alice", 0, tt.Asset.Test(300), tt.Asset.Tbd(30), False, 3600)
-    wallet.api.create_order("initminer", 1, tt.Asset.Tbd(100), tt.Asset.Test(1000), False, 3600)
-
-    node.wait_number_of_blocks(blocks_to_wait)
-
-    wallet.api.create_order("alice", 2, tt.Asset.Test(100), tt.Asset.Tbd(10), False, 3600)
-    buckets = node.api.market_history.get_market_history(
-        bucket_seconds=bucket_seconds, start=tt.Time.from_now(weeks=-1), end=tt.Time.from_now(weeks=1)
-    )["buckets"]
-    assert len(buckets) == 2
-
-
+@pytest.mark.parametrize("execution_number", range(1000))
 @pytest.mark.parametrize(
     ("tbds", "hive_high", "non_hive_high", "hive_low", "non_hive_low", "non_hive_volume"),
     [
-        (40, 200_000, 40_000, 100_000, 10_000, 90_000),
         (25, 300_000, 40_000, 100_000, 10_000, 75_000),
-        (8, 300_000, 40_000, 200_000, 8_000, 58_000),
     ],
 )
 @run_for("testnet", enable_plugins=["market_history_api"])
@@ -114,6 +24,7 @@ def test_bucket_output_parameters(
     non_hive_high: int,
     non_hive_low: int,
     non_hive_volume: int,
+    execution_number,
 ) -> None:
     wallet = tt.Wallet(attach_to=node)
     wallet.create_account("alice", hives=tt.Asset.Test(1000), vests=tt.Asset.Test(100))
