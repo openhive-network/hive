9525384ae066300ceafc2e060e7bd921ce4ba5c2
“Mariusz Trela” <mtrela@syncad.com>
2024-09-13 08:50:50 +0200

    Add `app_status_api`

---
+++ libraries/plugins/apis/app_status_api/include/hive/plugins/app_status_api/app_status_api.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <hive/protocol/block.hpp>
+
+#include <hive/plugins/chain/chain_plugin.hpp>
+#include <hive/plugins/json_rpc/utility.hpp>
+
+#include <fc/optional.hpp>
+#include <fc/variant.hpp>
+#include <fc/vector.hpp>
+#include <fc/time.hpp>
+#include <fc/network/ip.hpp>
+
+#include <boost/thread/mutex.hpp>
+
+namespace hive { namespace plugins { namespace app_status_api {
+
+using hive::plugins::json_rpc::void_type;
+
+struct current_status
+{
+  std::string current_status;
+};
+
+/* get_app_status */
+typedef void_type get_app_status_args;
+struct get_app_status_return
+{
+  current_status  value;
+  fc::time_point  time;
+  std::string     name;
+};
+
+namespace detail{ class app_status_api_impl; }
+
+class app_status_api
+{
+  public:
+    app_status_api( appbase::application& app );
+    ~app_status_api();
+
+    DECLARE_API((get_app_status))
+
+  private:
+    std::unique_ptr<detail::app_status_api_impl> my;
+};
+
+} } } // hive::plugins::app_status_api
+
+FC_REFLECT(hive::plugins::app_status_api::current_status, (current_status))
+FC_REFLECT(hive::plugins::app_status_api::get_app_status_return, (value)(time)(name))
