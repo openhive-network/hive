a455190a569a64724769f5632ec6cdb48e31a09c
“Mariusz Trela” <mtrela@syncad.com>
2024-09-13 08:50:50 +0200

    Add `app_status_api`

---
+++ libraries/plugins/apis/app_status_api/include/hive/plugins/app_status_api/app_status_api_plugin.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <hive/plugins/json_rpc/json_rpc_plugin.hpp>
+#include <hive/plugins/chain/chain_plugin.hpp>
+
+#include <appbase/application.hpp>
+
+#define HIVE_API_STATUS_API_PLUGIN_NAME "app_status_api"
+
+namespace hive { namespace plugins { namespace app_status_api {
+
+using namespace appbase;
+
+class app_status_api_plugin : public appbase::plugin< app_status_api_plugin >
+{
+  public:
+    APPBASE_PLUGIN_REQUIRES((hive::plugins::json_rpc::json_rpc_plugin)
+                            (hive::plugins::chain::chain_plugin))
+
+    app_status_api_plugin();
+    virtual ~app_status_api_plugin();
+
+    static const std::string& name()
+    {
+      static std::string name = HIVE_API_STATUS_API_PLUGIN_NAME;
+      return name;
+    }
+
+    virtual void set_program_options(options_description& cli, options_description& cfg) override;
+    virtual void plugin_initialize(const variables_map& options) override;
+    virtual void plugin_startup() override;
+    virtual void plugin_shutdown() override;
+
+    std::shared_ptr<class app_status_api> api;
+};
+
+} } } // hive::plugins::app_status_api
