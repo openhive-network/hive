2e9467467be9c88505d2a63770ed466bc22476f8
“Mariusz Trela” <mtrela@syncad.com>
2024-09-17 08:46:06 +0200

    Refactoring: some changes related to different names

--- libraries/plugins/webserver/include/hive/plugins/webserver/webserver_plugin.hpp
+++ libraries/plugins/webserver/include/hive/plugins/webserver/webserver_plugin.hpp
@@ -13,7 +13,7 @@ namespace hive { namespace plugins { namespace webserver {
 namespace detail { class webserver_base; }
 
 using namespace appbase;
-using collector_t = hive::utilities::collector_t;
+using data_collector = hive::utilities::data_collector;
 
 /**
   * This plugin starts an HTTP/ws webserver and dispatches queries to
