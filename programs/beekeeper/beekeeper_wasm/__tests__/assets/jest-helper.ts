a8a7196916118fcc1e3895944511939ba8cc9a59
mtyszczak <mateusz.tyszczak@gmail.com>
2024-09-23 12:24:38 +0200

    Allow to create one-environment only tests

--- programs/beekeeper/beekeeper_wasm/__tests__/assets/jest-helper.ts
+++ programs/beekeeper/beekeeper_wasm/__tests__/assets/jest-helper.ts
@@ -11,7 +11,7 @@ export interface IBeekeeperTest {
    * Runs given function in both environments: web and Node.js
    * Created specifically for testing the beekeeper code
    *
-   * Checks if results are equal. If your tests may differ please use {@link dual.dynamic}
+   * Checks if results are equal. If your tests may differ please use {@link beekeeperTest.dynamic}
    */
   beekeeperTest: (<R, Args extends any[]>(fn: TBeekeeperTestCallable<R, Args>, ...args: Args) => Promise<R>) & {
     /**
@@ -26,7 +26,7 @@ export interface IBeekeeperTest {
    * Runs given function in both environments: web and Node.js
    * Created specifically for testing WASM code
    *
-   * Checks if results are equal. If your tests may differ please use {@link dual.dynamic}
+   * Checks if results are equal. If your tests may differ please use {@link beekeeperWasmTest.dynamic}
    */
   beekeeperWasmTest: (<R, Args extends any[]>(fn: TBeekeeperWasmTestCallable<R, Args>, ...args: Args) => Promise<R>) & {
     /**
@@ -36,20 +36,43 @@ export interface IBeekeeperTest {
      */
     dynamic: <R, Args extends any[]>(fn: TBeekeeperWasmTestCallable<R, Args>, ...args: Args) => Promise<R>;
   };
+
+  /**
+   * Runs given function in web-only environment
+   * Created specifically for testing WASM code
+   */
+  beekeeperWasmTestWebOnly: (<R, Args extends any[]>(fn: TBeekeeperWasmTestCallable<R, Args>, ...args: Args) => Promise<R>);
+
+  /**
+   * Runs given function in Node-only environment
+   * Created specifically for testing Node code
+   */
+  beekeeperWasmTestNodeOnly: (<R, Args extends any[]>(fn: TBeekeeperWasmTestCallable<R, Args>, ...args: Args) => Promise<R>);
 }
 
+/**
+ * @param page Current playwright page
+ * @param globalFunction function injecting globals based on the environment
+ * @param forceEnv optional parameter for forcing the environment to be used (web or node). If none provided - tests both
+ */
 const envTestFor = <GlobalType extends IBeekeeperGlobals | IBeekeeperWasmGlobals> (
   page: Page,
   globalFunction: (env: TEnvType) => Promise<GlobalType>,
+  forceEnv?: TEnvType
 ): IBeekeeperTest[GlobalType extends IBeekeeperGlobals ? 'beekeeperTest' : 'beekeeperWasmTest'] => {
   const runner = async <R, Args extends any[]> (checkEqual: Boolean, fn: GlobalType extends IBeekeeperGlobals ? TBeekeeperTestCallable<R, Args> : TBeekeeperWasmTestCallable<R, Args>, ...args: Args): Promise<R> => {
-    let nodeData = await fn(await (globalFunction as Function)('node'), ...args);
+    let nodeData: any, webData: any;
 
-    const webData = await page.evaluate(async ({ args, globalFunction, webFn }) => {
-      eval(`window.webEvalFn = ${webFn};`);
+    if (forceEnv === undefined || forceEnv === 'node')
+      nodeData = await fn(await (globalFunction as Function)('node'), ...args);
 
-      return (window as Window & typeof globalThis & { webEvalFn: Function }).webEvalFn(await globalThis[globalFunction]('web'), ...args);
-    }, { args, globalFunction: globalFunction.name, webFn: fn.toString() });
+
+    if (forceEnv === undefined || forceEnv === 'web')
+      webData = await page.evaluate(async ({ args, globalFunction, webFn }) => {
+        eval(`window.webEvalFn = ${webFn};`);
+
+        return (window as Window & typeof globalThis & { webEvalFn: Function }).webEvalFn(await globalThis[globalFunction]('web'), ...args);
+      }, { args, globalFunction: globalFunction.name, webFn: fn.toString() });
 
     if(typeof nodeData === "object") // Remove prototype data from the node result to match webData
       nodeData = JSON.parse(JSON.stringify(nodeData));
@@ -61,9 +84,10 @@ const envTestFor = <GlobalType extends IBeekeeperGlobals | IBeekeeperWasmGlobals
   };
 
   const using = function<R, Args extends any[]>(fn: GlobalType extends IBeekeeperGlobals ? TBeekeeperTestCallable<R, Args> : TBeekeeperWasmTestCallable<R, Args>, ...args: Args) {
-    return runner.bind(undefined, true)(fn as any, ...args);
+    return runner.bind(undefined, forceEnv === undefined)(fn as any, ...args);
   };
-  using.dynamic = runner.bind(undefined, false);
+  if (forceEnv === undefined)
+    using.dynamic = runner.bind(undefined, false);
 
   return using as IBeekeeperTest[GlobalType extends IBeekeeperGlobals ? 'beekeeperTest' : 'beekeeperWasmTest'];
 };
@@ -74,5 +98,11 @@ export const test = base.extend<IBeekeeperTest>({
   },
   beekeeperWasmTest: async({ page }, use) => {
     use(envTestFor(page, createBeekeeperWasmTestFor));
+  },
+  beekeeperWasmTestWebOnly: async({ page }, use) => {
+    use(envTestFor(page, createBeekeeperWasmTestFor, 'web'));
+  },
+  beekeeperWasmTestNodeOnly: async({ page }, use) => {
+    use(envTestFor(page, createBeekeeperWasmTestFor, 'node'));
   }
 });
