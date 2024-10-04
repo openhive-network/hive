b53a736c5f96b15a9441f9703aa1a044c7f0d64a
Krzysztof Leśniak <klesniak@syncad.com>
2024-10-04 16:43:40 +0200

    Reduce shared file size to 1G in some tests

--- tests/integration/test_several_node_restarts.sh
+++ tests/integration/test_several_node_restarts.sh
@@ -30,12 +30,12 @@ echo "Create replay environment..."
 "$SCRIPTS_PATH/ci-helpers/prepare_data_and_shm_dir.sh" --data-base-dir="$SOURCE_DATA_DIR" --block-log-source-dir="$BLOCK_LOG_SOURCE_DIR" --config-ini-source="$CONFIG_INI_SOURCE"
 
 echo "Start first replay: "
-"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=first-replay-container --docker-option="--network=bridge" --replay --stop-at-block=$first_replay_block_limit --detach
+"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=first-replay-container --docker-option="--network=bridge" --replay --stop-at-block=$first_replay_block_limit --detach --shared-file-size=1G
 wait_for_instance first-replay-container $first_replay_block_limit
 stop_instance "first-replay-container"
 
 echo "Start second replay: "
-"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=second-replay-container --docker-option="--network=bridge" --replay --stop-at-block=$second_replay_block_limit --detach
+"$SCRIPTS_PATH"/run_hived_img.sh "$HIVED_IMAGE_NAME" --data-dir="$SOURCE_DATA_DIR"/datadir --name=second-replay-container --docker-option="--network=bridge" --replay --stop-at-block=$second_replay_block_limit --detach --shared-file-size=1G
 wait_for_instance second-replay-container $second_replay_block_limit
 stop_instance "second-replay-container"
 
