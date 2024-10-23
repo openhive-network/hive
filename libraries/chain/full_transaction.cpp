a96fa61d8167d3f05ad86cf95d7734a84a94e02d
“Mariusz Trela” <mtrela@syncad.com>
2024-10-23 08:56:50 +0200

    `runtime_expiration` variable should be always set

--- libraries/chain/full_transaction.cpp
+++ libraries/chain/full_transaction.cpp
@@ -339,17 +339,9 @@ void full_transaction_type::sign_transaction(const std::vector<hive::protocol::p
   }
 }
 
-fc::time_point_sec full_transaction_type::get_runtime_expiration( bool is_hf_28 ) const
+fc::time_point_sec full_transaction_type::get_runtime_expiration() const
 {
-  if( is_hf_28 )
-  {
-    if( runtime_expiration )
-      return *runtime_expiration;
-    else
-      return get_transaction().expiration;
-  }
-  else
-    return get_transaction().expiration;
+  return runtime_expiration;
 }
 
 /* static */ full_transaction_ptr full_transaction_type::create_from_block(const std::shared_ptr<decoded_block_storage_type>& block_storage, 
