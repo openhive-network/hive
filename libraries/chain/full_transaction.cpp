391d1e75aa6a56f55587811709680979e3213918
Bartek Wrona <wrona@syncad.com>
2024-10-25 20:35:01 +0200

    full_transaction_type::get_runtime_expiration() moved to header file to make possible inlining.

--- libraries/chain/full_transaction.cpp
+++ libraries/chain/full_transaction.cpp
@@ -339,11 +339,6 @@ void full_transaction_type::sign_transaction(const std::vector<hive::protocol::p
   }
 }
 
-fc::time_point_sec full_transaction_type::get_runtime_expiration() const
-{
-  return runtime_expiration;
-}
-
 /* static */ full_transaction_ptr full_transaction_type::create_from_block(const std::shared_ptr<decoded_block_storage_type>& block_storage, 
                                                                            uint32_t index_in_block, 
                                                                            const serialized_transaction_data& serialized_transaction,
