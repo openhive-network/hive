030275201db1f3151affce47e9456e5ce8c94d05
“Mariusz Trela” <mtrela@syncad.com>
2024-11-21 16:57:33 +0100

    Don't process transactions/blocks when the working thread is finished

--- libraries/plugins/chain/chain_plugin.cpp
+++ libraries/plugins/chain/chain_plugin.cpp
@@ -2069,4 +2069,10 @@ void chain_plugin::finish_request()
 {
   my->finish_request();
 }
+
+bool chain_plugin::is_finished_write_processing() const
+{
+  return my->finish.status.load();
+}
+
 } } } // namespace hive::plugis::chain::chain_apis
