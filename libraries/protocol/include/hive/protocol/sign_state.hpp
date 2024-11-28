a0be7ffc4eb35085c9e80d03a055aaab9448d3ee
“Mariusz Trela” <mtrela@syncad.com>
2024-11-28 14:05:24 +0100

    Introduce `change_current_authority` into `sign_state`

--- libraries/protocol/include/hive/protocol/sign_state.hpp
+++ libraries/protocol/include/hive/protocol/sign_state.hpp
@@ -18,38 +18,41 @@ struct sign_limits
 
 struct sign_state
 {
-  /** returns true if we have a signature for this key or can
-    * produce a signature for this key, else returns false.
-    */
-  bool signed_by( const public_key_type& k );
-  bool check_authority( const string& id );
-
-  /**
-    *  Checks to see if we have signatures of the active authorites of
-    *  the accounts specified in authority or the keys specified.
-    */
-  bool check_authority( const authority& au, uint32_t depth = 0, uint32_t account_auth_count = 0 );
-
-  bool remove_unused_signatures();
-
-  void init_approved();
-  void add_approved( const flat_set<account_name_type>& approvals );
-  void extend_provided_signatures( const flat_set<public_key_type>& keys );
-  const flat_map<public_key_type,bool>&  get_provided_signatures() const { return provided_signatures; }
-
-  sign_state( const flat_set<public_key_type>& sigs,
-          const authority_getter& a,
-          const sign_limits& limits );
-
-  const authority_getter&          get_current_authority;
-
   private:
+
+    authority_getter                get_current_authority;
 
     const sign_limits               limits;
     flat_set<string>                approved_by;
     flat_map<public_key_type,bool>  provided_signatures;
 
     bool check_authority_impl( const authority& au, uint32_t depth, uint32_t* account_auth_count );
+
+  public:
+
+    sign_state( const flat_set<public_key_type>& sigs, const authority_getter& a, const sign_limits& limits );
+
+    /** returns true if we have a signature for this key or can
+      * produce a signature for this key, else returns false.
+      */
+    bool signed_by( const public_key_type& k );
+    bool check_authority( const string& id );
+
+    /**
+      *  Checks to see if we have signatures of the active authorites of
+      *  the accounts specified in authority or the keys specified.
+      */
+    bool check_authority( const authority& au, uint32_t depth = 0, uint32_t account_auth_count = 0 );
+
+    bool remove_unused_signatures();
+
+    void init_approved();
+    void add_approved( const flat_set<account_name_type>& approvals );
+
+    void extend_provided_signatures( const flat_set<public_key_type>& keys );
+    const flat_map<public_key_type,bool>&  get_provided_signatures() const { return provided_signatures; }
+
+    void change_current_authority( const authority_getter& a );
 };
 
 } } // hive::protocol
