9953b6638476d865b0c005761875d8f156a35001
mtyszczak <mateusz.tyszczak@gmail.com>
2025-01-14 13:00:41 +0100

    Fix directory creation in fs init in beekeeper

--- programs/beekeeper/beekeeper_wasm/src/detailed/fs.ts
+++ programs/beekeeper/beekeeper_wasm/src/detailed/fs.ts
@@ -1,4 +1,5 @@
 import type { BeekeeperModule } from "../beekeeper.js";
+import { BeekeeperError } from "../errors.js";
 import { safeWasmCall } from "../util/wasm_error.js";
 
 export class BeekeeperFileSystem {
@@ -16,9 +17,30 @@ export class BeekeeperFileSystem {
     });
   }
 
+  private ensureCreateDir(paths: string[]) {
+    // We need an absolute path in web environment in order to create proper directories
+    const dir = (process.env.ROLLUP_TARGET_ENV === "web" ? "/" : "") + paths.join("/");
+
+    let analysis = safeWasmCall(() => this.fs.analyzePath(dir));
+
+    if (!analysis.exists) {
+      if (paths.length > 1)
+        this.ensureCreateDir(paths.slice(0, -1));
+
+      safeWasmCall(() => this.fs.mkdir(dir));
+    }
+  }
+
   public init(walletDir: string): Promise<void> {
-    if(!safeWasmCall(() => this.fs.analyzePath(walletDir).exists))
-      safeWasmCall(() => this.fs.mkdir(walletDir));
+    if(process.env.ROLLUP_TARGET_ENV === "web" && !walletDir.startsWith("/"))
+      throw new BeekeeperError("Storage root directory must be an absolute path in web environment");
+
+    const walletDirPathParts = walletDir.split("/").filter(node => !!node && node !== ".");
+
+    if (walletDirPathParts.length === 0)
+      throw new BeekeeperError("Storage root directory must not be empty");
+
+    this.ensureCreateDir(walletDirPathParts);
 
     if(process.env.ROLLUP_TARGET_ENV === "web")
       safeWasmCall(() => this.fs.mount(this.fs.filesystems.IDBFS, {}, walletDir));
--- programs/beekeeper/beekeeper_wasm/src/util/wasm_error.ts
+++ programs/beekeeper/beekeeper_wasm/src/util/wasm_error.ts
@@ -1,10 +1,25 @@
 import { BeekeeperError } from "../errors.js";
 
+// TODO: Check for errno on Windows
+const stringifyError = (error: unknown): string => {
+  if (error instanceof Error)
+    return error.message;
+
+  const name = error && typeof error === "object" && "name" in error ? error.name : undefined;
+
+  if (name === "ErrnoError" && "errno" in (error as object)) {
+    const errno = (error as any).errno as number;
+    return `ErrnoError: #${errno}`;
+  }
+
+  return "Unknown error" + (name ? `: ${name}` : "");
+};
+
 export const safeWasmCall = <T extends () => any>(fn: T): ReturnType<T> => {
   try {
     return fn()
   } catch (error) {
-    throw new BeekeeperError(`Error during Wasm call: ${error instanceof Error ? error.message : error}`);
+    throw new BeekeeperError(`Error during Wasm call: ${stringifyError(error)}`);
   }
 };
 
@@ -12,6 +27,6 @@ export const safeAsyncWasmCall = async <T extends () => any>(fn: T): Promise<Ret
   try {
     return await fn();
   } catch (error) {
-    throw new BeekeeperError(`Error during Wasm call: ${error instanceof Error ? error.message : error}`);
+    throw new BeekeeperError(`Error during Wasm call: ${stringifyError(error)}`);
   }
 };
