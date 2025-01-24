72caefca2c2edc4993de5b6da0846b9f530f5d45
Michał Kudela <kudmich@syncad.com>
2025-01-24 15:53:49 +0100

    Resurrect deprecated script: datagen block-log generator

--- tests/python/functional/datagen_tests/recalculation_proposal_vote_tests/test_recalculation_proposal_vote.py
+++ tests/python/functional/datagen_tests/recalculation_proposal_vote_tests/test_recalculation_proposal_vote.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import os
 from pathlib import Path
 
 import test_tools as tt
@@ -12,27 +13,30 @@ from schemas.operations.virtual import ProposalPayOperation
 
 
 def test_recalculation_proposal_votes() -> None:
+    destination_variable = os.environ.get("TESTING_BLOCK_LOGS_DESTINATION")
+    assert destination_variable is not None, "Path TESTING_BLOCK_LOGS_DESTINATION must be set!"
+    block_log_directory = Path(destination_variable) / "recalculation_proposal_vote"
+    block_log = tt.BlockLog(block_log_directory, "auto")
+    acs = tt.AlternateChainSpecs.parse_file(block_log_directory / tt.AlternateChainSpecs.FILENAME)
+
     node = tt.InitNode()
 
     # load block_log to node
     node.config.shared_file_size = "2G"
     node.config.plugin.append("account_history_api")
-    node.config.block_log_split = -1
-    node.run(
-        replay_from=Path(__file__).parent.joinpath("block_log"),
-        exit_before_synchronization=True,
-    )
+    node.config.block_log_split = 9999
+    node.run(replay_from=block_log, exit_before_synchronization=True, alternate_chain_specs=acs)
 
     # run node with specific timestamp ( end of block_log )
-    node.run(time_control=tt.StartTimeControl(start_time="head_block_time"))
+    node.run(time_control=tt.StartTimeControl(start_time="head_block_time"), alternate_chain_specs=acs)
 
     tt.logger.info(f"next_maintenance_time: {get_next_maintenance_time(node)}")
     tt.logger.info(f"LBN: {node.get_last_block_number()}")
     tt.logger.info(f"HBT: {node.get_head_block_time()}")
 
-    # the first maintenance block has the number 1262 ( processed in block_log )
+    # the first maintenance block has the number 107 ( processed in block_log )
     vops_in_first_maintenance_block = get_virtual_operations(
-        node, ProposalPayOperation, start_block=1262, end_block=1263
+        node, ProposalPayOperation, start_block=107, end_block=108
     )
     proposal_id_from_first_maintenance_block = vops_in_first_maintenance_block[0]["op"]["value"]["proposal_id"]
     receiver_from_first_maintenance_block = vops_in_first_maintenance_block[0]["op"]["value"]["receiver"]
@@ -41,19 +45,19 @@ def test_recalculation_proposal_votes() -> None:
     assert len(get_virtual_operations(node, ProposalPayOperation, start_block=lbn, end_block=lbn + 100)) == 0
 
     wait_for_maintenance_block(node, get_next_maintenance_time(node))
+    maintenance_block_number = node.get_last_block_number()
 
     tt.logger.info(f"next_maintenance_time: {get_next_maintenance_time(node)}")
     tt.logger.info(f"LBN: {node.get_last_block_number()}")
     tt.logger.info(f"HBT: {node.get_head_block_time()}")
 
-    assert len(get_virtual_operations(node, ProposalPayOperation, start_block=2300, end_block=2500)) > 0
-
     vops_in_second_maintenance_block = get_virtual_operations(
         node,
         ProposalPayOperation,
-        start_block=node.get_last_block_number() - 100,
-        end_block=node.get_last_block_number() + 100,
+        start_block=maintenance_block_number-1,
+        end_block=maintenance_block_number,
     )
+    assert len(vops_in_second_maintenance_block) > 0
     proposal_id_from_second_maintenance_block = vops_in_second_maintenance_block[0]["op"]["value"]["proposal_id"]
     receiver_from_second_maintenance_block = vops_in_second_maintenance_block[0]["op"]["value"]["receiver"]
     assert proposal_id_from_first_maintenance_block != proposal_id_from_second_maintenance_block
