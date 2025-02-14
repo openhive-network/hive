36fe8d3abfa491e7344f61d780ea524335b3bc32
Bartek Wrona <wrona@syncad.com>
2025-02-14 02:19:42 +0100

    fc extended by support for uncompressing deflate format

--- libraries/fc/include/fc/compress/zlib.hpp
+++ libraries/fc/include/fc/compress/zlib.hpp
@@ -10,6 +10,15 @@ namespace fc
   *   \return byte buffer holding compressed data.
   */
   string zlib_compress(const string& in);
+
+  /** Allows to perform low level ZLib uncompression (inflate process, i.e. being a part of work done by [g]zip tools)
+  *   \param in input byte buffer to be inflated (it should not contain any additional header specific to [g]zip tools).
+  *             For example this tool: `zopfli -c --deflate <in-file>` is able to produce pure output next to be passed
+                to this function.
+  *   \return byte buffer holding compressed data.
+  */
+  string zlib_inflate(const string& in);
+
   /** Allows to unpack contents of zip archive passed as input byte string.
    *  \param in input byte buffer to be opened as .zip archive
    *  \returns byte buffer holding inflated (uncompressed) data.
