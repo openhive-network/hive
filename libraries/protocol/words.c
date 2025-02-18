d01c8e1d7e87c17648e220625ca093709658caf4
Marek <marek@syncad.com>
2025-02-18 18:45:57 +0000

    Add static_assert for non-empty 'words.deflate' and 'words.b64' files

--- libraries/protocol/words.c
+++ libraries/protocol/words.c
@@ -6,5 +6,6 @@ const uint8_t word_list_zipped[] = {
 #embed "words.deflate"
 };
 const uint32_t word_list_zipped_size = sizeof(word_list_zipped);
+static_assert(sizeof(word_list_zipped_size) != 0);
 
 #endif // C23_EMBED_SUPPORTED
