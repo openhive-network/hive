ca70436b1f710667ef48b56d6f7342bd8fe6f263
Michał Kudela <kudmich@syncad.com>
2025-02-24 13:14:22 +0100

    Create conftest to allow tests to use testing block logs from container registry

--- tests/python/functional/foundation_layer_tests/conftest.py
+++ tests/python/functional/foundation_layer_tests/conftest.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import os
+from pathlib import Path
+
 import pytest
 
 import test_tools as tt
@@ -8,3 +11,16 @@ import test_tools as tt
 @pytest.fixture()
 def wallet(node):
     return tt.Wallet(attach_to=node)
+
+
+@pytest.fixture()
+def block_logs_for_testing_location() -> Path:
+    destination_variable = os.environ.get("TESTING_BLOCK_LOGS_DESTINATION")
+    assert destination_variable is not None, "Path TESTING_BLOCK_LOGS_DESTINATION must be set!"
+    return Path(destination_variable)
+
+
+@pytest.fixture()
+def block_log_empty_30_mono(block_logs_for_testing_location: Path) -> tt.BlockLog:
+    block_logs_location: Path = block_logs_for_testing_location
+    return tt.BlockLog(path=block_logs_location / "empty_30", mode="monolithic")
