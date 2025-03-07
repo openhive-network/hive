2df581206743b4007b050b4f73c362f4b8992bf6
asuch <asuch@syncad.com>
2025-03-07 11:14:18 +0100

    Add new test for snapshot loading

--- tests/python/functional/hived/test_snapshot.py
+++ tests/python/functional/hived/test_snapshot.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 
+import pytest
 import test_tools as tt
+
 from hive_local_tools.functional.python.compare_snapshot import compare_snapshots_contents
 
 if TYPE_CHECKING:
@@ -68,3 +71,64 @@ def test_snapshots_existing_dir(block_log: Path, block_log_length: int) -> None:
 
     node.wait_for_block_with_number(block_log_length + 7)
     node.close()
+
+def test_snapshots_has_more_plugins(block_log: Path, block_log_length: int) -> None:
+    def clear_state(node: tt.InitNode):
+        from os.path import join as join_paths
+        from shutil import rmtree
+
+        rmtree(join_paths(str(node.directory), "blockchain", "shared_memory.bin"), ignore_errors=True)
+
+    node = tt.InitNode()
+    # extra plugin with new indexes: `block_log_info`
+    node.config.plugin.append("block_log_info")
+    node.run(exit_before_synchronization=True, replay_from=block_log)
+    node.close()
+
+    snap_0 = node.dump_snapshot(close=True)
+
+    clear_state(node)
+    node.config.plugin.remove("block_log_info")
+    node.run(exit_before_synchronization=True, replay_from=block_log)
+    node.run(load_snapshot_from=snap_0)
+    node.wait_for_block_with_number(block_log_length + 5)
+    node.close()
+
+    # two files should be created which will show difference between state definitions in shm and current hived
+    assert os.path.isfile(node.directory / "current_decoded_types_details.log")
+    assert os.path.isfile(node.directory / "loaded_from_shm_decoded_types_details.log")
+
+    warning_msg = "Snaphot has more plugins than current hived configuration"
+    assert warning_msg in (node.directory / "stderr.txt").read_text()
+
+def test_snapshots_has_less_plugins(block_log: Path, block_log_length: int) -> None:
+    def clear_state(node: tt.InitNode):
+        from os.path import join as join_paths
+        from shutil import rmtree
+
+        rmtree(join_paths(str(node.directory), "blockchain", "shared_memory.bin"), ignore_errors=True)
+
+    node = tt.InitNode()
+    node.run(exit_before_synchronization=True, replay_from=block_log)
+    node.close()
+
+    snap_0 = node.dump_snapshot(close=True)
+
+    clear_state(node)
+    # add extra plugin to node - now snapshot has less plugins
+    node.config.plugin.append("block_log_info")
+    node.run(exit_before_synchronization=True, replay_from=block_log)
+
+    with pytest.raises(RuntimeError):
+        node.run(load_snapshot_from=snap_0, exit_before_synchronization=True)
+
+    # two files should be created which will show difference between state definitions in shm and current hived
+    assert os.path.isfile(node.directory / "current_decoded_types_details.log")
+    assert os.path.isfile(node.directory / "loaded_from_shm_decoded_types_details.log")
+
+    error_msg_1 = "Snapshot misses plugins"
+    error_msg_2 = "State objects definitions from shared memory file mismatch current version of app"
+
+    log = (node.directory / "stderr.txt").read_text()
+    assert error_msg_1 in log
+    assert error_msg_2 in log
