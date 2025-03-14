0c6971d921c7218bcc68dcc225e95169d76cbdec
Radek Masłowski <rmaslowski@syncad.com>
2025-03-14 12:49:08 +0100

    wip

--- tests/python/functional/beekeeper_wallet_tests/test_account_creating.py
+++ tests/python/functional/beekeeper_wallet_tests/test_account_creating.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 from pydantic import ValidationError
-
+from helpy._interfaces.wax import WaxOperationFailedError
 if TYPE_CHECKING:
     import test_tools as tt
 
@@ -19,22 +19,22 @@ def test_account_creation(wallet: tt.Wallet) -> None:
     [
         (
             "too-long-account-name",
-            "ensure this value has at most 16 characters",
+            "Input too large: `too-long-account-name` (21) for fixed size string: (16)",
         ),
         (
             "to",
-            "ensure this value has at least 3 characters",
+            "Account name \'to\' is too short. Use at least 3 characters.",
         ),
         (
             "...",
-            "string does not match regex",
+            "Account name \'...\' is not valid. Please follow the RFC 1035 rules",
         ),
     ],
 )
 def test_creation_of_account_with_invalid_name(
     wallet: tt.Wallet, account_name: str, expected_error_message: str
 ) -> None:
-    with pytest.raises(ValidationError) as exception:
+    with pytest.raises(WaxOperationFailedError) as exception:
         wallet.api.create_account("initminer", account_name, "{}")
 
-    assert expected_error_message in exception.value.json()
+    assert expected_error_message in str(exception.value)
