f6f4d7fe18925577db47650d3a1c78a23f7b4c6e
Marek <marek@syncad.com>
2025-03-19 18:22:15 +0000

    Use bip::offset_ptr instead of raw pointers

--- libraries/chainbase/include/chainbase/pool_allocator.hpp
+++ libraries/chainbase/include/chainbase/pool_allocator.hpp
@@ -35,17 +35,19 @@ namespace chainbase {
 
   // BLOCK_SIZE - it means how many objects fit in allocated memory block
   template <typename T, uint32_t BLOCK_SIZE, typename TSegmentManager = segment_manager_t, bool USE_MANAGED_MAPPED_FILE = !_ENABLE_STD_ALLOCATOR>
-  class pool_allocator_t
+  class pool_allocator_t : private allocator<T>
     {
+    private:
+      typedef allocator<T> base_class_t;
+
     public:
-      typedef T value_type;
-      typedef T* pointer;
-      typedef const T* const_pointer;
-      typedef T& reference;
-      typedef const T& const_reference;
-      typedef std::size_t size_type;
-      typedef std::ptrdiff_t difference_type;
-      typedef std::true_type propagate_on_container_move_assignment;
+      typedef typename base_class_t::value_type value_type;
+      typedef typename base_class_t::pointer pointer;
+      typedef typename base_class_t::const_pointer const_pointer;
+      typedef typename base_class_t::reference reference;
+      typedef typename base_class_t::const_reference const_reference;
+      typedef typename base_class_t::size_type size_type;
+      typedef typename base_class_t::difference_type difference_type;
 
       template <typename U>
       struct rebind
@@ -53,12 +55,13 @@ namespace chainbase {
         typedef pool_allocator_t<U, BLOCK_SIZE> other;
       };
 
-      typedef std::true_type is_always_equal;
+      typedef std::false_type is_always_equal;
 
     public:
       template <bool _USE_MANAGED_MAPPED_FILE = USE_MANAGED_MAPPED_FILE>
       pool_allocator_t(TSegmentManager* _segment_manager, std::enable_if_t<_USE_MANAGED_MAPPED_FILE>* = nullptr)
-        : block_index(typename decltype(block_index)::allocator_type(_segment_manager)),
+        : base_class_t(_segment_manager),
+          block_index(typename decltype(block_index)::allocator_type(_segment_manager)),
           block_list(typename decltype(block_list)::allocator_type(_segment_manager))
         {
         }
@@ -69,7 +72,8 @@ namespace chainbase {
       template <typename T2, bool _USE_MANAGED_MAPPED_FILE = USE_MANAGED_MAPPED_FILE>
       pool_allocator_t(const pool_allocator_t<T2, BLOCK_SIZE, TSegmentManager, USE_MANAGED_MAPPED_FILE>& other,
         std::enable_if_t<_USE_MANAGED_MAPPED_FILE>* = nullptr)
-        : block_index(typename decltype(block_index)::allocator_type(other.get_segment_manager())),
+        : base_class_t(other.get_segment_manager()),
+          block_index(typename decltype(block_index)::allocator_type(other.get_segment_manager())),
           block_list(typename decltype(block_list)::allocator_type(other.get_segment_manager()))
         {
         }
@@ -83,28 +87,28 @@ namespace chainbase {
         {
         }
 
-      T* allocate(size_type n = 1)
+      pointer allocate(size_type n = 1)
         {
         assert(n == 1);
         ++allocated_count;
-        return get_object_memory();
+        return pointer(get_object_memory());
         }
 
-      void deallocate(T* p, size_type n = 1)
+      void deallocate(pointer p, size_type n = 1)
         {
         assert(n == 1);
         --allocated_count;
-        return_object_memory(p);
+        return_object_memory(&(*p));
         }
 
       template <bool _USE_MANAGED_MAPPED_FILE = USE_MANAGED_MAPPED_FILE>
-      segment_manager_t* get_segment_manager(std::enable_if_t<_USE_MANAGED_MAPPED_FILE>* = nullptr) const
+      segment_manager_t* get_segment_manager(std::enable_if_t<_USE_MANAGED_MAPPED_FILE>* = nullptr) const noexcept
         {
-        return block_index.get_allocator().get_segment_manager();
+        return base_class_t::get_segment_manager();
         }
 
       template <bool _USE_MANAGED_MAPPED_FILE = USE_MANAGED_MAPPED_FILE>
-      segment_manager_t* get_segment_manager(std::enable_if_t<!_USE_MANAGED_MAPPED_FILE>* = nullptr) const
+      segment_manager_t* get_segment_manager(std::enable_if_t<!_USE_MANAGED_MAPPED_FILE>* = nullptr) const noexcept
         {
         return nullptr;
         }
@@ -150,68 +154,82 @@ namespace chainbase {
         }
 
     private:
-      union chunk_t
+      union alignas(T) chunk_t
         {
-        alignas(T) char object_memory[sizeof(T)];
-        chunk_t*        next;
+        char      memory[sizeof(T)];
+        uint32_t  next;
 
         T* get_memory_address() noexcept
           {
-          return reinterpret_cast<T*>(object_memory);
+          return reinterpret_cast<T*>(memory);
           }
         };
 
-      struct block_t
+      class block_t
         {
-        chunk_t   chunks[BLOCK_SIZE];
-        chunk_t*  current = nullptr;
-        uint32_t  free_count = BLOCK_SIZE;
-        bool      on_list = false;
+        public:
+          block_t() noexcept
+            {
+            for (uint32_t i = 1; i < BLOCK_SIZE; ++i, ++current)
+              chunks[current].next = i;
+            chunks[current].next = 0;
+            current = 0;
+            }
 
-        block_t() noexcept
-          {
-          current = chunks;
-          for (uint32_t i = 1; i < BLOCK_SIZE; ++i, ++current)
-            current->next = chunks + i;
-          current->next = nullptr;
-          current = chunks;
-          }
+          T* get_object_memory() noexcept
+            {
+            assert(!empty());
+            T* result = chunks[current].get_memory_address();
+            current = chunks[current].next;
+            --free_count;
+            return result;
+            }
 
-        T* get_object_memory() noexcept
-          {
-          T* result = current->get_memory_address();
-          current = current->next;
-          --free_count;
-          assert((current == nullptr) == empty());
-          return result;
-          }
+          bool return_object_memory(T* object) noexcept
+            {
+            chunk_t* chunk = reinterpret_cast<chunk_t*>(object);
+            assert(chunk >= chunks && chunk < (chunks + BLOCK_SIZE) &&
+              ((reinterpret_cast<std::size_t>(chunk) - reinterpret_cast<std::size_t>(chunks)) % sizeof(T)) == 0);
+            chunk->next = current;
+            current = chunk - chunks;
+            return ++free_count == BLOCK_SIZE;
+            }
 
-        bool return_object_memory(chunk_t* chunk) noexcept
-          {
-          assert(chunk >= get_chunks_address() && chunk < (get_chunks_address() + BLOCK_SIZE) &&
-            ((reinterpret_cast<std::size_t>(chunk) - reinterpret_cast<std::size_t>(get_chunks_address())) % sizeof(T)) == 0);
-          chunk->next = current;
-          current = chunk;
-          return ++free_count == BLOCK_SIZE;
-          }
+          void set_on_list(bool value) noexcept
+            {
+            on_list = value;
+            }
 
-        const chunk_t* get_chunks_address() const noexcept
-          {
-          return chunks;
-          }
+          bool is_on_list() const noexcept
+            {
+            return on_list;
+            }
 
-        // true if whole memory is free
-        bool full() const noexcept
-          {
-          return free_count == BLOCK_SIZE;
-          }
+          // true if whole memory is free
+          bool full() const noexcept
+            {
+            return free_count == BLOCK_SIZE;
+            }
 
-        // true if no more free memory
-        bool empty() const noexcept
-          {
-          return free_count == 0;
-          }
+          // true if no more free memory
+          bool empty() const noexcept
+            {
+            return free_count == 0;
+            }
+
+          const chunk_t* get_chunks_address() const noexcept
+            {
+            return chunks;
+            }
+
+        private:
+          chunk_t   chunks[BLOCK_SIZE];
+          uint32_t  current = 0;
+          uint32_t  free_count = BLOCK_SIZE;
+          bool      on_list = false;
         };
+
+      using block_ptr_t = std::conditional_t<_ENABLE_STD_ALLOCATOR, block_t*, bip::offset_ptr<block_t>>;
 
       struct block_comparator_t
         {
@@ -222,7 +240,7 @@ namespace chainbase {
         };
 
       using block_index_t = t_set<block_t, block_comparator_t>;
-      using block_list_t = t_vector<block_t*>;
+      using block_list_t = t_vector<block_ptr_t>;
 
     private:
       T* get_object_memory()
@@ -231,7 +249,7 @@ namespace chainbase {
           {
           while (!block_list.empty())
             {
-            block_t* block = remove_from_block_list();
+            block_ptr_t block = remove_from_block_list();
 
             if (block->full() && !block_list.empty())
               {
@@ -262,32 +280,32 @@ namespace chainbase {
         const auto found = block_index.lower_bound(*block);
         assert(found != block_index.cend());
         block = const_cast<block_t*>(&(*found));
-        block->return_object_memory(reinterpret_cast<chunk_t*>(object));
+        block->return_object_memory(object);
         if (block != current_block)
           add_to_block_list(block);
         }
 
-      block_t* allocate_block()
+      block_ptr_t allocate_block()
         {
         auto insert_result = block_index.emplace();
-        block_t* block = const_cast<block_t*>(&(*insert_result.first));
+        block_ptr_t block = const_cast<block_t*>(&(*insert_result.first));
         return block;
         }
 
       void add_to_block_list(block_t* block)
         {
-        if (!block->on_list)
+        if (!block->is_on_list())
           {
-          block->on_list = true;
-          block_list.push_back(block);
+          block->set_on_list(true);
+          block_list.emplace_back(block);
           }
         }
 
-      block_t* remove_from_block_list() noexcept
+      block_ptr_t remove_from_block_list() noexcept
         {
         assert(!block_list.empty());
-        block_t* block = block_list.back();
-        block->on_list = false;
+        block_ptr_t block = block_list.back();
+        block->set_on_list(false);
         block_list.pop_back();
         return block;
         }
@@ -310,10 +328,10 @@ namespace chainbase {
         }
 
     private:
-      block_index_t block_index;
-      block_list_t  block_list;
-      block_t*      current_block = nullptr;
-      uint32_t      allocated_count = 0;
+      block_index_t             block_index;
+      block_list_t              block_list;
+      block_ptr_t               current_block = nullptr;
+      uint32_t                  allocated_count = 0;
     };
 
   template <typename T>
