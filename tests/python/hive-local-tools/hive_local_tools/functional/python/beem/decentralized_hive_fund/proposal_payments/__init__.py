6d31b20acde0b76d3cdf735d266352461f97b04f
Bartek Wrona <wrona@syncad.com>
2025-03-24 14:39:00 +0100

    Using enumerate primitive instead of manual loop counter variable to solve Python linter error: SIM113 Use `enumerate()` for index variable `idx` in `for` loop

--- tests/python/hive-local-tools/hive_local_tools/functional/python/beem/decentralized_hive_fund/proposal_payments/__init__.py
+++ tests/python/hive-local-tools/hive_local_tools/functional/python/beem/decentralized_hive_fund/proposal_payments/__init__.py
@@ -10,12 +10,12 @@ def vote_proposals(node, accounts, wif):
     tt.logger.info("Voting proposals...")
 
     idx = 0
-    for account in accounts:
+    for idx, account in enumerate(accounts):
         proposal_set = [idx]
         tt.logger.info(f"Account {account['name']} voted for proposals: {','.join(str(x) for x in proposal_set)}")
         op = Update_proposal_votes(**{"voter": account["name"], "proposal_ids": proposal_set, "approve": True})
         node.finalizeOp(op, account["name"], "active")
-        idx += 1
+
     if wif is not None:
         hive_utils.debug_generate_blocks(node.rpc.url, wif, 5)
     else:
