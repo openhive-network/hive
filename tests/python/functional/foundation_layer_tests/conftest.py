f7737888dc90e1992eca7bf89d4d547fc72b069a
Michał Kudela <kudmich@syncad.com>
2025-04-15 12:43:48 +0200

    Move empty block logs fixtures to upper level conftest

--- tests/python/functional/foundation_layer_tests/conftest.py
+++ tests/python/functional/foundation_layer_tests/conftest.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-import os
-from pathlib import Path
-
 import pytest
 
 import test_tools as tt
@@ -11,22 +8,3 @@ import test_tools as tt
 @pytest.fixture()
 def wallet(node):
     return tt.Wallet(attach_to=node)
-
-
-@pytest.fixture()
-def block_logs_for_testing_location() -> Path:
-    destination_variable = os.environ.get("TESTING_BLOCK_LOGS_DESTINATION")
-    assert destination_variable is not None, "Path TESTING_BLOCK_LOGS_DESTINATION must be set!"
-    return Path(destination_variable) / "empty_block_logs"
-
-
-@pytest.fixture()
-def block_log_empty_30_mono(block_logs_for_testing_location: Path) -> tt.BlockLog:
-    block_logs_location: Path = block_logs_for_testing_location
-    return tt.BlockLog(path=block_logs_location / "empty_30", mode="monolithic")
-
-
-@pytest.fixture()
-def block_log_empty_430_split(block_logs_for_testing_location: Path) -> tt.BlockLog:
-    block_logs_location: Path = block_logs_for_testing_location
-    return tt.BlockLog(path=block_logs_location / "empty_430", mode="split")
