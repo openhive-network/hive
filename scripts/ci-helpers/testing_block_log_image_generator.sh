738020f59d645e4dde144048d4dd816f34a82c9f
Michał Kudela <kudmich@syncad.com>
2025-05-07 09:58:28 +0200

    Moved another committed block log to docker image-service

--- scripts/ci-helpers/testing_block_log_image_generator.sh
+++ scripts/ci-helpers/testing_block_log_image_generator.sh
@@ -15,7 +15,7 @@ IMGNAME=testing-block-logs
 
 echo "Attempting to get commit for: $submodule_path"
 
-CHANGES=(tests/python/functional/util/testing_block_logs/generate_testing_block_logs.py)
+CHANGES=(tests/python/functional/util/testing_block_logs/generate_testing_block_logs.py tests/python/functional/comment_cashout_tests/block_log/generate_block_log.py)
 commit=$("$SCRIPTPATH/retrieve_last_commit.sh" "${submodule_path}" "${CHANGES[@]}")
 echo "commit with last source code changes is $commit"
 
@@ -41,9 +41,19 @@ then
 else
   echo "${img} image is missing. Starting generation of testing block logs..."
   echo "Save block logs to directory: $TESTING_BLOCK_LOGS_DIR"
-  cd tests/python/functional/util/testing_block_logs
+  pushd tests/python/functional/util/testing_block_logs
   python3 generate_testing_block_logs.py --output-block-log-directory="$TESTING_BLOCK_LOGS_DIR/block_logs_for_testing"
+  popd
   echo "Block logs saved in: $TESTING_BLOCK_LOGS_DIR"
+  #tree $TESTING_BLOCK_LOGS_DIR
+  ls -R $TESTING_BLOCK_LOGS_DIR
+  pushd tests/python/functional/comment_cashout_tests/block_log
+  export LOGURU_LEVEL=INFO
+  python3 generate_block_log.py --output-block-log-directory="$TESTING_BLOCK_LOGS_DIR/block_logs_for_testing"
+  popd
+  echo "More block logs saved in: $TESTING_BLOCK_LOGS_DIR"
+  #tree $TESTING_BLOCK_LOGS_DIR
+  ls -R $TESTING_BLOCK_LOGS_DIR
 
   echo "Build a Dockerfile"
 
