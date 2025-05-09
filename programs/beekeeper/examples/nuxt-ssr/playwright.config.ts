73e0356c8d282d9dfcacbdce8bdb5bbe7f4673f5
mtyszczak <mateusz.tyszczak@gmail.com>
2025-05-09 16:47:19 +0200

    Run examples on CI

---
+++ programs/beekeeper/examples/nuxt-ssr/playwright.config.ts
@@ -0,0 +1,15 @@
+// This is a workaround for https://github.com/microsoft/playwright/issues/18282#issuecomment-1612266345
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  projects: [
+    {
+      name: "beekeeper_nuxt_vite_testsuite",
+      testDir: "./__tests__"
+    }
+  ],
+  // Run your local dev server before starting the tests
+  webServer: {
+    command: 'pnpm dev'
+  }
+});
