1d7804782ee9e25bf10933b998e06b4ec3854ce6
“Mariusz Trela” <mtrela@syncad.com>
2025-05-15 13:19:58 +0200

    In `comment` class always assign a value to a raw pointer in order to simplify a code in methods

--- libraries/chain/external_storage/comment.cpp
+++ libraries/chain/external_storage/comment.cpp
@@ -11,41 +11,43 @@ namespace hive { namespace chain {
 
   comment::comment( const std::shared_ptr<comment_object>& external ): external( external )
   {
+    if( external )
+      shm = &( *external );
   }
 
   const comment_object::id_type comment::get_id() const
   {
-    return shm ? shm->get_id() : external->get_id();
+    return shm->get_id();
   }
 
   const comment_object::author_and_permlink_hash_type& comment::get_author_and_permlink_hash() const
   {
-    return shm ? shm->get_author_and_permlink_hash() : external->get_author_and_permlink_hash();
+    return shm->get_author_and_permlink_hash();
   }
 
   const comment_id_type comment::get_parent_id() const
   {
-    return shm ? shm->get_parent_id() : external->get_parent_id();
+    return shm->get_parent_id();
   }
 
   const bool comment::is_root() const
   {
-    return shm ? shm->is_root() : external->is_root();
+    return shm->is_root();
   }
 
   const uint16_t comment::get_depth() const
   {
-    return shm ? shm->get_depth() : external->get_depth();
+    return shm->get_depth();
   }
 
   comment::operator bool() const
   {
-    return shm || external;
+    return shm;
   }
 
   const comment_object& comment::operator*() const
   {
-    return shm ? ( *shm ) : ( *external );
+    return *shm;
   }
 
 } } // hive::chain
