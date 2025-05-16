b193f6da9e6dd23b3a9eb89129e2ea7941c22a07
Michał Kudela <kudmich@syncad.com>
2025-05-16 14:05:12 +0200

    wip

--- scripts/ci-helpers/testing_block_log_image_generator.sh
+++ scripts/ci-helpers/testing_block_log_image_generator.sh
@@ -17,11 +17,11 @@ echo "Attempting to get commit for: $submodule_path"
 
 CHANGES=(
   tests/python/functional/util/testing_block_logs/generate_testing_block_logs.py
-  tests/python/functional/comment_cashout_tests/block_log/generate_block_log.py
+  # tests/python/functional/comment_cashout_tests/block_log/generate_block_log.py
   tests/python/functional/datagen_tests/recalculation_proposal_vote_tests/block_log/generate_block_log.py
-  tests/python/functional/datagen_tests/recurrent_transfer_tests/block_logs/block_log_containing_many_to_one_recurrent_transfers/generate_block_log.py
-  tests/python/functional/datagen_tests/recurrent_transfer_tests/block_logs/block_log_recurrent_transfer_everyone_to_everyone/generate_block_log.py
-  tests/python/functional/datagen_tests/transaction_status_api_tests/block_log/generate_block_log.py
+  # tests/python/functional/datagen_tests/recurrent_transfer_tests/block_logs/block_log_containing_many_to_one_recurrent_transfers/generate_block_log.py
+  # tests/python/functional/datagen_tests/recurrent_transfer_tests/block_logs/block_log_recurrent_transfer_everyone_to_everyone/generate_block_log.py
+  # tests/python/functional/datagen_tests/transaction_status_api_tests/block_log/generate_block_log.py
 )
 commit=$("$SCRIPTPATH/retrieve_last_commit.sh" "${submodule_path}" "${CHANGES[@]}")
 echo "commit with last source code changes is $commit"
@@ -53,21 +53,21 @@ else
   pushd tests/python/functional/util/testing_block_logs
   python3 generate_testing_block_logs.py $OUTPUT_DIR_PARAMETER
   popd
-  pushd tests/python/functional/comment_cashout_tests/block_log
-  python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
-  popd
+  # pushd tests/python/functional/comment_cashout_tests/block_log
+  # python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
+  # popd
   pushd tests/python/functional/datagen_tests/recalculation_proposal_vote_tests/block_log
   python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
   popd
-  pushd tests/python/functional/datagen_tests/recurrent_transfer_tests/block_logs/block_log_containing_many_to_one_recurrent_transfers
-  python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
-  popd
-  pushd tests/python/functional/datagen_tests/recurrent_transfer_tests/block_logs/block_log_recurrent_transfer_everyone_to_everyone
-  python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
-  popd
-  pushd tests/python/functional/datagen_tests/transaction_status_api_tests/block_log
-  python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
-  popd
+  # pushd tests/python/functional/datagen_tests/recurrent_transfer_tests/block_logs/block_log_containing_many_to_one_recurrent_transfers
+  # python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
+  # popd
+  # pushd tests/python/functional/datagen_tests/recurrent_transfer_tests/block_logs/block_log_recurrent_transfer_everyone_to_everyone
+  # python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
+  # popd
+  # pushd tests/python/functional/datagen_tests/transaction_status_api_tests/block_log
+  # python3 generate_block_log.py $OUTPUT_DIR_PARAMETER
+  # popd
   echo "Block logs saved in: $TESTING_BLOCK_LOGS_DIR"
   #tree $TESTING_BLOCK_LOGS_DIR
   ls -R $TESTING_BLOCK_LOGS_DIR
