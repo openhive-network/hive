7c0bae8e569076468a280c05fced540aab0bcd6c
Radek Masłowski <rmaslowski@syncad.com>
2025-05-19 07:17:13 +0000

    Small modification only for tests

--- tests/python/api_tests/message_format_tests/condenser_api_tests/test_get_required_signatures.py
+++ tests/python/api_tests/message_format_tests/condenser_api_tests/test_get_required_signatures.py
@@ -8,15 +8,35 @@ from hive_local_tools import run_for
 def test_get_required_signatures_in_testnet(node: tt.InitNode) -> None:
     wallet = tt.OldWallet(attach_to=node)
     transaction = wallet.api.create_account("initminer", "alice", "{}")
-    keys = node.api.condenser.get_required_signatures(transaction, [tt.Account("initminer").public_key])
-    assert len(keys) != 0
-
-
-@run_for("mainnet_5m", "live_mainnet")
-def test_get_required_signatures_in_mainnet(node: tt.RemoteNode) -> None:
-    block = node.api.condenser.get_block(3652254)
-    account = node.api.wallet_bridge.get_accounts(["steem"])[0]
-    transaction = block.transactions[3]
+    block = node.api.condenser.get_block(transaction["block_num"])
+    account = node.api.wallet_bridge.get_accounts(["initminer"])[0]
+    transaction2 = block.transactions[0]
     active_key = account.active.key_auths[0][0]
-    keys = node.api.condenser.get_required_signatures(transaction, [active_key])
+    keys = node.api.condenser.get_required_signatures(transaction2, [active_key])
     assert len(keys) != 0
+
+
+
+#     keys = node.api.condenser.get_required_signatures(transaction, [tt.Account("initminer").public_key])
+#     assert len(keys) != 0
+
+
+# @run_for("mainnet_5m", "live_mainnet")
+# def test_get_required_signatures_in_mainnet(node: tt.RemoteNode) -> None:
+#     block = node.api.condenser.get_block(3652254)
+#     account = node.api.wallet_bridge.get_accounts(["steem"])[0]
+#     transaction = block.transactions[3]
+#     active_key = account.active.key_auths[0][0]
+#     keys = node.api.condenser.get_required_signatures(transaction, [active_key])
+#     assert len(keys) != 0
+
+
+
+# @run_for("mainnet_5m", "live_mainnet")
+# def test_get_required_signatures_in_mainnet(node: tt.RemoteNode) -> None:
+#     block = node.api.condenser.get_block(3652254)
+#     account = node.api.wallet_bridge.get_accounts(["steem"])[0]
+#     transaction = block.transactions[3]
+#     active_key = account.active.key_auths[0][0]
+#     keys = node.api.condenser.get_required_signatures(transaction, [active_key])
+#     assert len(keys) != 0
