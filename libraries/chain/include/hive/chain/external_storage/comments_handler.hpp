8a55ea9a173ffba4e19c5ea8da22aebf0910cd43
“Mariusz Trela” <mtrela@syncad.com>
2025-05-21 14:34:39 +0200

    In `comments_handler` interface only one `get_comment` method is required.

--- libraries/chain/include/hive/chain/external_storage/comments_handler.hpp
+++ libraries/chain/include/hive/chain/external_storage/comments_handler.hpp
@@ -17,7 +17,6 @@ class comments_handler
     virtual void allow_move_to_external_storage( const comment_id_type& comment_id, const account_id_type& account_id, const std::string& permlink ) = 0;
 
     virtual comment get_comment( const account_id_type& author, const std::string& permlink, bool comment_is_required ) const = 0;
-    virtual comment get_comment( const account_name_type& author, const std::string& permlink, bool comment_is_required ) const = 0;
 
 };
 
