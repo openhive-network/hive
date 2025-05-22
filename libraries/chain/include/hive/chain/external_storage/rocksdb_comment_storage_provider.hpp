7753a2e63cf8b8c7e3db9dbf3be71f859509dd97
“Mariusz Trela” <mtrela@syncad.com>
2025-05-22 11:01:58 +0200

    Put `rocksdb_storage_provider` and `rocksdb_comment_storage_provider` classes into different files

---
+++ libraries/chain/include/hive/chain/external_storage/rocksdb_comment_storage_provider.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <hive/chain/external_storage/external_storage_provider.hpp>
+#include <hive/chain/external_storage/rocksdb_storage_provider.hpp>
+
+#include <hive/chain/database.hpp>
+
+#include <rocksdb/options.h>
+#include <rocksdb/slice.h>
+
+#include <boost/core/demangle.hpp>
+#include <boost/filesystem/path.hpp>
+
+#include <fc/crypto/ripemd160.hpp>
+#include <fc/filesystem.hpp>
+
+namespace hive { namespace chain {
+
+class rocksdb_comment_storage_provider: public rocksdb_storage_provider, public external_comment_storage_provider
+{
+  private:
+
+    WriteBatch _writeBuffer;
+
+    void loadAdditionalData() override{}
+    ColumnDefinitions prepareColumnDefinitions(bool addDefaultColumn) override;
+
+    WriteBatch& getWriteBuffer() override;
+
+  public:
+
+    using ptr = std::shared_ptr<rocksdb_comment_storage_provider>;
+
+    rocksdb_comment_storage_provider( const bfs::path& blockchain_storage_path, const bfs::path& storage_path, appbase::application& app );
+    ~rocksdb_comment_storage_provider() override{}
+
+    std::unique_ptr<DB>& getStorage() override;
+
+    void openDb( bool cleanDatabase ) override;
+    void shutdownDb( bool removeDB = false ) override;
+
+    void save( const Slice& key, const Slice& value ) override;
+    bool read( const Slice& key, PinnableSlice& value ) override;
+    void flush() override;
+};
+
+}}
+
