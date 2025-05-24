0bbdd0ed38721cfec1f8b7ef51dfa78f4cbd1e02
Dan Notestein <dan@syncad.com>
2025-05-24 18:12:17 -0400

    add new memory_ops.cpp

---
+++ libraries/chainbase/src/memory_ops.cpp
@@ -0,0 +1,12 @@
+#include <chainbase/memory_ops.hpp>
+
+namespace chainbase {
+
+// Initialize static member variables
+long vm_dirty_params::dirty_bytes = 0;
+long vm_dirty_params::dirty_background_bytes = 0;
+long vm_dirty_params::dirty_expire_centisecs = 0;
+long vm_dirty_params::swappiness = 0;
+bool vm_dirty_params::values_saved = false;
+
+} // namespace chainbase
