a994c4a6806c3a3ccc10abd9ada02603b1f5fe98
Mariusz Trela <mtrela@syncad.com>
2025-06-30 15:45:39 +0200

    Embed `recurrent_transfer_extension_visitor` in `recurrent_transfer_operation` class

--- libraries/protocol/include/hive/protocol/hive_operations.hpp
+++ libraries/protocol/include/hive/protocol/hive_operations.hpp
@@ -1075,6 +1075,22 @@ namespace hive { namespace protocol {
     */
   struct recurrent_transfer_operation : public base_operation
   {
+    struct recurrent_transfer_extension_visitor
+    {
+      uint8_t pair_id = 0; // default recurrent transfer id is 0
+      bool was_pair_id = false;
+
+      typedef void result_type;
+
+      void operator()( const recurrent_transfer_pair_id& recurrent_transfer_pair_id )
+      {
+        was_pair_id = true;
+        pair_id = recurrent_transfer_pair_id.pair_id;
+      }
+
+      void operator()( const hive::void_t& ) {}
+    };
+
     account_name_type from;
     /// Account to transfer asset to
     account_name_type to;
