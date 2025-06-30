8ab354b9cceceef48796864e9688e513d72e7d86
Mariusz Trela <mtrela@syncad.com>
2025-06-30 15:39:46 +0200

    Refactor `recurrent_transfer_extension_visitor` - check a hardfork when it's really necessary

--- libraries/chain/hive_evaluator.cpp
+++ libraries/chain/hive_evaluator.cpp
@@ -3316,10 +3316,13 @@ void recurrent_transfer_evaluator::do_apply( const recurrent_transfer_operation&
   asset available = _db.get_balance( from_account, op.amount.symbol );
   FC_ASSERT( available >= op.amount, "Account does not have enough tokens for the first transfer, has ${has} needs ${needs}", ("has",  available)("needs", op.amount) );
 
-  recurrent_transfer_extension_visitor vtor( _db );
+  recurrent_transfer_extension_visitor vtor;
   for( const auto& e : op.extensions )
     e.visit( vtor );
   uint8_t rtp_id = vtor.pair_id;
+
+  FC_ASSERT( !vtor.was_pair_id || ( vtor.was_pair_id && _db.has_hardfork( HIVE_HARDFORK_1_28 ) ),
+    "recurrent_transfer_pair_id extension requires hardfork ${hf}", ( "hf", HIVE_HARDFORK_1_28 ) );
 
   const auto& rt_idx = _db.get_index< recurrent_transfer_index, by_from_to_id >();
   auto itr = rt_idx.find( boost::make_tuple( from_account.get_id(), to_account.get_id(), rtp_id ) );
--- libraries/chain/include/hive/chain/util/recurrent_transfer_extension_visitor.hpp
+++ libraries/chain/include/hive/chain/util/recurrent_transfer_extension_visitor.hpp
@@ -8,17 +8,14 @@ namespace hive { namespace chain {
 
 struct recurrent_transfer_extension_visitor
 {
-  recurrent_transfer_extension_visitor( database& db ) : _db( db ) {}
-
-  database& _db;
   uint8_t pair_id = 0; // default recurrent transfer id is 0
+  bool was_pair_id = false;
 
   typedef void result_type;
 
   void operator()( const recurrent_transfer_pair_id& recurrent_transfer_pair_id )
   {
-    FC_ASSERT( _db.has_hardfork( HIVE_HARDFORK_1_28 ), "recurrent_transfer_pair_id extension requires hardfork ${hf}",
-      ( "hf", HIVE_HARDFORK_1_28 ) );
+    was_pair_id = true;
     pair_id = recurrent_transfer_pair_id.pair_id;
   }
 
