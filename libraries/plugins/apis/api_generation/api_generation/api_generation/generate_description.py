b062fef007e2ecee3f23c3f17a6b90f007e49d42
Bartek Wrona <wrona@syncad.com>
2025-07-08 23:46:41 +0200

    generate_client and generate_description API generation helper functions take base_directory to simplify output path evaluation.

--- libraries/plugins/apis/api_generation/api_generation/api_generation/generate_description.py
+++ libraries/plugins/apis/api_generation/api_generation/api_generation/generate_description.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
     from api_generation.common import AvailableApis
 
 
-def generate_description(api: AvailableApis, base_directory: Path) -> None:
+def generate_description(api: AvailableApis, base_directory: Path) -> Path:
     apis_to_skip = available_apis.copy()
     apis_to_skip.remove(api)
 
@@ -21,9 +21,13 @@ def generate_description(api: AvailableApis, base_directory: Path) -> None:
 
     print(f"Attempting to process {api} from Swagger file: {openapi_json_path}")
 
+    output_path = api_generation_path / api / api / f"{api}_description.py"
+
     generate_json_rpc_api_description(
         api_description_dict_name,
         openapi_json_path,
-        api_generation_path / api / api / f"{api}_description.py",
+        output_path,
         apis_to_skip=apis_to_skip,
     )
+
+    return output_path
