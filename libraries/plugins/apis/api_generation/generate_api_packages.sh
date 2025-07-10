8101ff8a420076021dd62940a938816288084a1d
Jakub Ziebinski <jziebinski@gmail.com>
2025-07-10 09:28:50 +0000

    Generate the whole api packages

--- libraries/plugins/apis/api_generation/generate_api_packages.sh
+++ libraries/plugins/apis/api_generation/generate_api_packages.sh
@@ -3,20 +3,15 @@
 set -xeuo pipefail
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
 BASE_DIR="${SCRIPTPATH}"
 
-API_SPEC=${1:-"*_api"}
+API_GENERATION_PACKAGE_DIR="${BASE_DIR}/api_generation"
 
-poetry -vv install
+API_LIST=("database_api" "block_api" "rc_api" "network_broadcast_api" "account_by_key_api")
 
-for d in ${API_SPEC}; do
+for d in "${API_LIST[@]}"; do
   echo "Processing API: $d"
 
-  poetry run python "${BASE_DIR}"/api_generation/generate_api_definitions.py "$d" "${BASE_DIR}"
-
-  pushd "$d"
-  poetry build
-  popd
+  poetry run -C "${API_GENERATION_PACKAGE_DIR}" python  "${BASE_DIR}"/api_generation/generate_api_definitions.py "$d" "${BASE_DIR}"
 
 done
