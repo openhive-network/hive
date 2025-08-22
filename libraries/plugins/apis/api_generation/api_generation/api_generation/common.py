b014d3542992c15add1b1d1cd27504fb81aed5c5
Jakub Ziebinski <jziebinski@gmail.com>
2025-08-22 09:20:48 +0000

    Customise the Python API client generator to accept the bridge and account history api

--- libraries/plugins/apis/api_generation/api_generation/api_generation/common.py
+++ libraries/plugins/apis/api_generation/api_generation/api_generation/common.py
@@ -2,11 +2,13 @@ from __future__ import annotations
 
 from typing import Literal, Final
 
-AvailableApis = Literal["block_api", "database_api", "rc_api", "network_broadcast_api", "account_by_key_api",]
+AvailableApis = Literal["block_api", "database_api", "rc_api", "network_broadcast_api", "account_by_key_api", "account_history_api", "bridge"]
 available_apis: Final[list[AvailableApis]] = [
     "block_api",
     "database_api",
     "rc_api",
     "network_broadcast_api",
     "account_by_key_api",
+    "account_history_api",
+    "bridge",
 ]
