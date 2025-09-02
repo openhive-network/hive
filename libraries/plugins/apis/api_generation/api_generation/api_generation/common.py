2bb5d8c125ea340bbfa44eeba9441c3371e77cd4
Jakub Ziebinski <jziebinski@gmail.com>
2025-09-02 12:36:35 +0000

    WIP

--- libraries/plugins/apis/api_generation/api_generation/api_generation/common.py
+++ libraries/plugins/apis/api_generation/api_generation/api_generation/common.py
@@ -2,8 +2,31 @@ from __future__ import annotations
 
 from typing import Literal, Final
 
-AvailableApis = Literal["block_api", "database_api", "rc_api", "network_broadcast_api", "account_by_key_api", "account_history_api", "bridge"]
+AvailableApis = Literal[
+"condenser_api",
+"witness_api",
+"wallet_bridge_api",
+"transaction_status_api",
+"bridge",
+"block_api",
+"database_api",
+"rc_api",
+"network_broadcast_api",
+"account_by_key_api",
+"account_history_api",
+"tags_api",
+"rewards_api",
+"reputation_api",
+"market_history_api",
+"follow_api",
+"debug_node_api",
+]
+
 available_apis: Final[list[AvailableApis]] = [
+    "condenser_api",
+    "witness_api",
+    "wallet_bridge_api",
+    "transaction_status_api",
     "block_api",
     "database_api",
     "rc_api",
@@ -11,5 +34,10 @@ available_apis: Final[list[AvailableApis]] = [
     "account_by_key_api",
     "account_history_api",
     "bridge",
+    "tags_api",
+    "rewards_api",
+    "reputation_api",
+    "market_history_api",
+    "follow_api",
+    "debug_node_api",
 ]
-apis_to_skip: Final[list[str]] = ["bridge"]
