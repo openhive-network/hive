86294bd804a2634fe232cef25fb3f5afaf8f6aed
Bartek Wrona <wrona@syncad.com>
2025-09-30 18:26:20 +0200

    Added description of ARM toolchain build procedure and hived building steps targeting ARM.

--- scripts/build_arm.sh
+++ scripts/build_arm.sh
@@ -3,7 +3,14 @@ set -x
 set -e
 set -o pipefail
 
-export CROSS_ROOT=/storage1/src/buildroot/output/host
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+SRC_DIR="${SCRIPTPATH}/.."
+BUILD_DIR="${SRC_DIR}/build_arm/"
+
+mkdir -vp "${BUILD_DIR}"
+
+export CROSS_ROOT=${CROSS_ROOT:-${SRC_DIR}/../buildroot/output/host}
 export CROSS_TRIPPLE=aarch64-buildroot-linux-gnu
 export ARCH=aarch64
 export CROSS_COMPILE=${CROSS_TRIPPLE}
@@ -18,4 +25,4 @@ export LD=${CROSS_ROOT}/bin/${CROSS_TRIPPLE}-ld
 export QEMU_LD_PREFIX="${CROSS_ROOT}/${CROSS_TRIPPLE}/sysroot"
 export QEMU_SET_ENV="LD_LIBRARY_PATH=${CROSS_ROOT}/lib:${QEMU_LD_PREFIX}"
 
-cmake --toolchain ${CROSS_ROOT}/Toolchain.cmake -B build -S. -GNinja && cmake --build build "$@"
+cmake --toolchain ${CROSS_ROOT}/Toolchain.cmake -B "${BUILD_DIR}" -S "${SRC_DIR}" -GNinja 2>&1 | tee "${BUILD_DIR}/cmake.log" && cmake --build "${BUILD_DIR}" --target hived 2>&1 | tee "${BUILD_DIR}/build.log"
