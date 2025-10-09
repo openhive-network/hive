dd9afb1981e8139e222d1a19eafb23e096b4c5fa
Bartek Wrona <wrona@syncad.com>
2025-10-09 10:59:39 +0200

    Added buildroot .config file to define buildroot environment. It uses slightly reduced kernel and glibc dependencies to allow execution on older version of Raspian OS. Also added configuration to build 3rd party libs (boost/openssl/snappy).

---
+++ doc/aarch64/Toolchain.cmake
@@ -0,0 +1,21 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CROSS_TRIPPLE aarch64-buildroot-linux-gnu)
+set(CROSS_ROOT /storage1/src/buildroot/output/host)
+
+set(CMAKE_C_COMPILER ${CROSS_ROOT}/bin/aarch64-buildroot-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${CROSS_ROOT}/bin/aarch64-buildroot-linux-gnu-g++)
+
+set(CMAKE_CXX_FLAGS "-I ${CROSS_ROOT}/include/")
+
+list(APPEND CMAKE_FIND_ROOT_PATH ${CMAKE_PREFIX_PATH} ${CROSS_ROOT} ${CROSS_ROOT}/${CROSS_TRIPPLE})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_SYSROOT ${CROSS_ROOT}/${CROSS_TRIPPLE}/sysroot)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-aarch64)
+
