234faf2aceefb44a67f65120e92fc8311f60eb19
Michał Kudela <kudmich@syncad.com>
2025-10-29 16:25:03 +0100

    Increase test coverage for `account authority update` functionality

--- tests/python/functional/operation_tests/account_update_tests/conftest.py
+++ tests/python/functional/operation_tests/account_update_tests/conftest.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pytest
 
 import test_tools as tt
 from hive_local_tools.functional.python.operation import create_account_with_different_keys
 from python.functional.operation_tests.conftest import UpdateAccount
+from wax import WaxChainOptions, create_hive_chain
+
+if TYPE_CHECKING:
+    from wax.interfaces import IHiveChainInterface
 
 
 @pytest.fixture()
@@ -15,3 +21,47 @@ def alice(prepared_node: tt.InitNode, wallet: tt.Wallet) -> UpdateAccount:
     create_account_with_different_keys(wallet, "alice", "initminer")
     wallet.api.transfer_to_vesting("initminer", "alice", tt.Asset.Test(50), broadcast=True)
     return UpdateAccount("alice", prepared_node, wallet)
+
+
+@pytest.fixture()
+def owner() -> tt.Account:
+    return tt.Account("alice", secret="owner")
+
+
+@pytest.fixture()
+def active() -> tt.Account:
+    return tt.Account("alice", secret="active")
+
+
+@pytest.fixture()
+def posting() -> tt.Account:
+    return tt.Account("alice", secret="posting")
+
+
+@pytest.fixture()
+def node() -> tt.InitNode:
+    init_node = tt.InitNode()
+    init_node.run()
+    return init_node
+
+
+@pytest.fixture()
+def chain(node) -> IHiveChainInterface:
+    return create_hive_chain(WaxChainOptions(endpoint_url=node.http_endpoint))
+
+
+@pytest.fixture()
+def alice_wallet(node: tt.InitNode, posting, active, owner) -> tt.Wallet:
+    wallet = tt.Wallet(attach_to=node)
+    wallet.api.create_account_with_keys(
+        "initminer",
+        "alice",
+        "",
+        posting=posting.public_key,
+        active=active.public_key,
+        owner=owner.public_key,
+        memo=tt.Account("alice", secret="memo").public_key,
+    )
+    wallet.api.transfer_to_vesting("initminer", "alice", tt.Asset.Test(500))
+
+    return wallet
