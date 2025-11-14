3f53edab4fea57528b33aec2a2bfd3aa8a20bcb6
Fernando Henrique Soder <fhsoder@gmail.com>
2025-11-14 15:26:20 -0300

    Initial changes in hive_options and webserver_plugins.cpp for the API to be able to listen IPv6

--- libraries/plugins/webserver/webserver_plugin.cpp
+++ libraries/plugins/webserver/webserver_plugin.cpp
@@ -787,8 +787,11 @@ void webserver_plugin::plugin_initialize( const variables_map& options )
 
     FC_ASSERT( endpoints.size(), "${http-endpoint-type} ${hostname} did not resolve",
               ("http-endpoint-type", my->tls ? "webserver-https-endpoint" : "webserver-http-endpoint")("hostname", _http_or_https_endpoint) );
-
+#ifndef ENABLE_IPV6
     my->http_endpoint = tcp::endpoint( boost::asio::ip::address_v4::from_string( ( string )endpoints[0].get_address() ), endpoints[0].port() );
+#else
+    my->http_endpoint = tcp::endpoint( boost::asio::ip::make_address( ( string )endpoints[0].get_address() ), endpoints[0].port() );
+#endif
     ilog( "configured ${type} to listen on ${ep}", ("type", my->tls ? "https" : "http")("ep", endpoints[0]) );
   }
 
@@ -805,7 +808,11 @@ void webserver_plugin::plugin_initialize( const variables_map& options )
     auto ws_endpoint = options.at( "webserver-ws-endpoint" ).as< string >();
     auto endpoints = fc::resolve_string_to_ip_endpoints( ws_endpoint );
     FC_ASSERT( endpoints.size(), "ws-server-endpoint ${hostname} did not resolve", ("hostname", ws_endpoint) );
+#ifndef ENABLE_IPV6
     my->ws_endpoint = tcp::endpoint( boost::asio::ip::address_v4::from_string( ( string )endpoints[0].get_address() ), endpoints[0].port() );
+#else
+    my->ws_endpoint = tcp::endpoint( boost::asio::ip::make_address( ( string )endpoints[0].get_address() ), endpoints[0].port() );
+#endif
     ilog( "configured ws to listen on ${ep}", ("ep", endpoints[0]) );
   }
 }
