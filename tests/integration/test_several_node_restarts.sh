01aee29378b846efd0489e65ad7b6d909b27864d
Michał Kudela <kudmich@syncad.com>
2025-11-21 13:26:33 +0100

    Move test helper functions to one common place

--- tests/integration/test_several_node_restarts.sh
+++ tests/integration/test_several_node_restarts.sh
@@ -2,28 +2,12 @@
 
 set -xeuo pipefail
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+source "$SCRIPTPATH/conftest.sh"
+
 SOURCE_DATA_DIR=${1}
 first_replay_block_limit=${2}
 second_replay_block_limit=${3}
-
-wait_for_instance() {
-  local container_name=${1}
-  local number_of_blocks_to_replay=${2}
-  local LIMIT=300
-
-  echo "Waiting for instance hosted by container: ${container_name}."
-
-  echo "Querying for service status..."
-  docker logs "${container_name}"
-  # docker container exec -t "${container_name}" timeout $LIMIT bash -c "until curl --max-time 30  --data '{\"jsonrpc\": \"2.0\",\"method\": \"database_api.get_dynamic_global_properties\",\"id\": 1}' localhost:8091  | grep -o '\"head_block_number\":${number_of_blocks_to_replay},'; do sleep 3 ; done"
-  docker container exec -t "${container_name}" timeout $LIMIT bash -c "until wget -O - -qnv --timeout=30  --post-data='{\"jsonrpc\": \"2.0\",\"method\": \"database_api.get_dynamic_global_properties\",\"id\": 1}' localhost:8091  | grep -o '\"head_block_number\":${number_of_blocks_to_replay},'; do sleep 3 ; done"
-}
-
-stop_instance() {
-  local container_name="$1"
-  docker stop "$container_name"
-  docker wait "$container_name" || true
-}
 
 mkdir -p "$SOURCE_DATA_DIR"
 
