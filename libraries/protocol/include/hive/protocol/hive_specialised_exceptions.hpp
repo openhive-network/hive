d52bfcfa4686f11969e5e5b0e82444b3b6cd27d1
kmochocki <kmochocki@syncad.com>
2025-11-23 22:47:10 +0000

    Introduce specialised assertions
    Specialised assertions contains category, subject and subject type

---
+++ libraries/protocol/include/hive/protocol/hive_specialised_exceptions.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <fc/exception/exception.hpp>
+
+#define _HA_GET_FORMAT( FORMAT, ...) FORMAT
+#define _HA_GET_VARGS( FORMAT, ...) __VA_ARGS__
+
+#define HIVE_SPECIALISED_ASSERT( expr, category, subject_type, expr_str, ... )                        \
+  FC_EXPAND_MACRO(                                                                               \
+    FC_MULTILINE_MACRO_BEGIN                                                                     \
+      if( UNLIKELY(!(expr)) )                                                                    \
+      {                                                                                          \
+        FC_ASSERT_EXCEPTION_DECL(                                                                \
+          expr_str,                                                                              \
+          fc::assert_exception,                                                                  \
+          _HA_GET_FORMAT( "" __VA_ARGS__ ),                                                      \
+          _HA_GET_VARGS( "" __VA_ARGS__ )("category", category )("subject_type", subject_type )  \
+        );                                                                                       \
+        if( fc::enable_record_assert_trip )                                                      \
+          FC_RECORD_ASSERT( __e__ );                                                             \
+        throw __e__;                                                                             \
+      }                                                                                          \
+    FC_MULTILINE_MACRO_END                                                                       \
+  )
+
+#define HIVE_SPECIALISED_ASSERT_WITH_SUBJECT( expr, category, subject_type, subject, ... )                  \
+  FC_EXPAND_MACRO(                                                                                                    \
+    FC_MULTILINE_MACRO_BEGIN                                                                                          \
+      if( UNLIKELY(!(expr)) )                                                                                         \
+      {                                                                                                               \
+        FC_ASSERT_EXCEPTION_DECL(                                                                                     \
+          #expr,                                                                                                   \
+          fc::assert_exception,                                                                                       \
+          _HA_GET_FORMAT( "" __VA_ARGS__ ),                                                                           \
+          _HA_GET_VARGS( "" __VA_ARGS__ )("category", category )("subject_type", subject_type )("subject", subject)   \
+        );                                                                                                            \
+        if( fc::enable_record_assert_trip )                                                                           \
+          FC_RECORD_ASSERT( __e__ );                                                                                  \
+        throw __e__;                                                                                                  \
+      }                                                                                                               \
+    FC_MULTILINE_MACRO_END                                                                                            \
+  )
+
+
+#define HIVE_PROTOCOL_ASSET_ASSERT( expr, ... )           HIVE_SPECIALISED_ASSERT( expr, "protocol", "asset",      #expr, __VA_ARGS__ );
+#define HIVE_PROTOCOL_AUTHORITY_ASSERT( expr, ... )       HIVE_SPECIALISED_ASSERT( expr, "protocol", "authority",  #expr, __VA_ARGS__ );
+#define HIVE_PROTOCOL_OPERATIONS_ASSERT( expr, ... )      HIVE_SPECIALISED_ASSERT( expr, "protocol", "operation",  #expr, __VA_ARGS__ );
+#define HIVE_PROTOCOL_TRANSACTION_ASSERT( expr, ... )     HIVE_SPECIALISED_ASSERT( expr, "protocol", "transaction",  #expr, __VA_ARGS__ );
+#define HIVE_PROTOCOL_DHF_OPERATIONS_ASSERT( expr, ... )  HIVE_SPECIALISED_ASSERT( expr, "protocol", "dhf_operation",  #expr, __VA_ARGS__ );
+#define HIVE_PROTOCOL_VALIDATION_ASSERT( expr, ... )      HIVE_SPECIALISED_ASSERT( expr, "protocol", "any", #expr, __VA_ARGS__ );
+#define HIVE_PROTOCOL_PERMLINK_ASSERT( expr, ... )        HIVE_SPECIALISED_ASSERT( expr, "protocol", "permlink",  #expr, __VA_ARGS__ );
+#define HIVE_PROTOCOL_ACCOUNT_NAME_ASSERT( expr, ... )    HIVE_SPECIALISED_ASSERT( expr, "protocol", "account_name", #expr, __VA_ARGS__ );
+#define HIVE_PROTOCOL_HARDFORK_ASSERT( expr, ... )        HIVE_SPECIALISED_ASSERT( expr, "protocol", "hardfork",   #expr, __VA_ARGS__ );
