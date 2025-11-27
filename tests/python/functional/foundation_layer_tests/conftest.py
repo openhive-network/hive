0d1748e850c3e3e9f97a7b17bf1cac59a6fe9aad
Michał Kudela <kudmich@syncad.com>
2025-11-27 12:21:49 +0100

    Create test fixture - `snapshot_430_blocks`

--- tests/python/functional/foundation_layer_tests/conftest.py
+++ tests/python/functional/foundation_layer_tests/conftest.py
@@ -8,3 +8,11 @@ import test_tools as tt
 @pytest.fixture()
 def wallet(node):
     return tt.Wallet(attach_to=node)
+
+
+@pytest.fixture()
+def snapshot_430_blocks(block_log_empty_430_split: tt.BlockLog) -> tt.Snapshot:
+    node = tt.InitNode()
+    node.run(replay_from=block_log_empty_430_split, stop_at_block=430)
+    snapshot = node.dump_snapshot(close=True)
+    return snapshot
