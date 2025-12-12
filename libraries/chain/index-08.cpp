59ade967a7257bbe8bae4a4bb6d9d190ec6ccdb0
Bartek Wrona <bwrona@syncad.com>
2025-12-12 18:23:16 +0000

    Move escrow_object operations from database.cpp to index-08.cpp
    Extract escrow-related methods (get_escrow, find_escrow, expire_escrow_ratification,
    remove_pending_escrows, get_escrow_totals) to index-08.cpp where escrow_index is
    instantiated. This allows removing escrow_object_multiindex.hpp include from
    database.cpp, reducing template instantiation overhead.
    
    Build time: 4m25.062s (baseline: 4m26.042s)

--- libraries/chain/index-08.cpp
+++ libraries/chain/index-08.cpp
@@ -3,10 +3,13 @@
 #include <hive/chain/detail/state/decline_voting_rights_request_object_multiindex.hpp>
 #include <hive/chain/detail/state/limit_order_object_multiindex.hpp>
 
+#include <hive/chain/account_object_multiindex.hpp>
 #include <hive/chain/index.hpp>
 #include <chainbase/chainbase.inl>
 
 #include <hive/chain/util/type_registrar_definition.hpp>
+
+#include <hive/protocol/hive_operations.hpp>
 
 namespace hive { namespace chain {
 
@@ -17,6 +20,18 @@ void initialize_core_indexes_08( database& db )
   HIVE_ADD_CORE_INDEX(db, decline_voting_rights_request_index);
   HIVE_ADD_CORE_INDEX(db, limit_order_index);
 
+}
+
+const escrow_object& database::get_escrow( const account_name_type& name, uint32_t escrow_id )const
+{ try {
+  const auto* _escrow = find_escrow( name, escrow_id );
+  FC_ASSERT( _escrow != nullptr, "Escrow balance with 'name' ${name} 'escrow_id' ${escrow_id} doesn't exist.", (name)(escrow_id) );
+  return *_escrow;
+} FC_CAPTURE_AND_RETHROW( (name)(escrow_id) ) }
+
+const escrow_object* database::find_escrow( const account_name_type& name, uint32_t escrow_id )const
+{
+  return find< escrow_object, by_from_id >( boost::make_tuple( name, escrow_id ) );
 }
 
 const savings_withdraw_object* database::find_savings_withdraw( const account_name_type& owner, uint32_t request_id )const
@@ -32,6 +47,74 @@ const limit_order_object* database::find_limit_order( const account_name_type& n
   return find< limit_order_object, by_account >( boost::make_tuple( name, orderid ) );
 }
 
+void database::expire_escrow_ratification()
+{
+  const auto& escrow_idx = get_index< escrow_index >().indices().get< by_ratification_deadline >();
+  auto escrow_itr = escrow_idx.lower_bound( false );
+
+  while( escrow_itr != escrow_idx.end() && !escrow_itr->is_approved() && escrow_itr->ratification_deadline <= head_block_time() )
+  {
+    const auto& old_escrow = *escrow_itr;
+    ++escrow_itr;
+
+    adjust_balance( old_escrow.from, old_escrow.get_hive_balance() );
+    adjust_balance( old_escrow.from, old_escrow.get_hbd_balance() );
+    adjust_balance( old_escrow.from, old_escrow.get_fee() );
+
+    push_virtual_operation( escrow_rejected_operation( old_escrow.from, old_escrow.to, old_escrow.agent, old_escrow.escrow_id,
+      old_escrow.get_hbd_balance(), old_escrow.get_hive_balance(), old_escrow.get_fee() ) );
+
+    modify( get_account( old_escrow.from ), []( account_object& a )
+    {
+      a.pending_escrow_transfers--;
+    } );
+
+    remove( old_escrow );
+  }
+}
+
+void database::remove_pending_escrows( const account_object& account, const account_name_type& account_name )
+{
+  // Remove pending escrows (return balance to account - compare with expire_escrow_ratification())
+  const auto& escrow_idx = get_index< escrow_index, by_from_id >();
+  auto escrow_itr = escrow_idx.lower_bound( account_name );
+  while( escrow_itr != escrow_idx.end() && escrow_itr->from == account_name )
+  {
+    auto& escrow = *escrow_itr;
+    ++escrow_itr;
+
+    adjust_balance( account, escrow.get_hive_balance() );
+    adjust_balance( account, escrow.get_hbd_balance() );
+    adjust_balance( account, escrow.get_fee() );
+
+    modify( account, []( account_object& a )
+    {
+      a.pending_escrow_transfers--;
+    } );
+
+    remove( escrow );
+  }
+}
+
+void database::get_escrow_totals( asset& total_hive, asset& total_hbd, uint64_t& escrow_count ) const
+{
+  const auto& escrow_idx = get_index< escrow_index >().indices().get< by_id >();
+
+  for( auto itr = escrow_idx.begin(); itr != escrow_idx.end(); ++itr )
+  {
+    total_hive += itr->get_hive_balance();
+    total_hbd += itr->get_hbd_balance();
+
+    if( itr->get_fee().symbol == HIVE_SYMBOL )
+      total_hive += itr->get_fee();
+    else
+    {
+      FC_ASSERT( itr->get_fee().symbol == HBD_SYMBOL, "found escrow pending fee that is not HBD or HIVE" );
+      total_hbd += itr->get_fee();
+    }
+    ++escrow_count;
+  }
+}
 
 } }
 
