27dc9841a58d05b02d82ef9b10d54f45b9cf7567
Bartek Wrona <bwrona@syncad.com>
2025-12-14 09:38:22 +0000

    Move witness-related methods from database.cpp to index-04.cpp
    Move methods that use witness_index or witness_vote_index multiindex:
    - get_witness
    - adjust_witness_votes
    - adjust_witness_vote
    - clear_witness_votes
    - retally_witness_votes
    - retally_witness_vote_counts
    
    This reduces compilation time when database.cpp changes since these
    methods are now in a separate compilation unit with witness multiindex.

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -366,13 +366,6 @@ void database::set_chain_id( const chain_id_type& chain_id )
 #endif
 }
 
-const witness_object& database::get_witness( const account_name_type& name ) const
-{ try {
-  const auto* _witness = find_witness( name );
-  FC_ASSERT( _witness != nullptr, "Witness ${w} doesn't exist", ("w", name) );
-  return *_witness;
-} FC_CAPTURE_AND_RETHROW( (name) ) }
-
 std::string database::get_treasury_name( uint32_t hardfork ) const
 {
   if( hardfork >= HIVE_HARDFORK_1_24_TREASURY_RENAME )
@@ -1186,62 +1179,6 @@ void database::nullify_proxied_witness_votes( const account_object& a )
   for( int i = 0; i < HIVE_MAX_PROXY_RECURSION_DEPTH; ++i )
     delta[ i + 1 ] = -a.proxied_vsf_votes[ i ];
   adjust_proxied_witness_votes( a, delta );
-}
-
-void database::adjust_witness_votes( const account_object& a, const share_type& delta )
-{
-  const auto& vidx = get_index< witness_vote_index >().indices().get< by_account_witness >();
-  auto itr = vidx.lower_bound( boost::make_tuple( a.get_name(), account_name_type() ) );
-  while( itr != vidx.end() && itr->account == a.get_name() )
-  {
-    adjust_witness_vote( get< witness_object, by_name >(itr->witness), delta );
-    ++itr;
-  }
-}
-
-void database::adjust_witness_vote( const witness_object& witness, share_type delta )
-{
-  const witness_schedule_object& wso = has_hardfork(HIVE_HARDFORK_1_27_FIX_TIMESHARE_WITNESS_SCHEDULING) ?
-                                       get_witness_schedule_object_for_irreversibility() : get_witness_schedule_object();
-  modify( witness, [&]( witness_object& w )
-  {
-    auto delta_pos = w.votes.value * (wso.current_virtual_time - w.virtual_last_update);
-    w.virtual_position += delta_pos;
-
-    w.virtual_last_update = wso.current_virtual_time;
-    w.votes += delta;
-    FC_ASSERT( w.votes <= get_dynamic_global_properties().total_vesting_shares.amount, "", ("w.votes", w.votes)("props",get_dynamic_global_properties().total_vesting_shares) );
-
-    if( has_hardfork( HIVE_HARDFORK_0_2 ) )
-      w.virtual_scheduled_time = w.virtual_last_update + (HIVE_VIRTUAL_SCHEDULE_LAP_LENGTH2 - w.virtual_position)/(w.votes.value+1);
-    else
-      w.virtual_scheduled_time = w.virtual_last_update + (HIVE_VIRTUAL_SCHEDULE_LAP_LENGTH - w.virtual_position)/(w.votes.value+1);
-
-    /** witnesses with a low number of votes could overflow the time field and end up with a scheduled time in the past */
-    if( has_hardfork( HIVE_HARDFORK_0_4 ) )
-    {
-      if( w.virtual_scheduled_time < wso.current_virtual_time )
-        w.virtual_scheduled_time = fc::uint128_max_value();
-    }
-  } );
-}
-
-void database::clear_witness_votes( const account_object& a )
-{
-  const auto& vidx = get_index< witness_vote_index >().indices().get<by_account_witness>();
-  auto itr = vidx.lower_bound( boost::make_tuple( a.get_name(), account_name_type() ) );
-  while( itr != vidx.end() && itr->account == a.get_name() )
-  {
-    const auto& current = *itr;
-    ++itr;
-    remove(current);
-  }
-
-  if( has_hardfork( HIVE_HARDFORK_0_6__104 ) )
-    modify( a, [&](account_object& acc )
-    {
-      acc.witnesses_voted_for = 0;
-    });
 }
 
 bool database::collect_account_total_balance( const account_object& account, asset* total_hive, asset* total_hbd,
@@ -4585,68 +4522,6 @@ FC_TODO( "Supplement ^ once SMT rewards are implemented" )
   FC_CAPTURE_LOG_AND_RETHROW( (head_block_num()) );
 }
 #endif
-
-void database::retally_witness_votes()
-{
-  const auto& witness_idx = get_index< witness_index >().indices();
-
-  // Clear all witness votes
-  for( auto itr = witness_idx.begin(); itr != witness_idx.end(); ++itr )
-  {
-    modify( *itr, [&]( witness_object& w )
-    {
-      w.votes = 0;
-      w.virtual_position = 0;
-    } );
-  }
-
-  const auto& account_idx = get_index< account_index >().indices();
-
-  // Apply all existing votes by account
-  for( auto itr = account_idx.begin(); itr != account_idx.end(); ++itr )
-  {
-    if( itr->has_proxy() ) continue;
-
-    const auto& a = *itr;
-
-    const auto& vidx = get_index<witness_vote_index>().indices().get<by_account_witness>();
-    auto wit_itr = vidx.lower_bound( boost::make_tuple( a.get_name(), account_name_type() ) );
-    while( wit_itr != vidx.end() && wit_itr->account == a.get_name() )
-    {
-      adjust_witness_vote( get< witness_object, by_name >(wit_itr->witness), a.get_governance_vote_power() );
-      ++wit_itr;
-    }
-  }
-}
-
-void database::retally_witness_vote_counts( bool force )
-{
-  const auto& account_idx = get_index< account_index >().indices();
-
-  // Check all existing votes by account
-  for( auto itr = account_idx.begin(); itr != account_idx.end(); ++itr )
-  {
-    const auto& a = *itr;
-    uint16_t witnesses_voted_for = 0;
-    if( force || a.has_proxy() )
-    {
-      const auto& vidx = get_index< witness_vote_index >().indices().get< by_account_witness >();
-      auto wit_itr = vidx.lower_bound( boost::make_tuple( a.get_name(), account_name_type() ) );
-      while( wit_itr != vidx.end() && wit_itr->account == a.get_name() )
-      {
-        ++witnesses_voted_for;
-        ++wit_itr;
-      }
-    }
-    if( a.witnesses_voted_for != witnesses_voted_for )
-    {
-      modify( a, [&]( account_object& account )
-      {
-        account.witnesses_voted_for = witnesses_voted_for;
-      } );
-    }
-  }
-}
 
 void database::remove_expired_governance_votes()
 {
--- libraries/chain/index-04.cpp
+++ libraries/chain/index-04.cpp
@@ -1,4 +1,5 @@
 #include <hive/chain/witness_objects_multiindex.hpp>
+#include <hive/chain/account_object_multiindex.hpp>
 
 #include <hive/chain/index.hpp>
 #include <chainbase/chainbase.inl>
@@ -38,6 +39,130 @@ const witness_schedule_object& database::get_future_witness_schedule_object() co
   }
 }
 
+const witness_object& database::get_witness( const account_name_type& name ) const
+{ try {
+  const auto* _witness = find_witness( name );
+  FC_ASSERT( _witness != nullptr, "Witness ${w} doesn't exist", ("w", name) );
+  return *_witness;
+} FC_CAPTURE_AND_RETHROW( (name) ) }
+
+void database::adjust_witness_votes( const account_object& a, const share_type& delta )
+{
+  const auto& vidx = get_index< witness_vote_index >().indices().get< by_account_witness >();
+  auto itr = vidx.lower_bound( boost::make_tuple( a.get_name(), account_name_type() ) );
+  while( itr != vidx.end() && itr->account == a.get_name() )
+  {
+    adjust_witness_vote( get< witness_object, by_name >(itr->witness), delta );
+    ++itr;
+  }
+}
+
+void database::adjust_witness_vote( const witness_object& witness, share_type delta )
+{
+  const witness_schedule_object& wso = has_hardfork(HIVE_HARDFORK_1_27_FIX_TIMESHARE_WITNESS_SCHEDULING) ?
+                                       get_witness_schedule_object_for_irreversibility() : get_witness_schedule_object();
+  modify( witness, [&]( witness_object& w )
+  {
+    auto delta_pos = w.votes.value * (wso.current_virtual_time - w.virtual_last_update);
+    w.virtual_position += delta_pos;
+
+    w.virtual_last_update = wso.current_virtual_time;
+    w.votes += delta;
+    FC_ASSERT( w.votes <= get_dynamic_global_properties().total_vesting_shares.amount, "", ("w.votes", w.votes)("props",get_dynamic_global_properties().total_vesting_shares) );
+
+    if( has_hardfork( HIVE_HARDFORK_0_2 ) )
+      w.virtual_scheduled_time = w.virtual_last_update + (HIVE_VIRTUAL_SCHEDULE_LAP_LENGTH2 - w.virtual_position)/(w.votes.value+1);
+    else
+      w.virtual_scheduled_time = w.virtual_last_update + (HIVE_VIRTUAL_SCHEDULE_LAP_LENGTH - w.virtual_position)/(w.votes.value+1);
+
+    /** witnesses with a low number of votes could overflow the time field and end up with a scheduled time in the past */
+    if( has_hardfork( HIVE_HARDFORK_0_4 ) )
+    {
+      if( w.virtual_scheduled_time < wso.current_virtual_time )
+        w.virtual_scheduled_time = fc::uint128_max_value();
+    }
+  } );
+}
+
+void database::clear_witness_votes( const account_object& a )
+{
+  const auto& vidx = get_index< witness_vote_index >().indices().get<by_account_witness>();
+  auto itr = vidx.lower_bound( boost::make_tuple( a.get_name(), account_name_type() ) );
+  while( itr != vidx.end() && itr->account == a.get_name() )
+  {
+    const auto& current = *itr;
+    ++itr;
+    remove(current);
+  }
+
+  if( has_hardfork( HIVE_HARDFORK_0_6__104 ) )
+    modify( a, [&](account_object& acc )
+    {
+      acc.witnesses_voted_for = 0;
+    });
+}
+
+void database::retally_witness_votes()
+{
+  const auto& witness_idx = get_index< witness_index >().indices();
+
+  // Clear all witness votes
+  for( auto itr = witness_idx.begin(); itr != witness_idx.end(); ++itr )
+  {
+    modify( *itr, [&]( witness_object& w )
+    {
+      w.votes = 0;
+      w.virtual_position = 0;
+    } );
+  }
+
+  const auto& account_idx = get_index< account_index >().indices();
+
+  // Apply all existing votes by account
+  for( auto itr = account_idx.begin(); itr != account_idx.end(); ++itr )
+  {
+    if( itr->has_proxy() ) continue;
+
+    const auto& a = *itr;
+
+    const auto& vidx = get_index<witness_vote_index>().indices().get<by_account_witness>();
+    auto wit_itr = vidx.lower_bound( boost::make_tuple( a.get_name(), account_name_type() ) );
+    while( wit_itr != vidx.end() && wit_itr->account == a.get_name() )
+    {
+      adjust_witness_vote( get< witness_object, by_name >(wit_itr->witness), a.get_governance_vote_power() );
+      ++wit_itr;
+    }
+  }
+}
+
+void database::retally_witness_vote_counts( bool force )
+{
+  const auto& account_idx = get_index< account_index >().indices();
+
+  // Check all existing votes by account
+  for( auto itr = account_idx.begin(); itr != account_idx.end(); ++itr )
+  {
+    const auto& a = *itr;
+    uint16_t witnesses_voted_for = 0;
+    if( force || a.has_proxy() )
+    {
+      const auto& vidx = get_index< witness_vote_index >().indices().get< by_account_witness >();
+      auto wit_itr = vidx.lower_bound( boost::make_tuple( a.get_name(), account_name_type() ) );
+      while( wit_itr != vidx.end() && wit_itr->account == a.get_name() )
+      {
+        ++witnesses_voted_for;
+        ++wit_itr;
+      }
+    }
+    if( a.witnesses_voted_for != witnesses_voted_for )
+    {
+      modify( a, [&]( account_object& account )
+      {
+        account.witnesses_voted_for = witnesses_voted_for;
+      } );
+    }
+  }
+}
 
 } }
 
