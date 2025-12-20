e98a46f4742ca813f8e384d9d0ef31587ba6c55e
Dan Notestein <dan@syncad.com>
2025-12-20 05:02:31 -0500

    Convert libraries/fc to git submodule
    - Extract fc library to separate hive/fc repository
    - fc now manages its own vendor submodules (secp256k1-zkp, websocketpp)
    - Preserves 332 commits of fc history in the new repo
    - Tagged as fc-v1.0.0 in the fc repository
    
    This enables fc to be shared across multiple projects (hive, beekeeper)
    without code duplication.

--- libraries/fc/include/fc/network/have_so_reuseport.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace fc { namespace detail {
-#if defined( __APPLE__ ) || defined( __linux__ )
-  static bool have_so_reuseport = true;
-#endif
-}} // namespace fc::detail
--- libraries/fc/tests/all_tests.cpp
+++
@@ -1,3 +0,0 @@
-#define BOOST_TEST_MODULE AllTests
-#include <boost/test/unit_test.hpp>
-
