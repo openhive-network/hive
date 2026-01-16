9ecbcf79aaec7f396e5f9c546fc4e0df3ed2bd2b
Mariusz Trela <mtrela@syncad.com>
2026-01-16 12:47:13 +0100

    Remove duplicate account_metadata_object from chain library
    The account_metadata_object was moved to the metadata plugin but its
    definition remained in the chain library, causing "type_id is already
    in use" error at runtime when the metadata plugin was enabled. Remove
    the duplicate definition and index registration from the chain library.

--- libraries/chain/database_account.cpp
+++ libraries/chain/database_account.cpp
@@ -12,14 +12,12 @@ void initialize_core_indexes_01( database& db )
 {
   HIVE_ADD_CORE_INDEX(db, dynamic_global_property_index);
   HIVE_ADD_CORE_INDEX(db, account_index);
-  HIVE_ADD_CORE_INDEX(db, account_metadata_index);
 }
 
 } }
 
 HIVE_DEFINE_TYPE_REGISTRAR_REGISTER_TYPE(hive::chain::dynamic_global_property_index)
 HIVE_DEFINE_TYPE_REGISTRAR_REGISTER_TYPE(hive::chain::account_index)
-HIVE_DEFINE_TYPE_REGISTRAR_REGISTER_TYPE(hive::chain::account_metadata_index)
 
 // Explicit template instantiations for chainbase::database methods
 template const chainbase::generic_index<hive::chain::dynamic_global_property_index>& chainbase::database::get_index<hive::chain::dynamic_global_property_index>() const;
@@ -27,6 +25,3 @@ template chainbase::generic_index<hive::chain::dynamic_global_property_index>& c
 
 template const chainbase::generic_index<hive::chain::account_index>& chainbase::database::get_index<hive::chain::account_index>() const;
 template chainbase::generic_index<hive::chain::account_index>& chainbase::database::get_mutable_index<hive::chain::account_index>();
-
-template const chainbase::generic_index<hive::chain::account_metadata_index>& chainbase::database::get_index<hive::chain::account_metadata_index>() const;
-template chainbase::generic_index<hive::chain::account_metadata_index>& chainbase::database::get_mutable_index<hive::chain::account_metadata_index>();
