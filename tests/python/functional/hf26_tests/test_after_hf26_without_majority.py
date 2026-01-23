404cf8d35f1534a9349075c8f7702a997994a4da
ABW <andrzejl@syncad.com>
2026-01-23 15:25:47 +0000

    fix test checking authority before HF26
    The check on HF26 was removed. It is now possible to use hf26 pack even before HF26.
    The change only affects testnet (and mirrornet if it was reconverted) making it easier (you can always use hf26 pack instead of switching between packs depending on HF)

--- tests/python/functional/hf26_tests/test_after_hf26_without_majority.py
+++
@@ -1,5 +0,0 @@
-# def test_after_hf26_without_majority(network_after_hf26_without_majority):
-#     wallet_legacy, wallet_hf26 = prepare_wallets(network_after_hf26_without_majority)
-
-#     legacy_operation_passed(wallet_legacy)
-#     hf26_operation_failed(wallet_hf26)
--- tests/python/functional/hf26_tests/test_before_hf26.py
+++ tests/python/functional/hf26_tests/test_before_hf26.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from hive_local_tools.functional.python.hf26 import hf26_operation_failed, legacy_operation_passed, prepare_wallets
+from hive_local_tools.functional.python.hf26 import hf26_operation_passed, legacy_operation_passed, prepare_wallets
 
 if TYPE_CHECKING:
     import test_tools as tt
@@ -12,4 +12,4 @@ def test_before_hf26(node_hf25: tt.InitNode) -> None:
     wallet_legacy, wallet_hf26 = prepare_wallets(node_hf25)
 
     legacy_operation_passed(wallet_legacy)
-    hf26_operation_failed(wallet_hf26)
+    hf26_operation_passed(wallet_hf26)
