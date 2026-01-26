openhive-network/hive

commit 3af2709be1c169abc5a787ec0747d88b4ca82488

Introduce `encrypt_data`, `decrypt_data` API endpoints into network/WASM beekeepers

libraries/protocol/crypto_memo.cpp
@@ -1,0 +1,35 @@
+#include <hive/protocol/crypto_memo.hpp>
+
+namespace hive { namespace protocol {
+
+std::string crypto_memo::encrypt( const crypto_data::private_key_type& from, const crypto_data::public_key_type& to, const std::string& memo )
+{
+  FC_ASSERT( memo.size() > 0 && memo[0] == marker );
+  content _c = encrypt_impl( from, to, memo.substr(1) );
+
+  memo_content _result;
+  _result.check = _c.check;
+  _result.nonce = _c.nonce;
+  _result.encrypted = std::move( _c.encrypted );
+  _result.from = from.get_public_key();
+  _result.to = to;
+
+  return marker + to_string_impl( _result );
+}
+
+std::string crypto_memo::decrypt( key_finder_type key_finder, const std::string& encrypted_memo )
+{
+  FC_ASSERT( encrypted_memo.size() > 0 && encrypted_memo[0] == marker );
+
+  auto _c = from_string_impl<memo_content>( encrypted_memo.substr(1) );
+  if( !_c )
+    return encrypted_memo;
+
+  auto _result = decrypt_impl( key_finder, _c->from, _c->to, _c.value() );
+  if( !_result.has_value() )
+    return encrypted_memo;
+
+  return _result.value();
+}
+
+} } // hive::protocol

libraries/protocol/include/hive/protocol/crypto_memo.hpp
@@ -1,0 +1,42 @@
+#pragma once
+
+#include <fc/crypto/crypto_data.hpp>
+
+namespace hive { namespace protocol {
+
+using crypto_data = fc::crypto_data;
+
+class crypto_memo: public crypto_data
+{
+  public:
+
+    struct memo_content: public content
+    {
+      public_key_type   from;
+      public_key_type   to;
+    };
+
+  private:
+
+    const char marker = '#';
+
+  protected:
+
+  public:
+
+    ~crypto_memo() override = default;
+
+    /**
+     * Encrypts given memo
+     */
+    std::string encrypt( const crypto_data::private_key_type& from, const crypto_data::public_key_type& to, const std::string& data );
+
+    /**
+     * Decrypts given memo (it has to start with #).
+     */
+    std::string decrypt( key_finder_type key_finder, const std::string& encrypted_data );
+};
+
+} } // hive::protocol
+
+FC_REFLECT_DERIVED( hive::protocol::crypto_memo::memo_content, (fc::crypto_data::content), (from)(to) )

libraries/fc/CMakeLists.txt
@@ -195,6 +195,7 @@ set( fc_minimal_sources
      src/crypto/base58.cpp
      src/crypto/city.cpp
      src/crypto/crc.cpp
+     src/crypto/crypto_data.cpp
      src/io/json.cpp
      src/io/datastream.cpp
      src/io/sstream.cpp

libraries/fc/include/fc/crypto/crypto_data.hpp
@@ -1,0 +1,72 @@
+#pragma once
+
+#include <fc/crypto/elliptic.hpp>
+#include <fc/crypto/base58.hpp>
+#include <fc/io/raw.hpp>
+
+namespace fc {
+
+class crypto_data
+{
+  public:
+
+    using private_key_type  = fc::ecc::private_key;
+    using public_key_type   = fc::ecc::public_key;
+
+    using key_finder_type = std::function<fc::optional<crypto_data::private_key_type>( const crypto_data::public_key_type& )>;
+
+    struct content
+    {
+      uint64_t          nonce = 0;
+      uint32_t          check = 0;
+      std::vector<char> encrypted;
+    };
+
+  private:
+
+    fc::sha512 generate_encrypted_key( uint64_t nonce, const fc::sha512& shared_secret );
+
+  protected:
+
+    template<typename ContentType>
+    std::optional<ContentType> from_string_impl( const std::string& encrypted_data )
+    {
+      try
+      {
+        auto data = fc::from_base58( encrypted_data );
+        ContentType _c;
+        fc::raw::unpack_from_vector( data, _c );
+        FC_ASSERT( to_string_impl( _c ) == encrypted_data );
+        return _c;
+      }
+      catch( ... ) {}
+      return std::optional<ContentType>();
+    }
+
+    template<typename ContentType>
+    std::string to_string_impl( const ContentType& data )
+    {
+      return fc::to_base58( fc::raw::pack_to_vector( data ) );
+    }
+
+    content encrypt_impl( const crypto_data::private_key_type& from, const crypto_data::public_key_type& to, const std::string& data );
+    std::optional<std::string> decrypt_impl( key_finder_type key_finder, const crypto_data::public_key_type& from, const crypto_data::public_key_type& to, const content& encrypted_content );
+
+  public:
+
+    virtual ~crypto_data() = default;
+
+    /**
+     * Encrypts given data
+     */
+    std::string encrypt( const crypto_data::private_key_type& from, const crypto_data::public_key_type& to, const std::string& data );
+
+    /**
+     * Decrypts given data.
+     */
+    std::string decrypt( key_finder_type key_finder, const crypto_data::public_key_type& from, const crypto_data::public_key_type& to, const std::string& encrypted_data );
+};
+
+}//fc
+
+FC_REFLECT( fc::crypto_data::content, (nonce)(check)(encrypted) )

libraries/fc/src/crypto/crypto_data.cpp
@@ -1,0 +1,89 @@
+#include <fc/crypto/crypto_data.hpp>
+
+#include <fc/crypto/aes.hpp>
+
+namespace fc {
+
+fc::sha512 crypto_data::generate_encrypted_key( uint64_t nonce, const fc::sha512& shared_secret )
+{
+  fc::sha512::encoder enc;
+  fc::raw::pack( enc, nonce );
+  fc::raw::pack( enc, shared_secret );
+  return enc.result();
+}
+
+crypto_data::content crypto_data::encrypt_impl( const crypto_data::private_key_type& from, const crypto_data::public_key_type& to, const std::string& data )
+{
+  content _c;
+
+  _c.nonce = fc::time_point::now().time_since_epoch().count();
+
+  auto shared_secret = from.get_shared_secret( to );
+
+  auto _encrypt_key = generate_encrypted_key( _c.nonce, shared_secret );
+
+  _c.encrypted = fc::aes_encrypt( _encrypt_key, fc::raw::pack_to_vector( data ) );
+  _c.check = fc::sha256::hash( _encrypt_key )._hash[0];
+
+  return _c;
+}
+
+std::string crypto_data::encrypt( const crypto_data::private_key_type& from, const crypto_data::public_key_type& to, const std::string& data )
+{
+  content _result = encrypt_impl( from, to, data );
+  return to_string_impl( _result );
+}
+
+std::optional<std::string> crypto_data::decrypt_impl( key_finder_type key_finder,
+                                                      const crypto_data::public_key_type& from, const crypto_data::public_key_type& to,
+                                                      const content& encrypted_content )
+{
+  fc::sha512 _shared_secret;
+
+  auto _from_key = key_finder( from );
+  if( !_from_key )
+  {
+    auto _to_key = key_finder( to );
+    if( !_to_key )
+      return std::optional<std::string>();
+    _shared_secret = _to_key->get_shared_secret( from );
+  }
+  else
+  {
+    _shared_secret = _from_key->get_shared_secret( to );
+  }
+
+  auto _encryption_key = generate_encrypted_key( encrypted_content.nonce, _shared_secret );
+
+  uint32_t _check = fc::sha256::hash( _encryption_key )._hash[0];
+  if( _check != encrypted_content.check )
+    return std::optional<std::string>();
+
+  try
+  {
+    std::vector<char> _decrypted = fc::aes_decrypt( _encryption_key, encrypted_content.encrypted );
+    std::string _decrypted_string;
+    fc::raw::unpack_from_vector( _decrypted, _decrypted_string );
+    return _decrypted_string;
+  }
+  catch( ... ) {}
+
+  return std::optional<std::string>();
+}
+
+std::string crypto_data::decrypt( key_finder_type key_finder,
+                                  const crypto_data::public_key_type& from, const crypto_data::public_key_type& to,
+                                  const std::string& encrypted_data )
+{
+  auto _c = from_string_impl<content>( encrypted_data );
+  if( !_c )
+    FC_ASSERT( false, "Decryption failed");
+
+  auto _result = decrypt_impl( key_finder, from, to, _c.value() );
+  if( !_result.has_value() )
+    FC_ASSERT( false, "Decryption failed");
+
+  return _result.value();
+}
+
+}// fc

libraries/protocol/CMakeLists.txt
@@ -44,6 +44,7 @@ SET( SOURCES testnet_blockchain_configuration.cpp
    misc_utilities.cpp
    forward_impacted.cpp
    fixed_string.cpp
+   crypto_memo.cpp
    ${GET_CONFIG_CPP}
    ${HEADERS}
    ${hardfork_hpp_file}

libraries/protocol/include/hive/protocol/transaction_util.hpp
@@ -83,28 +83,5 @@ void verify_authority(const vector<AuthContainerType>& auth_containers,
  */
 void collect_potential_keys( std::vector< public_key_type >* keys, const account_name_type& account, const std::string& str );
 
-struct memo_data
-{
-  public_key_type from;
-  public_key_type to;
-  uint64_t        nonce = 0;
-  uint32_t        check = 0;
-  vector<char>    encrypted;
-
-  static std::optional<memo_data> from_string( std::string str );
-  operator std::string() const;
-};
-
-/**
- * Encrypts given memo (it has to start with #).
- */
-std::string encrypt_memo( const private_key_type& from_priv, const public_key_type& to_key, std::string memo );
-
-/**
- * Decrypts given memo (it has to start with #).
- */
-std::string decrypt_memo( std::function<fc::optional<private_key_type>( const public_key_type& )> key_finder, std::string encrypted_memo );
-
 } } // hive::protocol
 
-FC_REFLECT( hive::protocol::memo_data, (from)(to)(nonce)(check)(encrypted) )

libraries/protocol/transaction_util.cpp
@@ -1,8 +1,5 @@
 #include <hive/protocol/transaction_util.hpp>
 
-#include <fc/crypto/aes.hpp>
-#include <fc/crypto/base58.hpp>
-
 namespace hive { namespace protocol {
 
 void verify_authority(const required_authorities_type& required_authorities, 
@@ -137,92 +134,6 @@ void collect_potential_keys( std::vector< public_key_type >* keys,
   keys->push_back( generate_key( "active" ) );
   keys->push_back( generate_key( "posting" ) );
   keys->push_back( generate_key( "memo" ) );
-}
-
-std::optional<memo_data> memo_data::from_string( std::string str )
-{
-  try
-  {
-    if( str.size() > sizeof( memo_data ) && str[0] == '#' )
-    {
-      auto data = fc::from_base58( str.substr( 1 ) );
-      memo_data m;
-      fc::raw::unpack_from_vector( data, m );
-      FC_ASSERT( string( m ) == str );
-      return m;
-    }
-  }
-  catch( ... ) {}
-  return std::optional<memo_data>();
-}
-
-memo_data::operator std::string() const
-{
-  auto data = fc::raw::pack_to_vector( *this );
-  auto base58 = fc::to_base58( data );
-  return '#' + base58;
-}
-
-std::string encrypt_memo( const private_key_type& from_priv, const public_key_type& to_key, std::string memo )
-{
-  FC_ASSERT( memo.size() > 0 && memo[0] == '#' );
-  memo_data m;
-
-  m.from = from_priv.get_public_key();
-  m.to = to_key;
-  m.nonce = fc::time_point::now().time_since_epoch().count();
-
-  auto shared_secret = from_priv.get_shared_secret( m.to );
-
-  fc::sha512::encoder enc;
-  fc::raw::pack( enc, m.nonce );
-  fc::raw::pack( enc, shared_secret );
-  auto encrypt_key = enc.result();
-
-  m.encrypted = fc::aes_encrypt( encrypt_key, fc::raw::pack_to_vector( memo.substr( 1 ) ) );
-  m.check = fc::sha256::hash( encrypt_key )._hash[0];
-  return m;
-}
-
-std::string decrypt_memo( std::function< fc::optional<private_key_type>( const public_key_type& )> key_finder, std::string encrypted_memo )
-{
-  FC_ASSERT( encrypted_memo.size() > 0 && encrypted_memo[0] == '#' );
-  auto m = memo_data::from_string( encrypted_memo );
-  if( !m )
-    return encrypted_memo;
-
-  fc::sha512 shared_secret;
-  auto from_key = key_finder( m->from );
-  if( !from_key )
-  {
-    auto to_key = key_finder( m->to );
-    if( !to_key )
-      return encrypted_memo;
-    shared_secret = to_key->get_shared_secret( m->from );
-  }
-  else
-  {
-    shared_secret = from_key->get_shared_secret( m->to );
-  }
-  fc::sha512::encoder enc;
-  fc::raw::pack( enc, m->nonce );
-  fc::raw::pack( enc, shared_secret );
-  auto encryption_key = enc.result();
-
-  uint32_t check = fc::sha256::hash( encryption_key )._hash[0];
-  if( check != m->check )
-    return encrypted_memo;
-
-  try
-  {
-    vector<char> decrypted = fc::aes_decrypt( encryption_key, m->encrypted );
-    std::string decrypted_string;
-    fc::raw::unpack_from_vector( decrypted, decrypted_string );
-    return decrypted_string;
-  }
-  catch( ... ) {}
-
-  return encrypted_memo;
 }
 
 } } // hive::protocol

commit 08e3455d8d0db01b00ef5f1bbcd1ef15ac0a2925

Add dual-format detection for encrypted memo decryption
Support decryption of memos encrypted with both the correct format
(from, to, nonce, check, encrypted) and the legacy broken format
(nonce, check, encrypted, from, to) introduced in commit 3af2709be.

During decryption, the code first attempts to parse and decrypt using
the current correct format. If that fails, it falls back to trying the
legacy format. This ensures backward compatibility with any memos that
were encrypted during the period when the broken version was in use.

Encryption always uses the correct format for forward compatibility.

libraries/protocol/crypto_memo.cpp
@@ -22,6 +22,19 @@ std::optional<crypto_memo::memo_content> crypto_memo::load_from_string( const st
   return from_string_impl<memo_content>( data.substr(1) );
 }
 
+std::optional<crypto_memo::memo_content> crypto_memo::load_from_string_buggy_format( const std::string& data )
+{
+  // Defensive check - protects against future refactoring that might call this directly
+  if( data.empty() || data[0] != marker )
+    return std::optional<memo_content>();
+
+  auto buggy = from_string_impl<memo_content_buggy_format>( data.substr(1) );
+  if( !buggy )
+    return std::optional<memo_content>();
+  // Convert buggy format to current format
+  return memo_content{ buggy->from, buggy->to, buggy->nonce, buggy->check, std::move( buggy->encrypted ) };
+}
+
 std::string crypto_memo::dump_to_string( const memo_content& content )
 {
   return marker + to_string_impl( content );
@@ -35,15 +48,36 @@ std::string crypto_memo::encrypt( const crypto_data::private_key_type& from, con
 
 std::string crypto_memo::decrypt( key_finder_type key_finder, const std::string& encrypted_memo )
 {
-  auto _c = load_from_string( encrypted_memo );
-  if( !_c )
-    return encrypted_memo;
+  try
+  {
+    // Try current format first (from, to, nonce, check, encrypted)
+    auto _c = load_from_string( encrypted_memo );
+    if( _c )
+    {
+      auto _result = decrypt_impl( key_finder, _c->from, _c->to, { _c->nonce, _c->check, std::move( _c->encrypted ) } );
+      if( _result.has_value() )
+        return _result.value();
+      // Correct format parsed successfully but decryption failed (wrong key) - return original
+      // Don't try buggy format as it would misinterpret correctly-formatted data
+      return encrypted_memo;
+    }
 
-  auto _result = decrypt_impl( key_finder, _c->from, _c->to, { _c->nonce, _c->check, _c->encrypted } );
-  if( !_result.has_value() )
-    return encrypted_memo;
+    // Current format parsing failed - try buggy format (nonce, check, encrypted, from, to)
+    // This handles memos encrypted with the broken version from commit 3af2709be (versions 1.27.5-1.28.6)
+    auto _c_buggy = load_from_string_buggy_format( encrypted_memo );
+    if( _c_buggy )
+    {
+      auto _result = decrypt_impl( key_finder, _c_buggy->from, _c_buggy->to, { _c_buggy->nonce, _c_buggy->check, std::move( _c_buggy->encrypted ) } );
+      if( _result.has_value() )
+        return _result.value();
+    }
+  }
+  catch( ... )
+  {
+    // If parsing or decryption fails for any reason, return original memo unchanged
+  }
 
-  return _result.value();
+  return encrypted_memo;
 }
 
 } } // hive::protocol

libraries/protocol/include/hive/protocol/crypto_memo.hpp
@@ -19,9 +19,22 @@ class crypto_memo: public crypto_data
       std::vector<char> encrypted;
     };
 
+    /// Buggy format with incorrect field order (from commit 3af2709be, versions 1.27.5-1.28.6)
+    /// Only used for decoding memos encrypted with the broken version - NOT a legacy standard
+    struct memo_content_buggy_format
+    {
+      uint64_t          nonce = 0;
+      uint32_t          check = 0;
+      std::vector<char> encrypted;
+      public_key_type   from;
+      public_key_type   to;
+    };
+
   private:
 
     const char marker = '#';
+
+    std::optional<memo_content> load_from_string_buggy_format( const std::string& data );
 
   protected:
 
@@ -49,3 +62,4 @@ class crypto_memo: public crypto_data
 } } // hive::protocol
 
 FC_REFLECT( hive::protocol::crypto_memo::memo_content, (from)(to)(nonce)(check)(encrypted) )
+FC_REFLECT( hive::protocol::crypto_memo::memo_content_buggy_format, (nonce)(check)(encrypted)(from)(to) )