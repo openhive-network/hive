6462e4d84d4762cfb676048ea36247736164e5c6
ABW <andrzejl@syncad.com>
2026-01-30 17:42:03 +0000

    introduce tiny_price<> as potential replacement for use of price

--- libraries/protocol/include/hive/protocol/asset.hpp
+++ libraries/protocol/include/hive/protocol/asset.hpp
@@ -11,10 +11,16 @@ namespace hive { namespace protocol {
   using HIVE_asset = tiny_asset< HIVE_ASSET_NUM_HIVE >;
   using VEST_asset = tiny_asset< HIVE_ASSET_NUM_VESTS >;
 
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  class tiny_price;
+
+  using HBD_price = tiny_price< HIVE_ASSET_NUM_HBD, HIVE_ASSET_NUM_HIVE >;
+  using VEST_price = tiny_price< HIVE_ASSET_NUM_VESTS, HIVE_ASSET_NUM_HIVE >;
+
   struct asset
   {
-    asset( const asset& _asset, asset_symbol_type id )
-      : amount( _asset.amount ), symbol( id ) {}
+    share_type        amount; // TODO: make it private
+    asset_symbol_type symbol; // TODO: make it private
 
     asset( share_type a, asset_symbol_type id )
       : amount( a ), symbol( id ) {}
@@ -22,9 +28,8 @@ namespace hive { namespace protocol {
     asset()
       : amount( 0 ), symbol( HIVE_SYMBOL ) {}
 
-    share_type        amount;
-    asset_symbol_type symbol;
-
+    int64_t get_amount() const { return amount.value; }
+    asset_symbol_type get_symbol() const { return symbol; }
     asset& operator += ( const asset& o )
     {
       FC_ASSERT( symbol == o.symbol && "asset symbol mismatch +=", "${l} != ${r}", ( "l", symbol )( "r", o.symbol ) );
@@ -91,34 +96,33 @@ namespace hive { namespace protocol {
 
   struct legacy_asset
   {
-    public:
-      legacy_asset() {}
+    legacy_asset() {}
 
-      legacy_asset( const asset& a )
-      {
-        amount = a.amount;
-        symbol = a.symbol;
-      }
+    legacy_asset( const asset& a )
+    {
+      amount = a.get_amount();
+      symbol = a.get_symbol();
+    }
 
-      asset to_asset()const
-      {
-        return asset( amount, symbol );
-      }
+    asset to_asset()const
+    {
+      return asset( amount, symbol );
+    }
 
-      operator asset()const { return to_asset(); }
+    operator asset()const { return to_asset(); }
 
-      static legacy_asset from_asset( const asset& a )
-      {
-        return legacy_asset( a );
-      }
+    static legacy_asset from_asset( const asset& a )
+    {
+      return legacy_asset( a );
+    }
 
-      std::string asset_num_to_string() const;
+    std::string asset_num_to_string() const;
 
-      string to_string()const;
-      static legacy_asset from_string( const string& from );
+    string to_string()const;
+    static legacy_asset from_string( const string& from );
 
-      share_type                       amount;
-      asset_symbol_type                symbol = HIVE_SYMBOL;
+    share_type                       amount;
+    asset_symbol_type                symbol = HIVE_SYMBOL;
   };
 
   /** Represents quotation of the relative value of asset against another asset.
@@ -133,6 +137,9 @@ namespace hive { namespace protocol {
   */
   struct price
   {
+    asset base; //TODO: make it private
+    asset quote; //TODO: make it private
+
     /** Even non-single argument, lets make it an explicit one to avoid implicit calls for
         initialization lists.
 
@@ -154,21 +161,30 @@ namespace hive { namespace protocol {
     */
     price() = default;
 
-    asset base;
-    asset quote;
+    static price max( asset_symbol_type base, asset_symbol_type quote )
+    {
+      return price( asset( max_satoshis(), base ), asset( 1, quote ) );
+    }
+    static price min( asset_symbol_type base, asset_symbol_type quote )
+    {
+      return price( asset( 1, base ), asset( max_satoshis(), quote ) );
+    }
 
-    static price max(asset_symbol_type base, asset_symbol_type quote );
-    static price min(asset_symbol_type base, asset_symbol_type quote );
+    price max() const { return price::max( base.get_symbol(), quote.get_symbol() ); }
+    price min() const { return price::min( base.get_symbol(), quote.get_symbol() ); }
 
-    price max()const { return price::max( base.symbol, quote.symbol ); }
-    price min()const { return price::min( base.symbol, quote.symbol ); }
+    bool is_null() const;
+    void validate() const;
+    const asset& get_base() const { return base; }
+    const asset& get_quote() const { return quote; }
 
-    bool is_null()const;
-    void validate()const;
-
+  private:
+    template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+    friend class tiny_price;
+    static share_type max_satoshis();
   }; /// price
 
-  inline price operator~( const price& p ) { return price{p.quote,p.base}; }
+  inline price operator~( const price& p ) { return price{ p.get_quote(), p.get_base() }; }
 
   bool  operator <  ( const asset& a, const asset& b );
   bool  operator <= ( const asset& a, const asset& b );
@@ -200,23 +216,23 @@ namespace hive { namespace protocol {
   template< uint32_t _SYMBOL >
   struct tiny_asset
   {
+    share_type amount; // TODO: make it private
+
     tiny_asset() {}
     explicit tiny_asset( share_type _amount ) : amount( _amount ) {}
-    tiny_asset( const asset& val ) { set( val ); } //to be removed
-    tiny_asset( asset&& val )      { set( val ); } //to be removed
-
-    asset operator=( const asset& val )  { set( val ); return *this; } //to be removed
-    asset operator=( asset&& val )       { set( val ); return *this; } //to be removed
-
-    share_type amount;
+    tiny_asset( const tiny_asset& val ) = default;
+    tiny_asset( const asset& val ) { set( val ); } //to be made explicit
+    tiny_asset& operator=( const tiny_asset& val ) = default;
+    tiny_asset& operator=( const asset& val ) { set( val ); return *this; } //to be removed
 
     tiny_asset& operator+=( const asset& val ) { check( val ); amount += val.amount; return *this; } //to be removed
     tiny_asset& operator-=( const asset& val ) { check( val ); amount -= val.amount; return *this; } //to be removed
 
-    operator asset() const               { return to_asset(); } //to be removed
+    operator asset() const { return to_asset(); } //to be removed
 
     asset to_asset() const { return asset( amount, get_symbol() ); }
     static bool is_compatible( const asset& a ) { return a.symbol.asset_num == _SYMBOL; }
+    int64_t get_amount() const { return amount.value; }
     static asset_symbol_type get_symbol() { return asset_symbol_type::from_asset_num( _SYMBOL ); }
 
     tiny_asset& operator+=( const tiny_asset& val ) { amount += val.amount; return *this; }
@@ -262,29 +278,155 @@ namespace hive { namespace protocol {
     friend tiny_asset operator% ( const tiny_asset& a, share_type b ) { return tiny_asset( a.amount % b ); }
 
   private:
-
     void set( const asset& val ) { check( val ); amount = val.amount; }
     void check( const asset& val ) const { FC_ASSERT( val.symbol.asset_num == _SYMBOL ); }
   };
 
+  /** Represents thin version of price.
+   *  Just like tiny_asset, this one also MUST NOT be used in operations and its
+   *  main purpose is to keep typical prices in form that is both smaller and
+   *  typed enabling compiler to check for errors.
+   */
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  class tiny_price
+  {
+    static_assert( _BASE_SYMBOL != _QUOTE_SYMBOL );
+
+    tiny_asset< _BASE_SYMBOL > base;
+    tiny_asset< _QUOTE_SYMBOL > quote;
+
+  public:
+    tiny_price() {}
+    tiny_price( share_type _base_amount, share_type _quote_amount )
+      : base( _base_amount ), quote( _quote_amount ) { validate(); }
+    tiny_price( const tiny_asset< _BASE_SYMBOL >& _base, const tiny_asset< _QUOTE_SYMBOL >& _quote )
+      : base( _base ), quote( _quote ) { validate(); }
+    tiny_price( const tiny_price& val ) = default;
+    explicit tiny_price( const price& p )
+    {
+      if( is_compatible( p ) )
+      {
+        base = decltype( base )( p.base );
+        quote = decltype( quote )( p.quote );
+      }
+      else // normalize price
+      {
+        base = decltype( base )( p.quote );
+        quote = decltype( quote )( p.base );
+      }
+      validate();
+    }
+
+    tiny_price& operator=( const tiny_price& val ) = default;
+
+    price to_price() const { return price( asset( base ), asset( quote ) ); }
+    static bool is_compatible( const price& p ) { return decltype( base )::is_compatible( p.base ) && decltype( quote )::is_compatible( p.quote ); }
+    static bool is_assignable( const price& p ) { return is_compatible( p ) || ( decltype( base )::is_compatible( p.quote ) && decltype( quote )::is_compatible( p.base ) ); }
+    const decltype( base )& get_base() const { return base; }
+    const decltype( quote )& get_quote() const { return quote; }
+    static asset_symbol_type get_base_symbol() { return decltype( base )::get_symbol(); }
+    static asset_symbol_type get_quote_symbol() { return decltype( quote )::get_symbol(); }
+
+    static tiny_price max() { return tiny_price( price::max_satoshis(), 1); }
+    static tiny_price min() { return tiny_price( 1, price::max_satoshis() ); }
+
+    bool is_null() const { return base.get_amount() == 0 || quote.get_amount() == 0; }
+    void validate() const
+    {
+      try
+      {
+        FC_ASSERT( base.get_amount() > 0 );
+        FC_ASSERT( quote.get_amount() > 0 );
+      } FC_CAPTURE_AND_RETHROW( ( base )( quote ) )
+    }
+
+    friend class fc::reflector<tiny_price>;
+  }; // tiny_price<>
+
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  bool operator==( const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& a,
+                   const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& b )
+  {
+    const uint128_t amult = uint128_t( b.get_quote().get_amount() ) * a.get_base().get_amount();
+    const uint128_t bmult = uint128_t( a.get_quote().get_amount() ) * b.get_base().get_amount();
+    return amult == bmult;
+  }
+
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  bool operator!=( const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& a,
+                   const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& b )
+  {
+    return !( a == b );
+  }
+
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  bool operator<( const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& a,
+                  const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& b )
+  {
+    const uint128_t amult = uint128_t( b.get_quote().get_amount() ) * a.get_base().get_amount();
+    const uint128_t bmult = uint128_t( a.get_quote().get_amount() ) * b.get_base().get_amount();
+    return amult < bmult;
+  }
+
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  bool operator<=( const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& a,
+                   const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& b )
+  {
+    return !( b < a );
+  }
+
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  bool operator>( const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& a,
+                  const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& b )
+  {
+    return b < a;
+  }
+
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  bool operator>=( const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& a,
+                   const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& b )
+  {
+    return !( a < b );
+  }
+
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  tiny_asset< _BASE_SYMBOL > operator* ( const tiny_asset< _QUOTE_SYMBOL >& a, const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& p )
+  {
+    FC_ASSERT( p.get_quote().get_amount() > 0 );
+    bool is_negative = a.get_amount() < 0;
+    uint128_t result( is_negative ? -a.get_amount() : a.get_amount() );
+    result = ( result * p.get_base().get_amount() ) / p.get_quote().get_amount();
+    return tiny_asset< _BASE_SYMBOL >( is_negative ? -fc::uint128_to_uint64( result ) : fc::uint128_to_uint64( result ) );
+  }
+
+  template< uint32_t _BASE_SYMBOL, uint32_t _QUOTE_SYMBOL >
+  tiny_asset< _QUOTE_SYMBOL > operator* ( const tiny_asset< _BASE_SYMBOL >& a, const tiny_price< _BASE_SYMBOL, _QUOTE_SYMBOL >& p )
+  {
+    FC_ASSERT( p.get_base().get_amount() > 0 );
+    bool is_negative = a.get_amount() < 0;
+    uint128_t result( is_negative ? -a.get_amount() : a.get_amount() );
+    result = ( result * p.get_quote().get_amount() ) / p.get_base().get_amount();
+    return tiny_asset< _QUOTE_SYMBOL >( is_negative ? -fc::uint128_to_uint64( result ) : fc::uint128_to_uint64( result ) );
+  }
 
 } } // hive::protocol
 
 namespace fc {
-    void to_variant( const hive::protocol::asset& var,  fc::variant& vo );
-    void from_variant( const fc::variant& var,  hive::protocol::asset& vo );
 
-    void to_variant( const hive::protocol::legacy_asset& a, fc::variant& var );
-    void from_variant( const fc::variant& var, hive::protocol::legacy_asset& a );
+  void to_variant( const hive::protocol::asset& var,  fc::variant& vo );
+  void from_variant( const fc::variant& var,  hive::protocol::asset& vo );
 
-    template < uint32_t _SYMBOL >
-    void to_variant( const hive::protocol::tiny_asset<_SYMBOL>& ta, fc::variant& var )
-    {
-      if( hive::protocol::serialization_mode_controller::legacy_enabled() )
-        to_variant( hive::protocol::legacy_asset( ta.to_asset() ), var );
-      else
-        to_variant( ta.to_asset(), var );
-    }
+  void to_variant( const hive::protocol::legacy_asset& a, fc::variant& var );
+  void from_variant( const fc::variant& var, hive::protocol::legacy_asset& a );
+
+  template < uint32_t _SYMBOL >
+  void to_variant( const hive::protocol::tiny_asset<_SYMBOL>& ta, fc::variant& var )
+  {
+    if( hive::protocol::serialization_mode_controller::legacy_enabled() )
+      to_variant( hive::protocol::legacy_asset( ta.to_asset() ), var );
+    else
+      to_variant( ta.to_asset(), var );
+  }
 }
 
 FC_REFLECT( hive::protocol::asset, (amount)(symbol) )
@@ -295,3 +437,5 @@ FC_REFLECT( hive::protocol::HBD_asset, (amount) )
 FC_REFLECT( hive::protocol::HIVE_asset, (amount) )
 FC_REFLECT( hive::protocol::VEST_asset, (amount) )
 
+FC_REFLECT( hive::protocol::HBD_price, (base)(quote) )
+FC_REFLECT( hive::protocol::VEST_price, (base)(quote) )
