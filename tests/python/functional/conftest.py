135111f286e9c43d44661c386fa1b4a1ca696b87
Michał Kudela <kudmich@syncad.com>
2026-03-03 14:53:03 +0000

    Remove redundant parentheses from @pytest.fixture decorators

--- tests/python/functional/conftest.py
+++ tests/python/functional/conftest.py
@@ -63,20 +63,20 @@ def pytest_sessionstart() -> None:
     logging.getLogger("urllib3.connectionpool").propagate = False
 
 
-@pytest.fixture()
+@pytest.fixture
 def block_logs_for_testing_location() -> Path:
     destination_variable = os.environ.get("TESTING_BLOCK_LOGS_DESTINATION")
     assert destination_variable is not None, "Path TESTING_BLOCK_LOGS_DESTINATION must be set!"
     return Path(destination_variable)
 
 
-@pytest.fixture()
+@pytest.fixture
 def block_log_empty_30_mono(block_logs_for_testing_location: Path) -> tt.BlockLog:
     block_logs_location: Path = block_logs_for_testing_location
     return tt.BlockLog(path=block_logs_location / "empty_block_logs" / "empty_30", mode="monolithic")
 
 
-@pytest.fixture()
+@pytest.fixture
 def block_log_empty_430_split(block_logs_for_testing_location: Path) -> tt.BlockLog:
     block_logs_location: Path = block_logs_for_testing_location
     return tt.BlockLog(path=block_logs_location / "empty_block_logs" / "empty_430", mode="split")
