a4e8257414e7833f53ac2c6c6fc59ba3649ff374
Gandalf <gandalf@storm.pl>
2026-03-27 22:48:03 +0100

    Removed obsolete (unresponsive) seed nodes from default initial seed nodes list.

--- libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/hive/plugins/p2p/p2p_default_seeds.hpp
@@ -12,13 +12,9 @@ const std::vector< std::string > default_seeds = {
   "seed.openhive.network:2001",          // gtg
   "hive-seed.roelandp.nl:2001",          // roelandp
   "hive-seed.arcange.eu:2001",           // arcange
-  "anyx.io:2001",                        // anyx
-  "hived.splinterlands.com:2001",        // splinterlands
   "node.mahdiyari.info:2001",            // mahdiyari
-  "hive-seed.lukestokes.info:2001",      // lukestokes.mhth
   "seed.deathwing.me:2001",              // deathwing
   "hive-seed.actifit.io:2001",           // actifit
-  "seed.shmoogleosukami.co.uk:2001",     // shmoogleosukami
   "hiveseed.rishipanthee.com:2001"       // rishi556
 };
 #endif
